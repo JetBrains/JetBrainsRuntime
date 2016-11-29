9501fd9ac6b5dbc496a30d341716e9f041f9c6c8
Denis Fokin <denis.fokin@jetbrains.com>
2016-11-29 20:17:39 +0300

    IDEA-63779 Keymap shows characters for keys only for english keyboard layout correctly IDEA-27262 Wrong keys are picked up on dvorak layout in Mac OSX 10.6.2
    (cherry picked from commit 07780916770f9214a40068addd32579c10a27a72)

--- jdk/src/java.desktop/macosx/native/libawt_lwawt/awt/AWTEvent.m
+++ jdk/src/java.desktop/macosx/native/libawt_lwawt/awt/AWTEvent.m
@@ -387,7 +387,7 @@ unichar NsCharToJavaChar(unichar nsChar, NSUInteger modifiers, BOOL spaceKeyType
     if (nsChar == 0 && spaceKeyTyped == YES) {
         return java_awt_event_KeyEvent_VK_SPACE;
     }
-	
+
     // otherwise return character unchanged
     return nsChar;
 }
@@ -430,6 +430,96 @@ static unichar NsGetDeadKeyChar(unsigned short keyCode)
         }
     }
     return 0;
+}
+
+static NSDictionary* getDiacriticUnicharToVkCodeDictionary() {
+
+    static NSDictionary* diacriticUnicharToVkCodeDictionary = nil;
+    static dispatch_once_t onceToken;
+
+    dispatch_once(&onceToken, ^{
+         diacriticUnicharToVkCodeDictionary =
+             @{
+                  @"à": @java_awt_event_KeyEvent_VK_A_WITH_GRAVE,
+                  @"á": @java_awt_event_KeyEvent_VK_A_WITH_ACUTE,
+                  @"â": @java_awt_event_KeyEvent_VK_A_WITH_CIRCUMFLEX,
+                  @"ã": @java_awt_event_KeyEvent_VK_A_WITH_TILDE,
+                  @"ä": @java_awt_event_KeyEvent_VK_A_WITH_DIAERESIS,
+                  @"å": @java_awt_event_KeyEvent_VK_A_WITH_RING_ABOVE,
+                  @"æ": @java_awt_event_KeyEvent_VK_AE,
+                  @"ç": @java_awt_event_KeyEvent_VK_C_WITH_CEDILLA,
+                  @"è": @java_awt_event_KeyEvent_VK_E_WITH_GRAVE,
+                  @"é": @java_awt_event_KeyEvent_VK_E_WITH_ACUTE,
+                  @"ê": @java_awt_event_KeyEvent_VK_E_WITH_CIRCUMFLEX,
+                  @"ë": @java_awt_event_KeyEvent_VK_E_WITH_DIAERESIS,
+                  @"ì": @java_awt_event_KeyEvent_VK_I_WITH_GRAVE,
+                  @"í": @java_awt_event_KeyEvent_VK_I_WITH_ACUTE,
+                  @"î": @java_awt_event_KeyEvent_VK_I_WITH_CIRCUMFLEX,
+                  @"ï": @java_awt_event_KeyEvent_VK_I_WITH_DIAERESIS,
+                  @"ð": @java_awt_event_KeyEvent_VK_ETH,
+                  @"ñ": @java_awt_event_KeyEvent_VK_N_WITH_TILDE,
+                  @"ò": @java_awt_event_KeyEvent_VK_O_WITH_GRAVE,
+                  @"ó": @java_awt_event_KeyEvent_VK_O_WITH_ACUTE,
+                  @"ô": @java_awt_event_KeyEvent_VK_O_WITH_CIRCUMFLEX,
+                  @"õ": @java_awt_event_KeyEvent_VK_O_WITH_TILDE,
+                  @"ö": @java_awt_event_KeyEvent_VK_O_WITH_DIAERESIS,
+                  @"÷": @java_awt_event_KeyEvent_VK_DIVISION_SIGN,
+                  @"ø": @java_awt_event_KeyEvent_VK_O_WITH_SLASH,
+                  @"ù": @java_awt_event_KeyEvent_VK_U_WITH_GRAVE,
+                  @"ú": @java_awt_event_KeyEvent_VK_U_WITH_ACUTE,
+                  @"û": @java_awt_event_KeyEvent_VK_U_WITH_CIRCUMFLEX,
+                  @"ü": @java_awt_event_KeyEvent_VK_U_WITH_DIAERESIS,
+                  @"ý": @java_awt_event_KeyEvent_VK_Y_WITH_ACUTE,
+                  @"þ": @java_awt_event_KeyEvent_VK_THORN,
+                  @"ÿ": @java_awt_event_KeyEvent_VK_Y_WITH_DIAERESIS
+             };
+             [diacriticUnicharToVkCodeDictionary retain];
+    });
+    return diacriticUnicharToVkCodeDictionary;
+}
+
+static NSDictionary* getUnicharToVkCodeDictionary() {
+
+    static NSDictionary* unicharToVkCodeDictionary = nil;
+    static dispatch_once_t onceToken;
+
+    dispatch_once(&onceToken, ^{
+         unicharToVkCodeDictionary =
+             @{
+                 @"`" : @java_awt_event_KeyEvent_VK_BACK_QUOTE,
+                 @"1" : @java_awt_event_KeyEvent_VK_1,
+                 @"2" : @java_awt_event_KeyEvent_VK_2,
+                 @"3" : @java_awt_event_KeyEvent_VK_3,
+                 @"4" : @java_awt_event_KeyEvent_VK_4,
+                 @"5" : @java_awt_event_KeyEvent_VK_5,
+                 @"6" : @java_awt_event_KeyEvent_VK_6,
+                 @"7" : @java_awt_event_KeyEvent_VK_7,
+                 @"8" : @java_awt_event_KeyEvent_VK_8,
+                 @"9" : @java_awt_event_KeyEvent_VK_9,
+                 @"0" : @java_awt_event_KeyEvent_VK_0,
+                 @"=" : @java_awt_event_KeyEvent_VK_EQUALS,
+                 @"-" : @java_awt_event_KeyEvent_VK_MINUS,
+                 @"]" : @java_awt_event_KeyEvent_VK_CLOSE_BRACKET,
+                 @"[" : @java_awt_event_KeyEvent_VK_OPEN_BRACKET,
+                 @"\'": @java_awt_event_KeyEvent_VK_QUOTE,
+                 @";" : @java_awt_event_KeyEvent_VK_SEMICOLON,
+                 @"\\": @java_awt_event_KeyEvent_VK_BACK_SLASH,
+                 @"," : @java_awt_event_KeyEvent_VK_COMMA,
+                 @"/" : @java_awt_event_KeyEvent_VK_SLASH,
+                 @"." : @java_awt_event_KeyEvent_VK_PERIOD,
+                 @"." : @java_awt_event_KeyEvent_VK_DECIMAL,
+                 @"*" : @java_awt_event_KeyEvent_VK_MULTIPLY,
+                 @"+" : @java_awt_event_KeyEvent_VK_ADD,
+                 @"/" : @java_awt_event_KeyEvent_VK_DIVIDE,
+                 @"-" : @java_awt_event_KeyEvent_VK_SUBTRACT,
+                 @"=" : @java_awt_event_KeyEvent_VK_EQUALS,
+                 @"," : @java_awt_event_KeyEvent_VK_COMMA,
+                 @"#" : @java_awt_event_KeyEvent_VK_NUMBER_SIGN
+             };
+             [unicharToVkCodeDictionary retain];
+    });
+
+    return unicharToVkCodeDictionary;
 }
 
 /*
@@ -495,9 +585,32 @@ NsCharToJavaVirtualKeyCode(unichar ch, BOOL isDeadChar,
     }
 
     if (key < size) {
+
+        NSDictionary* unicharToVkCodeDictionary = getUnicharToVkCodeDictionary();
+        if ([[NSCharacterSet punctuationCharacterSet] characterIsMember:ch] ||
+             [[NSCharacterSet symbolCharacterSet] characterIsMember:ch])
+        {
+             *keyCode = [unicharToVkCodeDictionary [[NSString stringWithFormat:@"%C",ch]] intValue];
+             // we cannot find key location from a char, so let's use key code
+             *postsTyped = YES;
+             *keyLocation = keyTable[key].javaKeyLocation;
+             return;
+        }
+
+        NSDictionary* diacriticUnicharToVkCodeDictionary = getDiacriticUnicharToVkCodeDictionary();
+        NSNumber * jkc = diacriticUnicharToVkCodeDictionary [[NSString stringWithFormat:@"%C",ch]];
+        if (jkc != nil) {
+            *keyCode = [jkc intValue];
+            // we cannot find key location from a char, so let's use key code
+            *postsTyped = YES;
+            *keyLocation = keyTable[key].javaKeyLocation;
+            return;
+        }
+
         *postsTyped = keyTable[key].postsTyped;
         *keyCode = keyTable[key].javaKeyCode;
         *keyLocation = keyTable[key].javaKeyLocation;
+
     } else {
         // Should we report this? This means we've got a keyboard
         // we don't know about...
@@ -562,10 +675,10 @@ jint NsKeyModifiersToJavaModifiers(NSUInteger nsFlags, BOOL isExtMods)
 {
     jint javaModifiers = 0;
     const struct _nsKeyToJavaModifier* cur;
-	
+
     for (cur = nsKeyToJavaModifierTable; cur->nsMask != 0; ++cur) {
         if ((cur->nsMask & nsFlags) != 0) {
-			
+
             if (cur->nsMask == NSAlternateKeyMask) {
                 if (leftAltKeyPressed == YES) {
                     javaModifiers |= isExtMods? cur->javaExtMask : cur->javaMask;
@@ -767,19 +880,22 @@ JNF_COCOA_EXIT(env);
 /*
  * Class:     sun_lwawt_macosx_NSEvent
  * Method:    nsToJavaChar
- * Signature: (CI)C
+ * Signature: (CI)Ljava/lang/String
  */
-JNIEXPORT jint JNICALL
+JNIEXPORT jstring JNICALL
 Java_sun_lwawt_macosx_NSEvent_nsToJavaChar
 (JNIEnv *env, jclass cls, jchar nsChar, jint modifierFlags, jboolean spaceKeyTyped)
 {
-    jchar javaChar = 0;
+    jstring charAsString = NULL;
 
 JNF_COCOA_ENTER(env);
 
-    javaChar = NsCharToJavaChar(nsChar, modifierFlags, spaceKeyTyped);
+    if ([[NSCharacterSet alphanumericCharacterSet]  characterIsMember:nsChar]) {
+        NSString * nsStr = [NSString stringWithFormat: @"%C", nsChar];
+        charAsString = JNFNSToJavaString(env,  nsStr);
+    }
 
 JNF_COCOA_EXIT(env);
 
-    return javaChar;
+    return charAsString;
 }
--- jdk/src/java.desktop/share/classes/java/awt/event/KeyEvent.java
+++ jdk/src/java.desktop/share/classes/java/awt/event/KeyEvent.java
@@ -30,6 +30,8 @@ import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.HashMap;
+
 import sun.awt.AWTAccessor;
 
 /**
@@ -408,6 +410,41 @@ public class KeyEvent extends InputEvent {
 
     /** Constant for the "Z" key. */
     public static final int VK_Z              = 0x5A;
+
+    public static final int START_OF_LATIN_DIACRITIC_LETTERS = 0x0300;
+
+    public static final int VK_A_WITH_GRAVE = START_OF_LATIN_DIACRITIC_LETTERS + 0xE0;
+    public static final int VK_A_WITH_ACUTE = START_OF_LATIN_DIACRITIC_LETTERS + 0xE1;
+    public static final int VK_A_WITH_CIRCUMFLEX = START_OF_LATIN_DIACRITIC_LETTERS + 0xE2;
+    public static final int VK_A_WITH_TILDE = START_OF_LATIN_DIACRITIC_LETTERS + 0xE3;
+    public static final int VK_A_WITH_DIAERESIS = START_OF_LATIN_DIACRITIC_LETTERS + 0xE4;
+    public static final int VK_A_WITH_RING_ABOVE = START_OF_LATIN_DIACRITIC_LETTERS + 0xE5;
+    public static final int VK_AE = START_OF_LATIN_DIACRITIC_LETTERS + 0xE6;
+    public static final int VK_C_WITH_CEDILLA = START_OF_LATIN_DIACRITIC_LETTERS + 0xE7;
+    public static final int VK_E_WITH_GRAVE = START_OF_LATIN_DIACRITIC_LETTERS + 0xE8;
+    public static final int VK_E_WITH_ACUTE = START_OF_LATIN_DIACRITIC_LETTERS + 0xE9;
+    public static final int VK_E_WITH_CIRCUMFLEX = START_OF_LATIN_DIACRITIC_LETTERS + 0xEA;
+    public static final int VK_E_WITH_DIAERESIS = START_OF_LATIN_DIACRITIC_LETTERS + 0xEB;
+    public static final int VK_I_WITH_GRAVE = START_OF_LATIN_DIACRITIC_LETTERS + 0xEC;
+    public static final int VK_I_WITH_ACUTE = START_OF_LATIN_DIACRITIC_LETTERS + 0xED;
+    public static final int VK_I_WITH_CIRCUMFLEX = START_OF_LATIN_DIACRITIC_LETTERS + 0xEE;
+    public static final int VK_I_WITH_DIAERESIS = START_OF_LATIN_DIACRITIC_LETTERS + 0xEF;
+    public static final int VK_ETH = START_OF_LATIN_DIACRITIC_LETTERS + 0xF0;
+    public static final int VK_N_WITH_TILDE = START_OF_LATIN_DIACRITIC_LETTERS + 0xF1;
+    public static final int VK_O_WITH_GRAVE = START_OF_LATIN_DIACRITIC_LETTERS + 0xF2;
+    public static final int VK_O_WITH_ACUTE = START_OF_LATIN_DIACRITIC_LETTERS + 0xF3;
+    public static final int VK_O_WITH_CIRCUMFLEX = START_OF_LATIN_DIACRITIC_LETTERS + 0xF4;
+    public static final int VK_O_WITH_TILDE = START_OF_LATIN_DIACRITIC_LETTERS + 0xF5;
+    public static final int VK_O_WITH_DIAERESIS = START_OF_LATIN_DIACRITIC_LETTERS + 0xF6;
+    public static final int VK_DIVISION_SIGN = START_OF_LATIN_DIACRITIC_LETTERS + 0xF7;
+    public static final int VK_O_WITH_SLASH = START_OF_LATIN_DIACRITIC_LETTERS + 0xF8;
+    public static final int VK_U_WITH_GRAVE = START_OF_LATIN_DIACRITIC_LETTERS + 0xF9;
+    public static final int VK_U_WITH_ACUTE = START_OF_LATIN_DIACRITIC_LETTERS + 0xFA;
+    public static final int VK_U_WITH_CIRCUMFLEX = START_OF_LATIN_DIACRITIC_LETTERS + 0xFB;
+    public static final int VK_U_WITH_DIAERESIS = START_OF_LATIN_DIACRITIC_LETTERS + 0xFC;
+    public static final int VK_Y_WITH_ACUTE = START_OF_LATIN_DIACRITIC_LETTERS + 0xFD;
+    public static final int VK_THORN = START_OF_LATIN_DIACRITIC_LETTERS + 0xFE;
+    public static final int VK_Y_WITH_DIAERESIS = START_OF_LATIN_DIACRITIC_LETTERS + 0xFF;
 
     /**
      * Constant for the open bracket key, "["
@@ -1363,6 +1400,43 @@ public class KeyEvent extends InputEvent {
         return keyLocation;
     }
 
+    private static final HashMap<Integer, String> asciiCodeToString = new HashMap<Integer, String>() {
+        {
+            put(VK_A_WITH_GRAVE, "\u00e0");
+            put(VK_A_WITH_ACUTE, "\u00e1");
+            put(VK_A_WITH_CIRCUMFLEX, "\u00e2");
+            put(VK_A_WITH_TILDE, "\u00e3");
+            put(VK_A_WITH_DIAERESIS, "\u00e4");
+            put(VK_A_WITH_RING_ABOVE, "\u00e5");
+            put(VK_AE, "\u00e6");
+            put(VK_C_WITH_CEDILLA, "\u00e7");
+            put(VK_E_WITH_GRAVE, "\u00e8");
+            put(VK_E_WITH_ACUTE, "\u00e9");
+            put(VK_E_WITH_CIRCUMFLEX, "\u00ea");
+            put(VK_E_WITH_DIAERESIS, "\u00eb");
+            put(VK_I_WITH_GRAVE, "\u00ec");
+            put(VK_I_WITH_ACUTE, "\u00ed");
+            put(VK_I_WITH_CIRCUMFLEX, "\u00ee");
+            put(VK_I_WITH_DIAERESIS, "\u00ef");
+            put(VK_ETH, "\u00f0");
+            put(VK_N_WITH_TILDE, "\u00f1");
+            put(VK_O_WITH_GRAVE, "\u00f2");
+            put(VK_O_WITH_ACUTE, "\u00f3");
+            put(VK_O_WITH_CIRCUMFLEX, "\u00f4");
+            put(VK_O_WITH_TILDE, "\u00f5");
+            put(VK_O_WITH_DIAERESIS, "\u00f6");
+            put(VK_DIVISION_SIGN, "\u00f7");
+            put(VK_O_WITH_SLASH, "\u00f8");
+            put(VK_U_WITH_GRAVE, "\u00f9");
+            put(VK_U_WITH_ACUTE, "\u00fa");
+            put(VK_U_WITH_CIRCUMFLEX, "\u00fb");
+            put(VK_U_WITH_DIAERESIS, "\u00fc");
+            put(VK_Y_WITH_ACUTE, "\u00fd");
+            put(VK_THORN, "\u00fe");
+            put(VK_Y_WITH_DIAERESIS, "\u00ff");
+        }
+    };
+
     /**
      * Returns a String describing the keyCode, such as "HOME", "F1" or "A".
      * These strings can be localized by changing the awt.properties file.
@@ -1375,6 +1449,10 @@ public class KeyEvent extends InputEvent {
         if (keyCode >= VK_0 && keyCode <= VK_9 ||
             keyCode >= VK_A && keyCode <= VK_Z) {
             return String.valueOf((char)keyCode);
+        }
+
+        if (keyCode > 0x0300 && keyCode < 0x0400) {
+            return asciiCodeToString.get(keyCode);
         }
 
         switch(keyCode) {
