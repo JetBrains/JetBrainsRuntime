60be76b7258affcad5450a9f9830c52a17488a10
Anton Tarasov <anton.tarasov@jetbrains.com>
2016-11-30 12:32:13 +0300

    JRE-119 [Dynamically set DPI-awareness level to enable backward compatible HiDPI behavior]
    (cherry picked from commit d00cfa4dc62a14a4cf89df9d4c4899970c9fc9e8)
    
    Adopted

--- src/java.desktop/windows/native/libawt/windows/awt.h
+++ src/java.desktop/windows/native/libawt/windows/awt.h
@@ -44,6 +44,14 @@
 
 extern COLORREF DesktopColor2RGB(int colorIndex);
 
+#ifndef PROCESS_DPI_AWARENESS //_WIN32_WINNT_WINBLUE
+typedef enum _PROCESS_DPI_AWARENESS {
+    PROCESS_DPI_UNAWARE            = 0,
+    PROCESS_SYSTEM_DPI_AWARE       = 1,
+    PROCESS_PER_MONITOR_DPI_AWARE  = 2
+} PROCESS_DPI_AWARENESS;
+#endif
+
 class AwtObject;
 typedef AwtObject* PDATA;
 
