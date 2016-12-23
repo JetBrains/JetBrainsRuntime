a38c8677e6f9b2a504d71f9861d527831b98509d
Denis Fokin <denis.fokin@jetbrains.com>
2016-12-23 17:46:26 +0300

    macOS national keyboard support
    (cherry picked from commit 83356eb0bbda6461a87737bda18021c532b6f930)
    (cherry picked from commit c8a36e180467b2b9dfa8dd63dd23c9d1754bf9ba)

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CEmbeddedFrame.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CEmbeddedFrame.java
@@ -106,7 +106,8 @@ public final class CEmbeddedFrame extends EmbeddedFrame {
     public void handleKeyEvent(int eventType, int modifierFlags, String characters,
                                String charsIgnoringMods, boolean isRepeat, short keyCode,
                                boolean needsKeyTyped) {
-        responder.handleKeyEvent(eventType, modifierFlags, characters, charsIgnoringMods,
+        responder.handleKeyEvent(eventType, modifierFlags, characters,
+                charsIgnoringMods, /*charsIgnoringModifiersAndShift*/ null,
                 keyCode, needsKeyTyped, isRepeat);
     }
 
