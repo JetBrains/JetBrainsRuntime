7c3dccbe3903c5a3f5d07e5982ec4ccde11c3db4
Denis Fokin <denis.fokin@jetbrains.com>
2016-12-23 17:46:26 +0300

    macOS national keyboard support

---
+++ src/java.desktop/share/classes/sun/awt/event/KeyEventProcessing.java
@@ -0,0 +1,22 @@
+package sun.awt.event;
+
+import sun.font.FontUtilities;
+
+import java.lang.annotation.Native;
+
+public class KeyEventProcessing {
+    // This property is used to emulate old behavior
+    // when user should turn off national keycodes processing
+    // "com.jetbrains.use.old.keyevent.processing"
+    public final static String useNationalLayoutsOption = "com.sun.awt.use.national.layouts";
+    @Native
+    public final static boolean useNationalLayouts = "true".equals(
+            Util.getProperty(useNationalLayoutsOption,
+                FontUtilities.isMacOSX ? "true" : "false"));
+
+    // Used on windows to emulate latin OEM keys on cyrillic keyboards
+    public final static String useLatinNonAlphaNumKeycodesOption = "com.sun.awt.useLatinNonAlphaNumKeycodes";
+    @Native
+    public final static boolean useLatinNonAlphaNumKeycodes = "true".equals(
+            Util.getProperty(useLatinNonAlphaNumKeycodesOption, "false"));
+}
