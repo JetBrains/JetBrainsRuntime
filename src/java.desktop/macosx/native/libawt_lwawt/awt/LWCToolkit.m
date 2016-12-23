715b60cfa70a9acb3ceab61dd8bc9bf5f268e824
Denis Fokin <denis.fokin@jetbrains.com>
2016-12-23 17:46:26 +0300

    macOS national keyboard support
    (cherry picked from commit 83356eb0bbda6461a87737bda18021c532b6f930)
    (cherry picked from commit c8a36e180467b2b9dfa8dd63dd23c9d1754bf9ba)

--- src/java.desktop/macosx/native/libawt_lwawt/awt/LWCToolkit.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/LWCToolkit.m
@@ -834,6 +834,46 @@ Java_sun_lwawt_macosx_LWCToolkit_isEmbedded
 }
 
 /*
+ * Class:     sun_lwawt_macosx_LWCToolkit
+ * Method:    getKeyboardLayoutNativeId
+ * Signature: ()Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL
+JNICALL Java_sun_lwawt_macosx_LWCToolkit_getKeyboardLayoutNativeId(JNIEnv *env, jclass cls)
+{
+    __block NSString * layoutId = NULL;
+    JNI_COCOA_ENTER(env);
+    [ThreadUtilities performOnMainThreadWaiting:YES block:^(){
+        TISInputSourceRef source = TISCopyCurrentKeyboardInputSource();
+        layoutId = TISGetInputSourceProperty(source, kTISPropertyInputSourceID);
+    }];
+    JNI_COCOA_EXIT(env);
+    return NSStringToJavaString(env, layoutId);
+}
+
+/*
+ * Class:     sun_lwawt_macosx_LWCToolkit
+ * Method:    switchKeyboardLayoutNative
+ * Signature: (Ljava/lang/String;)V
+ */
+JNIEXPORT void JNICALL
+JNICALL Java_sun_lwawt_macosx_LWCToolkit_switchKeyboardLayoutNative(JNIEnv *env, jclass cls, jstring jLayoutId)
+{
+    JNI_COCOA_ENTER(env);
+    __block NSString* layoutId = [JavaStringToNSString(env, jLayoutId) retain];
+    [ThreadUtilities performOnMainThreadWaiting:NO block:^(){
+        NSArray* sources = CFBridgingRelease(TISCreateInputSourceList((__bridge CFDictionaryRef)@{ (__bridge NSString*)kTISPropertyInputSourceID : layoutId }, FALSE));
+        TISInputSourceRef source = (__bridge TISInputSourceRef)sources[0];
+        OSStatus status = TISSelectInputSource(source);
+        if (status != noErr) {
+            NSLog(@"error during keyboard layout switch");
+        }
+        [layoutId release];
+    }];
+    JNI_COCOA_EXIT(env);
+}
+
+/*
  * Class:     sun_awt_PlatformGraphicsInfo
  * Method:    isInAquaSession
  * Signature: ()Z
