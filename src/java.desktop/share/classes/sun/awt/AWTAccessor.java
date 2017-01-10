96308448e57b8436a48a6131d69a34575828d451
Anton Tarasov <anton.tarasov@jetbrains.com>
2017-01-10 15:54:59 +0300

    JRE-166 [macOS] deadlock with JFXPanel
    (cherry picked from commit a9dbb6990fac0c659297487a261ba9170e5fb3ad)
    
    (cherry picked from commit 8a44e1bb3785aaa4a94a440dfbca472ce6b79bab)

--- src/java.desktop/share/classes/sun/awt/AWTAccessor.java
+++ src/java.desktop/share/classes/sun/awt/AWTAccessor.java
@@ -49,6 +49,7 @@ import java.security.AccessControlContext;
 import java.io.File;
 import java.util.ResourceBundle;
 import java.util.Vector;
+import java.util.function.BooleanSupplier;
 import javax.accessibility.AccessibleBundle;
 
 import sun.java2d.cmm.Profile;
@@ -552,12 +553,17 @@ public final class AWTAccessor {
         /**
          * Sets the delegate for the EventQueue used by FX/AWT single threaded mode
          */
-        void setFwDispatcher(EventQueue eventQueue, FwDispatcher dispatcher);
+        public void setFwDispatcher(EventQueue eventQueue, FwDispatcher dispatcher);
 
         /**
          * Gets most recent event time in the EventQueue
          */
         long getMostRecentEventTime(EventQueue eventQueue);
+
+        /**
+         * Creates a secondary loop with the provided condition.
+         */
+        SecondaryLoop createSecondaryLoop(EventQueue eventQueue, BooleanSupplier cond);
     }
 
     /*
