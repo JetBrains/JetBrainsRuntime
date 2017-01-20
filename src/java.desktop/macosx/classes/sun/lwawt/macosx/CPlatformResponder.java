9a20d8561420d015b389b1b1e750a7b8fdaad9e7
Denis Fokin <denis.fokin@jetbrains.com>
2017-01-20 20:31:09 +0300

    IDEA-166921 Shift not handled correctly in keymap
    (cherry picked from commit 9328c07d5545e74d37ad1bf2016b6a433281d28e)

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformResponder.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformResponder.java
@@ -259,6 +259,14 @@ final class CPlatformResponder {
             ? nsEvent.getCharactersIgnoringModifiers().charAt(0)
             : charactersIsValid ? nsEvent.getCharacters().charAt(0) : KeyEvent.CHAR_UNDEFINED;
 
+        if (useShiftedCharacters && nsEvent.getCharactersIgnoringModifiers() != null && !nsEvent.getCharactersIgnoringModifiers().isEmpty()) {
+            characterToGetKeyCode = nsEvent.getCharactersIgnoringModifiers().charAt(0);
+        } else  if (nsEvent.getCharactersIgnoringModifiersAndShift() != null && !nsEvent.getCharactersIgnoringModifiersAndShift().isEmpty()) {
+            characterToGetKeyCode = nsEvent.getCharactersIgnoringModifiersAndShift().charAt(0);
+        } else if (nsEvent.getCharacters() != null && !nsEvent.getCharacters().isEmpty() && metaAltCtrlAreNotPressed && shiftIsPressed) {
+            characterToGetKeyCode = checkedChar;
+        }
+
         // We use char candidate if modifiers are not used
         // otherwise, we use char ignoring modifiers
         int[] in = new int[] {
@@ -272,7 +280,7 @@ final class CPlatformResponder {
 
         postsTyped = NSEvent.nsToJavaKeyInfo(in, out);
 
-        char characterToSendWithTheEvent = KeyEvent.CHAR_UNDEFINED;
+        char characterToSendWithTheEvent = characterToGetKeyCode;
 
         if(nsEvent.isHasDeadKey()){
             characterToSendWithTheEvent = (char) out[2];
@@ -301,24 +309,12 @@ final class CPlatformResponder {
 
         if (isISOControl) {
             characterToSendWithTheEvent = checkedChar;
-        } else  if (useShiftedCharacters && nsEvent.getCharactersIgnoringModifiers() != null && !nsEvent.getCharactersIgnoringModifiers().isEmpty()) {
-            characterToSendWithTheEvent = nsEvent.getCharactersIgnoringModifiers().charAt(0);
-        } else  if (nsEvent.getCharactersIgnoringModifiersAndShift() != null && !nsEvent.getCharactersIgnoringModifiersAndShift().isEmpty()) {
-            characterToSendWithTheEvent = nsEvent.getCharactersIgnoringModifiersAndShift().charAt(0);
-        } else if (nsEvent.getCharacters() != null && !nsEvent.getCharacters().isEmpty() && metaAltCtrlAreNotPressed && shiftIsPressed) {
-            characterToSendWithTheEvent = checkedChar;
-        }
+       }
 
-        characterToSendWithTheEvent = mapNsCharsToCompatibleWithJava(characterToSendWithTheEvent);
+       characterToSendWithTheEvent = mapNsCharsToCompatibleWithJava(characterToSendWithTheEvent);
 
-        if (jmodifiers != 0 && nsEvent.getCharactersIgnoringModifiersAndShift() != null && !nsEvent.getCharactersIgnoringModifiersAndShift().isEmpty()) {
-            String stringWithChar = NSEvent.nsToJavaChar(characterToSendWithTheEvent, nsEvent.getModifierFlags(), spaceKeyTyped);
-            characterToSendWithTheEvent = stringWithChar == null ? KeyEvent.CHAR_UNDEFINED : stringWithChar.charAt(0);
-        } else {
-            String stringWithChar = NSEvent.nsToJavaChar(characterToSendWithTheEvent, nsEvent.getModifierFlags(), spaceKeyTyped);
-            characterToSendWithTheEvent = stringWithChar == null ? KeyEvent.CHAR_UNDEFINED : stringWithChar.charAt(0);
-        }
-
+       String stringWithChar = NSEvent.nsToJavaChar(characterToSendWithTheEvent, nsEvent.getModifierFlags(), spaceKeyTyped);
+       characterToSendWithTheEvent = stringWithChar == null ? KeyEvent.CHAR_UNDEFINED : stringWithChar.charAt(0);
 
         long when = System.currentTimeMillis();
 
@@ -345,7 +341,7 @@ final class CPlatformResponder {
 
             if (nsEvent.getCharacters()!= null ) {
                 characterToSendWithTypedEvent = mapNsCharsToCompatibleWithJava(checkedChar);
-                String stringWithChar = NSEvent.nsToJavaChar(characterToSendWithTypedEvent, nsEvent.getModifierFlags(), spaceKeyTyped);
+                stringWithChar = NSEvent.nsToJavaChar(characterToSendWithTypedEvent, nsEvent.getModifierFlags(), spaceKeyTyped);
                 characterToSendWithTypedEvent = stringWithChar == null ? KeyEvent.CHAR_UNDEFINED :  stringWithChar.charAt(0);
             }
 
