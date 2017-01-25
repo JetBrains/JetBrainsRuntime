6825bbacd8a1d321c2eb26918578985a8293dbc1
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2017-01-25 17:18:33 +0300

    JRE-202 Deadlock in CGLGraphicsConfig.getCGLConfigInfo
    Added processing system events while waiting for OGLRenderQueue.lock
    Moved getCGLConfigInfo logic execution to AppKit thread so, awt lock is
     taken on one thread
    
    (cherry picked from commit d1c8bf03e1bd41cb075aa73cc39558103af7fe1a)
    (cherry picked from commit 6bf9f31986be64acf3755b34568802f9960a66ec)
    (cherry picked from commit 4e21d67e0369bffac45662c63699b39946218a7a)

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/LWCToolkit.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/LWCToolkit.java
@@ -728,8 +728,6 @@ public final class LWCToolkit extends LWToolkit {
     public static void invokeAndWait(Runnable runnable, Component component)
             throws InvocationTargetException
     {
-        Objects.requireNonNull(component, "Null component provided to invokeAndWait");
-
         boolean nonBlockingRunLoop;
 
         synchronized (priorityInvocationPending) {
