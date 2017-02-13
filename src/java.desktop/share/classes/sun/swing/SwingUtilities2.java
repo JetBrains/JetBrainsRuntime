cea67bc28a1257b011dad705ed5a85644ce319f6
Anton Tarasov <anton.tarasov@jetbrains.com>
2017-02-13 18:35:33 +0300

    JRE-225 [macos] IDEA hangs on attempt to call getDefaultScreenDevice() from EDT
    (cherry picked from commit 76aba25)
    
    (cherry picked from commit df11dcc97bb5556ac5d0299b773a512b4f0bb5bb)
    (cherry picked from commit aeea6c1ca3f4a8ead1b34b1ce2e74a9e2e1e5cf9)
    (cherry picked from commit 3fbb520b07bc397788f30488b616b39d8431895f)

--- src/java.desktop/share/classes/sun/swing/SwingUtilities2.java
+++ src/java.desktop/share/classes/sun/swing/SwingUtilities2.java
@@ -1244,7 +1244,8 @@ public class SwingUtilities2 {
 
             GraphicsConfiguration gc = c.getGraphicsConfiguration();
             AffineTransform tx = (gc == null) ? null : gc.getDefaultTransform();
-            if (tx == null && !GraphicsEnvironment.isHeadless()) {
+            // [tav] workaround deadlock on MacOSX until fixed, JRE-226
+            if (!FontUtilities.isMacOSX && tx == null && !GraphicsEnvironment.isHeadless()) {
                 tx =  GraphicsEnvironment
                         .getLocalGraphicsEnvironment()
                         .getDefaultScreenDevice()
