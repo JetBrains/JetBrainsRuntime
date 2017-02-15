7fe6162fd001039a405b1c885849c985ffcfdf87
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2017-02-15 13:26:58 +0300

    JRE-205 Font is wrong and without anti aliasing in 2017.1 EAP
    Added property to disable bundled font config:
      java2d.font.loadFontConf=false
    Do not load custom font.conf by default
    
    Moved hints adjusting logic from code to bundled font.conf file
    Applied correction only for regular fonts with platform sizes less than 12
    Some fonts are not corrected at all: Consolas, Noto Sans Mono
    Used family name instead of physical one in requests to Fontconfig
    Removed redundant call to FcConfigBuildFonts
    Added privileged access to the properties (JRE-235,JRE-235)
    
    (cherry picked from commit 4d4c915047077ebd966b0e3be056566d56ba11a4)
    (cherry picked from commit 9d6f325f72482405264852f3ee2636f5fedaeaf0)
    (cherry picked from commit e7e3372bf8db539c0f6bc85db9f1093f8fa4c380)
    (cherry picked from commit 3e724caed2f199be50d25d1ecb20b7819c86be2e)
    (cherry picked from commit d372b35963c096a32331b05b257e26841ace5d94)
    (cherry picked from commit 18a5f5de03eb107f89dca138a44b9aab2151235c)
    (cherry picked from commit 9ba320efef0539f75aa93fd1b5dd80266c954d0a)
    (cherry picked from commit b8c38f419972af61291953f7f452c1698f7a1624)
    (cherry picked from commit debba0128e200be60adc9a339d5985590ef4e230)
    (cherry picked from commit 2fa17b1bd7d6524e4b5fa4d0b3ce2bf02a8fcc78)
    (cherry picked from commit 09b4f61db0d4f5beea0e16ce9136c99e2185c10b)
    (cherry picked from commit 3b6782dd742f9c74a9535145db2f9f7ffaccf7c8)
    (cherry picked from commit f1b68149528c13a22fa64468c130b1405bf3d081)
    (cherry picked from commit db5cf5a2b9cb454630fb86783c2d58cd5446cba6)
    (cherry picked from commit 32140948578bc3c2a0c5f8adb537660421efe5e7)
    (cherry picked from commit b978e3d0b131ed642774c5a14a649e13f764c20b)
    
    (cherry picked from commit c75c1ef8b2b1b0e9e604ced306555663036167ae)
    
    (cherry picked from commit a29f19e6a2f1f30c2e77f85d801a93c88a6ea1bd)
    (cherry picked from commit c38c46744fc463be843cb22af53b93dc36d7001a)

--- src/java.desktop/share/classes/sun/font/FreetypeFontScaler.java
+++ src/java.desktop/share/classes/sun/font/FreetypeFontScaler.java
@@ -29,7 +29,9 @@ import java.awt.*;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.io.File;
 import java.lang.ref.WeakReference;
+import java.security.PrivilegedAction;
 
 /* This is Freetype based implementation of FontScaler.
  *
@@ -48,10 +50,21 @@ class FreetypeFontScaler extends FontScaler {
         /* At the moment fontmanager library depends on freetype library
            and therefore no need to load it explicitly here */
         FontManagerNativeLibrary.load();
-        initIDs(FreetypeFontScaler.class, Toolkit.class, PhysicalFont.class);
+
+        String jreFontConfName =
+                        "true".equals(System.getProperty(
+                                "java2d.font.loadFontConf", "")) ?
+                                System.getProperty("java.home", "") +
+                                File.separator + "lib" + File.separator +
+                                "fonts" + File.separator + "font.conf" :
+                                null;
+
+        initIDs(FreetypeFontScaler.class, Toolkit.class, PhysicalFont.class,
+                jreFontConfName);
     }
 
-    private static native void initIDs(Class<?> FFS, Class<?> toolkitClass, Class<?> pfClass);
+    private static native void initIDs(Class<?> FFS, Class<?> toolkitClass, Class<?> pfClass,
+                                       String jreFontDirName);
 
     private void invalidateScaler() throws FontScalerException {
         nativeScaler = 0;
