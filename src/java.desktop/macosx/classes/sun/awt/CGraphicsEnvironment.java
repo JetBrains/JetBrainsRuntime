e4e03cede8b9b7aaf6b1670ceab7271d9af3d21e
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2017-05-16 16:54:37 +0300

    JRE-359 CGraphicsEnvironment.getDefaultScreenDevice() returns null
    Moved CG api calls to AppKit thread
    
    (cherry picked from commit fd0210f035199e8612097a2c1d42b90cfd2111f8)
    (cherry picked from commit 5e99e376d9dfe477401121878704630c3c13f9f7)
    
    (cherry picked from commit 6d73b25130696c7265b4b95013f54dfd8f6c622f)
    (cherry picked from commit 9519ac1e6e66d2da3d7a45e5a24cd211c0d6b3a7)

--- src/java.desktop/macosx/classes/sun/awt/CGraphicsEnvironment.java
+++ src/java.desktop/macosx/classes/sun/awt/CGraphicsEnvironment.java
@@ -40,6 +40,7 @@ import java.util.Map;
 import sun.java2d.Disposer;
 import sun.java2d.DisposerRecord;
 import sun.java2d.SunGraphicsEnvironment;
+import sun.lwawt.macosx.CThreading;
 
 /**
  * This is an implementation of a GraphicsEnvironment object for the default
@@ -161,7 +162,15 @@ public final class CGraphicsEnvironment extends SunGraphicsEnvironment {
     private synchronized void initDevices() {
         Map<Integer, CGraphicsDevice> old = new HashMap<>(devices);
         devices.clear();
-        mainDisplayID = getMainDisplayID();
+        try {
+            mainDisplayID = CThreading.privilegedExecuteOnAppKit(
+                    CGraphicsEnvironment::getMainDisplayID);
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException("Could not get main display ID: " +
+                    e.getMessage() );
+        }
 
         // initialization of the graphics device may change list of displays on
         // hybrid systems via an activation of discrete video.
@@ -171,7 +180,17 @@ public final class CGraphicsEnvironment extends SunGraphicsEnvironment {
             old.put(mainDisplayID, new CGraphicsDevice(mainDisplayID));
         }
 
-        int[] displayIDs = getDisplayIDs();
+        int[] displayIDs;
+        try {
+            displayIDs = CThreading.privilegedExecuteOnAppKit(
+                    CGraphicsEnvironment::getDisplayIDs);
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException("Could not get display IDs: " +
+                    e.getMessage());
+        }
+
         if (displayIDs.length == 0) {
             // we could throw AWTError in this case.
             displayIDs = new int[]{mainDisplayID};
