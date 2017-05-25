41d44bf36a1d9d948d73b84308e58154e866b9f4
rprotacio <none@none>
2017-05-25 15:39:32 -0400

    8180020: Improve SymbolHashMap entry handling Reviewed-by: mschoene, coleenp, rhalade Contributed-by: rachel.protacio@oracle.com

--- hotspot/src/share/vm/oops/constantPool.hpp
+++ hotspot/src/share/vm/oops/constantPool.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -958,7 +958,7 @@ class SymbolHashMap: public CHeapObj<mtSymbol> {
         delete(cur);
       }
     }
-    delete _buckets;
+    FREE_C_HEAP_ARRAY(SymbolHashMapBucket, _buckets);
   }
 }; // End SymbolHashMap class
 
