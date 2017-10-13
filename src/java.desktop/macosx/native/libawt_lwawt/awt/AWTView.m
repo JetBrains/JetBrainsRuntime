cef970e1ba394d33e180af1f37008750c2b950e1
Alexey Ushakov <Alexey.Ushakov@jetbrains.com>
2017-10-13 17:16:25 +0300

    JRE-471 Crash on macOS Sierra after Sleep
    Replaced [NSScreen screens] 'objectAtIndex' with 'firstObject' to get nil instead of NSRangeException. Added nil checks
    
    (cherry picked from commit d6b98511262055c01522d9ec8024253af7e91564)

--- src/java.desktop/macosx/native/libawt_lwawt/awt/AWTView.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/AWTView.m
@@ -910,9 +910,11 @@ static BOOL shouldUsePressAndHold() {
     CDragSource *dragSource = self._dragSource;
     NSDragOperation dragOp = NSDragOperationNone;
 
-    if (dragSource != nil) {
+    if (dragSource != nil)
         dragOp = [dragSource draggingSourceOperationMaskForLocal:flag];
-    }
+    else if ([super respondsToSelector:@selector(draggingSourceOperationMaskForLocal:)])
+        dragOp = [super draggingSourceOperationMaskForLocal:flag];
+
     return dragOp;
 }
 
@@ -922,9 +924,11 @@ static BOOL shouldUsePressAndHold() {
     CDragSource *dragSource = self._dragSource;
     NSArray* array = nil;
 
-    if (dragSource != nil) {
+    if (dragSource != nil)
         array = [dragSource namesOfPromisedFilesDroppedAtDestination:dropDestination];
-    }
+    else if ([super respondsToSelector:@selector(namesOfPromisedFilesDroppedAtDestination:)])
+        array = [super namesOfPromisedFilesDroppedAtDestination:dropDestination];
+
     return array;
 }
 
@@ -964,9 +968,11 @@ static BOOL shouldUsePressAndHold() {
     CDragSource *dragSource = self._dragSource;
     BOOL result = FALSE;
 
-    if (dragSource != nil) {
+    if (dragSource != nil)
         result = [dragSource ignoreModifierKeysWhileDragging];
-    }
+    else if ([super respondsToSelector:@selector(ignoreModifierKeysWhileDragging)])
+        result = [super ignoreModifierKeysWhileDragging];
+
     return result;
 }
 
@@ -980,9 +986,11 @@ static BOOL shouldUsePressAndHold() {
     CDropTarget *dropTarget = self._dropTarget;
     NSDragOperation dragOp = NSDragOperationNone;
 
-    if (dropTarget != nil) {
+    if (dropTarget != nil)
         dragOp = [dropTarget draggingEntered:sender];
-    }
+    else if ([super respondsToSelector:@selector(draggingEntered:)])
+        dragOp = [super draggingEntered:sender];
+
     return dragOp;
 }
 
@@ -992,9 +1000,11 @@ static BOOL shouldUsePressAndHold() {
     CDropTarget *dropTarget = self._dropTarget;
     NSDragOperation dragOp = NSDragOperationNone;
 
-    if (dropTarget != nil) {
+    if (dropTarget != nil)
         dragOp = [dropTarget draggingUpdated:sender];
-    }
+    else if ([super respondsToSelector:@selector(draggingUpdated:)])
+        dragOp = [super draggingUpdated:sender];
+
     return dragOp;
 }
 
@@ -1014,9 +1024,11 @@ static BOOL shouldUsePressAndHold() {
     CDropTarget *dropTarget = self._dropTarget;
     BOOL result = FALSE;
 
-    if (dropTarget != nil) {
+    if (dropTarget != nil)
         result = [dropTarget prepareForDragOperation:sender];
-    }
+    else if ([super respondsToSelector:@selector(prepareForDragOperation:)])
+        result = [super prepareForDragOperation:sender];
+
     return result;
 }
 
@@ -1026,9 +1038,11 @@ static BOOL shouldUsePressAndHold() {
     CDropTarget *dropTarget = self._dropTarget;
     BOOL result = FALSE;
 
-    if (dropTarget != nil) {
+    if (dropTarget != nil)
         result = [dropTarget performDragOperation:sender];
-    }
+    else if ([super respondsToSelector:@selector(performDragOperation:)])
+        result = [super performDragOperation:sender];
+
     return result;
 }
 
