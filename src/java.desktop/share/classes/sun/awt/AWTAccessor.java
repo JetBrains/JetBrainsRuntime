2a29ce5492af82f83ff0121662d9a868a8cd8530
Anton Tarasov <anton.tarasov@jetbrains.com>
2017-12-28 17:39:18 +0300

    JRE-616 [linux] notify when dpi correction factor is applied to fonts
    (cherry picked from commit f57d41f3118bfd773c99ce32d58cfae16931be6a)
    (cherry picked from commit 6246abc72f1c651955419faef74dfcd68b5dd852)

--- src/java.desktop/share/classes/sun/awt/AWTAccessor.java
+++ src/java.desktop/share/classes/sun/awt/AWTAccessor.java
@@ -769,6 +769,7 @@ public final class AWTAccessor {
      */
     public interface ToolkitAccessor {
         void setPlatformResources(ResourceBundle bundle);
+        void setDesktopProperty(Toolkit tk, String prop, Object value);
     }
 
     /*
