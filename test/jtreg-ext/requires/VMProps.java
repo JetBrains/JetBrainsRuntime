16a40e6f7b0d1c0b12385736ef782d6380b116d4
Igor Ignatyev <iignatyev@openjdk.org>
2018-01-30 16:48:53 -0800

    8167276: jvmci/compilerToVM/MaterializeVirtualObjectTest.java fails with -XX:-EliminateAllocations
    Reviewed-by: kvn

--- test/jtreg-ext/requires/VMProps.java
+++ test/jtreg-ext/requires/VMProps.java
@@ -282,6 +282,7 @@ public class VMProps implements Callable<Map<String, String>> {
         vmOptFinalFlag(map, "ClassUnloading");
         vmOptFinalFlag(map, "UseCompressedOops");
         vmOptFinalFlag(map, "EnableJVMCI");
+        vmOptFinalFlag(map, "EliminateAllocations");
     }
 
     /**
