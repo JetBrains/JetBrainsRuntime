8787690d113af8f8d711788aaaaeb839935f7bbd
Alexey Ushakov <Alexey.Ushakov@jetbrains.com>
2018-02-09 00:55:18 +0300

    JRE-652 RepaintManager does not increase double buffer after attaching a device with higher resolution
    Reset double buffer max size on display change event
    Do not increase the buffer if the size was manually set
    
    (cherry picked from commit 5ee1b84d0bd751a113d7963cacf3c5a64ceada80)
    (cherry picked from commit bfb3d537995a1a378620bd6c3f4287eac4b886b1)

--- jdk/src/java.desktop/share/classes/javax/swing/RepaintManager.java
+++ jdk/src/java.desktop/share/classes/javax/swing/RepaintManager.java
@@ -113,6 +113,8 @@ public class RepaintManager
 
     private Dimension doubleBufferMaxSize;
 
+    private boolean isCustomMaxBufferSizeSet = false;
+
     // Support for both the standard and volatile offscreen buffers exists to
     // provide backwards compatibility for the [rare] programs which may be
     // calling getOffScreenBuffer() and not expecting to get a VolatileImage.
@@ -338,7 +340,13 @@ public class RepaintManager
     }
 
     private void displayChanged() {
-        clearImages();
+        if (isCustomMaxBufferSizeSet) {
+            clearImages();
+        } else {
+            // Reset buffer maximum size to get valid size from updated graphics
+            // environment in getDoubleBufferMaximumSize()
+            setDoubleBufferMaximumSize(null);
+        }
     }
 
     /**
@@ -1159,8 +1167,10 @@ public class RepaintManager
     public void setDoubleBufferMaximumSize(Dimension d) {
         doubleBufferMaxSize = d;
         if (doubleBufferMaxSize == null) {
+            isCustomMaxBufferSizeSet = false;
             clearImages();
         } else {
+            isCustomMaxBufferSizeSet = true;
             clearImages(d.width, d.height);
         }
     }
