ba22b31a498f8acf0a7d96977089e0b94f326cdc
Anton Tarasov <anton.tarasov@jetbrains.com>
2018-04-17 19:55:03 +0300

    JRE-729 [windows] unreasonable IME activity consumes CPU
    (cherry picked from commit c3040dd1fe1bf6c03c746c76f685629fd0a8e670)

--- src/java.desktop/share/classes/java/awt/Component.java
+++ src/java.desktop/share/classes/java/awt/Component.java
@@ -93,6 +93,10 @@ import javax.swing.JRootPane;
 import sun.awt.AWTAccessor;
 import sun.awt.AppContext;
 import sun.awt.ComponentFactory;
+import sun.security.action.GetBooleanAction;
+import sun.security.action.GetPropertyAction;
+import sun.awt.AppContext;
+import sun.awt.AWTAccessor;
 import sun.awt.ConstrainableGraphics;
 import sun.awt.EmbeddedFrame;
 import sun.awt.RequestFocusController;
@@ -611,6 +615,8 @@ public abstract class Component implements ImageObserver, MenuContainer,
      */
     long eventMask = AWTEvent.INPUT_METHODS_ENABLED_MASK;
 
+    private static final boolean INPUT_METHODS_DISABLED;
+
     /**
      * Static properties for incremental drawing.
      * @see #imageUpdate
@@ -632,6 +638,8 @@ public abstract class Component implements ImageObserver, MenuContainer,
         s = java.security.AccessController.doPrivileged(
                                                         new GetPropertyAction("awt.image.redrawrate"));
         incRate = (s != null) ? Integer.parseInt(s) : 100;
+
+        INPUT_METHODS_DISABLED = java.security.AccessController.doPrivileged(new GetBooleanAction("awt.ime.disabled"));
     }
 
     /**
@@ -1626,6 +1634,7 @@ public abstract class Component implements ImageObserver, MenuContainer,
      * @since 1.2
      */
     public void enableInputMethods(boolean enable) {
+        if (INPUT_METHODS_DISABLED) return;
         if (enable) {
             if ((eventMask & AWTEvent.INPUT_METHODS_ENABLED_MASK) != 0)
                 return;
@@ -5170,6 +5179,7 @@ public abstract class Component implements ImageObserver, MenuContainer,
     }
 
     boolean areInputMethodsEnabled() {
+        if (INPUT_METHODS_DISABLED) return false;
         // in 1.2, we assume input method support is required for all
         // components that handle key events, but components can turn off
         // input methods by calling enableInputMethods(false).
