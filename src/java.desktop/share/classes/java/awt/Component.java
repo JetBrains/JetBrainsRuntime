8f7f8127774435ba0c83a24a6fc86c08b2a63998
Anton Tarasov <anton.tarasov@jetbrains.com>
2018-04-17 19:55:03 +0300

    JRE-729 [windows] unreasonable IME activity consumes CPU
    (cherry picked from commit caf8462e0977250a4a713d786d4c897c67807324)

--- src/java.desktop/share/classes/java/awt/Component.java
+++ src/java.desktop/share/classes/java/awt/Component.java
@@ -94,6 +94,10 @@ import javax.swing.JRootPane;
 import sun.awt.AWTAccessor;
 import sun.awt.AppContext;
 import sun.awt.ComponentFactory;
+import sun.security.action.GetBooleanAction;
+import sun.security.action.GetPropertyAction;
+import sun.awt.AppContext;
+import sun.awt.AWTAccessor;
 import sun.awt.ConstrainableGraphics;
 import sun.awt.EmbeddedFrame;
 import sun.awt.RequestFocusController;
@@ -636,6 +640,10 @@ public abstract class Component implements ImageObserver, MenuContainer,
 
         String s2 = System.getProperty("awt.image.redrawrate");
         incRate = (s2 != null) ? Integer.parseInt(s2) : 100;
+
+        @SuppressWarnings("removal")
+        boolean imeDisabled = java.security.AccessController.doPrivileged(new GetBooleanAction("awt.ime.disabled"));
+        INPUT_METHODS_DISABLED = imeDisabled;
     }
 
     /**
@@ -1629,6 +1637,7 @@ public abstract class Component implements ImageObserver, MenuContainer,
      * @since 1.2
      */
     public void enableInputMethods(boolean enable) {
+        if (INPUT_METHODS_DISABLED) return;
         if (enable) {
             if ((eventMask & AWTEvent.INPUT_METHODS_ENABLED_MASK) != 0)
                 return;
@@ -5149,6 +5158,7 @@ public abstract class Component implements ImageObserver, MenuContainer,
     }
 
     boolean areInputMethodsEnabled() {
+        if (INPUT_METHODS_DISABLED) return false;
         // in 1.2, we assume input method support is required for all
         // components that handle key events, but components can turn off
         // input methods by calling enableInputMethods(false).
