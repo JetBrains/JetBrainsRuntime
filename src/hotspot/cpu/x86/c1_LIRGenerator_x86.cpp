bced9004605740d6430704fc73309c700dbbfbd0
shade <none@none>
2018-06-06 19:24:08 +0200

    C1 xchg_oop needs temp register operand

--- src/hotspot/cpu/x86/c1_LIRGenerator_x86.cpp
+++ src/hotspot/cpu/x86/c1_LIRGenerator_x86.cpp
@@ -691,7 +691,8 @@ LIR_Opr LIRGenerator::atomic_xchg(BasicType type, LIR_Opr addr, LIRItem& value) 
   // Because we want a 2-arg form of xchg and xadd
   __ move(value.result(), result);
   assert(type == T_INT || is_oop LP64_ONLY( || type == T_LONG ), "unexpected type");
-  __ xchg(addr, result, result, LIR_OprFact::illegalOpr);
+  LIR_Opr tmp = is_oop ? new_register(type) : LIR_OprFact::illegalOpr;
+  __ xchg(addr, result, result, tmp);
   return result;
 }
 
