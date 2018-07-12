a89dcffc71611c000a8fe1d5722760fc365f6ece
roland <none@none>
2018-07-12 15:42:17 +0200

    [backport] fix aarch64 membar elision with shenandoah

--- test/hotspot/jtreg/compiler/c2/aarch64/TestVolatiles.java
+++ test/hotspot/jtreg/compiler/c2/aarch64/TestVolatiles.java
@@ -36,7 +36,9 @@
  *                    CMS,
  *                    CMSCondMark,
  *                    Serial,
- *                    Parallel}
+ *                    Parallel,
+ *                    Shenandoah,
+ *                    ShenandoahTraversal}
  */
 
 
@@ -93,6 +95,18 @@ public class TestVolatiles {
             procArgs = new String[argcount];
             procArgs[argcount - 3] = "-XX:+UseConcMarkSweepGC";
             procArgs[argcount - 2] = "-XX:+UseCondCardMark";
+            break;
+        case "Shenandoah":
+            argcount = 8;
+            procArgs = new String[argcount];
+            procArgs[argcount - 2] = "-XX:+UseShenandoahGC";
+            break;
+        case "ShenandoahTraversal":
+            argcount = 10;
+            procArgs = new String[argcount];
+            procArgs[argcount - 4] = "-XX:+UseShenandoahGC";
+            procArgs[argcount - 3] = "-XX:+UnlockExperimentalVMOptions";
+            procArgs[argcount - 2] = "-XX:ShenandoahGCHeuristics=traversal";
             break;
         default:
             throw new RuntimeException("unexpected test type " + testType);
@@ -317,6 +331,17 @@ public class TestVolatiles {
                     "ret"
                 };
                 break;
+            case "Shenandoah":
+            case "ShenandoahTraversal":
+                 // Shenandoah generates normal object graphs for
+                 // volatile stores
+                matches = new String[] {
+                    "membar_release (elided)",
+                    "stlrw",
+                    "membar_volatile (elided)",
+                    "ret"
+                };
+                break;
             }
         } else {
             switch (testType) {
@@ -375,6 +400,20 @@ public class TestVolatiles {
                     "storestore",
                     "dmb ishst",
                     "strb",
+                    "membar_volatile",
+                    "dmb ish",
+                    "ret"
+                };
+                break;
+
+            case "Shenandoah":
+            case "ShenandoahTraversal":
+                 // Shenandoah generates normal object graphs for
+                 // volatile stores
+                matches = new String[] {
+                    "membar_release",
+                    "dmb ish",
+                    "strw",
                     "membar_volatile",
                     "dmb ish",
                     "ret"
@@ -474,6 +513,17 @@ public class TestVolatiles {
                     "ret"
                 };
                 break;
+            case "Shenandoah":
+            case "ShenandoahTraversal":
+                // For volatile CAS, Shenanodoah generates normal
+                // graphs with a shenandoah-specific cmpxchg
+                matches = new String[] {
+                    "membar_release (elided)",
+                    "cmpxchgw_acq_shenandoah",
+                    "membar_acquire (elided)",
+                    "ret"
+                };
+                break;
             }
         } else {
             switch (testType) {
@@ -532,6 +582,19 @@ public class TestVolatiles {
                     "storestore",
                     "dmb ishst",
                     "strb",
+                    "membar_acquire",
+                    "dmb ish",
+                    "ret"
+                };
+                break;
+            case "Shenandoah":
+            case "ShenandoahTraversal":
+                // For volatile CAS, Shenanodoah generates normal
+                // graphs with a shenandoah-specific cmpxchg
+                matches = new String[] {
+                    "membar_release",
+                    "dmb ish",
+                    "cmpxchgw_shenandoah",
                     "membar_acquire",
                     "dmb ish",
                     "ret"
