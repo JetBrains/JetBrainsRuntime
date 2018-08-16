d4314fc994808436e5f565917bb995a407446891
Anton Tarasov <anton.tarasov@jetbrains.com>
2018-08-16 17:23:28 +0300

    [jdk9] HiDPI scale is not detected on some linux desktops
    (cherry picked from commit 9279d80110cb573a5aae887b66b72b78f12aadbb)
    (cherry picked from commit 57f7da037b26f20cdd9108f55632ae6d65564c7b)

--- src/java.desktop/unix/classes/sun/awt/X11GraphicsDevice.java
+++ src/java.desktop/unix/classes/sun/awt/X11GraphicsDevice.java
@@ -72,6 +72,8 @@ public final class X11GraphicsDevice extends GraphicsDevice
     private volatile Insets insets;
     private boolean shutdownHookRegistered;
     private int scale;
+    private volatile boolean isNativeScaleDefault;
+    private static int globalScale; // derived from Xft.dpi
 
     // Wayland clients are by design not allowed to change the resolution in Wayland.
     // XRandR in Xwayland is just an emulation, it doesn't actually change the resolution.
@@ -580,6 +582,17 @@ public final class X11GraphicsDevice extends GraphicsDevice
         return (int)Math.round(getNativeScaleFactor(screen));
     }
 
+    public static void setGlobalScale(int scale) {
+        globalScale = scale;
+        for (GraphicsDevice gd : GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()) {
+            X11GraphicsDevice x11gd = (X11GraphicsDevice)gd;
+            if (x11gd.isNativeScaleDefault) {
+                x11gd.scale = globalScale;
+                x11gd.isNativeScaleDefault = false;
+            }
+        }
+    }
+
     private int initScaleFactor() {
 
         if (SunGraphicsEnvironment.isUIScaleEnabled()) {
@@ -590,7 +603,10 @@ public final class X11GraphicsDevice extends GraphicsDevice
                 return (int) debugScale;
             }
             int nativeScale = getNativeScale();
-            return nativeScale >= 1 ? nativeScale : 1;
+            if (nativeScale > 0) return nativeScale;
+            if (globalScale > 0) return globalScale;
+            isNativeScaleDefault = true;
+            return 1;
         }
 
         return 1;
