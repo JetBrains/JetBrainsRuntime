806ce3288115b4225d87bb7ef934ff6e9ad755a7
Anton Tarasov <anton.tarasov@jetbrains.com>
2018-08-16 17:23:28 +0300

    [jdk9] HiDPI scale is not detected on some linux desktops
    (cherry picked from commit 9279d80110cb573a5aae887b66b72b78f12aadbb)

--- src/java.desktop/unix/classes/sun/awt/X11GraphicsDevice.java
+++ src/java.desktop/unix/classes/sun/awt/X11GraphicsDevice.java
@@ -68,6 +68,8 @@ public final class X11GraphicsDevice extends GraphicsDevice
     private DisplayMode origDisplayMode;
     private boolean shutdownHookRegistered;
     private int scale;
+    private volatile boolean isNativeScaleDefault;
+    private static int globalScale; // derived from Xft.dpi
 
     public X11GraphicsDevice(int screennum) {
         this.screen = screennum;
@@ -546,6 +548,17 @@ public final class X11GraphicsDevice extends GraphicsDevice
         return (int)Math.round(getNativeScaleFactor(screen));
     }
 
+    public static void setGlobalScale(int scale) {
+        globalScale = scale;
+        for (GraphicsDevice gd : GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()) {
+            X11GraphicsDevice x11gd = (X11GraphicsDevice)gd;
+            if (x11gd.isNativeScaleDefault) {
+                x11gd.scale = globalScale;
+                x11gd.isNativeScaleDefault = false;
+            }
+        }
+    }
+
     private int initScaleFactor() {
 
         if (SunGraphicsEnvironment.isUIScaleEnabled()) {
@@ -556,7 +569,10 @@ public final class X11GraphicsDevice extends GraphicsDevice
                 return (int) debugScale;
             }
             int nativeScale = getNativeScale();
-            return nativeScale >= 1 ? nativeScale : 1;
+            if (nativeScale > 0) return nativeScale;
+            if (globalScale > 0) return globalScale;
+            isNativeScaleDefault = true;
+            return 1;
         }
 
         return 1;
