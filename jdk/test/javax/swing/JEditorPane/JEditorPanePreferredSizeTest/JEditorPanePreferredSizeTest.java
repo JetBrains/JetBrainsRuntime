1300897014d78346bbf4fa590f4abbb068a6ac44
Anton Tarasov <anton.tarasov@jetbrains.com>
2018-09-04 12:07:07 +0300

    JRE-934 [followup] Diff viewer errors are not visible on HiDPI Linux

--- jdk/test/javax/swing/JEditorPane/JEditorPanePreferredSizeTest/JEditorPanePreferredSizeTest.java
+++ jdk/test/javax/swing/JEditorPane/JEditorPanePreferredSizeTest/JEditorPanePreferredSizeTest.java
@@ -1,7 +1,4 @@
-import sun.awt.SunToolkit;
-
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.lang.reflect.InvocationTargetException;
 
@@ -11,62 +8,21 @@ import java.lang.reflect.InvocationTargetException;
  * @run main JEditorPanePreferredSizeTest
  */
 public class JEditorPanePreferredSizeTest {
-    private static JFrame frame;
-    private static MyLabel label;
-
     public static void main(String[] args) throws InvocationTargetException, InterruptedException {
-        SwingUtilities.invokeAndWait(JEditorPanePreferredSizeTest::show);
-
-        ((SunToolkit)Toolkit.getDefaultToolkit()).realSync();
-
         final Dimension size = new Dimension(0, 0);
         SwingUtilities.invokeAndWait(() -> {
-            size.setSize(label.getPreferredSize());
-            frame.dispose();
+            JEditorPane myEditorPane = new JEditorPane("text/html", "text");
+            myEditorPane.setBorder(null);
+            // triggers initial BasicTextUI layout
+            myEditorPane.getPreferredSize();
+            // triggers BasicTextUI.modelChanged(), font here should be different from component's default font
+            myEditorPane.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
+
+            size.setSize(myEditorPane.getPreferredSize());
         });
-        if (size.width <= 0 || size.height <= 0) throw new RuntimeException("Test FAILED: bad preferred size: " + size);
+        if (size.width <= 0 || size.height <= 0) {
+            throw new RuntimeException("Test FAILED: bad preferred size: " + size);
+        }
         System.out.println("Test PASSED");
-    }
-
-    private static void show() {
-        frame = new JFrame("frame");
-        frame.setLayout(new FlowLayout());
-        label = new MyLabel("text");
-        JPanel panel = new JPanel() {
-            @Override
-            public Dimension getPreferredSize() {
-                return label.getPreferredSize();
-            }
-        };
-        panel.add(label);
-        frame.add(panel);
-        frame.pack();
-        frame.setVisible(true);
-    }
-}
-
-class MyLabel extends JLabel {
-    MyLabel(String text) {
-        super(text);
-        setLayout(new BorderLayout());
-
-        JEditorPane myEditorPane = new JEditorPane();
-        myEditorPane.setContentType("text/html");
-        myEditorPane.setText(getText());
-        myEditorPane.setBorder(new EmptyBorder(0, 0, 0, 0));
-        add(myEditorPane, BorderLayout.CENTER);
-
-        // triggers initial BasicTextUI layout
-        getPreferredSize();
-
-        // triggers BasicTextUI.modelChanged()
-        myEditorPane.setFont(getFont());
-    }
-
-    @Override
-    public Dimension getPreferredSize() {
-        Dimension size = getLayout().preferredLayoutSize(this);
-        System.out.println("pref size: " + size);
-        return size;
     }
 }
