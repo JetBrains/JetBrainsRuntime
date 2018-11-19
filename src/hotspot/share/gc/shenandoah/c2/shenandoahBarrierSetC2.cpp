0840cb552c1c0fa5dc730ed5f468d8c818066d5f
shade <none@none>
2018-11-19 20:28:02 +0100

    [backport] is_gc_barrier_node should accept barriers called with both CallLeaf and CallLeafNoFP

--- src/hotspot/share/gc/shenandoah/c2/shenandoahBarrierSetC2.cpp
+++ src/hotspot/share/gc/shenandoah/c2/shenandoahBarrierSetC2.cpp
@@ -698,7 +698,7 @@ Node* ShenandoahBarrierSetC2::resolve_for_write(GraphKit* kit, Node* n) const {
 
 // Support for GC barriers emitted during parsing
 bool ShenandoahBarrierSetC2::is_gc_barrier_node(Node* node) const {
-  if (node->Opcode() != Op_CallLeaf) {
+  if (node->Opcode() != Op_CallLeaf && node->Opcode() != Op_CallLeafNoFP) {
     return false;
   }
   CallLeafNode *call = node->as_CallLeaf();
