52837cd4019c39bda2906377c6721b3cb374dd18
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2018-11-23 17:09:58 +0700

    JRE-430 added new regression test (Font fallback sometimes doesn't work in Swing text components)
    (cherry picked from commit d04debc8472cfefccd726a9288e4c58f99b54f9d)
    
    with fix for JBR-5300 Change source code and test files to use GPL license
    
    (cherry picked from commit 5a47f2a66cb95312dc4e90fbbafc2507e6d2090c)

---
+++ test/jdk/jb/java/awt/Font/font430.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#
+# Copyright 2000-2023 JetBrains s.r.o.
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+#
+# This code is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 only, as
+# published by the Free Software Foundation.
+#
+# This code is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# version 2 for more details (a copy is included in the LICENSE file that
+# accompanied this code).
+#
+# You should have received a copy of the GNU General Public License version
+# 2 along with this work; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+# or visit www.oracle.com if you need additional information or have any
+# questions.
+#
+
+# @test
+# @summary font430.sh checks the 'physical' font object, created for 'Segoe UI' font and font-fallback-enabled one
+#                     are not identical
+# @run shell font430.sh
+
+if [ -z "${TESTSRC}" ]; then
+  echo "TESTSRC undefined: set to ."
+  TESTSRC=.
+fi
+
+if [ -z "${TESTCLASSES}" ]; then
+  echo "TESTCLASSES undefined: set to ."
+  TESTCLASSES=.
+fi
+
+if [ -z "${TESTJAVA}" ]; then
+  echo "TESTJAVA undefined: testing cancelled"
+  exit 1
+fi
+
+cd ${TESTSRC}
+${TESTJAVA}/bin/javac -d ${TESTCLASSES} Font430.java
+
+${TESTJAVA}/bin/java -cp ${TESTCLASSES} Font430 t1.bmp true
+${TESTJAVA}/bin/java -cp ${TESTCLASSES} Font430 t2.bmp true
+
+cmp t1.bmp t2.bmp
+exit_code=$?
+
+case $exit_code in
+0) echo "PASSED"
+   ;;
+*) echo "FAILED: $exit_code"
+   exit 1
+   ;;
+esac
+exit 0
