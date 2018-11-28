ab739c64d86daf16e6167205c2a07b75d20edf4c
clanger <unknown>
2018-11-28 15:53:49 -0500

    8242154: Backport parts of JDK-4947890 to OpenJDK 11u Reviewed-by: sgehwolf

--- src/java.base/share/classes/java/lang/System.java
+++ src/java.base/share/classes/java/lang/System.java
@@ -772,8 +772,11 @@ public final class System {
         if (props == null) {
             props = new Properties();
             initProperties(props);
+            System.props = props;
+            VersionProps.init();
+        } else {
+            System.props = props;
         }
-        System.props = props;
     }
 
     /**
