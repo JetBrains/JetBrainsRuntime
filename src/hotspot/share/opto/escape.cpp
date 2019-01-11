165e0fed913ae3b226b3f864a758b174e5226fd1
roland <unknown>
2019-01-11 10:03:00 +0100

    8216549: Mismatched unsafe access to non escaping object fails Reviewed-by: vlivanov, kvn, thartmann

--- src/hotspot/share/opto/escape.cpp
+++ src/hotspot/share/opto/escape.cpp
@@ -1767,7 +1767,8 @@ void ConnectionGraph::adjust_scalar_replaceable_state(JavaObjectNode* jobj) {
     }
 
     for (DUIterator_Fast imax, i = n->fast_outs(imax); i < imax; i++) {
-      if (n->fast_out(i)->is_LoadStore()) {
+      Node* u = n->fast_out(i);
+      if (u->is_LoadStore() || (u->is_Mem() && u->as_Mem()->is_mismatched_access())) {
         jobj->set_scalar_replaceable(false);
         return;
       }
