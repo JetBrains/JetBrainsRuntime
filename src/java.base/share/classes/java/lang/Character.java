118e0312fc79d16744f472818bc4b2ab88acd144
Deepak Kejriwal <deepak.kejriwal@oracle.com>
2019-01-29 12:00:35 +0000

    8216546: Support new Japanese era in java.lang.Character for Java SE 11
    Reviewed-by: chegar, naoto, rriggs, coffeys

--- src/java.base/share/classes/java/lang/Character.java
+++ src/java.base/share/classes/java/lang/Character.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2002, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -34,27 +34,38 @@ import jdk.internal.HotSpotIntrinsicCandidate;
 
 /**
  * The {@code Character} class wraps a value of the primitive
- * type {@code char} in an object. An object of type
+ * type {@code char} in an object. An object of class
  * {@code Character} contains a single field whose type is
  * {@code char}.
  * <p>
- * In addition, this class provides several methods for determining
- * a character's category (lowercase letter, digit, etc.) and for converting
- * characters from uppercase to lowercase and vice versa.
+ * In addition, this class provides a large number of static methods for
+ * determining a character's category (lowercase letter, digit, etc.)
+ * and for converting characters from uppercase to lowercase and vice
+ * versa.
+ *
+ * <h3><a id="conformance">Unicode Conformance</a></h3>
  * <p>
- * Character information is based on the Unicode Standard, version 10.0.0.
+ * The fields and methods of class {@code Character} are defined in terms
+ * of character information from the Unicode Standard, specifically the
+ * <i>UnicodeData</i> file that is part of the Unicode Character Database.
+ * This file specifies properties including name and category for every
+ * assigned Unicode code point or character range. The file is available
+ * from the Unicode Consortium at
+ * <a href="http://www.unicode.org">http://www.unicode.org</a>.
  * <p>
- * The methods and data of class {@code Character} are defined by
- * the information in the <i>UnicodeData</i> file that is part of the
- * Unicode Character Database maintained by the Unicode
- * Consortium. This file specifies various properties including name
- * and general category for every defined Unicode code point or
- * character range.
- * <p>
- * The file and its description are available from the Unicode Consortium at:
- * <ul>
- * <li><a href="http://www.unicode.org">http://www.unicode.org</a>
- * </ul>
+ * The Java SE 11 Platform uses character information from version 10.0
+ * of the Unicode Standard, with an extension. The Java SE 11 Platform allows
+ * an implementation of class {@code Character} to use the Japanese Era
+ * code point, {@code U+32FF}, from the first version of the Unicode Standard
+ * after 10.0 that assigns the code point. Consequently, the behavior of
+ * fields and methods of class {@code Character} may vary across
+ * implementations of the Java SE 11 Platform when processing the
+ * aforementioned code point ( outside of version 10.0 ), except for
+ * the following methods that define Java identifiers:
+ * {@link #isJavaIdentifierStart(int)}, {@link #isJavaIdentifierStart(char)},
+ * {@link #isJavaIdentifierPart(int)}, and {@link #isJavaIdentifierPart(char)}.
+ * Code points in Java identifiers must be drawn from version 10.0 of
+ * the Unicode Standard.
  *
  * <h3><a id="unicode">Unicode Character Representations</a></h3>
  *
@@ -9249,13 +9260,16 @@ class Character implements java.io.Serializable, Comparable<Character> {
      * character in a Java identifier.
      * <p>
      * A character may start a Java identifier if and only if
-     * one of the following is true:
+     * one of the following conditions is true:
      * <ul>
      * <li> {@link #isLetter(char) isLetter(ch)} returns {@code true}
      * <li> {@link #getType(char) getType(ch)} returns {@code LETTER_NUMBER}
      * <li> {@code ch} is a currency symbol (such as {@code '$'})
      * <li> {@code ch} is a connecting punctuation character (such as {@code '_'}).
      * </ul>
+     *
+     * These conditions are tested against the character information from version
+     * 10.0 of the Unicode Standard.
      *
      * @param   ch the character to be tested.
      * @return  {@code true} if the character may start a Java
@@ -9278,8 +9292,8 @@ class Character implements java.io.Serializable, Comparable<Character> {
      * Determines if the specified character may be part of a Java
      * identifier as other than the first character.
      * <p>
-     * A character may be part of a Java identifier if and only if any
-     * of the following are true:
+     * A character may be part of a Java identifier if and only if one
+     * of the following conditions is true:
      * <ul>
      * <li>  it is a letter
      * <li>  it is a currency symbol (such as {@code '$'})
@@ -9291,6 +9305,9 @@ class Character implements java.io.Serializable, Comparable<Character> {
      * <li> {@code isIdentifierIgnorable} returns
      * {@code true} for the character.
      * </ul>
+     *
+     * These conditions are tested against the character information from version
+     * 10.0 of the Unicode Standard.
      *
      * @param   ch the character to be tested.
      * @return  {@code true} if the character may be part of a
@@ -9369,6 +9386,9 @@ class Character implements java.io.Serializable, Comparable<Character> {
      * <li> {@code ch} is a connecting punctuation character (such as {@code '_'}).
      * </ul>
      *
+     * These conditions are tested against the character information from version
+     * 10.0 of the Unicode Standard.
+     *
      * <p><b>Note:</b> This method cannot handle <a
      * href="#supplementary"> supplementary characters</a>. To support
      * all Unicode characters, including supplementary characters, use
@@ -9403,6 +9423,9 @@ class Character implements java.io.Serializable, Comparable<Character> {
      *      (such as {@code '_'}).
      * </ul>
      *
+     * These conditions are tested against the character information from version
+     * 10.0 of the Unicode Standard.
+     *
      * @param   codePoint the character (Unicode code point) to be tested.
      * @return  {@code true} if the character may start a Java identifier;
      *          {@code false} otherwise.
@@ -9421,7 +9444,7 @@ class Character implements java.io.Serializable, Comparable<Character> {
      * identifier as other than the first character.
      * <p>
      * A character may be part of a Java identifier if any of the following
-     * are true:
+     * conditions are true:
      * <ul>
      * <li>  it is a letter
      * <li>  it is a currency symbol (such as {@code '$'})
@@ -9433,6 +9456,9 @@ class Character implements java.io.Serializable, Comparable<Character> {
      * <li> {@code isIdentifierIgnorable} returns
      * {@code true} for the character
      * </ul>
+     *
+     * These conditions are tested against the character information from version
+     * 10.0 of the Unicode Standard.
      *
      * <p><b>Note:</b> This method cannot handle <a
      * href="#supplementary"> supplementary characters</a>. To support
@@ -9458,7 +9484,7 @@ class Character implements java.io.Serializable, Comparable<Character> {
      * identifier as other than the first character.
      * <p>
      * A character may be part of a Java identifier if any of the following
-     * are true:
+     * conditions are true:
      * <ul>
      * <li>  it is a letter
      * <li>  it is a currency symbol (such as {@code '$'})
@@ -9469,8 +9495,11 @@ class Character implements java.io.Serializable, Comparable<Character> {
      * <li>  it is a non-spacing mark
      * <li> {@link #isIdentifierIgnorable(int)
      * isIdentifierIgnorable(codePoint)} returns {@code true} for
-     * the character
+     * the code point
      * </ul>
+     *
+     * These conditions are tested against the character information from version
+     * 10.0 of the Unicode Standard.
      *
      * @param   codePoint the character (Unicode code point) to be tested.
      * @return {@code true} if the character may be part of a
