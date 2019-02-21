389c63ac0d9dc084dc90226a41c5d6ee1d215c7b
Anton Tarasov <anton.tarasov@jetbrains.com>
2019-02-21 20:03:47 +0300

    JRE-1232 forwardport: JRE-1228 support custom frame decoration
    (cherry picked from commit d2820524a1aa211b1c49b30f659b9b4d07a6f96e)
    (cherry picked from commit 1ebd6a000b3360a54c51acc31a857a4b8067ff3c)

--- src/java.desktop/windows/native/libawt/windows/awt_Window.h
+++ src/java.desktop/windows/native/libawt/windows/awt_Window.h
@@ -176,7 +176,7 @@ public:
     virtual MsgRouting WmSettingChange(UINT wFlag, LPCTSTR pszSection);
     virtual MsgRouting WmNcCalcSize(BOOL fCalcValidRects,
                                     LPNCCALCSIZE_PARAMS lpncsp, LRESULT& retVal);
-    virtual MsgRouting WmNcHitTest(UINT x, UINT y, LRESULT& retVal);
+    virtual MsgRouting WmNcHitTest(int x, int y, LRESULT& retVal);
     virtual MsgRouting WmNcMouseDown(WPARAM hitTest, int x, int y, int button);
     virtual MsgRouting WmGetIcon(WPARAM iconType, LRESULT& retVal);
     virtual LRESULT WindowProc(UINT message, WPARAM wParam, LPARAM lParam);
@@ -256,6 +256,8 @@ public:
 
     inline HWND GetOverriddenHWnd() { return m_overriddenHwnd; }
     inline void OverrideHWnd(HWND hwnd) { m_overriddenHwnd = hwnd; }
+
+    virtual BOOL HasCustomDecoration() { return FALSE; }
 
 private:
     static int ms_instanceCounter;
