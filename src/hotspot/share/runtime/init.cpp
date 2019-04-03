d3e987b137096b0046aaeac40f74b4b9ea46f0f7
lucy <unknown>
2019-04-03 16:55:41 +0200

    8221482: Initialize VMRegImpl::regName[] earlier to prevent assert during PrintStubCode Reviewed-by: kvn

--- src/hotspot/share/runtime/init.cpp
+++ src/hotspot/share/runtime/init.cpp
@@ -119,6 +119,7 @@ jint init_globals() {
   accessFlags_init();
   templateTable_init();
   InterfaceSupport_init();
+  VMRegImpl::set_regName();  // need this before generate_stubs (for printing oop maps).
   SharedRuntime::generate_stubs();
   universe2_init();  // dependent on codeCache_init and stubRoutines_init1
   javaClasses_init();// must happen after vtable initialization, before referenceProcessor_init
@@ -136,7 +137,6 @@ jint init_globals() {
   if (!compileBroker_init()) {
     return JNI_EINVAL;
   }
-  VMRegImpl::set_regName();
 
   if (!universe_post_init()) {
     return JNI_ERR;
