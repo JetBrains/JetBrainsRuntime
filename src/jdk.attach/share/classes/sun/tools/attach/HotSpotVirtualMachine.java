f40217f012c9b7a9727af408e835c80a352d89aa
Egor Ushakov <egor.ushakov@jetbrains.com>
2019-04-09 11:35:16 +0300

    JBR-1354 com/sun/tools/attach/PermissionTest.java: access denied ("java.util.PropertyPermission" "sun.tools.attach.tmp.only" "read")
    (cherry picked from commit 3a09f6c1dbeab41c4b2fd6b789b3c462dda37981)
    (cherry picked from commit 3b2399fc35e6ae52038afdfd1fd516bb36533d09)

--- src/jdk.attach/share/classes/sun/tools/attach/HotSpotVirtualMachine.java
+++ src/jdk.attach/share/classes/sun/tools/attach/HotSpotVirtualMachine.java
@@ -578,6 +578,10 @@ public abstract class HotSpotVirtualMachine extends VirtualMachine {
     }
 
     boolean attachOnlyInTmp() {
-        return Boolean.getBoolean("sun.tools.attach.tmp.only");
+        try {
+            return Boolean.getBoolean("sun.tools.attach.tmp.only");
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
