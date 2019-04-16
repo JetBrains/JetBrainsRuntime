77427db89ec140a8b2a562467fb81ef9a478885b
Xue-Lei Andrew Fan <xuelei@openjdk.org>
2019-04-16 16:59:09 -0700

    8216326: SSLSocket stream close() does not close the associated socket
    Reviewed-by: jnimeh

--- src/java.base/share/classes/sun/security/ssl/SSLSocketImpl.java
+++ src/java.base/share/classes/sun/security/ssl/SSLSocketImpl.java
@@ -878,7 +878,7 @@ public final class SSLSocketImpl
             }
 
             try {
-                shutdownInput(false);
+                SSLSocketImpl.this.close();
             } catch (IOException ioe) {
                 // ignore the exception
                 if (SSLLogger.isOn && SSLLogger.isOn("ssl")) {
@@ -1003,7 +1003,7 @@ public final class SSLSocketImpl
             }
 
             try {
-                shutdownOutput();
+                SSLSocketImpl.this.close();
             } catch (IOException ioe) {
                 // ignore the exception
                 if (SSLLogger.isOn && SSLLogger.isOn("ssl")) {
