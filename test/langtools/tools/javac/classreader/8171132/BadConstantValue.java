50cdd824b835855b923b69391f65d2b80a676db9
mdoerr <unknown>
2019-04-23 11:30:43 +0200

    8220625: tools/javac/classreader/8171132/BadConstantValue.java failed with "did not see expected error" Reviewed-by: clanger

--- test/langtools/tools/javac/classreader/8171132/BadConstantValue.java
+++ test/langtools/tools/javac/classreader/8171132/BadConstantValue.java
@@ -136,6 +136,8 @@ public class BadConstantValue {
                 "class Lib { static final int A = %s; static final %s B = %s; }",
                 value, type, (type.equals("boolean") ? "false" : "0")));
         compile("-d", classesdir.getPath(), lib.getPath());
+        // Lib.class may possibly not get a newer timestamp. Make sure .java file won't get used.
+        lib.delete();
         File libClass = new File(classesdir, "Lib.class");
         // Rewrite the class to only have field B of type "type" and with "value" (potentially
         // out of range).
@@ -149,6 +151,8 @@ public class BadConstantValue {
                 "class Lib { static final String A = \"hello\"; static final %s CONST = %s; }",
                 type, type.equals("boolean") ? "false" : "0"));
         compile("-d", classesdir.getPath(), lib.getPath());
+        // Lib.class may possibly not get a newer timestamp. Make sure .java file won't get used.
+        lib.delete();
         File libClass = new File(classesdir, "Lib.class");
         swapConstantValues(libClass);
 
