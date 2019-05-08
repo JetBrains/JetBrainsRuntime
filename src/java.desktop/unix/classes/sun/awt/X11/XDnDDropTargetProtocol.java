84cd72ceba370897e054510ab8bc15909d41abfe
Denis Fokin <Denis.Fokin@jetbrains.com>
2019-05-08 09:45:51 +0300

    JBR-1414 DnD on linux  (XToolkit) does not honor HIDPI scale
    (cherry picked from commit 85dfaefecb75d2876d299e444dcba8c2009f9b2b)

--- src/java.desktop/unix/classes/sun/awt/X11/XDnDDropTargetProtocol.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XDnDDropTargetProtocol.java
@@ -586,16 +586,19 @@ class XDnDDropTargetProtocol extends XDropTargetProtocol {
             return false;
         }
 
+        x = (int)(xclient.get_data(2) >> 16);
+        y = (int)(xclient.get_data(2) & 0xFFFF);
+
         XWindow xwindow = null;
         {
             XBaseWindow xbasewindow = XToolkit.windowToXWindow(xclient.get_window());
             if (xbasewindow instanceof XWindow) {
                 xwindow = (XWindow)xbasewindow;
+                x = xbasewindow.scaleDown(x);
+                y = xbasewindow.scaleDown(y);
             }
         }
 
-        x = (int)(xclient.get_data(2) >> 16);
-        y = (int)(xclient.get_data(2) & 0xFFFF);
 
         if (xwindow == null) {
             long receiver =
