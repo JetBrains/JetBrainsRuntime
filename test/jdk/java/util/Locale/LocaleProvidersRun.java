18b07b5b7ee9a7d7be28e6e291d223f2141d6151
clanger <unknown>
2019-06-28 20:46:25 +0100

    8226869: Test java/util/Locale/LocaleProvidersRun.java should enable assertions Reviewed-by: naoto
    (cherry picked from commit 2d8557b92eefd49640639fdae20815df3788d6dd)

--- test/jdk/java/util/Locale/LocaleProvidersRun.java
+++ test/jdk/java/util/Locale/LocaleProvidersRun.java
@@ -153,7 +153,9 @@ public class LocaleProvidersRun {
     private static void testRun(String prefList, String methodName,
             String param1, String param2, String param3) throws Throwable{
         JDKToolLauncher launcher = JDKToolLauncher.createUsingTestJDK("java");
-        launcher.addToolArg("-cp")
+        launcher.addToolArg("-ea")
+                .addToolArg("-esa")
+                .addToolArg("-cp")
                 .addToolArg(Utils.TEST_CLASS_PATH)
                 .addToolArg("-Djava.locale.providers=" + prefList)
                 .addToolArg("LocaleProviders")
