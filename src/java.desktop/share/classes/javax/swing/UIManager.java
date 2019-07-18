15bd87a24808341b1aa8c7737259fb7147725e3c
Anton Tarasov <anton.tarasov@jetbrains.com>
2019-07-18 17:01:18 +0300

    JBR-1669 IDE-managed HiDPI mode is broken
    (cherry picked from commit 461b0b5cd4c9a770497f7a6e4ef0bcbf59d08aea)
    (cherry picked from commit 3049d42f8cdbc4187aa11ecf64ef93314d586f83)

--- src/java.desktop/share/classes/javax/swing/UIManager.java
+++ src/java.desktop/share/classes/javax/swing/UIManager.java
@@ -58,8 +58,8 @@ import java.util.HashMap;
 import java.util.Objects;
 import sun.awt.AppContext;
 import sun.awt.AWTAccessor;
-
 import sun.swing.SwingAccessor;
+import java.awt.GraphicsEnvironment;
 
 /**
  * {@code UIManager} manages the current look and feel, the set of
@@ -375,6 +375,11 @@ public class UIManager implements Serializable
         if (osType == OSInfo.OSType.WINDOWS) {
             iLAFs.add(new LookAndFeelInfo("Windows",
                         "com.sun.java.swing.plaf.windows.WindowsLookAndFeel"));
+            if (!GraphicsEnvironment.isHeadless()) {
+                // Force DPI settings in Win32GraphicsEnvironment.initDisplayWrapper()
+                // prior to font size settings in desktop properties.
+                GraphicsEnvironment.getLocalGraphicsEnvironment();
+            }
             if (Toolkit.getDefaultToolkit().getDesktopProperty(
                     "win.xpstyle.themeActive") != null) {
                 iLAFs.add(new LookAndFeelInfo("Windows Classic",
