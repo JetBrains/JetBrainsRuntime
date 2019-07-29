0cb04c8fdae10ffc8e77089696c3974168074c29
Alexey Ushakov <Alexey.Ushakov@jetbrains.com>
2019-07-29 18:00:23 +0200

    JBR-1699 Use platform font rendering for bundled fonts on MacOS
    Used CFont instead of TrueTypeFont for bundled fonts on mac
    Use different family for specific font faces. Refactoring
    
    (cherry picked from commit 8c86ad3e961a0ba00733db670632f781a2b7cb82)
    (cherry picked from commit c423003bd492a4c53d44d0d5ad52b95cfd14a781)
    (cherry picked from commit bcae402dc8e9de4bbbdf861276fb1051edb4f399)

--- src/java.desktop/macosx/classes/sun/font/CFontManager.java
+++ src/java.desktop/macosx/classes/sun/font/CFontManager.java
@@ -27,6 +27,7 @@ package sun.font;
 
 import java.awt.*;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.ArrayList;
@@ -34,13 +35,11 @@ import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Locale;
 import java.util.TreeMap;
-import java.util.Vector;
 
 import javax.swing.plaf.FontUIResource;
 
 import sun.awt.FontConfiguration;
 import sun.awt.HeadlessToolkit;
-import sun.awt.util.ThreadGroupUtils;
 import sun.lwawt.macosx.*;
 
 public final class CFontManager extends SunFontManager {
@@ -141,6 +140,18 @@ public final class CFontManager extends SunFontManager {
         }
     }
 
+    @Override
+    protected void registerJREFonts() {
+        String[] files = AccessController.doPrivileged((PrivilegedAction<String[]>) () ->
+                new File(jreFontDirName).list(getTrueTypeFilter()));
+
+        if (files != null) {
+            for (String f : files) {
+                loadNativeDirFonts(jreFontDirName + File.separator + f);
+            }
+        }
+    }
+
     protected void registerFontsInDir(final String dirName, boolean useJavaRasterizer,
                                       int fontRank, boolean defer, boolean resolveSymLinks) {
 
@@ -163,6 +174,11 @@ public final class CFontManager extends SunFontManager {
     private native void loadNativeFonts();
 
     void registerFont(String fontName, String fontFamilyName) {
+        // Use different family for specific font faces
+        String newFontFamily = jreFamilyMap.get(fontName);
+        if (newFontFamily != null) {
+            fontFamilyName = newFontFamily;
+        }
         final CFont font = new CFont(fontName, fontFamilyName);
 
         registerGenericFont(font);
