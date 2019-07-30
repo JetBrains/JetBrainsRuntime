59d5657530784ac6fd1fb226f4df93b7e80d732d
Aleksey Shipilev <shade@openjdk.org>
2019-07-30 22:16:48 +0200

    8228725: AArch64: Purge method call format support
    Reviewed-by: adinn

--- src/hotspot/cpu/aarch64/macroAssembler_aarch64.hpp
+++ src/hotspot/cpu/aarch64/macroAssembler_aarch64.hpp
@@ -1170,8 +1170,6 @@ public:
   //
 
   public:
-  // enum used for aarch64--x86 linkage to define return type of x86 function
-  enum ret_type { ret_type_void, ret_type_integral, ret_type_float, ret_type_double};
 
   void ldr_constant(Register dest, const Address &const_addr) {
     if (NearCpool) {
