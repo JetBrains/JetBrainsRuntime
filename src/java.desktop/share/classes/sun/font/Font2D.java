b219ce9b49f3be8d0ace8553ac3617e5b9c25d07
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2019-07-31 14:13:03 +0300

    JBR-1699 Use platform font rendering for bundled fonts on MacOS
    Use different family for specific font faces. Refactoring
    
    JBR-3071 Remove naming workaround for Fira Code
    
    (cherry picked from commits c423003bd492a4c53d44d0d5ad52b95cfd14a781, aee4b48d20d09f05cd0b67fd120621f8a3209f8a)

--- src/java.desktop/share/classes/sun/font/Font2D.java
+++ src/java.desktop/share/classes/sun/font/Font2D.java
@@ -55,7 +55,6 @@ public abstract class Font2D {
     public static final int NATIVE_RANK  = 5;
     public static final int UNKNOWN_RANK = 6;
     public static final int DEFAULT_RANK = 4;
-    public static final int IDEA_RANK    = 7;
 
     private static final String[] boldNames = {
         "bold", "demibold", "demi-bold", "demi bold", "negreta", "demi", };
