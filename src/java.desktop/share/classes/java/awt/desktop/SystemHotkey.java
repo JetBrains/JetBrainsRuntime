bdcf5585f0affe21459913cd4a9d5265d3241ba7
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2019-08-02 19:15:15 +0300

    JBR-1668: add hardcoded descriptions of system actions
    and minor fixes
    
    fix memory management
    
    (cherry picked from commit 15f736830978c1a6e80be5c529ae7e590297a743)

--- src/java.desktop/share/classes/java/awt/desktop/SystemHotkey.java
+++ src/java.desktop/share/classes/java/awt/desktop/SystemHotkey.java
@@ -1,82 +1,39 @@
 package java.awt.desktop;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Collection;
 
 import sun.util.logging.PlatformLogger;
 
+import javax.swing.*;
+import java.awt.event.KeyEvent;
 import java.awt.event.InputEvent;
 
 /**
  * Provides info about system hotkeys
  */
-public class SystemHotkey {
-    private static final PlatformLogger ourLog = PlatformLogger.getLogger(SystemHotkey.class.getName());
-    private static final Map<Integer, String> ourCache = new HashMap<>();
+public class SystemHotkey extends AWTKeyStroke {
+    private static final long serialVersionUID = -6593119259058157651L;
+    private static final PlatformLogger ourLog = PlatformLogger.getLogger(java.awt.desktop.SystemHotkey.class.getName());
+    private static final Map<Integer, String> ourCodeDescriptionCache = new HashMap<>();
 
-    private final boolean myEnabled;
-    private final int myKeyCode;
-    private final int myModifiers;
-    private final String myChar;
-    private final String mySource;
+    private final int myNativeKeyCode;
     private final String myDescription;
-
-    SystemHotkey(boolean enabled, int keyCode, int modifiers, String keyChar, String source, String description) {
-        this.myEnabled = enabled;
-        this.myKeyCode = keyCode;
-        this.myModifiers = modifiers;
-        this.myChar = keyChar;
-        this.mySource = source;
-        this.myDescription = description;
+    
+    SystemHotkey(char keyChar, int javaKeyCode, int javaModifiers, String description, int nativeKeyCode) {
+      super(keyChar, javaKeyCode, javaModifiers, true);
+      this.myNativeKeyCode = nativeKeyCode;
+      this.myDescription = description;
     }
-
+    
     public String toString() {
-        return String.format("enable=%s vkey=0x%X ['%s'] mod='%s' char=%s src=%s desc='%s'",
-                String.valueOf(myEnabled),
-                myKeyCode, getVirtualCodeDescription(myKeyCode), InputEvent.getModifiersExText(myModifiers),
-                String.valueOf(myChar), String.valueOf(mySource), String.valueOf(myDescription));
-    }
-
-    /**
-     * Checks whether hotkey is enabled
-     * @return true when hotkey is active
-     */
-    public boolean isEnabled() {
-        return myEnabled;
-    }
-
-    /**
-     * Gets virtual keycode of hotkey
-     * @return virtual keycode
-     */
-    public int getKeyCode() {
-        return myKeyCode;
-    }
-
-    /**
-     * Gets modifiers of hotkey
-     * @return modifiers of hotkey
-     */
-    public int getModifiers() {
-        return myModifiers;
-    }
-
-    /**
-     * Gets char (or string description) of hotkey
-     * @return char (or string description) of hotkey
-     */
-    public String getChar() {
-        return myChar;
-    }
-
-    /**
-     * Gets source description
-     * @return source description
-     */
-    public String getSource() {
-        return mySource;
+      return String.format("desc='%s' char=%s mod='%s' nativeKeyCode=0x%X ['%s'] javaKeyCode=0x%X",
+              String.valueOf(myDescription), String.valueOf(getKeyChar()), InputEvent.getModifiersExText(getModifiers()),
+              myNativeKeyCode, getOsxKeyCodeDescription(myNativeKeyCode), getKeyCode());
     }
 
     /**
@@ -102,24 +59,30 @@ public class SystemHotkey {
      * @param code (virtual keycode)
      * @return virtual keycode description
      */
-    public static String getVirtualCodeDescription(int code) {
-        return ourCache.computeIfAbsent(code, (c)->{
-            final String desc = virtualCodeDescription(c);
+    private static String getOsxKeyCodeDescription(int code) {
+        return ourCodeDescriptionCache.computeIfAbsent(code, (c)->{
+            final String desc = osxKeyCodeDescription(c);
             return desc == null || desc.isEmpty() ? String.format("Unknown_key_code_0x%X", c) : desc;
         });
     }
 
-    private static native String virtualCodeDescription(int code);
+    private static native String osxKeyCodeDescription(int osxCode);
 }
 
 class SystemHotkeyReader {
     private final List<SystemHotkey> myResult = new ArrayList<>();
 
-    void add(boolean enabled, int keyCode, String keyChar, int modifiers, String source, String desc) {
-        myResult.add(new SystemHotkey(enabled, keyCode, modifiers, keyChar, source, desc));
+    void add(int keyCode, String keyChar, int modifiers, String desc) {
+        myResult.add(new SystemHotkey(
+                keyChar == null || keyChar.isEmpty() ? KeyEvent.CHAR_UNDEFINED : keyChar.charAt(0),
+                keyCode == -1 ? KeyEvent.VK_UNDEFINED : osx2java(keyCode),
+                modifiers, desc, keyCode
+        ));
     }
 
     List<SystemHotkey> getResult() { return myResult; }
 
     native void readSystemHotkeys();
+
+    private static native int osx2java(int osxCode);
 }
