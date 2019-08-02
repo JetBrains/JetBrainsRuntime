ddae97296d945003f47e8a7273ec7efcf7051a0a
Markus Grönlund <mgronlun@openjdk.org>
2019-08-02 10:43:30 +0200

    8228834: Regression caused by JDK-8214542 not installing complete checkpoint data to candidates
    Reviewed-by: egahlin

--- src/hotspot/share/jfr/recorder/checkpoint/types/jfrType.cpp
+++ src/hotspot/share/jfr/recorder/checkpoint/types/jfrType.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -310,7 +310,7 @@ void ClassUnloadTypeSet::serialize(JfrCheckpointWriter& writer) {
   if (LeakProfiler::is_running()) {
     JfrCheckpointWriter leakp_writer(false, true, Thread::current());
     type_set.write(writer, &leakp_writer);
-    ObjectSampleCheckpoint::install(leakp_writer, true);
+    ObjectSampleCheckpoint::install(leakp_writer, true, true);
     return;
   }
   type_set.write(writer, NULL);
@@ -321,7 +321,7 @@ void TypeSet::serialize(JfrCheckpointWriter& writer) {
   if (LeakProfiler::is_running()) {
     JfrCheckpointWriter leakp_writer(false, true, Thread::current());
     type_set.write(writer, &leakp_writer);
-    ObjectSampleCheckpoint::install(leakp_writer, false);
+    ObjectSampleCheckpoint::install(leakp_writer, false, true);
     return;
   }
   type_set.write(writer, NULL);
