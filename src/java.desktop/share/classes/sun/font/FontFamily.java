d45a8d2113e3ff12c5f686e012c6f0438335de87
Dennis Ushakov <dennis.ushakov@gmail.com>
2019-08-07 12:13:57 +0300

    faster font family loading & lazy font family population
    1. NSFont.familyName is faster than loading allFamilyNames
    2. Prebuilt list of system fonts
    3. Cleanup San Francisco family loading
    4. Avoid calling expensive native getWidth on font when creating font family, load styles only when they would be used.

--- src/java.desktop/share/classes/sun/font/FontFamily.java
+++ src/java.desktop/share/classes/sun/font/FontFamily.java
@@ -28,7 +28,9 @@ package sun.font;
 import java.io.File;
 import java.awt.Font;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.Locale;
@@ -44,6 +46,8 @@ public class FontFamily {
     protected Font2D bold;
     protected Font2D italic;
     protected Font2D bolditalic;
+    private final List<FontAndStyle> fontSequence = new ArrayList<FontAndStyle>();
+    private boolean initialized = false;
     protected boolean logicalFont = false;
     protected int familyRank;
 
@@ -66,6 +70,7 @@ public class FontFamily {
         if (family == null) {
             return;
         }
+        family.ensureFontsLoaded();
         if (family.plain == font2D) {
             family.plain = null;
         }
@@ -244,16 +249,48 @@ public class FontFamily {
             }
             FontUtilities.getLogger().info(msg);
         }
+        synchronized (fontSequence) {
+            if (initialized) {
+                doSetFont(font, style);
+                return;
+            }
+            fontSequence.add(new FontAndStyle(font, style));
+        }
+    }
+
+    private void ensureFontsLoaded() {
+        synchronized (fontSequence) {
+            if (initialized) {
+                return;
+            }
+            for (FontAndStyle fontAndStyle : fontSequence) {
+                doSetFont(fontAndStyle.font, fontAndStyle.style);
+            }
+            if (italic == null && plain instanceof FontWithDerivedItalic) {
+                italic = ((FontWithDerivedItalic)plain).createItalic();
+            }
+            if (bolditalic == null) {
+                Font2D boldItalicPrototype = bold != null ? bold : plain;
+                if (boldItalicPrototype instanceof FontWithDerivedItalic) {
+                    bolditalic = ((FontWithDerivedItalic)boldItalicPrototype).createItalic();
+                }
+            }
+            fontSequence.clear();
+            initialized = true;
+        }
+    }
+
+    private void doSetFont(Font2D font, int style) {
         /* Allow a lower-rank font only if its a file font
          * from the exact same source as any previous font.
          */
         if ((font.getRank() > familyRank) && !isFromSameSource(font)) {
             if (FontUtilities.isLogging()) {
                 FontUtilities.getLogger()
-                                  .warning("Rejecting adding " + font +
-                                           " of lower rank " + font.getRank() +
-                                           " to family " + this +
-                                           " of rank " + familyRank);
+                  .warning("Rejecting adding " + font +
+                           " of lower rank " + font.getRank() +
+                           " to family " + this +
+                           " of rank " + familyRank);
             }
             return;
         }
@@ -290,7 +327,7 @@ public class FontFamily {
     }
 
     public Font2D getFontWithExactStyleMatch(int style) {
-
+        ensureFontsLoaded();
         switch (style) {
 
         case Font.PLAIN:
@@ -319,7 +356,7 @@ public class FontFamily {
      * same location.
      */
     public Font2D getFont(int style) {
-
+        ensureFontsLoaded();
         switch (style) {
 
         case Font.PLAIN:
@@ -367,7 +404,7 @@ public class FontFamily {
      * it might as well be from the same family.
      */
      Font2D getClosestStyle(int style) {
-
+        ensureFontsLoaded();
         switch (style) {
             /* if you ask for a plain font try to return a non-italic one,
              * then a italic one, finally a bold italic one */
@@ -444,8 +481,19 @@ public class FontFamily {
             " plain="+plain+
             " bold=" + bold +
             " italic=" + italic +
-            " bolditalic=" + bolditalic;
+            " bolditalic=" + bolditalic +
+            " initialized=" + initialized;
 
+    }
+
+    private static class FontAndStyle {
+        private final Font2D font;
+        private final int style;
+
+        private FontAndStyle(Font2D inFont, int inStyle) {
+            font = inFont;
+            style = inStyle;
+        }
     }
 
 }
