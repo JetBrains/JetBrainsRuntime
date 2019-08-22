44e9d1f3c1a27d6596de6d6cfe52514b93b46672
Alexey Ushakov <Alexey.Ushakov@jetbrains.com>
2019-08-22 18:59:59 +0300

    JBR-1778 Font in editor incorrect (always italics)
    Added -it pattern into italic detection code
    Added some more patterns to bold and italic detection code
    'Anka/Coder' font support
    
    (cherry picked from commit 5119eeee12076a99816f0bab264e5723e0ef238c)
    (cherry picked from commit ec241e4a0a932dbcb96b91108d564f41a4a338d1)
    (cherry picked from commit 251068294ee28697885cf6442f6fbbc27bfa13ec)
    (cherry picked from commit 08ae9ff034ee78d49bc477f49436e9bdc2c26197)
    (cherry picked from commit b472b89fba7950fd49e6b95bc3a17344feb3ed4d)

--- src/java.desktop/share/classes/sun/font/Font2D.java
+++ src/java.desktop/share/classes/sun/font/Font2D.java
@@ -57,15 +57,16 @@ public abstract class Font2D {
     public static final int DEFAULT_RANK = 4;
 
     private static final String[] boldNames = {
-        "bold", "demibold", "demi-bold", "demi bold", "negreta", "demi", };
+        "bold", "demibold", "demi-bold", "demi bold", "negreta", "demi", "black", "coder-b"};
 
     private static final String[] italicNames = {
-        "italic", "cursiva", "oblique", "inclined", };
+        "italic", "cursiva", "oblique", "inclined", "-it", "-lightit", "coder-i"};
 
     private static final String[] boldItalicNames = {
           "bolditalic", "bold-italic", "bold italic",
           "boldoblique", "bold-oblique", "bold oblique",
-          "demibold italic", "negreta cursiva","demi oblique", };
+          "demibold italic", "negreta cursiva","demi oblique",
+          "-boldit", "-blackit", "coder-bi"};
 
     private static final FontRenderContext DEFAULT_FRC =
         new FontRenderContext(null, false, false);
