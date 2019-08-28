e1c6172e9a945c809aee89e6259359577d0c5002
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2019-08-28 17:06:25 +0300

    JBR-1777 Wrong glyphs are rendered for Roboto font

--- src/java.desktop/share/classes/sun/font/FontFamily.java
+++ src/java.desktop/share/classes/sun/font/FontFamily.java
@@ -50,6 +50,7 @@ public class FontFamily {
     private boolean initialized = false;
     protected boolean logicalFont = false;
     protected int familyRank;
+    private byte stylesMask;
 
     public static FontFamily getFamily(String name) {
         return familyNameMap.get(name.toLowerCase(Locale.ENGLISH));
@@ -295,6 +296,14 @@ public class FontFamily {
             return;
         }
 
+        if (FontUtilities.isWindows && stylesMask != 0x10) {
+            // Windows seems to ignore bold attribute from fsSelection field, and consider only font's weight
+            int winFontStyle = (font.getWeight() >= 410 ? 1 : 0) | (style & Font.ITALIC);
+            byte styleBit = (byte) (1 << winFontStyle);
+            byte newMask = (byte) (stylesMask | styleBit);
+            stylesMask = newMask == stylesMask ? 0x10 : newMask;
+        }
+
         switch (style) {
 
         case Font.PLAIN:
@@ -473,6 +482,10 @@ public class FontFamily {
     public static FontFamily[] getAllFontFamilies() {
        Collection<FontFamily> families = familyNameMap.values();
        return families.toArray(new FontFamily[0]);
+    }
+
+    boolean hasMultipleCandidatesForSameStyle() {
+        return stylesMask == 0x10;
     }
 
     public String toString() {
