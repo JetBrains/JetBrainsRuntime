ba69b0d47dd9d52ccf36c4afd3cbaae4d4f9b1c3
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2019-08-29 20:59:47 +0300

    Revert all attempts to fix JBR-1076

--- src/java.desktop/share/classes/javax/swing/BufferStrategyPaintManager.java
+++ src/java.desktop/share/classes/javax/swing/BufferStrategyPaintManager.java
@@ -495,20 +495,6 @@ class BufferStrategyPaintManager extends RepaintManager.PaintManager {
                         LOGGER.finer("prepare: contents lost on expose");
                     }
                 }
-
-                if (isPaint) {
-                    // Fallback to normal painting in undecorated non-opaque dialogs
-                    // and frames to resolve black background problem
-                    Window window = SunToolkit.getContainingWindow(c);
-                    if ((window instanceof Dialog && ((Dialog)window).isUndecorated() ||
-                            window instanceof Frame && ((Frame)window).isUndecorated()) &&
-                            bsg != null && !window.isOpaque() &&
-                            ((SunGraphics2D)bsg).getSurfaceData().getTransparency() ==
-                                    Transparency.OPAQUE) {
-                        return false;
-                    }
-                }
-
                 if (isPaint && c == rootJ && x == 0 && y == 0 &&
                       c.getWidth() == w && c.getHeight() == h) {
                     bufferInfo.setInSync(true);
