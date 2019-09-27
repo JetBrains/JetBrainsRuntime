598452f0792bd89d18c6b39159db6858d1814ee9
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2019-09-27 14:53:33 +0300

    JBR-1875 Modal dialogs text partly lost
    This reverts JBR-1838 (Backport fix for JDK-8226513 from OpenJDK)

--- src/java.desktop/share/classes/javax/swing/plaf/basic/BasicTextUI.java
+++ src/java.desktop/share/classes/javax/swing/plaf/basic/BasicTextUI.java
@@ -692,11 +692,11 @@ public abstract class BasicTextUI extends TextUI implements ViewFactory {
      */
     protected void modelChanged() {
         // create a view hierarchy
+        rootViewInitialized = false;
         ViewFactory f = rootView.getViewFactory();
         Document doc = editor.getDocument();
         Element elem = doc.getDefaultRootElement();
         setView(f.create(elem));
-        rootViewNeedsLayout = false;
     }
 
     /**
@@ -942,10 +942,11 @@ public abstract class BasicTextUI extends TextUI implements ViewFactory {
             if ((d.width > (i.left + i.right + caretMargin)) && (d.height > (i.top + i.bottom))) {
                 rootView.setSize(d.width - i.left - i.right -
                         caretMargin, d.height - i.top - i.bottom);
-            } if (!rootViewNeedsLayout) {
+            }
+            else if (!rootViewInitialized && (d.width <= 0 || d.height <= 0)) {
                 // Probably haven't been layed out yet, force some sort of
                 // initial sizing.
-                rootViewNeedsLayout = true;
+                rootViewInitialized = true;
                 rootView.setSize(Integer.MAX_VALUE, Integer.MAX_VALUE);
             }
             d.width = (int) Math.min((long) rootView.getPreferredSpan(View.X_AXIS) +
@@ -1397,7 +1398,7 @@ public abstract class BasicTextUI extends TextUI implements ViewFactory {
     private static final Position.Bias[] discardBias = new Position.Bias[1];
     private DefaultCaret dropCaret;
     private int caretMargin;
-    private boolean rootViewNeedsLayout;
+    private boolean rootViewInitialized;
 
     /**
      * Root view that acts as a gateway between the component
@@ -1960,6 +1961,7 @@ public abstract class BasicTextUI extends TextUI implements ViewFactory {
          * @see DocumentListener#insertUpdate
          */
         public final void insertUpdate(DocumentEvent e) {
+            rootViewInitialized = false;
             Document doc = e.getDocument();
             Object o = doc.getProperty("i18n");
             if (o instanceof Boolean) {
@@ -1975,7 +1977,6 @@ public abstract class BasicTextUI extends TextUI implements ViewFactory {
             // normal insert update
             Rectangle alloc = (painted) ? getVisibleEditorRect() : null;
             rootView.insertUpdate(e, alloc, rootView.getViewFactory());
-            rootViewNeedsLayout = false;
         }
 
         /**
@@ -1989,9 +1990,9 @@ public abstract class BasicTextUI extends TextUI implements ViewFactory {
          * @see DocumentListener#removeUpdate
          */
         public final void removeUpdate(DocumentEvent e) {
+            rootViewInitialized = false;
             Rectangle alloc = (painted) ? getVisibleEditorRect() : null;
             rootView.removeUpdate(e, alloc, rootView.getViewFactory());
-            rootViewNeedsLayout = false;
         }
 
         /**
@@ -2005,9 +2006,9 @@ public abstract class BasicTextUI extends TextUI implements ViewFactory {
          * @see DocumentListener#changedUpdate(DocumentEvent)
          */
         public final void changedUpdate(DocumentEvent e) {
+            rootViewInitialized = false;
             Rectangle alloc = (painted) ? getVisibleEditorRect() : null;
             rootView.changedUpdate(e, alloc, rootView.getViewFactory());
-            rootViewNeedsLayout = false;
         }
 
         // --- LayoutManager2 methods --------------------------------
