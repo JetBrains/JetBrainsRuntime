5925904333eacffb580056db1a8e4be6e7164509
Denis Konoplev <denis.konoplev@jetbrains.com>
2019-10-11 18:48:24 +0300

    Touch scroll handling
    (cherry picked from commit 6dcec3dc3182b688fd61c2c505be244acc21b823)
    (cherry picked from commit c585901afe89b3e29bcf61b326983aa14200621f)

--- src/java.desktop/unix/classes/sun/awt/X11/XBaseWindow.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XBaseWindow.java
@@ -265,6 +265,9 @@ public class XBaseWindow {
 
     public XBaseWindow (XCreateWindowParams params) {
         init(params);
+
+        // TODO find propper place for setup
+        XlibWrapper.SetupXI2(XToolkit.getDisplay(), window);
     }
 
     /* This create is used by the XEmbeddedFramePeer since it has to create the window
@@ -1087,6 +1090,10 @@ public class XBaseWindow {
         width = scaleDown(xe.get_width());
         height = scaleDown(xe.get_height());
     }
+
+    public void handleTouchEvent(XEvent xev) {
+    }
+
     /**
      * Checks ButtonRelease released all Mouse buttons
      */
@@ -1126,9 +1133,19 @@ public class XBaseWindow {
         if (target == null || !isGrabbedEvent(ev, target)) {
             target = XToolkit.windowToXWindow(ev.get_xany().get_window());
         }
+
+        if (target == null && ev.get_type() == XConstants.GenericEvent) {
+            if (XlibWrapper.XGetEventData(ev.get_xgeneric().get_display(), ev.pData)) {
+                target = XToolkit.windowToXWindow(XlibWrapper.GetXIDeviceEvent(ev.get_xcookie()).get_event());
+            }
+        }
+
         if (target != null && target.checkInitialised()) {
             target.dispatchEvent(ev);
         }
+
+        // finally
+        XlibWrapper.XFreeEventData(ev.get_xgeneric().get_display(), ev.pData);
     }
 
     public void dispatchEvent(XEvent xev) {
@@ -1191,6 +1208,10 @@ public class XBaseWindow {
               break;
           case XConstants.CreateNotify:
               handleCreateNotify(xev);
+              break;
+          case XConstants.GenericEvent:
+              // TODO add XI switch
+              handleTouchEvent(xev);
               break;
         }
     }
