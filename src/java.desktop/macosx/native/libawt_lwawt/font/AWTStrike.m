20546081aa3ce06e4a76db019001067a7f2fd8e8
Alexey Ushakov <Alexey.Ushakov@jetbrains.com>
2019-10-16 20:34:13 +0300

    Revert "JBR-1863, JBR-1868 correct advances on Catalina"
    This reverts commit d5868e8f

--- src/java.desktop/macosx/native/libawt_lwawt/font/AWTStrike.m
+++ src/java.desktop/macosx/native/libawt_lwawt/font/AWTStrike.m
@@ -31,7 +31,6 @@
 #import "CGGlyphOutlines.h"
 #import "CoreTextSupport.h"
 #include "fontscalerdefs.h"
-#import "LWCToolkit.h"
 
 /* Use THIS_FILE when it is available. */
 #ifndef THIS_FILE
@@ -162,7 +161,7 @@ JNF_COCOA_ENTER(env);
     CGGlyph glyph;
     const CTFontRef fallback = CTS_CopyCTFallbackFontAndGlyphForJavaGlyphCode(awtFont, glyphCode, &glyph);
     const CGFontRef cgFallback = CTFontCopyGraphicsFont(fallback, NULL);
-    if (IS_OSX_GT10_14 || CGGI_IsColorFont(cgFallback)) {
+    if (CGGI_IsColorFont(cgFallback)) {
         CGAffineTransform matrix = awtStrike->fAltTx;
         CGFloat fontSize = sqrt(fabs(matrix.a * matrix.d - matrix.b * matrix.c));
         CTFontRef font = CTFontCreateWithGraphicsFont(cgFallback, fontSize, NULL, NULL);
