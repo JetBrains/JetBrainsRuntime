dc417274b0918ae7f72e8733ec27254535dc1c53
prr <unknown>
2019-10-30 15:57:15 +0100

    8212071: Need to set the FreeType LCD Filter to reduce fringing. Reviewed-by: prr, lbourges Contributed-by: John Neffenger <john@status6.com>
    (cherry picked from commit 620deb17d4f6e3f468ac6c15f6fab577417ff113)

--- src/java.desktop/share/native/libfontmanager/freetypeScaler.c
+++ src/java.desktop/share/native/libfontmanager/freetypeScaler.c
@@ -46,6 +46,7 @@
 #include FT_SIZES_H
 #include FT_OUTLINE_H
 #include FT_SYNTHESIS_H
+#include FT_LCD_FILTER_H
 #include FT_MODULE_H
 #include FT_LCD_FILTER_H
 
@@ -976,6 +977,8 @@ static int setupFTContext(JNIEnv *env, jobject font2D, FTScalerInfo *scalerInfo,
             if (logFC) fprintf(stderr, "\n");
 #endif
         }
+
+        FT_Library_SetLcdFilter(scalerInfo->library, FT_LCD_FILTER_DEFAULT);
     }
 
     return 0;
