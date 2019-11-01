5b3c412357f73e5b09bccfb930573bc83964a4e9
Artem Bochkarev <abochkarev@openjdk.org>
2019-11-01 17:23:42 +0300

    8233039: minor code cleanup
    minor simplification for MTLContext.createRenderEncoder, fixed logging utils

--- src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLUtils.m
+++ src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLUtils.m
@@ -18,12 +18,12 @@ void checkTransform(float * position, simd_float4x4 transform4x4) {
     J2dTraceTraceVector(fpt);
     J2dTraceImpl(J2D_TRACE_VERBOSE, JNI_FALSE, "  ===>>>  ");
     J2dTraceTraceVector(fpt_trans);
-    J2dTraceLn(J2D_TRACE_VERBOSE, " ");
+    J2dTraceImpl(J2D_TRACE_VERBOSE, JNI_TRUE, " ");
 }
 
 static void traceMatrix(simd_float4x4 * mtx) {
     for (int row = 0; row < 4; ++row) {
-        J2dTraceLn4(J2D_TRACE_VERBOSE, "  [%lf %lf %lf %lf]",
+        J2dTraceImpl(J2D_TRACE_VERBOSE, JNI_FALSE, "  [%lf %lf %lf %lf]",
                     mtx->columns[0][row], mtx->columns[1][row], mtx->columns[2][row], mtx->columns[3][row]);
     }
 }
@@ -31,17 +31,17 @@ static void traceMatrix(simd_float4x4 * mtx) {
 void traceRaster(char * p, int width, int height, int stride) {
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
-            char pix0 = p[y*stride + x*4];
-            char pix1 = p[y*stride + x*4 + 1];
-            char pix2 = p[y*stride + x*4 + 2];
-            char pix3 = p[y*stride + x*4 + 3];
-            J2dTrace4(J2D_TRACE_INFO, "[%d,%d,%d,%d], ", pix0, pix1, pix2, pix3);
+            unsigned char pix0 = p[y*stride + x*4];
+            unsigned char pix1 = p[y*stride + x*4 + 1];
+            unsigned char pix2 = p[y*stride + x*4 + 2];
+            unsigned char pix3 = p[y*stride + x*4 + 3];
+            J2dTraceImpl(J2D_TRACE_INFO, JNI_FALSE,"[%u,%u,%u,%u], ", pix0, pix1, pix2, pix3);
         }
-        J2dTraceLn(J2D_TRACE_INFO, "");
+        J2dTraceImpl(J2D_TRACE_INFO, JNI_TRUE, "");
     }
 }
 
 void tracePoints(jint nPoints, jint *xPoints, jint *yPoints) {
     for (int i = 0; i < nPoints; i++)
-        J2dTraceLn2(J2D_TRACE_INFO, "\t(%d, %d)", *(xPoints++), *(yPoints++));
+        J2dTraceImpl(J2D_TRACE_INFO, JNI_TRUE, "\t(%d, %d)", *(xPoints++), *(yPoints++));
 }
