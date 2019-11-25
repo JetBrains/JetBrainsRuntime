d7d2d478a927ecae2a8dbe227f9cb1c5e2a4b9d9
Xue-Lei Andrew Fan <xuelei@openjdk.org>
2019-11-25 09:50:30 -0800

    8234408: Improve TLS session handling
    Reviewed-by: ascarpino, jjiang, ahgross, ssahoo, mullan, andrew

--- src/java.base/share/classes/sun/security/ssl/SSLSocketImpl.java
+++ src/java.base/share/classes/sun/security/ssl/SSLSocketImpl.java
@@ -334,7 +334,7 @@ public final class SSLSocketImpl
                 SSLLogger.severe("handshake failed", ioe);
             }
 
-            return SSLSessionImpl.nullSession;
+            return new SSLSessionImpl();
         }
 
         return conContext.conSession;
