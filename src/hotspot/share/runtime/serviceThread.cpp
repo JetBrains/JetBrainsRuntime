7dc0618259593150bb55ecc072887df9d8178fb4
coleenp <unknown>
2019-12-05 16:57:17 -0500

    8212160: JVMTI agent crashes with "assert(_value != 0LL) failed: resolving NULL _value" Summary: Add local deferred event list to thread to post events outside CodeCache_lock. Reviewed-by: eosterlund, dholmes, sspitsyn

--- src/hotspot/share/runtime/serviceThread.cpp
+++ src/hotspot/share/runtime/serviceThread.cpp
@@ -37,6 +37,9 @@
 
 ServiceThread* ServiceThread::_instance = NULL;
 JvmtiDeferredEvent* ServiceThread::_jvmti_event = NULL;
+// The service thread has it's own static deferred event queue.
+// Events can be posted before the service thread is created.
+JvmtiDeferredEventQueue ServiceThread::_jvmti_service_queue;
 
 void ServiceThread::initialize() {
   EXCEPTION_MARK;
@@ -99,7 +102,7 @@ void ServiceThread::service_thread_entry(JavaThread* jt, TRAPS) {
 
       MutexLockerEx ml(Service_lock, Mutex::_no_safepoint_check_flag);
       while (!(sensors_changed = LowMemoryDetector::has_pending_requests()) &&
-             !(has_jvmti_events = JvmtiDeferredEventQueue::has_events()) &&
+             !(has_jvmti_events = _jvmti_service_queue.has_events()) &&
               !(has_gc_notification_event = GCNotifier::has_event()) &&
               !(has_dcmd_notification_event = DCmdFactory::has_pending_jmx_notification()) &&
               !(stringtable_work = StringTable::has_work())) {
@@ -110,7 +113,7 @@ void ServiceThread::service_thread_entry(JavaThread* jt, TRAPS) {
 
       if (has_jvmti_events) {
         // Get the event under the Service_lock
-        jvmti_event = JvmtiDeferredEventQueue::dequeue();
+        jvmti_event = _jvmti_service_queue.dequeue();
         _jvmti_event = &jvmti_event;
       }
     }
@@ -138,6 +141,12 @@ void ServiceThread::service_thread_entry(JavaThread* jt, TRAPS) {
   }
 }
 
+void ServiceThread::enqueue_deferred_event(JvmtiDeferredEvent* event) {
+  MutexLockerEx ml(Service_lock, Mutex::_no_safepoint_check_flag);
+  _jvmti_service_queue.enqueue(*event);
+  Service_lock->notify_all();
+ }
+
 void ServiceThread::oops_do(OopClosure* f, CodeBlobClosure* cf) {
   JavaThread::oops_do(f, cf);
   // The ServiceThread "owns" the JVMTI Deferred events, scan them here
@@ -146,8 +155,9 @@ void ServiceThread::oops_do(OopClosure* f, CodeBlobClosure* cf) {
     if (_jvmti_event != NULL) {
       _jvmti_event->oops_do(f, cf);
     }
+    // Requires a lock, because threads can be adding to this queue.
     MutexLockerEx ml(Service_lock, Mutex::_no_safepoint_check_flag);
-    JvmtiDeferredEventQueue::oops_do(f, cf);
+    _jvmti_service_queue.oops_do(f, cf);
   }
 }
 
@@ -157,7 +167,8 @@ void ServiceThread::nmethods_do(CodeBlobClosure* cf) {
     if (_jvmti_event != NULL) {
       _jvmti_event->nmethods_do(cf);
     }
+    // Requires a lock, because threads can be adding to this queue.
     MutexLockerEx ml(Service_lock, Mutex::_no_safepoint_check_flag);
-    JvmtiDeferredEventQueue::nmethods_do(cf);
+    _jvmti_service_queue.nmethods_do(cf);
   }
 }
