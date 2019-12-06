c2728be2ddbba0cc1b2b7140073de672987b07a5
Denis Fokin <Denis.Fokin@jetbrains.com>
2019-12-06 12:37:04 +0300

    JBR-4788 "Activate window by mouse hover" Windows option isn't supported by JBR 17
    backport JBR-1991 (Focus problems in Windows with X-Mouse style focus) from JBR 11
    
    (cherry picked from commit c8ad353f45c98f2f697a3dd6777ee1aadc53e997)
    (cherry picked from commit a52ed88377fe31febe4a65c3c27733417f36e75d)

--- src/java.desktop/windows/native/libawt/windows/awt_Toolkit.cpp
+++ src/java.desktop/windows/native/libawt/windows/awt_Toolkit.cpp
@@ -699,6 +699,13 @@ BOOL AwtToolkit::Initialize(BOOL localPump) {
     tk.m_touchKbrdAutoShowIsEnabled = env->CallStaticBooleanMethod(
         sunToolkitCls, isTouchKeyboardAutoShowEnabledMID);
 
+    BOOL tracking = FALSE;
+    int result = 0;
+    result = ::SystemParametersInfo(SPI_GETACTIVEWINDOWTRACKING, 0,
+                                  &tracking, 0);
+
+    tk.m_active_window_tracking_mode_enabled = (tracking && (result != 0));
+
     CATCH_BAD_ALLOC_RET(FALSE);
 
     if (tk.m_isWin8OrLater && tk.m_touchKbrdAutoShowIsEnabled) {
@@ -801,6 +808,10 @@ void AwtToolkit::SetDynamicLayout(BOOL dynamic) {
 
 BOOL AwtToolkit::IsDynamicLayoutSet() {
     return m_isDynamicLayoutSet;
+}
+
+BOOL AwtToolkit::IsActiveWindowTrackingEnabled() {
+    return m_active_window_tracking_mode_enabled;
 }
 
 BOOL AwtToolkit::IsDynamicLayoutSupported() {
