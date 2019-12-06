7a71479cb876ac81a32ebf5d8db0cbc2546df13b
Denis Fokin <Denis.Fokin@jetbrains.com>
2019-12-06 12:37:04 +0300

    JBR-4788 "Activate window by mouse hover" Windows option isn't supported by JBR 17
    backport JBR-1991 (Focus problems in Windows with X-Mouse style focus) from JBR 11
    
    (cherry picked from commit c8ad353f45c98f2f697a3dd6777ee1aadc53e997)
    (cherry picked from commit a52ed88377fe31febe4a65c3c27733417f36e75d)
    (cherry picked from commit 37c6d0b417b6a120acad461c6dac694155b1be96)

--- src/java.desktop/windows/native/libawt/windows/awt_Component.cpp
+++ src/java.desktop/windows/native/libawt/windows/awt_Component.cpp
@@ -1583,7 +1583,7 @@ LRESULT AwtComponent::WindowProc(UINT message, WPARAM wParam, LPARAM lParam)
               window->AwtSetActiveWindow(TRUE, LOWORD(lParam)/*hittest*/);
           }
           mr = mrConsume;
-          retValue = MA_NOACTIVATE;
+          retValue = AwtToolkit::GetInstance().IsActiveWindowTrackingEnabled() ? MA_ACTIVATE : MA_NOACTIVATE;
           break;
       }
       case WM_CTLCOLORMSGBOX:
