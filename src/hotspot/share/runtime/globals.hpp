4f4adf16f79f025d7d027a4b72cdb2721569be2e
Claes Redestad <redestad@openjdk.org>
2019-12-10 14:18:25 +0100

    8234863: Increase default value of MaxInlineLevel
    Reviewed-by: kvn, neliasso

--- src/hotspot/share/runtime/globals.hpp
+++ src/hotspot/share/runtime/globals.hpp
@@ -1680,7 +1680,7 @@ define_pd_global(uint64_t,MaxRAM,                    1ULL*G);
   notproduct(intx, MaxSubklassPrintSize, 4,                                 \
           "maximum number of subklasses to print when printing klass")      \
                                                                             \
-  product(intx, MaxInlineLevel, 9,                                          \
+  product(intx, MaxInlineLevel, 15,                                         \
           "maximum number of nested calls that are inlined")                \
           range(0, max_jint)                                                \
                                                                             \
