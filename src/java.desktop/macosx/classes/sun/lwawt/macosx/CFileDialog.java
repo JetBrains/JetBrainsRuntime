JetBrains/JetBrainsRuntime

commit 8fd21b3746e3a751c87a2558cc85558a4f3e8d1b

New (CFRetainedResource) memory management approach to access window ptr.

src/java.desktop/macosx/classes/sun/lwawt/macosx/CFileDialog.java
@@ -39,7 +39,6 @@ import sun.lwawt.LWWindowPeer;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import sun.awt.CausedFocusEvent.Cause;
 import sun.awt.AWTAccessor;
 import sun.java2d.pipe.Region;
 import sun.lwawt.LWWindowPeer;
@@ -63,56 +62,53 @@ class CFileDialog implements FileDialogPeer {
                         new GetBooleanAction("apple.awt.fileDialogForDirectories"));
 
                 int dialogMode = target.getMode();
-                String title = target.getTitle();
-                if (title == null) {
-                    title = " ";
-                }
+                String title = (target.getTitle() == null) ? "" : target.getTitle();
 
                 Window owner = target.getOwner();
 
-                LWWindowPeer lwWindowPeer = (LWWindowPeer) AWTAccessor.getComponentAccessor().getPeer(owner);
+                final Object peer = AWTAccessor.getComponentAccessor().getPeer(owner);
+                Object platformWindow = ((LWWindowPeer) peer).getPlatformWindow();
+                ((CPlatformWindow)platformWindow).execute(ownerPtr -> {
+                    String[] userFileNames = nativeRunFileDialog(
+                            ownerPtr,
+                            title,
+                            dialogMode,
+                            target.isMultipleMode(),
+                            navigateApps,
+                            chooseDirectories,
+                            target.getFilenameFilter() != null,
+                            target.getDirectory(),
+                            target.getFile());
 
-                long ownerPtr = owner == null ?
-                        0L :
-                        ((CPlatformWindow) lwWindowPeer.getPlatformWindow()).executeGet(ptr -> ptr);
+                    String directory = null;
+                    String file = null;
+                    File[] files = null;
 
-                String[] userFileNames = nativeRunFileDialog(
-                        ownerPtr,
-                        title,
-                        dialogMode,
-                        target.isMultipleMode(),
-                        navigateApps,
-                        chooseDirectories,
-                        target.getFilenameFilter() != null,
-                        target.getDirectory(),
-                        target.getFile());
+                    if (userFileNames != null && userFileNames.length > 0) {
+                        // the dialog wasn't cancelled
+                        int filesNumber = userFileNames.length;
+                        files = new File[filesNumber];
+                        for (int i = 0; i < filesNumber; i++) {
+                            files[i] = new File(userFileNames[i]);
+                        }
 
-                String directory = null;
-                String file = null;
-                File[] files = null;
+                        directory = files[0].getParent();
+                        // make sure directory always ends in '/'
+                        if (!directory.endsWith(File.separator)) {
+                            directory = directory + File.separator;
+                        }
 
-                if (userFileNames != null && userFileNames.length > 0) {
-                    // the dialog wasn't cancelled
-                    int filesNumber = userFileNames.length;
-                    files = new File[filesNumber];
-                    for (int i = 0; i < filesNumber; i++) {
-                        files[i] = new File(userFileNames[i]);
+                        file = files[0].getName(); // pick any file
                     }
 
-                    directory = files[0].getParent();
-                    // make sure directory always ends in '/'
-                    if (!directory.endsWith(File.separator)) {
-                        directory = directory + File.separator;
-                    }
+                    // store results back in component
+                    AWTAccessor.FileDialogAccessor accessor = AWTAccessor.getFileDialogAccessor();
+                    accessor.setDirectory(target, directory);
+                    accessor.setFile(target, file);
+                    accessor.setFiles(target, files);
+                });
 
-                    file = files[0].getName(); // pick any file
-                }
 
-                // store results back in component
-                AWTAccessor.FileDialogAccessor accessor = AWTAccessor.getFileDialogAccessor();
-                accessor.setDirectory(target, directory);
-                accessor.setFile(target, file);
-                accessor.setFiles(target, files);
             } finally {
                 // Java2 Dialog waits for hide to let show() return
                 target.dispose();

commit a9f00fc57c8d0fcf43171982ef2f160ebc040b1c

JBR-2044 [backport to 193] JBR-1089: allow to create FileDialog without parent
was broken in 8fd21b3746e3a751c87a2558cc85558a4f3e8d1b
fixed test FileDialogModalFocusTest.java

(cherry picked from commit 5e0a9f5993fe5f019f47ce06df102a8de5055a53)

src/java.desktop/macosx/classes/sun/lwawt/macosx/CFileDialog.java
@@ -64,11 +64,7 @@ class CFileDialog implements FileDialogPeer {
                 int dialogMode = target.getMode();
                 String title = (target.getTitle() == null) ? "" : target.getTitle();
 
-                Window owner = target.getOwner();
-
-                final Object peer = AWTAccessor.getComponentAccessor().getPeer(owner);
-                Object platformWindow = ((LWWindowPeer) peer).getPlatformWindow();
-                ((CPlatformWindow)platformWindow).execute(ownerPtr -> {
+                final CFRetainedResource.CFNativeAction nativeAction = ownerPtr -> {
                     String[] userFileNames = nativeRunFileDialog(
                             ownerPtr,
                             title,
@@ -106,9 +102,16 @@ class CFileDialog implements FileDialogPeer {
                     accessor.setDirectory(target, directory);
                     accessor.setFile(target, file);
                     accessor.setFiles(target, files);
-                });
+                };
 
-
+                final Window owner = target.getOwner();
+                if (owner == null) {
+                    nativeAction.run(0);
+                } else {
+                    final Object peer = AWTAccessor.getComponentAccessor().getPeer(owner);
+                    Object platformWindow = ((LWWindowPeer) peer).getPlatformWindow();
+                    ((CPlatformWindow)platformWindow).execute(nativeAction);
+                }
             } finally {
                 // Java2 Dialog waits for hide to let show() return
                 target.dispose();