895ba78eeb7270062943cf2c865d4702e03ffbaa
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2020-01-23 20:22:30 +0700

    Revert "add signing Contents/MacOS/libjli.dylib (follow up)"
    This reverts commit cc57068c

--- jb/project/tools/mac/scripts/sign.sh
+++ jb/project/tools/mac/scripts/sign.sh
@@ -25,6 +25,7 @@ find "$APP_DIRECTORY" -name '*.cstemp' -exec rm '{}' \;
 log "Signing libraries and executables..."
 # -perm +111 searches for executables
 for f in \
+   "Contents/MacOS" \
    "Contents/Home/bin" \
    "Contents/Home/lib"; do
   if [ -d "$APP_DIRECTORY/$f" ]; then
@@ -35,7 +36,6 @@ for f in \
       --entitlements entitlements.xml {} \;
   fi
 done
-cp "$APP_DIRECTORY"/Contents/Home/lib/jli/libjli.dylib "$APP_DIRECTORY"/Contents/MacOS/libjli.dylib
 
 log "Signing libraries in jars in $PWD"
 
