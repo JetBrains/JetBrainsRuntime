41ddfccc204ea6767491b0a27e849eae34119e63
Denis Konoplev <denis.konoplev@jetbrains.com>
2020-01-30 15:50:24 +0300

    JBR-2041: Project view tap fix, recovery? constants & logging
    (cherry picked from commit 1e904db3b040ee207db06b1f8f61e21a7f1db898)

---
+++ src/java.desktop/share/classes/sun/awt/event/TouchEvent.java
@@ -0,0 +1,21 @@
+package sun.awt.event;
+
+import java.lang.annotation.Native;
+
+public class TouchEvent {
+    @Native public static final int TOUCH_BEGIN = 2;
+    @Native public static final int TOUCH_UPDATE = 3;
+    @Native public static final int TOUCH_END = 4;
+
+    /**
+     * The allowable difference between coordinates of touch update events
+     * in pixels
+     */
+    @Native public static final int CLICK_RADIUS = 10;
+
+    /**
+     * Stop owning touch processing after timeout
+     * in ms
+     */
+    public static final long NO_UPDATE_TIMEOUT = 1000L;
+}
