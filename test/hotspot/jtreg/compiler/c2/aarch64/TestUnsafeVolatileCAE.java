ibmruntimes/openj9-openjdk-jdk17

8209835: Aarch64: elide barriers on all volatile operations Reviewed-by: aph, adinn

test/hotspot/jtreg/compiler/c2/aarch64/TestUnsafeVolatileCAE.java
@@ -26,7 +26,7 @@ package compiler.c2.aarch64;
 import java.lang.reflect.Field;
 import jdk.internal.misc.Unsafe;
 
-class TestUnsafeVolatileCAS
+class TestUnsafeVolatileCAE
 {
     public volatile int f_int = 0;
     public volatile Integer f_obj = Integer.valueOf(0);
@@ -48,11 +48,11 @@ class TestUnsafeVolatileCAS
 
     static {
         try {
-            f_int_field = TestUnsafeVolatileCAS.class.getField("f_int");
-            f_obj_field = TestUnsafeVolatileCAS.class.getField("f_obj");
-            f_long_field = TestUnsafeVolatileCAS.class.getField("f_long");
-            f_byte_field = TestUnsafeVolatileCAS.class.getField("f_byte");
-            f_short_field = TestUnsafeVolatileCAS.class.getField("f_short");
+            f_int_field = TestUnsafeVolatileCAE.class.getField("f_int");
+            f_obj_field = TestUnsafeVolatileCAE.class.getField("f_obj");
+            f_long_field = TestUnsafeVolatileCAE.class.getField("f_long");
+            f_byte_field = TestUnsafeVolatileCAE.class.getField("f_byte");
+            f_short_field = TestUnsafeVolatileCAE.class.getField("f_short");
             f_int_off = unsafe.objectFieldOffset(f_int_field);
             f_obj_off = unsafe.objectFieldOffset(f_obj_field);
             f_long_off = unsafe.objectFieldOffset(f_long_field);
@@ -66,69 +66,68 @@ class TestUnsafeVolatileCAS
 
     public static void main(String[] args)
     {
-        final TestUnsafeVolatileCAS t = new TestUnsafeVolatileCAS();
+        final TestUnsafeVolatileCAE t = new TestUnsafeVolatileCAE();
         for (int i = 0; i < 100_000; i++) {
             t.f_int = -1;
-            t.testInt(-1, i);
-            if (t.f_int != i) {
+            int res = t.testInt(-1, i);
+            if (res != -1 || t.f_int != i) {
                 throw new RuntimeException("bad result!");
             }
         }
         for (int i = 0; i < 100_000; i++) {
             t.f_long = -1;
-            t.testLong(-1, i);
-            if (t.f_long != i) {
+            long res = t.testLong(-1, i);
+            if (res != -1 || t.f_long != i) {
                 throw new RuntimeException("bad result!");
             }
         }
         for (int i = 0; i < 100_000; i++) {
             t.f_byte = -1;
             byte i_b = (byte)i;
-            t.testByte((byte)-1, i_b);
-            if (t.f_byte != i_b) {
+            byte res = t.testByte((byte)-1, i_b);
+            if (res != (byte)-1 || t.f_byte != i_b) {
                 throw new RuntimeException("bad result!");
             }
         }
         for (int i = 0; i < 100_000; i++) {
             t.f_short = -1;
             short i_s = (short)i;
-            t.testShort((byte)-1, i_s);
-            if (t.f_short != i_s) {
+            short res = t.testShort((byte)-1, i_s);
+            if (res != (short)-1 || t.f_short != i_s) {
                 throw new RuntimeException("bad result!");
             }
         }
         Integer minusOne = Integer.valueOf(-1);
         for (int i = 0; i < 100_000; i++) {
             t.f_obj = minusOne;
-            t.testObj(minusOne, Integer.valueOf(i));
-            if (t.f_obj != i) {
+            Object res = t.testObj(minusOne, Integer.valueOf(i));
+            if (res != minusOne || t.f_obj != i) {
                 throw new RuntimeException("bad result!");
             }
         }
     }
 
-    public void testInt(int x, int i)
+    public int testInt(int x, int i)
     {
-        unsafe.compareAndSetInt(this, f_int_off, x, i);
+        return unsafe.compareAndExchangeInt(this, f_int_off, x, i);
     }
 
-    public void testObj(Object x, Object o)
+    public Object testObj(Object x, Object o)
     {
-        unsafe.compareAndSetReference(this, f_obj_off, x, o);
+        return unsafe.compareAndExchangeReference(this, f_obj_off, x, o);
+    }
+    public long testLong(long x, long i)
+    {
+        return unsafe.compareAndExchangeLong(this, f_long_off, x, i);
     }
 
-    public void testLong(long x, long i)
+    public byte testByte(byte x, byte i)
     {
-        unsafe.compareAndSetLong(this, f_long_off, x, i);
+        return unsafe.compareAndExchangeByte(this, f_byte_off, x, i);
     }
 
-    public void testByte(byte x, byte i)
+    public short testShort(short x, short i)
     {
-        unsafe.compareAndSetByte(this, f_byte_off, x, i);
-    }
-
-    public void testShort(short x, short i)
-    {
-        unsafe.compareAndSetShort(this, f_short_off, x, i);
+        return unsafe.compareAndExchangeShort(this, f_short_off, x, i);
     }
 }

test/hotspot/jtreg/compiler/c2/aarch64/TestUnsafeVolatileGAS.java
@@ -1,0 +1,90 @@
+/*
+ * Copyright (c) 2018, Red Hat, Inc. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package compiler.c2.aarch64;
+
+import java.lang.reflect.Field;
+import jdk.internal.misc.Unsafe;
+
+class TestUnsafeVolatileGAS
+{
+    public volatile int f_int = -1;
+    public volatile Integer f_obj = Integer.valueOf(-1);
+    public volatile long f_long = -1;
+
+    public static Unsafe unsafe = Unsafe.getUnsafe();
+    public static Field f_int_field;
+    public static Field f_obj_field;
+    public static Field f_long_field;
+    public static long f_int_off;
+    public static long f_obj_off;
+    public static long f_long_off;
+
+    static {
+        try {
+            f_int_field = TestUnsafeVolatileGAS.class.getField("f_int");
+            f_obj_field = TestUnsafeVolatileGAS.class.getField("f_obj");
+            f_long_field = TestUnsafeVolatileGAS.class.getField("f_long");
+            f_int_off = unsafe.objectFieldOffset(f_int_field);
+            f_obj_off = unsafe.objectFieldOffset(f_obj_field);
+            f_long_off = unsafe.objectFieldOffset(f_long_field);
+        } catch (Exception e) {
+            System.out.println("reflection failed " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        final TestUnsafeVolatileGAS t = new TestUnsafeVolatileGAS();
+        for (int i = 0; i < 100_000; i++) {
+            if (t.testInt(i) != i-1) {
+                throw new RuntimeException("bad result!");
+            }
+        }
+        for (int i = 0; i < 100_000; i++) {
+            if (t.testLong(i) != i-1) {
+                throw new RuntimeException("bad result!");
+            }
+        }
+        for (int i = 0; i < 100_000; i++) {
+            if ((Integer)t.testObj(Integer.valueOf(i)) != i-1) {
+                throw new RuntimeException("bad result!");
+            }
+        }
+    }
+
+    public int testInt(int i)
+    {
+        return unsafe.getAndSetInt(this, f_int_off, i);
+    }
+
+    public Object testObj(Object o)
+    {
+        return unsafe.getAndSetReference(this, f_obj_off, o);
+    }
+    public long testLong(long i)
+    {
+        return unsafe.getAndSetLong(this, f_long_off, i);
+    }
+}

test/hotspot/jtreg/compiler/c2/aarch64/TestUnsafeVolatileWeakCAS.java
@@ -26,7 +26,7 @@ package compiler.c2.aarch64;
 import java.lang.reflect.Field;
 import jdk.internal.misc.Unsafe;
 
-class TestUnsafeVolatileCAS
+class TestUnsafeVolatileWeakCAS
 {
     public volatile int f_int = 0;
     public volatile Integer f_obj = Integer.valueOf(0);
@@ -48,11 +48,11 @@ class TestUnsafeVolatileCAS
 
     static {
         try {
-            f_int_field = TestUnsafeVolatileCAS.class.getField("f_int");
-            f_obj_field = TestUnsafeVolatileCAS.class.getField("f_obj");
-            f_long_field = TestUnsafeVolatileCAS.class.getField("f_long");
-            f_byte_field = TestUnsafeVolatileCAS.class.getField("f_byte");
-            f_short_field = TestUnsafeVolatileCAS.class.getField("f_short");
+            f_int_field = TestUnsafeVolatileWeakCAS.class.getField("f_int");
+            f_obj_field = TestUnsafeVolatileWeakCAS.class.getField("f_obj");
+            f_long_field = TestUnsafeVolatileWeakCAS.class.getField("f_long");
+            f_byte_field = TestUnsafeVolatileWeakCAS.class.getField("f_byte");
+            f_short_field = TestUnsafeVolatileWeakCAS.class.getField("f_short");
             f_int_off = unsafe.objectFieldOffset(f_int_field);
             f_obj_off = unsafe.objectFieldOffset(f_obj_field);
             f_long_off = unsafe.objectFieldOffset(f_long_field);
@@ -66,69 +66,74 @@ class TestUnsafeVolatileCAS
 
     public static void main(String[] args)
     {
-        final TestUnsafeVolatileCAS t = new TestUnsafeVolatileCAS();
+        final TestUnsafeVolatileWeakCAS t = new TestUnsafeVolatileWeakCAS();
         for (int i = 0; i < 100_000; i++) {
             t.f_int = -1;
-            t.testInt(-1, i);
-            if (t.f_int != i) {
-                throw new RuntimeException("bad result!");
+            if (t.testInt(-1, i)) {
+                if (t.f_int != i) {
+                    throw new RuntimeException("bad result!");
+                }
             }
         }
         for (int i = 0; i < 100_000; i++) {
             t.f_long = -1;
-            t.testLong(-1, i);
-            if (t.f_long != i) {
-                throw new RuntimeException("bad result!");
+            if (t.testLong(-1, i)) {
+                if (t.f_long != i) {
+                    throw new RuntimeException("bad result!");
+                }
             }
         }
         for (int i = 0; i < 100_000; i++) {
             t.f_byte = -1;
             byte i_b = (byte)i;
-            t.testByte((byte)-1, i_b);
-            if (t.f_byte != i_b) {
-                throw new RuntimeException("bad result!");
+            if (t.testByte((byte)-1, i_b)) {
+                if (t.f_byte != i_b) {
+                    throw new RuntimeException("bad result!");
+                }
             }
         }
         for (int i = 0; i < 100_000; i++) {
             t.f_short = -1;
             short i_s = (short)i;
-            t.testShort((byte)-1, i_s);
-            if (t.f_short != i_s) {
-                throw new RuntimeException("bad result!");
+            if (t.testShort((byte)-1, i_s)) {
+                if (t.f_short != i_s) {
+                    throw new RuntimeException("bad result!");
+                }
             }
         }
         Integer minusOne = Integer.valueOf(-1);
         for (int i = 0; i < 100_000; i++) {
             t.f_obj = minusOne;
-            t.testObj(minusOne, Integer.valueOf(i));
-            if (t.f_obj != i) {
-                throw new RuntimeException("bad result!");
+            if (t.testObj(minusOne, Integer.valueOf(i))) {
+                if (t.f_obj != i) {
+                    throw new RuntimeException("bad result!");
+                }
             }
         }
     }
 
-    public void testInt(int x, int i)
+    public boolean testInt(int x, int i)
     {
-        unsafe.compareAndSetInt(this, f_int_off, x, i);
+        return unsafe.weakCompareAndSetInt(this, f_int_off, x, i);
     }
 
-    public void testObj(Object x, Object o)
+    public boolean testObj(Object x, Object o)
     {
-        unsafe.compareAndSetReference(this, f_obj_off, x, o);
+        return unsafe.weakCompareAndSetReference(this, f_obj_off, x, o);
     }
 
-    public void testLong(long x, long i)
+    public boolean testLong(long x, long i)
     {
-        unsafe.compareAndSetLong(this, f_long_off, x, i);
+        return unsafe.weakCompareAndSetLong(this, f_long_off, x, i);
     }
 
-    public void testByte(byte x, byte i)
+    public boolean testByte(byte x, byte i)
     {
-        unsafe.compareAndSetByte(this, f_byte_off, x, i);
+        return unsafe.weakCompareAndSetByte(this, f_byte_off, x, i);
     }
 
-    public void testShort(short x, short i)
+    public boolean testShort(short x, short i)
     {
-        unsafe.compareAndSetShort(this, f_short_off, x, i);
+        return unsafe.weakCompareAndSetShort(this, f_short_off, x, i);
     }
 }

JetBrains/JetBrainsRuntime

(cherry picked from commit c3d8700c8c333783be46d0c7218969bb4a4fddc8)

test/hotspot/jtreg/compiler/c2/aarch64/TestUnsafeVolatileCAE.java
@@ -26,7 +26,7 @@ package compiler.c2.aarch64;
 import java.lang.reflect.Field;
 import jdk.internal.misc.Unsafe;
 
-class TestUnsafeVolatileCAS
+class TestUnsafeVolatileCAE
 {
     public volatile int f_int = 0;
     public volatile Integer f_obj = Integer.valueOf(0);
@@ -48,11 +48,11 @@ class TestUnsafeVolatileCAS
 
     static {
         try {
-            f_int_field = TestUnsafeVolatileCAS.class.getField("f_int");
-            f_obj_field = TestUnsafeVolatileCAS.class.getField("f_obj");
-            f_long_field = TestUnsafeVolatileCAS.class.getField("f_long");
-            f_byte_field = TestUnsafeVolatileCAS.class.getField("f_byte");
-            f_short_field = TestUnsafeVolatileCAS.class.getField("f_short");
+            f_int_field = TestUnsafeVolatileCAE.class.getField("f_int");
+            f_obj_field = TestUnsafeVolatileCAE.class.getField("f_obj");
+            f_long_field = TestUnsafeVolatileCAE.class.getField("f_long");
+            f_byte_field = TestUnsafeVolatileCAE.class.getField("f_byte");
+            f_short_field = TestUnsafeVolatileCAE.class.getField("f_short");
             f_int_off = unsafe.objectFieldOffset(f_int_field);
             f_obj_off = unsafe.objectFieldOffset(f_obj_field);
             f_long_off = unsafe.objectFieldOffset(f_long_field);
@@ -66,69 +66,68 @@ class TestUnsafeVolatileCAS
 
     public static void main(String[] args)
     {
-        final TestUnsafeVolatileCAS t = new TestUnsafeVolatileCAS();
+        final TestUnsafeVolatileCAE t = new TestUnsafeVolatileCAE();
         for (int i = 0; i < 100_000; i++) {
             t.f_int = -1;
-            t.testInt(-1, i);
-            if (t.f_int != i) {
+            int res = t.testInt(-1, i);
+            if (res != -1 || t.f_int != i) {
                 throw new RuntimeException("bad result!");
             }
         }
         for (int i = 0; i < 100_000; i++) {
             t.f_long = -1;
-            t.testLong(-1, i);
-            if (t.f_long != i) {
+            long res = t.testLong(-1, i);
+            if (res != -1 || t.f_long != i) {
                 throw new RuntimeException("bad result!");
             }
         }
         for (int i = 0; i < 100_000; i++) {
             t.f_byte = -1;
             byte i_b = (byte)i;
-            t.testByte((byte)-1, i_b);
-            if (t.f_byte != i_b) {
+            byte res = t.testByte((byte)-1, i_b);
+            if (res != (byte)-1 || t.f_byte != i_b) {
                 throw new RuntimeException("bad result!");
             }
         }
         for (int i = 0; i < 100_000; i++) {
             t.f_short = -1;
             short i_s = (short)i;
-            t.testShort((byte)-1, i_s);
-            if (t.f_short != i_s) {
+            short res = t.testShort((byte)-1, i_s);
+            if (res != (short)-1 || t.f_short != i_s) {
                 throw new RuntimeException("bad result!");
             }
         }
         Integer minusOne = Integer.valueOf(-1);
         for (int i = 0; i < 100_000; i++) {
             t.f_obj = minusOne;
-            t.testObj(minusOne, Integer.valueOf(i));
-            if (t.f_obj != i) {
+            Object res = t.testObj(minusOne, Integer.valueOf(i));
+            if (res != minusOne || t.f_obj != i) {
                 throw new RuntimeException("bad result!");
             }
         }
     }
 
-    public void testInt(int x, int i)
+    public int testInt(int x, int i)
     {
-        unsafe.compareAndSetInt(this, f_int_off, x, i);
+        return unsafe.compareAndExchangeInt(this, f_int_off, x, i);
     }
 
-    public void testObj(Object x, Object o)
+    public Object testObj(Object x, Object o)
     {
-        unsafe.compareAndSetObject(this, f_obj_off, x, o);
+        return unsafe.compareAndExchangeObject(this, f_obj_off, x, o);
+    }
+    public long testLong(long x, long i)
+    {
+        return unsafe.compareAndExchangeLong(this, f_long_off, x, i);
     }
 
-    public void testLong(long x, long i)
+    public byte testByte(byte x, byte i)
     {
-        unsafe.compareAndSetLong(this, f_long_off, x, i);
+        return unsafe.compareAndExchangeByte(this, f_byte_off, x, i);
     }
 
-    public void testByte(byte x, byte i)
+    public short testShort(short x, short i)
     {
-        unsafe.compareAndSetByte(this, f_byte_off, x, i);
-    }
-
-    public void testShort(short x, short i)
-    {
-        unsafe.compareAndSetShort(this, f_short_off, x, i);
+        return unsafe.compareAndExchangeShort(this, f_short_off, x, i);
     }
 }

test/hotspot/jtreg/compiler/c2/aarch64/TestUnsafeVolatileGAS.java
@@ -1,0 +1,90 @@
+/*
+ * Copyright (c) 2018, Red Hat, Inc. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package compiler.c2.aarch64;
+
+import java.lang.reflect.Field;
+import jdk.internal.misc.Unsafe;
+
+class TestUnsafeVolatileGAS
+{
+    public volatile int f_int = -1;
+    public volatile Integer f_obj = Integer.valueOf(-1);
+    public volatile long f_long = -1;
+
+    public static Unsafe unsafe = Unsafe.getUnsafe();
+    public static Field f_int_field;
+    public static Field f_obj_field;
+    public static Field f_long_field;
+    public static long f_int_off;
+    public static long f_obj_off;
+    public static long f_long_off;
+
+    static {
+        try {
+            f_int_field = TestUnsafeVolatileGAS.class.getField("f_int");
+            f_obj_field = TestUnsafeVolatileGAS.class.getField("f_obj");
+            f_long_field = TestUnsafeVolatileGAS.class.getField("f_long");
+            f_int_off = unsafe.objectFieldOffset(f_int_field);
+            f_obj_off = unsafe.objectFieldOffset(f_obj_field);
+            f_long_off = unsafe.objectFieldOffset(f_long_field);
+        } catch (Exception e) {
+            System.out.println("reflection failed " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        final TestUnsafeVolatileGAS t = new TestUnsafeVolatileGAS();
+        for (int i = 0; i < 100_000; i++) {
+            if (t.testInt(i) != i-1) {
+                throw new RuntimeException("bad result!");
+            }
+        }
+        for (int i = 0; i < 100_000; i++) {
+            if (t.testLong(i) != i-1) {
+                throw new RuntimeException("bad result!");
+            }
+        }
+        for (int i = 0; i < 100_000; i++) {
+            if ((Integer)t.testObj(Integer.valueOf(i)) != i-1) {
+                throw new RuntimeException("bad result!");
+            }
+        }
+    }
+
+    public int testInt(int i)
+    {
+        return unsafe.getAndSetInt(this, f_int_off, i);
+    }
+
+    public Object testObj(Object o)
+    {
+        return unsafe.getAndSetObject(this, f_obj_off, o);
+    }
+    public long testLong(long i)
+    {
+        return unsafe.getAndSetLong(this, f_long_off, i);
+    }
+}

test/hotspot/jtreg/compiler/c2/aarch64/TestUnsafeVolatileWeakCAS.java
@@ -26,7 +26,7 @@ package compiler.c2.aarch64;
 import java.lang.reflect.Field;
 import jdk.internal.misc.Unsafe;
 
-class TestUnsafeVolatileCAS
+class TestUnsafeVolatileWeakCAS
 {
     public volatile int f_int = 0;
     public volatile Integer f_obj = Integer.valueOf(0);
@@ -48,11 +48,11 @@ class TestUnsafeVolatileCAS
 
     static {
         try {
-            f_int_field = TestUnsafeVolatileCAS.class.getField("f_int");
-            f_obj_field = TestUnsafeVolatileCAS.class.getField("f_obj");
-            f_long_field = TestUnsafeVolatileCAS.class.getField("f_long");
-            f_byte_field = TestUnsafeVolatileCAS.class.getField("f_byte");
-            f_short_field = TestUnsafeVolatileCAS.class.getField("f_short");
+            f_int_field = TestUnsafeVolatileWeakCAS.class.getField("f_int");
+            f_obj_field = TestUnsafeVolatileWeakCAS.class.getField("f_obj");
+            f_long_field = TestUnsafeVolatileWeakCAS.class.getField("f_long");
+            f_byte_field = TestUnsafeVolatileWeakCAS.class.getField("f_byte");
+            f_short_field = TestUnsafeVolatileWeakCAS.class.getField("f_short");
             f_int_off = unsafe.objectFieldOffset(f_int_field);
             f_obj_off = unsafe.objectFieldOffset(f_obj_field);
             f_long_off = unsafe.objectFieldOffset(f_long_field);
@@ -66,69 +66,74 @@ class TestUnsafeVolatileCAS
 
     public static void main(String[] args)
     {
-        final TestUnsafeVolatileCAS t = new TestUnsafeVolatileCAS();
+        final TestUnsafeVolatileWeakCAS t = new TestUnsafeVolatileWeakCAS();
         for (int i = 0; i < 100_000; i++) {
             t.f_int = -1;
-            t.testInt(-1, i);
-            if (t.f_int != i) {
-                throw new RuntimeException("bad result!");
+            if (t.testInt(-1, i)) {
+                if (t.f_int != i) {
+                    throw new RuntimeException("bad result!");
+                }
             }
         }
         for (int i = 0; i < 100_000; i++) {
             t.f_long = -1;
-            t.testLong(-1, i);
-            if (t.f_long != i) {
-                throw new RuntimeException("bad result!");
+            if (t.testLong(-1, i)) {
+                if (t.f_long != i) {
+                    throw new RuntimeException("bad result!");
+                }
             }
         }
         for (int i = 0; i < 100_000; i++) {
             t.f_byte = -1;
             byte i_b = (byte)i;
-            t.testByte((byte)-1, i_b);
-            if (t.f_byte != i_b) {
-                throw new RuntimeException("bad result!");
+            if (t.testByte((byte)-1, i_b)) {
+                if (t.f_byte != i_b) {
+                    throw new RuntimeException("bad result!");
+                }
             }
         }
         for (int i = 0; i < 100_000; i++) {
             t.f_short = -1;
             short i_s = (short)i;
-            t.testShort((byte)-1, i_s);
-            if (t.f_short != i_s) {
-                throw new RuntimeException("bad result!");
+            if (t.testShort((byte)-1, i_s)) {
+                if (t.f_short != i_s) {
+                    throw new RuntimeException("bad result!");
+                }
             }
         }
         Integer minusOne = Integer.valueOf(-1);
         for (int i = 0; i < 100_000; i++) {
             t.f_obj = minusOne;
-            t.testObj(minusOne, Integer.valueOf(i));
-            if (t.f_obj != i) {
-                throw new RuntimeException("bad result!");
+            if (t.testObj(minusOne, Integer.valueOf(i))) {
+                if (t.f_obj != i) {
+                    throw new RuntimeException("bad result!");
+                }
             }
         }
     }
 
-    public void testInt(int x, int i)
+    public boolean testInt(int x, int i)
     {
-        unsafe.compareAndSetInt(this, f_int_off, x, i);
+        return unsafe.weakCompareAndSetInt(this, f_int_off, x, i);
     }
 
-    public void testObj(Object x, Object o)
+    public boolean testObj(Object x, Object o)
     {
-        unsafe.compareAndSetObject(this, f_obj_off, x, o);
+        return unsafe.weakCompareAndSetObject(this, f_obj_off, x, o);
     }
 
-    public void testLong(long x, long i)
+    public boolean testLong(long x, long i)
     {
-        unsafe.compareAndSetLong(this, f_long_off, x, i);
+        return unsafe.weakCompareAndSetLong(this, f_long_off, x, i);
     }
 
-    public void testByte(byte x, byte i)
+    public boolean testByte(byte x, byte i)
     {
-        unsafe.compareAndSetByte(this, f_byte_off, x, i);
+        return unsafe.weakCompareAndSetByte(this, f_byte_off, x, i);
     }
 
-    public void testShort(short x, short i)
+    public boolean testShort(short x, short i)
     {
-        unsafe.compareAndSetShort(this, f_short_off, x, i);
+        return unsafe.weakCompareAndSetShort(this, f_short_off, x, i);
     }
 }