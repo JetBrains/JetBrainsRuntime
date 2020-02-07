907f9ee7083df384a7433cb933522680e70dcb1f
Christoph Langer <clanger@openjdk.org>
2020-02-07 08:38:40 +0100

    8237192: Generate stripped/public pdbs on Windows for jdk images
    Co-authored-by: Matthias Baesken <matthias.baesken@sap.com>
    Reviewed-by: erikj, ihse

--- make/scripts/compare.sh
+++ make/scripts/compare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2012, 2018, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2012, 2020, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # This code is free software; you can redistribute it and/or modify it
@@ -657,7 +657,6 @@ compare_bin_file() {
         # pdb files.
         PDB_DIRS="$(ls -d \
             {$OTHER,$THIS}/support/modules_{cmds,libs}/{*,*/*} \
-            {$OTHER,$THIS}/support/native/java.base/java_objs \
             )"
         export _NT_SYMBOL_PATH="$(echo $PDB_DIRS | tr ' ' ';')"
     fi
