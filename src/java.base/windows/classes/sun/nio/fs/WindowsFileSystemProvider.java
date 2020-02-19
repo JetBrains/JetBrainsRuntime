22172f8333bf4e207bdeeaf244c8384211580fcb
Renaud Paquay <rpaquay@google.com>
2020-02-19 12:06:27 -0800

    Add BasicWithKeyFileAttributeView interface
    This new interface is similar to `BasicFileAttributeView` except it
    gives implementations a hint that the fileKey() should be acquired
    even at some performance cost.
    
    `FileTreeWalker` uses this new interface to request a file key
    in addition to regular file attributes so that file equality can
    be efficiently performed when checking for loops during file
    tree traversal.
    
    This makes `FileTreeWalker` about 2x faster when traversing non
    trivial file system trees with the FOLLOW_LINKS option.
    
    Change-Id: I8de047c8fc241dbab9ad57c5e361118a3a94893d
    
    (AKA JBR-3680 Cherry-pick Google's NIO patches to get faster file listing)
    
    (cherry picked from commit 6d1c3f06c41a7281beb4d8f2ad07deb65f4dc4e9)

--- src/java.base/windows/classes/sun/nio/fs/WindowsFileSystemProvider.java
+++ src/java.base/windows/classes/sun/nio/fs/WindowsFileSystemProvider.java
@@ -166,6 +166,8 @@ class WindowsFileSystemProvider
         boolean followLinks = Util.followLinks(options);
         if (view == BasicFileAttributeView.class)
             return (V) WindowsFileAttributeViews.createBasicView(file, followLinks);
+        if (view == BasicWithKeyFileAttributeView.class)
+            return (V) WindowsFileAttributeViews.createBasicWithKeyView(file, followLinks);
         if (view == DosFileAttributeView.class)
             return (V) WindowsFileAttributeViews.createDosView(file, followLinks);
         if (view == AclFileAttributeView.class)
@@ -203,6 +205,8 @@ class WindowsFileSystemProvider
         boolean followLinks = Util.followLinks(options);
         if (name.equals("basic"))
             return WindowsFileAttributeViews.createBasicView(file, followLinks);
+        if (name.equals("basicwithkey"))
+            return WindowsFileAttributeViews.createBasicWithKeyView(file, followLinks);
         if (name.equals("dos"))
             return WindowsFileAttributeViews.createDosView(file, followLinks);
         if (name.equals("acl"))
