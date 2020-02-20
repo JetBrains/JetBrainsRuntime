fa09cca489bb12e3405ff1cf0ca44d0a8ca25054
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2020-02-20 16:14:08 +0300

    JBR-1929 FractionalMetricsSupport
    (cherry picked from commit bbdc159762ab0e62b09bdfcec4812df9fd619b12)

--- src/java.desktop/share/classes/javax/swing/JComponent.java
+++ src/java.desktop/share/classes/javax/swing/JComponent.java
@@ -427,6 +427,7 @@ public abstract class JComponent extends Container implements Serializable,
      * AA text hints.
      */
     private transient Object aaHint;
+    private transient Object fmHint;
     private transient Object lcdRenderingHint;
 
     static {
@@ -732,6 +733,8 @@ public abstract class JComponent extends Container implements Serializable,
         // aaText shouldn't persist between look and feels, reset it.
         aaHint = UIManager.getDefaults().get(
                 RenderingHints.KEY_TEXT_ANTIALIASING);
+        fmHint = UIManager.getDefaults().get(
+                RenderingHints.KEY_FRACTIONALMETRICS);
         lcdRenderingHint = UIManager.getDefaults().get(
                 RenderingHints.KEY_TEXT_LCD_CONTRAST);
         ComponentUI oldUI = ui;
@@ -4110,6 +4113,8 @@ public abstract class JComponent extends Container implements Serializable,
     public final Object getClientProperty(Object key) {
         if (key == RenderingHints.KEY_TEXT_ANTIALIASING) {
             return aaHint;
+        } else if (key == RenderingHints.KEY_FRACTIONALMETRICS) {
+            return fmHint;
         } else if (key == RenderingHints.KEY_TEXT_LCD_CONTRAST) {
             return lcdRenderingHint;
         }
@@ -4153,6 +4158,9 @@ public abstract class JComponent extends Container implements Serializable,
     public final void putClientProperty(Object key, Object value) {
         if (key == RenderingHints.KEY_TEXT_ANTIALIASING) {
             aaHint = value;
+            return;
+        } else if (key == RenderingHints.KEY_FRACTIONALMETRICS) {
+            fmHint = value;
             return;
         } else if (key == RenderingHints.KEY_TEXT_LCD_CONTRAST) {
             lcdRenderingHint = value;
