4f7ad29ce9ecbd4ca7e36bb5cb76c38f593a9838
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2020-03-13 15:44:47 +0700

    JBR-2212 add scripts for linux_x86, linux_aarch64, linux_x64_fastdebug, osx_fastdebug, windows_x86
    (cherry picked from commit 211b3069491ebd7b0fde90527abab1e1a23a4846)

--- jb/project/tools/windows/scripts/mkbundles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-
-JBSDK_VERSION=$1
-JDK_BUILD_NUMBER=$2
-build_number=$3
-
-jb/project/tools/windows/scripts/mkimages.sh $JBSDK_VERSION $JDK_BUILD_NUMBER $build_number "jcef" || exit $?
-jb/project/tools/windows/scripts/mkimages.sh $JBSDK_VERSION $JDK_BUILD_NUMBER $build_number "jfx" || exit $?
