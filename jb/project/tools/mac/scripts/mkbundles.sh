be8180c26905633bbd0f4e177879f5b6b0d5b305
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2020-03-13 15:44:47 +0700

    JBR-2212 add scripts for linux_x86, linux_aarch64, linux_x64_fastdebug, osx_fastdebug, windows_x86

--- jb/project/tools/mac/scripts/mkbundles.sh
+++ jb/project/tools/mac/scripts/mkbundles.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash -x
 
 JBSDK_VERSION=$1
 JDK_BUILD_NUMBER=$2
 build_number=$3
-
-bash -x jb/project/tools/mac/scripts/mkimages.sh $JBSDK_VERSION $JDK_BUILD_NUMBER $build_number "jcef"
-bash -x jb/project/tools/mac/scripts/mkimages.sh $JBSDK_VERSION $JDK_BUILD_NUMBER $build_number "jfx"
+script_dir=jb/project/tools/mac/scripts
+${script_dir}/mkimages.sh $JBSDK_VERSION $JDK_BUILD_NUMBER $build_number "jcef"
+${script_dir}/mkimages.sh $JBSDK_VERSION $JDK_BUILD_NUMBER $build_number "jfx"
