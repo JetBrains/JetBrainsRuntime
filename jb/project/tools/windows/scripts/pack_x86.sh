6404a58babc10c3a6a6bbd14a14b1c9339a9687a
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2020-03-13 15:44:47 +0700

    JBR-2212 add scripts for linux_x86, linux_aarch64, linux_x64_fastdebug, osx_fastdebug, windows_x86

---
+++ jb/project/tools/windows/scripts/pack_x86.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -x
+
+# The following parameters must be specified:
+#   JBSDK_VERSION    - specifies the current version of OpenJDK e.g. 11_0_6
+#   JDK_BUILD_NUMBER - specifies the number of OpenJDK build or the value of --with-version-build argument to configure
+#   build_number     - specifies the number of JetBrainsRuntime build
+#   bundle_type      - specifies bundle to bu built; possible values:
+#                        jcef - the bundles 1) jbr with jcef+javafx, 2) jbrsdk and 3) test will be created
+#                        jfx  - the bundle 1) jbr with javafx only will be created
+#
+# jbrsdk-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
+# jbr-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
+#
+# $ ./java --version
+# openjdk 11.0.6 2020-01-14
+# OpenJDK Runtime Environment (build 11.0.6+${JDK_BUILD_NUMBER}-b${build_number})
+# OpenJDK 64-Bit Server VM (build 11.0.6+${JDK_BUILD_NUMBER}-b${build_number}, mixed mode)
+#
+
+JBSDK_VERSION=$1
+JDK_BUILD_NUMBER=$2
+build_number=$3
+
+JBRSDK_BASE_NAME=jbrsdk-$JBSDK_VERSION
+JBR_BASE_NAME=jbr-$JBSDK_VERSION
+
+IMAGES_DIR=build/windows-x86-normal-server-release/images
+JSDK=$IMAGES_DIR/jdk
+JBSDK=$JBRSDK_BASE_NAME-windows-x86-b$build_number
+BASE_DIR=.
+
+JBRSDK_BUNDLE=jbrsdk
+echo Creating $JBSDK.tar.gz ...
+/usr/bin/tar -czf $JBSDK.tar.gz $JBRSDK_BUNDLE || exit 1
+
+JBR_BUNDLE=jbr
+JBR_BASE_NAME=jbr-${JBSDK_VERSION}
+
+JBR=$JBR_BASE_NAME-windows-x86-b$build_number
+echo Creating $JBR.tar.gz ...
+/usr/bin/tar -czf $JBR.tar.gz -C $BASE_DIR ${JBR_BUNDLE} || exit 1
+
+JBRSDK_TEST=$JBRSDK_BASE_NAME-windows-test-x86-b$build_number
+echo Creating $JBRSDK_TEST.tar.gz ...
+/usr/bin/tar -czf $JBRSDK_TEST.tar.gz -C $IMAGES_DIR --exclude='test/jdk/demos' test || exit 1
