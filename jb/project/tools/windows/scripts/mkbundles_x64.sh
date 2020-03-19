a408ed6550f5c28a8422088bf0669b64a11d71f4
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2020-03-19 15:21:15 +0700

    JBR-2217 provide JCEF-only (no JavaFX) bundle for master/202 branches

--- jb/project/tools/windows/scripts/mkbundles_x64.sh
+++ jb/project/tools/windows/scripts/mkbundles_x64.sh
@@ -6,3 +6,4 @@ build_number=$3
 script_dir=jb/project/tools/windows/scripts
 ${script_dir}/mkimages_x64.sh $JBSDK_VERSION $JDK_BUILD_NUMBER $build_number "jcef" || exit $?
 ${script_dir}/mkimages_x64.sh $JBSDK_VERSION $JDK_BUILD_NUMBER $build_number "jfx" || exit $?
+${script_dir}/mkimages_x64.sh $JBSDK_VERSION $JDK_BUILD_NUMBER $build_number "jfx_jcef" || exit $?
