a50a5f1bea82615ee4984b2281b8873723153c4d
Anton Tarasov <anton.tarasov@jetbrains.com>
2020-04-09 15:20:54 +0300

    [a11y] Separate base part from JavaComponentAccessibility
    - Create JavaBaseAccessibility and JavaListAccessibility for the new a11y
    - Adopt NSAccessibilityList protocol (based on investigations by Artem Semenov)
    - Use JavaComponentAccessibility for the legacy a11y

---
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/JavaListAccessibility.h
@@ -0,0 +1,16 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+#ifndef NATIVE_JAVALISTACCESSIBILITY_H
+#define NATIVE_JAVALISTACCESSIBILITY_H
+
+#endif // NATIVE_JAVALISTACCESSIBILITY_H
+
+#import "JavaBaseAccessibility.h"
+
+@interface JavaListAccessibility : JavaBaseAccessibility<NSAccessibilityList> {
+}
+@end
+
+//@interface JavaRowAccessibility : JavaElementAccessibility<NSAccessibilityRow> {
+//}
+//@end
