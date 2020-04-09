0d7bca664310ad14bdc0968c8381958d215473b8
Anton Tarasov <anton.tarasov@jetbrains.com>
2020-04-09 15:20:54 +0300

    [a11y] Separate base part from JavaComponentAccessibility
    - Create JavaBaseAccessibility and JavaListAccessibility for the new a11y
    - Adopt NSAccessibilityList protocol (based on investigations by Artem Semenov)
    - Use JavaComponentAccessibility for the legacy a11y

---
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/JavaBaseAccessibility.h
@@ -0,0 +1,52 @@
+#include "jni.h"
+
+#import <AppKit/AppKit.h>
+
+//#define JAVA_AX_DEBUG 1
+//#define JAVA_AX_NO_IGNORES 1
+//#define JAVA_AX_DEBUG_PARMS 1
+
+// these constants are duplicated in CAccessibility.java
+#define JAVA_AX_ALL_CHILDREN (-1)
+#define JAVA_AX_SELECTED_CHILDREN (-2)
+#define JAVA_AX_VISIBLE_CHILDREN (-3)
+// If the value is >=0, it's an index
+
+@interface JavaBaseAccessibility : NSAccessibilityElement {
+    NSView *fView;
+    NSObject *fParent;
+
+    NSString *fNSRole;
+    NSString *fJavaRole;
+
+    jint fIndex;
+    jobject fAccessible;
+    jobject fComponent;
+}
+
+- (id)initWithParent:(NSObject*)parent withEnv:(JNIEnv *)env withAccessible:(jobject)accessible withIndex:(jint)index withView:(NSView *)view withJavaRole:(NSString *)javaRole;
+- (void)unregisterFromCocoaAXSystem;
+- (void)postValueChanged;
+- (void)postSelectedTextChanged;
+- (void)postSelectionChanged;
+- (BOOL)isEqual:(id)anObject;
+- (BOOL)isAccessibleWithEnv:(JNIEnv *)env forAccessible:(jobject)accessible;
+
++ (void)postFocusChanged:(id)message;
+
++ (NSArray*)childrenOfParent:(JavaBaseAccessibility*)parent withEnv:(JNIEnv *)env withChildrenCode:(NSInteger)whichChildren allowIgnored:(BOOL)allowIgnored;
++ (JavaBaseAccessibility *) createWithParent:(JavaBaseAccessibility *)parent accessible:(jobject)jaccessible role:(NSString *)javaRole index:(jint)index withEnv:(JNIEnv *)env withView:(NSView *)view;
++ (JavaBaseAccessibility *) createWithAccessible:(jobject)jaccessible role:(NSString *)role index:(jint)index withEnv:(JNIEnv *)env withView:(NSView *)view;
++ (JavaBaseAccessibility *) createWithAccessible:(jobject)jaccessible withEnv:(JNIEnv *)env withView:(NSView *)view;
+
+- (jobject)axContextWithEnv:(JNIEnv *)env;
+- (NSView*)view;
+- (NSWindow*)window;
+- (id)parent;
+- (NSString *)javaRole;
+- (BOOL)isMenu;
+- (BOOL)isSelected:(JNIEnv *)env;
+- (BOOL)isSelectable:(JNIEnv *)env;
+- (BOOL)isVisible:(JNIEnv *)env;
+
+@end
