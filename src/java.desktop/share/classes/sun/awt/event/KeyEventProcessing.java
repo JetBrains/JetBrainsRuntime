a3e3c23cb17aba0bf2288cf2132505496ead93ec
Denis Konoplev <denis.konoplev@jetbrains.com>
2020-04-15 17:26:06 +0300

    JBR-2280: Fix regression. Mode compatible with old option.

--- src/java.desktop/share/classes/sun/awt/event/KeyEventProcessing.java
+++ src/java.desktop/share/classes/sun/awt/event/KeyEventProcessing.java
@@ -5,5 +5,25 @@ import java.security.PrivilegedAction;
 
 public class KeyEventProcessing {
     @Native
-    public final static boolean useNationalLayouts = java.security.AccessController.doPrivileged((PrivilegedAction<Boolean>)()-> "true".equals(System.getProperty("com.sun.awt.use.national.layouts")));
+    public final static boolean useNationalLayoutsNonNull;
+
+    // This property is used to emulate old behavior
+    // when user should turn off national keycodes processing
+    // "com.jetbrains.use.old.keyevent.processing"
+    @Native
+    public final static boolean useNationalLayouts;
+
+    public final static String VMOptionString = "com.sun.awt.use.national.layouts";
+
+    static {
+        String propertyValue = getProperty();
+        useNationalLayoutsNonNull = "true".equals(propertyValue);
+        useNationalLayouts = useNationalLayoutsNonNull || propertyValue == null;
+    }
+
+    static String getProperty() {
+        return java.security.AccessController.doPrivileged(
+                (PrivilegedAction<String>)() -> System.getProperty(VMOptionString)
+        );
+    }
 }
