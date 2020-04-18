236b2762a985aeedb2f486dd5f92903fe67dfc3c
Anton Tarasov <anton.tarasov@jetrains.com>
2020-04-18 16:01:56 +0300

    JBR-2305 jcef: jb/java/jcef/JCEFStartupTest.java throws java.lang.ExceptionInInitializerError
    (cherry picked from commit f0385f01ecef3232a449230e56c155a9c33c2f86)
    (cherry picked from commit c7d407e0a67e6ba934a475159e9c3a5e6dac14f9)

--- test/jdk/jb/java/jcef/JBCefApp.java
+++ test/jdk/jb/java/jcef/JBCefApp.java
@@ -129,8 +129,10 @@ public abstract class JBCefApp {
             settings.resources_dir_path = JCEF_PATH;
             settings.locales_dir_path = JCEF_PATH + "/locales";
             settings.browser_subprocess_path = JCEF_PATH + "/jcef_helper";
+            double scale = sysScale();
+            System.setProperty("jcef.forceDeviceScaleFactor", Double.toString(scale));
             return new String[] {
-                "--force-device-scale-factor=" + sysScale()
+                "--force-device-scale-factor=" + scale
             };
         }
     }
@@ -152,7 +154,7 @@ public abstract class JBCefApp {
 
     public static double sysScale() {
         try {
-            GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration().getDefaultTransform().getScaleX();
+            return GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration().getDefaultTransform().getScaleX();
         } catch (NullPointerException ignore) {}
         return 1.0;
     }
