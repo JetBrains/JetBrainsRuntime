83dab3ff6f8c69b37f84e4c267e8b74e2c54a96c
xuelei <unknown>
2020-04-24 13:30:47 -0700

    8236464: SO_LINGER option is ignored by SSLSocket in JDK 11 Reviewed-by: ascarpino

--- src/java.base/share/classes/sun/security/ssl/TransportContext.java
+++ src/java.base/share/classes/sun/security/ssl/TransportContext.java
@@ -239,7 +239,7 @@ class TransportContext implements ConnectionContext {
                 (handshakeContext instanceof PostHandshakeContext);
     }
 
-    // Note: close_notify is delivered as a warning alert.
+    // Note: Don't use this method for close_nofity, use closeNotify() instead.
     void warning(Alert alert) {
         // For initial handshaking, don't send a warning alert message to peer
         // if handshaker has not started.
@@ -250,6 +250,30 @@ class TransportContext implements ConnectionContext {
                 if (SSLLogger.isOn && SSLLogger.isOn("ssl")) {
                     SSLLogger.warning(
                         "Warning: failed to send warning alert " + alert, ioe);
+                }
+            }
+        }
+    }
+
+    // Note: close_notify is delivered as a warning alert.
+    void closeNotify(boolean isUserCanceled) throws IOException {
+        // Socket transport is special because of the SO_LINGER impact.
+        if (transport instanceof SSLSocketImpl) {
+            ((SSLSocketImpl)transport).closeNotify(isUserCanceled);
+        } else {
+            // Need a lock here so that the user_canceled alert and the
+            // close_notify alert can be delivered together.
+            synchronized (outputRecord) {
+                try {
+                    // send a user_canceled alert if needed.
+                    if (isUserCanceled) {
+                        warning(Alert.USER_CANCELED);
+                    }
+
+                    // send a close_notify alert
+                    warning(Alert.CLOSE_NOTIFY);
+                } finally {
+                    outputRecord.close();
                 }
             }
         }
@@ -502,14 +526,7 @@ class TransportContext implements ConnectionContext {
             }
 
             if (needCloseNotify) {
-                synchronized (outputRecord) {
-                    try {
-                        // send a close_notify alert
-                        warning(Alert.CLOSE_NOTIFY);
-                    } finally {
-                        outputRecord.close();
-                    }
-                }
+                closeNotify(false);
             }
         }
     }
@@ -545,21 +562,7 @@ class TransportContext implements ConnectionContext {
             useUserCanceled = true;
         }
 
-        // Need a lock here so that the user_canceled alert and the
-        // close_notify alert can be delivered together.
-        synchronized (outputRecord) {
-            try {
-                // send a user_canceled alert if needed.
-                if (useUserCanceled) {
-                    warning(Alert.USER_CANCELED);
-                }
-
-                // send a close_notify alert
-                warning(Alert.CLOSE_NOTIFY);
-            } finally {
-                outputRecord.close();
-            }
-        }
+        closeNotify(useUserCanceled);
     }
 
     // Note; HandshakeStatus.FINISHED status is retrieved in other places.
