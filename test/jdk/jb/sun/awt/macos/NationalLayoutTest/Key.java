489475a00e9f9325870b66deeb7269fcaca86fe9
Elena Sayapina <elena.sayapina@jetbrains.com>
2020-05-07 01:04:04 +0700

    IDEA-165950 [TESTUPDATE] National keyboard layouts support
    Update regression test after the following commits:
    
    02fad83c: Remove public constants from KeyEvent
    f4227faf: Impossible to assign cmd+ß shortcuts
    (cherry picked from commit 264802cf4be5456d766070094a4c656f54221a6b)

--- test/jdk/jb/sun/awt/macos/NationalLayoutTest/Key.java
+++ test/jdk/jb/sun/awt/macos/NationalLayoutTest/Key.java
@@ -17,6 +17,9 @@
 import java.awt.event.KeyEvent;
 import java.util.HashMap;
 
+import static java.awt.event.KeyEvent.*;
+import static sun.awt.event.KeyEvent.*; /* comment this line to compile with jbrsdk8 */
+
 /*
  * Class containing common key functionality
  */
@@ -93,40 +96,41 @@ public class Key {
     private static final HashMap<Character, Integer> latinKeyCodesMap = new HashMap<Character, Integer>() {
         {
             // Please see:
-            // jbruntime/src/java.desktop/share/classes/java/awt/event/KeyEvent.java
+            // jbruntime/src/java.desktop/share/classes/sun/awt/event/KeyEvent.java
 
-            put((char) 0x00E0, KeyEvent.VK_A_WITH_GRAVE);
-            put((char) 0x00E1, KeyEvent.VK_A_WITH_ACUTE);
-            put((char) 0x00E2, KeyEvent.VK_A_WITH_CIRCUMFLEX);
-            put((char) 0x00E3, KeyEvent.VK_A_WITH_TILDE);
-            put((char) 0x00E4, KeyEvent.VK_A_WITH_DIAERESIS);
-            put((char) 0x00E5, KeyEvent.VK_A_WITH_RING_ABOVE);
-            put((char) 0x00E6, KeyEvent.VK_AE);
-            put((char) 0x00E7, KeyEvent.VK_C_WITH_CEDILLA);
-            put((char) 0x00E8, KeyEvent.VK_E_WITH_GRAVE);
-            put((char) 0x00E9, KeyEvent.VK_E_WITH_ACUTE);
-            put((char) 0x00EA, KeyEvent.VK_E_WITH_CIRCUMFLEX);
-            put((char) 0x00EB, KeyEvent.VK_E_WITH_DIAERESIS);
-            put((char) 0x00EC, KeyEvent.VK_I_WITH_GRAVE);
-            put((char) 0x00ED, KeyEvent.VK_I_WITH_GRAVE);
-            put((char) 0x00EE, KeyEvent.VK_I_WITH_CIRCUMFLEX);
-            put((char) 0x00EF, KeyEvent.VK_I_WITH_DIAERESIS);
-            put((char) 0x00F0, KeyEvent.VK_ETH);
-            put((char) 0x00F1, KeyEvent.VK_N_WITH_TILDE);
-            put((char) 0x00F2, KeyEvent.VK_O_WITH_GRAVE);
-            put((char) 0x00F3, KeyEvent.VK_O_WITH_ACUTE);
-            put((char) 0x00F4, KeyEvent.VK_O_WITH_CIRCUMFLEX);
-            put((char) 0x00F5, KeyEvent.VK_O_WITH_TILDE);
-            put((char) 0x00F6, KeyEvent.VK_O_WITH_DIAERESIS);
-            put((char) 0x00F7, KeyEvent.VK_DIVISION_SIGN);
-            put((char) 0x00F8, KeyEvent.VK_O_WITH_SLASH);
-            put((char) 0x00F9, KeyEvent.VK_U_WITH_GRAVE);
-            put((char) 0x00FA, KeyEvent.VK_U_WITH_ACUTE);
-            put((char) 0x00FB, KeyEvent.VK_U_WITH_CIRCUMFLEX);
-            put((char) 0x00FC, KeyEvent.VK_U_WITH_DIAERESIS);
-            put((char) 0x00FD, KeyEvent.VK_Y_WITH_ACUTE);
-            put((char) 0x00FE, KeyEvent.VK_THORN);
-            put((char) 0x00FF, KeyEvent.VK_Y_WITH_DIAERESIS);
+            put((char) 0x00DF, VK_ESZETT);
+            put((char) 0x00E0, VK_A_WITH_GRAVE);
+            put((char) 0x00E1, VK_A_WITH_ACUTE);
+            put((char) 0x00E2, VK_A_WITH_CIRCUMFLEX);
+            put((char) 0x00E3, VK_A_WITH_TILDE);
+            put((char) 0x00E4, VK_A_WITH_DIAERESIS);
+            put((char) 0x00E5, VK_A_WITH_RING_ABOVE);
+            put((char) 0x00E6, VK_AE);
+            put((char) 0x00E7, VK_C_WITH_CEDILLA);
+            put((char) 0x00E8, VK_E_WITH_GRAVE);
+            put((char) 0x00E9, VK_E_WITH_ACUTE);
+            put((char) 0x00EA, VK_E_WITH_CIRCUMFLEX);
+            put((char) 0x00EB, VK_E_WITH_DIAERESIS);
+            put((char) 0x00EC, VK_I_WITH_GRAVE);
+            put((char) 0x00ED, VK_I_WITH_GRAVE);
+            put((char) 0x00EE, VK_I_WITH_CIRCUMFLEX);
+            put((char) 0x00EF, VK_I_WITH_DIAERESIS);
+            put((char) 0x00F0, VK_ETH);
+            put((char) 0x00F1, VK_N_WITH_TILDE);
+            put((char) 0x00F2, VK_O_WITH_GRAVE);
+            put((char) 0x00F3, VK_O_WITH_ACUTE);
+            put((char) 0x00F4, VK_O_WITH_CIRCUMFLEX);
+            put((char) 0x00F5, VK_O_WITH_TILDE);
+            put((char) 0x00F6, VK_O_WITH_DIAERESIS);
+            put((char) 0x00F7, VK_DIVISION_SIGN);
+            put((char) 0x00F8, VK_O_WITH_SLASH);
+            put((char) 0x00F9, VK_U_WITH_GRAVE);
+            put((char) 0x00FA, VK_U_WITH_ACUTE);
+            put((char) 0x00FB, VK_U_WITH_CIRCUMFLEX);
+            put((char) 0x00FC, VK_U_WITH_DIAERESIS);
+            put((char) 0x00FD, VK_Y_WITH_ACUTE);
+            put((char) 0x00FE, VK_THORN);
+            put((char) 0x00FF, VK_Y_WITH_DIAERESIS);
         }
     };
 
@@ -137,30 +141,30 @@ public class Key {
             // jbruntime/src/java.desktop/windows/native/libawt/windows/awt_Component.cpp
             // jbruntime/src/java.desktop/macosx/native/libawt_lwawt/awt/AWTEvent.m
 
-            put((char) 0x02D9, KeyEvent.VK_DEAD_ABOVEDOT);
-            put((char) 0x02DA, KeyEvent.VK_DEAD_ABOVERING);
-            put((char) 0x00B4, KeyEvent.VK_DEAD_ACUTE);              // ACUTE ACCENT
-            put((char) 0x0384, KeyEvent.VK_DEAD_ACUTE);              // GREEK TONOS
-            // TODO Should test map ' to acute as sometimes it may add either acute or cedilla to the next key
-            //put((char) 0x0027 /* ' */, KeyEvent.VK_DEAD_ACUTE);      // APOSTROPHE
-            put((char) 0x02D8, KeyEvent.VK_DEAD_BREVE);
-            put((char) 0x02C7, KeyEvent.VK_DEAD_CARON);
-            put((char) 0x00B8, KeyEvent.VK_DEAD_CEDILLA);            // CEDILLA
-            put((char) 0x002C /* , */, KeyEvent.VK_DEAD_CEDILLA);    // COMMA
-            put((char) 0x02C6, KeyEvent.VK_DEAD_CIRCUMFLEX);         // MODIFIER LETTER CIRCUMFLEX ACCENT
-            put((char) 0x005E, KeyEvent.VK_DEAD_CIRCUMFLEX);         // CIRCUMFLEX ACCENT
-            put((char) 0x00A8, KeyEvent.VK_DEAD_DIAERESIS);          // DIAERESIS
-            put((char) 0x0022 /* " */, KeyEvent.VK_DEAD_DIAERESIS);  // QUOTATION MARK
-            put((char) 0x02DD, KeyEvent.VK_DEAD_DOUBLEACUTE);
-            put((char) 0x0060, KeyEvent.VK_DEAD_GRAVE);
-            put((char) 0x037A, KeyEvent.VK_DEAD_IOTA);
-            put((char) 0x02C9, KeyEvent.VK_DEAD_MACRON);             // MODIFIER LETTER MACRON
-            put((char) 0x00AF, KeyEvent.VK_DEAD_MACRON);             // MACRON
-            put((char) 0x02DB, KeyEvent.VK_DEAD_OGONEK);
-            put((char) 0x02DC, KeyEvent.VK_DEAD_TILDE);              // SMALL TILDE
-            put((char) 0x007E, KeyEvent.VK_DEAD_TILDE);              // TILDE
-            put((char) 0x309B, KeyEvent.VK_DEAD_VOICED_SOUND);
-            put((char) 0x309C, KeyEvent.VK_DEAD_SEMIVOICED_SOUND);
+            put((char) 0x02D9, VK_DEAD_ABOVEDOT);
+            put((char) 0x02DA, VK_DEAD_ABOVERING);
+            put((char) 0x00B4, VK_DEAD_ACUTE);              // ACUTE ACCENT
+            put((char) 0x0384, VK_DEAD_ACUTE);              // GREEK TONOS
+            // TODO No corresponding VK_DEAD constant for this key as it may add either acute or cedilla to the next key
+            //put((char) 0x0027 /* ' */, VK_DEAD_QUOTE);      // APOSTROPHE, QUOTE
+            put((char) 0x02D8, VK_DEAD_BREVE);
+            put((char) 0x02C7, VK_DEAD_CARON);
+            put((char) 0x00B8, VK_DEAD_CEDILLA);            // CEDILLA
+            put((char) 0x002C /* , */, VK_DEAD_CEDILLA);    // COMMA
+            put((char) 0x02C6, VK_DEAD_CIRCUMFLEX);         // MODIFIER LETTER CIRCUMFLEX ACCENT
+            put((char) 0x005E, VK_DEAD_CIRCUMFLEX);         // CIRCUMFLEX ACCENT
+            put((char) 0x00A8, VK_DEAD_DIAERESIS);          // DIAERESIS
+            put((char) 0x0022 /* " */, VK_DEAD_DIAERESIS);  // QUOTATION MARK
+            put((char) 0x02DD, VK_DEAD_DOUBLEACUTE);
+            put((char) 0x0060, VK_DEAD_GRAVE);
+            put((char) 0x037A, VK_DEAD_IOTA);
+            put((char) 0x02C9, VK_DEAD_MACRON);             // MODIFIER LETTER MACRON
+            put((char) 0x00AF, VK_DEAD_MACRON);             // MACRON
+            put((char) 0x02DB, VK_DEAD_OGONEK);
+            put((char) 0x02DC, VK_DEAD_TILDE);              // SMALL TILDE
+            put((char) 0x007E, VK_DEAD_TILDE);              // TILDE
+            put((char) 0x309B, VK_DEAD_VOICED_SOUND);
+            put((char) 0x309C, VK_DEAD_SEMIVOICED_SOUND);
         }
     };
 }
