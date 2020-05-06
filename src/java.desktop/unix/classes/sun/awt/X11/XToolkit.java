9126908ce57b3047e1f09ffdbae8ac1cadc8f8b9
Denis Konoplev <denis.konoplev@jetbrains.com>
2020-05-06 15:17:45 +0300

    JBR-2347: Don't free memory when thread is dead
    (cherry picked from commit afb3303db5397619b77f1f26e90c622ecaff79e7)

--- src/java.desktop/unix/classes/sun/awt/X11/XToolkit.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XToolkit.java
@@ -742,6 +742,8 @@ public final class XToolkit extends UNIXToolkit implements Runnable {
                 }
 
                 dispatchEvent(ev);
+                // free event data if XGetEventData was called
+                XlibWrapper.XFreeEventData(getDisplay(), ev.pData);
             } catch (ThreadDeath td) {
                 XBaseWindow.ungrabInput();
                 return;
@@ -749,8 +751,6 @@ public final class XToolkit extends UNIXToolkit implements Runnable {
                 XBaseWindow.ungrabInput();
                 processException(thr);
             } finally {
-                // free event data if XGetEventData was called
-                XlibWrapper.XFreeEventData(getDisplay(), ev.pData);
                 awtUnlock();
             }
         }
