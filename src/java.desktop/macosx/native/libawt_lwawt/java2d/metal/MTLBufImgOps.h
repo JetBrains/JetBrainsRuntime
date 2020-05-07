e2e015cb5e59d3df5204893e030b6800f7b5108b
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2020-05-08 00:40:46 +0300

    8242354: implemented remaining bufImgOps (LookupOp, ConvolveOp)

--- src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLBufImgOps.h
+++ src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLBufImgOps.h
@@ -37,15 +37,38 @@
 @property (readonly) jboolean isNonPremult;
 @end
 
-void MTLBufImgOps_EnableConvolveOp(MTLContext *mtlc, jlong pSrcOps,
-                                   jboolean edgeZeroFill,
-                                   jint kernelWidth, jint KernelHeight,
-                                   unsigned char *kernelVals);
-void MTLBufImgOps_DisableConvolveOp(MTLContext *mtlc);
-void MTLBufImgOps_EnableLookupOp(MTLContext *mtlc, jlong pSrcOps,
-                                 jboolean nonPremult, jboolean shortData,
-                                 jint numBands, jint bandLength, jint offset,
-                                 void *tableValues);
-void MTLBufImgOps_DisableLookupOp(MTLContext *mtlc);
+@interface MTLConvolveOp : NSObject
+- (id)init:(jboolean)edgeZeroFill kernelWidth:(jint)kernelWidth
+                                 kernelHeight:(jint)kernelHeight
+                                     srcWidth:(jint)srcWidth
+                                    srcHeight:(jint)srcHeight
+                                       kernel:(unsigned char *)kernel
+                                       device:(id<MTLDevice>)device;
+- (void) dealloc;
+
+- (id<MTLBuffer>) getBuffer;
+- (const float *) getImgEdge;
+- (NSString *)getDescription; // creates autorelease string
+
+@property (readonly) jboolean isEdgeZeroFill;
+@property (readonly) int kernelSize;
+@end
+
+@interface MTLLookupOp : NSObject
+- (id)init:(jboolean)nonPremult shortData:(jboolean)shortData
+                                 numBands:(jint)numBands
+                               bandLength:(jint)bandLength
+                                   offset:(jint)offset
+                              tableValues:(void *)tableValues
+                                   device:(id<MTLDevice>)device;
+- (void) dealloc;
+
+- (jfloat *)getOffset;
+- (id<MTLTexture>) getLookupTexture;
+- (NSString *)getDescription; // creates autorelease string
+
+@property (readonly) jboolean isUseSrcAlpha;
+@property (readonly) jboolean isNonPremult;
+@end
 
 #endif /* MTLBufImgOps_h_Included */
