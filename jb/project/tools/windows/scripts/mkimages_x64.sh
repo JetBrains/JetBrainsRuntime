1c906f0b8d2171bbdae1869e492a4bc25df91e3d
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2020-05-15 12:08:17 +0700

    split checkout before building JBR+JFX or JBR+JCEF on two separate commands

--- jb/project/tools/windows/scripts/mkimages_x64.sh
+++ jb/project/tools/windows/scripts/mkimages_x64.sh
@@ -54,7 +54,8 @@ function create_jbr {
 JBRSDK_BASE_NAME=jbrsdk-${JBSDK_VERSION}
 WORK_DIR=$(pwd)
 
-git checkout -- modules.list src/java.desktop/share/classes/module-info.java
+git checkout -- modules.list
+git checkout -- src/java.desktop/share/classes/module-info.java
 case "$bundle_type" in
   "jfx")
     echo "Excluding jcef modules"
