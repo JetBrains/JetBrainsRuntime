946b7a9289e860fe9cc3195e8c91e9b03f7525a8
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2020-05-17 05:26:32 +0700

    JBR-2395 eliminate JavaFX from JBR

--- jb/project/tools/linux/scripts/mkimages_aarch64.sh
+++ jb/project/tools/linux/scripts/mkimages_aarch64.sh
@@ -4,9 +4,6 @@
 #   JBSDK_VERSION    - specifies the current version of OpenJDK e.g. 11_0_6
 #   JDK_BUILD_NUMBER - specifies the number of OpenJDK build or the value of --with-version-build argument to configure
 #   build_number     - specifies the number of JetBrainsRuntime build
-#   bundle_type      - specifies bundle to bu built; possible values:
-#                        jcef - the bundles 1) jbr with jcef+javafx, 2) jbrsdk and 3) test will be created
-#                        jfx  - the bundle 1) jbr with javafx only will be created
 #
 # jbrsdk-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
 # jbr-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
