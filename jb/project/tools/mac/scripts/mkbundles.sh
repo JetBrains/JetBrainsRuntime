1eab59be8af5a62999a5c6b5703c8bb0c27ce7eb
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2020-05-17 05:26:32 +0700

    JBR-2395 eliminate JavaFX from JBR
    (cherry picked from commit cd7e5395611c1b40146573ca64fd36677bb07991)

--- jb/project/tools/mac/scripts/mkbundles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-JBSDK_VERSION=$1
-JDK_BUILD_NUMBER=$2
-build_number=$3
-script_dir=jb/project/tools/mac/scripts
-${script_dir}/mkimages.sh $JBSDK_VERSION $JDK_BUILD_NUMBER $build_number "jcef" || exit $?
-${script_dir}/mkimages.sh $JBSDK_VERSION $JDK_BUILD_NUMBER $build_number "jfx" || exit $?
-${script_dir}/mkimages.sh $JBSDK_VERSION $JDK_BUILD_NUMBER $build_number "jfx_jcef" || exit $?
