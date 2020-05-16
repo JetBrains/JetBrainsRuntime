c7a3bca1b58a49ebbbd81f40b5bafe01eb1be6f7
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2020-05-17 05:26:32 +0700

    JBR-2395 eliminate JavaFX from JBR
    (cherry picked from commit cd7e5395611c1b40146573ca64fd36677bb07991)

--- jb/project/tools/windows/scripts/pack_x64.sh
+++ jb/project/tools/windows/scripts/pack_x64.sh
@@ -4,9 +4,8 @@
 #   JBSDK_VERSION    - specifies the current version of OpenJDK e.g. 11_0_6
 #   JDK_BUILD_NUMBER - specifies the number of OpenJDK build or the value of --with-version-build argument to configure
 #   build_number     - specifies the number of JetBrainsRuntime build
-#   bundle_type      - specifies bundle to bu built; possible values:
-#                        jcef - the bundles 1) jbr with jcef+javafx, 2) jbrsdk and 3) test will be created
-#                        jfx  - the bundle 1) jbr with javafx only will be created
+#                        jcef - the bundles with jcef
+#                        empty - the bundles without jcef
 #
 # jbrsdk-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
 # jbr-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
@@ -24,28 +23,19 @@ bundle_type=$4
 
 function pack_jbr {
 
-  case "$1" in
-  "${bundle_type}_lw")
-    JBR_BASE_NAME=jbr_${bundle_type}_lw-${JBSDK_VERSION}
-    ;;
-  "jfx" | "jcef")
-    JBR_BASE_NAME=jbr_${bundle_type}-${JBSDK_VERSION}
-    ;;
-  "jfx_jcef")
-    JBR_BASE_NAME=jbr-${JBSDK_VERSION}
-    ;;
-  *)
-    echo "***ERR*** bundle was not specified" && exit 1
-    ;;
-  esac
+  if [ -z "${bundle_type}" ]; then
+    JBR_BUNDLE=jbr
+  else
+    JBR_BUNDLE=jbr_${bundle_type}
+    rm -rf ${BASE_DIR}/jbr
+    cp -R ${BASE_DIR}/${JBR_BUNDLE} ${BASE_DIR}/jbr
+  fi
+  JBR_BASE_NAME=${JBR_BUNDLE}-${JBSDK_VERSION}
 
   JBR=$JBR_BASE_NAME-windows-x64-b$build_number
   echo Creating $JBR.tar.gz ...
-  rm -rf ${BASE_DIR}/jbr
-  cp -R ${BASE_DIR}/${JBR_BUNDLE} ${BASE_DIR}/jbr
 
   /usr/bin/tar -czf $JBR.tar.gz -C $BASE_DIR jbr || exit 1
-  #rm -rf ${BASE_DIR}/${JBR_BUNDLE}
 }
 
 JBRSDK_BASE_NAME=jbrsdk-$JBSDK_VERSION
@@ -56,17 +46,12 @@ JSDK=$IMAGES_DIR/jdk
 JBSDK=$JBRSDK_BASE_NAME-windows-x64-b$build_number
 BASE_DIR=.
 
-if [ "$bundle_type" == "jfx_jcef" ]; then
-  JBRSDK_BUNDLE=jbrsdk
-  echo Creating $JBSDK.tar.gz ...
-  /usr/bin/tar -czf $JBSDK.tar.gz $JBRSDK_BUNDLE || exit 1
-fi
+JBRSDK_BUNDLE=jbrsdk
+echo Creating $JBSDK.tar.gz ...
+/usr/bin/tar -czf $JBSDK.tar.gz $JBRSDK_BUNDLE || exit 1
 
-JBR_BUNDLE=jbr_${bundle_type}
 pack_jbr $bundle_type
 
-if [ "$bundle_type" == "jfx_jcef" ]; then
-  JBRSDK_TEST=$JBRSDK_BASE_NAME-windows-test-x64-b$build_number
-  echo Creating $JBRSDK_TEST.tar.gz ...
-  /usr/bin/tar -czf $JBRSDK_TEST.tar.gz -C $IMAGES_DIR --exclude='test/jdk/demos' test || exit 1
-fi
+JBRSDK_TEST=$JBRSDK_BASE_NAME-windows-test-x64-b$build_number
+echo Creating $JBRSDK_TEST.tar.gz ...
+/usr/bin/tar -czf $JBRSDK_TEST.tar.gz -C $IMAGES_DIR --exclude='test/jdk/demos' test || exit 1
