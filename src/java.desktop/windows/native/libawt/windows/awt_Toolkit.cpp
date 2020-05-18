b66a21a9d140235e22f9154310cecc789140f8ea
serb <unknown>
2020-05-18 15:27:54 +0100

    8243925: Toolkit#getScreenInsets() returns wrong value on HiDPI screens (Windows) Reviewed-by: prr, psadhukhan

--- src/java.desktop/windows/native/libawt/windows/awt_Toolkit.cpp
+++ src/java.desktop/windows/native/libawt/windows/awt_Toolkit.cpp
@@ -2734,22 +2734,14 @@ Java_sun_awt_windows_WToolkit_getScreenInsets(JNIEnv *env,
         jclass insetsClass = env->FindClass("java/awt/Insets");
         DASSERT(insetsClass != NULL);
         CHECK_NULL_RETURN(insetsClass, NULL);
-
         Devices::InstanceAccess devices;
         AwtWin32GraphicsDevice *device = devices->GetDevice(screen);
-        if (device != NULL) {
-            ::SetRect(&rect,
-                device->ScaleDownX(rect.left),
-                device->ScaleDownY(rect.top),
-                device->ScaleDownX(rect.right),
-                device->ScaleDownY(rect.bottom));
-        }
         insets = env->NewObject(insetsClass,
                 AwtToolkit::insetsMID,
-                rect.top,
-                rect.left,
-                rect.bottom,
-                rect.right);
+                device == NULL ? rect.top : device->ScaleDownY(rect.top),
+                device == NULL ? rect.left : device->ScaleDownX(rect.left),
+                device == NULL ? rect.bottom : device->ScaleDownY(rect.bottom),
+                device == NULL ? rect.right : device->ScaleDownX(rect.right));
     }
 
     if (safe_ExceptionOccurred(env)) {
