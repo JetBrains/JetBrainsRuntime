67fa73482088763922ff3b07e472e39555730df5
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2020-05-19 16:27:40 +0700

    JDK14: exclude dependencies on jcef in x86, fastdebug builds
    (cherry picked from commit 14e98eaf9498b95851a34f4018bac4cb2ba32e3a)

--- jb/project/tools/mac/scripts/mkimages_fd.sh
+++ jb/project/tools/mac/scripts/mkimages_fd.sh
@@ -27,6 +27,8 @@ source jb/project/tools/common.sh
 
 JBRSDK_BASE_NAME=jbrsdk-${JBSDK_VERSION}
 
+[ -z "$bundle_type" ] && git apply -p0 < jb/project/tools/exclude_jcef_module.patch
+
 sh configure \
   --disable-warnings-as-errors \
   --with-debug-level=fastdebug \
