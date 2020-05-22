caf366f6f3992646c6f7592927ae4694f2f05c64
Denis Konoplev <denis.konoplev@jetbrains.com>
2020-05-22 03:15:38 +0300

    JBR-215: Separate LatinNonAlphaNumKeycodes option

--- src/java.desktop/share/classes/sun/awt/event/KeyEventProcessing.java
+++ src/java.desktop/share/classes/sun/awt/event/KeyEventProcessing.java
@@ -9,22 +9,20 @@ public class KeyEventProcessing {
     // This property is used to emulate old behavior
     // when user should turn off national keycodes processing
     // "com.jetbrains.use.old.keyevent.processing"
+    public final static String useNationalLayoutsOption = "com.sun.awt.use.national.layouts";
     @Native
-    public final static boolean useNationalLayouts = "true".equals(getProperty());
+    public final static boolean useNationalLayouts = "true".equals(
+            getProperty(useNationalLayoutsOption, SystemInfo.isMac ? "true" : "false"));
 
-    public final static String VMOptionString = "com.sun.awt.use.national.layouts";
+    // Used on windows to emulate latin OEM keys on cyrillic keyboards
+    public final static String useLatinNonAlphaNumKeycodesOption = "com.sun.awt.useLatinNonAlphaNumKeycodes";
+    @Native
+    public final static boolean useLatinNonAlphaNumKeycodes = "true".equals(
+            getProperty(useLatinNonAlphaNumKeycodesOption, "false"));
 
-    private static String getProperty() {
+    private static String getProperty(String option, String dflt) {
         return java.security.AccessController.doPrivileged(
-                (PrivilegedAction<String>)() -> System.getProperty(VMOptionString, getDefaultValue())
+                (PrivilegedAction<String>) () -> System.getProperty(option, dflt)
         );
-    }
-
-    private static String getDefaultValue() {
-        if (SystemInfo.isWindows || SystemInfo.isMac) {
-            return "true";
-        } else {
-            return "false";
-        }
     }
 }
