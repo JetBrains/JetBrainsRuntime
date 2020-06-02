dc24658b316f66a4dbb662a90f3fcbe47a7b89f9
Mikhail Grishchenko <mikhail.grishchenko@jetbrains.com>
2020-06-02 16:51:35 +0700

    JBR-2430 [jcef] Fixed regression test
    Refactoring + changed EDT awaiting method

--- test/jdk/jb/java/jcef/HandleJSQueryTest.java
+++ test/jdk/jb/java/jcef/HandleJSQueryTest.java
@@ -1,12 +1,5 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-/**
- * @test
- * @key headful
- * @summary Regression test for JBR-2430. The test checks that JS Query is handled in 2nd opened browser.
- * @run main/othervm HandleJSQueryTest
- */
-
 import org.cef.browser.CefBrowser;
 import org.cef.browser.CefFrame;
 import org.cef.browser.CefMessageRouter;
@@ -18,22 +11,33 @@ import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @test
+ * @key headful
+ * @summary Regression test for JBR-2430. The test checks that JS Query is handled in 2nd opened browser.
+ * @run main/othervm HandleJSQueryTest
+ */
 
 public class HandleJSQueryTest {
 
     public static void main(String[] args) throws InvocationTargetException, InterruptedException {
-        CefBrowserFrame firstBrowser = new CefBrowserFrame();
-        CefBrowserFrame secondBrowser = new CefBrowserFrame();
+        final CountDownLatch firstLatch = new CountDownLatch(1);
+        final CountDownLatch secondLatch = new CountDownLatch(1);
+        CefBrowserFrame firstBrowser = new CefBrowserFrame(firstLatch);
+        CefBrowserFrame secondBrowser = new CefBrowserFrame(secondLatch);
 
         try {
             SwingUtilities.invokeLater(firstBrowser::initUI);
-            Thread.sleep(3000);
+            firstLatch.await(3, TimeUnit.SECONDS);
 
             SwingUtilities.invokeLater(secondBrowser::initUI);
-            Thread.sleep(3000);
+            secondLatch.await(3, TimeUnit.SECONDS);
 
             if (CefBrowserFrame.callbackCounter < 2) {
-                throw new RuntimeException("Test FAILED. callbackCounter: " + CefBrowserFrame.callbackCounter);
+                throw new RuntimeException("Test FAILED. JS Query was not handled in 2nd opened browser");
             }
             System.out.println("Test PASSED");
         } finally {
@@ -49,25 +53,31 @@ public class HandleJSQueryTest {
 
 class CefBrowserFrame extends JFrame {
 
-    static volatile int callbackCounter;
-    static volatile int queryCounter;
+    static int callbackCounter;
+    static int browserNumber;
 
-    private JBCefBrowser browser = new JBCefBrowser();
+    private final JBCefBrowser browser = new JBCefBrowser();
+
+    private final CountDownLatch latch;
+
+    public CefBrowserFrame(final CountDownLatch latch) {
+        this.latch=latch;
+    }
 
     public void initUI() {
-        queryCounter++;
+        browserNumber++;
         CefMessageRouter.CefMessageRouterConfig config = new org.cef.browser.CefMessageRouter.CefMessageRouterConfig();
-        config.jsQueryFunction = "cef_query_" + queryCounter;
-        config.jsCancelFunction = "cef_query_cancel_" + queryCounter;
+        config.jsQueryFunction = "cef_query_" + browserNumber;
+        config.jsCancelFunction = "cef_query_cancel_" + browserNumber;
         CefMessageRouter msgRouter = CefMessageRouter.create(config);
 
-        msgRouter.addHandler(new CefMessageRouterHandlerAdapter(){
+        msgRouter.addHandler(new CefMessageRouterHandlerAdapter() {
             @Override
             public boolean onQuery(CefBrowser browser, CefFrame frame, long query_id, String request,
                                    boolean persistent, CefQueryCallback callback) {
-                System.out.println(request);
+                System.out.println("The query with request " + request + " is handled.");
                 callbackCounter++;
-                System.out.println(callbackCounter);
+                latch.countDown();
                 return true;
             }
         }, true);
@@ -77,8 +87,8 @@ class CefBrowserFrame extends JFrame {
         browser.getCefClient().addLoadHandler(new CefLoadHandlerAdapter() {
             @Override
             public void onLoadEnd(CefBrowser browser, CefFrame frame, int httpStatusCode) {
-                System.out.println("onLoadEnd " + browser.getURL());
-                String jsFunc = "cef_query_" + queryCounter;
+                System.out.println("Browser " + browserNumber + " is loaded.");
+                String jsFunc = "cef_query_" + browserNumber;
                 String jsQuery = "window." + jsFunc + "({request: '" + jsFunc + "'});";
                 browser.executeJavaScript(jsQuery, "", 0);
             }
