5a4d341818ed8f158f4eecf027505a24d1e57875
Alexey Ushakov <Alexey.Ushakov@jetbrains.com>
2020-06-16 20:16:12 +0300

    JBR-1929 Improve rendering of San Francisco font of macOS Catalina
    Reverting gamma correction because of rendering artifacts in the light theme
    
    This reverts commit 5016db51
    
    (cherry picked from commit c1d644a004e1b9e98865f217f987b463a04ecd00)
    (cherry picked from commit ade5b22a83c6e882e65ff5522a7f55fd06da82b4)

--- src/java.desktop/macosx/native/libawt_lwawt/font/CGGlyphImages.m
+++ src/java.desktop/macosx/native/libawt_lwawt/font/CGGlyphImages.m
@@ -282,13 +282,7 @@ CGGI_CopyImageFromCanvasToRGBInfo(CGGI_GlyphCanvas *canvas, GlyphInfo *info)
 static inline UInt8
 CGGI_ConvertBWPixelToByteGray(UInt32 p)
 {
-    UInt8* lut = getReverseGammaLut();
-
-    UInt8 r = lut[0xFF - (p >> 24 & 0xFF)];
-    UInt8 g = lut[0xFF - (p >> 16 & 0xFF)];
-    UInt8 b = lut[0xFF - (p >>  8 & 0xFF)];
-
-    return ((r + g + b) / 3);
+    return 0xFF - (((p >> 24 & 0xFF) + (p >> 16 & 0xFF) + (p >> 8 & 0xFF)) / 3);
 }
 
 static void
