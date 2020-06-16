d829872d93b4791f7c305bcb39cfd05e65809ed9
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2020-06-16 13:13:04 +0300

    JBR-2496 Prevent JVM stealing focus from other applications on Linux (JBR-2497, JBR-2499, JBR-2503, JBR-2652)
    (cherry picked from commits 87525d1d2a997ce91819df7f76bb00422729bec0, 66381f0dec2a402c6205efda40dbd00d98c39551, 8a789e04e9479728ceea9951fca3fad147336c26, 665ebc5d479bdfd1161650d080486b6ee2683936, 98a9219c238c62231a8d31e89ff5766e497d17f4)
    
    with fix for JBR-5300 Change source code and test files to use GPL license
    
    (cherry picked from commit 42aad68ab4cfaa622c60242f79f678a9253b58c0)

--- src/java.desktop/unix/classes/sun/awt/X11/XToolkit.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XToolkit.java
@@ -205,6 +205,8 @@ public final class XToolkit extends UNIXToolkit implements Runnable {
     static int awt_multiclick_time;
     static boolean securityWarningEnabled;
 
+    private static String desktopStartupId;
+
     /**
      * Dimensions of default virtual screen in pixels. These values are used to
      * limit the maximum size of the window.
@@ -407,6 +409,9 @@ public final class XToolkit extends UNIXToolkit implements Runnable {
 
     static native String getEnv(String key);
 
+    static String getDesktopStartupId() {
+        return desktopStartupId;
+    }
 
     static String getAWTAppClassName() {
         return awtAppClassName;
@@ -431,6 +436,10 @@ public final class XToolkit extends UNIXToolkit implements Runnable {
                 mainClassName = "AWT";
             }
             awtAppClassName = getCorrectXIDString(mainClassName);
+
+            // this should be done before 'load_gtk', as the latter clears the environment variable
+            desktopStartupId = AccessController.doPrivileged((PrivilegedAction<String>) () ->
+                    getEnv("DESKTOP_STARTUP_ID"));
 
             init();
             XWM.init();
