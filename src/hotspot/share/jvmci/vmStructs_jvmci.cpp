dd3a4f42d613242bc83537fd1ff0dd4a30fb88f8
never <unknown>
2020-07-02 17:17:53 +0200

    8247246: Add explicit ResolvedJavaType.link and expose presence of default methods Reviewed-by: kvn, sgehwolf, adinn Contributed-by: Foivos Zakkak <fzakkak@redhat.com>

--- src/hotspot/share/jvmci/vmStructs_jvmci.cpp
+++ src/hotspot/share/jvmci/vmStructs_jvmci.cpp
@@ -530,7 +530,14 @@
                                                                           \
   declare_constant(InstanceKlass::linked)                                 \
   declare_constant(InstanceKlass::fully_initialized)                      \
+                                                                          \
+  /*********************************/                                     \
+  /* InstanceKlass _misc_flags */                                         \
+  /*********************************/                                     \
+                                                                          \
   declare_constant(InstanceKlass::_misc_is_anonymous)                     \
+  declare_constant(InstanceKlass::_misc_has_nonstatic_concrete_methods)   \
+  declare_constant(InstanceKlass::_misc_declares_nonstatic_concrete_methods) \
                                                                           \
   declare_constant(JumpData::taken_off_set)                               \
   declare_constant(JumpData::displacement_off_set)                        \
