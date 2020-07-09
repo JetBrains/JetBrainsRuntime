315b4d89940db3ef60070a26c5374260bf8ceebf
Alexey Titov <alexey.titov@jetbrains.com>
2020-07-09 16:47:23 +0300

    JBR-2479: moved new JIT API to com.jetbrains.management

---
+++ src/java.management/share/classes/com/jetbrains/management/JitState.java
@@ -0,0 +1,8 @@
+package com.jetbrains.management;
+
+public class JitState {
+
+    public static native boolean isCompilationEnabled();
+    public static native boolean isCompilationStoppedForever();
+}
+
