26d2443d5fb13e27012b529a96cb68982ebc1523
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2020-08-08 10:58:42 +0700

    Revert "IDEA-242347: Remove redundant conversion for Latin unicode"
    This reverts commit 5017e2e3

--- src/java.desktop/macosx/native/libawt_lwawt/awt/AWTEvent.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/AWTEvent.m
@@ -446,13 +446,53 @@ static unichar NsGetDeadKeyChar(unsigned short keyCode)
     return 0;
 }
 
-static const int UNICODE_OFFSET = 0x01000000;
+static NSDictionary* getDiacriticUnicharToVkCodeDictionary() {
 
-static BOOL isLatinUnicode(unichar ch) {
-    // Latin-1 Supplement 0x0080 - 0x00FF
-    // Latin Extended-A   0x0100 - 0x017F
-    // Latin Extended-B   0x0180 - 0x024F
-    return 0x0080 <= ch && ch <= 0x024F;
+    static NSDictionary* diacriticUnicharToVkCodeDictionary = nil;
+    static dispatch_once_t onceToken;
+
+    dispatch_once(&onceToken, ^{
+         diacriticUnicharToVkCodeDictionary =
+             [NSDictionary dictionaryWithObjectsAndKeys:
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_A_WITH_GRAVE], @"à",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_A_WITH_ACUTE], @"á",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_A_WITH_CIRCUMFLEX], @"â",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_A_WITH_TILDE], @"ã",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_A_WITH_DIAERESIS], @"ä",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_A_WITH_RING_ABOVE], @"å",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_AE], @"æ",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_C_WITH_CEDILLA], @"ç",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_E_WITH_GRAVE], @"è",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_E_WITH_ACUTE], @"é",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_E_WITH_CIRCUMFLEX], @"ê",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_E_WITH_DIAERESIS], @"ë",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_I_WITH_GRAVE], @"ì",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_I_WITH_ACUTE], @"í",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_I_WITH_CIRCUMFLEX], @"î",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_I_WITH_DIAERESIS], @"ï",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_ETH], @"ð",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_N_WITH_TILDE], @"ñ",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_O_WITH_GRAVE], @"ò",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_O_WITH_ACUTE], @"ó",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_O_WITH_CIRCUMFLEX], @"ô",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_O_WITH_TILDE], @"õ",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_O_WITH_DIAERESIS], @"ö",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_DIVISION_SIGN], @"÷",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_O_WITH_SLASH], @"ø",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_U_WITH_GRAVE], @"ù",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_U_WITH_ACUTE], @"ú",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_U_WITH_CIRCUMFLEX], @"û",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_U_WITH_DIAERESIS], @"ü",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_Y_WITH_ACUTE], @"ý",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_THORN], @"þ",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_Y_WITH_DIAERESIS], @"ÿ",
+                  [NSNumber numberWithInt:sun_awt_event_KeyEvent_VK_ESZETT], @"ß",
+                  nil
+             ];
+             // This is ok to retain a singleton object
+             [diacriticUnicharToVkCodeDictionary retain];
+    });
+    return diacriticUnicharToVkCodeDictionary;
 }
 
 static NSDictionary* getUnicharToVkCodeDictionary() {
@@ -485,19 +525,10 @@ static NSDictionary* getUnicharToVkCodeDictionary() {
                  [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_COMMA], @",",
                  [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_SLASH], @"/",
                  [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_PERIOD], @".",
-                 [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_ASTERISK], @"*",
-                 [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_PLUS], @"+",
+                 [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_MULTIPLY], @"*",
+                 [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_ADD], @"+",
                  [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_COMMA], @",",
                  [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_NUMBER_SIGN], @"#",
-                 [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_DOLLAR], @"$",
-                 [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_CIRCUMFLEX], @"^",
-                 [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_LEFT_PARENTHESIS], @"(",
-                 [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_RIGHT_PARENTHESIS], @")",
-                 [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_UNDERSCORE], @"_",
-                 [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_AMPERSAND], @"&",
-                 [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_QUOTEDBL], @"\"",
-                 [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_EXCLAMATION_MARK], @"!",
-                 [NSNumber numberWithInt:java_awt_event_KeyEvent_VK_LESS], @"<",
                  nil
              ];
              // This is ok to retain a singleton object
@@ -583,21 +614,17 @@ NsCharToJavaVirtualKeyCode(unichar ch, BOOL isDeadChar,
         if ([[NSCharacterSet punctuationCharacterSet] characterIsMember:ch] ||
              [[NSCharacterSet symbolCharacterSet] characterIsMember:ch])
         {
-            // punctuationCharacterSet and symbolCharacterSet are too big
-            // to store them all in UnicharToVkCodeDictionary
-            int tmpKeyCode = [[unicharToVkCodeDictionary objectForKey:[NSString stringWithFormat:@"%C",ch]] intValue];
-            if (tmpKeyCode != 0) {
-                *keyCode = tmpKeyCode;
-                // we cannot find key location from a char, so let's use key code
-                *postsTyped = YES;
-                *keyLocation = keyTable[key].javaKeyLocation;
-                return;
-            }
+             *keyCode = [[unicharToVkCodeDictionary objectForKey:[NSString stringWithFormat:@"%C",ch]] intValue];
+             // we cannot find key location from a char, so let's use key code
+             *postsTyped = YES;
+             *keyLocation = keyTable[key].javaKeyLocation;
+             return;
         }
 
-        // Latin-1 suplement & Latin Extended A & B
-        if (isLatinUnicode(ch)) {
-            *keyCode = ((int) ch) + UNICODE_OFFSET;
+        NSDictionary* diacriticUnicharToVkCodeDictionary = getDiacriticUnicharToVkCodeDictionary();
+        NSNumber * jkc = [diacriticUnicharToVkCodeDictionary objectForKey:[NSString stringWithFormat:@"%C",ch]];
+        if (jkc != nil) {
+            *keyCode = [jkc intValue];
             // we cannot find key location from a char, so let's use key code
             *postsTyped = YES;
             *keyLocation = keyTable[key].javaKeyLocation;
