c9e0ac1f9f72b7e5b43d51b1a393fa03ef413b98
phh <unknown>
2020-09-16 07:42:25 -0700

    8253134: JMM_VERSION should remain at 0x20020000 (JDK 10) in JDK 11 Summary: Change JMM_VERSION to 0x2002000, use @since 11.0.9 for c.s.m.ThreadMXBean.currentThreadAllocatedBytes Reviewed-by: simonis

--- src/jdk.management/share/classes/com/sun/management/ThreadMXBean.java
+++ src/jdk.management/share/classes/com/sun/management/ThreadMXBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -135,7 +135,7 @@ public interface ThreadMXBean extends java.lang.management.ThreadMXBean {
      * @see #isThreadAllocatedMemoryEnabled
      * @see #setThreadAllocatedMemoryEnabled
      *
-     * @since 14
+     * @since 11.0.9
      */
     public default long getCurrentThreadAllocatedBytes() {
         return getThreadAllocatedBytes(Thread.currentThread().getId());
