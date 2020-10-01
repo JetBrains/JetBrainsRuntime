f3adc341e854e600e6ef8a730d56e4d8171c4d73
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2020-10-02 06:29:44 +0700

    JBR-2758 refactor building scripts to apply patches adding required modules instead of excluding
    Add jogl and gluegen modules to support jcef osr mode
    
    (cherry picked from commit e2cf692ec5420a3b60f9ee6a7a9e8bda667bbb8b)

--- jb/project/tools/mac/scripts/mkimages.sh
+++ jb/project/tools/mac/scripts/mkimages.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -x
 
 # The following parameters must be specified:
-#   JBSDK_VERSION    - specifies the current version of OpenJDK e.g. 11_0_6
-#   JDK_BUILD_NUMBER - specifies the number of OpenJDK build or the value of --with-version-build argument to configure
+#   JBSDK_VERSION    - specifies major version of OpenJDK e.g. 11_0_6 (instead of dots '.' underbars "_" are used)
+#   JDK_BUILD_NUMBER - specifies update release of OpenJDK build or the value of --with-version-build argument to configure
 #   build_number     - specifies the number of JetBrainsRuntime build
-#   bundle_type      - specifies bundle to bu built; possible values:
-#                        jcef - the bundles with jcef
-#                        empty - the bundles without jcef
+#   bundle_type      - specifies bundle to be built; possible values:
+#                        <empty> or nomod - the release bundles without any additional modules (jcef)
+#                        jcef - the release bundles with jcef
+#                        fd - the fastdebug bundles which also include the jcef module
 #
 # jbrsdk-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
 # jbr-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
@@ -16,12 +17,19 @@
 # OpenJDK Runtime Environment (build 11.0.6+${JDK_BUILD_NUMBER}-b${build_number})
 # OpenJDK 64-Bit Server VM (build 11.0.6+${JDK_BUILD_NUMBER}-b${build_number}, mixed mode)
 #
+# Environment variables:
+#   MODULAR_SDK_PATH - specifies the path to the directory where imported modules are located.
+#               By default imported modules should be located in ./modular-sdk
+#   JCEF_PATH - specifies the path to the directory with JCEF binaries.
+#               By default JCEF binaries should be located in ./jcef_mac
 
 JBSDK_VERSION=$1
 JDK_BUILD_NUMBER=$2
 build_number=$3
 bundle_type=$4
 JBSDK_VERSION_WITH_DOTS=$(echo $JBSDK_VERSION | sed 's/_/\./g')
+WITH_IMPORT_MODULES="--with-import-modules=${MODULAR_SDK_PATH:=./modular-sdk}"
+JCEF_PATH=${JCEF_PATH:=./jcef_mac}
 MAJOR_JBSDK_VERSION=$(echo $JBSDK_VERSION_WITH_DOTS | awk -F "." '{print $1}')
 
 source jb/project/tools/common.sh
@@ -50,71 +58,89 @@ function create_jbr {
   echo Running jlink....
   ${BASE_DIR}/$JBRSDK_BUNDLE/Contents/Home/bin/jlink \
       --module-path ${BASE_DIR}/${JBRSDK_BUNDLE}/Contents/Home/jmods --no-man-pages --compress=2 \
-      --add-modules $(xargs < modules_tmp.list | sed s/" "//g) --output ${JRE_HOME}  || exit $?
+      --add-modules $(xargs < modules_tmp.list | sed s/" "//g) --output ${JRE_HOME}  || do_exit $?
   grep -v "^JAVA_VERSION" ${BASE_DIR}/${JBRSDK_BUNDLE}/Contents/Home/release | grep -v "^MODULES" >> ${JRE_HOME}/release
   cp -R ${BASE_DIR}/${JBRSDK_BUNDLE}/Contents/MacOS ${JRE_CONTENTS}
   cp ${BASE_DIR}/${JBRSDK_BUNDLE}/Contents/Info.plist ${JRE_CONTENTS}
 
-  rm -rf ${JRE_CONTENTS}/Frameworks || exit $?
-  [ ! -z "${bundle_type}" ] && (cp -a jcef_mac/Frameworks ${JRE_CONTENTS} || exit $?)
+  rm -rf ${JRE_CONTENTS}/Frameworks || do_exit $?
+  [ ! -z "${bundle_type}" ] && (cp -a ${JCEF_PATH}/Frameworks ${JRE_CONTENTS} || do_exit $?)
 
   echo Creating ${JBR}.tar.gz ...
   [ ! -z "${bundle_type}" ] && cp -R ${BASE_DIR}/${JBR_BUNDLE} ${BASE_DIR}/jbr
-  COPYFILE_DISABLE=1 tar -pczf ${JBR}.tar.gz --exclude='*.dSYM' --exclude='man' -C ${BASE_DIR} jbr || exit $?
+  COPYFILE_DISABLE=1 tar -pczf ${JBR}.tar.gz --exclude='*.dSYM' --exclude='man' -C ${BASE_DIR} jbr || do_exit $?
   rm -rf ${BASE_DIR}/${JBR_BUNDLE}
 }
 
 JBRSDK_BASE_NAME=jbrsdk-${JBSDK_VERSION}
-
-#git checkout -- modules.list
-#git checkout -- src/java.desktop/share/classes/module-info.java
-[ -z "$bundle_type" ] && (git apply -p0 < jb/project/tools/patches/exclude_jcef_module.patch || exit $?)
+WITH_DEBUG_LEVEL="--with-debug-level=release"
+RELEASE_NAME=macosx-x86_64-server-release
+JBSDK=${JBRSDK_BASE_NAME}-osx-x64-b${build_number}
+case "$bundle_type" in
+  "jcef")
+    git apply -p0 < jb/project/tools/patches/add_jcef_module.patch || do_exit $?
+    do_reset_changes=1
+    ;;
+  "nomod" | "")
+    bundle_type=""
+    WITH_IMPORT_MODULES=""
+    ;;
+  "fd")
+    git apply -p0 < jb/project/tools/patches/add_jcef_module.patch || do_exit $?
+    do_reset_changes=1
+    WITH_DEBUG_LEVEL="--with-debug-level=fastdebug"
+    RELEASE_NAME=macosx-x86_64-server-fastdebug
+    JBSDK=${JBRSDK_BASE_NAME}-osx-x64-fastdebug-b${build_number}
+    ;;
+esac
 
 sh configure \
   --disable-warnings-as-errors \
-  --with-debug-level=release \
+  $WITH_DEBUG_LEVEL \
   --with-vendor-name="${VENDOR_NAME}" \
   --with-vendor-version-string="${VENDOR_VERSION_STRING}" \
   --with-version-pre= \
   --with-version-build=${JDK_BUILD_NUMBER} \
   --with-version-opt=b${build_number} \
-  --with-import-modules=./modular-sdk \
+  $WITH_IMPORT_MODULES \
   --with-boot-jdk=`/usr/libexec/java_home -v 14` \
-  --enable-cds=yes || exit $?
+  --enable-cds=yes || do_exit $?
 
-make images CONF=macosx-x86_64-server-release || exit $?
+make clean CONF=$RELEASE_NAME || do_exit $?
+make images CONF=$RELEASE_NAME || do_exit $?
 
-JSDK=build/macosx-x86_64-server-release/images/jdk-bundle
-JBSDK=${JBRSDK_BASE_NAME}-osx-x64-b${build_number}
+JSDK=build/$RELEASE_NAME/images/jdk-bundle
 
 BASE_DIR=jre
 JBRSDK_BUNDLE=jbrsdk
 
 rm -rf $BASE_DIR
-mkdir $BASE_DIR || exit $?
-cp -a $JSDK/jdk-$MAJOR_JBSDK_VERSION.jdk $BASE_DIR/$JBRSDK_BUNDLE || exit $?
+mkdir $BASE_DIR || do_exit $?
+cp -a $JSDK/jdk-$MAJOR_JBSDK_VERSION.jdk $BASE_DIR/$JBRSDK_BUNDLE || do_exit $?
 
-if [[ ! -z "$bundle_type" ]]; then
-  cp -a jcef_mac/Frameworks $BASE_DIR/$JBRSDK_BUNDLE/Contents/
-fi
-if [ "$bundle_type" == "jcef" ]; then
+if [ "$bundle_type" == "jcef" ] || [ "$bundle_type" == "fd" ]; then
+  cp -a ${JCEF_PATH}/Frameworks $BASE_DIR/$JBRSDK_BUNDLE/Contents/
   echo Creating $JBSDK.tar.gz ...
   sed 's/JBR/JBRSDK/g' ${BASE_DIR}/${JBRSDK_BUNDLE}/Contents/Home/release > release
   mv release ${BASE_DIR}/${JBRSDK_BUNDLE}/Contents/Home/release
+  [ -f "$JBSDK.tar.gz" ] && rm "$JBSDK.tar.gz"
   COPYFILE_DISABLE=1 tar -pczf $JBSDK.tar.gz -C $BASE_DIR \
     --exclude='._*' --exclude='.DS_Store' --exclude='*~' \
     --exclude='Home/demo' --exclude='Home/man' --exclude='Home/sample' \
-    $JBRSDK_BUNDLE || exit $?
+    $JBRSDK_BUNDLE || do_exit $?
 fi
 
-create_jbr || exit $?
+create_jbr || do_exit $?
 
 if [ "$bundle_type" == "jcef" ]; then
-  make test-image || exit $?
+  make test-image CONF=$RELEASE_NAME || do_exit $?
 
   JBRSDK_TEST=$JBRSDK_BASE_NAME-osx-test-x64-b$build_number
 
   echo Creating $JBRSDK_TEST.tar.gz ...
-  COPYFILE_DISABLE=1 tar -pczf $JBRSDK_TEST.tar.gz -C build/macosx-x86_64-server-release/images \
-    --exclude='test/jdk/demos' test || exit $?
+  [ -f "$JBRSDK_TEST.tar.gz" ] && rm "$JBRSDK_TEST.tar.gz"
+  COPYFILE_DISABLE=1 tar -pczf $JBRSDK_TEST.tar.gz -C build/${RELEASE_NAME}/images \
+    --exclude='test/jdk/demos' test || do_exit $?
 fi
+
+do_exit 0
