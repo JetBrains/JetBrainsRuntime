a5725d317872ccdcbb6c223f45e69167acf47327
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2020-10-02 06:29:44 +0700

    JBR-2758 refactor building scripts to apply patches adding required modules instead of excluding
    Add jogl and gluegen modules to support jcef osr mode

--- jb/project/tools/windows/scripts/mkimages_x64.sh
+++ jb/project/tools/windows/scripts/mkimages_x64.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -x
 
 # The following parameters must be specified:
-#   JBSDK_VERSION    - specifies the current version of OpenJDK e.g. 11_0_6
-#   JDK_BUILD_NUMBER - specifies the number of OpenJDK build or the value of --with-version-build argument to configure
+#   JBSDK_VERSION    - specifies major version of OpenJDK e.g. 11_0_6 (instead of dots '.' underbars "_" are used)
+#   JDK_BUILD_NUMBER - specifies update release of OpenJDK build or the value of --with-version-build argument to configure
 #   build_number     - specifies the number of JetBrainsRuntime build
-#   bundle_type      - specifies bundle to bu built; possible values:
-#                        jcef - the bundles with jcef
-#                        empty - the bundles without jcef
+#   bundle_type      - specifies bundle to be built; possible values:
+#                        <empty> or nomod - the release bundles without any additional modules (jcef)
+#                        jcef - the release bundles with jcef
+#                        fd - the fastdebug bundles which also include the jcef module
 #
 # jbrsdk-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
 # jbr-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
@@ -16,12 +17,20 @@
 # OpenJDK Runtime Environment (build 11.0.6+${JDK_BUILD_NUMBER}-b${build_number})
 # OpenJDK 64-Bit Server VM (build 11.0.6+${JDK_BUILD_NUMBER}-b${build_number}, mixed mode)
 #
+# Environment variables:
+#   MODULAR_SDK_PATH - specifies the path to the directory where imported modules are located.
+#               By default imported modules should be located in ./modular-sdk
+#   JCEF_PATH - specifies the path to the directory with JCEF binaries.
+#               By default JCEF binaries should be located in ./jcef_win_x64
 
 JBSDK_VERSION=$1
 JDK_BUILD_NUMBER=$2
 build_number=$3
 bundle_type=$4
 JBSDK_VERSION_WITH_DOTS=$(echo $JBSDK_VERSION | sed 's/_/\./g')
+WORK_DIR=$(pwd)
+WITH_IMPORT_MODULES="--with-import-modules=${MODULAR_SDK_PATH:=$WORK_DIR/modular-sdk}"
+JCEF_PATH=${JCEF_PATH:=$WORK_DIR/jcef_win_x64}
 
 source jb/project/tools/common.sh
 
@@ -38,51 +47,67 @@ function create_jbr {
   echo Running jlink....
   ${JSDK}/bin/jlink \
     --module-path ${JSDK}/jmods --no-man-pages --compress=2 \
-    --add-modules $(xargs < modules_tmp.list | sed s/" "//g) --output ${JBR_BUNDLE} || exit $?
+    --add-modules $(xargs < modules_tmp.list | sed s/" "//g) --output ${JBR_BUNDLE} || do_exit $?
 
-  [ ! -z "${bundle_type}" ] && (cp -R jcef_win_x64/* ${JBR_BUNDLE}/bin || exit $?)
+  [ ! -z "${bundle_type}" ] && (cp -R jcef_win_x64/* ${JBR_BUNDLE}/bin || do_exit $?)
   echo Modifying release info ...
   cat ${JSDK}/release | tr -d '\r' | grep -v 'JAVA_VERSION' | grep -v 'MODULES' >> ${JBR_BUNDLE}/release
 }
 
 JBRSDK_BASE_NAME=jbrsdk-${JBSDK_VERSION}
-WORK_DIR=$(pwd)
+WITH_DEBUG_LEVEL="--with-debug-level=release"
+RELEASE_NAME=windows-x86_64-server-release
+JBSDK=${JBRSDK_BASE_NAME}-osx-x64-b${build_number}
+case "$bundle_type" in
+  "jcef")
+    git apply -p0 < jb/project/tools/patches/add_jcef_module.patch || do_exit $?
+    do_reset_changes=1
+    ;;
+  "nomod" | "")
+    bundle_type=""
+    WITH_IMPORT_MODULES=""
+    ;;
+  "fd")
+    git apply -p0 < jb/project/tools/patches/add_jcef_module.patch || do_exit $?
+    do_reset_changes=1
+    WITH_DEBUG_LEVEL="--with-debug-level=fastdebug"
+    RELEASE_NAME=windows-x86_64-server-fastdebug
+    JBSDK=${JBRSDK_BASE_NAME}-osx-x64-fastdebug-b${build_number}
+    ;;
+esac
 
-#git checkout -- modules.list
-#git checkout -- src/java.desktop/share/classes/module-info.java
-[ -z "$bundle_type" ] && (git apply -p0 < jb/project/tools/patches/exclude_jcef_module.patch || exit $?)
-
-PATH="/usr/local/bin:/usr/bin:${PATH}"
 sh ./configure \
   --disable-warnings-as-errors \
-  --with-target-bits=64 \
+  $WITH_DEBUG_LEVEL \
   --with-vendor-name="${VENDOR_NAME}" \
   --with-vendor-version-string="${VENDOR_VERSION_STRING}" \
   --with-version-pre= \
   --with-version-build=${JDK_BUILD_NUMBER} \
   --with-version-opt=b${build_number} \
+  $WITH_IMPORT_MODULES \
   --with-toolchain-version=${TOOLCHAIN_VERSION} \
-  --with-import-modules=${WORK_DIR}/modular-sdk \
   --with-boot-jdk=${BOOT_JDK} \
   --disable-ccache \
-  --enable-cds=yes || exit 1
+  --enable-cds=yes || do_exit $?
 
 if [ "$bundle_type" == "jcef" ]; then
-  make LOG=info images CONF=windows-x86_64-server-release test-image || exit 1else
+  make LOG=info CONF=$RELEASE_NAME clean images test-image   || do_exit $?
+else
+  make LOG=info CONF=$RELEASE_NAME clean images || do_exit $?
 fi
 
+JSDK=build/$RELEASE_NAME/images/jdk
 
-
-JSDK=build/windows-x86_64-server-release/images/jdk
-if [[ "$bundle_type" == "jcef" ]]; then
-  JBSDK=${JBRSDK_BASE_NAME}-windows-x64-b${build_number}
-fi
-BASE_DIR=build/windows-x86_64-server-release/images
+BASE_DIR=build/$RELEASE_NAME/images
 JBRSDK_BUNDLE=jbrsdk
 
-rm -rf ${BASE_DIR}/${JBRSDK_BUNDLE} && rsync -a --exclude demo --exclude sample ${JSDK}/ ${JBRSDK_BUNDLE} || exit 1
-cp -R jcef_win_x64/* ${JBRSDK_BUNDLE}/bin
-sed 's/JBR/JBRSDK/g' ${JSDK}/release > release
-mv release ${JBRSDK_BUNDLE}/release
+rm -rf ${BASE_DIR}/${JBRSDK_BUNDLE} && rsync -a --exclude demo --exclude sample ${JSDK}/ ${JBRSDK_BUNDLE} || do_exit $?
+if [ "$bundle_type" == "jcef" ] || [ "$bundle_type" == "fd" ]; then
+  cp -R jcef_win_x64/* ${JBRSDK_BUNDLE}/bin
+  sed 's/JBR/JBRSDK/g' ${JSDK}/release > release
+  mv release ${JBRSDK_BUNDLE}/release
+fi
 
 create_jbr || exit $?
+
+do_exit 0
