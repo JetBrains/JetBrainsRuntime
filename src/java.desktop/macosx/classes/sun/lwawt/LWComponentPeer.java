3b5f73aa7d3909f5ec56a54e979a17e9c09a80a4
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2020-10-02 12:09:26 +0300

    JBR-2712 Typeahead mechanism doesn't work on Windows
    (cherry picked from commits 1a9838082e3eb48d43e6bac6a412463923173fc7, f5b62228352bacb1628d232f0af5d8d387b6bf97, acd7e3b2da62332992f986903271a9eb7b1a2597, cd6dd5c3cf8556f97f3113cb7d615a92393b57bf(partially), e8bbd8ffdd90f57cd12d7d7e89188be97ee4be0b(partially), 37901295e1f6084b423c73df2b700552e5a4f519, cafb374afc6f8865ace6e107ab62a4a733fb5b97, 12034dcf619a84ae0bbb45861e93eec0dbfc7e54)
    
    includes fix for JBR-4974 jb/java/awt/Focus/ModalDialogFromMenuTest.java intermittently clicks at tittle bar
    
    Pause before getting the coordinates of the component to be clicked on,
    not right before the click itself.
    
    with fix for JBR-5300 Change source code and test files to use GPL license
    
    (cherry picked from commit 7855e07658b93602492e9f9906d36526306a0a99)

--- src/java.desktop/macosx/classes/sun/lwawt/LWComponentPeer.java
+++ src/java.desktop/macosx/classes/sun/lwawt/LWComponentPeer.java
@@ -917,7 +917,7 @@ public abstract class LWComponentPeer<T extends Component, D extends JComponent>
 
         int result = LWKeyboardFocusManagerPeer.shouldNativelyFocusHeavyweight(
                 getTarget(), lightweightChild, temporary,
-                focusedWindowChangeAllowed, time, cause);
+                focusedWindowChangeAllowed, time, cause, false);
         switch (result) {
             case LWKeyboardFocusManagerPeer.SNFH_FAILURE:
                 return false;
@@ -971,9 +971,7 @@ public abstract class LWComponentPeer<T extends Component, D extends JComponent>
                 KeyboardFocusManagerPeer kfmPeer = LWKeyboardFocusManagerPeer.getInstance();
                 Component focusOwner = kfmPeer.getCurrentFocusOwner();
                 return LWKeyboardFocusManagerPeer.deliverFocus(lightweightChild,
-                        getTarget(), temporary,
-                        focusedWindowChangeAllowed,
-                        time, cause, focusOwner);
+                        getTarget(), false, cause, focusOwner);
 
             case LWKeyboardFocusManagerPeer.SNFH_SUCCESS_HANDLED:
                 return true;
