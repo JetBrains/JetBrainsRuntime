98b27c4e1ef24496dbaba677bced9fbb79444cc9
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2020-10-05 11:53:16 +0700

    JBR-2758 refactor building scripts to apply patches adding required modules instead of excluding them

--- jb/project/tools/windows/scripts/pack_x64.sh
+++ jb/project/tools/windows/scripts/pack_x64.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -x
 
 # The following parameters must be specified:
-#   JBSDK_VERSION    - specifies the current version of OpenJDK e.g. 11_0_6
-#   JDK_BUILD_NUMBER - specifies the number of OpenJDK build or the value of --with-version-build argument to configure
+#   JBSDK_VERSION    - specifies major version of OpenJDK e.g. 11_0_6 (instead of dots '.' underbars "_" are used)
+#   JDK_BUILD_NUMBER - specifies update release of OpenJDK build or the value of --with-version-build argument to configure
 #   build_number     - specifies the number of JetBrainsRuntime build
-#   bundle_type      - specifies bundle to bu built; possible values:
-#                        jcef - the bundles 1) jbr with jcef+javafx, 2) jbrsdk and 3) test will be created
-#                        jfx  - the bundle 1) jbr with javafx only will be created
+#   bundle_type      - specifies bundle to be built; possible values:
+#                        jcef - the release bundles with jcef
+#                        jfx - the release bundles with javafx
+#                        jcef_jfx - the release bundles with jcef and javafx
+#                        dcevm - the release bundles with dcevm patches
+#                        nomod - the release bundles without any additional modules (jcef)
+#                        fd - the fastdebug bundles which also include the jcef module
 #
 # jbrsdk-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
 # jbr-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
@@ -16,59 +20,69 @@
 # OpenJDK Runtime Environment (build 11.0.6+${JDK_BUILD_NUMBER}-b${build_number})
 # OpenJDK 64-Bit Server VM (build 11.0.6+${JDK_BUILD_NUMBER}-b${build_number}, mixed mode)
 #
+# Environment variables:
+#   MODULAR_SDK_PATH - specifies the path to the directory where imported modules are located.
+#               By default imported modules should be located in ./modular-sdk
+#   JCEF_PATH - specifies the path to the directory with JCEF binaries.
+#               By default JCEF binaries should be located in ./jcef_win_x64
 
 JBSDK_VERSION=$1
 JDK_BUILD_NUMBER=$2
 build_number=$3
 bundle_type=$4
 
+source jb/project/tools/common.sh
+
 function pack_jbr {
 
-  case "$1" in
-  "${bundle_type}_lw")
-    JBR_BASE_NAME=jbr_${bundle_type}_lw-${JBSDK_VERSION}
-    ;;
-  "jfx" | "jcef" | "dcevm" | "nomod")
+  JBR_BUNDLE=jbr_${bundle_type}
+
+  case "${bundle_type}" in
+  "jfx" | "jcef" | "dcevm" | "nomod" | "fd")
     JBR_BASE_NAME=jbr_${bundle_type}-${JBSDK_VERSION}
     ;;
   "jfx_jcef")
     JBR_BASE_NAME=jbr-${JBSDK_VERSION}
     ;;
   *)
-    echo "***ERR*** bundle was not specified" && exit 1
+    echo "***ERR*** bundle was not specified" && do_exit 1
     ;;
   esac
 
   JBR=$JBR_BASE_NAME-windows-x64-b$build_number
   echo Creating $JBR.tar.gz ...
-  rm -rf ${BASE_DIR}/jbr
-  cp -R ${BASE_DIR}/${JBR_BUNDLE} ${BASE_DIR}/jbr
 
-  /usr/bin/tar -czf $JBR.tar.gz -C $BASE_DIR jbr || exit 1
-  #rm -rf ${BASE_DIR}/${JBR_BUNDLE}
+  /usr/bin/tar -czf $JBR.tar.gz -C $BASE_DIR jbr || do_exit $?
 }
 
+JBRSDK_BASE_NAME=jbrsdk-${JBSDK_VERSION}
+WITH_DEBUG_LEVEL="--with-debug-level=release"
 RELEASE_NAME=windows-x86_64-normal-server-release
-
-JBRSDK_BASE_NAME=jbrsdk-$JBSDK_VERSION
-JBR_BASE_NAME=jbr-$JBSDK_VERSION
+JBSDK=${JBRSDK_BASE_NAME}-windows-x64-b${build_number}
+case "$bundle_type" in
+  "fd")
+    WITH_DEBUG_LEVEL="--with-debug-level=fastdebug"
+    RELEASE_NAME=windows-x86_64-normal-server-fastdebug
+    JBRSDK_BASE_NAME=jbrsdk-${JBSDK_VERSION}-fastdebug
+    JBSDK=${JBRSDK_BASE_NAME}-windows-x64-fastdebug-b${build_number}
+    ;;
+esac
 
 IMAGES_DIR=build/$RELEASE_NAME/images
 JSDK=$IMAGES_DIR/jdk
-JBSDK=$JBRSDK_BASE_NAME-windows-x64-b$build_number
 BASE_DIR=.
 
-if [ "$bundle_type" == "jfx_jcef" ]; then
+if [ "${bundle_type}" == "jcef" ] || [ "{$bundle_type}" == "fd" ]; then
   JBRSDK_BUNDLE=jbrsdk
   echo Creating $JBSDK.tar.gz ...
-  /usr/bin/tar -czf $JBSDK.tar.gz $JBRSDK_BUNDLE || exit 1
+  [ -f "$JBSDK.tar.gz" ] && rm "$JBSDK.tar.gz"
+  /usr/bin/tar -czf $JBSDK.tar.gz $JBRSDK_BUNDLE || do_exit $?
 fi
 
-JBR_BUNDLE=jbr_${bundle_type}
 pack_jbr $bundle_type
 
-if [ "$bundle_type" == "jfx_jcef" ]; then
+if [ "$bundle_type" == "jcef" ]; then
   JBRSDK_TEST=$JBRSDK_BASE_NAME-windows-test-x64-b$build_number
   echo Creating $JBRSDK_TEST.tar.gz ...
-  /usr/bin/tar -czf $JBRSDK_TEST.tar.gz -C $IMAGES_DIR --exclude='test/jdk/demos' test || exit 1
+  /usr/bin/tar -czf $JBRSDK_TEST.tar.gz -C $IMAGES_DIR --exclude='test/jdk/demos' test || do_exit $?
 fi
