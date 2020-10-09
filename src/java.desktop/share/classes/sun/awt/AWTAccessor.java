e7e95729aa61edb0e23cc22e6df875801c3165ba
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2020-10-09 13:57:15 +0300

    JBR-2759 Typeahead issue on Linux
    (cherry picked from commits 76bdaf1131c2d424df947d8bf9d6dc8bcdb334af, b20c56ff3e86ddb638958a0f741427958b9452f5)

--- src/java.desktop/share/classes/sun/awt/AWTAccessor.java
+++ src/java.desktop/share/classes/sun/awt/AWTAccessor.java
@@ -464,6 +464,10 @@ public final class AWTAccessor {
          * Return the current focus cycle root
          */
         Container getCurrentFocusCycleRoot();
+
+        void enqueueKeyEvents(Component untilFocused);
+
+        void dequeueKeyEvents(Component untilFocused);
     }
 
     /**
