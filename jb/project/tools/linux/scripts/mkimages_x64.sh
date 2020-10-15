738c94f743e7727738d3eddb7ab0bf68be31ff6c
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2020-10-15 08:56:23 +0700

    JBR-2787 fix copying jcef files into jbr/jbrsdk binaries
    (cherry picked from commit fcc56cc8a2a8cbef18e9999f428b10a39b383cbb)

--- jb/project/tools/linux/scripts/mkimages_x64.sh
+++ jb/project/tools/linux/scripts/mkimages_x64.sh
@@ -63,7 +63,7 @@ function create_jbr {
 
   if [[ "${bundle_type}" == *jcef* ]] || [[ "${bundle_type}" == *dcevm* ]] || [[ "${bundle_type}" == fd ]]; then
     cp -R $BASE_DIR/$JBR_BUNDLE $BASE_DIR/jbr
-    cp -R ${JCEF_PATH}/* $BASE_DIR/$JBR_BUNDLE/lib || exit $?
+    rsync -av ${JCEF_PATH}/ $BASE_DIR/$JBR_BUNDLE/lib --exclude="modular-sdk" || do_exit $?
   fi
   grep -v "^JAVA_VERSION" $JSDK/release | grep -v "^MODULES" >> $BASE_DIR/$JBR_BUNDLE/release
 
@@ -140,7 +140,7 @@ rm -rf ${BASE_DIR}/${JBRSDK_BUNDLE}
 cp -r $JSDK $BASE_DIR/$JBRSDK_BUNDLE || do_exit $?
 
 if [[ "${bundle_type}" == *jcef* ]] || [[ "${bundle_type}" == *dcevm* ]] || [[ "${bundle_type}" == fd ]]; then
-  cp -R ${JCEF_PATH}/* $BASE_DIR/$JBRSDK_BUNDLE/lib || do_exit $?
+  rsync -av ${JCEF_PATH}/ $BASE_DIR/$JBRSDK_BUNDLE/lib --exclude="modular-sdk" || do_exit $?
 fi
 if [ "${bundle_type}" == "jcef" ] || [ "${bundle_type}" == "fd" ]; then
   echo Creating $JBSDK.tar.gz ...
