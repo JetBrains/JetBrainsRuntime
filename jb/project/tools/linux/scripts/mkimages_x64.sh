92a5acd1facb9139cf30258289def90036e9d367
Anton Tarasov <anton.tarasov@jetbrains.com>
2020-10-17 00:59:36 +0300

    JBR-2812 bundle jcef in jmod format instead of modular-sdk
    Build test-image with non-jcef build target

--- jb/project/tools/linux/scripts/mkimages_x64.sh
+++ jb/project/tools/linux/scripts/mkimages_x64.sh
@@ -18,8 +18,6 @@
 # OpenJDK 64-Bit Server VM (build 11.0.6+${JDK_BUILD_NUMBER}-b${build_number}, mixed mode)
 #
 # Environment variables:
-#   MODULAR_SDK_PATH - specifies the path to the directory where imported modules are located.
-#               By default imported modules should be located in ./modular-sdk
 #   JCEF_PATH - specifies the path to the directory with JCEF binaries.
 #               By default JCEF binaries should be located in ./jcef_linux_x64
 
@@ -28,114 +26,100 @@ JDK_BUILD_NUMBER=$2
 build_number=$3
 bundle_type=$4
 JBSDK_VERSION_WITH_DOTS=$(echo $JBSDK_VERSION | sed 's/_/\./g')
-WITH_IMPORT_MODULES="--with-import-modules=${MODULAR_SDK_PATH:=./modular-sdk}"
 JCEF_PATH=${JCEF_PATH:=./jcef_linux_x64}
 
-source jb/project/tools/common.sh
+source jb/project/tools/common/scripts/common.sh
 
-function create_jbr {
+function create_image_bundle {
+  __bundle_name=$1
+  __arch_name=$2
+  __modules_path=$3
+  __modules=$4
 
-  if [ -z "${bundle_type}" ]; then
-    JBR_BUNDLE=jbr
-  else
-    JBR_BUNDLE=jbr_${bundle_type}
-  fi
-  JBR_BASE_NAME=${JBR_BUNDLE}-${JBSDK_VERSION}
-  cat modules.list > modules_tmp.list
-  rm -rf ${BASE_DIR}/jbr
-  rm -rf ${BASE_DIR}/${JBR_BUNDLE}
-
-  JBR=$JBR_BASE_NAME-linux-x64-b$build_number
+  [ "$bundle_type" == "fd" ] && fastdebug_infix="fastdebug-"
+  JBR=${__bundle_name}-${JBSDK_VERSION}-linux-x64-${fastdebug_infix}b${build_number}
 
   echo Running jlink....
+  [ -d "$IMAGES_DIR"/"$__arch_name" ] && rm -rf "${IMAGES_DIR:?}"/"$__arch_name"
   $JSDK/bin/jlink \
-    --module-path $JSDK/jmods --no-man-pages --compress=2 \
-    --add-modules $(xargs < modules_tmp.list | sed s/" "//g) --output $BASE_DIR/$JBR_BUNDLE
+    --module-path "$__modules_path" --no-man-pages --compress=2 \
+    --add-modules "$__modules" --output "$IMAGES_DIR"/"$__arch_name"
 
-  if [ ! -z "${bundle_type}" ]; then
-    cp -R ${BASE_DIR}/${JBR_BUNDLE} ${BASE_DIR}/jbr
-    rsync -av ${JCEF_PATH}/ $BASE_DIR/$JBR_BUNDLE/lib --exclude="modular-sdk" || do_exit $?
-  fi
-  grep -v "^JAVA_VERSION" $JSDK/release | grep -v "^MODULES" >> $BASE_DIR/$JBR_BUNDLE/release
+  # jmod does not preserve file permissions (JDK-8173610)
+  [ -f "$IMAGES_DIR"/"$__arch_name"/lib/jcef_helper ] && chmod a+x "$IMAGES_DIR"/"$__arch_name"/lib/jcef_helper
 
-  echo Creating $JBR.tar.gz ...
-  tar -pcf $JBR.tar -C $BASE_DIR jbr || do_exit $?
-  [ -f "$JBR.tar.gz" ] && rm "$JBR.tar.gz"
-  gzip $JBR.tar || do_exit $?
-  rm -rf ${BASE_DIR}/${JBR_BUNDLE}
+  echo Creating "$JBR".tar.gz ...
+  tar -pcf "$JBR".tar -C "$IMAGES_DIR" "$__arch_name" || do_exit $?
+  [ -f "$JBR".tar.gz ] && rm "$JBR.tar.gz"
+  gzip "$JBR".tar || do_exit $?
+  rm -rf "${IMAGES_DIR:?}"/"$__arch_name"
 }
 
-JBRSDK_BASE_NAME=jbrsdk-${JBSDK_VERSION}
 WITH_DEBUG_LEVEL="--with-debug-level=release"
 RELEASE_NAME=linux-x86_64-server-release
-JBSDK=${JBRSDK_BASE_NAME}-linux-x64-b${build_number}
+
 case "$bundle_type" in
   "jcef")
-    git apply -p0 < jb/project/tools/patches/add_jcef_module.patch
     do_reset_changes=1
     ;;
   "nomod" | "")
     bundle_type=""
-    WITH_IMPORT_MODULES=""
     ;;
   "fd")
-    git apply -p0 < jb/project/tools/patches/add_jcef_module.patch
     do_reset_changes=1
     WITH_DEBUG_LEVEL="--with-debug-level=fastdebug"
     RELEASE_NAME=linux-x86_64-server-fastdebug
-    JBSDK=${JBRSDK_BASE_NAME}-linux-x64-fastdebug-b${build_number}
     ;;
 esac
 
 sh configure \
   --disable-warnings-as-errors \
   $WITH_DEBUG_LEVEL \
-  --with-vendor-name="${VENDOR_NAME}" \
-  --with-vendor-version-string="${VENDOR_VERSION_STRING}" \
+  --with-vendor-name="$VENDOR_NAME" \
+  --with-vendor-version-string="$VENDOR_VERSION_STRING" \
   --with-version-pre= \
-  --with-version-build=${JDK_BUILD_NUMBER} \
-  --with-version-opt=b${build_number} \
-  $WITH_IMPORT_MODULES \
-  --with-boot-jdk=${BOOT_JDK} \
+  --with-version-build="$JDK_BUILD_NUMBER" \
+  --with-version-opt=b"$build_number" \
+  --with-boot-jdk="$BOOT_JDK" \
   --enable-cds=yes || do_exit $?
 
 make clean CONF=$RELEASE_NAME || exit $?
 make images CONF=$RELEASE_NAME || do_exit $?
 
-JSDK=build/$RELEASE_NAME/images/jdk
+IMAGES_DIR=build/$RELEASE_NAME/images
+JSDK=$IMAGES_DIR/jdk
+JSDK_MODS_DIR=$IMAGES_DIR/jmods
+JBRSDK_BUNDLE=jbrsdk
 
 echo Fixing permissions
 chmod -R a+r $JSDK
 
-BASE_DIR=build/$RELEASE_NAME/images
-JBRSDK_BUNDLE=jbrsdk
-
-rm -rf $BASE_DIR/$JBRSDK_BUNDLE
-cp -r $JSDK $BASE_DIR/$JBRSDK_BUNDLE || do_exit $?
-
 if [ "$bundle_type" == "jcef" ] || [ "$bundle_type" == "fd" ]; then
-  rsync -av ${JCEF_PATH}/ $BASE_DIR/$JBRSDK_BUNDLE/lib --exclude="modular-sdk" || do_exit $?
-  echo Creating $JBSDK.tar.gz ...
-  sed 's/JBR/JBRSDK/g' ${BASE_DIR}/${JBRSDK_BUNDLE}/release > release
-  mv release ${BASE_DIR}/${JBRSDK_BUNDLE}/release
+  git apply -p0 < jb/project/tools/patches/add_jcef_module.patch || do_exit $?
+  update_jsdk_mods $JSDK $JCEF_PATH/jmods $JSDK/jmods $JSDK_MODS_DIR || do_exit $?
+  cp $JCEF_PATH/jmods/* $JSDK_MODS_DIR # $JSDK/jmods is not changed
 
-  tar -pcf $JBSDK.tar --exclude=*.debuginfo --exclude=demo --exclude=sample --exclude=man \
-    -C $BASE_DIR $JBRSDK_BUNDLE || do_exit $?
-  [ -f "$JBSDK.tar.gz" ] && rm "$JBSDK.tar.gz"
-  gzip $JBSDK.tar || do_exit $?
+  jbr_name_postfix="_${bundle_type}"
 fi
 
-create_jbr || do_exit $?
+# create runtime image bundle
+modules=$(xargs < modules.list | sed s/" "//g) || do_exit $?
+create_image_bundle "jbr${jbr_name_postfix}" "jbr" $JSDK_MODS_DIR "$modules" || do_exit $?
 
-if [ "$bundle_type" == "jcef" ]; then
-  make test-image CONF=$RELEASE_NAME || do_exit $?
+# create sdk image bundle
+modules=$(cat $JSDK/release | grep MODULES | sed s/MODULES=//g | sed s/' '/,/g | sed s/\"//g | sed s/\\n//g) || do_exit $?
+if [ "$bundle_type" == "jcef" ] || [ "$bundle_type" == "fd" ]; then
+  modules=${modules},$(get_mods_list "$JCEF_PATH"/jmods)
+fi
+create_image_bundle $JBRSDK_BUNDLE $JBRSDK_BUNDLE $JSDK_MODS_DIR "$modules" || do_exit $?
 
-  JBRSDK_TEST=$JBRSDK_BASE_NAME-linux-test-x64-b$build_number
-
-  echo Creating $JBSDK_TEST.tar.gz ...
-  tar -pcf $JBRSDK_TEST.tar -C $BASE_DIR --exclude='test/jdk/demos' test || do_exit $?
-  [ -f "$JBRSDK_TEST.tar.gz" ] && rm "$JBRSDK_TEST.tar.gz"
-  gzip $JBRSDK_TEST.tar || do_exit $?
+if [ -z "$bundle_type" ]; then
+    JBRSDK_TEST=${JBRSDK_BUNDLE}-${JBSDK_VERSION}-linux-test-x64-b${build_number}
+    echo Creating "$JBRSDK_TEST" ...
+    make test-image CONF=$RELEASE_NAME || do_exit $?
+    tar -pcf "$JBRSDK_TEST".tar -C $IMAGES_DIR --exclude='test/jdk/demos' test || do_exit $?
+    [ -f "$JBRSDK_TEST.tar.gz" ] && rm "$JBRSDK_TEST.tar.gz"
+    gzip "$JBRSDK_TEST".tar || do_exit $?
 fi
 
 do_exit 0
