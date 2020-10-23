6479cec02cab8fce9b2cdde73cc0cac12925793e
Denis Konoplev <denis.konoplev@jetbrains.com>
2020-10-23 11:42:02 +0300

    JBR-2490  Add option to work with Surface Pen
    (cherry picked from commit 5acc7680a10628ed423ebbd7b9e5976f7df83231)
    (cherry picked from commit b604023cef599f8ff5fea787e6e3c3b230056dd8)

--- src/java.desktop/windows/native/libawt/windows/awt_Component.cpp
+++ src/java.desktop/windows/native/libawt/windows/awt_Component.cpp
@@ -1333,6 +1333,19 @@ static BOOL IsMouseEventFromTouch()
 {
     return (::GetMessageExtraInfo() & MOUSEEVENTF_FROMTOUCH_MASK) == MOUSEEVENTF_FROMTOUCH;
 }
+
+// consider making general function
+// T getClassStaticField(cls, field, default_val)
+static BOOL IsDefaultTouch()
+{
+    JNIEnv *env = (JNIEnv *)JNU_GetEnv(jvm, JNI_VERSION_1_2);
+    jclass cls = env->FindClass("sun/awt/event/TouchEvent");
+    CHECK_NULL_RETURN(cls, FALSE);
+    jfieldID fieldID = env->GetStaticFieldID(cls, "defaultTouchHandling", "Z");
+    CHECK_NULL_RETURN(fieldID, FALSE);
+    return static_cast<BOOL>(env->GetStaticBooleanField(cls, fieldID));
+}
+
 /*
  * Dispatch messages for this window class--general component
  */
@@ -1357,6 +1370,8 @@ LRESULT AwtComponent::WindowProc(UINT message, WPARAM wParam, LPARAM lParam)
     // resource hogging ::SetProp
         return (LRESULT)TRUE;
     }
+
+    static const BOOL PROCESS_TOUCH_EVENTS = !IsDefaultTouch();
 
     DWORD curPos = 0;
 
@@ -1641,7 +1656,7 @@ LRESULT AwtComponent::WindowProc(UINT message, WPARAM wParam, LPARAM lParam)
       case WM_MOUSEHWHEEL:
       case WM_AWT_MOUSEENTER:
       case WM_AWT_MOUSEEXIT:
-          if (IsMouseEventFromTouch()) {
+          if (IsMouseEventFromTouch() && PROCESS_TOUCH_EVENTS) {
               break;
           }
           curPos = ::GetMessagePos();
@@ -1721,8 +1736,10 @@ LRESULT AwtComponent::WindowProc(UINT message, WPARAM wParam, LPARAM lParam)
           }
           break;
       case WM_TOUCH:
-          WmTouch(wParam, lParam);
-          break;
+          if (PROCESS_TOUCH_EVENTS) {
+              WmTouch(wParam, lParam);
+              break;
+          }
       case WM_SETCURSOR:
           mr = mrDoDefault;
           if (LOWORD(lParam) == HTCLIENT) {
