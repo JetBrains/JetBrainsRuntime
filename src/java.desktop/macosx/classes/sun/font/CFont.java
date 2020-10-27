a0e22ad1e12564ca6fbbe96f09c29f29f7bcf300
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2020-10-27 13:00:41 +0300

    JBR-2819 Create API to determine typographic family/subfamily for available fonts
    (cherry picked from commit 89e519a4ef2c924997f2e575e083316ea2df1d9d)
    (cherry picked from commit 21129ca270ff9f239b7441142c58dec7d66838fb)

--- src/java.desktop/macosx/classes/sun/font/CFont.java
+++ src/java.desktop/macosx/classes/sun/font/CFont.java
@@ -96,6 +96,7 @@ public final class CFont extends PhysicalFont implements FontSubstitution, FontW
                                                 final int style);
     private static native void disposeNativeFont(final long nativeFontPtr);
 
+    private String faceName;
     private boolean isFakeItalic;
     private String nativeFontName;
     private long nativeFontPtr;
@@ -162,14 +163,15 @@ public final class CFont extends PhysicalFont implements FontSubstitution, FontW
 
     // this constructor is called from CFontWrapper.m
     public CFont(String name) {
-        this(name, name);
+        this(name, name, null);
     }
 
-    public CFont(String name, String inFamilyName) {
+    public CFont(String name, String inFamilyName, String faceName) {
         handle = new Font2DHandle(this);
         fullName = name;
         familyName = inFamilyName;
         nativeFontName = fullName;
+        this.faceName = faceName;
         setStyle();
     }
 
@@ -260,6 +262,11 @@ public final class CFont extends PhysicalFont implements FontSubstitution, FontW
 
     String getNativeFontName() {
         return nativeFontName;
+    }
+
+    @Override
+    public String getTypographicSubfamilyName() {
+        return faceName == null ? super.getTypographicSubfamilyName() : faceName;
     }
 
     // <rdar://problem/5321707> sun.font.Font2D caches the last used strike,
