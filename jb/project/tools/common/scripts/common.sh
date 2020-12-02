d54f0f0173a3ceb56bf262935a293a0a27a1a7d4
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2020-12-02 18:14:55 +0700

    JBR-2912 add JBR 15 builds with DCEVM
    (cherry picked from commit 95416c501aab37c5c28d1983930a0929376810cc)

--- jb/project/tools/common/scripts/common.sh
+++ jb/project/tools/common/scripts/common.sh
@@ -3,9 +3,15 @@ VENDOR_VERSION_STRING="JBR-${JBSDK_VERSION_WITH_DOTS}.${JDK_BUILD_NUMBER}-${buil
 [ -z "$bundle_type" ] || VENDOR_VERSION_STRING="${VENDOR_VERSION_STRING}-${bundle_type}"
 
 do_reset_changes=0
+do_reset_dcevm=0
+HEAD_REVISION=0
+
 function do_exit() {
   exit_code=$1
   [ $do_reset_changes -eq 1 ] && git checkout HEAD modules.list src/java.desktop/share/classes/module-info.java
+  if [ $do_reset_dcevm -eq 1 ]; then
+    [ ! -z $HEAD_REVISION ] && git reset --hard $HEAD_REVISION
+  fi
   exit "$exit_code"
 }
 
