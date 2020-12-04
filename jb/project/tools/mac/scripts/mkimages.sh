4e28354e0718bb9c92dade1790b5e1c44d59abd5
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2020-12-04 23:43:25 +0300

    JBR-2526 support aarch64 in build script
    fix script

--- jb/project/tools/mac/scripts/mkimages.sh
+++ jb/project/tools/mac/scripts/mkimages.sh
@@ -30,6 +30,8 @@ JBSDK_VERSION=$1
 JDK_BUILD_NUMBER=$2
 build_number=$3
 bundle_type=$4
+architecture=$5 # aarch64 or x64
+enable_aot=$6 # temporary param for building test jre with aot under aarch64
 JBSDK_VERSION_WITH_DOTS=$(echo $JBSDK_VERSION | sed 's/_/\./g')
 WITH_IMPORT_MODULES="--with-import-modules=${MODULAR_SDK_PATH:=./modular-sdk}"
 JCEF_PATH=${JCEF_PATH:=./jcef_mac}
@@ -51,7 +53,15 @@ function create_jbr {
     echo "***ERR*** bundle was not specified" && do_exit 1
     ;;
   esac
-  cat modules.list > modules_tmp.list
+  if [[ "${architecture}" == *aarch64* ]] && [[ "${enable_aot}" != *enable_aot* ]]; then
+    # aot isn't supported yet, so remove dependent modules
+    echo "Exclude jdk.internal.vm.compiler and jdk.aot (because aot not supported yet)"
+    cat modules.list | \
+      grep -v "jdk.internal.vm.compiler\|jdk.aot" \
+      > modules_tmp.list
+  else
+    cat modules.list > modules_tmp.list
+  fi
   rm -rf ${BASE_DIR}/${JBR_BUNDLE}
 
   JRE_CONTENTS=${BASE_DIR}/${JBR_BUNDLE}/Contents
@@ -61,7 +71,7 @@ function create_jbr {
   fi
   mkdir -p ${JRE_CONTENTS}
 
-  JBR=${JBR_BASE_NAME}-osx-x64-b${build_number}
+  JBR=${JBR_BASE_NAME}-osx-${architecture}-b${build_number}
 
   echo Running jlink....
   ${BASE_DIR}/$JBRSDK_BUNDLE/Contents/Home/bin/jlink \
@@ -71,22 +81,39 @@ function create_jbr {
   cp -R ${BASE_DIR}/${JBRSDK_BUNDLE}/Contents/MacOS ${JRE_CONTENTS}
   cp ${BASE_DIR}/${JBRSDK_BUNDLE}/Contents/Info.plist ${JRE_CONTENTS}
 
+  rm -rf ${JRE_CONTENTS}/Frameworks || do_exit $?
+  if [[ "${architecture}" == *aarch64* ]]; then
+    # we can't notarize this library as usual framework (with headers and tbd-file)
+    # but single library notarizes correctly
+    mkdir -p ${JRE_CONTENTS}/Frameworks/JavaNativeFoundation.framework/Resources
+    cp -p Frameworks/JavaNativeFoundation.framework/JavaNativeFoundation \
+      ${JRE_CONTENTS}/Frameworks/JavaNativeFoundation.framework || do_exit $?
+    cp -p Frameworks/JavaNativeFoundation.framework/Resources/Info.plist \
+      ${JRE_CONTENTS}/Frameworks/JavaNativeFoundation.framework/Resources || do_exit $?
+    # unsign JavaNativeFoundation binary (otherwise notarization will fail)
+    codesign --remove-signature ${JRE_CONTENTS}/Frameworks/JavaNativeFoundation.framework/JavaNativeFoundation || do_exit $?
+  fi
   if [[ "${bundle_type}" == *jcef* ]] || [[ "${bundle_type}" == *dcevm* ]] || [[ "${bundle_type}" == fd ]]; then
-    rm -rf ${JRE_CONTENTS}/Frameworks || do_exit $?
     cp -a ${JCEF_PATH}/Frameworks ${JRE_CONTENTS} || do_exit $?
   fi
 
   echo Creating ${JBR}.tar.gz ...
   rm -rf ${BASE_DIR}/jbr
   cp -R ${BASE_DIR}/${JBR_BUNDLE} ${BASE_DIR}/jbr
+
   COPYFILE_DISABLE=1 tar -pczf ${JBR}.tar.gz --exclude='*.dSYM' --exclude='man' -C ${BASE_DIR} jbr || do_exit $?
   rm -rf ${BASE_DIR}/${JBR_BUNDLE}
 }
 
 JBRSDK_BASE_NAME=jbrsdk-${JBSDK_VERSION}
 WITH_DEBUG_LEVEL="--with-debug-level=release"
-RELEASE_NAME=macosx-x86_64-normal-server-release
-JBSDK=${JBRSDK_BASE_NAME}-osx-x64-b${build_number}
+CONF_ARCHITECTURE=x86_64
+if [[ "${architecture}" == *aarch64* ]]; then
+  CONF_ARCHITECTURE=aarch64
+fi
+CONF_NAME=macosx-${CONF_ARCHITECTURE}-normal-server-release
+
+JBSDK=${JBRSDK_BASE_NAME}-osx-${architecture}-b${build_number}
 case "$bundle_type" in
   "jfx")
     git apply -p0 < jb/project/tools/patches/add_jfx_module.patch || do_exit $?
@@ -115,15 +142,22 @@ case "$bundle_type" in
     git apply -p0 < jb/project/tools/patches/add_jcef_module.patch || do_exit $?
     do_reset_changes=1
     WITH_DEBUG_LEVEL="--with-debug-level=fastdebug"
-    RELEASE_NAME=macosx-x86_64-normal-server-fastdebug
-    JBSDK=${JBRSDK_BASE_NAME}-osx-x64-fastdebug-b${build_number}
+    CONF_NAME=macosx-${CONF_ARCHITECTURE}-normal-server-fastdebug
+    JBSDK=${JBRSDK_BASE_NAME}-osx-${architecture}-fastdebug-b${build_number}
     ;;
   *)
     echo "***ERR*** bundle was not specified" && do_exit 1
     ;;
 esac
 
-sh configure \
+if [[ "${architecture}" == *aarch64* ]]; then
+  # NOTE: aot, cds aren't supported yet
+  WITH_JVM_FEATURES="--with-jvm-features=-aot"
+  if [[ "${enable_aot}" == *enable_aot* ]]; then
+    echo "Enable unstable jvm feature: AOT"
+    WITH_JVM_FEATURES=""
+  fi
+  sh configure \
   --disable-warnings-as-errors \
   $WITH_DEBUG_LEVEL \
   --with-vendor-name="${VENDOR_NAME}" \
@@ -133,12 +167,30 @@ sh configure \
   --with-version-opt=b${build_number} \
   $WITH_IMPORT_MODULES \
   --with-boot-jdk=`/usr/libexec/java_home -v 11` \
-  --enable-cds=yes || do_exit $?
+  --disable-hotspot-gtest --disable-javac-server --disable-full-docs --disable-manpages \
+  --enable-cds=no \
+  $WITH_JVM_FEATURES \
+  --with-extra-cflags="-F$(pwd)/Frameworks" \
+  --with-extra-cxxflags="-F$(pwd)/Frameworks" \
+  --with-extra-ldflags="-F$(pwd)/Frameworks" || do_exit $?
+else
+  sh configure \
+    --disable-warnings-as-errors \
+    $WITH_DEBUG_LEVEL \
+    --with-vendor-name="${VENDOR_NAME}" \
+    --with-vendor-version-string="${VENDOR_VERSION_STRING}" \
+    --with-version-pre= \
+    --with-version-build=${JDK_BUILD_NUMBER} \
+    --with-version-opt=b${build_number} \
+    $WITH_IMPORT_MODULES \
+    --with-boot-jdk=`/usr/libexec/java_home -v 11` \
+    --enable-cds=yes || do_exit $?
+fi
 
-make clean CONF=$RELEASE_NAME || do_exit $?
-make images CONF=$RELEASE_NAME || do_exit $?
+make clean CONF=$CONF_NAME || do_exit $?
+make images CONF=$CONF_NAME || do_exit $?
 
-JSDK=build/${RELEASE_NAME}/images/jdk-bundle
+JSDK=build/${CONF_NAME}/images/jdk-bundle
 
 BASE_DIR=jre
 JBRSDK_BUNDLE=jbrsdk
@@ -146,8 +198,7 @@ JBRSDK_BUNDLE=jbrsdk
 rm -rf $BASE_DIR
 mkdir $BASE_DIR || do_exit $?
 cp -a $JSDK/jdk-$JBSDK_VERSION_WITH_DOTS.jdk $BASE_DIR/$JBRSDK_BUNDLE || do_exit $?
-
-  if [[ "${bundle_type}" == *jcef* ]] || [[ "${bundle_type}" == *dcevm* ]] || [[ "${bundle_type}" == fd ]]; then
+if [[ "${bundle_type}" == *jcef* ]] || [[ "${bundle_type}" == *dcevm* ]] || [[ "${bundle_type}" == fd ]]; then
   cp -a ${JCEF_PATH}/Frameworks $BASE_DIR/$JBRSDK_BUNDLE/Contents/
 fi
 if [ "${bundle_type}" == "jcef" ] || [ "${bundle_type}" == "fd" ]; then
@@ -164,13 +215,13 @@ fi
 create_jbr || do_exit $?
 
 if [ "$bundle_type" == "jcef" ]; then
-  make test-image CONF=$RELEASE_NAME || do_exit $?
+  make test-image CONF=$CONF_NAME || do_exit $?
 
-  JBRSDK_TEST=$JBRSDK_BASE_NAME-osx-test-x64-b$build_number
+  JBRSDK_TEST=$JBRSDK_BASE_NAME-osx-test-${architecture}-b$build_number
 
   echo Creating $JBRSDK_TEST.tar.gz ...
   [ -f "${JBRSDK_TEST}.tar.gz" ] && rm "${JBRSDK_TEST}.tar.gz"
-  COPYFILE_DISABLE=1 tar -pczf ${JBRSDK_TEST}.tar.gz -C build/${RELEASE_NAME}/images \
+  COPYFILE_DISABLE=1 tar -pczf ${JBRSDK_TEST}.tar.gz -C build/${CONF_NAME}/images \
     --exclude='test/jdk/demos' test || do_exit $?
 fi
 
