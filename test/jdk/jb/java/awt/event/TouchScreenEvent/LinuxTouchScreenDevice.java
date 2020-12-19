8b454a61826a6fff5a8241937e801bbe4fa3e220
Elena Sayapina <elena.sayapina@jetbrains.com>
2020-12-19 15:14:40 +0700

    JBR-2041 [TEST] Added new regression test (Touchscreen devices support)
    (cherry picked from commit 2d587b372803f98722ff517fd5751c1184c23dd6)
    (cherry picked from commit 92606f2c7f8ba5db9cb803e897059b7aeafaefef)
    (cherry picked from commit 05af37590949cfef077518ff6298ff9ddbc4a8b3)
    (cherry picked from commit 0f895bf1b2f87e9ddacb5e196f2d937f0f43d07b)
    (cherry picked from commit 08aa0852b79f98f298fb339c15bb212acb4db5d6)

---
+++ test/jdk/jb/java/awt/event/TouchScreenEvent/LinuxTouchScreenDevice.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2000-2020 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.awt.Toolkit;
+import java.io.IOException;
+
+public class LinuxTouchScreenDevice implements AutoCloseable {
+    // TODO add product id
+    private int width;
+    private int height;
+    private int fileDescriptor;
+
+    static {
+        System.loadLibrary("touchscreen_device");
+    }
+
+    public LinuxTouchScreenDevice() throws IOException {
+        this(Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
+    }
+
+    public LinuxTouchScreenDevice(int width, int height) throws IOException {
+        this.width = width;
+        this.height = height;
+        fileDescriptor = create(getWidth(), getHeight());
+        checkCompletion(fileDescriptor,
+                "Failed to create virtual touchscreen device");
+    }
+
+    @Override
+    public void close() throws Exception {
+        checkCompletion(destroy(fileDescriptor),
+                "Failed to close touchscreen device");
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void click(int trackingId, int x, int y) throws IOException {
+        checkCompletion(clickImpl(fileDescriptor, trackingId, x, y),
+                "Failed to click on touchscreen device");
+
+    }
+
+    public void move(int trackingId, int fromX, int fromY, int toX, int toY) throws IOException {
+        checkCompletion(moveImpl(fileDescriptor, trackingId, fromX, fromY, toX, toY),
+                "Failed to move on virtual touchscreen device");
+    }
+
+    private void checkCompletion(int code, String errorMessage) throws IOException {
+        if (code < 0) {
+            throw new IOException(errorMessage);
+        }
+    }
+
+    private native int create(int width, int height);
+
+    private native int destroy(int fd);
+
+    private native int clickImpl(int fd, int trackingId, int x, int y);
+
+    private native int moveImpl(int fd, int trackingId, int fromX, int fromY, int toX, int toY);
+}
+
+
