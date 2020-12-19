e7920ab23d54a712b1ace5ba3cb927e1a77451d0
Elena Sayapina <elena.sayapina@jetbrains.com>
2020-12-19 15:28:58 +0700

    JBR-2585 [TESTBUG] TouchScreenEvent tests affect tests simulating mouse actions
    - added workaround for JBR-2585
    - added README.md about manual test run
    - made an update to close LinuxTouchScreenDevice properly
    - added an error exit from linux shell script if sudo password is empty or chown fails
    
    (cherry picked from commit 4deb3bbe61087de1afb636e493ea337f8accee27)
    (cherry picked from commit a13b014719fb868806ab152a220aeb1f544cef42)

--- test/jdk/jb/java/awt/event/TouchScreenEvent/LinuxTouchRobot.java
+++ test/jdk/jb/java/awt/event/TouchScreenEvent/LinuxTouchRobot.java
@@ -45,4 +45,8 @@ public class LinuxTouchRobot extends TouchRobot {
     public void touchMove(int fingerId, int fromX, int fromY, int toX, int toY) throws IOException {
         device.move(fingerId, fromX, fromY, toX, toY);
     }
+
+    public void closeDevice() throws IOException {
+        device.close();
+    }
 }
