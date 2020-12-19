3bfdbfea9d61ab7cd274b9cfc13de2fdc31613cf
Elena Sayapina <elena.sayapina@jetbrains.com>
2020-12-19 15:14:40 +0700

    JBR-2041 [TEST] Added new regression test (Touchscreen devices support)
    (cherry picked from commit 2d587b372803f98722ff517fd5751c1184c23dd6)
    (cherry picked from commit 92606f2c7f8ba5db9cb803e897059b7aeafaefef)
    (cherry picked from commit 05af37590949cfef077518ff6298ff9ddbc4a8b3)
    (cherry picked from commit 0f895bf1b2f87e9ddacb5e196f2d937f0f43d07b)
    (cherry picked from commit 08aa0852b79f98f298fb339c15bb212acb4db5d6)
    
    with fix for JBR-5300 Change source code and test files to use GPL license
    
    (cherry picked from commit 9bd62fade99e97c6952798eae00f54fd8a5f2e7b)

---
+++ test/jdk/jb/java/awt/event/TouchScreenEvent/TouchRobot.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2000-2023 JetBrains s.r.o.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+import java.awt.AWTException;
+import java.awt.GraphicsDevice;
+import java.awt.Robot;
+import java.io.IOException;
+
+public abstract class TouchRobot extends Robot {
+    public TouchRobot() throws AWTException {
+    }
+
+    public TouchRobot(GraphicsDevice screen) throws AWTException {
+        super(screen);
+    }
+
+    public abstract void touchClick(int fingerId, int x, int y) throws IOException;
+
+    public abstract void touchMove(int fingerId, int fromX, int fromY, int toX, int toY) throws IOException;
+}
