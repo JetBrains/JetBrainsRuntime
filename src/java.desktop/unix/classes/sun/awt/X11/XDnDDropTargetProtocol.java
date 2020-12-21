18f3b76eaf9ee7f53c6e4d4a99bda88e256fea13
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2020-12-21 18:31:49 +0300

    JBR-2973 Copy/Move dialog not in the focus on drag-n-drop to Project Tool window from external application
    (cherry picked from commit 20fe78b650a2103fd767e53b1dffcd96a07d5dde)
    (cherry picked from commit 9f6b9baaa8bf0a6b69be31ffdbaf1e2602a0be72)

--- src/java.desktop/unix/classes/sun/awt/X11/XDnDDropTargetProtocol.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XDnDDropTargetProtocol.java
@@ -715,6 +715,7 @@ final class XDnDDropTargetProtocol extends XDropTargetProtocol {
         }
 
         if (targetXWindow != null) {
+            targetXWindow.setUserTime(xclient.get_data(2), true);
             notifyProtocolListener(targetXWindow, sourceX, sourceY, userAction,
                                    xclient, MouseEvent.MOUSE_RELEASED);
         }
