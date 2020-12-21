4c5656d4bb86e3bac0f5fae20e0250a17f473998
chagedorn <unknown>
2020-12-21 08:06:48 +0000

    8257513: C2: assert((constant_addr - _masm.code()->consts()->start()) == con.offset()) Reviewed-by: kvn, thartmann

--- src/hotspot/share/opto/output.cpp
+++ src/hotspot/share/opto/output.cpp
@@ -1119,7 +1119,10 @@ void Compile::fill_buffer(CodeBuffer* cb, uint* blk_starts) {
 
   // Emit the constant table.
   if (has_mach_constant_base_node()) {
-    constant_table().emit(*cb);
+    if (!constant_table().emit(*cb)) {
+      C->record_failure("consts section overflow");
+      return;
+    }
   }
 
   // Create an array of labels, one for each basic block
