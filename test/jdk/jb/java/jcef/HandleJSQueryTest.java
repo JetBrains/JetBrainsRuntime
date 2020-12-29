01f93262691d7dffaeecd52adb8e5a4e3345d643
Elena Sayapina <elena.sayapina@jetbrains.com>
2020-12-29 20:37:14 +0700

    JBR-2890 [TESTUPDATE] Enable jcef tests on macOS aarch64 platform
    (cherry picked from commit 1714d7b627535fa4b228336b13466bf4045779dd)

--- test/jdk/jb/java/jcef/HandleJSQueryTest.java
+++ test/jdk/jb/java/jcef/HandleJSQueryTest.java
@@ -17,7 +17,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * @test
  * @key headful
- * @requires (os.arch != "x86" & os.arch != "i386" & os.arch != "aarch64")
+ * @requires (os.arch == "amd64" | os.arch == "x86_64" | (os.arch == "aarch64" & os.family == "mac"))
  * @summary Regression test for JBR-2430. The test checks that JS Query is handled in 2nd opened browser.
  * @run main/othervm HandleJSQueryTest
  */
