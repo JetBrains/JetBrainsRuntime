dfc685ba2472d6c2371a9e8b1dae447734678c52
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2021-01-14 19:06:19 +0300

    fix occasional freezes of JBR-3017 reproducer after the fix
    (cherry picked from commit 7e6db54f77eb34d9eb1f2ddb62480376b9c4d2d0)
    (cherry picked from commit 2b7f78f50ed30c4966970a7d020d6f58a961929b)

--- src/java.desktop/share/classes/sun/awt/AWTThreading.java
+++ src/java.desktop/share/classes/sun/awt/AWTThreading.java
@@ -5,6 +5,7 @@ import sun.font.FontUtilities;
 import java.awt.*;
 import java.awt.event.InvocationEvent;
 import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
 import java.util.Map;
 import java.util.Stack;
 import java.util.concurrent.*;
@@ -178,7 +179,7 @@ public class AWTThreading {
                         // - guarantees a single run either from dispatch or dispose
                         // - removes the invocation event from the tracking queue
                         new Runnable() {
-                            SoftReference<TrackingQueue> queueRef = new SoftReference<>(queue);
+                            WeakReference<TrackingQueue> queueRef = new WeakReference<>(queue);
 
                             @Override
                             public void run() {
@@ -189,7 +190,6 @@ public class AWTThreading {
                                     TrackingQueue q = queueRef.get();
                                     if (q != null) {
                                         q.remove(eventRef[0]);
-                                        q.clear();
                                     }
                                     queueRef = null;
                                 }
