6442af6e17333a1d0cc68724287da459adb7b782
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2021-01-15 12:00:23 +0300

    log LWCToolkit invokeAndWait requests
    as part of JBR-3017, to make investigation of similar issues simpler in the future
    
    (cherry-picked from commit a7fd723e43d02995c3f50236a4ff744a67a982f1)
    
    (cherry picked from commit d8ca0763a5dde47d1724d8326b72db92d9b36c8e)

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/LWCToolkit.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/LWCToolkit.java
@@ -110,6 +110,7 @@ import sun.lwawt.PlatformComponent;
 import sun.lwawt.PlatformDropTarget;
 import sun.lwawt.PlatformWindow;
 import sun.lwawt.SecurityWarningWindow;
+import sun.util.logging.PlatformLogger;
 
 @SuppressWarnings("serial") // JDK implementation class
 final class NamedCursor extends Cursor {
@@ -176,10 +177,11 @@ public final class LWCToolkit extends LWToolkit {
             = !Boolean.parseBoolean(
                     System.getProperty("javafx.embed.singleThread", "false"));
 
+    private static final PlatformLogger log = PlatformLogger.getLogger("sun.lwawt.macosx.LWCToolkit");
+
     public LWCToolkit() {
         final String extraButtons = "sun.awt.enableExtraMouseButtons";
-            areExtraMouseButtonsEnabled =
-                 Boolean.parseBoolean(System.getProperty(extraButtons, "true"));
+            areExtraMouseButtonsEnabled = Boolean.parseBoolean(System.getProperty(extraButtons, "true"));
             //set system property if not yet assigned
             System.setProperty(extraButtons, "" + areExtraMouseButtonsEnabled);
             initAppkit(ThreadGroupUtils.getRootThreadGroup(),
@@ -680,6 +682,11 @@ public final class LWCToolkit extends LWToolkit {
             if (e != null) throw e;
             return object;
         }
+
+        @Override
+        public String toString() {
+            return "CallableWrapper{" + callable + "}";
+        }
     }
 
     private static final AtomicInteger blockingRunLoopCounter = new AtomicInteger(0);
@@ -717,6 +724,10 @@ public final class LWCToolkit extends LWToolkit {
     public static void invokeAndWait(Runnable runnable, Component component)
             throws InvocationTargetException
     {
+        if (log.isLoggable(PlatformLogger.Level.FINE)) {
+            log.fine("invokeAndWait started: " + runnable);
+        }
+
         boolean nonBlockingRunLoop;
 
         synchronized (priorityInvocationPending) {
@@ -750,6 +761,10 @@ public final class LWCToolkit extends LWToolkit {
 
         doAWTRunLoop(mediator, nonBlockingRunLoop);
         if (!nonBlockingRunLoop) blockingRunLoopCounter.decrementAndGet();
+
+        if (log.isLoggable(PlatformLogger.Level.FINE)) {
+            log.fine("invokeAndWait finished: " + runnable);
+        }
 
         checkException(invocationEvent);
     }
