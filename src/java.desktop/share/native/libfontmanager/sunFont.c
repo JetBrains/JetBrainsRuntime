a25fa4229734ddc9e7d58d9ccd643eac23ea69ea
Nikita Gubarkov <Nikita.Gubarkov@jetbrains.com>
2021-01-22 06:53:41 +0300

    JBR-2910 Implemented extended glyph cache for Linux

--- src/java.desktop/share/native/libfontmanager/sunFont.c
+++ src/java.desktop/share/native/libfontmanager/sunFont.c
@@ -314,7 +314,7 @@ Java_sun_font_StrikeCache_getGlyphCacheDescription
     GlyphInfo *info;
     size_t baseAddr;
 
-    if ((*env)->GetArrayLength(env, results) < 13) {
+    if ((*env)->GetArrayLength(env, results) < 15) {
         return;
     }
 
@@ -341,6 +341,8 @@ Java_sun_font_StrikeCache_getGlyphCacheDescription
     nresults[10] = (jlong)(uintptr_t)info; /* invisible glyph */
     nresults[11] = (size_t)&(info->cellInfo)-baseAddr;
     nresults[12] = (size_t)&(info->managed)-baseAddr;
+    nresults[13] = (size_t)&(info->subpixelResolutionX)-baseAddr;
+    nresults[14] = (size_t)&(info->subpixelResolutionY)-baseAddr;
 
     (*env)->ReleasePrimitiveArrayCritical(env, results, nresults, 0);
 }
