29387136f1ea4db774250e419010367c5b200620
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2021-02-02 22:40:55 +0300

    JBR-3131: support custom view for system menu items
    (cherry picked from commit 78d509ac0f32a5e088f78ace9c9d2bbf5eb62b2d)
    (cherry picked from commit ca05fefb2826f70c38fe7597b6bae98322918814)

--- src/java.desktop/macosx/classes/com/apple/laf/ScreenMenuPropertyListener.java
+++ src/java.desktop/macosx/classes/com/apple/laf/ScreenMenuPropertyListener.java
@@ -68,6 +68,11 @@ final class ScreenMenuPropertyListener implements PropertyChangeListener {
             return;
         }
 
+        if ("accelerator.text".equals(propertyName)) {
+            fMenu.setAcceleratorText((String)e.getNewValue());
+            return;
+        }
+
         if (AbstractButton.TEXT_CHANGED_PROPERTY.equals(propertyName)) {
             fMenu.setLabel((String)e.getNewValue());
             return;
