3ba18d62e840cd8a0fca9cfa94e708116ed067e0
Vladimir Dvorak <vladimir.dvorak@jetbrains.com>
2021-02-12 12:33:47 +0100

    Check InstanceKlass::has_nestmate_access_to with active classes
    Dcevm can leave old host in nested class if nested class is not
    redefined together with host class

--- src/hotspot/share/oops/instanceKlass.cpp
+++ src/hotspot/share/oops/instanceKlass.cpp
@@ -443,14 +443,19 @@ bool InstanceKlass::has_nestmate_access_to(InstanceKlass* k, TRAPS) {
     return false;
   }
 
+  // (DCEVM) cur_host can be old, decide accessibility based on active version
   if (AllowEnhancedClassRedefinition) {
-    // TODO: (DCEVM) check if it correct. It fix problems with lambdas (hidden)
-    cur_host = InstanceKlass::cast(cur_host->newest_version());
+    cur_host = InstanceKlass::cast(cur_host->active_version());
   }
 
   Klass* k_nest_host = k->nest_host(CHECK_false);
   if (k_nest_host == NULL) {
     return false;
+  }
+
+  // (DCEVM) k_nest_host can be old, decide accessibility based on active version
+  if (AllowEnhancedClassRedefinition) {
+    k_nest_host = InstanceKlass::cast(k_nest_host->active_version());
   }
 
   bool access = (cur_host == k_nest_host);
