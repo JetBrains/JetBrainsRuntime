0c423d30a4d2459b01efa0b3f1f294f84197f237
Anton Tarasov <anton.tarasov@jetbrains.com>
2021-03-19 17:23:17 +0300

    JBR-1351 Borderless UI: Bold frame around IDEA window appears on non-HiDPI display
    (cherry picked from commit 06d35de06951adacc04cba6c35bf7d568ff11f79)

--- src/java.desktop/windows/native/libawt/windows/awt_Toolkit.cpp
+++ src/java.desktop/windows/native/libawt/windows/awt_Toolkit.cpp
@@ -141,6 +141,8 @@ extern "C" JNIEXPORT jboolean JNICALL AWTIsHeadless() {
 
 #define IDT_AWT_MOUSECHECK 0x101
 
+AdjustWindowRectExForDpiFunc* AwtToolkit::lpAdjustWindowRectExForDpi = NULL;
+
 static LPCTSTR szAwtToolkitClassName = TEXT("SunAwtToolkit");
 
 static const int MOUSE_BUTTONS_WINDOWS_SUPPORTED = 5; //three standard buttons + XBUTTON1 + XBUTTON2.
@@ -664,6 +666,12 @@ BOOL AwtToolkit::Initialize() {
                                               0, tk.m_mainThreadId);
 
     awt_dnd_initialize();
+
+    HMODULE hLibUser32Dll = JDK_LoadSystemLibrary("User32.dll");
+    if (hLibUser32Dll != NULL) {
+        lpAdjustWindowRectExForDpi = (AdjustWindowRectExForDpiFunc*)GetProcAddress(hLibUser32Dll, "AdjustWindowRectExForDpi");
+        ::FreeLibrary(hLibUser32Dll);
+    }
 
     /*
      * Initialization of the touch keyboard related variables.
