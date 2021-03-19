bded41b966a5973f9d78b938b891d4343a17cbe1
Denis Konoplev <denis.konoplev@jetbrains.com>
2021-03-19 19:43:43 +0300

    JBR-3214: Reuse openjdk logic and add unicode keycodes
    (cherry picked from commit 3668d631caa461ad2b970eaf2d969d226608049a)

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/NSEvent.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/NSEvent.java
@@ -298,8 +298,7 @@ final class NSEvent {
      * There is a small number of NS characters that need to be converted
      * into other characters before we pass them to AWT.
      */
-    static native String nsToJavaChar(char nsChar, int modifierFlags, boolean spaceKeyTyped);
-    static native char nsToJavaCharOld(char nsChar, int modifierFlags);
+    static native char nsToJavaChar(char nsChar, int modifierFlags, boolean spaceKeyTyped);
 
     static boolean isPopupTrigger(int jmodifiers) {
         final boolean isRightButtonDown = ((jmodifiers & InputEvent.BUTTON3_DOWN_MASK) != 0);
