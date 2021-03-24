b99be427ced141676300e593b4a3a7b9fa92a3df
Artem Semenov <Artem.Semenov@jetbrains.com>
2021-03-24 16:32:30 +0300

    JR-CR-774: JBR-3218: IDEA crash with opening drop down menu and click whatever next.

--- src/java.desktop/macosx/native/libawt_lwawt/awt/JavaElementAccessibility.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/JavaElementAccessibility.m
@@ -95,8 +95,9 @@ static void RaiseMustOverrideException(NSString *method)
 
 - (id)accessibilityParent
 {
-    JavaBaseAccessibility *parent = (JavaBaseAccessibility *) [self.javaBase parent];
-    return parent.platformAxElement;
+    id parent = [self.javaBase parent];
+    // Checking for protocol compliance can slow down at runtime. See: https://developer.apple.com/documentation/objectivec/nsobject/1418893-conformstoprotocol?language=objc
+    return [parent respondsToSelector:@selector(platformAxElement)] ? [parent platformAxElement] : parent;
 }
 
 - (BOOL)accessibilityIsIgnored
