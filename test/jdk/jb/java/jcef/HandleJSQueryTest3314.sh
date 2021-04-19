3c868c53b8d28679bae7a479abd592bb674ce35f
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2021-04-19 09:41:38 +0700

    fixup! JBR-3314 add regression test

--- test/jdk/jb/java/jcef/HandleJSQueryTest3314.sh
+++ test/jdk/jb/java/jcef/HandleJSQueryTest3314.sh
@@ -23,7 +23,7 @@
 #          Number of iterations can be specified via the environment variable RUN_NUMBER, by default it is set to 100.
 # @run shell/timeout=300 HandleJSQueryTest3314.sh
 
-RUNS_NUMBER=${RUN_NUMBER:-100}
+RUNS_NUMBER=${RUN_NUMBER:-50}
 echo "number of iterations: $RUNS_NUMBER"
 
 if [ -z "${TESTSRC}" ]; then
@@ -41,19 +41,22 @@ if [ -z "${TESTJAVA}" ]; then
   exit 1
 fi
 
-pushd .
+curdir=$(pwd)
 cd ${TESTSRC}
 ${TESTJAVA}/bin/javac -d ${TESTCLASSES} HandleJSQueryTest.java
-popd
+cd $curdir
 
-for ((i = 0 ; i < $RUNS_NUMBER ; i++)); do
+i=0
+while [ "$i" -le "$RUNS_NUMBER" ]; do
     echo "iteration - $i"
     ${TESTJAVA}/bin/java -cp ${TESTCLASSES} HandleJSQueryTest
     exit_code=$?
+    echo "exit_xode=$exit_code"
     if [ $exit_code -ne "0" ]; then
       [[ $exit_code -eq "134" ]] && echo "FAILED: Test crashed" && exit $exit_code
       echo "Test failed because of not a crash. Execution is being conituned"
     fi
+    i=$(( i + 1 ))
 done
 echo "PASSED: Test did never crash during 100 iterations"
 exit 0
