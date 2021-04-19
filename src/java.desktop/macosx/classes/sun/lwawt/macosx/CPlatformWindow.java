007f70d118e2a484429ce28eb055aeffb0271118
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2021-04-19 20:10:22 +0300

    8265445: Introduce the new client property for mac: apple.awt.windowAppearance
    Implemented apple.awt.windowAppearance client property
    
    (cherry picked from commit ba60f7bd8fc397e7a64f7133985786b7378e5e38)

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformWindow.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformWindow.java
@@ -77,6 +77,7 @@ import sun.util.logging.PlatformLogger;
 public class CPlatformWindow extends CFRetainedResource implements PlatformWindow {
     private native long nativeCreateNSWindow(long nsViewPtr,long ownerPtr, long styleBits, double x, double y, double w, double h);
     private static native void nativeSetNSWindowStyleBits(long nsWindowPtr, int mask, int data);
+    private static native void nativeSetNSWindowAppearance(long nsWindowPtr, String appearanceName);
     private static native void nativeSetNSWindowMenuBar(long nsWindowPtr, long menuBarPtr);
     private static native Insets nativeGetNSWindowInsets(long nsWindowPtr);
     private static native void nativeSetNSWindowBounds(long nsWindowPtr, double x, double y, double w, double h);
@@ -132,6 +133,7 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
     public static final String WINDOW_TITLE_VISIBLE = "apple.awt.windowTitleVisible";
     public static final String WINDOW_ACCESSIBILITY_ELEMENT =
             "apple.awt.windowAccessibilityElement";
+    public static final String WINDOW_APPEARANCE = "apple.awt.windowAppearance";
 
     // This system property is named as jdk.* because it is not specific to AWT
     // and it is also used in JavaFX
@@ -278,6 +280,13 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
                                       final Object value) {
                 c.setAccessibilityElement(value == null ? true :
                         Boolean.parseBoolean(value.toString()));
+            }
+        },
+        new Property<CPlatformWindow>(WINDOW_APPEARANCE) {
+            public void applyProperty(final CPlatformWindow c, final Object value) {
+                if (value != null && (value instanceof String)) {
+                    c.execute(ptr -> nativeSetNSWindowAppearance(ptr, (String) value));
+                }
             }
         }
     }) {
