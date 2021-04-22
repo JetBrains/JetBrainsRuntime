4e5c9edfe6baaab13418c470fd503c779694bab3
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2021-04-22 15:06:10 +0300

    JBR-3353 Sibling popup window is shown below dialog on macOS
    (cherry picked from commit 4c6f3e45100681423d63ca2c60728837cafc4e95)
    (cherry picked from commit 73678a99d51c01d21fcf15c7a01bf07ce6a68ee1)

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformWindow.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformWindow.java
@@ -798,6 +798,8 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
             }
         }
 
+        this.visible = visible;
+
         // Actually show or hide the window
         LWWindowPeer blocker = (peer == null)? null : peer.getBlocker();
         if (!visible) {
@@ -860,7 +862,6 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
                 });
             });
         }
-        this.visible = visible;
 
         nativeSynthesizeMouseEnteredExitedEvents();
 
