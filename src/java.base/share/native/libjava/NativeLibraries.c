a3fcfa5cea765340fc9c727b5d5ea7ca436227ee
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2021-04-23 16:15:09 +0700

    JBR-1430 (8195129) Windows: use UTF16 version of Win32 API to load DLL
    Also correct library name encoding in exception messages.
    
    with fix for JBR-5300 Change source code and test files to use GPL license

--- src/java.base/share/native/libjava/NativeLibraries.c
+++ src/java.base/share/native/libjava/NativeLibraries.c
@@ -115,7 +115,6 @@ Java_jdk_internal_loader_NativeLibraries_load
   (JNIEnv *env, jclass cls, jobject lib, jstring name,
    jboolean isBuiltin, jboolean throwExceptionIfFail)
 {
-    const char *cname;
     jint jniVersion;
     jthrowable cause;
     void * handle;
@@ -124,14 +123,15 @@ Java_jdk_internal_loader_NativeLibraries_load
     if (!initIDs(env))
         return JNI_FALSE;
 
-    cname = JNU_GetStringPlatformChars(env, name, 0);
-    if (cname == 0)
+    const char * utf8_name = GetStringUTF8Chars(env, name);
+    if (utf8_name == NULL)
         return JNI_FALSE;
-    handle = isBuiltin ? procHandle : JVM_LoadLibrary(cname, throwExceptionIfFail);
+
+    handle = isBuiltin ? procHandle : JVM_LoadLibrary(utf8_name, throwExceptionIfFail);
     if (handle) {
         JNI_OnLoad_t JNI_OnLoad;
         JNI_OnLoad = (JNI_OnLoad_t)findJniFunction(env, handle,
-                                                   isBuiltin ? cname : NULL,
+                                                   isBuiltin ? utf8_name : NULL,
                                                    JNI_TRUE);
         if (JNI_OnLoad) {
             JavaVM *jvm;
@@ -156,7 +156,7 @@ Java_jdk_internal_loader_NativeLibraries_load
             char msg[256];
             jio_snprintf(msg, sizeof(msg),
                          "unsupported JNI version 0x%08X required by %s",
-                         jniVersion, cname);
+                         jniVersion, utf8_name);
             JNU_ThrowByName(env, "java/lang/UnsatisfiedLinkError", msg);
             if (!isBuiltin) {
                 JVM_UnloadLibrary(handle);
@@ -178,7 +178,7 @@ Java_jdk_internal_loader_NativeLibraries_load
     loaded = JNI_TRUE;
 
  done:
-    JNU_ReleaseStringPlatformChars(env, name, cname);
+    ReleaseStringUTF8Chars(env, name, utf8_name);
     return loaded;
 }
 
