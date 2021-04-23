a72bcf94febd2f4dbb2b65bc63ac09bff9af96f0
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2021-04-23 12:15:09 +0300

    JBR-1430 (8195129) Windows: use UTF16 version of Win32 API to load DLL
    Also correct library name encoding in exception messages.

--- src/hotspot/os/windows/os_windows.cpp
+++ src/hotspot/os/windows/os_windows.cpp
@@ -1514,9 +1514,221 @@ static int _print_module(const char* fname, address base_address,
   return 0;
 }
 
+static errno_t convert_to_UTF16(char const* source_str, UINT source_encoding, LPWSTR* dest_utf16_str) {
+  const int len_estimated = MultiByteToWideChar(source_encoding,
+                                                MB_ERR_INVALID_CHARS,
+                                                source_str,
+                                                -1, // source is null-terminated
+                                                NULL,
+                                                0); // estimate characters count
+  if (len_estimated == 0) {
+    // Probably source_str contains characters that cannot be represented in the source_encoding given.
+    *dest_utf16_str = NULL;
+    return EINVAL;
+  }
+
+  *dest_utf16_str = NEW_C_HEAP_ARRAY(WCHAR, len_estimated, mtInternal);
+
+  const int len_real = MultiByteToWideChar(source_encoding,
+                                           MB_ERR_INVALID_CHARS,
+                                           source_str,
+                                           -1, // source is null-terminated
+                                           *dest_utf16_str, len_estimated);
+  assert(len_real == len_estimated, "length already checked above");
+
+  return ERROR_SUCCESS;
+}
+
+// Converts a string in the "platform" encoding to UTF16.
+static errno_t convert_to_UTF16(char const* platform_str, LPWSTR* utf16_str) {
+  return convert_to_UTF16(platform_str, CP_ACP, utf16_str);
+}
+
+static errno_t convert_UTF8_to_UTF16(char const* utf8_str, LPWSTR* utf16_str) {
+  return convert_to_UTF16(utf8_str, CP_UTF8, utf16_str);
+}
+
+// Converts a wide-character string in UTF-16 encoding to the 8-bit "platform" encoding.
+// Unless the platform encoding is UTF-8, not all characters in the source string can be represented in the dest string.
+// The function succeeds in this case anyway and just replaces these with a certain character.
+static errno_t convert_UTF16_to_platform(LPWSTR source_utf16_str, char*& dest_str) {
+  const int len_estimated = WideCharToMultiByte(CP_ACP,
+                                                0,
+                                                source_utf16_str,
+                                                -1, // source is null-terminated
+                                                NULL,
+                                                0, // estimate characters count
+                                                NULL, NULL);
+  if (len_estimated == 0) {
+    dest_str = NULL;
+    return EINVAL;
+  }
+
+  dest_str = NEW_C_HEAP_ARRAY(CHAR, len_estimated, mtInternal);
+
+  const int len_real = WideCharToMultiByte(CP_ACP,
+                                           0,
+                                           source_utf16_str,
+                                           -1, // source is null-terminated
+                                           dest_str, len_estimated, NULL, NULL);
+  assert(len_real == len_estimated, "length already checked above");
+
+  return ERROR_SUCCESS;
+}
+
+class MemoryReleaserW : public StackObj {
+private:
+  WCHAR* _object_ptr;
+
+public:
+  MemoryReleaserW(WCHAR * object_ptr) : _object_ptr(object_ptr) {}
+  ~MemoryReleaserW() { if (_object_ptr != NULL) FREE_C_HEAP_ARRAY(WCHAR, _object_ptr); }
+};
+
+class MemoryReleaser : public StackObj {
+private:
+  CHAR* _object_ptr;
+
+public:
+  MemoryReleaser(CHAR * object_ptr) : _object_ptr(object_ptr) {}
+  ~MemoryReleaser() { if (_object_ptr != NULL) FREE_C_HEAP_ARRAY(CHAR, _object_ptr); }
+};
+
 // Loads .dll/.so and
 // in case of error it checks if .dll/.so was built for the
 // same architecture as Hotspot is running on
+void * os::dll_load_utf8(const char *utf8_name, char *ebuf, int ebuflen) {
+  LPWSTR utf16_name = NULL;
+  errno_t err = convert_UTF8_to_UTF16(utf8_name, &utf16_name);
+  MemoryReleaserW release_utf16_name(utf16_name);
+  if (err != ERROR_SUCCESS) {
+    errno = err;
+    return NULL;
+  }
+
+  char* platform_name = NULL; // name of the library converted to the "platform" encoding for use in log messages
+  errno_t ignored_err = convert_UTF16_to_platform(utf16_name, platform_name);
+  MemoryReleaser release_platform_name(platform_name);
+
+  log_info(os)("attempting shared library load of %s", platform_name);
+
+  void * result = LoadLibraryW(utf16_name);
+
+  if (result != NULL) {
+    Events::log(NULL, "Loaded shared library %s", platform_name);
+    // Recalculate pdb search path if a DLL was loaded successfully.
+    SymbolEngine::recalc_search_path();
+    log_info(os)("shared library load of %s was successful", platform_name);
+    return result;
+  }
+  DWORD errcode = GetLastError();
+  // Read system error message into ebuf
+  // It may or may not be overwritten below (in the for loop and just above)
+  lasterror(ebuf, (size_t) ebuflen);
+  ebuf[ebuflen - 1] = '\0';
+  Events::log(NULL, "Loading shared library %s failed, error code %lu", platform_name, errcode);
+  log_info(os)("shared library load of %s failed, error code %lu", platform_name, errcode);
+
+  if (errcode == ERROR_MOD_NOT_FOUND) {
+    strncpy(ebuf, "Can't find dependent libraries", ebuflen - 1);
+    ebuf[ebuflen - 1] = '\0';
+    return NULL;
+  }
+
+  // Parsing dll below
+  // If we can read dll-info and find that dll was built
+  // for an architecture other than Hotspot is running in
+  // - then print to buffer "DLL was built for a different architecture"
+  // else call os::lasterror to obtain system error message
+  int fd = ::wopen(utf16_name, O_RDONLY | O_BINARY, 0);
+  if (fd < 0) {
+    return NULL;
+  }
+
+  uint32_t signature_offset;
+  uint16_t lib_arch = 0;
+  bool failed_to_get_lib_arch =
+    ( // Go to position 3c in the dll
+     (os::seek_to_file_offset(fd, IMAGE_FILE_PTR_TO_SIGNATURE) < 0)
+     ||
+     // Read location of signature
+     (sizeof(signature_offset) !=
+     (::read(fd, (void*)&signature_offset, sizeof(signature_offset))))
+     ||
+     // Go to COFF File Header in dll
+     // that is located after "signature" (4 bytes long)
+     (os::seek_to_file_offset(fd,
+     signature_offset + IMAGE_FILE_SIGNATURE_LENGTH) < 0)
+     ||
+     // Read field that contains code of architecture
+     // that dll was built for
+     (sizeof(lib_arch) != (::read(fd, (void*)&lib_arch, sizeof(lib_arch))))
+    );
+
+  ::close(fd);
+  if (failed_to_get_lib_arch) {
+    // file i/o error - report os::lasterror(...) msg
+    return NULL;
+  }
+
+  typedef struct {
+    uint16_t arch_code;
+    char* arch_name;
+  } arch_t;
+
+  static const arch_t arch_array[] = {
+    {IMAGE_FILE_MACHINE_I386,      (char*)"IA 32"},
+    {IMAGE_FILE_MACHINE_AMD64,     (char*)"AMD 64"},
+    {IMAGE_FILE_MACHINE_ARM64,     (char*)"ARM 64"}
+  };
+#if (defined _M_ARM64)
+  static const uint16_t running_arch = IMAGE_FILE_MACHINE_ARM64;
+#elif (defined _M_AMD64)
+  static const uint16_t running_arch = IMAGE_FILE_MACHINE_AMD64;
+#elif (defined _M_IX86)
+  static const uint16_t running_arch = IMAGE_FILE_MACHINE_I386;
+#else
+  #error Method os::dll_load requires that one of following \
+         is defined :_M_AMD64 or _M_IX86 or _M_ARM64
+#endif
+
+
+  // Obtain a string for printf operation
+  // lib_arch_str shall contain string what platform this .dll was built for
+  // running_arch_str shall string contain what platform Hotspot was built for
+  char *running_arch_str = NULL, *lib_arch_str = NULL;
+  for (unsigned int i = 0; i < ARRAY_SIZE(arch_array); i++) {
+    if (lib_arch == arch_array[i].arch_code) {
+      lib_arch_str = arch_array[i].arch_name;
+    }
+    if (running_arch == arch_array[i].arch_code) {
+      running_arch_str = arch_array[i].arch_name;
+    }
+  }
+
+  assert(running_arch_str,
+         "Didn't find running architecture code in arch_array");
+
+  // If the architecture is right
+  // but some other error took place - report os::lasterror(...) msg
+  if (lib_arch == running_arch) {
+    return NULL;
+  }
+
+  if (lib_arch_str != NULL) {
+    ::_snprintf(ebuf, ebuflen - 1,
+                "Can't load %s-bit .dll on a %s-bit platform",
+                lib_arch_str, running_arch_str);
+  } else {
+    // don't know what architecture this dll was build for
+    ::_snprintf(ebuf, ebuflen - 1,
+                "Can't load this .dll (machine code=0x%x) on a %s-bit platform",
+                lib_arch, running_arch_str);
+  }
+
+  return NULL;
+}
+
 void * os::dll_load(const char *name, char *ebuf, int ebuflen) {
   log_info(os)("attempting shared library load of %s", name);
 
@@ -4463,7 +4675,7 @@ static wchar_t* wide_abs_unc_path(char const* path, errno_t & err, int additiona
   set_path_prefix(buf, &prefix, &prefix_off, &needs_fullpath);
 
   LPWSTR unicode_path = NULL;
-  err = convert_to_unicode(buf, &unicode_path);
+  err = convert_to_UTF16(buf, &unicode_path);
   FREE_C_HEAP_ARRAY(char, buf);
   if (err != ERROR_SUCCESS) {
     return NULL;
