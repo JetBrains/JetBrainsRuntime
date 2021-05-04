939e2212eee1aad8201c932cac32740b7f86cd44
Anton Tarasov <anton.tarasov@jetbrains.com>
2021-05-05 01:05:08 +0300

    JBR-3413 use timeout in CAccessibility.invokeAndWait
    (cherry picked from commit e7009db076b0584e6987bd1fdda6753a00f6577d)

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CAccessibility.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CAccessibility.java
@@ -42,6 +42,7 @@ import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.Arrays;
 import java.util.function.Function;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.accessibility.Accessible;
 import javax.accessibility.AccessibleAction;
@@ -69,6 +70,8 @@ import sun.lwawt.LWWindowPeer;
 
 class CAccessibility implements PropertyChangeListener {
     private static Set<String> ignoredRoles;
+    private static final int INVOKE_TIMEOUT_SECONDS_DEFAULT = 1;
+    private static /*final*/ int INVOKE_TIMEOUT_SECONDS;
 
     static {
         loadAWTLibrary();
@@ -76,14 +79,20 @@ class CAccessibility implements PropertyChangeListener {
 
     @SuppressWarnings("removal")
     private static void loadAWTLibrary() {
-            // Need to load the native library for this code.
+        AtomicInteger invokeTimeoutSecondsRef = new AtomicInteger();
+        // Need to load the native library for this code.
         java.security.AccessController.doPrivileged(
             new java.security.PrivilegedAction<Void>() {
                 public Void run() {
                     System.loadLibrary("awt");
+                    invokeTimeoutSecondsRef.set(
+                            // (-1) for the infinite timeout
+                            Integer.getInteger("sun.lwawt.macosx.CAccessibility.invokeTimeoutSeconds",
+                                    INVOKE_TIMEOUT_SECONDS_DEFAULT));
                     return null;
                 }
             });
+        INVOKE_TIMEOUT_SECONDS = invokeTimeoutSecondsRef.get();
     }
 
     static CAccessibility sAccessibility;
@@ -125,14 +134,14 @@ class CAccessibility implements PropertyChangeListener {
     private native void focusChanged();
 
     static <T> T invokeAndWait(final Callable<T> callable, final Component c) {
-        return invokeAndWait(callable, c, null);
+        return invokeAndWait(callable, c, (T)null);
     }
 
     static <T> T invokeAndWait(final Callable<T> callable, final Component c, final T defValue) {
         T value = null;
         if (c != null) {
             try {
-                value = LWCToolkit.invokeAndWait(callable, c);
+                value = LWCToolkit.invokeAndWait(callable, c, INVOKE_TIMEOUT_SECONDS);
             } catch (final Exception e) { e.printStackTrace(); }
         }
 
