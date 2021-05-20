3d746d109f71e71765cfea62972b1b2a18843e25
Vyacheslav Moklev <vyacheslav.moklev@jetbrains.com>
2021-05-20 16:08:29 +0300

    Fix compilation on windows platform: awt_ole.h must be included before awt.h
    (cherry picked from commit 6fdcfedd0e6048e005a3f1a0b14acc8945618c4f)

--- src/java.desktop/windows/native/libawt/windows/awt_FileDialog.cpp
+++ src/java.desktop/windows/native/libawt/windows/awt_FileDialog.cpp
@@ -23,11 +23,11 @@
  * questions.
  */
 
+#include "awt_ole.h"
 #include "awt.h"
 #include "awt_FileDialog.h"
 #include "awt_Dialog.h"
 #include "awt_Toolkit.h"
-#include "awt_ole.h"
 #include "ComCtl32Util.h"
 #include <commdlg.h>
 #include <cderr.h>
