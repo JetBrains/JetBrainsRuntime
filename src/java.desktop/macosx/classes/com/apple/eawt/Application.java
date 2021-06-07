c0be778e20fffeb327782ffa5ec0765f31bf561c
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2021-06-07 20:54:10 +0300

    JBR-3344 "Exit Full Screen" action doesn't work, the only way is mouse click on window's native "green" button.
    Restored JBR-1931 fix partially reverted by JBR-1718

--- src/java.desktop/macosx/classes/com/apple/eawt/Application.java
+++ src/java.desktop/macosx/classes/com/apple/eawt/Application.java
@@ -424,4 +424,19 @@ public class Application {
         ((CPlatformWindow)platformWindow).toggleFullScreen();
     }
 
+    public void requestEnterFullScreen(final Window window) {
+        final Object peer = AWTAccessor.getComponentAccessor().getPeer(window);
+        if (!(peer instanceof LWWindowPeer)) return;
+        Object platformWindow = ((LWWindowPeer) peer).getPlatformWindow();
+        if (!(platformWindow instanceof CPlatformWindow)) return;
+        ((CPlatformWindow)platformWindow).enterFullScreen();
+    }
+
+    public void requestLeaveFullScreen(final Window window) {
+        final Object peer = AWTAccessor.getComponentAccessor().getPeer(window);
+        if (!(peer instanceof LWWindowPeer)) return;
+        Object platformWindow = ((LWWindowPeer) peer).getPlatformWindow();
+        if (!(platformWindow instanceof CPlatformWindow)) return;
+        ((CPlatformWindow)platformWindow).leaveFullScreen();
+    }
 }
