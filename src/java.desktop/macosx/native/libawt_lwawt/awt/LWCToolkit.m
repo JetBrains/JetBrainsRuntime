ca00a74b84cfdec29a49a1f40b1a5535a2c10d7d
Anton Tarasov <anton.tarasov@jetbrains.com>
2021-06-21 15:04:15 +0300

    JBR-3413 use timeout in CAccessibility.invokeAndWait

--- src/java.desktop/macosx/native/libawt_lwawt/awt/LWCToolkit.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/LWCToolkit.m
@@ -67,6 +67,8 @@ static BOOL sAppKitStarted = NO;
 static pthread_mutex_t sAppKitStarted_mutex = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t sAppKitStarted_cv = PTHREAD_COND_INITIALIZER;
 
+static time_t YEAR_SECONDS = 60 * 60 * 24 * 365;
+
 @implementation AWTToolkit
 
 static long eventCount;
@@ -572,23 +574,30 @@ JNI_COCOA_EXIT(env);
 /*
  * Class:     sun_lwawt_macosx_LWCToolkit
  * Method:    doAWTRunLoopImpl
- * Signature: (JZZ)V
+ * Signature: (JZZI)Z
  */
-JNIEXPORT void JNICALL Java_sun_lwawt_macosx_LWCToolkit_doAWTRunLoopImpl
-(JNIEnv *env, jclass clz, jlong mediator, jboolean processEvents, jboolean inAWT)
+JNIEXPORT jboolean JNICALL Java_sun_lwawt_macosx_LWCToolkit_doAWTRunLoopImpl
+(JNIEnv *env, jclass clz, jlong mediator, jboolean processEvents, jboolean inAWT, jint timeoutSeconds/*(-1) for infinite*/)
 {
 AWT_ASSERT_APPKIT_THREAD;
-JNI_COCOA_ENTER(env);
+    jboolean result = JNI_TRUE;
 
+JNI_COCOA_ENTER(env);
     AWTRunLoopObject* mediatorObject = (AWTRunLoopObject*)jlong_to_ptr(mediator);
 
-    if (mediatorObject == nil) return;
+    if (mediatorObject == nil) return JNI_TRUE;
+
+    time_t timeThreshold = timeoutSeconds < 0 ? time(NULL) + YEAR_SECONDS : time(NULL) + timeoutSeconds;
 
     // Don't use acceptInputForMode because that doesn't setup autorelease pools properly
     BOOL isRunning = true;
     while (![mediatorObject shouldEndRunLoop] && isRunning) {
         isRunning = [[NSRunLoop currentRunLoop] runMode:(inAWT ? [ThreadUtilities javaRunLoopMode] : NSDefaultRunLoopMode)
                                              beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.010]];
+        if (difftime(timeThreshold, time(NULL)) < 0) {
+            result = JNI_FALSE;
+            break;
+        }
         if (processEvents) {
             //We do not spin a runloop here as date is nil, so does not matter which mode to use
             // Processing all events excluding NSApplicationDefined which need to be processed
@@ -605,6 +614,7 @@ JNI_COCOA_ENTER(env);
     }
     [mediatorObject release];
 JNI_COCOA_EXIT(env);
+    return result;
 }
 
 /*
