bebabd26c92dd74f67773cf96234c2aad0d85f4a
bell-sw <aleksei.voitylov@bell-sw.com>
2021-06-22 22:18:53 +0300

    8248238, JBR-2074 Windows AArch64 Support.

--- src/hotspot/cpu/aarch64/globalDefinitions_aarch64.hpp
+++ src/hotspot/cpu/aarch64/globalDefinitions_aarch64.hpp
@@ -53,4 +53,13 @@ const bool CCallingConventionRequiresIntsAsLongs = false;
 
 #define THREAD_LOCAL_POLL
 
+#if defined(_WIN64)
+#define R18_RESERVED
+#define R18_RESERVED_ONLY(code) code
+#define NOT_R18_RESERVED(code)
+#else
+#define R18_RESERVED_ONLY(code)
+#define NOT_R18_RESERVED(code) code
+#endif
+
 #endif // CPU_AARCH64_VM_GLOBALDEFINITIONS_AARCH64_HPP
