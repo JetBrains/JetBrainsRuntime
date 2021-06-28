4ad0db5e74096f1c484164190d76a32d49eb3a3e
Alexey Bakhtin <abakhtin@openjdk.org>
2021-06-28 16:43:59 +0000

    8140466: ChaCha20 and Poly1305 TLS Cipher Suites
    Reviewed-by: mdoerr
    Backport-of: 962e755c3ac6731566fe9f39520a414628f24ee0

--- test/jdk/javax/net/ssl/TLSCommon/CipherSuite.java
+++ test/jdk/javax/net/ssl/TLSCommon/CipherSuite.java
@@ -118,6 +118,8 @@ public enum CipherSuite {
             0x1302, null, Protocol.TLSV1_3, Protocol.TLSV1_3),
     TLS_AES_128_GCM_SHA256(
             0x1301, null, Protocol.TLSV1_3, Protocol.TLSV1_3),
+    TLS_CHACHA20_POLY1305_SHA256(
+            0x1303, null, Protocol.TLSV1_3, Protocol.TLSV1_3),
     TLS_DH_anon_WITH_AES_256_GCM_SHA384(
             0x00A7, KeyExAlgorithm.DH_ANON, Protocol.TLSV1_2, Protocol.TLSV1_2),
     TLS_DH_anon_WITH_AES_128_GCM_SHA256(
