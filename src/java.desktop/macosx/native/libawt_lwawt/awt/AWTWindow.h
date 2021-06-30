4af6cf545fcc0a107b8859de83e9a23c37cff1a5
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2021-06-30 18:52:17 +0300

    JBR-3017 Focus issue in presence of third-party accessibility tool
    (cherry picked from commit 88ead5d9e3d9fb407582a29a371af50a984838f1)
    (cherry picked from commit 97753f15b640aab13a885eda8b03c1cd545846f6)

--- src/java.desktop/macosx/native/libawt_lwawt/awt/AWTWindow.h
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/AWTWindow.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -46,7 +46,6 @@
     jint preFullScreenLevel;
     NSRect standardFrame;
     BOOL isMinimizing;
-    BOOL keyNotificationRecd;
 }
 
 // An instance of either AWTWindow_Normal or AWTWindow_Panel
@@ -62,7 +61,6 @@
 @property (nonatomic) jint preFullScreenLevel;
 @property (nonatomic) NSRect standardFrame;
 @property (nonatomic) BOOL isMinimizing;
-@property (nonatomic) BOOL keyNotificationRecd;
 
 - (id) initWithPlatformWindow:(jobject)javaPlatformWindow
                   ownerWindow:owner
