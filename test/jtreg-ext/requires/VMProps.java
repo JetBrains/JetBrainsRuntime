5d4873b6da802b895260ed8126322ca9bb2bf60a
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2021-07-05 07:59:22 -0700

    JBR-3572 Wayland: java/awt/Window/WindowTitleVisibleTest/WindowTitleVisibleTestLinuxGnome.java: title bar shown and hidden are the same.
    Exclude the test when running under XWayland that doesn't implement
    screen capture necessary for the test to function.
    
    (cherry picked from commit 4326028811b9954cc5314522498ec60a993b49f5)

--- test/jtreg-ext/requires/VMProps.java
+++ test/jtreg-ext/requires/VMProps.java
@@ -140,6 +140,7 @@ public class VMProps implements Callable<Map<String, String>> {
         map.put("vm.flagless", this::isFlagless);
         map.put("jdk.foreign.linker", this::jdkForeignLinker);
         map.put("jlink.packagedModules", this::packagedModules);
+        map.put("display.XWayland", this::displayXWayland);
         vmGC(map); // vm.gc.X = true/false
         vmGCforCDS(map); // may set vm.gc
         vmOptFinalFlags(map);
@@ -816,6 +817,13 @@ public class VMProps implements Callable<Map<String, String>> {
      */
     private String jdkForeignLinker() {
         return String.valueOf(CABI.current());
+    }
+
+    protected String displayXWayland() {
+        final boolean isXWayland =  (System.getenv("WAYLAND_DISPLAY") != null)
+                                 && (System.getenv("DISPLAY") != null);
+
+        return String.valueOf(isXWayland);
     }
 
     /**
