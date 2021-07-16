4e81ba9a2be025111a5095e60a87f6cbbffe86ba
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2021-07-16 16:33:08 +0300

    JBR-3611, JBR-3633, JBR-3666, JBR-3663, JBR-3671, JBR-3673, JBR-4181 Interoperability with macOS desktop spaces
    (cherry-picked from commits 43fdd6cd26e6365427bd346085ded41746b2f8dc, 75335543f282e4ed94bcd0bc3df16ddd68b840f9, a156c6b9bf3c612fcbde050be945152409d2de15, 9fdc75969b791cbd62433154c50cc848eec2520a, 1dcc612a81a39f1f4bbfbbdc8b3f5731ba16845a, 93588d073861f04dfe48f4bc8d311ec948a4462d, 94a3885bbe61f3258b00171d9402cc64f669c1f6, c040e05703935ebcbf01ca2e8c23509833204e2d)

---
+++ test/jdk/jb/java/awt/Window/FullScreenInactiveModalDialog.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2000-2021 JetBrains s.r.o.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+import com.apple.eawt.Application;
+import com.apple.eawt.FullScreenAdapter;
+import com.apple.eawt.FullScreenUtilities;
+import com.apple.eawt.event.FullScreenEvent;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @test
+ * @summary Regression test for JBR-3633 Modal dialog is shown not at the same space as its parent
+ * @key headful
+ * @requires (os.family == "mac")
+ * @modules java.desktop/com.apple.eawt
+ *          java.desktop/com.apple.eawt.event
+ */
+
+public class FullScreenInactiveModalDialog {
+    private static final CompletableFuture<Boolean> shownAtFullScreen = new CompletableFuture<>();
+
+    private static Robot robot;
+    private static JFrame frame;
+    private static JDialog dialog;
+
+    public static void main(String[] args) throws Exception {
+        robot = new Robot();
+        robot.setAutoDelay(50); // without delay between key presses, 'switchToPreviousSpace'/'switchToNextSpace' don't always work
+        try {
+            SwingUtilities.invokeAndWait(FullScreenInactiveModalDialog::initUI);
+            shownAtFullScreen.get(5, TimeUnit.SECONDS);
+            switchToPreviousSpace();
+            SwingUtilities.invokeLater(FullScreenInactiveModalDialog::openDialog);
+            robot.delay(1000);
+            if (isWindowReallyShowing(dialog)) {
+                throw new RuntimeException("Dialog is showing earlier than expected");
+            }
+            switchToNextSpace();
+            if (!isWindowReallyShowing(dialog)) {
+                throw new RuntimeException("Dialog isn't showing when expected");
+            }
+        } finally {
+            SwingUtilities.invokeAndWait(FullScreenInactiveModalDialog::disposeUI);
+        }
+    }
+
+    private static void initUI() {
+        frame = new JFrame("FullScreenInactiveModalDialog");
+        frame.setSize(300, 100);
+        frame.setVisible(true);
+
+
+        FullScreenUtilities.addFullScreenListenerTo(frame, new FullScreenAdapter() {
+            @Override
+            public void windowEnteredFullScreen(FullScreenEvent e) {
+                shownAtFullScreen.complete(true);
+            }
+        });
+        Application.getApplication().requestToggleFullScreen(frame);
+    }
+
+    private static void openDialog() {
+        dialog = new JDialog(frame, true);
+        dialog.setSize(100, 100);
+        dialog.setVisible(true);
+    }
+
+    private static void disposeUI() {
+        if (frame != null) frame.dispose();
+    }
+
+    private static void switchToPreviousSpace() {
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_LEFT);
+        robot.keyRelease(KeyEvent.VK_LEFT);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        robot.delay(1000); // wait for animation to finish
+    }
+
+    private static void switchToNextSpace() {
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_RIGHT);
+        robot.keyRelease(KeyEvent.VK_RIGHT);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        robot.delay(1000); // wait for animation to finish
+    }
+
+    private static boolean isWindowReallyShowing(Window window) throws Exception {
+        Point[] location = new Point[1];
+        AtomicBoolean movementDetected = new AtomicBoolean();
+        SwingUtilities.invokeAndWait(() -> {
+            if (window.isVisible()) {
+                Rectangle bounds = window.getBounds();
+                Insets insets = window.getInsets();
+                bounds.x += insets.left;
+                bounds.y += insets.top;
+                bounds.width -= insets.left + insets.right;
+                bounds.height -= insets.top + insets.bottom;
+                if (!bounds.isEmpty()) {
+                    location[0] = new Point(bounds.x + bounds.width / 2, bounds.y + bounds.height / 2);
+                    window.addMouseMotionListener(new MouseMotionAdapter() {
+                        @Override
+                        public void mouseMoved(MouseEvent e) {
+                            movementDetected.set(true);
+                        }
+                    });
+                }
+            }
+        });
+        Point target = location[0];
+        if (target == null) {
+            return false;
+        }
+        robot.mouseMove(target.x, target.y);
+        robot.delay(100);
+        robot.mouseMove(target.x + 1, target.y + 1);
+        robot.delay(1000);
+        return movementDetected.get();
+    }
+}
