aa420e1521a74d4b4ab03d38b114bb1dd758988b
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2021-07-16 16:33:08 +0300

    JBR-3611, JBR-3633, JBR-3666, JBR-3663, JBR-3671, JBR-3673, JBR-4181, JBR-4186, JBR-4893 Interoperability with macOS desktop spaces
    (cherry-picked from commits 43fdd6cd26e6365427bd346085ded41746b2f8dc, 75335543f282e4ed94bcd0bc3df16ddd68b840f9, a156c6b9bf3c612fcbde050be945152409d2de15, 9fdc75969b791cbd62433154c50cc848eec2520a, 1dcc612a81a39f1f4bbfbbdc8b3f5731ba16845a, 93588d073861f04dfe48f4bc8d311ec948a4462d, 94a3885bbe61f3258b00171d9402cc64f669c1f6, c040e05703935ebcbf01ca2e8c23509833204e2d, 67b6cd871fe7843c3b5823f6439e080a0a6193a2, 9040fd56cdbbaf2ede5cc7e7d8fb8d4d49b4c1b0, 6349b86b7fccc9f1022257b052989b253147e58e)
    
    (cherry picked from commit 4fb4d51b89d386da6bec581427f32fc6a4ad7569)

---
+++ test/jdk/jb/java/awt/Window/FullScreenInactiveModalDialog.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2000-2021 JetBrains s.r.o.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+import com.apple.eawt.Application;
+import com.apple.eawt.FullScreenAdapter;
+import com.apple.eawt.FullScreenUtilities;
+import com.apple.eawt.event.FullScreenEvent;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @test
+ * @summary Regression test for JBR-3633 Modal dialog is shown not at the same space as its parent
+ * @key headful
+ * @requires (os.family == "mac")
+ * @modules java.desktop/com.apple.eawt
+ *          java.desktop/com.apple.eawt.event
+ * @compile MacSpacesUtil.java
+ * @run main FullScreenInactiveModalDialog
+ */
+
+public class FullScreenInactiveModalDialog {
+    private static final CompletableFuture<Boolean> shownAtFullScreen = new CompletableFuture<>();
+
+    private static JFrame frame;
+    private static JDialog dialog;
+
+    public static void main(String[] args) throws Exception {
+        try {
+            SwingUtilities.invokeAndWait(FullScreenInactiveModalDialog::initUI);
+            shownAtFullScreen.get(5, TimeUnit.SECONDS);
+            MacSpacesUtil.switchToPreviousSpace();
+            SwingUtilities.invokeLater(FullScreenInactiveModalDialog::openDialog);
+            Thread.sleep(1000);
+            if (MacSpacesUtil.isWindowVisible(dialog)) {
+                throw new RuntimeException("Dialog is showing earlier than expected");
+            }
+            MacSpacesUtil.switchToNextSpace();
+            if (!MacSpacesUtil.isWindowVisible(dialog)) {
+                throw new RuntimeException("Dialog isn't showing when expected");
+            }
+        } finally {
+            SwingUtilities.invokeAndWait(FullScreenInactiveModalDialog::disposeUI);
+        }
+    }
+
+    private static void initUI() {
+        frame = new JFrame("FullScreenInactiveModalDialog");
+        frame.setSize(300, 100);
+        frame.setVisible(true);
+
+
+        FullScreenUtilities.addFullScreenListenerTo(frame, new FullScreenAdapter() {
+            @Override
+            public void windowEnteredFullScreen(FullScreenEvent e) {
+                shownAtFullScreen.complete(true);
+            }
+        });
+        Application.getApplication().requestToggleFullScreen(frame);
+    }
+
+    private static void openDialog() {
+        dialog = new JDialog(frame, true);
+        dialog.setSize(100, 100);
+        dialog.setVisible(true);
+    }
+
+    private static void disposeUI() {
+        if (frame != null) frame.dispose();
+    }
+}
