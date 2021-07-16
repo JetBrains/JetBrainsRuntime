7987b993c39522e91b8f3e79adcf38ac2c61eff0
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2021-07-16 16:33:08 +0300

    JBR-3611, JBR-3633, JBR-3666, JBR-3663, JBR-3671, JBR-3673, JBR-4181, JBR-4186 Interoperability with macOS desktop spaces
    (cherry-picked from commits 43fdd6cd26e6365427bd346085ded41746b2f8dc, 75335543f282e4ed94bcd0bc3df16ddd68b840f9, a156c6b9bf3c612fcbde050be945152409d2de15, 9fdc75969b791cbd62433154c50cc848eec2520a, 1dcc612a81a39f1f4bbfbbdc8b3f5731ba16845a, 93588d073861f04dfe48f4bc8d311ec948a4462d, 94a3885bbe61f3258b00171d9402cc64f669c1f6, c040e05703935ebcbf01ca2e8c23509833204e2d, 67b6cd871fe7843c3b5823f6439e080a0a6193a2, 9040fd56cdbbaf2ede5cc7e7d8fb8d4d49b4c1b0)

---
+++ test/jdk/jb/java/awt/Window/FullScreenChildWindowShownBefore.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2021 JetBrains s.r.o.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+import com.apple.eawt.Application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @test
+ * @summary Regression test for JBR-3666 Child window stays on default space
+ *          when full-screen mode is activated for parent window on macOS
+ * @key headful
+ * @requires (os.family == "mac")
+ * @modules java.desktop/com.apple.eawt
+ * @compile MacSpacesUtil.java
+ * @run main FullScreenChildWindowShownBefore
+ */
+
+public class FullScreenChildWindowShownBefore {
+    private static final CompletableFuture<Boolean> dialogShown = new CompletableFuture<>();
+
+    private static JFrame frame;
+    private static JDialog dialog;
+
+    public static void main(String[] args) throws Exception {
+        try {
+            SwingUtilities.invokeAndWait(FullScreenChildWindowShownBefore::initUI);
+            dialogShown.get(5, TimeUnit.SECONDS);
+            SwingUtilities.invokeAndWait(() -> Application.getApplication().requestToggleFullScreen(frame));
+            Thread.sleep(1000); // wait for transition to full screen to finish
+            ensureVisible(frame, 250, 150);
+            ensureVisible(dialog, 250, 250);
+            SwingUtilities.invokeAndWait(() -> Application.getApplication().requestToggleFullScreen(frame));
+            Thread.sleep(1000); // wait for transition from full screen to finish
+            ensureVisible(frame, 250, 150);
+            ensureVisible(dialog, 250, 250);
+        } finally {
+            SwingUtilities.invokeAndWait(FullScreenChildWindowShownBefore::disposeUI);
+        }
+    }
+
+    private static void initUI() {
+        frame = new JFrame("FullScreenChildWindowShownBefore");
+        frame.setBounds(100, 100, 300, 300);
+        frame.setVisible(true);
+
+        dialog = new JDialog(frame, false);
+        dialog.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                dialogShown.complete(true);
+            }
+        });
+        dialog.setBounds(200, 200, 100, 100);
+        dialog.setVisible(true);
+    }
+
+    private static void disposeUI() {
+        if (frame != null) frame.dispose();
+    }
+
+    private static void ensureVisible(Window window, int x, int y) throws Exception {
+        if (!MacSpacesUtil.isWindowVisibleAtPoint(window, x, y)) throw new RuntimeException(window + " isn't visible");
+    }
+}
