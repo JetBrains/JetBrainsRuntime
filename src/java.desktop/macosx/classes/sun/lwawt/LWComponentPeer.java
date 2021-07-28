3c36d39239bb882f14378315e9ecf255415b9b28
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2021-07-28 18:14:57 +0300

    JBR-3642 java/awt/Window/8159168/SetShapeTest.java fails on macOS-x64 & macOS-aarch64
    make sure jb/java/awt/Focus/Typeahead* tests still pass
    
    includes fixes for JBR-3786 javax/swing/plaf/aqua/CustomComboBoxFocusTest.java fails on MacOS by timeout
    (cherry picked from commit f5c5388fb5ff44c09359a70bdcb0fbc4d6594fef)
    
    and JBR-4113 java/awt/KeyboardFocusmanager/TypeAhead/TestDialogTypeAhead.java fails by time out on macOS
    
    (cherry picked from commit d8d4c55a61bd9e5e05efb47b370be15fa870a68e)

--- src/java.desktop/macosx/classes/sun/lwawt/LWComponentPeer.java
+++ src/java.desktop/macosx/classes/sun/lwawt/LWComponentPeer.java
@@ -930,7 +930,7 @@ public abstract class LWComponentPeer<T extends Component, D extends JComponent>
 
         int result = LWKeyboardFocusManagerPeer.shouldNativelyFocusHeavyweight(
                 getTarget(), lightweightChild, temporary,
-                focusedWindowChangeAllowed, time, cause, false);
+                focusedWindowChangeAllowed, time, cause, true);
         switch (result) {
             case LWKeyboardFocusManagerPeer.SNFH_FAILURE:
                 return false;
@@ -984,7 +984,7 @@ public abstract class LWComponentPeer<T extends Component, D extends JComponent>
                 KeyboardFocusManagerPeer kfmPeer = LWKeyboardFocusManagerPeer.getInstance();
                 Component focusOwner = kfmPeer.getCurrentFocusOwner();
                 return LWKeyboardFocusManagerPeer.deliverFocus(lightweightChild,
-                        getTarget(), false, cause, focusOwner);
+                        getTarget(), true, cause, focusOwner);
 
             case LWKeyboardFocusManagerPeer.SNFH_SUCCESS_HANDLED:
                 return true;
