ef9c18347f19bbe5acd85059f08031a69067d8e5
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2021-07-28 18:14:57 +0300

    JBR-3642 java/awt/Window/8159168/SetShapeTest.java fails on macOS-x64 & macOS-aarch64
    make sure jb/java/awt/Focus/Typeahead* tests still pass
    
    includes fixes for JBR-3786 javax/swing/plaf/aqua/CustomComboBoxFocusTest.java fails on MacOS by timeout
    (cherry picked from commit f5c5388fb5ff44c09359a70bdcb0fbc4d6594fef)
    
    and JBR-4113 java/awt/KeyboardFocusmanager/TypeAhead/TestDialogTypeAhead.java fails by time out on macOS
    
    (cherry picked from commit d8d4c55a61bd9e5e05efb47b370be15fa870a68e)

--- src/java.desktop/macosx/classes/sun/lwawt/LWWindowPeer.java
+++ src/java.desktop/macosx/classes/sun/lwawt/LWWindowPeer.java
@@ -1359,7 +1359,7 @@ public class LWWindowPeer
         WindowEvent windowEvent = new TimedWindowEvent(getTarget(), eventID, opposite, System.currentTimeMillis());
 
         // TODO: wrap in SequencedEvent
-        postEvent(windowEvent);
+        SunToolkit.postPriorityEvent(windowEvent);
     }
 
     /*
