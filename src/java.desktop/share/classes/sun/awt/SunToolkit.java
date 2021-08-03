a9da2213cb1f8aa5f4af23ac75d105e271764131
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2021-08-03 07:32:58 -0700

    JBR-3664 Logging for communications with X server
    Introduced logging controlled with -Dsun.awt.x11.trace.
    Currently, only looks at the AWT lock and reports methods holding it
    sorted by average hold time.
    
    (based on commit 792a58ea0eed5d3053d6a475dc298b82204f0c74)
    (based on commit 770b4dc9c120e5a26e21c0080f92c3cfa228e315)

--- src/java.desktop/share/classes/sun/awt/SunToolkit.java
+++ src/java.desktop/share/classes/sun/awt/SunToolkit.java
@@ -234,15 +234,38 @@ public abstract class SunToolkit extends Toolkit
     private static final ReentrantLock AWT_LOCK = new ReentrantLock();
     private static final Condition AWT_LOCK_COND = AWT_LOCK.newCondition();
 
+    public interface AwtLockListener {
+        void afterAwtLocked();
+        void beforeAwtUnlocked();
+    }
+
+    private static java.util.List<AwtLockListener> awtLockListeners;
+    public static synchronized void addAwtLockListener(AwtLockListener l) {
+        if (awtLockListeners == null) {
+             awtLockListeners = Collections.synchronizedList(new ArrayList<>());
+        }
+        awtLockListeners.add(l);
+    }
+
     public static final void awtLock() {
         AWT_LOCK.lock();
+        if (awtLockListeners != null) {
+            awtLockListeners.forEach(AwtLockListener::afterAwtLocked);
+        }
     }
 
     public static final boolean awtTryLock() {
-        return AWT_LOCK.tryLock();
+        final boolean wasLocked = AWT_LOCK.tryLock();
+        if (wasLocked && awtLockListeners != null) {
+            awtLockListeners.forEach(AwtLockListener::afterAwtLocked);
+        }
+        return wasLocked;
     }
 
     public static final void awtUnlock() {
+        if (awtLockListeners != null) {
+            awtLockListeners.forEach(AwtLockListener::beforeAwtUnlocked);
+        }
         AWT_LOCK.unlock();
     }
 
