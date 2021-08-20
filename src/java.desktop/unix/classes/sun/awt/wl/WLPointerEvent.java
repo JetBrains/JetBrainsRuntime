88bb9703abc08334e5eb0b60c2f239f50c95b707
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2021-08-21 00:01:49 +0400

    Initial version of WLToolkit and Vulkan support
    Co-authored-by: Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
    Co-authored-by: Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
    Co-authored-by: Maxim Kartashev <maxim.kartashev@jetbrains.com>
    (cherry picked from commit 2e26de3c459a99cac8d2facb222d42042544d95e)

---
+++ src/java.desktop/unix/classes/sun/awt/wl/WLPointerEvent.java
@@ -0,0 +1,268 @@
+/*
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, JetBrains s.r.o.. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package sun.awt.wl;
+
+import java.awt.event.InputEvent;
+import java.awt.event.MouseEvent;
+
+/**
+ * A wayland "pointer" (mouse/surface/trackpad) event received from the native code.
+ * It accumulates one or more events that occurred since the last time and gets sent
+ * when Wayland generates the "frame" event denoting some sort of finished state
+ * of the pointer input.
+ * Therefore, not all fields are valid at all times and the many 'hasXXX()' methods
+ * have to be consulted prior to calling any of the 'getXXX()' ones.
+ * For example, the event with only 'has_leave_event' will not have 'surface_x'
+ * or 'timestamp' set.
+ * The unset fields are initialized to their default values (0 or false).
+ * Refer to wayland.xml for the full documentation.
+ */
+class WLPointerEvent {
+    private boolean has_enter_event;
+    private boolean has_leave_event;
+    private boolean has_motion_event;
+    private boolean has_button_event;
+    private boolean has_axis_event;
+
+    private long    surface; /// 'struct wl_surface *' this event appertains to
+    private long    serial;
+    private long    timestamp;
+
+    private int     surface_x;
+    private int     surface_y;
+
+    private int     buttonCode; // pointer button code corresponding to PointerButtonCodes.linuxCode
+    private boolean isButtonPressed; // true if button was pressed, false if released
+
+    private boolean axis_0_valid; // is vertical scroll included in this event?
+    private int     axis_0_value; // "length of vector in surface-local coordinate space" (source: wayland.xml)
+
+    private WLPointerEvent() {}
+
+    static WLPointerEvent newInstance() {
+        // Invoked from the native code
+        return new WLPointerEvent();
+    }
+
+    public enum PointerButtonCodes {
+        // see <linux/input-event-codes.h>
+        LEFT(0x110, MouseEvent.BUTTON1, InputEvent.BUTTON1_DOWN_MASK),
+        MIDDLE(0x112, MouseEvent.BUTTON2, InputEvent.BUTTON2_DOWN_MASK),
+        RIGHT(0x111, MouseEvent.BUTTON3, InputEvent.BUTTON3_DOWN_MASK);
+
+        public final int linuxCode; // The code from <linux/input-event-codes.h>
+        public final int javaCode;  // The code from MouseEvents.BUTTONx
+        public final int javaMask;  // The mask from InputEvent.BUTTONx_DOWN_MASK
+
+        PointerButtonCodes(int linuxCode, int javaCode, int javaMask) {
+            this.linuxCode = linuxCode;
+            this.javaCode  = javaCode;
+            this.javaMask  = javaMask;
+        }
+
+        static PointerButtonCodes recognizedOrNull(int linuxCode) {
+            for (var e : values()) {
+                if (e.linuxCode == linuxCode) {
+                    return e;
+                }
+            }
+            return null;
+        }
+
+        static boolean anyMatchMask(int mask) {
+            for (var c : values()) {
+                if ((mask & c.javaMask) != 0) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static int combinedMask() {
+            int mask = 0;
+            for (var c : values()) {
+                mask |= c.javaMask;
+            }
+            return mask;
+        }
+
+        public String toString() {
+            return switch (this) {
+                case LEFT   -> "left";
+                case MIDDLE -> "middle";
+                case RIGHT  -> "right";
+            };
+        }
+
+        public boolean isPopupTrigger() {
+            // TODO: this should probably be configurable for left- and right-handed?
+            return this == RIGHT;
+        }
+    }
+
+    public boolean hasEnterEvent() {
+        return has_enter_event;
+    }
+
+    public boolean hasLeaveEvent() {
+        return has_leave_event;
+    }
+
+    public boolean hasMotionEvent() {
+        return has_motion_event;
+    }
+
+    public boolean hasButtonEvent() {
+        return has_button_event;
+    }
+
+    public boolean hasAxisEvent() {
+        return has_axis_event;
+    }
+
+    /**
+     * @return true if this event's field 'surface' is valid.
+     */
+    public boolean hasSurface() {
+        return hasEnterEvent() || hasLeaveEvent();
+    }
+
+    /**
+     * @return true if this event's field 'serial' is valid.
+     */
+    public boolean hasSerial() {
+        return hasEnterEvent() || hasLeaveEvent() || hasButtonEvent();
+    }
+
+    /**
+     * @return true if this event's field 'timestamp' is valid.
+     */
+    public boolean hasTimestamp() {
+        return hasMotionEvent() || hasButtonEvent();
+    }
+
+    /**
+     * @return true if this event's fields 'surface_x' and 'surface_y' are valid.
+     */
+    public boolean hasCoordinates() {
+        return hasMotionEvent() || hasEnterEvent();
+    }
+
+    public long getSurface() {
+        assert hasSurface();
+        return surface;
+    }
+
+    public long getSerial() {
+        assert hasSerial();
+        return serial;
+    }
+
+    public long getTimestamp() {
+        assert hasTimestamp();
+        return timestamp;
+    }
+
+    public int getSurfaceX() {
+        assert hasCoordinates();
+        return surface_x;
+    }
+
+    public int getSurfaceY() {
+        assert hasCoordinates();
+        return surface_y;
+    }
+
+    public int getButtonCode() {
+        assert hasButtonEvent();
+        return buttonCode;
+    }
+
+    public boolean getIsButtonPressed() {
+        assert hasButtonEvent();
+        return isButtonPressed;
+    }
+
+    public boolean getIsAxis0Valid() {
+        assert hasAxisEvent();
+        return axis_0_valid;
+    }
+
+    public int getAxis0Value() {
+        assert hasAxisEvent();
+        return axis_0_value;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("WLPointerEvent(");
+        if (hasSurface()) {
+            builder.append("surface: 0x").append(Long.toHexString(getSurface()));
+        }
+
+        if (hasSerial()) {
+            builder.append(", serial: ").append(getSerial());
+        }
+
+        if (hasTimestamp()) {
+            builder.append(", timestamp: ").append(getTimestamp());
+        }
+
+        if (hasEnterEvent()) builder.append(" enter");
+        if (hasLeaveEvent()) builder.append(" leave");
+
+        if (hasMotionEvent()) {
+            builder.append(" motion ");
+            builder.append("x: ").append(getSurfaceX()).append(", y: ").append(getSurfaceY());
+
+        }
+        if (hasButtonEvent()) {
+            builder.append(" button ");
+
+            builder.append(buttonCodeToString(getButtonCode())).append(" ");
+            builder.append(getIsButtonPressed() ? "pressed" : "released");
+        }
+
+        if (hasAxisEvent()) {
+            builder.append("axis");
+            if (axis_0_valid) {
+                builder.append("vertical-scroll: ").append(axis_0_value).append(" ");
+            }
+        }
+
+        builder.append(")");
+
+        return builder.toString();
+    }
+
+    private static String buttonCodeToString(int code) {
+        final PointerButtonCodes recognizedCode = PointerButtonCodes.recognizedOrNull(code);
+        return String.valueOf(recognizedCode);
+    }
+}
