51e2c8f9ffae8f989e0a736b90188d56801e8dbc
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2021-08-21 00:01:49 +0400

    Initial version of WLToolkit and Vulkan support
    Co-authored-by: Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
    Co-authored-by: Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
    Co-authored-by: Maxim Kartashev <maxim.kartashev@jetbrains.com>
    (cherry picked from commit 2e26de3c459a99cac8d2facb222d42042544d95e)

---
+++ src/java.desktop/unix/classes/sun/awt/wl/WLButtonPeer.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, JetBrains s.r.o.. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package sun.awt.wl;
+
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.FocusEvent;
+import java.awt.peer.ButtonPeer;
+import sun.util.logging.PlatformLogger;
+
+public class WLButtonPeer extends WLComponentPeer implements ButtonPeer {
+    private static final PlatformLogger log = PlatformLogger.getLogger("sun.awt.wl.WLButtonPeer");
+    String label;
+
+
+    public WLButtonPeer(Button target) {
+        super(target);
+        label = target.getLabel();
+    }
+
+    public boolean isFocusable() {
+        return true;
+    }
+
+    @Override
+    public void setLabel(String label) {
+        log.info("Not implemented: WLButtonPeer.setLabel(String)");
+    }
+
+    public void setBackground(Color c) {
+        super.setBackground(c);
+    }
+
+    public void focusGained(FocusEvent e) {
+        super.focusGained(e);
+        log.info("Not implemented: WLButtonPeer.focusGained(FocusEvent)");
+    }
+
+    public void focusLost(FocusEvent e) {
+        super.focusLost(e);
+        log.info("Not implemented: WLButtonPeer.focusLost(FocusEvent)");
+    }
+
+    @Override
+    void paintPeer(Graphics g) {
+        super.paintPeer(g);
+        log.info("Not implemented: WLButtonPeer.paintPeer(Graphics)");
+    }
+
+    public Dimension getMinimumSize() {
+        log.info("Not implemented: WLButtonPeer.getMinimumSize()");
+        return new Dimension(0, 0);
+    }
+}
