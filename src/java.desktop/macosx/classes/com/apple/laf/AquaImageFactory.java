438d9ac57d24deb7df4d94a4e505676a2a234a75
Martin Doerr <mdoerr@openjdk.org>
2021-09-02 14:43:10 +0000

    8251377: [macos11] JTabbedPane selected tab text is barely legible
    Reviewed-by: shade
    Backport-of: 3f67afd3a8c642f33ca58a9881dff4c81f83e821

--- src/java.desktop/macosx/classes/com/apple/laf/AquaImageFactory.java
+++ src/java.desktop/macosx/classes/com/apple/laf/AquaImageFactory.java
@@ -497,6 +497,10 @@ public class AquaImageFactory {
         return new SystemColorProxy(LWCToolkit.getAppleColor(LWCToolkit.INACTIVE_SELECTION_FOREGROUND_COLOR));
     }
 
+    public static Color getSelectedControlColorUIResource() {
+        return new SystemColorProxy(LWCToolkit.getAppleColor(LWCToolkit.SELECTED_CONTROL_TEXT_COLOR));
+    }
+
     private static class EmptyImage {
         static final BufferedImage INSTANCE;
         static {
