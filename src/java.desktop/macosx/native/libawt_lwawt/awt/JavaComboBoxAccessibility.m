9ceee497ef1c1d4b5fc04b96afce0521fc1b62f9
Artem Semenov <Artem.Semenov@jetbrains.com>
2021-09-09 19:59:33 +0300

    JBR-3761 : Native call to non-existent getAccessibleSelection method dl - [JavaComboboxAccessibility accessibilityValue]

--- src/java.desktop/macosx/native/libawt_lwawt/awt/JavaComboBoxAccessibility.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/JavaComboBoxAccessibility.m
@@ -1,12 +1,23 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 #import "JavaComboBoxAccessibility.h"
-#import "JavaAccessibilityAction.h"
 #import "JavaAccessibilityUtilities.h"
 #import "ThreadUtilities.h"
 #import "JNIUtilities.h"
-#import <JavaNativeFoundation/JavaNativeFoundation.h>
 
+static jclass sjc_CAccessibility = NULL;
+
+static jmethodID sjm_getAccessibleName = NULL;
+#define GET_ACCESSIBLENAME_METHOD_RETURN(ret) \
+    GET_CACCESSIBILITY_CLASS_RETURN(ret); \
+    GET_STATIC_METHOD_RETURN(sjm_getAccessibleName, sjc_CAccessibility, "getAccessibleName", \
+                     "(Ljavax/accessibility/Accessible;Ljava/awt/Component;)Ljava/lang/String;", ret);
+
+static jmethodID sjm_getAccessibleSelection = NULL;
+#define GET_ACCESSIBLESELECTION_METHOD_RETURN(ret) \
+    GET_CACCESSIBILITY_CLASS_RETURN(ret); \
+    GET_STATIC_METHOD_RETURN(sjm_getAccessibleSelection, sjc_CAccessibility, "getAccessibleSelection", \
+                     "(Ljavax/accessibility/AccessibleContext;Ljava/awt/Component;)Ljavax/accessibility/AccessibleSelection;", ret);
 
 @implementation JavaComboBoxAccessibility
 
@@ -16,24 +27,28 @@
     JNIEnv *env = [ThreadUtilities getJNIEnv];
     jobject axContext = [self axContextWithEnv:env];
     if (axContext == NULL) return nil;
-    jclass cls = (*env)->GetObjectClass(env, axContext);
-    DECLARE_METHOD_RETURN(jm_getAccessibleSelection, cls, "getAccessibleSelection", "(I)Ljavax/accessibility/Accessible;", nil);
-    jobject axSelectedChild = (*env)->CallObjectMethod(env, axContext, jm_getAccessibleSelection, 0);
+    GET_ACCESSIBLESELECTION_METHOD_RETURN(nil);
+    jobject axSelection = (*env)->CallStaticObjectMethod(env, sjc_CAccessibility, sjm_getAccessibleSelection, axContext, self->fComponent);
+    CHECK_EXCEPTION();
+    if (axSelection == NULL) {
+        return nil;
+    }
+    jclass axSelectionClass = (*env)->GetObjectClass(env, axSelection);
+    DECLARE_METHOD_RETURN(jm_getAccessibleSelection, axSelectionClass, "getAccessibleSelection", "(I)Ljavax/accessibility/Accessible;", nil);
+    jobject axSelectedChild = (*env)->CallObjectMethod(env, axSelection, jm_getAccessibleSelection, 0);
     CHECK_EXCEPTION();
     (*env)->DeleteLocalRef(env, axContext);
     if (axSelectedChild == NULL) {
         return nil;
     }
-    DECLARE_CLASS_RETURN(sjc_CAccessibility, "sun/lwawt/macosx/CAccessibility", nil);
-    DECLARE_STATIC_METHOD_RETURN(sjm_getAccessibleName, sjc_CAccessibility, "getAccessibleName",
-                          "(Ljavax/accessibility/Accessible;Ljava/awt/Component;)Ljava/lang/String;", nil);
+    GET_ACCESSIBLENAME_METHOD_RETURN(nil);
     jobject childName = (*env)->CallStaticObjectMethod(env, sjc_CAccessibility, sjm_getAccessibleName, axSelectedChild, fComponent);
     CHECK_EXCEPTION();
     if (childName == NULL) {
         (*env)->DeleteLocalRef(env, axSelectedChild);
         return nil;
     }
-    NSString *selectedText = JNIObjectToNSString(env, childName);
+    NSString *selectedText = JavaStringToNSString(env, childName);
     (*env)->DeleteLocalRef(env, axSelectedChild);
     (*env)->DeleteLocalRef(env, childName);
     return selectedText;
