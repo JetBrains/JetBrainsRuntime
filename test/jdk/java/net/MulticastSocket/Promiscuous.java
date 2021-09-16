777039b1e6a243d005d49d29e1d6d92aa6941daf
Jonathan Dowland <jdowland@openjdk.org>
2021-09-16 15:25:31 +0000

    8246114: java/net/MulticastSocket/Promiscuous.java fails after 8241072 (multi-homed systems)
    Fixed the test - an IPv4 group cannot be joined from an interface that has no IPv4 address configured
    
    Backport-of: 0f43de9f026d5405dd7a38799e499312ef8df890

--- test/jdk/java/net/MulticastSocket/Promiscuous.java
+++ test/jdk/java/net/MulticastSocket/Promiscuous.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -117,7 +117,7 @@ public class Promiscuous {
 
             // join groups on all network interfaces
             NetworkConfiguration.probe()
-                    .multicastInterfaces(false)
+                    .ip4MulticastInterfaces(false)
                     .forEach((nic) -> {
                 try {
                     mc1.joinGroup(toSocketAddress(group1), nic);
@@ -153,7 +153,7 @@ public class Promiscuous {
 
             // leave groups on all network interfaces
             NetworkConfiguration.probe()
-                    .multicastInterfaces(false)
+                    .ip4MulticastInterfaces(false)
                     .forEach((nic) -> {
                 try {
                     mc1.leaveGroup(toSocketAddress(group1), nic);
