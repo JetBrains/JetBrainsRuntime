593b3512b9aa25aedbeea31032f718134d3f6d91
Martin Doerr <mdoerr@openjdk.org>
2021-09-20 15:44:59 +0000

    8216318: The usage of Disposer in the java.awt.Robot can be deleted
    Reviewed-by: goetz
    Backport-of: 293f77f18f96fec80d9dd5cc082d045b5dd0b813

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CRobot.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CRobot.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -59,10 +59,6 @@ final class CRobot implements RobotPeer {
             safeDelayMillis = DEFAULT_SAFE_DELAY_MILLIS;
         }
         initRobot(safeDelayMillis);
-    }
-
-    @Override
-    public void dispose() {
     }
 
     /**
