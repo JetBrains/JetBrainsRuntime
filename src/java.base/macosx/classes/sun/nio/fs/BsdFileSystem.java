1779795eff0128b0d99d0d43a69ef663d8bc6e5a
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2021-10-09 13:46:20 +0300

    JBR-3862 Implement native WatchService on MacOS
    The watch service is based on FSEvents API that notifies about file
    system changes at a directory level. It is possible to go back to
    using the old polling watch service with -Dwatch.service.polling=true.
    
    Features include:
    - support for FILE_TREE option (recursive directory watching),
    - minimum necessary I/O (no filesystem access more than once
      unless needed),
    - one thread ("run loop") per WatchService instance,
    - changes are detected by comparing file modification times with
      millisecond precision,
    - a directory tree snapshot is taken at the time of WatchKey creation
      and can take a long time (proportional to the number of files).

--- src/java.base/macosx/classes/sun/nio/fs/BsdFileSystem.java
+++ src/java.base/macosx/classes/sun/nio/fs/BsdFileSystem.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,8 +28,6 @@ package sun.nio.fs;
 import java.nio.file.*;
 import java.io.IOException;
 import java.util.*;
-import java.security.AccessController;
-import sun.security.action.GetPropertyAction;
 
 /**
  * Bsd implementation of FileSystem
@@ -45,8 +43,8 @@ class BsdFileSystem extends UnixFileSystem {
     public WatchService newWatchService()
         throws IOException
     {
-        // use polling implementation until we implement a BSD/kqueue one
-        return new PollingWatchService();
+        final boolean usePollingWatchService = Boolean.getBoolean("watch.service.polling");
+        return usePollingWatchService ? new PollingWatchService() : new MacOSXWatchService();
     }
 
     // lazy initialization of the list of supported attribute views
