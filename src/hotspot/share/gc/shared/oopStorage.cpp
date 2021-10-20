eb03bf7804417db57e27802cc9479255e54dc429
Vladimir Kempik <vkempik@openjdk.org>
2021-10-20 06:12:26 +0000

    8261075: Create stubRoutines.inline.hpp with SafeFetch implementation
    Reviewed-by: phh, mdoerr
    Backport-of: b955f85e03bafe8ce39677d0af06bf1ceb7e2cbb

--- src/hotspot/share/gc/shared/oopStorage.cpp
+++ src/hotspot/share/gc/shared/oopStorage.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -34,8 +34,8 @@
 #include "runtime/mutex.hpp"
 #include "runtime/mutexLocker.hpp"
 #include "runtime/orderAccess.hpp"
+#include "runtime/safefetch.hpp"
 #include "runtime/safepoint.hpp"
-#include "runtime/stubRoutines.hpp"
 #include "runtime/thread.hpp"
 #include "utilities/align.hpp"
 #include "utilities/count_trailing_zeros.hpp"
