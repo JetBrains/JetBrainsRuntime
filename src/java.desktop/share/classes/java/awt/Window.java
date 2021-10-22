f6c40106e36b0d53616db2f7eb0298eb0ddd227c
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2021-10-22 23:13:32 +0200

    JBR-3901 jbr-dev compile problem
    Added transient keyword
    
    (cherry picked from commit 8bd487eca9f27177f0493b7e517fd1508866ffb8)

--- src/java.desktop/share/classes/java/awt/Window.java
+++ src/java.desktop/share/classes/java/awt/Window.java
@@ -3993,7 +3993,7 @@ public class Window extends Container implements Accessible {
         hasTabbingMode = true;
     }
 
-    private volatile Runnable moveTabToNewWindowCallback;
+    private transient volatile Runnable moveTabToNewWindowCallback;
 
     void runMoveTabToNewWindowCallback() {
         if (moveTabToNewWindowCallback != null) {
