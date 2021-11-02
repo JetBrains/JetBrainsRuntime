e36cd2a55b42c6a423dced1ad2363cfe25c1ade9
Nikita Gubarkov <Nikita.Gubarkov@jetbrains.com>
2021-11-02 14:40:41 +0300

    JBR-2917 Added emoji support for Windows JBR-3951 Pass real glyph type from native code instead of guessing it by rowBytes & width
    (cherry picked from commit 1a7a1db7989fecf4b94e75088b04fa750dd9d3ba)

--- src/java.desktop/unix/classes/sun/font/XRGlyphCacheEntry.java
+++ src/java.desktop/unix/classes/sun/font/XRGlyphCacheEntry.java
@@ -192,13 +192,11 @@ public final class XRGlyphCacheEntry {
     }
 
     public Type getType() {
-        int rowBytes = getSourceRowBytes();
-        int width = getWidth();
-        // 0x0 -> LCD is just for backward compatibiity
-        if (width == 0 || getHeight() == 0) return Type.LCD;
-        if (width == rowBytes) return Type.GRAYSCALE;
-        if (width * 4 == rowBytes) return Type.BGRA;
-        return Type.LCD;
+        byte format = StrikeCache.getGlyphFormat(glyphInfoPtr);
+        if (format == StrikeCache.PIXEL_FORMAT_GREYSCALE) return Type.GRAYSCALE;
+        else if (format == StrikeCache.PIXEL_FORMAT_LCD) return Type.LCD;
+        else if (format == StrikeCache.PIXEL_FORMAT_BGRA) return Type.BGRA;
+        else throw new IllegalStateException("Unknown glyph format: " + format);
     }
 
     public int getPaddedWidth() {
