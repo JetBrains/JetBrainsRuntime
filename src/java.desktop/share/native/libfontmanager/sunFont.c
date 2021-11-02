be46c4264028cf7940df53520a58140f2da1921b
Nikita Gubarkov <Nikita.Gubarkov@jetbrains.com>
2021-11-02 14:40:41 +0300

    JBR-2917 Added emoji support for Windows JBR-3951 Pass real glyph type from native code instead of guessing it by rowBytes & width

--- src/java.desktop/share/native/libfontmanager/sunFont.c
+++ src/java.desktop/share/native/libfontmanager/sunFont.c
@@ -308,7 +308,7 @@ Java_sun_font_StrikeCache_getGlyphCacheDescription
     GlyphInfo *info;
     size_t baseAddr;
 
-    if ((*env)->GetArrayLength(env, results) < 15) {
+    if ((*env)->GetArrayLength(env, results) < 16) {
         return;
     }
 
@@ -337,6 +337,7 @@ Java_sun_font_StrikeCache_getGlyphCacheDescription
     nresults[12] = (size_t)&(info->managed)-baseAddr;
     nresults[13] = (size_t)&(info->subpixelResolutionX)-baseAddr;
     nresults[14] = (size_t)&(info->subpixelResolutionY)-baseAddr;
+    nresults[15] = (size_t)&(info->format)-baseAddr;
 
     (*env)->ReleasePrimitiveArrayCritical(env, results, nresults, 0);
 }
