e8d18d4b8a221d7b3339f30d59ed994c369a7df3
Artem Semenov <Artem.Semenov@jetbrains.com>
2021-11-17 15:29:50 +0300

    JBR-3868 Combobox list is not voiced of VoiceOver

--- src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/ComboBoxAccessibility.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/ComboBoxAccessibility.m
@@ -43,9 +43,8 @@ static jmethodID sjm_getAccessibleSelection = NULL;
 
 @implementation ComboBoxAccessibility
 
-// NSAccessibilityElement protocol methods
-
-- (id)accessibilityValue {
+- (CommonComponentAccessibility *)accessibleSelection
+{
     JNIEnv *env = [ThreadUtilities getJNIEnv];
     jobject axContext = [self axContextWithEnv:env];
     if (axContext == NULL) return nil;
@@ -64,17 +63,19 @@ static jmethodID sjm_getAccessibleSelection = NULL;
     if (axSelectedChild == NULL) {
         return nil;
     }
-    GET_ACCESSIBLENAME_METHOD_RETURN(nil);
-    jobject childName = (*env)->CallStaticObjectMethod(env, sjc_CAccessibility, sjm_getAccessibleName, axSelectedChild, fComponent);
-    CHECK_EXCEPTION();
-    if (childName == NULL) {
-        (*env)->DeleteLocalRef(env, axSelectedChild);
-        return nil;
-    }
-    NSString *selectedText = JavaStringToNSString(env, childName);
-    (*env)->DeleteLocalRef(env, axSelectedChild);
-    (*env)->DeleteLocalRef(env, childName);
-    return selectedText;
+    return [CommonComponentAccessibility createWithAccessible:axSelectedChild withEnv:env withView:fView];
+}
+
+// NSAccessibilityElement protocol methods
+
+- (id)accessibilityValue
+{
+    return [[self accessibleSelection] accessibilityLabel];
+}
+
+- (NSArray *)accessibilitySelectedChildren
+{
+    return [NSArray arrayWithObject:[self accessibleSelection]];
 }
 
 @end
