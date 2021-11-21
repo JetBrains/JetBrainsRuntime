55557cff5a1c95dad76639205b954995ea877db6
Vladimir Dvorak <vladimir.dvorak@jetbrains.com>
2021-11-21 16:57:57 +0100

    JBR-4018 - fix zero variant compilation issues

--- src/hotspot/share/runtime/arguments.cpp
+++ src/hotspot/share/runtime/arguments.cpp
@@ -3967,12 +3967,14 @@ jint Arguments::parse(const JavaVMInitArgs* initial_cmd_args) {
   // Set object alignment values.
   set_object_alignment();
 
+#ifndef ZERO
   if (FlightRecorder) {
     if (AllowEnhancedClassRedefinition) {
       warning("EnhancedClassRedefinition was disabled, it is not allowed in FlightRecorder.");
       AllowEnhancedClassRedefinition = false;
     }
   }
+#endif
 
   setup_hotswap_agent();
 
