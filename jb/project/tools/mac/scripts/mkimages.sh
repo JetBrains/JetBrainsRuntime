42a22e0cc473febf87b528925184dd187cf20d76
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2021-12-03 10:48:58 +0700

    JBR-4082 create JBR & JBRSDK installer packages

--- jb/project/tools/mac/scripts/mkimages.sh
+++ jb/project/tools/mac/scripts/mkimages.sh
@@ -59,6 +59,8 @@ function do_configure {
       $WITH_DEBUG_LEVEL \
       --with-vendor-name="${VENDOR_NAME}" \
       --with-vendor-version-string="${VENDOR_VERSION_STRING}" \
+      --with-macosx-bundle-name-base=${VENDOR_VERSION_STRING} \
+      --with-macosx-bundle-id-base="com.jetbrains.jbr" \
       --with-jvm-features=shenandoahgc \
       --with-version-pre= \
       --with-version-build="${JDK_BUILD_NUMBER}" \
@@ -77,6 +79,8 @@ function do_configure {
       $WITH_DEBUG_LEVEL \
       --with-vendor-name="$VENDOR_NAME" \
       --with-vendor-version-string="$VENDOR_VERSION_STRING" \
+      --with-macosx-bundle-name-base=${VENDOR_VERSION_STRING} \
+      --with-macosx-bundle-id-base="com.jetbrains.jbr" \
       --with-jvm-features=shenandoahgc \
       --with-version-pre= \
       --with-version-build="$JDK_BUILD_NUMBER" \
