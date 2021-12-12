96b8e39a3c53f9d4349fb1f3b1eaa0b7a0b8f6b0
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2021-12-13 02:26:35 +0300

    JBR API v0.0.2
    Added file dialog customization via JBR API & fixed bugs in windows common item dialog
    
    with fix for JBR-5300 Change source code and test files to use GPL license

--- src/java.desktop/windows/native/libawt/windows/awt_FileDialog.h
+++ src/java.desktop/windows/native/libawt/windows/awt_FileDialog.h
@@ -56,10 +56,12 @@ public:
     static jfieldID dirID;
     static jfieldID fileID;
     static jfieldID filterID;
+    static jfieldID jbrDialogID;
+
+    /* com.jetbrains.desktop.JBRFileDialog field and method ids */
     static jfieldID openButtonTextID;
     static jfieldID selectFolderButtonTextID;
-    static jfieldID folderPickerModeID;
-    static jfieldID fileExclusivePickerModeID;
+    static jfieldID hintsID;
 
     static void Initialize(JNIEnv *env, jstring filterDescription);
     static void Show(void *peer);
