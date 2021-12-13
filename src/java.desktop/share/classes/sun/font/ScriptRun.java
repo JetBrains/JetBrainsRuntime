c785b3de0e79dbdb233c8d07c32e43c902e09eeb
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2021-12-13 12:20:59 +0300

    JBR-4114 Rollback JBR-10 fix

--- src/java.desktop/share/classes/sun/font/ScriptRun.java
+++ src/java.desktop/share/classes/sun/font/ScriptRun.java
@@ -43,7 +43,7 @@ package sun.font;
  * COMMON and INHERITED characters are first, they will be assigned to
  * the same script as the following characters.
  *
- * The iterator will try (optionally) to match paired punctuation. If it sees an
+ * The iterator will try to match paired punctuation. If it sees an
  * opening punctuation character, it will remember the script that
  * was assigned to that character, and assign the same script to the
  * matching closing punctuation.
@@ -83,8 +83,6 @@ public final class ScriptRun
     private int stack[];         // stack used to handle paired punctuation if encountered
     private int parenSP;
 
-    private boolean handlePairedChars;
-
     public ScriptRun() {
         // must call init later or we die.
     }
@@ -102,11 +100,7 @@ public final class ScriptRun
         init(chars, start, count);
     }
 
-    public void init(char[] chars, int start, int count) {
-        init(chars, start, count, true);
-    }
-
-    public void init(char[] chars, int start, int count, boolean pairedChars)
+    public void init(char[] chars, int start, int count)
     {
         if (chars == null || start < 0 || count < 0 || count > chars.length - start) {
             throw new IllegalArgumentException();
@@ -120,7 +114,6 @@ public final class ScriptRun
         scriptLimit = textStart;
         scriptCode = Script.INVALID_CODE;
         parenSP = 0;
-        handlePairedChars = pairedChars;
     }
 
     /**
@@ -172,7 +165,7 @@ public final class ScriptRun
 
         while ((ch = nextCodePoint()) != DONE) {
             int sc = ScriptRunData.getScript(ch);
-            int pairIndex = handlePairedChars && sc == Script.COMMON ? getPairIndex(ch) : -1;
+            int pairIndex = sc == Script.COMMON ? getPairIndex(ch) : -1;
 
             // Paired character handling:
             //
