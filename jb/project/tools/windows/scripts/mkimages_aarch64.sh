50da29220f365d474304ea7e4ba40a1052f54258
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2021-12-14 09:00:03 +0700

    JBR-4053 integrate DCEVM patches as usual commits
    (cherry picked from commit 45672abcadf8d434bedd0ca6365ddf7a3e545671)

--- jb/project/tools/windows/scripts/mkimages_aarch64.sh
+++ jb/project/tools/windows/scripts/mkimages_aarch64.sh
@@ -70,12 +70,6 @@ case "$bundle_type" in
     do_reset_changes=0
     do_maketest=1
     ;;
-  "dcevm")
-    HEAD_REVISION=$(git rev-parse HEAD)
-    git am jb/project/tools/patches/dcevm/*.patch || do_exit $?
-    do_reset_dcevm=0
-    do_reset_changes=0
-    ;;
   "nomod" | "")
     bundle_type=""
     ;;
@@ -111,7 +105,7 @@ if [ $? -eq 0 ]; then
   JCEF_PATH="$(cygpath -w $JCEF_PATH | sed 's/\\/\//g')"
 fi
 
-if [ "$bundle_type" == "jcef" ] || [ "$bundle_type" == "dcevm" ] || [ "$bundle_type" == "fd" ]; then
+if [ "$bundle_type" == "jcef" ] || [ "$bundle_type" == "fd" ]; then
   git apply -p0 < jb/project/tools/patches/add_jcef_module_aarch64.patch || do_exit $?
   update_jsdk_mods "$BOOT_JDK" "$JCEF_PATH"/jmods "$JSDK"/jmods "$JSDK_MODS_DIR" || do_exit $?
   cp $JCEF_PATH/jmods/* $JSDK_MODS_DIR # $JSDK/jmods is not unchanged
@@ -126,7 +120,7 @@ create_image_bundle "jbr${jbr_name_postfix}" "jbr" $JSDK_MODS_DIR "$modules" || 
 
 # create sdk image bundle
 modules=$(cat ${JSDK}/release | grep MODULES | sed s/MODULES=//g | sed s/' '/','/g | sed s/\"//g | sed s/\\r//g | sed s/\\n//g) || do_exit $?
-if [ "$bundle_type" == "jcef" ] || [ "$bundle_type" == "dcevm" ] || [ "$bundle_type" == "fd" ] || [ "$bundle_type" == "$JBRSDK_BUNDLE" ]; then
+if [ "$bundle_type" == "jcef" ] || [ "$bundle_type" == "fd" ] || [ "$bundle_type" == "$JBRSDK_BUNDLE" ]; then
   modules=${modules},$(get_mods_list "$JCEF_PATH"/jmods)
 fi
 create_image_bundle "$JBRSDK_BUNDLE${jbr_name_postfix}" "$JBRSDK_BUNDLE" "$JSDK_MODS_DIR" "$modules" || do_exit $?
