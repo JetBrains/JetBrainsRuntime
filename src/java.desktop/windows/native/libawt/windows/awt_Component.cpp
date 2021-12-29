30e75a5005188a09f0d31bc3165ad8b1250d5c70
Sergey Bylokhov <serb@openjdk.org>
2021-12-29 23:28:12 +0000

    8270874: JFrame paint artifacts when dragged from standard monitor to HiDPI monitor
    Backport-of: 03473b4c271b2ec7f0ebdb0edabadf7f36816b9d

--- src/java.desktop/windows/native/libawt/windows/awt_Component.cpp
+++ src/java.desktop/windows/native/libawt/windows/awt_Component.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -4954,7 +4954,7 @@ int AwtComponent::ScaleDownAbsY(int y) {
 }
 
 void AwtComponent::ScaleDownRect(RECT& r) {
-    int screen = AwtWin32GraphicsDevice::DeviceIndexForWindow(GetHWnd());
+    int screen = GetScreenImOn();
     Devices::InstanceAccess devices;
     AwtWin32GraphicsDevice* device = devices->GetDevice(screen);
     if (device == NULL) return;
