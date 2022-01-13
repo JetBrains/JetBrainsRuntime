ce0e60216a3162353b65e82d3e873593ead9e8f1
Goetz Lindenmaier <goetz@openjdk.org>
2022-01-13 13:37:48 +0000

    8249019: clean up FileInstaller $test.src $cwd in vmTestbase_vm_compiler tests
    Backport-of: c5ad1daa96010cf648a3a267782ee481c060be12
    (cherry picked from commit ffbef528990e87d8d86a0714a7fd994621e7ac44)

--- test/hotspot/jtreg/vmTestbase/jit/FloatingPoint/gen_math/Loops02/Loops02.java
+++ test/hotspot/jtreg/vmTestbase/jit/FloatingPoint/gen_math/Loops02/Loops02.java
@@ -29,7 +29,6 @@
  *
  * @library /vmTestbase
  *          /test/lib
- * @run driver jdk.test.lib.FileInstaller . .
  * @build jit.FloatingPoint.gen_math.Loops02.Loops02
  * @run driver ExecDriver --java jit.FloatingPoint.gen_math.Loops02.Loops02
  */
