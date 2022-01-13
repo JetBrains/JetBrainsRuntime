samkenxstream/SAMkenxjetbrainsruntime

JBR-4154 extract version info from tags

jb/project/tools/common/scripts/common.sh
@@ -1,5 +1,39 @@
+#!/bin/bash -x
+
+tag_prefix="jdk-"
+
+while getopts ":i?" o; do
+    case "${o}" in
+        i)
+            i="incremental build"
+            INC_BUILD=1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ $# -le $min_parameters_count ]; then
+  build_number=$1
+  bundle_type=$2
+  architecture=$3 # aarch64 or x64
+else
+  JBSDK_VERSION=$1
+  JDK_BUILD_NUMBER=$2
+  build_number=$3
+  bundle_type=$4
+  architecture=$5 # aarch64 or x64
+fi
+
+#OPENJDK_TAG=$(git describe --tags --abbrev=0 | awk -F"-" '{ print $2 }')
+OPENJDK_TAG=$(git log --simplify-by-decoration --decorate=short --pretty=short | grep "$tag_prefix" | cut -d "(" -f2 | cut -d ")" -f1 | awk '{print $2}' | sort -t "-" -k 2 -g | tail -n 1)
+JBSDK_VERSION=${JBSDK_VERSION:=$(echo $OPENJDK_TAG | awk -F "-|[+]" '{print $2}')}
+echo "##teamcity[setParameter name='env.JBSDK_VERSION' value='${JBSDK_VERSION}']"
+JDK_BUILD_NUMBER=${JDK_BUILD_NUMBER:=$(echo $OPENJDK_TAG | awk -F "-|[+]" '{print $3}')}
+echo "##teamcity[setParameter name='env.JDK_BUILD_NUMBER' value='${JDK_BUILD_NUMBER}']"
+JBSDK_VERSION_WITH_DOTS=$(echo $JBSDK_VERSION | sed 's/_/\./g')
+
 VENDOR_NAME="JetBrains s.r.o."
-VENDOR_VERSION_STRING="JBR-${JBSDK_VERSION_WITH_DOTS}.${JDK_BUILD_NUMBER}-${build_number}"
+VENDOR_VERSION_STRING="JBR-${JBSDK_VERSION_WITH_DOTS}+${JDK_BUILD_NUMBER}-${build_number}"
 [ -z "$bundle_type" ] || VENDOR_VERSION_STRING="${VENDOR_VERSION_STRING}-${bundle_type}"
 
 do_reset_changes=0

JetBrains/JetBrainsRuntime

jb/project/tools/common/scripts/common.sh
@@ -1,5 +1,38 @@
+#!/bin/bash -x
+
+tag_prefix="jdk-"
+
+while getopts ":i?" o; do
+    case "${o}" in
+        i)
+            i="incremental build"
+            INC_BUILD=1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ $# -le $min_parameters_count ]; then
+  build_number=$1
+  bundle_type=$2
+  architecture=$3 # aarch64 or x64
+else
+  JBSDK_VERSION=$1
+  JDK_BUILD_NUMBER=$2
+  build_number=$3
+  bundle_type=$4
+  architecture=$5 # aarch64 or x64
+fi
+
+OPENJDK_TAG=$(git log --simplify-by-decoration --decorate=short --pretty=short | grep "$tag_prefix" | cut -d "(" -f2 | cut -d ")" -f1 | awk '{print $2}' | sort -t "-" -k 2 -g | tail -n 1)
+JBSDK_VERSION=${JBSDK_VERSION:=$(echo $OPENJDK_TAG | awk -F "-|[+]" '{print $2}')}
+echo "##teamcity[setParameter name='env.JBSDK_VERSION' value='${JBSDK_VERSION}']"
+JDK_BUILD_NUMBER=${JDK_BUILD_NUMBER:=$(echo $OPENJDK_TAG | awk -F "-|[+]" '{print $3}')}
+echo "##teamcity[setParameter name='env.JDK_UPDATE_NUMBER' value='${JDK_BUILD_NUMBER}']"
+JBSDK_VERSION_WITH_DOTS=$(echo $JBSDK_VERSION | sed 's/_/\./g')
+
 VENDOR_NAME="JetBrains s.r.o."
-VENDOR_VERSION_STRING="JBR-${JBSDK_VERSION_WITH_DOTS}.${JDK_BUILD_NUMBER}-${build_number}"
+VENDOR_VERSION_STRING="JBR-${JBSDK_VERSION_WITH_DOTS}+${JDK_BUILD_NUMBER}-${build_number}"
 [ -z "$bundle_type" ] || VENDOR_VERSION_STRING="${VENDOR_VERSION_STRING}-${bundle_type}"
 
 do_reset_changes=0