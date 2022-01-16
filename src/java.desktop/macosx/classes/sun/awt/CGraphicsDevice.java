7eaa648e23f7dd2706367c22d98a4aaf43701274
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2022-01-16 08:39:24 +0700

    Revert "JBR-4150 IDE regularly locks up at sun.lwawt.macosx.LWCToolkit.getScreenInsets"
    This reverts commit 2aeb7aeab41c9ef20bf93ee3752d65149cb740fd.

--- src/java.desktop/macosx/classes/sun/awt/CGraphicsDevice.java
+++ src/java.desktop/macosx/classes/sun/awt/CGraphicsDevice.java
@@ -56,7 +56,6 @@ public final class CGraphicsDevice extends GraphicsDevice
     private volatile double yResolution;
     private volatile Rectangle bounds;
     private volatile int scale;
-    private volatile Insets screenInsets;
 
     private GraphicsConfiguration config;
     private static boolean metalPipelineEnabled = false;
@@ -184,7 +183,13 @@ public final class CGraphicsDevice extends GraphicsDevice
     }
 
     public Insets getScreenInsets() {
-        return screenInsets;
+        // the insets are queried synchronously and are not cached
+        // since there are no Quartz or Cocoa means to receive notifications
+        // on insets changes (e.g. when the Dock is resized):
+        // the existing CGDisplayReconfigurationCallBack is not notified
+        // as well as the NSApplicationDidChangeScreenParametersNotification
+        // is fired on the Dock location changes only
+        return nativeGetScreenInsets(displayID);
     }
 
     public int getScaleFactor() {
@@ -207,7 +212,6 @@ public final class CGraphicsDevice extends GraphicsDevice
         xResolution = nativeGetXResolution(displayID);
         yResolution = nativeGetYResolution(displayID);
         bounds = nativeGetBounds(displayID).getBounds(); //does integer rounding
-        screenInsets = nativeGetScreenInsets(displayID);
         initScaleFactor();
         resizeFSWindow(getFullScreenWindow(), bounds);
         //TODO configs?
