4a13091f2b5cc8e4926d6634faf767a465642f08
greg <greg.kargin@gmail.com>
2022-01-26 22:23:22 +0300

    macOS: add methods to setup transparent titlebar with custom height (#100)
    * macOS: add methods to setup transparent titlebar with custom height
    
    * make windowTransparentTitleBarHeight CPlatfromWindow property
    
    * add windowTransparentTitleBarHeight test
    
    * Prevent mouseUp events on the transparent header on macOS when the window is being dragged
    
    Co-authored-by: Manuel Unterhofer <manuel.unterhofer@jetbrains.com>
    
    Custom macOS window decorations via JBR API
    
    JBR-4460 Fix window drag with custom decorations on macOS
    
    JBR-4553 Add logging to setUpTransparentTitleBar and resetTitleBar

--- src/java.desktop/share/classes/java/awt/Window.java
+++ src/java.desktop/share/classes/java/awt/Window.java
@@ -4045,6 +4045,8 @@ public class Window extends Container implements Accessible {
             window.hasCustomDecoration = enabled;
             if (Win.INSTANCE != null) {
                 Win.INSTANCE.updateCustomDecoration(window.peer);
+            } else if (MacOS.INSTANCE != null && window.customDecorTitleBarHeight > 0f) {
+                MacOS.INSTANCE.setTitleBarHeight(window, window.peer, enabled ? window.customDecorTitleBarHeight : 0);
             }
         }
         boolean isCustomDecorationEnabled(Window window) {
@@ -4059,7 +4061,12 @@ public class Window extends Container implements Accessible {
         }
 
         void setCustomDecorationTitleBarHeight(Window window, int height) {
-            if (height >= 0) window.customDecorTitleBarHeight = height;
+            if (height >= 0) {
+                window.customDecorTitleBarHeight = height;
+                if (MacOS.INSTANCE != null && window.hasCustomDecoration) {
+                    MacOS.INSTANCE.setTitleBarHeight(window, window.peer, height);
+                }
+            }
         }
         int getCustomDecorationTitleBarHeight(Window window) {
             return window.customDecorTitleBarHeight;
@@ -4069,6 +4076,12 @@ public class Window extends Container implements Accessible {
             Win INSTANCE = (Win) JBRApi.internalServiceBuilder(MethodHandles.lookup(), null)
                     .withStatic("updateCustomDecoration", "sun.awt.windows.WFramePeer").build();
             void updateCustomDecoration(ComponentPeer peer);
+        }
+
+        private interface MacOS {
+            MacOS INSTANCE = (MacOS) JBRApi.internalServiceBuilder(MethodHandles.lookup(), null)
+                    .withStatic("setTitleBarHeight", "sun.lwawt.macosx.CPlatformWindow", "setCustomDecorationTitleBarHeight").build();
+            void setTitleBarHeight(Window target, ComponentPeer peer, float height);
         }
     }
 
