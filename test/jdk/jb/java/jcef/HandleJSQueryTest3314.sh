ced125f975f822b45af6cbb2f6b7fca069eff707
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2022-01-27 04:40:34 +0700

    fixup! JBR-3337 jb/java/jcef/HandleJSQueryTest3314.sh: fails on macOS-aarch64 with "JS Query was not handled in 2nd opened browser"

--- test/jdk/jb/java/jcef/HandleJSQueryTest3314.sh
+++ test/jdk/jb/java/jcef/HandleJSQueryTest3314.sh
@@ -58,5 +58,5 @@ while [ "$i" -le "$RUNS_NUMBER" ]; do
     fi
     i=$(( i + 1 ))
 done
-echo "PASSED: Test did never crash during 100 iterations"
+echo "PASSED: Test did never crash during $RUNS_NUMBER iterations"
 exit 0
