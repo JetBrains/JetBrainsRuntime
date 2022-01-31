ae1780bcfbf9fa4e80ec3ebfbf05e4709d5d137c
Martin Balao <mbalao@openjdk.org>
2022-01-31 10:54:14 +0000

    8269938: Enhance XML processing passes redux
    Reviewed-by: mbaesken
    Backport-of: ff4e11b1a83d6910e39f1d46bea154f0876f39da

--- src/java.xml/share/classes/com/sun/org/apache/xml/internal/serializer/ToHTMLStream.java
+++ src/java.xml/share/classes/com/sun/org/apache/xml/internal/serializer/ToHTMLStream.java
@@ -30,6 +30,7 @@ import org.xml.sax.SAXException;
 
 import com.sun.org.apache.xml.internal.serializer.utils.MsgKey;
 import com.sun.org.apache.xml.internal.serializer.utils.Utils;
+import jdk.xml.internal.JdkXmlUtils;
 
 /**
  * This serializer takes a series of SAX or
@@ -40,6 +41,7 @@ import com.sun.org.apache.xml.internal.serializer.utils.Utils;
  * because it is used from another package.
  *
  * @xsl.usage internal
+ * @LastModified: July 2021
  */
 public final class ToHTMLStream extends ToStream
 {
@@ -674,28 +676,10 @@ public final class ToHTMLStream extends ToStream
                 final java.io.Writer writer = m_writer;
                 try
                 {
-                writer.write("<!DOCTYPE html");
-
-                if (null != doctypePublic)
-                {
-                    writer.write(" PUBLIC \"");
-                    writer.write(doctypePublic);
-                    writer.write('"');
-                }
-
-                if (null != doctypeSystem)
-                {
-                    if (null == doctypePublic)
-                        writer.write(" SYSTEM \"");
-                    else
-                        writer.write(" \"");
-
-                    writer.write(doctypeSystem);
-                    writer.write('"');
-                }
-
-                writer.write('>');
-                outputLineSep();
+                    writer.write("<!DOCTYPE html");
+                    writer.write(JdkXmlUtils.getDTDExternalDecl(doctypePublic, doctypeSystem));
+                    writer.write('>');
+                    outputLineSep();
                 }
                 catch(IOException e)
                 {
