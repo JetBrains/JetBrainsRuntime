6ae61d033c44919e706083798dbe57663914d2ad
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2022-02-17 11:11:39 +0300

    JBR-3917 Problem using windows certificate store (trustStoreType=Windows-ROOT not recognized)

--- jb/project/tools/windows/scripts/mkimages_x64.sh
+++ jb/project/tools/windows/scripts/mkimages_x64.sh
@@ -115,8 +115,9 @@ if [ "$bundle_type" == "jcef" ] || [ "$bundle_type" == "dcevm" ] || [ "$bundle_t
   jbr_name_postfix="_${bundle_type}"
 fi
 
-# create runtime image bundle
-modules=$(xargs < modules.list | sed s/" "//g) || do_exit $?
+# create runtime image bundlef
+modules=$(xargs < jb/project/tools/common/modules.list | sed s/" "//g) || do_exit $?
+modules+=",jdk.crypto.mscapi"
 create_image_bundle "jbr${jbr_name_postfix}" "jbr" $JSDK_MODS_DIR "$modules" || do_exit $?
 
 # create sdk image bundle
