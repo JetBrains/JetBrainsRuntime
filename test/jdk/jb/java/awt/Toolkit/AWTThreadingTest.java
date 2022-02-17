ab15ffd740d5cc400e6640c86fdb1cffda0a1b6a
Anton Tarasov <anton.tarasov@jetbrains.com>
2022-02-17 16:27:26 +0300

    JBR-4106 PyCharm hangs with 100% CPU usage on one core
    with fix for JBR-5300 Change source code and test files to use GPL license
    
    (cherry picked from commit 9deef18d46ffa02a3d6507ccbbf849c71e822196)

--- test/jdk/jb/java/awt/Toolkit/AWTThreadingTest.java
+++ test/jdk/jb/java/awt/Toolkit/AWTThreadingTest.java
@@ -1,139 +1,158 @@
+/*
+ * Copyright 2000-2023 JetBrains s.r.o.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
 import java.awt.*;
-import javax.swing.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
+import java.util.Arrays;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Supplier;
 
 import sun.lwawt.macosx.CThreading;
 import sun.lwawt.macosx.LWCToolkit;
 import sun.awt.AWTThreading;
 
+import static helper.ToolkitTestHelper.*;
+
 /*
  * @test
- * @summary tests that AWTThreading can manage a stream of cross EDT/AppKit invocation requests
+ * @summary tests that AWTThreading can manage cross EDT/AppKit blocking invocation requests
  * @requires (os.family == "mac")
- * @compile --add-exports=java.desktop/sun.lwawt.macosx=ALL-UNNAMED --add-exports=java.desktop/sun.awt=ALL-UNNAMED AWTThreadingTest.java
- * @run main/othervm --add-exports=java.desktop/sun.lwawt.macosx=ALL-UNNAMED --add-exports=java.desktop/sun.awt=ALL-UNNAMED AWTThreadingTest
+ * @modules java.desktop/sun.lwawt.macosx java.desktop/sun.awt
+ * @run main AWTThreadingTest
  * @author Anton Tarasov
  */
+@SuppressWarnings("ConstantConditions")
 public class AWTThreadingTest {
-    static final ReentrantLock LOCK = new ReentrantLock();
-    static final Condition COND = LOCK.newCondition();
-    static final CountDownLatch LATCH = new CountDownLatch(1);
+    static final int TIMEOUT_SECONDS = 1;
 
-    static JFrame frame;
-    static Thread thread;
+    static final AtomicInteger ITER_COUNTER = new AtomicInteger();
+    static final AtomicBoolean DUMP_STACK = new AtomicBoolean(false);
 
-    final static AtomicBoolean passed = new AtomicBoolean(true);
-    final static AtomicInteger counter = new AtomicInteger(0);
+    static volatile Thread THREAD;
 
-    public static void main(String[] args) throws InterruptedException {
-        EventQueue.invokeLater(AWTThreadingTest::runGui);
+    public static void main(String[] args) {
+        DUMP_STACK.set(args.length > 0 && "dumpStack".equals(args[0]));
 
-        LATCH.await(5, TimeUnit.SECONDS);
+        initTest(AWTThreadingTest.class);
 
-        frame.dispose();
-        thread.interrupt();
+        testCase("certain threads superposition", AWTThreadingTest::test);
 
-        if (!passed.get()) {
-            throw new RuntimeException("Test FAILED!");
-        }
+        testCase("random threads superposition", AWTThreadingTest::test);
+
         System.out.println("Test PASSED");
     }
 
-    static void runGui() {
-        frame = new JFrame("frame");
-        frame.getContentPane().setBackground(Color.green);
-        frame.setLocationRelativeTo(null);
-        frame.setSize(200, 200);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowOpened(WindowEvent e) {
-                startThread();
+    static void test() {
+        ITER_COUNTER.set(0);
+
+        var timer = new TestTimer(TIMEOUT_SECONDS * 3, TimeUnit.SECONDS);
+        EventQueue.invokeLater(() -> startThread(() ->
+            FUTURE.isDone() ||
+            timer.hasFinished()));
+
+        tryRun(() -> {
+            if (!FUTURE.get(TIMEOUT_SECONDS * 4, TimeUnit.SECONDS)) {
+                throw new RuntimeException("Test FAILED! (negative result)");
             }
         });
-        frame.setVisible(true);
+
+        tryRun(THREAD::join);
+
+        System.out.println(ITER_COUNTER + " iterations passed");
     }
 
-    static void startThread() {
-        thread = new Thread(() -> {
-            while (true) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    break;
-                }
+    static void startThread(Supplier<Boolean> shouldExitLoop) {
+        THREAD = new Thread(() -> {
+
+            while (!shouldExitLoop.get()) {
+                ITER_COUNTER.incrementAndGet();
+
+                var point_1 = new CountDownLatch(1);
+                var point_2 = new CountDownLatch(1);
+                var point_3 = new CountDownLatch(1);
+                var invocations = new CountDownLatch(2);
 
                 //
-                // 1. Execute invokeAndWait() from AppKit to EDT
+                // 1. Blocking invocation from AppKit to EDT
                 //
                 CThreading.executeOnAppKit(() -> {
-                    try {
-                        LWCToolkit.invokeAndWait(counter::incrementAndGet, Window.getWindows()[0]);
-                    } catch (Exception e) {
-                        fail(e);
-                    }
+                    // We're on AppKit, wait for the 2nd invocation to be on the AWTThreading-pool thread.
+                    if (TEST_CASE == 1) await(point_1, TIMEOUT_SECONDS);
+
+                    tryRun(() -> LWCToolkit.invokeAndWait(() -> {
+                        // We're being dispatched on EDT.
+                        if (TEST_CASE == 1) point_2.countDown();
+
+                        // Wait for the 2nd invocation to be executed on AppKit.
+                        if (TEST_CASE == 1) await(point_3, TIMEOUT_SECONDS);
+                    }, FRAME));
+
+                    invocations.countDown();
                 });
 
                 //
-                // 2. Execute invokeAndBlock() from EDT to AppKit
+                // 2. Blocking invocation from EDT to AppKit
                 //
-                EventQueue.invokeLater(() -> {
-                    passed.set(false);
+                EventQueue.invokeLater(() -> AWTThreading.executeWaitToolkit(() -> {
+                    // We're on the AWTThreading-pool thread.
+                    if (TEST_CASE == 1) point_1.countDown();
 
-                    Boolean success = AWTThreading.executeWaitToolkit(() -> {
-                        try {
-                            return CThreading.executeOnAppKit(() -> Boolean.TRUE);
-                        } catch (Throwable e) {
-                            fail(e);
+                    // Wait for the 1st invocation to start NSRunLoop and be dispatched
+                    if (TEST_CASE == 1) await(point_2, TIMEOUT_SECONDS);
+
+                    // Perform in JavaRunLoopMode to be accepted by NSRunLoop started by LWCToolkit.invokeAndWait.
+                    LWCToolkit.performOnMainThreadAndWait(() -> {
+                        if (DUMP_STACK.get()) {
+                            dumpAllThreads();
                         }
-                        return null;
+                        // We're being executed on AppKit.
+                        if (TEST_CASE == 1) point_3.countDown();
                     });
-                    System.out.println("Success: " + counter.get() + ": " + success);
 
-                    passed.set(Boolean.TRUE.equals(success));
+                    invocations.countDown();
+                }));
 
-                    if (passed.get()) {
-                        lock(COND::signal);
-                    }
-                    else {
-                        fail(null);
-                    }
-                });
+                await(invocations, TIMEOUT_SECONDS * 2);
+            } // while
 
-                lock(COND::await);
-            }
+            FUTURE.complete(true);
         });
-        thread.setDaemon(true);
-        thread.start();
+        THREAD.setDaemon(true);
+        THREAD.start();
     }
 
-    static void lock(MyRunnable runnable) {
-        LOCK.lock();
-        try {
-            try {
-                runnable.run();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } finally {
-            LOCK.unlock();
+    static void await(CountDownLatch latch, int seconds) {
+        if (!tryCall(() -> latch.await(seconds, TimeUnit.SECONDS), false)) {
+            FUTURE.completeExceptionally(new Throwable("Awaiting has timed out"));
         }
     }
 
-    interface MyRunnable {
-        void run() throws Exception;
-    }
-
-    static void fail(Throwable e) {
-        if (e != null) e.printStackTrace();
-        passed.set(false);
-        LATCH.countDown();
+    static void dumpAllThreads() {
+        Thread.getAllStackTraces().keySet().forEach(t -> {
+            System.out.printf("%s\t%s\t%d\t%s\n", t.getName(), t.getState(), t.getPriority(), t.isDaemon() ? "Daemon" : "Normal");
+            Arrays.asList(t.getStackTrace()).forEach(frame -> System.out.println("\t" + frame));
+        });
+        System.out.println("\n\n");
     }
 }
