930a412be764106b1a8072f7812322a9840f89f8
Anton Tarasov <anton.tarasov@jetbrains.com>
2022-02-18 20:27:06 +0300

    JBR-4119 UI freezes at sun.lwawt.macosx.CAccessibility.getChildrenAndRoles
    (cherry picked from commit 6ba79774d8c0178cba8aade4bfff57484153796f)

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/LWCToolkit.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/LWCToolkit.java
@@ -189,7 +189,7 @@ public final class LWCToolkit extends LWToolkit {
         }
     });
 
-    private static final PlatformLogger log = PlatformLogger.getLogger("sun.lwawt.macosx.LWCToolkit");
+    private static final PlatformLogger log = PlatformLogger.getLogger(LWCToolkit.class.getName());
 
     @SuppressWarnings("removal")
     public LWCToolkit() {
@@ -764,6 +764,20 @@ public final class LWCToolkit extends LWToolkit {
             log.fine("invokeAndWait started: " + runnable);
         }
 
+        if (isBlockingEventDispatchThread()) {
+            String msg = "invokeAndWait is discarded as the EventDispatch thread is currently blocked";
+            if (log.isLoggable(PlatformLogger.Level.FINE)) {
+                log.fine(msg, new Throwable());
+                log.fine(AWTThreading.getInstance(component).printEventDispatchThreadStackTrace().toString());
+            } else if (log.isLoggable(PlatformLogger.Level.INFO)) {
+                StackTraceElement[] stack = new Throwable().getStackTrace();
+                log.info(msg + ". Originated at " + stack[stack.length - 1]);
+                Thread dispatchThread = AWTThreading.getInstance(component).getEventDispatchThread();
+                log.info(dispatchThread.getName() + " at: " + dispatchThread.getStackTrace()[0].toString());
+            }
+            return;
+        }
+
         boolean nonBlockingRunLoop;
 
         synchronized (priorityInvocationPending) {
@@ -812,6 +826,8 @@ public final class LWCToolkit extends LWToolkit {
 
         checkException(invocationEvent);
     }
+
+    private static native boolean isBlockingEventDispatchThread();
 
     public static void invokeLater(Runnable event, Component component)
             throws InvocationTargetException {
