9f069ca59e51eadae0ef6865dc23599f964c59ec
Mikhail Grishchenko <mikhail.grishchenko@jetbrains.com>
2022-02-24 17:11:00 +0700

    JBR-3975 Fix regexp in check_jbr_size.sh
    (cherry picked from commit 23131fc43508c0cc98ad760a075daca5721df213)

--- jb/project/tools/test/check_jbr_size.sh
+++ jb/project/tools/test/check_jbr_size.sh
@@ -25,16 +25,39 @@ case "${unameOut}" in
       echo "Unknown machine: ${unameOut}"
       exit 1
 esac
+FILENAME=$(basename ${NEWFILEPATH})
+#
+# Get pattern of artifact name
+# Base filename pattern: <BUNDLE_TYPE>-<JDK_VERSION>-<OS>-<ARCH>-b<BUILD>.tar.gz: jbr_dcevm-17.0.2-osx-x64-b1234.tar.gz
+# BUNDLE_TYPE: jbr, jbrsdk, jbr_dcevm, jbrsdk_jcef etc.
+# OS_ARCH_PATTERN - <os_architecture>: osx-x64, linux-aarch64, windows-x64 etc.
 
-echo "New size of $NEWFILEPATH = $NEWFILESIZE bytes."
+BUNDLE_TYPE=jbrsdk
+OS_ARCH_PATTERN=""
+re='(jbr[a-z_]*).+[0-9_]+-(.+)-b.+\.tar\.gz'
+if [[ $FILENAME =~ $re ]]; then
+  BUNDLE_TYPE=${BASH_REMATCH[1]}
+  OS_ARCH_PATTERN=${BASH_REMATCH[2]}
+fi
+echo "BUNDLE_TYPE: "  $BUNDLE_TYPE
+echo "OS_ARCH_PATTERN: " $OS_ARCH_PATTERN
+echo "New size of $FILENAME = $NEWFILESIZE bytes."
 
-# example: IntellijCustomJdk_Jdk17_Master_LinuxX64jcef
 
+#
+# Get previous successful build ID
+# Example:
+# CONFIGID=IntellijCustomJdk_Jdk17_Master_LinuxX64jcef
+# BUILDID=12345678
+#
+# expected return value
+# id="123".number="567"
+#
 CURL_RESPONSE=$(curl --header "Authorization: Bearer $TOKEN" "https://buildserver.labs.intellij.net/app/rest/builds/?locator=buildType:(id:$CONFIGID),status:success,count:1,finishDate:(build:$BUILDID,condition:before)")
+re='id=\"([0-9]+)\".+number=\"([0-9\.]+)\"'
 
-re='id=\"([0-9]+)\".+number=\"([0-9]+)\"'
+# ID: Previous successful build id
 ID=0
-
 if [[ $CURL_RESPONSE =~ $re ]]; then
   ID=${BASH_REMATCH[1]}
   echo "BUILD Number: ${BASH_REMATCH[2]}"
@@ -44,13 +67,18 @@ else
   exit 1
 fi
 
+#
+# Get artifacts from previous successful build
+#
+# expected return value
+# name="jbrsdk_jcef*.tar.gz size="123'
+#
 CURL_RESPONSE=$(curl --header "Authorization: Bearer $TOKEN" "https://buildserver.labs.intellij.net/app/rest/builds/$ID?fields=id,number,artifacts(file(name,size))")
-
-echo "Atrifacts of last pinned build of $CONFIGID :\n"
+echo "Atrifacts of previous build of $CONFIGID :"
 echo $CURL_RESPONSE
 
-# Find size (in response) with reg exp
-re='name=\"(jbrsdk_jcef[^\"]+\.tar\.gz)\" size=\"([0-9]+)\"'
+# Find binary size (in response) with reg exp
+re='name=\"('$BUNDLE_TYPE'[^\"]+'${OS_ARCH_PATTERN}'[^\"]+\.tar\.gz)\" size=\"([0-9]+)\"'
 
 if [[ $CURL_RESPONSE =~ $re ]]; then
   OLDFILENAME=${BASH_REMATCH[1]}
