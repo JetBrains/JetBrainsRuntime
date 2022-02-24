c56f5f8f10ade0b0986c6b4817f5636329ca65d0
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2022-02-24 11:49:29 +0300

    JBR-4281 Window losing focus isn't detected in some cases on macOS
    (cherry picked from commit 363650bbf48789e4c5f68840b66372442d3e481f)
    
    with JBR-4638 Regression: Unable to enter emoji in editor via Emoji & Symbols on macOS
    
    (cherry picked from commit 8b9a00915df501ae9778034e882c972a494c2767)
    
    with JBR-4652 With multiple projects open non-fullscreen, right-click on Dock icon to select the project from the context menu doesn't switch to that project
    
    (cherry picked from commit e34677587d2469cef4a4f4a2670e967c1440b8ec)
    
    with JBR-5134 Input methods can't be dynamically disabled on a focused JComponent
    
    (cherry picked from commit 0a3f4b206d9106e0bf093e09d62d3cfe4db2b7a1)
    (cherry picked from commit 0307ccab3bae9dd4053fcbd2687ad15137012efc)

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CInputMethod.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CInputMethod.java
@@ -279,9 +279,12 @@ public final class CInputMethod extends InputMethodAdapter {
     @Override
     public void removeNotify() {
         if (fAwtFocussedComponentPeer != null) {
-            nativeEndComposition(getNativeViewPtr(fAwtFocussedComponentPeer));
+            long modelPtr = getNativeViewPtr(fAwtFocussedComponentPeer);
+            nativeEndComposition(modelPtr);
+            nativeNotifyPeer(modelPtr, null);
         }
 
+        fAwtFocussedComponent = null;
         fAwtFocussedComponentPeer = null;
     }
 
@@ -293,34 +296,32 @@ public final class CInputMethod extends InputMethodAdapter {
      */
     @Override
     protected void setAWTFocussedComponent(Component component) {
-        LWComponentPeer<?, ?> peer = null;
-        long modelPtr = 0;
-        CInputMethod imInstance = this;
+        if (component == null || component == fAwtFocussedComponent) {
+            // Sometimes input happens for the natively unfocused window
+            // (e.g. in case of system emoji picker),
+            // so we don't reset last focused component on focus lost.
+            return;
+        }
 
-        // component will be null when we are told there's no focused component.
-        // When that happens we need to notify the native architecture to stop generating IMEs
-        if (component == null) {
-            peer = fAwtFocussedComponentPeer;
-            imInstance = null;
-        } else {
-            peer = getNearestNativePeer(component);
+        if (fAwtFocussedComponentPeer != null) {
+            long modelPtr = getNativeViewPtr(fAwtFocussedComponentPeer);
+            nativeNotifyPeer(modelPtr, null);
+        }
 
+        fAwtFocussedComponent = component;
+        fAwtFocussedComponentPeer = getNearestNativePeer(component);
+
+        if (fAwtFocussedComponentPeer != null) {
+            long modelPtr = getNativeViewPtr(fAwtFocussedComponentPeer);
+
+            CInputMethod imInstance = this;
             // If we have a passive client, don't pass input method events to it.
             if (component.getInputMethodRequests() == null) {
                 imInstance = null;
             }
-        }
 
-        if (peer != null) {
-            modelPtr = getNativeViewPtr(peer);
-
-            // modelPtr refers to the ControlModel that either got or lost focus.
             nativeNotifyPeer(modelPtr, imInstance);
         }
-
-        // Track the focused component and its nearest peer.
-        fAwtFocussedComponent = component;
-        fAwtFocussedComponentPeer = getNearestNativePeer(component);
     }
 
     /**
