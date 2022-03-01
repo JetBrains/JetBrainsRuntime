70cced0bd2d02efd02a8f9e8062110b2c7fcc216
Goetz Lindenmaier <goetz@openjdk.org>
2022-03-01 16:58:10 +0000

    8255410: Add ChaCha20 and Poly1305 support to SunPKCS11 provider
    Reviewed-by: mdoerr
    Backport-of: 5d8c1cc8a05e0d9aedd6d54b8147d374c2290024

--- test/jdk/sun/security/pkcs11/PKCS11Test.java
+++ test/jdk/sun/security/pkcs11/PKCS11Test.java
@@ -111,6 +111,40 @@ public abstract class PKCS11Test {
     static double nss3_version = -1;
     static Provider pkcs11 = newPKCS11Provider();
 
+    // Utility methods
+    // Used to backport HexFormat from 17.
+    protected static byte[] HexToBytes(String hexVal) {
+        if (hexVal == null) return new byte[0];
+        byte[] result = new byte[hexVal.length()/2];
+        for (int i = 0; i < result.length; i++) {
+            // 2 characters at a time
+            String byteVal = hexVal.substring(2*i, 2*i +2);
+            result[i] = Integer.valueOf(byteVal, 16).byteValue();
+        }
+        return result;
+    }
+    private static void byte2hex(byte b, StringBuffer buf) {
+        char[] hexChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8',
+                            '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        int high = ((b & 0xf0) >> 4);
+        int low = (b & 0x0f);
+        buf.append(hexChars[high]);
+        buf.append(hexChars[low]);
+    }
+    protected static String toHexString(byte[] block) {
+        StringBuffer buf = new StringBuffer();
+
+        int len = block.length;
+
+        for (int i = 0; i < len; i++) {
+             byte2hex(block[i], buf);
+             if (i < len-1) {
+                 buf.append(":");
+             }
+        }
+        return buf.toString();
+    }
+
     public static Provider newPKCS11Provider() {
         ServiceLoader sl = ServiceLoader.load(java.security.Provider.class);
         Iterator<Provider> iter = sl.iterator();
