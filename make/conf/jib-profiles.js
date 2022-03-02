481f2f31385c297881d969508286aaef027d5fcf
Vladimir Kempik <vkempik@openjdk.org>
2022-03-02 15:26:54 +0000

    8260518: Change default -mmacosx-version-min to 10.12
    Reviewed-by: phh, burban
    Backport-of: 53f1b93881a6404508628388ba590fe1f7f958ff
    (cherry picked from commit 5561fd1e4253489c667b18b79f261c96a3d5a9b4)

--- make/conf/jib-profiles.js
+++ make/conf/jib-profiles.js
@@ -415,7 +415,7 @@ var getJibProfilesProfiles = function (input, common, data) {
             target_cpu: "x64",
             dependencies: ["devkit", "graalunit_lib"],
             configure_args: concat(common.configure_args_64bit, "--with-zlib=system",
-                "--with-macosx-version-max=10.9.0"),
+                "--with-macosx-version-max=10.12.00"),
         },
 
         "solaris-x64": {
