822b4a8c49acd65bec8d7b594ca0ab886aad0ab0
Artem Semenov <Artem.Semenov@jetbrains.com>
2022-03-03 16:06:26 +0300

    JBR-4235 Context menu not readable after opening on Mac OS

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CAccessible.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CAccessible.java
@@ -205,6 +205,7 @@ class CAccessible extends CFRetainedResource implements Accessible {
                         } else if (oldValue != null &&
                                 ((AccessibleState) oldValue) == AccessibleState.VISIBLE) {
                             execute(ptr -> menuClosed(ptr));
+                            execute(ptr -> unregisterFromCocoaAXSystem(ptr));
                         }
                     } else if (thisRole == AccessibleRole.MENU_ITEM) {
                         if (newValue != null &&
