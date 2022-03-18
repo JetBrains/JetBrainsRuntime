d267171850a28191c8d19caad170ff094495754c
Goetz Lindenmaier <goetz@openjdk.org>
2022-03-18 12:31:01 +0000

    8231489: GC watermark_0_1 failed due to "metaspace.gc.Fault: GC has happened too rare"
    Backport-of: 9e17946ab21434df0f6780423f7998166265af2a

--- test/jtreg-ext/requires/VMProps.java
+++ test/jtreg-ext/requires/VMProps.java
@@ -304,6 +304,7 @@ public class VMProps implements Callable<Map<String, String>> {
      */
     protected void vmOptFinalFlags(SafeMap map) {
         vmOptFinalFlag(map, "ClassUnloading");
+        vmOptFinalFlag(map, "ClassUnloadingWithConcurrentMark");
         vmOptFinalFlag(map, "UseCompressedOops");
         vmOptFinalFlag(map, "EnableJVMCI");
         vmOptFinalFlag(map, "EliminateAllocations");
