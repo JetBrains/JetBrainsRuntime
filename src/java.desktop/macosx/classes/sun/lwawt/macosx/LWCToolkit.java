6c0e54fe58135718e7a515dfb776c68a79001534
Anton Tarasov <anton.tarasov@jetbrains.com>
2022-03-23 18:32:50 +0300

    JBR-4328 remove LWCToolkit.unsafeNonblockingExecute
    (cherry picked from commit 9a3f31a6c4df25331e7fd1847bce224e10609c5d)
    (cherry picked from commit f9a02266a0a4fe158242183e3ba9c42307ef4655)

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/LWCToolkit.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/LWCToolkit.java
@@ -692,30 +692,6 @@ public final class LWCToolkit extends LWToolkit {
         }
     }
 
-    private static final AtomicInteger blockingRunLoopCounter = new AtomicInteger(0);
-    private static final AtomicBoolean priorityInvocationPending = new AtomicBoolean(false);
-
-    @Override
-    public void unsafeNonblockingExecute(Runnable runnable) {
-        if (!EventQueue.isDispatchThread()) {
-            throw new Error("the method must be called on the Event Dispatching thread");
-        }
-        if (runnable == null) return;
-
-        synchronized (priorityInvocationPending) {
-            priorityInvocationPending.set(true);
-        }
-        AWTAccessor.getEventQueueAccessor().createSecondaryLoop(
-            getSystemEventQueue(),
-            () -> blockingRunLoopCounter.get() > 0).enter();
-
-        try {
-            runnable.run();
-        } finally {
-            priorityInvocationPending.set(false);
-        }
-    }
-
     /**
      * Kicks an event over to the appropriate eventqueue and waits for it to
      * finish To avoid deadlocking, we manually run the NSRunLoop while waiting
@@ -751,13 +727,6 @@ public final class LWCToolkit extends LWToolkit {
             return;
         }
 
-        boolean nonBlockingRunLoop;
-
-        synchronized (priorityInvocationPending) {
-            nonBlockingRunLoop = priorityInvocationPending.get();
-            if (!nonBlockingRunLoop) blockingRunLoopCounter.incrementAndGet();
-        }
-
         AWTThreading.TrackedInvocationEvent invocationEvent =
             AWTThreading.createAndTrackInvocationEvent(component, runnable, true);
 
@@ -788,11 +757,9 @@ public final class LWCToolkit extends LWToolkit {
             invocationEvent.onDone(() -> eventDispatchThreadFreeFuture.cancel(false));
         }
 
-        if (!doAWTRunLoop(mediator, nonBlockingRunLoop, timeoutSeconds)) {
+        if (!doAWTRunLoop(mediator, false, timeoutSeconds)) {
             invocationEvent.dispose("InvocationEvent has timed out");
         }
-
-        if (!nonBlockingRunLoop) blockingRunLoopCounter.decrementAndGet();
 
         if (log.isLoggable(PlatformLogger.Level.FINE)) {
             log.fine("invokeAndWait finished: " + runnable);
