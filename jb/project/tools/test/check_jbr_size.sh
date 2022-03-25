f3777b24ea27cce3aedf0b4c945c724e3668c066
Mikhail Grishchenko <mikhail.grishchenko@jetbrains.com>
2022-03-26 05:32:40 +0700

    JBR-3975 print Teamcity messages

--- jb/project/tools/test/check_jbr_size.sh
+++ jb/project/tools/test/check_jbr_size.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+
+while getopts ":t" o; do
+    case "${o}" in
+        t)
+          t="With Teamcity tests info"
+          TC_PRINT=1
+          ;;
+    esac
+done
+shift $((OPTIND-1))
+
 NEWFILEPATH=$1
 CONFIGID=$2
 BUILDID=$3
@@ -7,6 +18,7 @@ TOKEN=$4
 #
 # Get the size of new artifact
 #
+
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Linux*)
@@ -16,7 +28,11 @@ case "${unameOut}" in
       NEWFILESIZE=$(stat -f%z "$NEWFILEPATH")
       ;;
     CYGWIN*)
-      NEWFILESIZE=$(stat -c%s "$NEWFILEPATH")
+      NEWFILESIZE=$(stat -c%s$4
+#
+# Get the size of new artifact
+#
+ "$NEWFILEPATH")
       ;;
     MINGW*)
       NEWFILESIZE=$(stat -c%s "$NEWFILEPATH")
@@ -26,6 +42,7 @@ case "${unameOut}" in
       exit 1
 esac
 FILENAME=$(basename ${NEWFILEPATH})
+
 #
 # Get pattern of artifact name
 # Base filename pattern: <BUNDLE_TYPE>-<JDK_VERSION>-<OS>-<ARCH>-b<BUILD>.tar.gz: jbr_dcevm-17.0.2-osx-x64-b1234.tar.gz
@@ -34,16 +51,39 @@ FILENAME=$(basename ${NEWFILEPATH})
 
 BUNDLE_TYPE=jbrsdk
 OS_ARCH_PATTERN=""
-re='(jbr[a-z_]*)-[0-9_]+-(.+)-b.+\.tar\.gz'
+FILE_EXTENSION=tar.gz
+
+re='(jbr[a-z_]*).*-[0-9_\.]+-(.+)-b[0-9]+(.+)'
 if [[ $FILENAME =~ $re ]]; then
   BUNDLE_TYPE=${BASH_REMATCH[1]}
   OS_ARCH_PATTERN=${BASH_REMATCH[2]}
-  echo ${BASH_REMATCH[*]}
+  FILE_EXTENSION=${BASH_REMATCH[3]}
 fi
+
+if [ $TC_PRINT -eq 1 ]; then
+  testname_file_ext=`echo $FILE_EXTENSION | sed 's/\./_/g'`
+  testname=$BUNDLE_TYPE"_"$OS_ARCH_PATTERN$testname_file_ext
+  echo \#\#teamcity[testStarted name=\'$testname\']
+fi
+
+
 echo "BUNDLE_TYPE: "  $BUNDLE_TYPE
 echo "OS_ARCH_PATTERN: " $OS_ARCH_PATTERN
+echo "FILE_EXTENSION: " $FILE_EXTENSION
 echo "New size of $FILENAME = $NEWFILESIZE bytes."
 
+
+function test_failed_msg() {
+  if [ $3 -eq 1 ]; then
+    echo \#\#teamcity[testFailed name=\'$1\' message=\'$2\']
+  fi
+}
+
+function test_finished_msg() {
+  if [ $2 -eq 1 ]; then
+    echo \#\#teamcity[testFinished name=\'$1\']
+  fi
+}
 
 #
 # Get previous successful build ID
@@ -63,8 +103,11 @@ if [[ $CURL_RESPONSE =~ $re ]]; then
   ID=${BASH_REMATCH[1]}
   echo "BUILD Number: ${BASH_REMATCH[2]}"
 else
-  echo "ERROR: can't find previous build"
+  msg="ERROR: can't find previous build"
+  echo $msg
   echo $CURL_RESPONSE
+  test_failed_msg $testname $msg $TC_PRINT
+  test_finished_msg $testname $TC_PRINT
   exit 1
 fi
 
@@ -79,7 +122,7 @@ echo "Atrifacts of previous build of $CONFIGID :"
 echo $CURL_RESPONSE
 
 # Find binary size (in response) with reg exp
-re='name=\"('$BUNDLE_TYPE'[^\"]+'${OS_ARCH_PATTERN}'[^\"]+\.tar\.gz)\" size=\"([0-9]+)\"'
+re='name=\"('$BUNDLE_TYPE'[^\"]+'${OS_ARCH_PATTERN}'[^\"]+'${FILE_EXTENSION}')\" size=\"([0-9]+)\"'
 
 if [[ $CURL_RESPONSE =~ $re ]]; then
   OLDFILENAME=${BASH_REMATCH[1]}
@@ -90,14 +133,20 @@ if [[ $CURL_RESPONSE =~ $re ]]; then
   let allowedSize=OLDFILESIZE+OLDFILESIZE/20 # use 5% threshold
   echo "Allowed size = $allowedSize"
   if [[ "$NEWFILESIZE" -gt "$allowedSize" ]]; then
-    echo "ERROR: new size is significally greater than prev size (need to investigate)"
+    msg="ERROR: new size is significally greater than prev size (need to investigate)"
+    echo $msg
+    test_failed_msg $testname $msg $TC_PRINT
+    test_finished_msg $testname $TC_PRINT
     exit 1
   else
     echo "PASSED"
+   test_finished_msg $testname $TC_PRINT
   fi
 else
-  echo "ERROR: can't find string with size in xml response:"
+  msg="ERROR: can't find string with size in xml response:"
+  echo $msg
   echo $CURL_RESPONSE
+  test_failed_msg $testname $msg $TC_PRINT
+  test_finished_msg $testname $TC_PRINT
   exit 1
 fi
-
