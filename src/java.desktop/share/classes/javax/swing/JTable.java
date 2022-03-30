57faad5745f5140bf7cbc609c5c64a5cf7752d5c
Goetz Lindenmaier <goetz@openjdk.org>
2022-03-30 13:10:13 +0000

    8277922: Unable to click JCheckBox in JTable through Java Access Bridge
    Backport-of: 1668c02ee83cc4f7bd7b2fe5bb7ef59b5dbda081

--- src/java.desktop/share/classes/javax/swing/JTable.java
+++ src/java.desktop/share/classes/javax/swing/JTable.java
@@ -5424,6 +5424,21 @@ public class JTable extends JComponent implements TableModelListener, Scrollable
 
             return this;
         }
+
+        @Override
+        public AccessibleContext getAccessibleContext() {
+            if (accessibleContext == null) {
+                accessibleContext = new AccessibleBooleanRenderer();
+            }
+            return accessibleContext;
+        }
+
+        class AccessibleBooleanRenderer extends JCheckBox.AccessibleJCheckBox {
+            @Override
+            public AccessibleAction getAccessibleAction() {
+                return null;
+            }
+        }
     }
 
     /**
@@ -8344,7 +8359,11 @@ public class JTable extends JComponent implements TableModelListener, Scrollable
              * @return the <code>AccessibleAction</code>, or <code>null</code>
              */
             public AccessibleAction getAccessibleAction() {
-                return getCurrentAccessibleContext().getAccessibleAction();
+                AccessibleContext ac = getCurrentAccessibleContext();
+                if (ac != null) {
+                    return ac.getAccessibleAction();
+                }
+                return null;
             }
 
             /**
