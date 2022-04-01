e424a5bf19f2e9152f9a7b65ef804d05bbfaa044
Anton Tarasov <anton.tarasov@jetbrains.com>
2022-04-01 21:32:14 +0300

    JBR-4347 jb/java/awt/Toolkit/AWTThreadingCMenuTest.java fails by ClassNotFoundException

--- test/jdk/jb/java/awt/Toolkit/AWTThreadingCMenuTest.java
+++ test/jdk/jb/java/awt/Toolkit/AWTThreadingCMenuTest.java
@@ -1,6 +1,6 @@
 // Copyright 2000-2022 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package jdk.jb.java.awt.Toolkit;import javax.swing.*;
+import javax.swing.*;
 import java.util.function.Consumer;
 
 import static helper.ToolkitTestHelper.*;
