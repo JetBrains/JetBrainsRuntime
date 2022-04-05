a852ad152781b4323abfd590e37fb389a49e34e6
Goetz Lindenmaier <goetz@openjdk.org>
2022-04-05 09:52:21 +0000

    8272358: Some tests may fail when executed with other locales than the US
    Backport-of: 29e552c03a2825f9526330072668a1d63ac68fd4

--- test/langtools/jdk/jshell/ToolBasicTest.java
+++ test/langtools/jdk/jshell/ToolBasicTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -552,7 +552,7 @@ public class ToolBasicTest extends ReplToolTesting {
     }
 
     public void testOpenResource() {
-        test(
+        test(new String[]{"-R", "-Duser.language=en", "-R", "-Duser.country=US"},
                 (a) -> assertCommand(a, "/open PRINTING", ""),
                 (a) -> assertCommandOutputContains(a, "/list",
                         "void println", "System.out.printf"),
