a9ce9c763b10a47734f5005eb723d74e18c85183
Zhengyu Gu <zgu@openjdk.org>
2022-04-11 13:36:20 +0000

    JBR-4378 8284093: Memory leak: X11SD_DisposeXImage should also free obdata
    Reviewed-by: prr, andrew

--- src/java.desktop/unix/native/common/java2d/x11/X11SurfaceData.c
+++ src/java.desktop/unix/native/common/java2d/x11/X11SurfaceData.c
@@ -1457,6 +1457,7 @@ void X11SD_DisposeXImage(XImage * image) {
 #ifdef MITSHM
         if (image->obdata != NULL) {
             X11SD_DropSharedSegment((XShmSegmentInfo*)image->obdata);
+            free(image->obdata);
             image->obdata = NULL;
         }
 #endif /* MITSHM */
