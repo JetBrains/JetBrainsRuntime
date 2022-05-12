8ce7520a17c82ff4b2ef0869ba6c896b89d7ed61
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2022-05-13 06:15:29 +0700

    JBR-4437 add sources to JBRSDK distributions for Windows and Linux

--- jb/project/tools/linux/scripts/mkimages_aarch64.sh
+++ jb/project/tools/linux/scripts/mkimages_aarch64.sh
@@ -69,6 +69,7 @@ function create_image_bundle {
   if [ "$__arch_name" == "$JBRSDK_BUNDLE" ]; then
     sed 's/JBR/JBRSDK/g' "$IMAGES_DIR"/"$__arch_name"/release > release
     mv release "$IMAGES_DIR"/"$__arch_name"/release
+    cp $IMAGES_DIR/lib/src.zip $__arch_name/lib
     copy_jmods "$__modules" "$__modules_path" "$IMAGES_DIR"/"$__arch_name"/jmods
     zip_native_debug_symbols $IMAGES_DIR/jdk "${JBR}_diz"
   fi
