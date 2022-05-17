6ef8da61dccd7535580e985917f013315a10b5ec
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2022-05-18 02:24:51 +0700

    JBR-4451 Make bash scripts safer

--- jb/project/tools/windows/scripts/pack_aarch64.sh
+++ jb/project/tools/windows/scripts/pack_aarch64.sh
@@ -1,4 +1,7 @@
-#!/bin/bash -x
+#!/bin/bash
+
+set -euo pipefail
+set -x
 
 # The following parameters must be specified:
 #   build_number - specifies the number of JetBrainsRuntime build
@@ -19,6 +22,8 @@ function pack_jbr {
   __bundle_name=$1
   __arch_name=$2
 
+  fastdebug_infix=''
+
   [ "$bundle_type" == "fd" ] && [ "$__arch_name" == "$JBRSDK_BUNDLE" ] && __bundle_name=$__arch_name && fastdebug_infix="fastdebug-"
   JBR=${__bundle_name}-${JBSDK_VERSION}-windows-aarch64-${fastdebug_infix}b${build_number}
 
@@ -36,6 +41,8 @@ BASE_DIR=.
 
 if [ "$bundle_type" == "jcef" ] || [ "$bundle_type" == "dcevm" ] || [ "$bundle_type" == "fd" ]; then
   jbr_name_postfix="_${bundle_type}"
+else
+  jbr_name_postfix=""
 fi
 
 pack_jbr jbr${jbr_name_postfix} jbr
