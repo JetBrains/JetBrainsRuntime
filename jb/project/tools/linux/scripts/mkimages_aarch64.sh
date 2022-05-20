afe1cbc136f228bbce831a0c6f5b133d3edd59c6
VladimirKempik <69576004+VladimirKempik@users.noreply.github.com>
2022-05-20 11:18:10 +0300

    JBR-4452: Update crash report message with JBR youtrack link instead of bugreport.java.com (#151)

--- jb/project/tools/linux/scripts/mkimages_aarch64.sh
+++ jb/project/tools/linux/scripts/mkimages_aarch64.sh
@@ -32,6 +32,7 @@ function do_configure {
     --with-version-opt=b"$build_number" \
     --with-boot-jdk="$BOOT_JDK" \
     --enable-cds=yes \
+    $STATIC_CONF_ARGS \
     $REPRODUCIBLE_BUILD_OPTS \
     $WITH_ZIPPED_NATIVE_DEBUG_SYMBOLS \
     || do_exit $?
