98361bee9115a43f9b23236859903a434e05e02b
Martin Doerr <mdoerr@openjdk.org>
2022-05-23 19:11:57 +0000

    8139046: Compiler Control: IGVPrintLevel directive should set PrintIdealGraph
    Make !PrintIdealGraph a synonym for PrintIdealGraphLevel=-1 => print nothing
    
    Reviewed-by: xliu, rrich
    Backport-of: 892d998587a0a7a7ed17586f96ae7dc5b236adb2
    (cherry picked from commit b2c51c4daa4ef7d57ffeda8f7a5126e5ce4ac70d)

--- src/hotspot/share/opto/compile.cpp
+++ src/hotspot/share/opto/compile.cpp
@@ -683,8 +683,8 @@ Compile::Compile( ciEnv* ci_env, C2Compiler* compiler, ciMethod* target, int osr
 #ifndef PRODUCT
                   _trace_opto_output(directive->TraceOptoOutputOption),
                   _in_dump_cnt(0),
-                  _printer(IdealGraphPrinter::printer()),
 #endif
+                  NOT_PRODUCT(_printer(NULL) COMMA)
                   _congraph(NULL),
                   _comp_arena(mtCompiler),
                   _node_arena(mtCompiler),
@@ -706,11 +706,6 @@ Compile::Compile( ciEnv* ci_env, C2Compiler* compiler, ciMethod* target, int osr
                   _max_node_limit(MaxNodeLimit),
                   _has_reserved_stack_access(target->has_reserved_stack_access()) {
   C = this;
-#ifndef PRODUCT
-  if (_printer != NULL) {
-    _printer->set_compile(this);
-  }
-#endif
   CompileWrapper cw(this);
 
   if (CITimeVerbose) {
@@ -869,7 +864,7 @@ Compile::Compile( ciEnv* ci_env, C2Compiler* compiler, ciMethod* target, int osr
   // Drain the list.
   Finish_Warm();
 #ifndef PRODUCT
-  if (_printer && _printer->should_print(1)) {
+  if (should_print(1)) {
     _printer->print_inlining();
   }
 #endif
@@ -1002,8 +997,8 @@ Compile::Compile( ciEnv* ci_env,
 #ifndef PRODUCT
     _trace_opto_output(directive->TraceOptoOutputOption),
     _in_dump_cnt(0),
-    _printer(NULL),
 #endif
+    NOT_PRODUCT(_printer(NULL) COMMA)
     _comp_arena(mtCompiler),
     _node_arena(mtCompiler),
     _old_arena(mtCompiler),
