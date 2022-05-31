7b58ca2ab6561eee11e6a883a4215ac9966a7eac
Andrew John Hughes <andrew@openjdk.org>
2022-05-31 01:38:34 +0000

    8284094: Memory leak in invoker_completeInvokeRequest()
    Backport-of: 1dfa1eaea0c8958f4b793c0766e36607cbde5c7f

--- src/jdk.jdwp.agent/share/native/libjdwp/invoker.c
+++ src/jdk.jdwp.agent/share/native/libjdwp/invoker.c
@@ -845,6 +845,7 @@ invoker_completeInvokeRequest(jthread thread)
         (void)outStream_writeObjectTag(env, &out, exc);
         (void)outStream_writeObjectRef(env, &out, exc);
         outStream_sendReply(&out);
+        outStream_destroy(&out);
     }
 
     /*
