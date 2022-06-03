2926c1ec69ceadc4d09ef4a3ff7c148638f4bd47
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2022-06-03 13:23:24 +0300

    JBR-4535 Popup windows disappear on mouse hover when 'Focus strictly under mouse' policy is used in KDE
    (cherry picked from commit a70a83e7fe4ec5aa5b2bbf5691299528b372677b)

--- src/java.desktop/share/classes/sun/awt/SunToolkit.java
+++ src/java.desktop/share/classes/sun/awt/SunToolkit.java
@@ -1955,6 +1955,10 @@ public abstract class SunToolkit extends Toolkit
         return false;
     }
 
+    public boolean popupMenusAreSpecial() {
+        return false;
+    }
+
     /**
      * Returns whether or not a containing top level window for the passed
      * component is
