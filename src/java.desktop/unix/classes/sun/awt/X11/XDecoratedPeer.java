bae5b6931f0217c7fff48d48c5e01ee9c7162ca4
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2022-06-03 13:23:24 +0300

    JBR-4535 Popup windows disappear on mouse hover when 'Focus strictly under mouse' policy is used in KDE

--- src/java.desktop/unix/classes/sun/awt/X11/XDecoratedPeer.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XDecoratedPeer.java
@@ -1208,7 +1208,7 @@ abstract class XDecoratedPeer extends XWindowPeer {
 
     @Override
     boolean isOverrideRedirect() {
-        return Window.Type.POPUP.equals(getWindowType());
+        return Window.Type.POPUP.equals(getWindowType()) && !XWM.isKDE2();
     }
 
     public boolean requestWindowFocus(long time, boolean timeProvided) {
