5b43c7697637ac9a0a241bb1a3a2ee7c7d3ac0fc
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2022-06-10 22:09:58 +0200

    JBR-4959 [macOS Ventura] Screen flickering after OS update when IDE is full screen
    Remove displaysync

--- src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLLayer.h
+++ src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLLayer.h
@@ -31,31 +31,13 @@
 #import "common.h"
 
 @interface MTLLayer : CAMetalLayer
-{
-@private
-    jobject javaLayer;
-
-    // intermediate buffer, used the RQ lock to synchronize
-    MTLContext* ctx;
-    float bufferWidth;
-    float bufferHeight;
-    id<MTLTexture>* buffer;
-    int nextDrawableCount;
-    int topInset;
-    int leftInset;
-    CVDisplayLinkRef displayLink;
-}
 
 @property (nonatomic) jobject javaLayer;
 @property (readwrite, assign) MTLContext* ctx;
-@property (readwrite, assign) float bufferWidth;
-@property (readwrite, assign) float bufferHeight;
 @property (readwrite, assign) id<MTLTexture>* buffer;
-@property (readwrite, assign) int nextDrawableCount;
+@property (readwrite, atomic) int nextDrawableCount;
 @property (readwrite, assign) int topInset;
 @property (readwrite, assign) int leftInset;
-@property (readwrite, assign) CVDisplayLinkRef displayLink;
-@property (readwrite, atomic) int displayLinkCount;
 
 - (id) initWithJavaLayer:(jobject)layer;
 
@@ -69,8 +51,6 @@
 - (void) blitCallback;
 - (void) display;
 - (void) redraw;
-- (void) startDisplayLink;
-- (void) stopDisplayLink;
 @end
 
 #endif /* MTLLayer_h_Included */
