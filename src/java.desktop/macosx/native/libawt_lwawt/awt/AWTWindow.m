dd0d59e2a573937e579a269472453c3389354a66
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2022-06-22 21:38:02 +0200

    JBR-4591 macOS: SIGILL at [libsystem_kernel] __kill in -[__NSMallocBlock__ removeFromSuperview]: unrecognized selector sent to instance
    Verified if windowDragView is present

--- src/java.desktop/macosx/native/libawt_lwawt/awt/AWTWindow.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/AWTWindow.m
@@ -1405,8 +1405,6 @@ static const CGFloat DefaultHorizontalTitleBarButtonOffset = 20.0;
 {
     // See [setUpTransparentTitleBar] for the view hierarchy we're working with
     NSView* closeButtonView = [self.nsWindow standardWindowButton:NSWindowCloseButton];
-    NSView* zoomButtonView = [self.nsWindow standardWindowButton:NSWindowZoomButton];
-    NSView* miniaturizeButtonView = [self.nsWindow standardWindowButton:NSWindowMiniaturizeButton];
     NSView* titlebar = closeButtonView.superview;
     NSView* titlebarContainer = titlebar.superview;
     if (!titlebarContainer) {
@@ -1417,7 +1415,7 @@ static const CGFloat DefaultHorizontalTitleBarButtonOffset = 20.0;
 
     [NSLayoutConstraint deactivateConstraints:_transparentTitleBarConstraints];
 
-    AWTWindowDragView* windowDragView;
+    AWTWindowDragView* windowDragView = nil;
     for (NSView* view in [titlebar.subviews arrayByAddingObjectsFromArray:titlebarContainer.subviews]) {
         if ([view isMemberOfClass:[AWTWindowDragView class]]) {
             windowDragView = view;
@@ -1426,7 +1424,11 @@ static const CGFloat DefaultHorizontalTitleBarButtonOffset = 20.0;
             view.translatesAutoresizingMaskIntoConstraints = YES;
         }
     }
-    [windowDragView removeFromSuperview];
+
+    if (windowDragView != nil) {
+        [windowDragView removeFromSuperview];
+    }
+
     titlebarContainer.translatesAutoresizingMaskIntoConstraints = YES;
     titlebar.translatesAutoresizingMaskIntoConstraints = YES;
 
