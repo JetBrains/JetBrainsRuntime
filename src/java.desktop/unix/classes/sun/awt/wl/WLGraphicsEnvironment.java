3d2576cc490299c10fc097cf7e74d9743233618c
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2022-06-30 09:54:52 +0200

    Improved sun.awt.wl.WLGraphicsEnvironment to support createCraphics()

--- src/java.desktop/unix/classes/sun/awt/wl/WLGraphicsEnvironment.java
+++ src/java.desktop/unix/classes/sun/awt/wl/WLGraphicsEnvironment.java
@@ -26,50 +26,32 @@
 
 package sun.awt.wl;
 
-import java.awt.Font;
-import java.awt.Graphics2D;
 import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-import java.awt.HeadlessException;
-import java.awt.image.BufferedImage;
-import java.util.Locale;
+import sun.java2d.SunGraphicsEnvironment;
 import sun.java2d.SurfaceManagerFactory;
 import sun.java2d.UnixSurfaceManagerFactory;
+import sun.util.logging.PlatformLogger;
 
-public class WLGraphicsEnvironment extends GraphicsEnvironment {
-    private final WLGraphicsDevice device = new WLGraphicsDevice();
-
+public class WLGraphicsEnvironment extends SunGraphicsEnvironment {
+    private static final PlatformLogger log = PlatformLogger.getLogger("sun.awt.wl.WLComponentPeer");
     static {
         SurfaceManagerFactory.setInstance(new UnixSurfaceManagerFactory());
     }
 
     @Override
-    public GraphicsDevice[] getScreenDevices() throws HeadlessException {
-        return new GraphicsDevice[] {device};
+    protected int getNumScreens() {
+        log.info("Not implemented: WLGraphicsEnvironment.getNumScreens()");
+        return 1;
     }
 
     @Override
-    public GraphicsDevice getDefaultScreenDevice() throws HeadlessException {
-        return device;
+    protected GraphicsDevice makeScreenDevice(int screennum) {
+        log.info("Not implemented: WLGraphicsEnvironment.makeScreenDevice(int)");
+        return new WLGraphicsDevice();
     }
 
     @Override
-    public Graphics2D createGraphics(BufferedImage img) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Font[] getAllFonts() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public String[] getAvailableFontFamilyNames() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public String[] getAvailableFontFamilyNames(Locale l) {
-        throw new UnsupportedOperationException();
+    public boolean isDisplayLocal() {
+        return true;
     }
 }
