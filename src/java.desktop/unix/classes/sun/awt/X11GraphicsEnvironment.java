54fa475ff40a0a492442e07782eeed8378eda4ab
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2022-07-01 21:42:34 +0700

    JBR-4544 Enable OpenGL pipeline by default for Wayland sessions
    The OpenGL pipeline is enabled only if all of the following is true:
    - WAYLAND is detected,
    - VMWare virtualization is detected,
    - rendering pipeline is not a software one (llvmpipe).
    As a side effect, a system property 'jbr.virtualization.information'
    is set to the value of detected virtualization type. The value is the
    same as provided by JFR.
    
    (cherry picked from commit f6acd65d3245662117b7d064248918c394d2903e)

--- src/java.desktop/unix/classes/sun/awt/X11GraphicsEnvironment.java
+++ src/java.desktop/unix/classes/sun/awt/X11GraphicsEnvironment.java
@@ -69,6 +69,7 @@ public final class X11GraphicsEnvironment extends SunGraphicsEnvironment {
         if (!isHeadless()) {
             // first check the OGL system property
             boolean glxRequested = false;
+            boolean glxRecommended = false;
             String prop = System.getProperty("sun.java2d.opengl");
             if (prop != null) {
                 if (prop.equals("true") || prop.equals("t")) {
@@ -77,13 +78,16 @@ public final class X11GraphicsEnvironment extends SunGraphicsEnvironment {
                     glxRequested = true;
                     glxVerbose = true;
                 }
+            } else if (openGLRecommended()) {
+                glxRequested = true;
+                glxRecommended = true;
             }
 
             // Now check for XRender system property
             boolean xRenderRequested = true;
             boolean xRenderIgnoreLinuxVersion = false;
             String xProp = System.getProperty("sun.java2d.xrender");
-                if (xProp != null) {
+            if (xProp != null) {
                 if (xProp.equals("false") || xProp.equals("f")) {
                     xRenderRequested = false;
                 } else if (xProp.equals("True") || xProp.equals("T")) {
@@ -91,7 +95,7 @@ public final class X11GraphicsEnvironment extends SunGraphicsEnvironment {
                     xRenderVerbose = true;
                 }
 
-                if (xProp.equalsIgnoreCase("t") || xProp.equalsIgnoreCase("true")) {
+                if(xProp.equalsIgnoreCase("t") || xProp.equalsIgnoreCase("true")) {
                     xRenderIgnoreLinuxVersion = true;
                 }
             }
@@ -101,11 +105,11 @@ public final class X11GraphicsEnvironment extends SunGraphicsEnvironment {
 
             // only attempt to initialize GLX if it was requested
             if (glxRequested) {
-                glxAvailable = initGLX();
+                glxAvailable = initGLX(glxRecommended);
                 if (glxVerbose && !glxAvailable) {
                     System.out.println(
-                        "Could not enable OpenGL " +
-                        "pipeline (GLX 1.3 not available)");
+                            "Could not enable OpenGL " +
+                            "pipeline (GLX 1.3 not available)");
                 }
             }
 
@@ -114,7 +118,7 @@ public final class X11GraphicsEnvironment extends SunGraphicsEnvironment {
                 xRenderAvailable = initXRender(xRenderVerbose, xRenderIgnoreLinuxVersion);
                 if (xRenderVerbose && !xRenderAvailable) {
                     System.out.println(
-                                 "Could not enable XRender pipeline");
+                            "Could not enable XRender pipeline");
                 }
             }
 
@@ -124,11 +128,21 @@ public final class X11GraphicsEnvironment extends SunGraphicsEnvironment {
         }
     }
 
+    private static boolean isVMWare() {
+        final String virtName = System.getProperty("jbr.virtualization.information");
+        return virtName != null && virtName.equals("VMWare virtualization");
+    }
+
+    private static boolean openGLRecommended() {
+        final String sessionType = System.getenv("XDG_SESSION_TYPE");
+        return (sessionType != null && sessionType.equals("wayland") && isVMWare());
+    }
+
 
     private static boolean glxAvailable;
     private static boolean glxVerbose;
 
-    private static native boolean initGLX();
+    private static native boolean initGLX(boolean glxRecommended);
 
     public static boolean isGLXAvailable() {
         return glxAvailable;
