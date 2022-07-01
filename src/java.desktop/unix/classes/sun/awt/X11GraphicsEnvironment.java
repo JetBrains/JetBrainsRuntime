68c335ef3e392c7e650de24cb30a2b81aaae9495
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2022-07-01 17:42:34 +0300

    JBR-4544 Enable OpenGL pipeline by default for Wayland sessions
    The OpenGL pipeline is enabled only if all of the following is true:
    - WAYLAND is detected,
    - VMWare virtualization is detected,
    - rendering pipeline is not a software one (llvmpipe).
    As a side effect, a system property 'jbr.virtualization.information'
    is set to the value of detected virtualization type. The value is the
    same as provided by JFR.

--- src/java.desktop/unix/classes/sun/awt/X11GraphicsEnvironment.java
+++ src/java.desktop/unix/classes/sun/awt/X11GraphicsEnvironment.java
@@ -76,6 +76,7 @@ public final class X11GraphicsEnvironment extends SunGraphicsEnvironment {
                 if (!isHeadless()) {
                     // first check the OGL system property
                     boolean glxRequested = false;
+                    boolean glxRecommended = false;
                     String prop = System.getProperty("sun.java2d.opengl");
                     if (prop != null) {
                         if (prop.equals("true") || prop.equals("t")) {
@@ -84,6 +85,9 @@ public final class X11GraphicsEnvironment extends SunGraphicsEnvironment {
                             glxRequested = true;
                             glxVerbose = true;
                         }
+                    } else if (openGLRecommended()) {
+                        glxRequested = true;
+                        glxRecommended = true;
                     }
 
                     // Now check for XRender system property
@@ -108,7 +112,7 @@ public final class X11GraphicsEnvironment extends SunGraphicsEnvironment {
 
                     // only attempt to initialize GLX if it was requested
                     if (glxRequested) {
-                        glxAvailable = initGLX();
+                        glxAvailable = initGLX(glxRecommended);
                         if (glxVerbose && !glxAvailable) {
                             System.out.println(
                                 "Could not enable OpenGL " +
@@ -139,11 +143,21 @@ public final class X11GraphicsEnvironment extends SunGraphicsEnvironment {
 
     }
 
+    private static boolean isVMWare() {
+        final String virtName = System.getProperty("jbr.virtualization.information");
+        return virtName != null && virtName.equals("VMWare virtualization");
+    }
+
+    private static boolean openGLRecommended() {
+        final String sessionType = System.getenv("XDG_SESSION_TYPE");
+        return (sessionType != null && sessionType.equals("wayland") && isVMWare());
+    }
+
 
     private static boolean glxAvailable;
     private static boolean glxVerbose;
 
-    private static native boolean initGLX();
+    private static native boolean initGLX(boolean glxRecommended);
 
     public static boolean isGLXAvailable() {
         return glxAvailable;
