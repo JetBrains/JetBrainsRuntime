3f8ba2a7f1ee5fb823a2cf4fba2e49d2462a6322
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2022-07-25 18:48:39 +0300

    JBR-4665 Focus 'jitter' on window showing in WSLg
    (cherry picked from commit 9b32c2a5773e2808cd8d3531034f6a7d9ab8a319)
    (cherry picked from commit 2952a2eeb49f5a4e50150a459a90a933e18221bc)

--- src/java.desktop/unix/classes/sun/awt/X11/XWindowPeer.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XWindowPeer.java
@@ -1387,7 +1387,7 @@ class XWindowPeer extends XPanelPeer implements WindowPeer,
                 }
             }
         }
-        if (!isSimpleWindow() && shouldFocusOnMapNotify()) {
+        if (!isSimpleWindow() && shouldFocusOnMapNotify() && !XWM.isWeston()) {
             focusLog.fine("Automatically request focus on window");
             requestInitialFocus();
         }
