396f65078460f2a7dad1fed36a1923f6b7e85339
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2022-07-25 18:48:39 +0300

    JBR-4665 Focus 'jitter' on window showing in WSLg
    (cherry picked from commit 9b32c2a5773e2808cd8d3531034f6a7d9ab8a319)

--- src/java.desktop/unix/classes/sun/awt/X11/XWM.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XWM.java
@@ -111,7 +111,8 @@ final class XWM
         UNITY_COMPIZ_WM = 16,
         XMONAD_WM = 17,
         AWESOME_WM = 18,
-        I3_WM = 19;
+        I3_WM = 19,
+        WESTON_WM = 20;
 
     public String toString() {
         switch  (WMID) {
@@ -149,6 +150,8 @@ final class XWM
               return "XMonad";
           case AWESOME_WM:
               return "Awesome";
+          case WESTON_WM:
+              return "Weston";
           case UNDETERMINED_WM:
           default:
               return "Undetermined WM";
@@ -626,6 +629,10 @@ final class XWM
         return isNetWMName("i3");
     }
 
+    static boolean isWeston() {
+        return isNetWMName("Weston");
+    }
+
     static int awtWMNonReparenting = -1;
     static boolean isNonReparentingWM() {
         if (awtWMNonReparenting == -1) {
@@ -831,6 +838,8 @@ final class XWM
                 awt_wmgr = XWM.AWESOME_WM;
             } else if (isI3()) {
                 awt_wmgr = XWM.I3_WM;
+            } else if (isWeston()) {
+                awt_wmgr = XWM.WESTON_WM;
             }
             /*
              * We don't check for legacy WM when we already know that WM
