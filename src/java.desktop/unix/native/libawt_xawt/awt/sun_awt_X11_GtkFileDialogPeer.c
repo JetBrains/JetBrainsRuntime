dc2fdb05fd8bb01e0ceedb1fd5f52b3c60d062b2
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2022-07-27 18:36:30 +0300

    JBR-4673 Focus moves to another application on file dialog closing
    (cherry picked from commit 7468974488ac5c9369d2da9dd29e52b8a610e455)
    (cherry picked from commit 72a74312b740d0d61d08d96e80ee88fe8bbd1d3a)

--- src/java.desktop/unix/native/libawt_xawt/awt/sun_awt_X11_GtkFileDialogPeer.c
+++ src/java.desktop/unix/native/libawt_xawt/awt/sun_awt_X11_GtkFileDialogPeer.c
@@ -46,6 +46,7 @@ static jmethodID filenameFilterCallbackMethodID = NULL;
 static jmethodID setFileInternalMethodID = NULL;
 static jfieldID  widgetFieldID = NULL;
 static jmethodID  setWindowMethodID = NULL;
+static jmethodID  onCloseMethodID = NULL;
 
 JNIEXPORT void JNICALL Java_sun_awt_X11_GtkFileDialogPeer_initIDs
 (JNIEnv *env, jclass cx)
@@ -66,6 +67,9 @@ JNIEXPORT void JNICALL Java_sun_awt_X11_GtkFileDialogPeer_initIDs
 
     setWindowMethodID = (*env)->GetMethodID(env, cx, "setWindow", "(J)Z");
     DASSERT(setWindowMethodID != NULL);
+
+    onCloseMethodID = (*env)->GetMethodID(env, cx, "onClose", "()V");
+    DASSERT(onCloseMethodID != NULL);
 }
 
 static gboolean filenameFilterCallback(const GtkFileFilterInfo * filter_info, gpointer obj)
@@ -92,6 +96,8 @@ static void quit(JNIEnv * env, jobject jpeer, gboolean isSignalHandler)
     GtkWidget * dialog = (GtkWidget*)jlong_to_ptr(
             (*env)->GetLongField(env, jpeer, widgetFieldID));
 
+    (*env)->CallVoidMethod(env, jpeer, onCloseMethodID);
+
     if (dialog != NULL)
     {
         // Callbacks from GTK signals are made within the GTK lock
@@ -105,8 +111,6 @@ static void quit(JNIEnv * env, jobject jpeer, gboolean isSignalHandler)
         gtk->gtk_widget_destroy (dialog);
 
         gtk->gtk_main_quit ();
-
-        (*env)->SetLongField(env, jpeer, widgetFieldID, 0);
 
         if (!isSignalHandler) {
             gtk->gdk_threads_leave();
