f7d53d70ce8eb6eb53774d9ee736a7ac907ce38d
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2022-08-23 08:52:23 +0700

    JBR-4754 make root directory with the same name as archive name
    (cherry picked from commit 97a17c9a36376584a79d630da17abd9486fb3fce)

--- jb/project/tools/windows/scripts/mkimages_x86.sh
+++ jb/project/tools/windows/scripts/mkimages_x86.sh
@@ -47,7 +47,7 @@ function create_image_bundle {
   fastdebug_infix=''
 
   [ "$bundle_type" == "fd" ] && [ "$__arch_name" == "$JBRSDK_BUNDLE" ] && __bundle_name=$__arch_name && fastdebug_infix="fastdebug-"
-  __root_dir=${__bundle_name}-${JBSDK_VERSION}-x86-${fastdebug_infix:-}b${build_number%%.*}
+  __root_dir=${__bundle_name}-${JBSDK_VERSION}-windows-x86-${fastdebug_infix}b${build_number}
 
   echo Running jlink ...
   ${JSDK}/bin/jlink \
