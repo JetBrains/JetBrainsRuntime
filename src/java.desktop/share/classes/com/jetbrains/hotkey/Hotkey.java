f76704f8d5d554e5f0404fa98ccdb0c7422ebf73
Nikita Tsarev <nikts53@gmail.com>
2022-08-24 22:58:08 +0300

    Add toString implementation to Hotkey

--- src/java.desktop/share/classes/com/jetbrains/hotkey/Hotkey.java
+++ src/java.desktop/share/classes/com/jetbrains/hotkey/Hotkey.java
@@ -1,5 +1,6 @@
 package com.jetbrains.hotkey;
 
+import java.awt.event.KeyEvent;
 import java.util.Set;
 
 public record Hotkey(int keyCode, Set<Modifier> modifiers) {
@@ -8,5 +9,29 @@ public record Hotkey(int keyCode, Set<Modifier> modifiers) {
         CONTROL,
         SHIFT,
         SUPER,
+    }
+
+    @Override
+    public String toString() {
+        var result = new StringBuilder();
+
+        if (modifiers.contains(Modifier.SUPER)) {
+            result.append("Super+");
+        }
+
+        if (modifiers.contains(Modifier.CONTROL)) {
+            result.append("Ctrl+");
+        }
+
+        if (modifiers.contains(Modifier.ALT)) {
+            result.append("Alt+");
+        }
+
+        if (modifiers.contains(Modifier.SHIFT)) {
+            result.append("Shift+");
+        }
+
+        result.append(KeyEvent.getKeyText(keyCode));
+        return result.toString();
     }
 }
