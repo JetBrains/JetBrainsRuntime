ade4bb1cba37cbdee0b3cb680ad8276a75d33e92
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2022-09-05 23:12:00 +0300

    Allow registering multiple implementations for JBR API services
    This greatly reduces boilerplate when dealing with platform-specific code, one can specify multiple target implementation classes, one per platform. First found will be used.
    
    Also added JBRApi.ServiceNotAvailableException to give services ability to validate any custom condition they want.

--- test/jdk/jb/java/api/backend/com/jetbrains/jbr/ProxyInfoResolving.java
+++ test/jdk/jb/java/api/backend/com/jetbrains/jbr/ProxyInfoResolving.java
@@ -27,4 +27,5 @@ public class ProxyInfoResolving {
     public static class ClientProxyClass {}
     public static class ProxyClassImpl {}
     public static class ValidApiImpl {}
+    public static class ValidApi2Impl {}
 }
