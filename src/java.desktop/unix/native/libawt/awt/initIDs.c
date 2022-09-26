e4f6adf309038dd6e8d719b466b452242d7f801d
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2022-09-26 16:25:24 +0300

    JBR-4839 Report if wrong shared library is detected at run time

--- src/java.desktop/unix/native/libawt/awt/initIDs.c
+++ src/java.desktop/unix/native/libawt/awt/initIDs.c
@@ -33,6 +33,10 @@
 
 #include "jni_util.h"
 
+#include <stdlib.h>
+#include <string.h>
+#include <link.h>
+
 /*
  * This file contains stubs for JNI field and method id initializers
  * which are used in the win32 awt.
@@ -90,4 +94,73 @@ JNIEXPORT void JNICALL
 Java_java_awt_event_MouseEvent_initIDs
   (JNIEnv *env, jclass clazz)
 {
+}
+
+struct shared_libs {
+    int      count;
+    int      index;
+    char **  names;
+};
+
+static int
+dl_iterate_callback
+    (struct dl_phdr_info *info, size_t size, void *data)
+{
+    struct shared_libs *libs = (struct shared_libs*)data;
+    if (libs->names == NULL) {
+        libs->count++;
+    } else {
+        // The number of libraries may have grown since the last time we asked.
+        if (libs->index < libs->count) {
+            libs->names[libs->index++] = strdup(info->dlpi_name);
+        }
+    }
+
+    return 0;
+}
+
+static jarray
+convert_to_java_array
+    (JNIEnv *env, struct shared_libs* libs)
+{
+    jclass stringClazz = (*env)->FindClass(env, "java/lang/String");
+    CHECK_NULL_RETURN(stringClazz, NULL);
+    jarray libsArray = (*env)->NewObjectArray(env, libs->count, stringClazz, NULL);
+    JNU_CHECK_EXCEPTION_RETURN(env, NULL);
+
+    for (uint32_t i = 0; i < libs->count; i++) {
+        const char * name = libs->names[i];
+        if (name) {
+            jstring jName = (*env)->NewStringUTF(env, name);
+            JNU_CHECK_EXCEPTION_RETURN(env, NULL);
+            (*env)->SetObjectArrayElement(env, libsArray, i, jName);
+            JNU_CHECK_EXCEPTION_RETURN(env, NULL);
+        }
+    }
+
+    return libsArray;
+}
+
+JNIEXPORT jarray JNICALL
+Java_sun_font_FontManagerNativeLibrary_loadedLibraries
+    (JNIEnv *env, jclass cls)
+{
+    struct shared_libs libs = {0, 0, NULL};
+    dl_iterate_phdr(&dl_iterate_callback, &libs);
+    if (libs.count <= 0) {
+        return NULL;
+    }
+
+    libs.names = (char **) calloc(libs.count, sizeof(libs.names[0]));
+    CHECK_NULL_RETURN(libs.names, NULL);
+    dl_iterate_phdr(&dl_iterate_callback, &libs);
+
+    jarray libsArray = convert_to_java_array(env, &libs);
+
+    for (uint32_t i = 0; i < libs.count; i++) {
+        free(libs.names[i]);
+    }
+    free(libs.names);
+
+    return libsArray;
 }
