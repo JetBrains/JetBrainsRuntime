33da68e7a7adef62d32eedf93fd42df3b001b5ac
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2022-10-05 10:27:31 +0200

    IDEA-141456 Multimonitor HIDPI support for Linux
    (cherry picked from commit 980c18ae92ebb98c5f24cc06a3978e70c4fbc21a)

--- src/java.desktop/unix/classes/sun/awt/X11/XComponentPeer.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XComponentPeer.java
@@ -1381,7 +1381,10 @@ public class XComponentPeer extends XWindow implements ComponentPeer, DropTarget
 
                     int scale = getScale();
                     if (scale != 1) {
-                        shape = shape.getScaledRegion(scale, scale);
+                        Rectangle bounds = graphicsConfig.getBounds();
+                        shape = shape.getTranslatedRegion(-bounds.x, -bounds.y)
+                                .getScaledRegion(scale, scale)
+                                .getTranslatedRegion(bounds.x, bounds.y);
                     }
 
                     XlibWrapper.SetRectangularShape(
@@ -1430,7 +1433,7 @@ public class XComponentPeer extends XWindow implements ComponentPeer, DropTarget
         }
 
         initGraphicsConfiguration();
-        doValidateSurface();
+        syncBounds();
         return false;
     }
 }
