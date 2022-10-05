119e4e9599c57bee4ae4cddb2f186d80f6ac16b5
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2022-10-05 10:27:31 +0200

    IDEA-141456 Multimonitor HIDPI support for Linux

--- src/java.desktop/unix/classes/sun/awt/X11/XDecoratedPeer.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XDecoratedPeer.java
@@ -861,17 +861,19 @@ abstract class XDecoratedPeer extends XWindowPeer {
                     currentInsets, newDimensions);
         }
 
-        checkIfOnNewScreen(newDimensions.getBounds());
+        checkIfOnNewScreen(newDimensions.getBounds(), () -> {
 
-        Point oldLocation = getLocation();
-        dimensions = newDimensions;
-        if (!newLocation.equals(oldLocation)) {
-            handleMoved(newDimensions);
-        }
-        reconfigureContentWindow(newDimensions);
-        updateChildrenSizes();
+            Point oldLocation = getLocation();
+            dimensions = newDimensions;
+            if (!newLocation.equals(oldLocation)) {
+                handleMoved(newDimensions);
+            }
+            reconfigureContentWindow(newDimensions);
+            updateChildrenSizes();
 
-        repositionSecurityWarning();
+            repositionSecurityWarning();
+
+        });
     }
 
     private void checkShellRectSize(Rectangle shellRect) {
@@ -899,7 +901,8 @@ abstract class XDecoratedPeer extends XWindowPeer {
         }
         updateSizeHints(rec.x, rec.y, rec.width, rec.height);
         XlibWrapper.XMoveResizeWindow(XToolkit.getDisplay(), getShell(),
-                                      scaleUp(rec.x), scaleUp(rec.y),
+                                      parentWindow == null ? scaleUpX(rec.x) : scaleUp(rec.x),
+                                      parentWindow == null ? scaleUpY(rec.y) : scaleUp(rec.y),
                                       scaleUp(rec.width), scaleUp(rec.height));
     }
 
@@ -918,7 +921,8 @@ abstract class XDecoratedPeer extends XWindowPeer {
         }
         updateSizeHints(rec.x, rec.y, rec.width, rec.height);
         XlibWrapper.XMoveWindow(XToolkit.getDisplay(), getShell(),
-                                scaleUp(rec.x), scaleUp(rec.y));
+                parentWindow == null ? scaleUpX(rec.x) : scaleUp(rec.x),
+                parentWindow == null ? scaleUpY(rec.y) : scaleUp(rec.y));
     }
 
     public void setResizable(boolean resizable) {
@@ -1395,5 +1399,16 @@ abstract class XDecoratedPeer extends XWindowPeer {
 
     public final boolean getWindowTitleVisible() {
         return getMWMDecorTitleProperty().orElse(true);
+    }
+
+    @Override
+    public boolean updateGraphicsData(GraphicsConfiguration gc) {
+        boolean ret = super.updateGraphicsData(gc);
+        if (content != null) {
+            content.initGraphicsConfiguration();
+            content.syncBounds();
+        }
+        updateMinimumSize();
+        return ret;
     }
 }
