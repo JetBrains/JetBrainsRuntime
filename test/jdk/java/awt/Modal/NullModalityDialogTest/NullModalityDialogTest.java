0013d849ff851fe180bfb0b6aeb5e868cf042568
Joshua Cao <joshcao@amazon.com>
2022-10-06 22:34:45 +0000

    8233565: [TESTBUG] NullModalityDialogTest.java fails on MacOS
    Reviewed-by: phh
    Backport-of: 2b5869ad09e8f34277a4c965abbaed3f79fc5b59

--- test/jdk/java/awt/Modal/NullModalityDialogTest/NullModalityDialogTest.java
+++ test/jdk/java/awt/Modal/NullModalityDialogTest/NullModalityDialogTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -82,7 +82,8 @@ public class NullModalityDialogTest {
     NullModalityDialogTest() throws Exception {
 
         robot = new ExtendedRobot();
-        EventQueue.invokeLater(this::createGUI);
+        robot.setAutoDelay(100);
+        EventQueue.invokeAndWait(this::createGUI);
     }
 
     private void createGUI() {
@@ -133,7 +134,9 @@ public class NullModalityDialogTest {
 
         dialog.openGained.reset();
 
-        robot.type(KeyEvent.VK_TAB);
+        robot.keyPress(KeyEvent.VK_TAB);
+        robot.keyRelease(KeyEvent.VK_TAB);
+        robot.waitForIdle();
 
         dialog.openGained.waitForFlagTriggered();
         assertTrue(dialog.openGained.flag(),
