a48bd8734857eb29a2cac3879e737383759baf93
Goetz Lindenmaier <goetz@openjdk.org>
2022-10-10 07:37:08 +0000

    8249694: java/lang/StringBuffer/HugeCapacity.java and j/l/StringBuilder/HugeCapacity.java tests shouldn't be @ignore-d
    Remove @ignore since @requires os.maxMemory is enough to ensure tests will not be executed if memory requirements are not satisfied
    
    Reviewed-by: clanger
    Backport-of: 0dadf81e149c4a6f5496dd23675e3986efe157f3

--- test/jdk/java/lang/StringBuilder/HugeCapacity.java
+++ test/jdk/java/lang/StringBuilder/HugeCapacity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,8 +26,8 @@
  * @bug 8149330
  * @summary Capacity should not get close to Integer.MAX_VALUE unless
  *          necessary
+ * @requires (sun.arch.data.model == "64" & os.maxMemory >= 6G)
  * @run main/othervm -Xmx5G HugeCapacity
- * @ignore This test has huge memory requirements
  */
 
 public class HugeCapacity {
