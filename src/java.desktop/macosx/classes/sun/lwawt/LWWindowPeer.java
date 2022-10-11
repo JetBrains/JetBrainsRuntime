ed6db4e6c34942d7da2152ab2b347ac21c959205
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2022-10-11 08:36:14 +0200

    JBR-4897 Reconnecting multiscreen monitor displays blank IDE
    Resolved race condition in window peer displayChanged listener
    
    UPD. And introduced another one, see investigation related to JBR-9998

--- src/java.desktop/macosx/classes/sun/lwawt/LWWindowPeer.java
+++ src/java.desktop/macosx/classes/sun/lwawt/LWWindowPeer.java
@@ -1106,10 +1106,13 @@ public class LWWindowPeer
                 setPlatformMaximizedBounds(getDefaultMaximizedBounds());
             }
         }
-        // Replace surface unconditionally, because internal state of the
-        // GraphicsDevice could be changed.
-        replaceSurfaceData();
-        repaintPeer();
+
+        SunToolkit.executeOnEventHandlerThread(getTarget(), () -> {
+            // Replace surface unconditionally, because internal state of the
+            // GraphicsDevice could be changed.
+            replaceSurfaceData();
+            repaintPeer();
+        });
     }
 
     @Override
