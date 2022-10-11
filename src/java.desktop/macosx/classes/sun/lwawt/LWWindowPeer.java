3bab7e9d6cb6c7a000c7cadb84dd33e658753fa1
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2022-10-11 08:36:14 +0200

    JBR-4897 Reconnecting multiscreen monitor displays blank IDE
    Handled NSWindowDidChangeScreenNotification, resolved race condition in window peer displayChanged listener
    
    (cherry picked from commit 1e697c8623e581df80126428bfbce665c032fe15)

--- src/java.desktop/macosx/classes/sun/lwawt/LWWindowPeer.java
+++ src/java.desktop/macosx/classes/sun/lwawt/LWWindowPeer.java
@@ -1107,10 +1107,13 @@ public class LWWindowPeer
                 setPlatformMaximizedBounds(getDefaultMaximizedBounds());
             }
         }
-        // Replace surface unconditionally, because internal state of the
-        // GraphicsDevice could be changed.
-        replaceSurfaceData();
-        repaintPeer();
+
+        SunToolkit.executeOnEventHandlerThread(getTarget(), () -> {
+            // Replace surface unconditionally, because internal state of the
+            // GraphicsDevice could be changed.
+            replaceSurfaceData();
+            repaintPeer();
+        });
     }
 
     @Override
