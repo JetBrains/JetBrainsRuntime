1abecf69ccfb06aba9c9feecfc94e059d24b7ccd
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2022-11-07 11:53:05 +0700

    JBR-4986 Revert "8265360: several compiler/whitebox tests fail with "private compiler.whitebox.SimpleTestCaseHelper(int) must be compiled""
    This reverts commit dc13aa574d38f1323bb34fd7a29a4d6b59f8138c.

--- src/hotspot/share/compiler/compilerDefinitions.cpp
+++ src/hotspot/share/compiler/compilerDefinitions.cpp
@@ -267,11 +267,12 @@ void CompilerConfig::set_legacy_emulation_flags() {
         FLAG_SET_ERGO(Tier0BackedgeNotifyFreqLog, MAX2<intx>(10, osr_threshold_log));
       }
       // Adjust the tiered policy flags to approximate the legacy behavior.
-      FLAG_SET_ERGO(Tier3InvocationThreshold, threshold);
-      FLAG_SET_ERGO(Tier3MinInvocationThreshold, threshold);
-      FLAG_SET_ERGO(Tier3CompileThreshold, threshold);
-      FLAG_SET_ERGO(Tier3BackEdgeThreshold, osr_threshold);
-      if (CompilerConfig::is_c2_or_jvmci_compiler_only()) {
+      if (CompilerConfig::is_c1_only()) {
+        FLAG_SET_ERGO(Tier3InvocationThreshold, threshold);
+        FLAG_SET_ERGO(Tier3MinInvocationThreshold, threshold);
+        FLAG_SET_ERGO(Tier3CompileThreshold, threshold);
+        FLAG_SET_ERGO(Tier3BackEdgeThreshold, osr_threshold);
+      } else {
         FLAG_SET_ERGO(Tier4InvocationThreshold, threshold);
         FLAG_SET_ERGO(Tier4MinInvocationThreshold, threshold);
         FLAG_SET_ERGO(Tier4CompileThreshold, threshold);
@@ -338,20 +339,6 @@ void CompilerConfig::set_compilation_policy_flags() {
     if (FLAG_IS_DEFAULT(Tier4BackEdgeThreshold)) {
       FLAG_SET_DEFAULT(Tier4BackEdgeThreshold, 15000);
     }
-
-    if (FLAG_IS_DEFAULT(Tier3InvocationThreshold)) {
-      FLAG_SET_DEFAULT(Tier3InvocationThreshold, Tier4InvocationThreshold);
-    }
-    if (FLAG_IS_DEFAULT(Tier3MinInvocationThreshold)) {
-      FLAG_SET_DEFAULT(Tier3MinInvocationThreshold, Tier4MinInvocationThreshold);
-    }
-    if (FLAG_IS_DEFAULT(Tier3CompileThreshold)) {
-      FLAG_SET_DEFAULT(Tier3CompileThreshold, Tier4CompileThreshold);
-    }
-    if (FLAG_IS_DEFAULT(Tier3BackEdgeThreshold)) {
-      FLAG_SET_DEFAULT(Tier3BackEdgeThreshold, Tier4BackEdgeThreshold);
-    }
-
   }
 
   // Scale tiered compilation thresholds.
