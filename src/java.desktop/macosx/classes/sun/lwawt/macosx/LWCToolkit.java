c26be1b74d1e2c140c596a1b64769494abbb174d
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2022-11-11 09:47:26 +0700

    JBR-3575 use flag processEvents in LWCToolkit.invokeAndWait

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/LWCToolkit.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/LWCToolkit.java
@@ -744,6 +744,12 @@ public final class LWCToolkit extends LWToolkit {
     public static void invokeAndWait(Runnable runnable, Component component, int timeoutSeconds)
             throws InvocationTargetException
     {
+        invokeAndWait(runnable, component, false, -1);
+    }
+
+    public static void invokeAndWait(Runnable runnable, Component component, boolean processEvents, int timeoutSeconds)
+            throws InvocationTargetException
+    {
         if (log.isLoggable(PlatformLogger.Level.FINE)) {
             log.fine("invokeAndWait started: " + runnable);
         }
@@ -792,7 +798,7 @@ public final class LWCToolkit extends LWToolkit {
             invocationEvent.onDone(() -> eventDispatchThreadFreeFuture.cancel(false));
         }
 
-        if (!doAWTRunLoop(mediator, false, timeoutSeconds)) {
+        if (!doAWTRunLoop(mediator, processEvents, timeoutSeconds)) {
             invocationEvent.dispose("InvocationEvent has timed out");
         }
 
