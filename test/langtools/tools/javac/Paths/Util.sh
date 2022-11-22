3bdd37064e111d322b856e8afb2d25a97f812970
George Adams <gdams@openjdk.org>
2022-11-22 12:13:26 +0000

    8287895: Some langtools tests fail on msys2
    Backport-of: f7791ad0ea984d49ff26e6f30233d8dcee4305b8

--- test/langtools/tools/javac/Paths/Util.sh
+++ test/langtools/tools/javac/Paths/Util.sh
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2003, 2016, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2003, 2022, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # This code is free software; you can redistribute it and/or modify it
@@ -30,7 +30,7 @@
 jimage="${TESTJAVA+${TESTJAVA}/bin/}jimage"
 
 case `uname -s` in
-  Windows*|CYGWIN*)
+  Windows*|CYGWIN*|MSYS*|MINGW*)
     WindowsOnly() { "$@"; }
     UnixOnly() { :; }
     PS=";" ;;
