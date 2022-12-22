4aea12c3018d100632db6be632b9acf291edf391
Nikita Gubarkov <Nikita.Gubarkov@jetbrains.com>
2022-12-22 16:05:12 +0100

    JBR-5124 Rewrite custom decorations support
    JBR API v0.0.8
    Added new WindowDecorations API, deprecated old CustomWindowDecoration.
    
    JBR-4641 JBR-4630 Fix client area calculation with custom decorations on Windows.
    
    - Window insets are rounded up, which causes visible & unusable border in fullscreen on some scales, round down instead.
    
    - Clipping in Swing components sometimes cuts what it shouldn't, fixed.
    
    (cherry picked from commit 03d850e722d86982c29a2ecdd2fea5d37655c139)

--- src/java.desktop/windows/native/libawt/windows/awt_DesktopProperties.cpp
+++ src/java.desktop/windows/native/libawt/windows/awt_DesktopProperties.cpp
@@ -49,6 +49,8 @@ jmethodID AwtDesktopProperties::setColorPropertyID = 0;
 jmethodID AwtDesktopProperties::setFontPropertyID = 0;
 jmethodID AwtDesktopProperties::setSoundPropertyID = 0;
 
+BOOL AppsUseLightThemeCached = TRUE;
+
 AwtDesktopProperties::AwtDesktopProperties(jobject self) {
     this->self = GetEnv()->NewGlobalRef(self);
     GetEnv()->SetLongField( self, AwtDesktopProperties::pDataID,
@@ -699,15 +701,15 @@ void AwtDesktopProperties::GetOtherParameters() {
         // Add property for light/dark theme detection
         value = getWindowsPropFromReg(TEXT("Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize"),
                                       TEXT("AppsUseLightTheme"), &valueType);
+        BOOL lightTheme = TRUE;
         if (value != NULL) {
             if (valueType == REG_DWORD) {
-                SetBooleanProperty(TEXT("win.lightTheme.on"), (BOOL)((int)*value == 1));
+                lightTheme = (BOOL)((int)*value == 1);
             }
             free(value);
         }
-        else {
-            SetBooleanProperty(TEXT("win.lightTheme.on"), TRUE);
-        }
+        SetBooleanProperty(TEXT("win.lightTheme.on"), lightTheme);
+        AppsUseLightThemeCached = lightTheme;
 
     }
     catch (std::bad_alloc&) {
