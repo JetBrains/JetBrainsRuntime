b704844450cfebb59a6f0fd199bc816b88ddaf1a
Sergey Bylokhov <serb@openjdk.org>
2022-12-29 21:29:10 +0000

    8288854: getLocalGraphicsEnvironment() on for multi-screen setups throws exception NPE
    Backport-of: cfc9a881afd300bd7c1ce784287d1669308e89fc

--- src/java.desktop/unix/classes/sun/awt/X11GraphicsEnvironment.java
+++ src/java.desktop/unix/classes/sun/awt/X11GraphicsEnvironment.java
@@ -248,7 +248,7 @@ public final class X11GraphicsEnvironment extends SunGraphicsEnvironment {
             throw new AWTError("no screen devices");
         }
         int index = getDefaultScreenNum();
-        mainScreen = 0 < index && index < screens.length ? index : 0;
+        mainScreen = 0 < index && index < numScreens ? index : 0;
 
         updateWaylandMonitorScaling();
 
