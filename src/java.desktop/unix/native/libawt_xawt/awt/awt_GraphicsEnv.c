c898a3ed8863b3dff61bd2b8d7b64caab8371d3f
Goetz Lindenmaier <goetz@openjdk.org>
2023-01-02 11:41:52 +0000

    8284023: java.sun.awt.X11GraphicsDevice.getDoubleBufferVisuals() leaks XdbeScreenVisualInfo
    Backport-of: ec205f68a883cef6b98f26a06baf675f7da26928

--- src/java.desktop/unix/native/libawt_xawt/awt/awt_GraphicsEnv.c
+++ src/java.desktop/unix/native/libawt_xawt/awt/awt_GraphicsEnv.c
@@ -1640,6 +1640,9 @@ Java_sun_awt_X11GraphicsDevice_getDoubleBufferVisuals(JNIEnv *env,
             break;
         }
     }
+    AWT_LOCK();
+    XdbeFreeVisualInfo(visScreenInfo);
+    AWT_UNLOCK();
 #endif /* !HEADLESS */
 }
 
