d1f6bd1690f995bfce5db1724e76c6b152c3aa91
Goetz Lindenmaier <goetz@openjdk.org>
2023-01-02 11:44:49 +0000

    8285399: JNI exception pending in awt_GraphicsEnv.c:1432
    Backport-of: c156bcc599534ae989bc9cbd001e7c150da8096c

--- src/java.desktop/unix/native/libawt_xawt/awt/awt_GraphicsEnv.c
+++ src/java.desktop/unix/native/libawt_xawt/awt/awt_GraphicsEnv.c
@@ -1635,10 +1635,10 @@ Java_sun_awt_X11GraphicsDevice_getDoubleBufferVisuals(JNIEnv *env,
     AWT_FLUSH_UNLOCK();
     for (i = 0; i < visScreenInfo->count; i++) {
         XdbeVisualInfo* visInfo = visScreenInfo->visinfo;
-        (*env)->CallVoidMethod(env, this, midAddVisual, (visInfo[i]).visual);
         if ((*env)->ExceptionCheck(env)) {
             break;
         }
+        (*env)->CallVoidMethod(env, this, midAddVisual, (visInfo[i]).visual);
     }
     AWT_LOCK();
     XdbeFreeVisualInfo(visScreenInfo);
