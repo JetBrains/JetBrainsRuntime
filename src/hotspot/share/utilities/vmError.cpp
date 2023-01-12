c99a576581876beb6138d4850f43f5ede2116cb8
Thomas Stuefe <stuefe@openjdk.org>
2023-01-12 07:50:16 +0000

    8284726: Print active locale settings in hs_err reports and in VM.info
    Backport-of: 6ce4e755a47daa980e522faa27a059cc9df5c304

--- src/hotspot/share/utilities/vmError.cpp
+++ src/hotspot/share/utilities/vmError.cpp
@@ -1036,6 +1036,15 @@ void VMError::report(outputStream* st, bool _verbose) {
        st->cr();
      }
 
+#ifndef _WIN32
+  STEP("printing locale settings")
+
+     if (_verbose) {
+       os::Posix::print_active_locale(st);
+       st->cr();
+     }
+#endif
+
   STEP("printing signal handlers")
 
      if (_verbose) {
@@ -1232,6 +1241,12 @@ void VMError::print_vm_info(outputStream* st) {
 
   os::print_environment_variables(st, env_list);
   st->cr();
+
+  // STEP("printing locale settings")
+#ifndef _WIN32
+  os::Posix::print_active_locale(st);
+  st->cr();
+#endif
 
   // STEP("printing signal handlers")
 
