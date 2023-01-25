5bde174ce6ef989f7114f8c06f208898d840a125
Artem Semenov <36081106+savoptik@users.noreply.github.com>
2023-01-25 18:28:51 +0300

    JBR-4170 Implement API for announcing

--- src/java.desktop/macosx/native/libawt_lwawt/awt/JavaAccessibilityUtilities.h
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/JavaAccessibilityUtilities.h
@@ -28,7 +28,9 @@
 extern NSString *const JavaAccessibilityIgnore;
 
 extern NSMutableDictionary *sRoles;
+extern NSMutableDictionary *sAnnouncePriorities;
 extern void initializeRoles();
+extern void initializeAnnouncePriorities();
 
 #define GET_CACCESSIBILITY_CLASS() \
     GET_CLASS(sjc_CAccessibility, "sun/lwawt/macosx/CAccessibility");
