901a001b361305bc4e0ddfa4d71b8c7aa695f2e4
AMPivovarov <AMPivovarov@gmail.com>
2023-01-30 13:13:28 +0100

    JBR-5213 JBR API v0.0.9 - add GraphicsUtils (#208)
    * relax type constraints in BltBufferStrategy.getDrawGraphics
    
    (cherry picked from commit 715b615d882988b429bb4ced0bfbf84322ae7016)

--- src/java.desktop/share/classes/java/awt/Component.java
+++ src/java.desktop/share/classes/java/awt/Component.java
@@ -4417,10 +4417,10 @@ public abstract class Component implements ImageObserver, MenuContainer,
             if (backBuffer == null) {
                 return getGraphics();
             }
-            SunGraphics2D g = (SunGraphics2D)backBuffer.getGraphics();
-            g.constrain(-insets.left, -insets.top,
-                        backBuffer.getWidth(null) + insets.left,
-                        backBuffer.getHeight(null) + insets.top);
+            Graphics g = backBuffer.getGraphics();
+            ((ConstrainableGraphics)g).constrain(-insets.left, -insets.top,
+                                                 backBuffer.getWidth(null) + insets.left,
+                                                 backBuffer.getHeight(null) + insets.top);
             return g;
         }
 
