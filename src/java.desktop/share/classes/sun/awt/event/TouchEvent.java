4a3ba19747e128c7dbcc706b873ac10c7d789e01
Nikita Tsarev <nikita.tsarev@jetbrains.com>
2023-01-31 17:32:58 +0100

    JBR-5173 macOS keyboard support rewrite
    with fix for JBR-5300 Change source code and test files to use GPL license

--- src/java.desktop/share/classes/sun/awt/event/TouchEvent.java
+++ src/java.desktop/share/classes/sun/awt/event/TouchEvent.java
@@ -23,5 +23,5 @@ public class TouchEvent {
 
     @Native
     public final static boolean defaultTouchHandling = "true".equalsIgnoreCase(
-            Util.getProperty(defaultTouchHandlingOption, "false"));
+            System.getProperty(defaultTouchHandlingOption, "false"));
 }
