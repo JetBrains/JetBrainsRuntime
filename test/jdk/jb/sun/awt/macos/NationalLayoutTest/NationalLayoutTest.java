846705cd37371870efff2ba3ddf3415d73af7c90
Nikita Tsarev <nikita.tsarev@jetbrains.com>
2023-01-31 17:32:58 +0100

    JBR-5173 macOS keyboard support rewrite
    with fix for JBR-5300 Change source code and test files to use GPL license
    
    (cherry picked from commit 88c281a243ea77c75f7e9518f8e4a29366d312fd)

--- test/jdk/jb/sun/awt/macos/NationalLayoutTest/NationalLayoutTest.java
+++ test/jdk/jb/sun/awt/macos/NationalLayoutTest/NationalLayoutTest.java
@@ -400,11 +400,6 @@ public class NationalLayoutTest {
             // Get Key object from LayoutKey enum
             Key key = layoutKey.getKey();
 
-            if (key.isDead() || key.isDead(modifier)) {
-                // TODO: Disable skipping testing dead keys once they work properly on macOS
-                continue;
-            }
-
             // Obtain the key code for the current layout
             int keyCode = key.getKeyCode();
 
