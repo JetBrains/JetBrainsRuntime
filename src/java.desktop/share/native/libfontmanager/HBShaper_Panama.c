57addfb68d0f86201cf446250b3ba808655b5726
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2023-02-01 16:33:10 +0100

    Emoji PR sync (experimental)
    - Removed font fallback hacks.
    - Changed composite font glyph code encoding scheme, refactored to use Font2D instead of PhysicalFont.
    - New Emoji logical font type with 2 underlying physical fonts.
    - Removed getGlyphVectorOutline - it's unused and broken.
    - Got rid of charsToGlyphs[NS] boilerplate.
    - Moved emoji tests to separate directory.
    
    (cherry picked from commit 485bdd1d1abe338b06c1e460f95f51fa7336a4a2)

--- src/java.desktop/share/native/libfontmanager/HBShaper_Panama.c
+++ src/java.desktop/share/native/libfontmanager/HBShaper_Panama.c
@@ -76,6 +76,7 @@ JDKEXPORT void jdk_hb_shape(
      int ltrDirection,
      const char *features,
      int slot,
+     int slotShift,
      hb_font_funcs_t* font_funcs,
      store_layoutdata_func_t store_layout_results_fn) {
 
@@ -111,7 +112,7 @@ JDKEXPORT void jdk_hb_shape(
      glyphPos = hb_buffer_get_glyph_positions(buffer, &buflen);
 
      (*store_layout_results_fn)
-               (slot, baseIndex, offset, startX, startY, devScale,
+               (slot, slotShift, baseIndex, offset, startX, startY, devScale,
                 charCount, glyphCount, glyphInfo, glyphPos);
 
      hb_buffer_destroy (buffer);
