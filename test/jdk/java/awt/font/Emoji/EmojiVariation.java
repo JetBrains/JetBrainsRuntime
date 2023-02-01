261afebffa05185918d380194793d292ae5c0929
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2023-02-01 16:33:10 +0100

    Emoji PR sync (experimental)
    - Removed font fallback hacks.
    - Changed composite font glyph code encoding scheme, refactored to use Font2D instead of PhysicalFont.
    - New Emoji logical font type with 2 underlying physical fonts.
    - Removed getGlyphVectorOutline - it's unused and broken.
    - Got rid of charsToGlyphs[NS] boilerplate.
    - Moved emoji tests to separate directory.

--- test/jdk/java/awt/font/Emoji/EmojiVariation.java
+++ test/jdk/java/awt/font/Emoji/EmojiVariation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 JetBrains s.r.o.
+ * Copyright 2021-2023 JetBrains s.r.o.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,6 +23,8 @@
 
 /*
  * @test
+ * @key headful
+ * @bug 8269806
  * @summary Checks that variation selectors work.
  */
 
@@ -106,12 +108,16 @@ public class EmojiVariation {
         BufferedImage img = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, BufferedImage.TYPE_INT_RGB);
         String errors = "";
         for (String s : SYMBOLS) {
-            if (s == null) continue;
+            if (s == null) {
+                continue;
+            }
             errors += test(img, s, Variation.DEFAULT);
             errors += test(img, s, Variation.MONO);
             errors += test(img, s, Variation.COLOR);
         }
-        if (!errors.isEmpty()) throw new RuntimeException(errors);
+        if (!errors.isEmpty()) {
+            throw new RuntimeException(errors);
+        }
     }
 
     private static String test(BufferedImage img, String symbol, Variation variation) {
@@ -166,10 +172,15 @@ public class EmojiVariation {
     private static void requireFont(String macOS, String windows, String linux) {
         String os = System.getProperty("os.name").toLowerCase();
         String font;
-        if (os.contains("mac")) font = macOS;
-        else if (os.contains("windows")) font = windows;
-        else if (os.contains("linux")) font = linux;
-        else return;
+        if (os.contains("mac")) {
+            font = macOS;
+        } else if (os.contains("windows")) {
+            font = windows;
+        } else if (os.contains("linux")) {
+            font = linux;
+        } else {
+            return;
+        }
         String[] fs = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
         if (Stream.of(fs).noneMatch(s -> s.equals(font))) {
             throw new Error("Required font not found: " + font);
