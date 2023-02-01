7c6ad5dab306e2c584ed6433a056ab784e0f58e9
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2023-02-01 16:33:10 +0100

    Emoji PR sync (experimental)
    - Removed font fallback hacks.
    - Changed composite font glyph code encoding scheme, refactored to use Font2D instead of PhysicalFont.
    - New Emoji logical font type with 2 underlying physical fonts.
    - Removed getGlyphVectorOutline - it's unused and broken.
    - Got rid of charsToGlyphs[NS] boilerplate.
    - Moved emoji tests to separate directory.
    
    (cherry picked from commit 485bdd1d1abe338b06c1e460f95f51fa7336a4a2)

--- src/java.desktop/share/classes/sun/font/FileFontStrike.java
+++ src/java.desktop/share/classes/sun/font/FileFontStrike.java
@@ -482,16 +482,17 @@ public class FileFontStrike extends PhysicalStrike {
 
     /* The following method is called from CompositeStrike as a special case.
      */
-    int getSlot0GlyphImagePtrs(int[] glyphCodes, long[] images, int len) {
+    int getSlot0GlyphImagePtrs(int[] glyphCodes, long[] images, int len, int slotMask, int slotShift) {
 
         int convertedCnt = 0;
 
         for (int i=0; i<len; i++) {
             int glyphCode = glyphCodes[i];
-            if (glyphCode >>> 24 != 0) {
+            if ((glyphCode & slotMask) != 0) {
                 return convertedCnt;
             } else {
                 convertedCnt++;
+                glyphCode >>>= slotShift;
             }
             if (glyphCode >= INVISIBLE_GLYPHS) {
                 images[i] = StrikeCache.invisibleGlyphPtr;
@@ -843,7 +844,9 @@ public class FileFontStrike extends PhysicalStrike {
     private int getGlyphImageMinX(long ptr, int origMinX) {
 
         byte format = StrikeCache.getGlyphFormat(ptr);
-        if (format != StrikeCache.PIXEL_FORMAT_LCD) return origMinX;
+        if (format != StrikeCache.PIXEL_FORMAT_LCD) {
+            return origMinX;
+        }
 
         int height = StrikeCache.getGlyphHeight(ptr);
         int rowBytes = StrikeCache.getGlyphRowBytes(ptr);
@@ -1028,11 +1031,6 @@ public class FileFontStrike extends PhysicalStrike {
             gp.transform(AffineTransform.getTranslateInstance(x, y));
         }
         return gp;
-    }
-
-    GeneralPath getGlyphVectorOutline(int[] glyphs, float x, float y) {
-        return fileFont.getGlyphVectorOutline(pScalerContext,
-                                              glyphs, glyphs.length, x, y);
     }
 
     private
