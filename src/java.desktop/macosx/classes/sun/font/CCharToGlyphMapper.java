ec5da7ef3b33588160eacc787641adc13c05fe4c
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2023-02-01 16:33:10 +0100

    Emoji PR sync (experimental)
    - Removed font fallback hacks.
    - Changed composite font glyph code encoding scheme, refactored to use Font2D instead of PhysicalFont.
    - New Emoji logical font type with 2 underlying physical fonts.
    - Removed getGlyphVectorOutline - it's unused and broken.
    - Got rid of charsToGlyphs[NS] boilerplate.
    - Moved emoji tests to separate directory.
    
    (cherry picked from commit 485bdd1d1abe338b06c1e460f95f51fa7336a4a2)

--- src/java.desktop/macosx/classes/sun/font/CCharToGlyphMapper.java
+++ src/java.desktop/macosx/classes/sun/font/CCharToGlyphMapper.java
@@ -79,7 +79,8 @@ public class CCharToGlyphMapper extends CharToGlyphMapper {
 
             if (code < FontUtilities.MIN_LAYOUT_CHARCODE) {
                 continue;
-            } else if (FontUtilities.isComplexCharCode(code)) {
+            } else if (FontUtilities.isComplexCharCode(code) ||
+                    CharToGlyphMapper.isVariationSelector(code)) {
                 return true;
             } else if (code >= 0x10000) {
                 i += 1; // Empty glyph slot after surrogate
@@ -88,6 +89,21 @@ public class CCharToGlyphMapper extends CharToGlyphMapper {
         }
 
         return false;
+    }
+
+    public synchronized int charToVariationGlyph(int unicode, int variationSelector) {
+        if (variationSelector == 0) {
+            return charToGlyph(unicode);
+        }
+        final char[] unicodeArray = new char[4];
+        final int[] glyphArray = new int[4];
+
+        int size = Character.toChars(unicode, unicodeArray, 0);
+        size += Character.toChars(variationSelector, unicodeArray, size);
+
+        nativeCharsToGlyphs(fFont.getNativeFontPtr(), size, unicodeArray, glyphArray);
+
+        return glyphArray[0];
     }
 
     public synchronized int charToGlyph(char unicode) {
