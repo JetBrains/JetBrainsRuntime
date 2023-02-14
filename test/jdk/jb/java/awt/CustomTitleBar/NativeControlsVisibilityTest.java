15f5d3782508a91b22a64e3ea69fcc4bd0d381a6
Sergey Shelomentsev <sergey.shelomentsev@jetbrains.com>
2023-02-14 13:53:23 +0200

    JBR-5194 add regressions tests for custom decorations support
    with fix for JBR-5300 Change source code and test files to use GPL license
    
    (cherry picked from commit 5fc82006c44235e52ca08203ba00a70140bb6cd2)

---
+++ test/jdk/jb/java/awt/CustomTitleBar/NativeControlsVisibilityTest.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2000-2023 JetBrains s.r.o.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+import com.jetbrains.JBR;
+import util.CommonAPISuite;
+import util.Rect;
+import util.Task;
+import util.ScreenShotHelpers;
+import util.TestUtils;
+
+import java.awt.Robot;
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+/*
+ * @test
+ * @summary Verify a property to change visibility of native controls
+ * @requires (os.family == "windows" | os.family == "mac")
+ * @run main NativeControlsVisibilityTest
+ * @run main NativeControlsVisibilityTest -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=1.0
+ * @run main NativeControlsVisibilityTest -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=1.25
+ * @run main NativeControlsVisibilityTest -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=1.5
+ * @run main NativeControlsVisibilityTest -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=2.0
+ * @run main NativeControlsVisibilityTest -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=2.5
+ * @run main NativeControlsVisibilityTest -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=3.0
+ * @run main NativeControlsVisibilityTest -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=3.5
+ * @run main NativeControlsVisibilityTest -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=4.0
+ */
+public class NativeControlsVisibilityTest {
+
+    public static void main(String... args) {
+        boolean status = CommonAPISuite.runTestSuite(TestUtils.getWindowCreationFunctions(), visibleControls);
+
+        if (!status) {
+            throw new RuntimeException("NativeControlsVisibilityTest FAILED");
+        }
+    }
+
+    private static final Task visibleControls = new Task("Visible native controls") {
+        private static final String PROPERTY_NAME = "controls.visible";
+        @Override
+        public void prepareTitleBar() {
+            titleBar = JBR.getWindowDecorations().createCustomTitleBar();
+            titleBar.setHeight(TestUtils.TITLE_BAR_HEIGHT);
+        }
+
+        @Override
+        public void test() throws Exception {
+            Robot robot = new Robot();
+            robot.delay(500);
+            robot.mouseMove(window.getLocationOnScreen().x + window.getWidth() / 2,
+                    window.getLocationOnScreen().y + window.getHeight() / 2);
+            robot.delay(500);
+
+            passed = passed && TestUtils.checkTitleBarHeight(titleBar, TestUtils.TITLE_BAR_HEIGHT);
+            passed = passed && TestUtils.checkFrameInsets(window);
+
+            if (titleBar.getLeftInset() == 0 && titleBar.getRightInset() == 0) {
+                passed = false;
+                System.out.println("Left or right inset must be non-zero");
+            }
+
+            BufferedImage image = ScreenShotHelpers.takeScreenshot(window);
+
+//            List<Rect> foundControls = ScreenShotHelpers.detectControls(image, (int) titleBar.getHeight(),
+//                    (int) titleBar.getLeftInset(), (int) titleBar.getRightInset());
+            List<Rect> foundControls = ScreenShotHelpers.detectControlsByBackground(image, (int) titleBar.getHeight(), TestUtils.TITLE_BAR_COLOR);
+            System.out.println("Found controls at the title bar:");
+            foundControls.forEach(System.out::println);
+
+            if (foundControls.size() != 3) {
+                passed = false;
+                System.out.println("Error: there are must be 3 controls");
+            }
+
+            if (!passed) {
+                String path = ScreenShotHelpers.storeScreenshot("visible-controls-test-" + window.getName(), image);
+                System.out.println("Screenshot stored in " + path);
+            }
+        }
+    };
+
+
+
+
+}
