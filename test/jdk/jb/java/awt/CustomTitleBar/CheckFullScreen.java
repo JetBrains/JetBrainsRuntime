3e0c6bd63779fd0a4e547e9c50a60065ac38090f
Sergey Shelomentsev <sergey.shelomentsev@jetbrains.com>
2023-02-14 13:53:23 +0200

    JBR-5194 add regressions tests for custom decorations support
    with fix for JBR-5300 Change source code and test files to use GPL license

---
+++ test/jdk/jb/java/awt/CustomTitleBar/CheckFullScreen.java
@@ -0,0 +1,56 @@
+import com.jetbrains.JBR;
+import com.jetbrains.WindowDecorations;
+import util.ScreenShotHelpers;
+
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/*
+ * @test
+ * @summary Verify modifying of title bar height
+ * @requires (os.family == "windows" | os.family == "mac")
+ * @run main CheckFullScreen
+ * @run main CheckFullScreen -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=1.0
+ * @run main CheckFullScreen -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=1.25
+ * @run main CheckFullScreen -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=1.5
+ * @run main CheckFullScreen -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=2.0
+ * @run main CheckFullScreen -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=2.5
+ * @run main CheckFullScreen -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=3.0
+ * @run main CheckFullScreen -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=3.5
+ * @run main CheckFullScreen -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=4.0
+ */
+public class CheckFullScreen {
+
+    public static void main(String... args) throws AWTException, IOException {
+        Frame f = new Frame(){
+            @Override
+            public void paint(Graphics g) {
+                Rectangle r = g.getClipBounds();
+                g.setColor(Color.BLUE);
+                g.fillRect(r.x, r.y, r.width, 100);
+                super.paint(g);
+            }
+        };
+
+        WindowDecorations.CustomTitleBar titleBar = JBR.getWindowDecorations().createCustomTitleBar();
+        titleBar.setHeight(100);
+        JBR.getWindowDecorations().setCustomTitleBar(f, titleBar);
+
+        f.setVisible(true);
+        GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().setFullScreenWindow(f);
+
+        Robot robot = new Robot();
+        robot.delay(5000);
+
+        BufferedImage image = ScreenShotHelpers.takeScreenshot(f);
+        ScreenShotHelpers.storeScreenshot("fullscreen", image);
+    }
+
+}
