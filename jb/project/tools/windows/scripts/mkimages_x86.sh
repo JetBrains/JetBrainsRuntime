0cf3ada8c85a706d345c71212b0d79d7cd054670
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2023-02-22 11:02:18 +0700

    Revert "JBR-5217 enable NVDA support in Windows builds"
    This reverts commit 370fa4907e2f3ee42ab93993485574b4da071125.

--- jb/project/tools/windows/scripts/mkimages_x86.sh
+++ jb/project/tools/windows/scripts/mkimages_x86.sh
@@ -19,8 +19,6 @@ set -x
 source jb/project/tools/common/scripts/common.sh
 
 WORK_DIR=$(pwd)
-NVDA_PATH=${NVDA_PATH:=$WORK_DIR/nvda_controllerClient}
-
 
 function do_configure {
   sh ./configure \
@@ -33,7 +31,6 @@ function do_configure {
     --with-version-opt=b${build_number} \
     --with-toolchain-version=$TOOLCHAIN_VERSION \
     --with-boot-jdk=$BOOT_JDK \
-    --with-nvdacontrollerclient=$NVDA_PATH \
     --disable-ccache \
     --enable-cds=yes \
     $STATIC_CONF_ARGS \
