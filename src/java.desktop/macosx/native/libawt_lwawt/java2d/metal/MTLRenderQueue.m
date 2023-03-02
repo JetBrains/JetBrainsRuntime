695d08bfc25fcb539563d271ebdfd1dfe7f2f794
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2023-03-02 13:23:19 +0100

    JBR-4883 macOS: SIGSEGV at MTLVertexCache_FlushGlyphVertexCache
    Use separate glyph cache for each MTLContext instance. Refactored MTLGlyphCache

--- src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLRenderQueue.m
+++ src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLRenderQueue.m
@@ -577,8 +577,8 @@ Java_sun_java2d_metal_MTLRenderQueue_flushBuffer
 
                     if (mtlc != NULL) {
                         if (dstOps != NULL && dstOps->pTexture != NULL) {
-                            MTLTR_FreeGlyphCacheAA();
-                            MTLTR_FreeGlyphCacheLCD();
+                            MTLTR_FreeGlyphCacheAA(mtlc);
+                            MTLTR_FreeGlyphCacheLCD(mtlc);
                         }
                         [mtlc commitCommandBuffer:NO display:NO];
                     }
@@ -595,8 +595,8 @@ Java_sun_java2d_metal_MTLRenderQueue_flushBuffer
 
                     if (mtlc != NULL) {
                         if (dstOps != NULL && dstOps->pTexture != NULL) {
-                            MTLTR_FreeGlyphCacheAA();
-                            MTLTR_FreeGlyphCacheLCD();
+                            MTLTR_FreeGlyphCacheAA(mtlc);
+                            MTLTR_FreeGlyphCacheLCD(mtlc);
                         }
                         [mtlc commitCommandBuffer:NO display:NO];
                     }
@@ -616,8 +616,8 @@ Java_sun_java2d_metal_MTLRenderQueue_flushBuffer
                     BMTLSDOps *mtlsdo = (BMTLSDOps *)jlong_to_ptr(pData);
                     if (mtlsdo != NULL) {
                         CONTINUE_IF_NULL(mtlc);
-                        MTLTR_FreeGlyphCacheAA();
-                        MTLTR_FreeGlyphCacheLCD();
+                        MTLTR_FreeGlyphCacheAA(mtlc);
+                        MTLTR_FreeGlyphCacheLCD(mtlc);
                         MTLSD_Delete(env, mtlsdo);
                     }
                     break;
@@ -641,8 +641,8 @@ Java_sun_java2d_metal_MTLRenderQueue_flushBuffer
                     CHECK_PREVIOUS_OP(MTL_OP_OTHER);
                     jlong pConfigInfo = NEXT_LONG(b);
                     CONTINUE_IF_NULL(mtlc);
-                    MTLTR_FreeGlyphCacheAA();
-                    MTLTR_FreeGlyphCacheLCD();
+                    MTLTR_FreeGlyphCacheAA(mtlc);
+                    MTLTR_FreeGlyphCacheLCD(mtlc);
                     [mtlc.encoderManager endEncoder];
                     MTLGC_DestroyMTLGraphicsConfig(pConfigInfo);
                     mtlc = NULL;
