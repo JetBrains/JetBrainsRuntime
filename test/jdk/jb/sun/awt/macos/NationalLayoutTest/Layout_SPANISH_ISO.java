fb81b49e8def1d78ef204452ac192f690e9e600f
Nikita Tsarev <nikita.tsarev@jetbrains.com>
2023-03-07 11:48:19 +0100

    JBR-5295: Fix wrong keycodes for non-letter keys that lack a corresponding VK_ constant on macOS

--- test/jdk/jb/sun/awt/macos/NationalLayoutTest/Layout_SPANISH_ISO.java
+++ test/jdk/jb/sun/awt/macos/NationalLayoutTest/Layout_SPANISH_ISO.java
@@ -37,6 +37,7 @@ public enum Layout_SPANISH_ISO implements LayoutKey {
     // Enum name must be the same as KeyEvent.VK_ constant name corresponding to the key on US keyboard layout
     // Note that '\u0000' may be used if no char is mapped to a key + modifier or if one wants to skip its testing
 
+    VK_SECTION       ('º', '\\', 'ª', '°', '0'),
     VK_MINUS         ('\'', '´', '?', '¸', '\u001F'),
     VK_EQUALS        ('¡',  '‚', '¿', '˛', '='),
 
