a8cdbbfb8d5eee90c85b20a20f2ed7ac146a4573
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2023-03-16 22:10:10 +0200

    Added debug messenger.

--- src/java.desktop/share/native/common/java2d/vulkan/VKBase.cpp
+++ src/java.desktop/share/native/common/java2d/vulkan/VKBase.cpp
@@ -6,6 +6,34 @@ static const uint32_t REQUIRED_VULKAN_VERSION = VK_MAKE_API_VERSION(0, 1, 0, 0);
 static vk::raii::Context* context;
 vk::raii::Instance vkInstance = nullptr;
 
+
+#if defined(DEBUG)
+static vk::raii::DebugUtilsMessengerEXT debugMessenger = nullptr;
+
+static VkBool32 debugCallback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+        VkDebugUtilsMessageTypeFlagsEXT messageType,
+        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+        void* pUserData
+) {
+    auto data = (const vk::DebugUtilsMessengerCallbackDataEXT*) pCallbackData;
+    if (data == nullptr) return 0;
+    // Here we can filter messages like this:
+    // if (std::strcmp(data->pMessageIdName, "UNASSIGNED-BestPractices-DrawState-ClearCmdBeforeDraw") == 0) return 0;
+
+    int level = J2D_TRACE_OFF;
+    if (messageSeverity == VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT) level = J2D_TRACE_VERBOSE;
+    else if (messageSeverity == VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT) level = J2D_TRACE_INFO;
+    else if (messageSeverity == VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT) level = J2D_TRACE_WARNING;
+    else if (messageSeverity == VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT) level = J2D_TRACE_ERROR;
+
+    J2dRlsTraceLn(level, data->pMessage);
+
+    // TODO if (messageSeverity == VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT) ABORT?
+    return 0;
+}
+#endif
+
 static bool createInstance() {
     try {
         // Load library.
@@ -33,6 +61,7 @@ static bool createInstance() {
         }
 
         std::vector<const char*> enabledLayers, enabledExtensions;
+        const void* pNext = nullptr;
 
         // Check required layers & extensions.
 #if defined(VK_USE_PLATFORM_WAYLAND_KHR)
@@ -48,13 +77,21 @@ static bool createInstance() {
         }
         if (requiredNotFound) return false;
 
-        // Check optional layers & extensions.
+        // Configure validation
 #ifdef DEBUG
+        std::array<vk::ValidationFeatureEnableEXT, 4> enabledValidationFeatures = {
+                vk::ValidationFeatureEnableEXT::eGpuAssisted,
+                vk::ValidationFeatureEnableEXT::eGpuAssistedReserveBindingSlot,
+                vk::ValidationFeatureEnableEXT::eBestPractices,
+                vk::ValidationFeatureEnableEXT::eSynchronizationValidation
+        };
+        vk::ValidationFeaturesEXT validationFeatures {enabledValidationFeatures};
         const char* const VALIDATION_LAYER_NAME = "VK_LAYER_KHRONOS_validation";
         if (layers.find(VALIDATION_LAYER_NAME) != layers.end() &&
             extensions.find(VK_EXT_DEBUG_UTILS_EXTENSION_NAME) != extensions.end()) {
             enabledLayers.push_back(VALIDATION_LAYER_NAME);
             enabledExtensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+            pNext = &validationFeatures;
         } else {
             J2dRlsTraceLn2(J2D_TRACE_WARNING, "%s and %s are not supported",
                            VALIDATION_LAYER_NAME, VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
@@ -73,7 +110,8 @@ static bool createInstance() {
                 /*flags*/                   {},
                 /*pApplicationInfo*/        &applicationInfo,
                 /*ppEnabledLayerNames*/     enabledLayers,
-                /*ppEnabledExtensionNames*/ enabledExtensions
+                /*ppEnabledExtensionNames*/ enabledExtensions,
+                /*pNext*/                   pNext
         };
 
         // Save context object at persistent address before passing it further.
@@ -81,6 +119,26 @@ static bool createInstance() {
 
         vkInstance = vk::raii::Instance(*context, instanceCreateInfo);
         J2dRlsTraceLn(J2D_TRACE_VERBOSE, "Vulkan instance created");
+
+        // Create debug messenger
+#if defined(DEBUG)
+        if (pNext) {
+            debugMessenger = vk::raii::DebugUtilsMessengerEXT(vkInstance, vk::DebugUtilsMessengerCreateInfoEXT {
+                    /*flags*/           {},
+                    /*messageSeverity*/ vk::DebugUtilsMessageSeverityFlagBitsEXT::eError |
+                                        vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning |
+                                        vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo |
+                                        vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose,
+                    /*messageType*/     vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral |
+                                        vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation |
+                                        vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance,
+                    /*pfnUserCallback*/ &debugCallback
+            });
+        }
+
+#endif
+
+        vkInstance.createWaylandSurfaceKHR(vk::WaylandSurfaceCreateInfoKHR {vk::WaylandSurfaceCreateFlagsKHR(123542)}); // TODO just to verify that debug messenger works
 
         return true;
     } catch (std::exception& e) {
