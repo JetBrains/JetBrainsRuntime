2474e4aac40d446f34b7555b250e2671318aae94
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2023-03-26 19:20:56 +0400

    JBR-5084 Add ability to log additional data to jstack output
    The data can be provided via this JBR API call:
    JBR.getJstack().includeInfoFrom(Supplier<String>)

--- src/hotspot/share/runtime/vmOperations.hpp
+++ src/hotspot/share/runtime/vmOperations.hpp
@@ -156,6 +156,9 @@ class VM_PrintThreads: public VM_Operation {
   void doit();
   bool doit_prologue();
   void doit_epilogue();
+
+private:
+  void print_additional_info();
 };
 
 class VM_PrintJNI: public VM_Operation {
