1d5bc2152d7b96792f9abc020d26aa2fc743315d
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2023-03-29 20:44:08 +0200

    JBR-5274 recreate CGraphicsDevice if it was changed.
    - AWT code heavily relies on reference comparison when updating graphics devices & configurations, so we need to actually re-create CGraphicsDevice if it was changed.
    
    - Also do not rely on graphicsConfig.getDefaultTransform() when firing `graphicsContextScaleTransform` property change, as graphics devices are mutable and returned default transform may change over time, e.g. when device is invalidated.

--- src/java.desktop/macosx/classes/sun/awt/CGraphicsEnvironment.java
+++ src/java.desktop/macosx/classes/sun/awt/CGraphicsEnvironment.java
@@ -149,7 +149,8 @@ public final class CGraphicsEnvironment extends SunGraphicsEnvironment {
      */
     private void rebuildDevices() {
         initDevices();
-        displayChanged();
+        // Do not notify devices, this was already done in initDevices.
+        displayChanger.notifyListeners();
     }
 
     /**
@@ -210,8 +211,17 @@ public final class CGraphicsEnvironment extends SunGraphicsEnvironment {
             displayIDs = new int[]{mainDisplayID};
         }
         for (int id : displayIDs) {
-            devices.put(id, old.containsKey(id) ? old.remove(id)
-                                                : new CGraphicsDevice(id));
+            old.compute(id, (i, d) -> {
+                if (d != null && d.updateDevice()) {
+                    // Device didn't change -> reuse
+                    devices.put(i, d);
+                    return null;
+                } else {
+                    // Device changed -> create new
+                    devices.put(i, new CGraphicsDevice(i));
+                    return d;
+                }
+            });
         }
         // fetch the main display again, the old value might be outdated
         mainDisplayID = LWCToolkit.isDispatchingOnMainThread() ? getMainDisplayIDAppKit() : getMainDisplayID();
@@ -247,9 +257,12 @@ public final class CGraphicsEnvironment extends SunGraphicsEnvironment {
     }
 
     private CGraphicsDevice getSimilarDevice(CGraphicsDevice old) {
+        CGraphicsDevice sameId = devices.get(old.getDisplayID());
+        if (sameId != null) {
+            return sameId;
+        }
         for (CGraphicsDevice device : devices.values()) {
             if (device.getBounds().equals(old.getBounds())) {
-                // for now we will use the bounds only
                 return device;
             }
         }
