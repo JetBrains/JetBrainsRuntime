94ec07f25aad4890e5076dfa416fbc9597690075
Serguei Spitsyn <sspitsyn@openjdk.org>
2023-04-04 00:46:43 +0000

    8297286: runtime/vthread tests crashing after JDK-8296324
    Reviewed-by: lmesnik, pchilanomate, cjplummer
    (cherry picked from commit a1a9ec6e46b70d5436711f89f4bf603ebacc8060)

--- src/hotspot/share/prims/jvm.cpp
+++ src/hotspot/share/prims/jvm.cpp
@@ -3920,6 +3920,8 @@ JVM_ENTRY(void, JVM_VirtualThreadMount(JNIEnv* env, jobject vthread, jboolean hi
   }
   if (!JvmtiVTMSTransitionDisabler::VTMS_notify_jvmti_events()) {
     thread->set_is_in_VTMS_transition(hide);
+    oop vt = JNIHandles::resolve_external_guard(vthread);
+    java_lang_Thread::set_is_in_VTMS_transition(vt, hide);
     return;
   }
   if (hide) {
@@ -3942,6 +3944,8 @@ JVM_ENTRY(void, JVM_VirtualThreadUnmount(JNIEnv* env, jobject vthread, jboolean 
   }
   if (!JvmtiVTMSTransitionDisabler::VTMS_notify_jvmti_events()) {
     thread->set_is_in_VTMS_transition(hide);
+    oop vt = JNIHandles::resolve_external_guard(vthread);
+    java_lang_Thread::set_is_in_VTMS_transition(vt, hide);
     return;
   }
   if (hide) {
@@ -3954,14 +3958,11 @@ JVM_ENTRY(void, JVM_VirtualThreadUnmount(JNIEnv* env, jobject vthread, jboolean 
 #endif
 JVM_END
 
-// If notifications are enabled then just update the temporary VTMS transition bit.
+// Always update the temporary VTMS transition bit.
 JVM_ENTRY(void, JVM_VirtualThreadHideFrames(JNIEnv* env, jobject vthread, jboolean hide))
 #if INCLUDE_JVMTI
   if (!DoJVMTIVirtualThreadTransitions) {
     assert(!JvmtiExport::can_support_virtual_threads(), "sanity check");
-    return;
-  }
-  if (!JvmtiVTMSTransitionDisabler::VTMS_notify_jvmti_events()) {
     return;
   }
   assert(!thread->is_in_VTMS_transition(), "sanity check");
