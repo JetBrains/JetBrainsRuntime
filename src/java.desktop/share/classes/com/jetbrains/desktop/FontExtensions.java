e4abb50860bdf1d5d21600883147f251e343855a
Dmitrii <dmitrii.morskii@jetbrains.com>
2023-04-06 16:24:42 +0200

    JBR-5246 add OpenType's features support

--- src/java.desktop/share/classes/com/jetbrains/desktop/FontExtensions.java
+++ src/java.desktop/share/classes/com/jetbrains/desktop/FontExtensions.java
@@ -23,11 +23,30 @@
  * questions.
  */
 
-package com.jetbrains;
+package com.jetbrains.desktop;
+
+import com.jetbrains.internal.JBRApi;
 
 import java.awt.*;
+import java.lang.invoke.MethodHandles;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
-@Deprecated(forRemoval=true)
-public interface ExtendedGlyphCache {
-    Dimension getSubpixelResolution();
+public class FontExtensions {
+    private interface FontExtension {
+        FontExtension INSTANCE = (FontExtension) JBRApi.internalServiceBuilder(MethodHandles.lookup())
+                .withStatic("getFeatures", "getFeatures", "java.awt.Font").build();
+
+        TreeMap<String, Integer> getFeatures(Font font);
+    }
+
+    public static String[] featuresToStringArray(Map<String, Integer> features) {
+        return features.entrySet().stream().map(feature -> (feature.getKey() + "=" + feature.getValue())).
+                toArray(String[]::new);
+    }
+
+    public static TreeMap<String, Integer> getFeatures(Font font) {
+        return FontExtension.INSTANCE.getFeatures(font);
+    }
 }
