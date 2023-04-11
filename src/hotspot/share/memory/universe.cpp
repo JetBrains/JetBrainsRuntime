ed9811850b4062c803516b5cc1b556de9ddc2cb1
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2023-04-11 19:14:03 +0400

    JBR-5480 Include more information in OOME crash reports

--- src/hotspot/share/memory/universe.cpp
+++ src/hotspot/share/memory/universe.cpp
@@ -84,6 +84,7 @@
 #include "utilities/macros.hpp"
 #include "utilities/ostream.hpp"
 #include "utilities/preserveException.hpp"
+#include "utilities/vmError.hpp"
 
 // Known objects
 Klass* Universe::_typeArrayKlassObjs[T_LONG+1]        = { nullptr /*, nullptr...*/ };
@@ -835,6 +836,8 @@ jint universe_init() {
   }
 
   ResolvedMethodTable::create_table();
+
+  VMError::init();
 
   return JNI_OK;
 }
