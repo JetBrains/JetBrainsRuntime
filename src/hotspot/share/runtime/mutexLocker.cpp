00592880edbf4f7635d1f7c94b142db0d6ed2f40
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2023-04-11 19:14:03 +0400

    JBR-5480 Include more information in OOME crash reports

--- src/hotspot/share/runtime/mutexLocker.cpp
+++ src/hotspot/share/runtime/mutexLocker.cpp
@@ -156,6 +156,8 @@ Monitor* JVMCI_lock                   = nullptr;
 Monitor* JVMCIRuntime_lock            = nullptr;
 #endif
 
+Mutex*   OOMEStacks_lock              = nullptr;
+
 // Only one RecursiveMutex
 RecursiveMutex* MultiArray_lock       = nullptr;
 
@@ -360,6 +362,7 @@ void mutex_init() {
   // JVMCIRuntime_lock must be acquired before JVMCI_lock to avoid deadlock
   MUTEX_DEFL(JVMCI_lock                     , PaddedMonitor, JVMCIRuntime_lock);
 #endif
+  MUTEX_DEFN(OOMEStacks_lock                , PaddedMutex  , safepoint);
 
   // Allocate RecursiveMutex
   MultiArray_lock = new RecursiveMutex();
