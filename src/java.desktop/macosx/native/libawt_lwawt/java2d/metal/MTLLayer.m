1dc15b8723c2581371dae922110fdfb687ca8096
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2023-04-20 21:14:19 +0200

    JBR-5559 SwingMark performance drop after removing additional command queue
    Added command queue and provided synchronization between the command queues
    
    (cherry picked from commit 6b76f79eeee868bba968007a60897575ce819b26)

--- src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLLayer.m
+++ src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLLayer.m
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -111,7 +111,7 @@ BOOL isDisplaySyncEnabled() {
             return;
         }
 
-        id<MTLCommandBuffer> commandBuf = [self.ctx createCommandBuffer];
+        id<MTLCommandBuffer> commandBuf = [self.ctx createBlitCommandBuffer];
         if (commandBuf == nil) {
             J2dTraceLn(J2D_TRACE_VERBOSE, "MTLLayer.blitTexture: commandBuf is null");
             return;
@@ -122,6 +122,11 @@ BOOL isDisplaySyncEnabled() {
             return;
         }
         self.nextDrawableCount++;
+        id<MTLCommandBuffer> renderBuffer =  [self.ctx createCommandBuffer];
+        self.ctx.syncCount++;
+        if (@available(macOS 10.14, *)) {
+            [renderBuffer encodeWaitForEvent:self.ctx.syncEvent value:self.ctx.syncCount];
+        }
 
         id <MTLBlitCommandEncoder> blitEncoder = [commandBuf blitCommandEncoder];
 
@@ -132,6 +137,10 @@ BOOL isDisplaySyncEnabled() {
                 toTexture:mtlDrawable.texture destinationSlice:0 destinationLevel:0
                 destinationOrigin:MTLOriginMake(0, 0, 0)];
         [blitEncoder endEncoding];
+
+        if (@available(macOS 10.14, *)) {
+            [commandBuf encodeSignalEvent:self.ctx.syncEvent value:self.ctx.syncCount];
+        }
 
         [commandBuf presentDrawable:mtlDrawable];
         __block MTLLayer* layer = self;
