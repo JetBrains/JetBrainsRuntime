c4fcd13dd6211bd9841efad586da15194a998255
Dmitrii Morskii <dmitrii.morskii@jetbrains.com>
2023-04-28 16:49:05 +0200

    JBR-1775: improved logic for choosing newer font between system and bundled ones
    (cherry picked from commit 41cbdf720cc28ca8995e16f120d7a03f9ce30597)

--- src/java.desktop/unix/classes/sun/font/FontConfigManager.java
+++ src/java.desktop/unix/classes/sun/font/FontConfigManager.java
@@ -454,6 +454,7 @@ public class FontConfigManager {
         return fcInfo;
     }
 
-    private static native int
-    getFontConfigAASettings(String locale, String fcFamily);
+    private static native int getFontConfigAASettings(String locale, String fcFamily);
+
+    public static native String getFontProperty(String name, String pattern);
 }
