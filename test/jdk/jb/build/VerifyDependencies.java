2db07280730b8947a6b7fff98672dd6f3d16cdec
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2023-05-12 12:27:31 +0700

    JBR-5603 build aarch64 Linux from arm64v8/centos:7 and check glibc to be not higher 2.17 (fix more notes)

--- test/jdk/jb/build/VerifyDependencies.java
+++ test/jdk/jb/build/VerifyDependencies.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright 2000-2022 JetBrains s.r.o.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FilenameFilter;
@@ -29,9 +52,7 @@ public class VerifyDependencies {
             if (line.contains("GLIBC_")) {
                 String version = extractVersion(line);
                 if (compareVersions(version, "2.17") > 0) {
-                    //if (Double.parseDouble(version) > 2.17) {
-                    System.out.println("ERROR: " + libraryPath + " has a dependency on glibc version " + version);
-                    System.exit(1);
+                    throw new RuntimeException(libraryPath + " has a dependency on glibc version " + version);
                 }
             }
         }
@@ -95,7 +116,7 @@ public class VerifyDependencies {
 
         FilenameFilter filter = new FilenameFilter() {
             public boolean accept(File dir, String name) {
-                return dir.getAbsolutePath().contains("lib") ? name.endsWith(libPattern) : true;
+                return dir.getAbsolutePath().lastIndexOf("lib") != -1 ? name.endsWith(libPattern) : true;
             }
         };
 
