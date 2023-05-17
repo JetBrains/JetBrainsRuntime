25a4e749ad7010df916f25699d919e34d35cb84f
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2023-05-18 00:42:11 +0200

    JBR-5649 Flickering in multi-monitor configuration
    Provided corrected initial value for currentDisplayID
    
    (cherry picked from commit 9ba9286595be365af2ae67cbeca64712ae4f079a)

--- src/java.desktop/macosx/native/libawt_lwawt/awt/AWTWindow.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/AWTWindow.m
@@ -111,6 +111,8 @@ static BOOL orderingScheduled = NO;
          selector:@selector(windowDidChangeProfile)             \
          name:NSWindowDidChangeScreenProfileNotification        \
          object:self];                                          \
+    [self addObserver:self forKeyPath:@"visible"                \
+        options:NSKeyValueObservingOptionNew context:nil];      \
     return self;                                                \
 }                                                               \
                                                                 \
@@ -145,6 +147,21 @@ static BOOL orderingScheduled = NO;
     return ((AWTWindow*)[self delegate]).javaWindowTabbingMode; \
 }                                                               \
                                                                 \
+- (void)observeValueForKeyPath:(NSString *)keyPath              \
+    ofObject:(id)object                                         \
+    change:(NSDictionary<NSKeyValueChangeKey,id> *)change       \
+    context:(void *)context {                                   \
+    if ([keyPath isEqualToString:@"visible"]) {                 \
+        BOOL isVisible =                                        \
+            [[change objectForKey:NSKeyValueChangeNewKey]       \
+            boolValue];                                         \
+        if (isVisible) {                                        \
+            [(AWTWindow*)[self delegate]                        \
+                _windowDidBecomeVisible];                       \
+        }                                                       \
+    }                                                           \
+}                                                               \
+                                                                \
 - (void)windowDidChangeScreen {                                 \
    [(AWTWindow*)[self delegate] _displayChanged:NO];           \
 }                                                               \
@@ -159,6 +176,7 @@ static BOOL orderingScheduled = NO;
    [[NSNotificationCenter defaultCenter] removeObserver:self    \
        name:NSWindowDidChangeScreenProfileNotification          \
        object:self];                                            \
+   [self removeObserver:self forKeyPath:@"visible"];            \
    [super dealloc];                                             \
 }                                                               \
 
@@ -585,7 +603,7 @@ AWT_ASSERT_APPKIT_THREAD;
         [self setUpCustomTitleBar];
     }
 
-    self.currentDisplayID = [AWTWindow getNSWindowDisplayID_AppKitThread:nsWindow];;
+    self.currentDisplayID = nil;
     return self;
 }
 
@@ -936,6 +954,10 @@ AWT_ASSERT_APPKIT_THREAD;
 
 
 // NSWindowDelegate methods
+
+- (void)_windowDidBecomeVisible {
+    self.currentDisplayID = [AWTWindow getNSWindowDisplayID_AppKitThread:nsWindow];
+}
 
 - (void)_displayChanged:(BOOL)profileOnly {
     AWT_ASSERT_APPKIT_THREAD;
