5159e2b955206ed1735b953be01cdf3342dc48b4
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2023-05-24 16:12:15 +0400

    JBR-5658 Wayland: incorrect scaling of window content
    The buffer scale is changed atomically with the size.
    WLGraphicsConfig made immutable and is re-created when scaling changes.
    WLGraphicsDevice is also re-created when its position changes.
    
    (cherry picked from commit 2d0d950d5b948215c4e03d60951948e59323d95e)

--- src/java.desktop/unix/classes/sun/awt/wl/WLGraphicsDevice.java
+++ src/java.desktop/unix/classes/sun/awt/wl/WLGraphicsDevice.java
@@ -1,38 +1,91 @@
 package sun.awt.wl;
 
 import sun.awt.AWTAccessor;
-import sun.awt.DisplayChangedListener;
 
 import java.awt.*;
-import java.util.ArrayList;
 
-public class WLGraphicsDevice extends GraphicsDevice implements DisplayChangedListener {
-    private final int wlID; // ID of wl_output object received from Wayland
-    private String name;
-    private int x;
-    private int y;
+/**
+ * Corresponds to Wayland's output and is identified by its wlID and x, y coordinates
+ * in the multi-monitor setup. Whenever those change, this device is re-created.
+ */
+public class WLGraphicsDevice extends GraphicsDevice {
+    /**
+     *  ID of the corresponding wl_output object received from Wayland.
+     */
+    private volatile int wlID; // only changes when the device gets invalidated
 
-    private final java.util.List<WLComponentPeer> peers = new ArrayList<>();
-    private final WLGraphicsConfig config = new WLGraphicsConfig(this);
+    /**
+     * Some human-readable name of the device that came from Wayland.
+     * NOT considered part of device's identity.
+     */
+    private volatile String name;
 
-    public WLGraphicsDevice(int id) {
+    /**
+     * The horizontal location of this device in the multi-monitor configuration.
+     */
+    private volatile int x; // only changes when the device gets invalidated
+
+    /**
+     * The vertical location of this device in the multi-monitor configuration.
+     */
+    private volatile int y; // only changes when the device gets invalidated
+
+    private volatile WLGraphicsConfig config = null;
+
+    private WLGraphicsDevice(int id, int x, int y) {
         this.wlID = id;
+        this.x = x;
+        this.y = y;
     }
 
-    int getWLID() {
+    int getID() {
         return wlID;
     }
 
-    void updateConfiguration(String name, int x, int y, int width, int height, int scale) {
+    void updateConfiguration(String name, int width, int height, int scale) {
         this.name = name == null ? "wl_output." + wlID : name;
-        this.x = x;
-        this.y = y;
-        config.update(width, height, scale);
+        if (config == null || config.differsFrom(width, height, scale)) {
+            // It is necessary to create a new object whenever config changes as its
+            // identity is used to detect changes in scale, among other things.
+            config = new WLGraphicsConfig(this, width, height, scale);
+        }
     }
 
-    void updateConfiguration(WLGraphicsDevice gd) {
-        final Rectangle bounds = config.getBounds();
-        updateConfiguration(gd.name, gd.x, gd.y, bounds.width, bounds.height, config.getScale());
+    /**
+     * Changes all aspects of this device including its identity to be that of the given
+     * device. Only used for devices that are no longer physically present, but references
+     * to which may still exist in the program.
+     */
+    void invalidate(WLGraphicsDevice similarDevice) {
+        this.wlID = similarDevice.wlID;
+        this.x = similarDevice.x;
+        this.y = similarDevice.y;
+
+        final int newScale = similarDevice.getScale();
+        final Rectangle newBounds = similarDevice.config.getBounds();
+        updateConfiguration(similarDevice.name, newBounds.width, newBounds.height, newScale);
+    }
+
+    public static WLGraphicsDevice createWithConfiguration(int id, String name, int x, int y, int width, int height, int scale) {
+        final WLGraphicsDevice device = new WLGraphicsDevice(id, x, y);
+        device.updateConfiguration(name, width, height, scale);
+        return device;
+    }
+
+    /**
+     * Compares the identity of this device with the given attributes
+     * and returns true iff the attributes identify the same device.
+     */
+    public boolean isSameDeviceAs(int wlID, int x, int y) {
+        return this.wlID == wlID && this.x == x && this.y == y;
+    }
+
+    public boolean hasSameNameAs(WLGraphicsDevice otherDevice) {
+        return name != null && otherDevice.name != null && name.equals(otherDevice.name);
+    }
+
+    public boolean hasSameSizeAs(WLGraphicsDevice modelDevice) {
+        return config != null && modelDevice.config != null && config.getBounds().equals(modelDevice.config.getBounds());
     }
 
     @Override
@@ -86,17 +139,6 @@ public class WLGraphicsDevice extends GraphicsDevice implements DisplayChangedLi
         }
     }
 
-    @Override
-    public void displayChanged() {
-       synchronized (peers) {
-           peers.forEach(WLComponentPeer::displayChanged);
-       }
-    }
-
-    @Override
-    public void paletteChanged() {
-    }
-
     private void enterFullScreenExclusive(Window w) {
         WLComponentPeer peer = AWTAccessor.getComponentAccessor().getPeer(w);
         if (peer != null) {
@@ -112,14 +154,17 @@ public class WLGraphicsDevice extends GraphicsDevice implements DisplayChangedLi
     }
 
     public void addWindow(WLComponentPeer peer) {
-        synchronized (peers) {
-            peers.add(peer);
-        }
+        // TODO: may be needed to keep track of windows on the device to notify
+        // them of display change events, perhaps.
     }
 
     public void removeWindow(WLComponentPeer peer) {
-        synchronized (peers) {
-            peers.remove(peer);
-        }
+        // TODO: may be needed to keep track of windows on the device to notify
+        // them of display change events, perhaps.
+    }
+
+    @Override
+    public String toString() {
+        return String.format("WLGraphicsDevice: id=%d at (%d, %d) with %s", wlID, x, y, config);
     }
 }
