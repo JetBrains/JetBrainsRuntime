682728ef0a0a95abb2a86e85b5b4443689259f5a
Alexey Pavlyutkin <apavlyutkin@azul.com>
2023-05-26 07:07:22 +0000

    8275233: Incorrect line number reported in exception stack trace thrown from a lambda expression
    Backport-of: 9cb01207b371e3e0f0af3652b78dd623205b537f

--- test/langtools/tools/javac/lambda/deduplication/DeduplicationTest.java
+++ test/langtools/tools/javac/lambda/deduplication/DeduplicationTest.java
@@ -92,6 +92,7 @@ public class DeduplicationTest {
                         Arrays.asList(
                                 "-d",
                                 ".",
+                                "-g:none",
                                 "-XDdebug.dumpLambdaToMethodDeduplication",
                                 "-XDdebug.dumpLambdaToMethodStats"),
                         null,
