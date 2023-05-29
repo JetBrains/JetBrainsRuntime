1b52626892df917a2fc457896146dd6b0595b63b
Sergey Shelomentsev <sergey.shelomentsev@jetbrains.com>
2023-05-29 16:51:06 +0300

    JBR-5577 fix MouseEventsOnClientArea test
    - add Swing/AWT specific Task runners
    - split MouseEventsOnClientArea to separate AWT/Swing tests
    - use CountDownLatch for tracking mouse events
    
    (cherry picked from commit c6a43fe84f6741f94d0dd495d97da8a7132cbcdb)

---
+++ test/jdk/jb/java/awt/CustomTitleBar/util/AWTTask.java
@@ -0,0 +1,58 @@
+package util;
+
+import com.jetbrains.WindowDecorations;
+
+import java.awt.*;
+import java.util.function.Function;
+
+abstract public class AWTTask extends Task {
+
+    public AWTTask(String name) {
+        super(name);
+    }
+
+    @Override
+    public TaskResult run(Function<WindowDecorations.CustomTitleBar, Window> windowCreator) {
+        System.out.printf("RUN TEST CASE: %s%n", name);
+        passed = true;
+        error = "";
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+            return new TaskResult(false, "ERROR: unable to init robot");
+        }
+
+        try {
+            runInternal(windowCreator);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new TaskResult(false, "ERROR: An error occurred during test execution");
+        }
+
+        if (!TestUtils.isBasicTestCase()) {
+            boolean isMetConditions = TestUtils.checkScreenSizeConditions(window);
+            if (!isMetConditions) {
+                error += "SKIPPED: environment don't match screen size conditions";
+                return new TaskResult(false, true, error);
+            }
+        }
+
+        return new TaskResult(passed, error);
+    }
+
+    private void runInternal(Function<WindowDecorations.CustomTitleBar, Window> windowCreator) throws Exception {
+        try {
+            EventQueue.invokeAndWait(() -> {
+                prepareTitleBar();
+                window = windowCreator.apply(titleBar);
+                customizeWindow();
+                window.setVisible(true);
+            });
+
+            test();
+        } finally {
+            EventQueue.invokeAndWait(this::disposeUI);
+        }
+    }
+}
