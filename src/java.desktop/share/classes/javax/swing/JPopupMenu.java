065ac4552f95cc2ac227ac473c754e64f136125e
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2023-05-30 13:49:38 +0400

    JBR-5661 Wayland: implement heavy-weight popup windows
    (cherry picked from commit abdfb7231b38fbd41e860a6dc78a0a1b6a30a769)

--- src/java.desktop/share/classes/javax/swing/JPopupMenu.java
+++ src/java.desktop/share/classes/javax/swing/JPopupMenu.java
@@ -153,6 +153,13 @@ public class JPopupMenu extends JComponent implements Accessible,MenuElement {
     private static final boolean VERBOSE = false; // show reuse hits/misses
     private static final boolean DEBUG =   false;  // show bad params, misc.
 
+    static {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        if (toolkit != null && "sun.awt.wl.WLToolkit".equals(toolkit.getClass().getName())) {
+            JPopupMenu.setDefaultLightWeightPopupEnabled(false);
+        }
+    }
+
     /**
      *  Sets the default value of the <code>lightWeightPopupEnabled</code>
      *  property.
@@ -341,7 +348,9 @@ public class JPopupMenu extends JComponent implements Accessible,MenuElement {
     Point adjustPopupLocationToFitScreen(int xPosition, int yPosition) {
         Point popupLocation = new Point(xPosition, yPosition);
 
-        if (popupPositionFixDisabled || GraphicsEnvironment.isHeadless()) {
+        if(popupPositionFixDisabled
+                || GraphicsEnvironment.isHeadless()
+                || PopupFactory.isPopupPositionedRelatively()) {
             return popupLocation;
         }
 
@@ -993,7 +1002,7 @@ public class JPopupMenu extends JComponent implements Accessible,MenuElement {
             }
         }
         Point invokerOrigin;
-        if (invoker != null) {
+        if (invoker != null && !PopupFactory.isPopupPositionedRelatively()) {
             invokerOrigin = invoker.getLocationOnScreen();
 
             // To avoid integer overflow
