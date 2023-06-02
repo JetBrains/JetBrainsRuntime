827c36cee84e66a6dcd9c33f3e4cd1549ba0daf2
Nikita Tsarev <nikita.tsarev@jetbrains.com>
2023-06-02 15:26:15 +0200

    JBR-5630: vmoption to change dead key reporting behavior on macOS

--- test/jdk/jb/sun/awt/macos/InputMethodTest/NextAppWinKeyTest.java
+++ test/jdk/jb/sun/awt/macos/InputMethodTest/NextAppWinKeyTest.java
@@ -25,7 +25,8 @@
  * @test
  * @summary Regression test for JBR-5469 Something weird going on with Cmd+Backtick / Cmd+Dead Grave
  * @modules java.desktop/sun.lwawt.macosx
- * @run main/othervm -Dapple.awt.captureNextAppWinKey=true InputMethodTest NextAppWinKeyTest
+ * @run main/othervm -Dapple.awt.captureNextAppWinKey=true -Dcom.sun.awt.reportDeadKeysAsNormal=false InputMethodTest NextAppWinKeyTestDead
+ * @run main/othervm -Dapple.awt.captureNextAppWinKey=true -Dcom.sun.awt.reportDeadKeysAsNormal=true InputMethodTest NextAppWinKeyTestNormal
  * @requires (jdk.version.major >= 8 & os.family == "mac")
  */
 
@@ -37,6 +38,12 @@ import java.awt.event.FocusListener;
 import static java.awt.event.KeyEvent.*;
 
 public class NextAppWinKeyTest implements Runnable {
+    private final boolean deadKeys;
+
+    public NextAppWinKeyTest(boolean deadKeys) {
+        this.deadKeys = deadKeys;
+    }
+
     @Override
     public void run() {
         var extraFrame = new JFrame();
@@ -62,12 +69,12 @@ public class NextAppWinKeyTest implements Runnable {
         InputMethodTest.section("US-Intl");
         InputMethodTest.layout("com.apple.keylayout.USInternational-PC");
         InputMethodTest.type(VK_BACK_QUOTE, META_DOWN_MASK);
-        InputMethodTest.expectKeyPress(VK_DEAD_GRAVE, KEY_LOCATION_STANDARD, META_DOWN_MASK, false);
+        InputMethodTest.expectKeyPress(deadKeys ? VK_DEAD_GRAVE : VK_BACK_QUOTE, KEY_LOCATION_STANDARD, META_DOWN_MASK, false);
 
         InputMethodTest.section("French");
         InputMethodTest.layout("com.apple.keylayout.French");
         InputMethodTest.type(VK_BACK_SLASH, META_DOWN_MASK);
-        InputMethodTest.expectKeyPress(VK_DEAD_GRAVE, KEY_LOCATION_STANDARD, META_DOWN_MASK, false);
+        InputMethodTest.expectKeyPress(deadKeys ? VK_DEAD_GRAVE : VK_BACK_QUOTE, KEY_LOCATION_STANDARD, META_DOWN_MASK, false);
 
         extraFrame.setVisible(false);
         extraFrame.dispose();
