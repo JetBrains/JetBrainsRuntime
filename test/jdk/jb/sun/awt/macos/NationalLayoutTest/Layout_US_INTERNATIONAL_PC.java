e56da05a94274fd151547041b08f705b45fdc235
Nikita Tsarev <nikita.tsarev@jetbrains.com>
2023-06-02 15:26:15 +0200

    JBR-5630: vmoption to change dead key reporting behavior on macOS
    (cherry picked from commit aeb6569d4cb7a3d1217bfe2f723bafba3d34bf53)

--- test/jdk/jb/sun/awt/macos/NationalLayoutTest/Layout_US_INTERNATIONAL_PC.java
+++ test/jdk/jb/sun/awt/macos/NationalLayoutTest/Layout_US_INTERNATIONAL_PC.java
@@ -26,7 +26,8 @@
  * @summary Regression test for IDEA-165950: National keyboard layouts support
  * @requires (jdk.version.major >= 8 & os.family == "mac")
  * @modules java.desktop/sun.lwawt.macosx
- * @run main NationalLayoutTest US_INTERNATIONAL_PC
+ * @run main/othervm -Dcom.sun.awt.reportDeadKeysAsNormal=false NationalLayoutTest US_INTERNATIONAL_PC
+ * @run main/othervm -Dcom.sun.awt.reportDeadKeysAsNormal=true NationalLayoutTest nodead US_INTERNATIONAL_PC
  */
 
 /*
