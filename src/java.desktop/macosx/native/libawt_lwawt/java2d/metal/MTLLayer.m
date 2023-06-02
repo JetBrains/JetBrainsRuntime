2fcace701ac62a11e8b724c94f1769ca37d7eeb2
Alexey Ushakov <alexey.v.ushakov@gmail.com>
2023-06-02 19:27:32 +0200

    JBR-5704 displaySyncOFF: javax/swing/JDialog/Transparency/TransparencyTest.java: JDialog transparency lost upon iconify/deiconify sequence
    Corrected startRedraw method to call setNeedsDisplay in displaySync=false mode

--- src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLLayer.m
+++ src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLLayer.m
@@ -186,8 +186,12 @@ BOOL isDisplaySyncEnabled() {
 }
 
 - (void)startRedraw {
-    if (self.ctx != nil) {
-        [self.ctx performSelectorOnMainThread:@selector(startRedraw:) withObject:self waitUntilDone:NO];
+    if (isDisplaySyncEnabled()) {
+        if (self.ctx != nil) {
+            [self.ctx performSelectorOnMainThread:@selector(startRedraw:) withObject:self waitUntilDone:NO];
+        }
+    } else {
+        [self performSelectorOnMainThread:@selector(setNeedsDisplay) withObject:nil waitUntilDone:NO];
     }
 }
 
