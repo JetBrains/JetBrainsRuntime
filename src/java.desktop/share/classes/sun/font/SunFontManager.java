845dc9612fd3287b7a99e1f544389aa9db3388e8
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2023-07-03 05:41:43 +0700

    Revert "fixup! JBR-1775: improved logic for choosing newer font between system and bundled ones"
    This reverts commit e4e9a016e06c0599d2b4c524eb0a8bfbce7a9768.

--- src/java.desktop/share/classes/sun/font/SunFontManager.java
+++ src/java.desktop/share/classes/sun/font/SunFontManager.java
@@ -3092,11 +3092,7 @@ public abstract class SunFontManager implements FontSupport, FontManagerForSGE {
         }
     }
 
-    // SunFontManager could be extended outside JBR scope (e.g. IdeFontManager) therefore default implementation of this
-    // method needed for compatibility. Returning of "0" repeats the logic of setting field versionCheckEnabled to false
-    protected String getSystemFontVersion(TrueTypeFont bundledFont) {
-        return "0";
-    }
+    abstract protected String getSystemFontVersion(TrueTypeFont bundledFont);
 
     protected void loadJREFonts(String[] fonts) {
         addDirFonts(fonts, FONTFORMAT_TRUETYPE, true,
