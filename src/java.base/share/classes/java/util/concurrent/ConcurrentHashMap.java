dbf8820d604c30d169ee6247fb383411d07e4253
Lutz Schmidt <lucy@openjdk.org>
2023-07-05 12:52:42 +0000

    8300098: java/util/concurrent/ConcurrentHashMap/ConcurrentAssociateTest.java fails with internal timeout when executed with TieredCompilation1/3
    Reviewed-by: mdoerr
    Backport-of: ecf8842cd2309210f3d5eee7f9f28a198a860686

--- src/java.base/share/classes/java/util/concurrent/ConcurrentHashMap.java
+++ src/java.base/share/classes/java/util/concurrent/ConcurrentHashMap.java
@@ -2852,22 +2852,20 @@ public class ConcurrentHashMap<K,V> extends AbstractMap<K,V>
          * Possibly blocks awaiting root lock.
          */
         private final void contendedLock() {
-            boolean waiting = false;
+            Thread current = Thread.currentThread(), w;
             for (int s;;) {
                 if (((s = lockState) & ~WAITER) == 0) {
                     if (U.compareAndSetInt(this, LOCKSTATE, s, WRITER)) {
-                        if (waiting)
-                            waiter = null;
+                        if (waiter == current)
+                            U.compareAndSetObject(this, WAITERTHREAD, current, null);
                         return;
                     }
                 }
-                else if ((s & WAITER) == 0) {
-                    if (U.compareAndSetInt(this, LOCKSTATE, s, s | WAITER)) {
-                        waiting = true;
-                        waiter = Thread.currentThread();
-                    }
-                }
-                else if (waiting)
+                else if ((s & WAITER) == 0)
+                    U.compareAndSetInt(this, LOCKSTATE, s, s | WAITER);
+                else if ((w = waiter) == null)
+                    U.compareAndSetObject(this, WAITERTHREAD, null, current);
+                else if (w == current)
                     LockSupport.park(this);
             }
         }
@@ -3287,6 +3285,8 @@ public class ConcurrentHashMap<K,V> extends AbstractMap<K,V>
         private static final Unsafe U = Unsafe.getUnsafe();
         private static final long LOCKSTATE
                 = U.objectFieldOffset(TreeBin.class, "lockState");
+        private static final long WAITERTHREAD
+                = U.objectFieldOffset(TreeBin.class, "waiter");
     }
 
     /* ----------------Table Traversal -------------- */
