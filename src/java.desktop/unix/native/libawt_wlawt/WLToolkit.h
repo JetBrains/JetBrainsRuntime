df3ffe41aafc083b4fdcdd8ba893bcf1568c7d7c
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2023-07-20 19:54:57 +0400

    JBR-5857 Wayland: implement clipboard support
    (cherry picked from commit c155acb7f5d2701da51cc9f80240a2cefc366eb2)

--- src/java.desktop/unix/native/libawt_wlawt/WLToolkit.h
+++ src/java.desktop/unix/native/libawt_wlawt/WLToolkit.h
@@ -27,6 +27,7 @@
 #include <wayland-cursor.h>
 #include "xdg-shell-client-protocol.h"
 #include "xdg-activation-v1-client-protocol.h"
+#include "primary-selection-client-protocol.h"
 
 #define CHECK_NULL_THROW_OOME_RETURN(env, x, msg, z)\
     do {                                        \
@@ -55,6 +56,8 @@ extern struct xdg_activation_v1 *xdg_activation_v1;
 extern struct gtk_shell1* gtk_shell1; // optional, check for NULL before use
 
 extern struct wl_cursor_theme *wl_cursor_theme;
+extern struct wl_data_device_manager *wl_ddm;
+extern struct zwp_primary_selection_device_manager_v1 *zwp_selection_dm;
 
 extern struct wl_surface *wl_surface_in_focus;
 
