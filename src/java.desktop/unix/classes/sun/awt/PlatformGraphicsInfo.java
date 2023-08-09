60abb74a7c90ccea34aaa4d830e7399a766a7a90
Dmitrii Morskii <dmitrii.morskii@jetbrains.com>
2023-08-09 18:12:02 +0200

    JBR-5965 Wayland: implement SplashScreen

--- src/java.desktop/unix/classes/sun/awt/PlatformGraphicsInfo.java
+++ src/java.desktop/unix/classes/sun/awt/PlatformGraphicsInfo.java
@@ -148,4 +148,11 @@ public class PlatformGraphicsInfo {
             but this program performed an operation which requires it,
             """;
     }
+
+    public static String getSplashScreenLib() {
+        if (PlatformGraphicsInfo.getToolkitID() == PlatformGraphicsInfo.TK_WAYLAND) {
+            return "wlsplashscreen";
+        }
+        return "splashscreen";
+    }
 }
