58874196e5978e7b780fac15e0760d0f22c47cdd
Dmitrii Morskii <dmitrii.morskii@jetbrains.com>
2023-08-09 20:12:02 +0400

    JBR-5965 Wayland: implement SplashScreen
    (cherry picked from commit c0247eae4f72fab14b71d88a1902d13ee03c0c9a)

--- src/java.desktop/unix/native/common/wayland/memory_utils.h
+++ src/java.desktop/unix/native/common/wayland/memory_utils.h
@@ -1,5 +1,7 @@
 /*
- * Copyright (c) 2005, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, JetBrains s.r.o.. All rights reserved.
+ *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,38 +25,12 @@
  * questions.
  */
 
-/* platform-dependent definitions */
+#ifndef MEMORY_UTILS_H
+#define MEMORY_UTILS_H
 
-#ifndef SPLASHSCREEN_CONFIG_H
-#define SPLASHSCREEN_CONFIG_H
-
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-#include <X11/extensions/shape.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <signal.h>
-#include <inttypes.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "systemScale.h"
 
-typedef uint32_t rgbquad_t;
-typedef uint16_t word_t;
-typedef uint8_t byte_t;
-typedef XRectangle RECT_T;
+int
+AllocateSharedMemoryFile(size_t size, const char* baseName);
 
-#define RECT_EQ_X(r1,r2)        ((r1).x==(r2).x && (r1).width==(r2).width)
-#define RECT_SET(r,xx,yy,ww,hh) (r).x=(xx), (r).y=(yy); (r).width=(ww); \
-                                      (r).height=(hh);
-#define RECT_INC_HEIGHT(r)      (r).height++;
-
-#define SPLASHCTL_QUIT          'Q'
-#define SPLASHCTL_UPDATE        'U'
-#define SPLASHCTL_RECONFIGURE   'R'
-
-#define INLINE static
-
-#endif
+#endif // MEMORY_UTILS_H
