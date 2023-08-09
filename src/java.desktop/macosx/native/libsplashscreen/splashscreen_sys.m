30e4aa67272a3322865d341844e017eeac553845
Dmitrii Morskii <dmitrii.morskii@jetbrains.com>
2023-08-09 18:12:02 +0200

    JBR-5965 Wayland: implement SplashScreen

--- src/java.desktop/macosx/native/libsplashscreen/splashscreen_sys.m
+++ src/java.desktop/macosx/native/libsplashscreen/splashscreen_sys.m
@@ -290,7 +290,7 @@ SplashRedrawWindow(Splash * splash) {
     [ThreadUtilities performOnMainThreadWaiting:YES block:^(){
         // drop the reference to the old view and image
         [splash->window setContentView: nil];
-        SplashUpdateScreenData(splash);
+        SplashUpdateScreenData(splash, false);
 
         // NSDeviceRGBColorSpace vs. NSCalibratedRGBColorSpace ?
         NSBitmapImageRep * rep = [[NSBitmapImageRep alloc]
