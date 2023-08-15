b607fc1ea69be6f89b90d2424a1e530e0092678c
Nikita Tsarev <nikita.tsarev@jetbrains.com>
2023-08-15 12:37:27 +0200

    JBR-5676: Support emulating input events in Wakefield
    (cherry picked from commit 6544d9d97644dc5d430854dbe7cd44a3e3caed28)

--- src/java.desktop/unix/native/libawt_wlawt/WLRobotPeer.c
+++ src/java.desktop/unix/native/libawt_wlawt/WLRobotPeer.c
@@ -31,6 +31,8 @@
 
 #include <Trace.h>
 #include <jni_util.h>
+#include <java_awt_event_KeyEvent.h>
+#include <java_awt_event_InputEvent.h>
 
 #include "sun_awt_wl_WLRobotPeer.h"
 #include "WLToolkit.h"
@@ -116,6 +118,158 @@ init_mutex_and_cond(JNIEnv *env, pthread_mutex_t *mutex, pthread_cond_t *cond)
 
 static void
 handle_wakefield_error(JNIEnv *env, uint32_t error_code);
+
+struct wayland_keycode_map_item {
+    int java_key_code;
+    int wayland_key_code;
+};
+
+// Key codes correspond to the Linux event codes:
+// https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
+static struct wayland_keycode_map_item wayland_keycode_map[] = {
+        { java_awt_event_KeyEvent_VK_ESCAPE, 1 },
+        { java_awt_event_KeyEvent_VK_1, 2 },
+        { java_awt_event_KeyEvent_VK_2, 3 },
+        { java_awt_event_KeyEvent_VK_3, 4 },
+        { java_awt_event_KeyEvent_VK_4, 5 },
+        { java_awt_event_KeyEvent_VK_5, 6 },
+        { java_awt_event_KeyEvent_VK_6, 7 },
+        { java_awt_event_KeyEvent_VK_7, 8 },
+        { java_awt_event_KeyEvent_VK_8, 9 },
+        { java_awt_event_KeyEvent_VK_9, 10 },
+        { java_awt_event_KeyEvent_VK_0, 11 },
+        { java_awt_event_KeyEvent_VK_MINUS, 12 },
+        { java_awt_event_KeyEvent_VK_EQUALS, 13 },
+        { java_awt_event_KeyEvent_VK_BACK_SPACE, 14 },
+        { java_awt_event_KeyEvent_VK_TAB, 15 },
+        { java_awt_event_KeyEvent_VK_Q, 16 },
+        { java_awt_event_KeyEvent_VK_W, 17 },
+        { java_awt_event_KeyEvent_VK_E, 18 },
+        { java_awt_event_KeyEvent_VK_R, 19 },
+        { java_awt_event_KeyEvent_VK_T, 20 },
+        { java_awt_event_KeyEvent_VK_Y, 21 },
+        { java_awt_event_KeyEvent_VK_U, 22 },
+        { java_awt_event_KeyEvent_VK_I, 23 },
+        { java_awt_event_KeyEvent_VK_O, 24 },
+        { java_awt_event_KeyEvent_VK_P, 25 },
+        { java_awt_event_KeyEvent_VK_OPEN_BRACKET, 26 },
+        { java_awt_event_KeyEvent_VK_CLOSE_BRACKET, 27 },
+        { java_awt_event_KeyEvent_VK_ENTER, 28 },
+        { java_awt_event_KeyEvent_VK_CONTROL, 29 },
+        { java_awt_event_KeyEvent_VK_A, 30 },
+        { java_awt_event_KeyEvent_VK_S, 31 },
+        { java_awt_event_KeyEvent_VK_D, 32 },
+        { java_awt_event_KeyEvent_VK_F, 33 },
+        { java_awt_event_KeyEvent_VK_G, 34 },
+        { java_awt_event_KeyEvent_VK_H, 35 },
+        { java_awt_event_KeyEvent_VK_J, 36 },
+        { java_awt_event_KeyEvent_VK_K, 37 },
+        { java_awt_event_KeyEvent_VK_L, 38 },
+        { java_awt_event_KeyEvent_VK_SEMICOLON, 39 },
+        { java_awt_event_KeyEvent_VK_QUOTE, 40 },
+        { java_awt_event_KeyEvent_VK_BACK_QUOTE, 41 },
+        { java_awt_event_KeyEvent_VK_SHIFT, 42 },
+        { java_awt_event_KeyEvent_VK_BACK_SLASH, 43 },
+        { java_awt_event_KeyEvent_VK_Z, 44 },
+        { java_awt_event_KeyEvent_VK_X, 45 },
+        { java_awt_event_KeyEvent_VK_C, 46 },
+        { java_awt_event_KeyEvent_VK_V, 47 },
+        { java_awt_event_KeyEvent_VK_B, 48 },
+        { java_awt_event_KeyEvent_VK_N, 49 },
+        { java_awt_event_KeyEvent_VK_M, 50 },
+        { java_awt_event_KeyEvent_VK_COMMA, 51 },
+        { java_awt_event_KeyEvent_VK_PERIOD, 52 },
+        { java_awt_event_KeyEvent_VK_SLASH, 53 },
+        { java_awt_event_KeyEvent_VK_MULTIPLY, 55 },
+        { java_awt_event_KeyEvent_VK_ALT, 56 },
+        { java_awt_event_KeyEvent_VK_SPACE, 57 },
+        { java_awt_event_KeyEvent_VK_CAPS_LOCK, 58 },
+        { java_awt_event_KeyEvent_VK_F1, 59 },
+        { java_awt_event_KeyEvent_VK_F2, 60 },
+        { java_awt_event_KeyEvent_VK_F3, 61 },
+        { java_awt_event_KeyEvent_VK_F4, 62 },
+        { java_awt_event_KeyEvent_VK_F5, 63 },
+        { java_awt_event_KeyEvent_VK_F6, 64 },
+        { java_awt_event_KeyEvent_VK_F7, 65 },
+        { java_awt_event_KeyEvent_VK_F8, 66 },
+        { java_awt_event_KeyEvent_VK_F9, 67 },
+        { java_awt_event_KeyEvent_VK_F10, 68 },
+        { java_awt_event_KeyEvent_VK_NUM_LOCK, 69 },
+        { java_awt_event_KeyEvent_VK_SCROLL_LOCK, 70 },
+        { java_awt_event_KeyEvent_VK_NUMPAD7, 71 },
+        { java_awt_event_KeyEvent_VK_KP_UP, 72 },
+        { java_awt_event_KeyEvent_VK_NUMPAD8, 72 },
+        { java_awt_event_KeyEvent_VK_NUMPAD9, 73 },
+        { java_awt_event_KeyEvent_VK_SUBTRACT, 74 },
+        { java_awt_event_KeyEvent_VK_KP_LEFT, 75 },
+        { java_awt_event_KeyEvent_VK_NUMPAD4, 75 },
+        { java_awt_event_KeyEvent_VK_NUMPAD5, 76 },
+        { java_awt_event_KeyEvent_VK_KP_RIGHT, 77 },
+        { java_awt_event_KeyEvent_VK_NUMPAD6, 77 },
+        { java_awt_event_KeyEvent_VK_ADD, 78 },
+        { java_awt_event_KeyEvent_VK_NUMPAD1, 79 },
+        { java_awt_event_KeyEvent_VK_KP_DOWN, 80 },
+        { java_awt_event_KeyEvent_VK_NUMPAD2, 80 },
+        { java_awt_event_KeyEvent_VK_NUMPAD3, 81 },
+        { java_awt_event_KeyEvent_VK_NUMPAD0, 82 },
+        { java_awt_event_KeyEvent_VK_DECIMAL, 83 },
+        { java_awt_event_KeyEvent_VK_LESS, 86 },
+        { java_awt_event_KeyEvent_VK_F11, 87 },
+        { java_awt_event_KeyEvent_VK_F12, 88 },
+        { java_awt_event_KeyEvent_VK_KATAKANA, 90 },
+        { java_awt_event_KeyEvent_VK_HIRAGANA, 91 },
+        { java_awt_event_KeyEvent_VK_INPUT_METHOD_ON_OFF, 92 },
+        { java_awt_event_KeyEvent_VK_NONCONVERT, 94 },
+        { java_awt_event_KeyEvent_VK_DIVIDE, 98 },
+        { java_awt_event_KeyEvent_VK_PRINTSCREEN, 99 },
+        { java_awt_event_KeyEvent_VK_ALT_GRAPH, 100 },
+        { java_awt_event_KeyEvent_VK_HOME, 102 },
+        { java_awt_event_KeyEvent_VK_UP, 103 },
+        { java_awt_event_KeyEvent_VK_PAGE_UP, 104 },
+        { java_awt_event_KeyEvent_VK_LEFT, 105 },
+        { java_awt_event_KeyEvent_VK_RIGHT, 106 },
+        { java_awt_event_KeyEvent_VK_END, 107 },
+        { java_awt_event_KeyEvent_VK_DOWN, 108 },
+        { java_awt_event_KeyEvent_VK_PAGE_DOWN, 109 },
+        { java_awt_event_KeyEvent_VK_INSERT, 110 },
+        { java_awt_event_KeyEvent_VK_DELETE, 111 },
+        { java_awt_event_KeyEvent_VK_PAUSE, 119 },
+        { java_awt_event_KeyEvent_VK_DECIMAL, 121 },
+        { java_awt_event_KeyEvent_VK_META, 125 },
+        { java_awt_event_KeyEvent_VK_WINDOWS, 125 },
+        { java_awt_event_KeyEvent_VK_STOP, 128 },
+        { java_awt_event_KeyEvent_VK_AGAIN, 129 },
+        { java_awt_event_KeyEvent_VK_UNDO, 131 },
+        { java_awt_event_KeyEvent_VK_FIND, 136 },
+        { java_awt_event_KeyEvent_VK_HELP, 138 },
+        { java_awt_event_KeyEvent_VK_LEFT_PARENTHESIS, 179 },
+        { java_awt_event_KeyEvent_VK_RIGHT_PARENTHESIS, 180 },
+        { java_awt_event_KeyEvent_VK_F13, 183 },
+        { java_awt_event_KeyEvent_VK_F14, 184 },
+        { java_awt_event_KeyEvent_VK_F15, 185 },
+        { java_awt_event_KeyEvent_VK_F16, 186 },
+        { java_awt_event_KeyEvent_VK_F17, 187 },
+        { java_awt_event_KeyEvent_VK_F18, 188 },
+        { java_awt_event_KeyEvent_VK_F19, 189 },
+        { java_awt_event_KeyEvent_VK_F20, 190 },
+        { java_awt_event_KeyEvent_VK_F21, 191 },
+        { java_awt_event_KeyEvent_VK_F22, 192 },
+        { java_awt_event_KeyEvent_VK_F23, 193 },
+        { java_awt_event_KeyEvent_VK_F24, 194 },
+        { java_awt_event_KeyEvent_VK_UNDEFINED, -1 }
+};
+
+struct wayland_button_map_item {
+    int java_button_mask;
+    int wayland_button_code;
+};
+
+static struct wayland_button_map_item wayland_button_map[] = {
+        { java_awt_event_InputEvent_BUTTON1_DOWN_MASK | java_awt_event_InputEvent_BUTTON1_MASK, 0x110 },
+        { java_awt_event_InputEvent_BUTTON2_DOWN_MASK | java_awt_event_InputEvent_BUTTON2_MASK, 0x112 },
+        { java_awt_event_InputEvent_BUTTON3_DOWN_MASK | java_awt_event_InputEvent_BUTTON3_MASK, 0x111 },
+        { -1, -1 },
+};
 #endif
 
 static jclass    pointClass;          // java.awt.Point
@@ -295,6 +449,79 @@ Java_sun_awt_wl_WLRobotPeer_getRGBPixelsImpl
     return arrayObj;
 #else
     return NULL;
+#endif
+}
+
+JNIEXPORT void JNICALL
+Java_sun_awt_wl_WLRobotPeer_sendJavaKeyImpl
+        (JNIEnv *env, jclass clazz, jint javaKeyCode, jboolean pressed)
+{
+#ifdef WAKEFIELD_ROBOT
+    if (!wakefield) {
+        JNU_ThrowByName(env, "java/awt/AWTError", "no 'wakefield' protocol extension");
+        return;
+    }
+
+    uint32_t key = 0;
+
+    for (struct wayland_keycode_map_item* item = wayland_keycode_map; item->wayland_key_code != -1; ++item) {
+        if (item->java_key_code == javaKeyCode) {
+            key = item->wayland_key_code;
+            break;
+        }
+    }
+
+    if (!key) {
+        return;
+    }
+
+    wakefield_send_key(wakefield, key, pressed ? 1 : 0);
+#endif
+}
+
+JNIEXPORT void JNICALL
+Java_sun_awt_wl_WLRobotPeer_mouseMoveImpl
+        (JNIEnv *env, jclass clazz, jint x, jint y)
+{
+#ifdef WAKEFIELD_ROBOT
+    if (!wakefield) {
+        JNU_ThrowByName(env, "java/awt/AWTError", "no 'wakefield' protocol extension");
+        return;
+    }
+
+    wakefield_send_cursor(wakefield, x, y);
+#endif
+}
+
+JNIEXPORT void JNICALL
+Java_sun_awt_wl_WLRobotPeer_sendMouseButtonImpl
+        (JNIEnv *env, jclass clazz, jint buttons, jboolean pressed)
+{
+#ifdef WAKEFIELD_ROBOT
+    if (!wakefield) {
+        JNU_ThrowByName(env, "java/awt/AWTError", "no 'wakefield' protocol extension");
+        return;
+    }
+
+    for (struct wayland_button_map_item* item = wayland_button_map; item->wayland_button_code != -1; ++item) {
+        if (item->java_button_mask & buttons) {
+            wakefield_send_button(wakefield, item->wayland_button_code, pressed ? 1 : 0);
+        }
+    }
+#endif
+}
+
+JNIEXPORT void JNICALL
+Java_sun_awt_wl_WLRobotPeer_mouseWheelImpl
+        (JNIEnv *env, jclass clazz, jint amount)
+{
+#ifdef WAKEFIELD_ROBOT
+    if (!wakefield) {
+        JNU_ThrowByName(env, "java/awt/AWTError", "no 'wakefield' protocol extension");
+        return;
+    }
+
+    wakefield_send_wheel(wakefield, amount);
 #endif
 }
 
