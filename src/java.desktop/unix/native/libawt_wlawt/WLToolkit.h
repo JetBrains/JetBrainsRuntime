65b68091593d39ae101936af8fec23ee343f9d43
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2023-09-28 17:48:58 +0400

    JBR-6138 Wayland: utilize gtk_shell1 protocol to mark dialogs as modal
    (cherry picked from commit ba60bfa1f4e27c0e1851fc38b01ee5045340caa7)

--- src/java.desktop/unix/native/libawt_wlawt/WLToolkit.h
+++ src/java.desktop/unix/native/libawt_wlawt/WLToolkit.h
@@ -44,12 +44,16 @@
         }                                       \
     } while(0)                                  \
 
+struct gtk_shell1;
+
 extern struct wl_seat *wl_seat;
 extern struct wl_display *wl_display;
 extern struct wl_pointer *wl_pointer;
 extern struct wl_compositor *wl_compositor;
 extern struct xdg_wm_base *xdg_wm_base;
 extern struct xdg_activation_v1 *xdg_activation_v1;
+extern struct gtk_shell1* gtk_shell1; // optional, check for NULL before use
+
 extern struct wl_cursor_theme *wl_cursor_theme;
 
 extern struct wl_surface *wl_surface_in_focus;
