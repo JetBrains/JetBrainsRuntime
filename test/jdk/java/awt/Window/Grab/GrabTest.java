13f6aa89f27178b338cd196196d13896bd82517c
Nikita Provotorov <nikita.provotorov@jetbrains.com>
2023-10-02 21:10:39 +0200

    JBR-6125: macOS14 java/awt/Window/Grab/GrabTest.java Frame can't be focused.
    Fixes a data race in the test.

--- test/jdk/java/awt/Window/Grab/GrabTest.java
+++ test/jdk/java/awt/Window/Grab/GrabTest.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, JetBrains s.r.o.. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -178,6 +179,8 @@ public class GrabTest {
         f1.setVisible(true);
         Util.waitForIdle(robot);
         Util.clickOnTitle(f1, robot);
+        robot.delay(100);
+        Util.waitForIdle(robot);
         if (!ungrabbed) {
             passed = false;
             System.err.println("Failure: [5] Press inside of other Frame's title didn't cause ungrab");
