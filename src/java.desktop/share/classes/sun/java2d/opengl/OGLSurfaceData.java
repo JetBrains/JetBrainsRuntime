ae35d4ba599de2f67186067d43b0c13acc874786
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2023-10-04 10:05:41 +0400

    JBR-5405: implementation of direct raster loading for VolatileImage
    (cherry picked from commit d96303544554c61dc1d2b527e1fa88f743ba95b3)

--- src/java.desktop/share/classes/sun/java2d/opengl/OGLSurfaceData.java
+++ src/java.desktop/share/classes/sun/java2d/opengl/OGLSurfaceData.java
@@ -642,4 +642,30 @@ public abstract class OGLSurfaceData extends SurfaceData
     boolean isOnScreen() {
         return getType() == WINDOW;
     }
+
+    /**
+     * Loads native image raster into surface.
+     *
+     * @param pRaster native pointer image raster with 8-bit RGBA color components packed into integer pixels.
+     * Note: The color data in this image is considered to be premultiplied with alpha.
+     * @param width width of image in pixels
+     * @param height height of image in pixels
+     * @param pRects native pointer to array of "dirty" rects, each rect is a sequence of four 32-bit integers: x, y, width, heigth
+     * Note: can be null (then whole image used)
+     * @param rectsCount count of "dirty" rects (if 0 then whole image used)
+     */
+    @Override
+    protected void loadNativeRaster(long pRaster, int width, int height, long pRects, int rectsCount) {
+        OGLRenderQueue rq = OGLRenderQueue.getInstance();
+        rq.lock();
+        try {
+            OGLContext.setScratchSurface(getOGLGraphicsConfig());
+            rq.flushAndInvokeNow(() -> loadNativeRasterWithRects(getNativeOps(), pRaster, width, height, pRects, rectsCount));
+        } finally {
+            rq.unlock();
+        }
+        markDirty();
+    }
+
+    private static native boolean loadNativeRasterWithRects(long sdops, long pRaster, int width, int height, long pRects, int rectsCount);
 }
