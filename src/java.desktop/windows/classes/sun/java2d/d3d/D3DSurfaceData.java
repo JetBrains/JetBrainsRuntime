2e341c944575addf9d7b35fafeae9b91e7a0e7be
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2023-10-04 10:05:41 +0400

    JBR-5405: implementation of direct raster loading for VolatileImage
    (cherry picked from commit d96303544554c61dc1d2b527e1fa88f743ba95b3)

--- src/java.desktop/windows/classes/sun/java2d/d3d/D3DSurfaceData.java
+++ src/java.desktop/windows/classes/sun/java2d/d3d/D3DSurfaceData.java
@@ -1036,4 +1036,32 @@ public class D3DSurfaceData extends SurfaceData implements AccelSurface {
      */
     public static native boolean updateWindowAccelImpl(long pd3dsd, long pData,
                                                        int w, int h);
+
+    /**
+     * Loads native image raster into surface.
+     *
+     * @param pRaster native pointer image raster with 8-bit RGBA color components packed into integer pixels.
+     * Note: The color data in this image is considered to be premultiplied with alpha.
+     * @param width width of image in pixels
+     * @param height height of image in pixels
+     * @param pRects native pointer to array of "dirty" rects, each rect is a sequence of four 32-bit integers: x, y, width, heigth
+     * Note: can be null (then whole image used)
+     * @param rectsCount count of "dirty" rects (if 0 then whole image used)
+     */
+    @Override
+    protected void loadNativeRaster(long pRaster, int width, int height, long pRects, int rectsCount) {
+        D3DRenderQueue rq = D3DRenderQueue.getInstance();
+        rq.lock();
+        try {
+            // make sure we have a current context before uploading
+            // the sysmem data to the texture object
+            D3DContext.setScratchSurface(getContext());
+            rq.flushAndInvokeNow(() -> loadNativeRasterWithRects(getNativeOps(), pRaster, width, height, pRects, rectsCount));
+        } finally {
+            rq.unlock();
+        }
+        markDirty();
+    }
+
+    private static native boolean loadNativeRasterWithRects(long pData, long pRaster, int width, int height, long pRects, int rectsCount);
 }
