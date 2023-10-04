e22fbf2a34c0038456632294a47f58bf0a206efd
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2023-10-04 10:05:41 +0400

    JBR-5405: implementation of direct raster loading for VolatileImage
    (cherry picked from commit d96303544554c61dc1d2b527e1fa88f743ba95b3)

--- src/java.desktop/share/classes/sun/java2d/SurfaceData.java
+++ src/java.desktop/share/classes/sun/java2d/SurfaceData.java
@@ -1078,4 +1078,19 @@ public abstract class SurfaceData
             throw new InvalidPipeException("wrong surface data type: " + surfaceData);
         }
     }
+
+    /**
+     * Loads native image raster into surface.
+     *
+     * @param pRaster native pointer image raster with 8-bit RGBA color components packed into integer pixels.
+     * Note: The color data in this image is considered to be premultiplied with alpha.
+     * @param width width of image in pixels
+     * @param height height of image in pixels
+     * @param pRects native pointer to array of "dirty" rects, each rect is a sequence of four 32-bit integers: x, y, width, heigth
+     * Note: can be null (then whole image used)
+     * @param rectsCount count of "dirty" rects (if 0 then whole image used)
+     */
+    protected void loadNativeRaster(long pRaster, int width, int height, long pRects, int rectsCount) {
+        System.err.println("ERROR: loadNativeRaster must be overriden.");
+    }
 }
