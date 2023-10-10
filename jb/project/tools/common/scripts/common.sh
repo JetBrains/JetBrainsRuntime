c454199debfcb7609d195dec68ac6a7b2597d9dd
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2023-10-10 14:29:40 +0400

    JBR-6181 add Linux executables with bundled FreeType
    (cherry picked from commit 12449b3de580a2073c5974c121236e0f4f06a426)

--- jb/project/tools/common/scripts/common.sh
+++ jb/project/tools/common/scripts/common.sh
@@ -5,7 +5,7 @@ set -x
 
 function check_bundle_type_maketest() {
   # check whether last char is 't', if so remove it
-  if [ "${bundle_type: -1}" == "t" ]; then
+  if [ "${bundle_type: -1}" == "t" ] && [ "${bundle_type: -2}" != "ft" ]; then
     bundle_type="${bundle_type%?}"
     do_maketest=1
   else
@@ -37,7 +37,7 @@ architecture=${3:-x64} # aarch64 or x64
 check_bundle_type_maketest
 
 tag_prefix="jdk-"
-OPENJDK_TAG=$(git log --simplify-by-decoration --decorate=short --pretty=short | grep "$tag_prefix" | cut -d "(" -f2 | cut -d ")" -f1 | awk '{print $2}' | tr -d ',' | sort -t "-" -k 2 -g | tail -n 1)
+OPENJDK_TAG=$(git log --simplify-by-decoration --decorate=short --pretty=short | grep "$tag_prefix" | cut -d "(" -f2 | cut -d ")" -f1 | awk '{print $2}' | sort -t "-" -k 2 -g | tail -n 1 | tr -d ",")
 VERSION_FEATURE=$(getVersionProp "DEFAULT_VERSION_FEATURE")
 VERSION_INTERIM=$(getVersionProp "DEFAULT_VERSION_INTERIM")
 VERSION_UPDATE=$(getVersionProp "DEFAULT_VERSION_UPDATE")
@@ -93,6 +93,12 @@ case "$OS_NAME" in
 esac
 
 WITH_ZIPPED_NATIVE_DEBUG_SYMBOLS="--with-native-debug-symbols=zipped"
+
+if [ "$bundle_type" == "nomodft" ]; then
+  WITH_BUNDLED_FREETYPE="--with-freetype=bundled"
+else
+  WITH_BUNDLED_FREETYPE=""
+fi
 
 REPRODUCIBLE_BUILD_OPTS="--with-source-date=$SOURCE_DATE_EPOCH
   --with-hotspot-build-time=$BUILD_TIME
