8290653a8dcf53b49c18c849892f46ae15b0f100
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2023-10-31 15:01:59 +0400

    JBR-6220 javax/swing/GraphicsConfigNotifier/StalePreferredSize.java became failing by time out on Linux
    (cherry picked from commit 3f785907073e62e661adc2f4c8adc27faeedf14e)

--- src/java.desktop/unix/classes/sun/awt/X11/XWM.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XWM.java
@@ -1841,6 +1841,7 @@ final class XWM
             Native.putLong(data, 2, 0);
             Native.putLong(data, 3, 0);
 
+            XToolkit.awtLock();
             XErrorHandlerUtil.WITH_XERROR_HANDLER(XErrorHandler.VerifyChangePropertyHandler.getInstance());
             XlibWrapper.XChangeProperty(XToolkit.getDisplay(), window,
                     XA_GTK_FRAME_EXTENTS.getAtom(),
@@ -1849,8 +1850,8 @@ final class XWM
                     data, 4);
             XErrorHandlerUtil.RESTORE_XERROR_HANDLER();
         } finally {
+            XToolkit.awtUnlock();
             unsafe.freeMemory(data);
-            data = 0;
         }
     }
 
