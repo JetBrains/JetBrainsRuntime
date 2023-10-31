ffeb81753675914d02a054c7b83128a42af1f69d
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2023-10-31 11:23:23 +0700

    JBR-6246 add default CDS archives into jbrsdk distributions

--- jb/project/tools/mac/scripts/mkimages.sh
+++ jb/project/tools/mac/scripts/mkimages.sh
@@ -72,6 +72,7 @@ function create_image_bundle {
     sed 's/JBR/JBRSDK/g' $JRE_CONTENTS/Home/release > release
     mv release $JRE_CONTENTS/Home/release
     cp $IMAGES_DIR/jdk-bundle/jdk-$JBSDK_VERSION.jdk/Contents/Home/lib/src.zip $JRE_CONTENTS/Home/lib
+    cp $IMAGES_DIR/jdk-bundle/jdk-$JBSDK_VERSION.jdk/Contents/Home/lib/server/*.jsa $JRE_CONTENTS/Home/lib/server
     copy_jmods "$__modules" "$__modules_path" "$JRE_CONTENTS"/Home/jmods
     zip_native_debug_symbols $IMAGES_DIR/jdk-bundle/jdk-$JBSDK_VERSION.jdk "${JBR}_diz"
   fi
@@ -87,7 +88,7 @@ function create_image_bundle {
 
   echo Creating "$JBR".tar.gz ...
   # Normalize timestamp
-  find "$tmp"/"$__root_dir" -print0 | xargs -0 touch -c -h -t "$TOUCH_TIME"
+  #find "$tmp"/"$__root_dir" -print0 | xargs -0 touch -c -h -t "$TOUCH_TIME"
 
   (cd "$tmp" &&
       find "$__root_dir" -print0 | LC_ALL=C sort -z | \
