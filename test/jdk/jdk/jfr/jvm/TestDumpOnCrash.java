b64a1acb212a64629094bf7e992d98500f91d629
Ralf Schmelter <rschmelter@openjdk.org>
2023-11-06 12:35:24 +0000

    8293166: jdk/jfr/jvm/TestDumpOnCrash.java fails on Linux ppc64le and Linux aarch64
    Reviewed-by: aph, stuefe
    Backport-of: 5551cb66ba58cadce7291a95c5af662ade2cb7b8

--- test/jdk/jdk/jfr/jvm/TestDumpOnCrash.java
+++ test/jdk/jdk/jfr/jvm/TestDumpOnCrash.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -95,6 +95,7 @@ public class TestDumpOnCrash {
                 "-Xmx64m",
                 "-XX:-TransmitErrorReport",
                 "-XX:-CreateCoredumpOnCrash",
+                "-XX:-TieredCompilation", // Avoid secondary crashes (see JDK-8293166)
                 "--add-exports=java.base/jdk.internal.misc=ALL-UNNAMED",
                 "-XX:StartFlightRecording=" + flightRecordingOptions,
                 crasher,
