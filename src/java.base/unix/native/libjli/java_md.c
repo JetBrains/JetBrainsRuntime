506a88c6c0551ada7e5b39b4d3220079c9f707f9
Dmitrii Morskii <dmitrii.morskii@jetbrains.com>
2023-11-10 12:53:39 +0100

    fix choosing default awt toolkit

--- src/java.base/unix/native/libjli/java_md.c
+++ src/java.base/unix/native/libjli/java_md.c
@@ -160,7 +160,10 @@ typedef enum awt_toolkit {
     TK_WAYLAND = 2
 } awt_toolkit;
 
-static awt_toolkit _awt_toolkit = TK_UNKNOWN;
+// TODO when wayland support will be fully implemented change to TK_UNKNOWN
+// currently wayland support is not Production-Ready ready so default awt toolkit is X11
+// wayland could be chosen manually via passing -Dawt.toolkit.name=WLToolkit argument
+static awt_toolkit _awt_toolkit = TK_X11;
 
 /*
  * execname accessor from other parts of platform dependent logic
