b39bdd9440fb8d3d921acc7eaa7fce613189a9e9
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2023-11-14 17:38:16 +0100

    JBR-5621 Test failures with -Dsun.java2d.metal.displaySync=false
    Created intermediate buffer in the MTLLayer
    Implemented frame separation of window updates for AWT and Swing
    Remove frame delays as they greatly affect throughput
    Fixed test/jdk/sun/java2d/GdiRendering/ClipShapeRendering.java
    Resolved crash on multi-GPU systems
    
    (cherry picked from commit 20bf935c82a9452e2271a8e167adc115e52f1911)

--- src/java.desktop/share/classes/sun/awt/SunToolkit.java
+++ src/java.desktop/share/classes/sun/awt/SunToolkit.java
@@ -2010,6 +2010,10 @@ public abstract class SunToolkit extends Toolkit
         return false;
     }
 
+    public boolean needUpdateWindowAfterPaint() {
+        return false;
+    }
+
     /**
      * Descendants of the SunToolkit should override and put their own logic here.
      */
