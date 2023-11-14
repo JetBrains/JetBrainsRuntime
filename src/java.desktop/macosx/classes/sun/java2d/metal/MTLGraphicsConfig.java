86aa4e1c672ef6be40129b77096139b389c155fc
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2023-11-14 17:38:16 +0100

    JBR-5621 Test failures with -Dsun.java2d.metal.displaySync=false
    Created intermediate buffer in the MTLLayer
    Implemented frame separation of window updates for AWT and Swing
    Remove frame delays as they greatly affect throughput
    Fixed test/jdk/sun/java2d/GdiRendering/ClipShapeRendering.java
    Resolved crash on multi-GPU systems
    
    (cherry picked from commit 20bf935c82a9452e2271a8e167adc115e52f1911)

--- src/java.desktop/macosx/classes/sun/java2d/metal/MTLGraphicsConfig.java
+++ src/java.desktop/macosx/classes/sun/java2d/metal/MTLGraphicsConfig.java
@@ -293,6 +293,11 @@ public final class MTLGraphicsConfig extends CGraphicsConfig
         }
     }
 
+    @Override
+    public void flush(LWComponentPeer<?, ?> peer) {
+        MTLSurfaceData.flushBuffer(peer.getPlatformWindow().getLayerPtr());
+    }
+
     private static class MTLBufferCaps extends BufferCapabilities {
         public MTLBufferCaps(boolean dblBuf) {
             super(imageCaps, imageCaps,
