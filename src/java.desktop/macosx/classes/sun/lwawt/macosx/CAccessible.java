4ec770ff3d5fa7335117a41636a759a40d025a14
Dmitry Drobotov <dmitry.drobotov@jetbrains.com>
2023-11-15 19:18:19 +0100

    JBR-4479 Add text caret tracking for macOS Accessibility Zoom
    (cherry picked from commit 0dfbf34b37fba229810485e0ec92ae8670b87571)

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CAccessible.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CAccessible.java
@@ -87,6 +87,7 @@ class CAccessible extends CFRetainedResource implements Accessible {
     private static native void treeNodeCollapsed(long ptr);
     private static native void selectedCellsChanged(long ptr);
     private static native void tableContentCacheClear(long ptr);
+    private static native void updateZoomCaretFocus(long ptr);
 
     private Accessible accessible;
 
@@ -132,6 +133,8 @@ class CAccessible extends CFRetainedResource implements Accessible {
                 Object oldValue = e.getOldValue();
                 if (name.equals(ACCESSIBLE_CARET_PROPERTY)) {
                     execute(ptr -> selectedTextChanged(ptr));
+                    // Notify macOS Accessibility Zoom to move focus to the new caret location.
+                    execute(ptr -> updateZoomCaretFocus(ptr));
                 } else if (name.equals(ACCESSIBLE_TEXT_PROPERTY)) {
                     execute(ptr -> valueChanged(ptr));
                 } else if (name.equals(ACCESSIBLE_SELECTION_PROPERTY)) {
