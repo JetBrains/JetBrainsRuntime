7c697ab7cbd76e448b66c4b3f2b36ddddf56b42c
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2023-11-23 07:13:58 +0700

    Revert "JBR-5724: fixed serialization and backward compatibility of Font"
    This reverts commit c5cc5b4dedbdf73871f14db92afaac15fd985d86.
    
    (cherry picked from commit 34ed77394f8814dcfd4806fd6a3ea9881f4e9ccf)

--- src/java.desktop/share/classes/java/awt/Font.java
+++ src/java.desktop/share/classes/java/awt/Font.java
@@ -1896,7 +1896,7 @@ public class Font implements java.io.Serializable
      *
      * @serial
      */
-    private int fontSerializedDataVersion = 2;
+    private int fontSerializedDataVersion = 1;
 
     /**
      * Writes default serializable fields to a stream.
@@ -1965,13 +1965,9 @@ public class Font implements java.io.Serializable
             } catch (Throwable t) {
                 throw new IOException(t);
             } finally {
-                fRequestedAttributes = null; // don't need it any more
-            }
+            fRequestedAttributes = null; // don't need it any more
         }
-
-        if (features == null) {
-            features = new TreeMap<>();
-        }
+    }
     }
 
     /**
