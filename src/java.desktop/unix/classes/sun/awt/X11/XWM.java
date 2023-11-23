5223e9920dd9be8bbb9ce433299dbd255209f43d
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2023-11-23 16:55:14 +0100

    JBR-6264 Improved coordinate conversion in XWayland mode

--- src/java.desktop/unix/classes/sun/awt/X11/XWM.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XWM.java
@@ -1083,10 +1083,10 @@ final class XWM
             }
             window.updateSizeHints(window.getDimensions());
             requestWMExtents(window.getWindow());
+            Point p = window.scaleUp(shellBounds.x, shellBounds.y);
             XlibWrapper.XMoveResizeWindow(XToolkit.getDisplay(),
                                           window.getShell(),
-                                          window.scaleUpX(shellBounds.x),
-                                          window.scaleUpY(shellBounds.y),
+                                          p.x, p.y,
                                           window.scaleUp(shellBounds.width),
                                           window.scaleUp(shellBounds.height));
             /* REMINDER: will need to revisit when setExtendedStateBounds is added */
@@ -1121,10 +1121,10 @@ final class XWM
                 window.updateSizeHints(newDimensions);
                 requestWMExtents(window.getWindow());
                 XToolkit.XSync();
+                Point p = window.scaleUp(shellBounds.x, shellBounds.y);
                 XlibWrapper.XMoveResizeWindow(XToolkit.getDisplay(),
                                               window.getShell(),
-                                              window.scaleUpX(shellBounds.x),
-                                              window.scaleUpY(shellBounds.y),
+                                              p.x, p.y,
                                               window.scaleUp(shellBounds.width),
                                               window.scaleUp(shellBounds.height));
             }
