5dbfd5b6fbee7bd4adac93cce9578acae751d5ce
Nikita Tsarev <nikita.tsarev@jetbrains.com>
2023-11-27 00:42:01 +0100

    JBR-6331: Fix some memory safety issues in macOS keyboards

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/LWCToolkit.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/LWCToolkit.java
@@ -143,9 +143,7 @@ public final class LWCToolkit extends LWToolkit {
 
     private static native String[] getKeyboardLayoutListNative(boolean includeAll);
 
-    private static native boolean enableKeyboardLayoutNative(String layoutName);
-
-    private static native boolean disableKeyboardLayoutNative(String layoutName);
+    private static native boolean setKeyboardLayoutEnabledNative(String layoutName, boolean enabled);
 
     public static void switchKeyboardLayout (String layoutName) {
         if (layoutName == null || layoutName.isEmpty()) {
@@ -172,7 +170,7 @@ public final class LWCToolkit extends LWToolkit {
         if (layoutName == null || layoutName.isEmpty()) {
             throw new RuntimeException("A valid layout ID is expected. Found:  " + layoutName);
         }
-        if (!enableKeyboardLayoutNative(layoutName)) {
+        if (!setKeyboardLayoutEnabledNative(layoutName, true)) {
             throw new RuntimeException("Couldn't enable layout " + layoutName);
         }
     }
@@ -181,7 +179,7 @@ public final class LWCToolkit extends LWToolkit {
         if (layoutName == null || layoutName.isEmpty()) {
             throw new RuntimeException("A valid layout ID is expected. Found:  " + layoutName);
         }
-        if (!disableKeyboardLayoutNative(layoutName)) {
+        if (!setKeyboardLayoutEnabledNative(layoutName, false)) {
             throw new RuntimeException("Couldn't disable layout " + layoutName);
         }
     }
