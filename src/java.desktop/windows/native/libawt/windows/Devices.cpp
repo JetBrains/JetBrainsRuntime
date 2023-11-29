11a87bbb5740252724622114f37835d9250ae2d2
Nikita Gubarkov <Nikita.Gubarkov@jetbrains.com>
2023-11-29 22:19:41 +0100

    JBR-4618 Force window size update after display reconfiguration
    - Re-create all GraphicsDevices on displayChanged()
    
    (cherry picked from commit a59d4903b2a5d45aca7b1b223a503b9442b44427)

--- src/java.desktop/windows/native/libawt/windows/Devices.cpp
+++ src/java.desktop/windows/native/libawt/windows/Devices.cpp
@@ -233,6 +233,7 @@ BOOL Devices::UpdateInstance(JNIEnv *env)
     }
     for (i = 0; i < numScreens; ++i) {
         rawDevices[i]->Initialize();
+        rawDevices[i]->InitDesktopScales();
     }
     {
         CriticalSection::Lock l(arrayLock);
