5316bc3b505e141a5a2b426378162629f93f5b02
Nikita Gubarkov <Nikita.Gubarkov@jetbrains.com>
2023-12-04 18:35:10 +0100

    JBR-6387 Revert "8315701: [macos] Regression: KeyEvent has different keycode on different keyboard layouts"

--- src/java.desktop/macosx/classes/sun/lwawt/LWWindowPeer.java
+++ src/java.desktop/macosx/classes/sun/lwawt/LWWindowPeer.java
@@ -1044,7 +1044,7 @@ public class LWWindowPeer
      */
     @Override
     public void notifyKeyEvent(int id, long when, int modifiers,
-                               int keyCode, char keyChar, int keyLocation, int extendedKeyCode)
+                               int keyCode, char keyChar, int keyLocation)
     {
         LWKeyboardFocusManagerPeer kfmPeer = LWKeyboardFocusManagerPeer.getInstance();
         Component focusOwner = kfmPeer.getCurrentFocusOwner();
@@ -1058,13 +1058,9 @@ public class LWWindowPeer
 
         KeyEvent keyEvent = new KeyEvent(focusOwner, id, when, modifiers,
             keyCode, keyChar, keyLocation);
-        if (extendedKeyCode >= 0) {
-            AWTAccessor.getKeyEventAccessor().setExtendedKeyCode(keyEvent, extendedKeyCode);
-        } else {
-            AWTAccessor.getKeyEventAccessor().setExtendedKeyCode(keyEvent,
-                    (keyChar == KeyEvent.CHAR_UNDEFINED) ? keyCode
-                            : ExtendedKeyCodes.getExtendedKeyCodeForChar(keyChar));
-        }
+        AWTAccessor.getKeyEventAccessor().setExtendedKeyCode(keyEvent,
+                (keyChar == KeyEvent.CHAR_UNDEFINED) ? keyCode
+                : ExtendedKeyCodes.getExtendedKeyCodeForChar(keyChar));
         postEvent(keyEvent);
     }
 
