04d2033be9cdbe64c8a27959e26bf06c46320fe7
Sonia Zaldana Calles <szaldana@openjdk.org>
2023-12-05 17:58:42 +0000

    8273803: Zero: Handle "zero" variant in CommandLineOptionTest.java
    Backport-of: 2d13fb21af783ebbe3dd5c2632aea11347317026

--- test/lib/jdk/test/lib/cli/CommandLineOptionTest.java
+++ test/lib/jdk/test/lib/cli/CommandLineOptionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -502,6 +502,8 @@ public abstract class CommandLineOptionTest {
             return "-minimal";
         } else if (Platform.isGraal()) {
             return "-graal";
+        } else if (Platform.isZero()) {
+            return "-zero";
         }
         throw new RuntimeException("Unknown VM mode.");
     }
