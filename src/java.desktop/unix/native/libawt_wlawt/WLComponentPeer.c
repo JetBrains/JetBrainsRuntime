a6aa4f2b9797bc934e35bf7d67042385de912cf1
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2023-12-05 18:51:47 +0400

    JBR-6391 Wayland: memory indicator tooltip flickers

--- src/java.desktop/unix/native/libawt_wlawt/WLComponentPeer.c
+++ src/java.desktop/unix/native/libawt_wlawt/WLComponentPeer.c
@@ -482,8 +482,7 @@ Java_sun_awt_wl_WLComponentPeer_nativeCreateWLSurface
 
 static struct xdg_positioner *
 newPositioner
-        (jint parentX, jint parentY, jint parentWidth, jint parentHeight,
-         jint width, jint height, jint offsetX, jint offsetY)
+        (jint width, jint height, jint offsetX, jint offsetY)
 {
     struct xdg_positioner *xdg_positioner = xdg_wm_base_create_positioner(xdg_wm_base);
     CHECK_NULL_RETURN(xdg_positioner, NULL);
@@ -492,20 +491,20 @@ newPositioner
     // a non-zero size set by set_size, and a non-zero anchor rectangle
     // set by set_anchor_rect."
     xdg_positioner_set_size(xdg_positioner, width, height);
-    xdg_positioner_set_anchor_rect(xdg_positioner, parentX, parentY, parentWidth, parentHeight);
-    xdg_positioner_set_offset(xdg_positioner, offsetX, offsetY);
-    xdg_positioner_set_anchor(xdg_positioner, XDG_POSITIONER_ANCHOR_TOP_LEFT);
+    xdg_positioner_set_anchor_rect(xdg_positioner, offsetX, offsetY, 1, 1);
+    xdg_positioner_set_offset(xdg_positioner, 0, 0);
+    xdg_positioner_set_anchor(xdg_positioner, XDG_POSITIONER_ANCHOR_BOTTOM_LEFT);
     xdg_positioner_set_gravity(xdg_positioner, XDG_POSITIONER_GRAVITY_BOTTOM_RIGHT);
-    xdg_positioner_set_constraint_adjustment(xdg_positioner, XDG_POSITIONER_CONSTRAINT_ADJUSTMENT_SLIDE_X |
-                                                             XDG_POSITIONER_CONSTRAINT_ADJUSTMENT_SLIDE_Y);
+    xdg_positioner_set_constraint_adjustment(xdg_positioner,
+        XDG_POSITIONER_CONSTRAINT_ADJUSTMENT_FLIP_Y
+        | XDG_POSITIONER_CONSTRAINT_ADJUSTMENT_SLIDE_X
+        | XDG_POSITIONER_CONSTRAINT_ADJUSTMENT_SLIDE_Y);
     return xdg_positioner;
 }
 
 JNIEXPORT void JNICALL
 Java_sun_awt_wl_WLComponentPeer_nativeCreateWLPopup
         (JNIEnv *env, jobject obj, jlong ptr, jlong parentPtr,
-         jint parentX, jint parentY,
-         jint parentWidth, jint parentHeight,
          jint width, jint height,
          jint offsetX, jint offsetY)
 {
@@ -522,8 +521,7 @@ Java_sun_awt_wl_WLComponentPeer_nativeCreateWLPopup
     frame->toplevel = JNI_FALSE;
 
     assert(parentFrame);
-    struct xdg_positioner *xdg_positioner = newPositioner(parentX, parentY, parentWidth, parentHeight,
-        width, height, offsetX, offsetY);
+    struct xdg_positioner *xdg_positioner = newPositioner(width, height, offsetX, offsetY);
     CHECK_NULL(xdg_positioner);
     frame->xdg_popup = xdg_surface_get_popup(frame->xdg_surface, parentFrame->xdg_surface, xdg_positioner);
     CHECK_NULL(frame->xdg_popup);
@@ -540,8 +538,6 @@ Java_sun_awt_wl_WLComponentPeer_nativeCreateWLPopup
 JNIEXPORT void JNICALL
 Java_sun_awt_wl_WLComponentPeer_nativeRepositionWLPopup
         (JNIEnv *env, jobject obj, jlong ptr,
-         jint parentX, jint parentY,
-         jint parentWidth, jint parentHeight,
          jint width, jint height,
          jint offsetX, jint offsetY)
 {
@@ -549,8 +545,7 @@ Java_sun_awt_wl_WLComponentPeer_nativeRepositionWLPopup
     assert (!frame->toplevel);
 
     if (wl_proxy_get_version((struct wl_proxy *)xdg_wm_base) >= 3) {
-        struct xdg_positioner *xdg_positioner = newPositioner(parentX, parentY, parentWidth, parentHeight,
-            width, height, offsetX, offsetY);
+        struct xdg_positioner *xdg_positioner = newPositioner(width, height, offsetX, offsetY);
         CHECK_NULL(xdg_positioner);
         static int token = 42; // This will be received by xdg_popup_repositioned(); unused for now.
         xdg_popup_reposition(frame->xdg_popup, xdg_positioner, token++);
