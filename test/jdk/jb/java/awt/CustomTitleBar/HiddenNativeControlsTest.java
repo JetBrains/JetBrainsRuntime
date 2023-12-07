a74cc1051d0b9f5a2efe1bf8894bd4d2038c4f18
Sergey Shelomentsev <sergey.shelomentsev@jetbrains.com>
2023-12-07 15:17:15 +0200

    JBR-6354 fix custom title bar tests to avoid failure in MacOS 14 fullscreen mode
    - moved common logic to a separate part
    - moved parts of tests to swing package
    
    (cherry picked from commit 80b6e4ba8cad4c19b0f306ccd64dad9c085a8bab)
    (cherry picked from commit 22d1e57928b701a4683c21b428c4a62177cdb340)

--- test/jdk/jb/java/awt/CustomTitleBar/HiddenNativeControlsTest.java
+++ test/jdk/jb/java/awt/CustomTitleBar/HiddenNativeControlsTest.java
@@ -21,7 +21,13 @@
  * questions.
  */
 import com.jetbrains.JBR;
-import util.*;
+
+import test.jb.testhelpers.screenshot.ScreenShotHelpers;
+import test.jb.testhelpers.screenshot.Rect;
+import test.jb.testhelpers.TitleBar.CommonAPISuite;
+import test.jb.testhelpers.TitleBar.TaskResult;
+import test.jb.testhelpers.TitleBar.TestUtils;
+import test.jb.testhelpers.TitleBar.Task;
 
 import java.awt.Robot;
 import java.awt.image.BufferedImage;
@@ -32,6 +38,8 @@ import java.util.List;
  * @test
  * @summary Verify a property to change visibility of native controls
  * @requires (os.family == "windows" | os.family == "mac")
+ * @library ../../../testhelpers/screenshot ../../../testhelpers/TitleBar ../../../testhelpers/utils
+ * @build TestUtils TaskResult Task CommonAPISuite MouseUtils ScreenShotHelpers Rect RectCoordinates MouseUtils
  * @run main/othervm HiddenNativeControlsTest
  * @run main/othervm -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=1.0 HiddenNativeControlsTest
  * @run main/othervm -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=1.25 HiddenNativeControlsTest
@@ -89,7 +97,7 @@ public class HiddenNativeControlsTest {
             System.out.println("Found controls at the title bar:");
             foundControls.forEach(System.out::println);
 
-            if (foundControls.size() != 0) {
+            if (!foundControls.isEmpty()) {
                 err("controls are disabled, but found in the screenshot");
             }
 
