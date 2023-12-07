283cf2ab8f9287570fd2c3a69b113acf6d9314fb
Sergey Shelomentsev <sergey.shelomentsev@jetbrains.com>
2023-12-07 15:17:15 +0200

    JBR-6354 fix custom title bar tests to avoid failure in MacOS 14 fullscreen mode
    - moved common logic to a separate part
    - moved parts of tests to swing package
    
    (cherry picked from commit 80b6e4ba8cad4c19b0f306ccd64dad9c085a8bab)
    (cherry picked from commit 22d1e57928b701a4683c21b428c4a62177cdb340)

--- test/jdk/jb/java/awt/CustomTitleBar/JDialogNativeControlsTest.java
+++ test/jdk/jb/java/awt/CustomTitleBar/JDialogNativeControlsTest.java
@@ -22,7 +22,12 @@
  */
 import com.jetbrains.JBR;
 import util.*;
-
+import test.jb.testhelpers.screenshot.ScreenShotHelpers;
+import test.jb.testhelpers.screenshot.Rect;
+import test.jb.testhelpers.TitleBar.CommonAPISuite;
+import test.jb.testhelpers.TitleBar.TaskResult;
+import test.jb.testhelpers.TitleBar.TestUtils;
+import test.jb.testhelpers.TitleBar.Task;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
 import java.awt.event.WindowAdapter;
@@ -37,6 +42,8 @@ import java.util.List;
  * @test
  * @summary Detect and check behavior of clicking to native controls
  * @requires (os.family == "windows" | os.family == "mac")
+ * @library ../../../testhelpers/screenshot ../../../testhelpers/TitleBar ../../../testhelpers/utils
+ * @build TestUtils TaskResult Task CommonAPISuite MouseUtils ScreenShotHelpers Rect RectCoordinates MouseUtils
  * @run main/othervm JDialogNativeControlsTest
  * @run main/othervm -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=1.0 JDialogNativeControlsTest
  * @run main/othervm -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=1.25 JDialogNativeControlsTest
