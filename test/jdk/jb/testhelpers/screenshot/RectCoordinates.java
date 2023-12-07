6df8a2c4ebc9713d14429feeb191b89d7bf7f2fc
Sergey Shelomentsev <sergey.shelomentsev@jetbrains.com>
2023-12-07 15:17:15 +0200

    JBR-6354 fix custom title bar tests to avoid failure in MacOS 14 fullscreen mode
    - moved common logic to a separate part
    - moved parts of tests to swing package
    
    (cherry picked from commit 80b6e4ba8cad4c19b0f306ccd64dad9c085a8bab)

--- test/jdk/jb/testhelpers/screenshot/RectCoordinates.java
+++ test/jdk/jb/testhelpers/screenshot/RectCoordinates.java
@@ -20,30 +20,8 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+package test.jb.testhelpers.screenshot;
 
-package util;
-
-import com.jetbrains.WindowDecorations;
-
-import java.awt.Window;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
-public class CommonAPISuite {
-
-    public static TaskResult runTestSuite(List<Function<WindowDecorations.CustomTitleBar, Window>> functions, Task task) {
-        AtomicBoolean testPassed = new AtomicBoolean(true);
-        List<String> errors = new ArrayList<>();
-        functions.forEach(function -> {
-            TaskResult result = task.run(function);
-            testPassed.set(testPassed.get() && result.isPassed());
-            errors.add(result.getError());
-        });
-
-        return new TaskResult(testPassed.get(), errors.stream().collect(Collectors.joining("\n")));
-    }
+public record RectCoordinates(int x1, int y1, int x2, int y2) {
 
 }
