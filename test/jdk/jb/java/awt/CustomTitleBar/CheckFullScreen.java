dfa88ecc0ed258a2503c91eda7aa7fd2f4c7dfda
Sergey Shelomentsev <sergey.shelomentsev@jetbrains.com>
2023-12-07 15:17:15 +0200

    JBR-6354 fix custom title bar tests to avoid failure in MacOS 14 fullscreen mode
    - moved common logic to a separate part
    - moved parts of tests to swing package
    
    (cherry picked from commit 80b6e4ba8cad4c19b0f306ccd64dad9c085a8bab)

--- test/jdk/jb/java/awt/CustomTitleBar/CheckFullScreen.java
+++ test/jdk/jb/java/awt/CustomTitleBar/CheckFullScreen.java
@@ -1,6 +1,29 @@
+/*
+ * Copyright 2000-2023 JetBrains s.r.o.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
 import com.jetbrains.JBR;
 import com.jetbrains.WindowDecorations;
-import util.ScreenShotHelpers;
+import test.jb.testhelpers.screenshot.ScreenShotHelpers;
 
 import java.awt.AWTException;
 import java.awt.Color;
@@ -16,6 +39,8 @@ import java.io.IOException;
  * @test
  * @summary Verify modifying of title bar height
  * @requires (os.family == "windows" | os.family == "mac")
+ * @library ../../../testhelpers/screenshot ../../../testhelpers/TitleBar ../../../testhelpers/utils
+ * @build TestUtils TaskResult Task CommonAPISuite MouseUtils ScreenShotHelpers Rect RectCoordinates MouseUtils
  * @run main/othervm CheckFullScreen
  * @run main/othervm -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=1.0 CheckFullScreen
  * @run main/othervm -Dsun.java2d.uiScale.enabled=true -Dsun.java2d.uiScale=1.25 CheckFullScreen
