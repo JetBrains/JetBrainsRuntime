1d4738301613a561636eb4d50be678499b4e3153
Dmitrii Morskii <dmitrii.morskii@jetbrains.com>
2023-12-12 16:34:57 +0100

    JBR-6372: implement detecting of OS theme on linux

--- src/java.desktop/unix/classes/sun/awt/X11/XToolkit.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XToolkit.java
@@ -1923,6 +1923,7 @@ public final class XToolkit extends UNIXToolkit implements Runnable {
                                                      localEnv.displayChanged());
             }
         }
+        super.initializeDesktopProperties();
     }
 
     /**
