20f2ef4687ba692b20d206a257b31cf7106989ab
Dmitrii Morskii <dmitrii.morskii@jetbrains.com>
2023-12-12 16:34:57 +0100

    JBR-6376: implement detecting of OS theme on linux
    (cherry picked from commit d8aaa3da4720b852eea2cfd015b858794ac91d3b)

--- src/java.desktop/unix/classes/sun/awt/X11/XToolkit.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XToolkit.java
@@ -1891,6 +1891,7 @@ public final class XToolkit extends UNIXToolkit implements Runnable {
                                                      localEnv.displayChanged());
             }
         }
+        super.initializeDesktopProperties();
     }
 
     /**
