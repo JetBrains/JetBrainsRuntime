351878c506ef96c14e9136c8caccce8a2a2d15c4
Dmitrii Morskii <dmitrii.morskii@jetbrains.com>
2023-12-12 16:34:57 +0100

    JBR-6372: implement detecting of OS theme on linux

--- src/java.desktop/unix/classes/sun/awt/UNIXToolkit.java
+++ src/java.desktop/unix/classes/sun/awt/UNIXToolkit.java
@@ -25,6 +25,7 @@
 package sun.awt;
 
 import java.awt.RenderingHints;
+import java.awt.Toolkit;
 
 import static java.awt.RenderingHints.KEY_TEXT_ANTIALIASING;
 import static java.awt.RenderingHints.VALUE_TEXT_ANTIALIAS_DEFAULT;
@@ -55,11 +56,13 @@ import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.Arrays;
 
+import jdk.internal.misc.InnocuousThread;
 import sun.awt.X11.XBaseWindow;
 import sun.security.action.GetIntegerAction;
 import com.sun.java.swing.plaf.gtk.GTKConstants.TextDirection;
 import sun.java2d.opengl.OGLRenderQueue;
 import sun.security.action.GetPropertyAction;
+import sun.util.logging.PlatformLogger;
 
 public abstract class UNIXToolkit extends SunToolkit
 {
@@ -112,6 +115,18 @@ public abstract class UNIXToolkit extends SunToolkit
     private Boolean nativeGTKAvailable;
     private Boolean nativeGTKLoaded;
     private BufferedImage tmpImage = null;
+    private static final PlatformLogger log = PlatformLogger.getLogger("sun.awt.UNIXToolkit");
+
+    private static void printError(String str) {
+        log.fine(str);
+    }
+
+    private static native void toolkitInit();
+
+    protected UNIXToolkit() {
+        toolkitInit();
+        initSystemPropertyWatcher();
+    }
 
     public static int getDatatransferTimeout() {
         @SuppressWarnings("removal")
@@ -548,6 +563,8 @@ public abstract class UNIXToolkit extends SunToolkit
         return result;
     }
 
+    private static native int isSystemDarkColorScheme();
+
     @Override
     public boolean isRunningOnWayland() {
         return isOnWayland();
@@ -565,6 +582,35 @@ public abstract class UNIXToolkit extends SunToolkit
     // (e.g. the window's own title or the area in the side dock without
     // application icons).
     private static final WindowFocusListener waylandWindowFocusListener;
+
+    private static final String OS_THEME_IS_DARK = "awt.os.theme.isDark";
+
+    private static Thread systemPropertyWatcher = null;
+
+    private void initSystemPropertyWatcher() {
+        int initialSystemDarkColorScheme = isSystemDarkColorScheme();
+
+        if (initialSystemDarkColorScheme >= 0) {
+            setDesktopProperty(OS_THEME_IS_DARK, initialSystemDarkColorScheme != 0);
+
+            systemPropertyWatcher = InnocuousThread.newThread("SystemPropertyWatcher",
+                    () -> {
+                        while (true) {
+                            try {
+                                int isSystemDarkColorScheme = isSystemDarkColorScheme();
+                                if (isSystemDarkColorScheme >= 0) {
+                                    setDesktopProperty(OS_THEME_IS_DARK, isSystemDarkColorScheme != 0);
+                                }
+
+                                Thread.sleep(1000);
+                            } catch (Exception ignored) {
+                            }
+                        }
+                    });
+            systemPropertyWatcher.setDaemon(true);
+            systemPropertyWatcher.start();
+        }
+    }
 
     static {
         if (isOnWayland()) {
