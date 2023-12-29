823b65adc0d57538a3338a83f4e1c99f08c44b75
Andrew Lu <andrewlu@openjdk.org>
2023-12-29 02:22:14 +0000

    8271224: runtime/EnclosingMethodAttr/EnclMethodAttr.java doesn't check exit code
    Backport-of: 68dd8280886ede7f5cd8d34811ad0f9ffac440f3

--- test/hotspot/jtreg/runtime/EnclosingMethodAttr/EnclMethodAttr.java
+++ test/hotspot/jtreg/runtime/EnclosingMethodAttr/EnclMethodAttr.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -44,6 +44,7 @@ public class EnclMethodAttr {
         ProcessBuilder pb = ProcessTools.createJavaProcessBuilder(
             "-jar", testsrc + File.separator + "enclMethodAttr.jar");
         OutputAnalyzer output = new OutputAnalyzer(pb.start());
+        output.shouldNotHaveExitValue(0);
         output.shouldContain("java.lang.ClassFormatError: Wrong EnclosingMethod");
     }
 }
