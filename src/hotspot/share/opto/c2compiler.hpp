51c458db6f475a9b0cfa47506134d37a03ca25fc
Vladimir Dvorak <vladimir.dvorak@jetbrains.com>
2024-01-21 09:14:53 +0100

    JBR-6419 - support for enhanced redefinition of java.lang.Object

--- src/hotspot/share/opto/c2compiler.hpp
+++ src/hotspot/share/opto/c2compiler.hpp
@@ -36,6 +36,7 @@ enum {
 class C2Compiler : public AbstractCompiler {
  private:
   static bool init_c2_runtime();
+  static bool _reinitialize_vm_klasses;
 
 public:
   C2Compiler() : AbstractCompiler(compiler_c2) {}
@@ -58,6 +59,11 @@ public:
   static const char* retry_no_reduce_allocation_merges();
   static const char* retry_no_locks_coarsening();
   static const char* retry_no_superword();
+
+  // (DCEVM)
+  static bool is_reinitialize_vm_klasses() { return _reinitialize_vm_klasses; }
+  static void set_reinitialize_vm_klasses() { _reinitialize_vm_klasses = true; }
+  static void clear_reinitialize_vm_klasses() { _reinitialize_vm_klasses = false; }
 
   // Print compilation timers and statistics
   void print_timers();
