79c31bf4de13ae94c01801a0026d53787ebe3a8f
Vladimir Dvorak <vladimir.dvorak@jetbrains.com>
2024-01-21 12:14:53 +0400

    JBR-6419 - support for enhanced redefinition of java.lang.Object

--- src/hotspot/share/ci/ciEnv.cpp
+++ src/hotspot/share/ci/ciEnv.cpp
@@ -255,7 +255,7 @@ ciEnv::ciEnv(Arena* arena) : _ciEnv_arena(mtCompiler, Arena::Tag::tag_cienv) {
   _break_at_compile = false;
   _compiler_data = nullptr;
 #ifndef PRODUCT
-  assert(firstEnv, "must be first");
+  assert(AllowEnhancedClassRedefinition || firstEnv, "must be first");
   firstEnv = false;
 #endif /* !PRODUCT */
 
