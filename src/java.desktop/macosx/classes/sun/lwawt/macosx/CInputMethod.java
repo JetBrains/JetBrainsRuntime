19de0f040cb4e8758b4d06ac8ffcf68d414be17d
Nikita Provotorov <nikita.provotorov@jetbrains.com>
2024-01-30 10:11:09 +0100

    JBR-3697: Letter-based command mode actions are not triggered when using Chinese input method.
    Fixes different platform-specific issues with disabling/enabling input methods support via java.awt.Component#enableInputMethods(boolean):
    * Windows: disabling used to require to deactivate and then activate back the app window to be performed completely ;
    * macOS: disabling used to leave the input method window visible (however, it wasn't affecting the input) ;
    * Linux: with fcitx5 IMF (may not be reproduced with iBus) disabling and then enabling back the input method support used to reset the used input source (keyboard layout) to a default one. It's done via disabling the current XIM when the input method support is disabled instead of disposing it .
    
    (cherry picked from commit 2933ea89f038c417540eb29fea7fd852444b8a21)
    
    The patch also contains the fix for "JBR-6711: java/awt/LightweightDispatcher/LWDispatcherMemoryLeakTest.java: JButton JPanel not collected."
    
    It unsets its references to tracked components thus not preventing GC from collecting them.
    
    (cherry picked from commit e9aab98a6c061881ffcf647bdc0c0fe931442964)
    (cherry picked from commit 13284aa5084b1f236033a717b34f20c152bf1435)

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CInputMethod.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CInputMethod.java
@@ -238,6 +238,8 @@ public class CInputMethod extends InputMethodAdapter {
      */
     public void activate() {
         isActive = true;
+
+        enableListening(true);
     }
 
     public void deactivate(boolean isTemporary) {
@@ -270,6 +272,7 @@ public class CInputMethod extends InputMethodAdapter {
         if (fAwtFocussedComponentPeer != null) {
             long modelPtr = getNativeViewPtr(fAwtFocussedComponentPeer);
             nativeEndComposition(modelPtr, fAwtFocussedComponent);
+            nativeEnableListening(modelPtr, false);
             nativeNotifyPeer(modelPtr, null);
         }
 
@@ -310,6 +313,11 @@ public class CInputMethod extends InputMethodAdapter {
 
             nativeNotifyPeer(modelPtr, imInstance);
         }
+    }
+
+    @Override
+    protected void stopListening() {
+        enableListening(false);
     }
 
     /**
@@ -415,6 +423,16 @@ public class CInputMethod extends InputMethodAdapter {
             return (LWComponentPeer)peer;
 
         return null;
+    }
+
+    private void enableListening(boolean enable) {
+        if (fAwtFocussedComponentPeer != null) {
+            final long modelPtr = getNativeViewPtr(fAwtFocussedComponentPeer);
+
+            if (modelPtr != 0) {
+                nativeEnableListening(modelPtr, enable);
+            }
+        }
     }
 
     // =========================== NSTextInput callbacks ===========================
@@ -812,6 +830,13 @@ public class CInputMethod extends InputMethodAdapter {
     private native void nativeNotifyPeer(long nativePeer, CInputMethod imInstance);
     private native void nativeEndComposition(long nativePeer, Component component);
     private native void nativeHandleEvent(LWComponentPeer<?, ?> peer, AWTEvent event);
+
+    /*
+     * Passing false to the second parameter disables any interaction with
+     *   the AppKit text input management subsystem (i.e. input methods, dead keys, maybe smth else)
+     * Passing true there enables it back
+     */
+    private native void nativeEnableListening(long nativePeerTarget, boolean enable);
 
     // Returns the locale of the active input method.
     static native Locale getNativeLocale();
