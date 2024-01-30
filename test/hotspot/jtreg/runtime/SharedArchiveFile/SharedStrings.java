c97f19a4bf31d1768f0d1855c7cb4f78b70a8b34
Amos Shi <ashi@openjdk.org>
2024-01-30 12:57:11 +0000

    8272552: mark hotspot runtime/cds tests which ignore external VM flags
    Reviewed-by: mdoerr
    Backport-of: 989f39f8106a22498053a4ca5f2becf8df5f2420

--- test/hotspot/jtreg/runtime/SharedArchiveFile/SharedStrings.java
+++ test/hotspot/jtreg/runtime/SharedArchiveFile/SharedStrings.java
@@ -26,6 +26,7 @@
  * @summary Check to make sure that shared strings in the bootstrap CDS archive
  *          are actually shared
  * @requires vm.cds.archived.java.heap
+ * @requires vm.flagless
  * @library /test/lib
  * @modules java.base/jdk.internal.misc
  *          java.management
