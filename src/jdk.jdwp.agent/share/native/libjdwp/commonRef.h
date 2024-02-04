c26bd3fca101d320728439f62867d99d142f86b1
Vladimir Dvorak <lada.dvorak7@gmail.com>
2024-02-04 12:48:58 +0100

    JBR-6648 - Fix ObjectCollectedException in enhanced redefineClasses due to stale jvmtiTagMap entries

--- src/jdk.jdwp.agent/share/native/libjdwp/commonRef.h
+++ src/jdk.jdwp.agent/share/native/libjdwp/commonRef.h
@@ -43,6 +43,7 @@ void commonRef_compact(void);
 void commonRef_lock(void);
 void commonRef_unlock(void);
 
+jvmtiError commonRef_deleteTags(JNIEnv *env, jlong id);
 jvmtiError commonRef_updateTags(JNIEnv *env, jlong id);
 
 #endif
