9ac142653c187110b5aee7d1a7d6ad93e2b3223f
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2024-02-23 18:28:48 +0700

    JBR-6639 add more packages required for jdk tests & deploy jbr for i386

--- jb/project/docker/mkdocker_xvfb.sh
+++ jb/project/docker/mkdocker_xvfb.sh
@@ -41,8 +41,30 @@ function build {
   return 0
 }
 
+
+function downloadJBR {
+  JBR_REMOTE_FILE=jbr-11_0_16-linux-x86-b2043.64.tar.gz
+  JBR_SHA=d3e4d488bb8e0edf4fa0832a4ff5b5dcac69e646640ae06afb1b49e419536762
+  JBR_LOCAL_FILE=$JBR_REMOTE_FILE
+
+  if [ ! -f $JBR_LOCAL_FILE ]; then
+      # Obtain "JBR" from outside of the container.
+      curl -o $JBR_LOCAL_FILE https://cache-redirector.jetbrains.com/intellij-jbr/${JBR_REMOTE_FILE} -O
+  else
+      echo "Runtime \"$JBR_LOCAL_FILE\" present, skipping download"
+  fi
+
+  # Verify that what we've downloaded can be trusted.
+#  sha256sum -c - <<EOF
+#  $JBR_SHA *$JBR_LOCAL_FILE
+#  EOF
+
+  return 0
+}
+
 build $arch "ubuntu" "20.04" ${arch_from}
 if [ "$arch" == "amd64" ]; then
+  downloadJBR
   build "i386" "ubuntu" "20.04" $arch
 fi
 build $arch "ubuntu" "22.04" ${arch_from}
