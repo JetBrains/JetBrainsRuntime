3b2f4485f670837be1e4b06e2b78bd81f5c0f45f
Vladimir Dvorak <vladimir.dvorak@jetbrains.com>
2024-02-23 21:12:22 +0100

    JBR-6746 Fix linux build failing on: "error: 'this' pointer is null"

--- src/hotspot/share/prims/jvmtiEnhancedRedefineClasses.cpp
+++ src/hotspot/share/prims/jvmtiEnhancedRedefineClasses.cpp
@@ -1533,8 +1533,8 @@ void VM_EnhancedRedefineClasses::calculate_instance_update_information(Klass* ne
         const InjectedField* const injected = JavaClasses::get_injected(old_klass->name(), &num_injected);
         for (int i = java_fields_count; i < java_fields_count+num_injected; i++) {
           FieldInfo *old_field = old_klass->field(i);
-          if (old_field->is_internal() && old_field->name(NULL) == internal_field->name(NULL)) {
-            copy(old_field->offset(), type2aelembytes(Signature::basic_type(internal_field->signature(NULL))));
+          if (old_field->is_internal() && old_field->name_internal_dcevm() == internal_field->name_internal_dcevm()) {
+            copy(old_field->offset(), type2aelembytes(Signature::basic_type(internal_field->signature_internal_dcevm())));
             if (old_field->offset() < internal_field->offset()) {
               _copy_backwards = true;
             }
