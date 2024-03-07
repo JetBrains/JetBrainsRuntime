ff8b4ea08205da39670e0d9019cb64f4199985ef
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2024-03-07 13:57:25 +0400

    JBR-6814 Wayland: support sun.java2d.uiScale property

--- src/java.desktop/unix/classes/sun/awt/wl/WLGraphicsEnvironment.java
+++ src/java.desktop/unix/classes/sun/awt/wl/WLGraphicsEnvironment.java
@@ -49,6 +49,7 @@ public class WLGraphicsEnvironment extends SunGraphicsEnvironment {
     private static boolean verboseVulkanStatus = false;
     private static boolean vulkanRequested = false;
     private static int vulkanRequestedDeviceNumber = -1;
+    private static final boolean debugScaleEnabled;
     @SuppressWarnings("removal")
     private static String vulkanOption =
             AccessController.doPrivileged(
@@ -79,6 +80,8 @@ public class WLGraphicsEnvironment extends SunGraphicsEnvironment {
         if (log.isLoggable(Level.FINE)) {
             log.fine("Vulkan rendering enabled: " + (vulkanEnabled?"YES":"NO"));
         }
+
+        debugScaleEnabled = SunGraphicsEnvironment.isUIScaleEnabled() && SunGraphicsEnvironment.getDebugScale() >= 1;
 
         // Make sure the toolkit is loaded because otherwise this GE is going to be empty
         WLToolkit.isInitialized();
@@ -143,6 +146,7 @@ public class WLGraphicsEnvironment extends SunGraphicsEnvironment {
                 if (gd.getID() == wlID) {
                     newOutput = false;
                     if (gd.isSameDeviceAs(wlID, x, y)) {
+                        // These coordinates and the size are not scaled.
                         gd.updateConfiguration(humanID, width, height, scale);
                     } else {
                         final WLGraphicsDevice updatedDevice = WLGraphicsDevice.createWithConfiguration(wlID, humanID,
@@ -249,5 +253,13 @@ public class WLGraphicsEnvironment extends SunGraphicsEnvironment {
                 totalDisplayBounds.setSize(virtualBounds.getSize());
             }
         }
+    }
+
+    static double effectiveScaleFrom(int displayScale) {
+        return debugScaleEnabled ? SunGraphicsEnvironment.getDebugScale() : displayScale;
+    }
+
+    static boolean isDebugScaleEnabled() {
+        return debugScaleEnabled;
     }
 }
