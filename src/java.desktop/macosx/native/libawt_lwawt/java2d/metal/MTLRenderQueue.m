c50963e6cef32972ff70f2618d5cc91973c3d636
Nikita Gubarkov <Nikita.Gubarkov@jetbrains.com>
2024-03-11 14:52:19 +0100

    JBR-6723 flush vertex cache and command buffer before freeing glyph cache. (#334)
    (cherry picked from commit 1faccf39951f6ac2a2db6ad11b80d0b5012538b4)

--- src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLRenderQueue.m
+++ src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLRenderQueue.m
@@ -628,6 +628,7 @@ Java_sun_java2d_metal_MTLRenderQueue_flushBuffer
                         CONTINUE_IF_NULL(mtlc);
                         [mtlc.glyphCacheAA free];
                         [mtlc.glyphCacheLCD free];
+                        [mtlc commitCommandBuffer:YES display:NO];
                         MTLSD_Delete(env, mtlsdo);
                     }
                     break;
@@ -651,9 +652,9 @@ Java_sun_java2d_metal_MTLRenderQueue_flushBuffer
                     CHECK_PREVIOUS_OP(MTL_OP_OTHER);
                     jlong pConfigInfo = NEXT_LONG(b);
                     CONTINUE_IF_NULL(mtlc);
-                    [mtlc commitCommandBuffer:YES display:NO];
                     [mtlc.glyphCacheAA free];
                     [mtlc.glyphCacheLCD free];
+                    [mtlc commitCommandBuffer:YES display:NO];
                     MTLGC_DestroyMTLGraphicsConfig(pConfigInfo);
                     mtlc = NULL;
                     break;
