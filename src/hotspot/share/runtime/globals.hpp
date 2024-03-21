078c542fa6aa94b5040e60c6795c6dae031cbdf3
Aleksey Shipilev <shade@openjdk.org>
2024-03-21 09:49:41 +0000

    8319048: Monitor deflation unlink phase prolongs time to safepoint
    Backport-of: efc392259c64986bbbe880259e95b09058b9076a

--- src/hotspot/share/runtime/globals.hpp
+++ src/hotspot/share/runtime/globals.hpp
@@ -732,6 +732,10 @@ const int ObjectAlignmentInBytes = 8;
           "at one time (minimum is 1024).")                      \
           range(1024, max_jint)                                             \
                                                                             \
+  product(intx, MonitorUnlinkBatch, 500, DIAGNOSTIC,                        \
+          "The maximum number of monitors to unlink in one batch. ")        \
+          range(1, max_jint)                                                \
+                                                                            \
   product(intx, MonitorUsedDeflationThreshold, 90, DIAGNOSTIC,              \
           "Percentage of used monitors before triggering deflation (0 is "  \
           "off). The check is performed on GuaranteedSafepointInterval, "   \
