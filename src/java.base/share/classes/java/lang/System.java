b7f78a39c2c11e351a927da97455efa112b70d1c
Ben Taylor <btaylor@openjdk.org>
2024-03-25 23:27:02 +0000

    8319436: Proxy.newProxyInstance throws NPE if loader is null and interface not visible from class loader
    Backport-of: 8eb6f617b3f8e2a990e4244af4e56287f9741b20

--- src/java.base/share/classes/java/lang/System.java
+++ src/java.base/share/classes/java/lang/System.java
@@ -2227,7 +2227,7 @@ public final class System {
             }
 
             public String getLoaderNameID(ClassLoader loader) {
-                return loader.nameAndId();
+                return loader != null ? loader.nameAndId() : "null";
             }
         });
     }
