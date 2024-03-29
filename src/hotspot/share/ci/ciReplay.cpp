4ced080f1e5acc1fe4f0117833038cd056864bf9
Goetz Lindenmaier <goetz@openjdk.org>
2024-03-29 21:02:45 +0000

    8276227: ciReplay: SIGSEGV if classfile for replay compilation is not present after JDK-8275868
    Reviewed-by: mdoerr
    Backport-of: a1f4c428ba1b78a4e18afb87c94a5c731a5aa706

--- src/hotspot/share/ci/ciReplay.cpp
+++ src/hotspot/share/ci/ciReplay.cpp
@@ -103,6 +103,7 @@ class CompileReplay : public StackObj {
   FILE*   _stream;
   Thread* _thread;
   Handle  _protection_domain;
+  bool    _protection_domain_initialized;
   Handle  _loader;
 
   GrowableArray<ciMethodRecord*>     _ci_method_records;
@@ -130,6 +131,7 @@ class CompileReplay : public StackObj {
     _thread = THREAD;
     _loader = Handle(_thread, SystemDictionary::java_system_loader());
     _protection_domain = Handle();
+    _protection_domain_initialized = false;
 
     _stream = fopen(filename, "rt");
     if (_stream == NULL) {
@@ -685,12 +687,18 @@ class CompileReplay : public StackObj {
   void process_instanceKlass(TRAPS) {
     // just load the referenced class
     Klass* k = parse_klass(CHECK);
-    if (_protection_domain() == NULL) {
+    if (!_protection_domain_initialized && k != NULL) {
+      assert(_protection_domain() == NULL, "must be uninitialized");
       // The first entry is the holder class of the method for which a replay compilation is requested.
       // Use the same protection domain to load all subsequent classes in order to resolve all classes
       // in signatures of inlinees. This ensures that inlining can be done as stated in the replay file.
       _protection_domain = Handle(_thread, k->protection_domain());
     }
+
+    // Only initialize the protection domain handle with the protection domain of the very first entry.
+    // This also ensures that older replay files work.
+    _protection_domain_initialized = true;
+
   }
 
   // ciInstanceKlass <name> <is_linked> <is_initialized> <length> tag*
