97cb9a74b674a4e8f35408139dfad6d3499317e1
Andrew Lu <andrewlu@openjdk.org>
2024-04-02 08:37:43 +0400

    8305942: Open source several AWT Focus related tests
    Backport-of: 8346ae2bc1152f13bc77c643252d84e2043ffe0b

--- test/jdk/java/awt/Focus/RowToleranceTransitivityTest.java
+++ test/jdk/java/awt/Focus/RowToleranceTransitivityTest.java
@@ -130,12 +130,14 @@ public class RowToleranceTransitivityTest {
     static void test() throws Exception {
         robot.delay(500);
 
-        clickOn(ft);
-
-        robot.delay(500);
-
-        if (KeyboardFocusManager.getCurrentKeyboardFocusManager().getFocusOwner() != ft) {
-            throw new RuntimeException("Cannot set initial focus");
+        // Set focus on the first component to start traversal
+        if (!setFocusOn(ft, new Runnable() {
+            public void run() {
+                clickOn(ft);
+            }
+            })) {
+            System.out.println("Couldn't set focus on " + ft);
+            throw new RuntimeException("Test couldn't be performed.");
         }
 
         robot.delay(500);
