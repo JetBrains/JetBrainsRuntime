8fc7dee31c2094e29e59b8ac764e0465ae0721cf
Goetz Lindenmaier <goetz@openjdk.org>
2024-04-05 10:43:11 +0000

    8295026: Remove unused fields in StyleSheet
    Backport-of: be3b3354523917c024fb9d7d99de7c7eec3d175a

--- src/java.desktop/share/classes/javax/swing/text/html/StyleSheet.java
+++ src/java.desktop/share/classes/javax/swing/text/html/StyleSheet.java
@@ -1821,8 +1821,6 @@ public class StyleSheet extends StyleContext {
     }
 
 
-    static final Border noBorder = new EmptyBorder(0,0,0,0);
-
     /**
      * Class to carry out some of the duties of
      * CSS formatting.  Implementations of this
@@ -2526,7 +2524,6 @@ public class StyleSheet extends StyleContext {
         private boolean checkedForStart;
         private int start;
         private CSS.Value type;
-        URL imageurl;
         private StyleSheet ss = null;
         Icon img = null;
         private int bulletgap = 5;
@@ -3229,8 +3226,6 @@ public class StyleSheet extends StyleContext {
 
 
     // ---- Variables ---------------------------------------------
-
-    static final int DEFAULT_FONT_SIZE = 3;
 
     private transient Object fontSizeInherit;
 
