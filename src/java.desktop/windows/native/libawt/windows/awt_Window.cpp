dce71ea3f744ef42c3fea3b8347d8965b272adf8
Nikita Gubarkov <Nikita.Gubarkov@jetbrains.com>
2024-04-09 13:27:31 +0200

    Fix Windows AWT compilation errors
    Frame, Window, FileDialog, CustomTitleBar
    
    (cherry picked from commit 2ada4a575b18d8b357f06fbb9c64d1192bd0cb2d)

--- src/java.desktop/windows/native/libawt/windows/awt_Window.cpp
+++ src/java.desktop/windows/native/libawt/windows/awt_Window.cpp
@@ -2861,8 +2861,9 @@ void AwtWindow::_SetRoundedCorners(void *param) {
     jobject self = rcs->window;
 
     PDATA pData;
+    AwtWindow *window;
     JNI_CHECK_PEER_GOTO(self, ret);
-    AwtWindow *window = (AwtWindow *)pData;
+    window = (AwtWindow *)pData;
 
     DwmSetWindowAttribute(window->GetHWnd(), DWMWA_WINDOW_CORNER_PREFERENCE, &rcs->type, sizeof(DWM_WINDOW_CORNER_PREFERENCE));
 
