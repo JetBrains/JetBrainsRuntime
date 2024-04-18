ibmruntimes/openj9-openjdk-jdk21

8319103: Popups that request focus are not shown on Linux with Wayland
Backport-of: f3ed27582e16c3a323f590863cbeec6d35e20b58

src/java.desktop/unix/classes/sun/awt/UNIXToolkit.java
@@ -502,6 +502,21 @@ public abstract class UNIXToolkit extends SunToolkit
                 @Override
                 public void windowLostFocus(WindowEvent e) {
                     Window window = e.getWindow();
+                    Window oppositeWindow = e.getOppositeWindow();
+
+                    // The focus can move between the window calling the popup,
+                    // and the popup window itself.
+                    // We only dismiss the popup in other cases.
+                    if (oppositeWindow != null) {
+                        if (window == oppositeWindow.getParent() ) {
+                            addWaylandWindowFocusListenerToWindow(oppositeWindow);
+                            return;
+                        }
+                        if (window.getParent() == oppositeWindow) {
+                            return;
+                        }
+                    }
+
                     window.removeWindowFocusListener(this);
 
                     // AWT
@@ -516,18 +531,22 @@ public abstract class UNIXToolkit extends SunToolkit
         }
     }
 
+    private static void addWaylandWindowFocusListenerToWindow(Window window) {
+        if (!Arrays
+                .asList(window.getWindowFocusListeners())
+                .contains(waylandWindowFocusListener)
+        ) {
+            window.addWindowFocusListener(waylandWindowFocusListener);
+        }
+    }
+
     @Override
     public void dismissPopupOnFocusLostIfNeeded(Window invoker) {
-        if (!isOnWayland()
-                || invoker == null
-                || Arrays
-                    .asList(invoker.getWindowFocusListeners())
-                    .contains(waylandWindowFocusListener)
-        ) {
+        if (!isOnWayland() || invoker == null) {
             return;
         }
 
-        invoker.addWindowFocusListener(waylandWindowFocusListener);
+        addWaylandWindowFocusListenerToWindow(invoker);
     }
 
     @Override
@@ -537,5 +556,8 @@ public abstract class UNIXToolkit extends SunToolkit
         }
 
         invoker.removeWindowFocusListener(waylandWindowFocusListener);
+        for (Window ownedWindow : invoker.getOwnedWindows()) {
+            ownedWindow.removeWindowFocusListener(waylandWindowFocusListener);
+        }
     }
 }

JetBrains/JetBrainsRuntime

src/java.desktop/unix/classes/sun/awt/UNIXToolkit.java
@@ -548,6 +548,21 @@ public abstract class UNIXToolkit extends SunToolkit
                 @Override
                 public void windowLostFocus(WindowEvent e) {
                     Window window = e.getWindow();
+                    Window oppositeWindow = e.getOppositeWindow();
+
+                    // The focus can move between the window calling the popup,
+                    // and the popup window itself.
+                    // We only dismiss the popup in other cases.
+                    if (oppositeWindow != null) {
+                        if (window == oppositeWindow.getParent() ) {
+                            addWaylandWindowFocusListenerToWindow(oppositeWindow);
+                            return;
+                        }
+                        if (window.getParent() == oppositeWindow) {
+                            return;
+                        }
+                    }
+
                     window.removeWindowFocusListener(this);
 
                     // AWT
@@ -562,18 +577,22 @@ public abstract class UNIXToolkit extends SunToolkit
         }
     }
 
+    private static void addWaylandWindowFocusListenerToWindow(Window window) {
+        if (!Arrays
+                .asList(window.getWindowFocusListeners())
+                .contains(waylandWindowFocusListener)
+        ) {
+            window.addWindowFocusListener(waylandWindowFocusListener);
+        }
+    }
+
     @Override
     public void dismissPopupOnFocusLostIfNeeded(Window invoker) {
-        if (!isRunningOnXWayland()
-                || invoker == null
-                || Arrays
-                    .asList(invoker.getWindowFocusListeners())
-                    .contains(waylandWindowFocusListener)
-        ) {
+        if (!isRunningOnXWayland() || invoker == null) {
             return;
         }
 
-        invoker.addWindowFocusListener(waylandWindowFocusListener);
+        addWaylandWindowFocusListenerToWindow(invoker);
     }
 
     @Override
@@ -583,5 +602,8 @@ public abstract class UNIXToolkit extends SunToolkit
         }
 
         invoker.removeWindowFocusListener(waylandWindowFocusListener);
+        for (Window ownedWindow : invoker.getOwnedWindows()) {
+            ownedWindow.removeWindowFocusListener(waylandWindowFocusListener);
+        }
     }
 }