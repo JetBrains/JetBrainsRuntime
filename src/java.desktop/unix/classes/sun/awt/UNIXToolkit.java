d9821157ff221ad9ccb26ac47e039644bf32b694
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2024-04-20 15:37:38 +0700

    Revert "JBR-6372: implement detecting of OS theme on linux"
    This reverts commit 51d67613bbef8f3dac2faeb728835873c78393dd.

--- src/java.desktop/unix/classes/sun/awt/UNIXToolkit.java
+++ src/java.desktop/unix/classes/sun/awt/UNIXToolkit.java
@@ -25,7 +25,6 @@
 package sun.awt;
 
 import java.awt.RenderingHints;
-import java.awt.Toolkit;
 
 import static java.awt.RenderingHints.KEY_TEXT_ANTIALIASING;
 import static java.awt.RenderingHints.VALUE_TEXT_ANTIALIAS_DEFAULT;
@@ -56,13 +55,11 @@ import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.Arrays;
 
-import jdk.internal.misc.InnocuousThread;
 import sun.awt.X11.XBaseWindow;
 import sun.security.action.GetIntegerAction;
 import com.sun.java.swing.plaf.gtk.GTKConstants.TextDirection;
 import sun.java2d.opengl.OGLRenderQueue;
 import sun.security.action.GetPropertyAction;
-import sun.util.logging.PlatformLogger;
 
 public abstract class UNIXToolkit extends SunToolkit
 {
@@ -115,18 +112,6 @@ public abstract class UNIXToolkit extends SunToolkit
     private Boolean nativeGTKAvailable;
     private Boolean nativeGTKLoaded;
     private BufferedImage tmpImage = null;
-    private static final PlatformLogger log = PlatformLogger.getLogger("sun.awt.UNIXToolkit");
-
-    private static void printError(String str) {
-        log.fine(str);
-    }
-
-    private static native void toolkitInit();
-
-    protected UNIXToolkit() {
-        toolkitInit();
-        initSystemPropertyWatcher();
-    }
 
     public static int getDatatransferTimeout() {
         @SuppressWarnings("removal")
@@ -563,8 +548,6 @@ public abstract class UNIXToolkit extends SunToolkit
         return result;
     }
 
-    private static native int isSystemDarkColorScheme();
-
     @Override
     public boolean isRunningOnXWayland() {
         return isOnXWayland();
@@ -582,35 +565,6 @@ public abstract class UNIXToolkit extends SunToolkit
     // (e.g. the window's own title or the area in the side dock without
     // application icons).
     private static final WindowFocusListener waylandWindowFocusListener;
-
-    private static final String OS_THEME_IS_DARK = "awt.os.theme.isDark";
-
-    private static Thread systemPropertyWatcher = null;
-
-    private void initSystemPropertyWatcher() {
-        int initialSystemDarkColorScheme = isSystemDarkColorScheme();
-
-        if (initialSystemDarkColorScheme >= 0) {
-            setDesktopProperty(OS_THEME_IS_DARK, initialSystemDarkColorScheme != 0);
-
-            systemPropertyWatcher = InnocuousThread.newThread("SystemPropertyWatcher",
-                    () -> {
-                        while (true) {
-                            try {
-                                int isSystemDarkColorScheme = isSystemDarkColorScheme();
-                                if (isSystemDarkColorScheme >= 0) {
-                                    setDesktopProperty(OS_THEME_IS_DARK, isSystemDarkColorScheme != 0);
-                                }
-
-                                Thread.sleep(1000);
-                            } catch (Exception ignored) {
-                            }
-                        }
-                    });
-            systemPropertyWatcher.setDaemon(true);
-            systemPropertyWatcher.start();
-        }
-    }
 
     static {
         if (isOnXWayland()) {
