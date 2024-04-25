33241333bd16f6eeaa75257ec69cc8dc97e9e163
Amos Shi <ashi@openjdk.org>
2024-04-25 15:11:49 +0000

    8318854: [macos14] Running any AWT app prints Secure coding warning
    Backport-of: 940f67c1a62c6f9462266f3a108649aca114cffa

--- src/java.desktop/macosx/native/libawt_lwawt/awt/AWTWindow.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/AWTWindow.m
@@ -841,7 +841,7 @@ AWT_ASSERT_APPKIT_THREAD;
         isDisabled = !awtWindow.isEnabled;
     }
 
-    if (menuBar == nil) {
+    if (menuBar == nil && [ApplicationDelegate sharedDelegate] != nil) {
         menuBar = [[ApplicationDelegate sharedDelegate] defaultMenuBar];
         isDisabled = NO;
     }
@@ -1228,7 +1228,7 @@ JNI_COCOA_ENTER(env);
         window.javaMenuBar = menuBar;
 
         CMenuBar* actualMenuBar = menuBar;
-        if (actualMenuBar == nil) {
+        if (actualMenuBar == nil && [ApplicationDelegate sharedDelegate] != nil) {
             actualMenuBar = [[ApplicationDelegate sharedDelegate] defaultMenuBar];
         }
 
