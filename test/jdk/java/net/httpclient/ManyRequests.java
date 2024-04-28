71630ebd1bc9a1dc8c5f3b811edf3b5ded5678c1
Amos Shi <ashi@openjdk.org>
2024-04-28 21:50:20 +0000

    8327989: java/net/httpclient/ManyRequest.java should not use "localhost" in URIs
    Backport-of: da4dd7c008da4be54ee5f58d1ac19f440fd2a74e

--- test/jdk/java/net/httpclient/ManyRequests.java
+++ test/jdk/java/net/httpclient/ManyRequests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -176,7 +176,7 @@ public class ManyRequests {
 
         URI baseURI = URIBuilder.newBuilder()
                 .scheme("https")
-                .host(InetAddress.getLoopbackAddress().getHostName())
+                .loopback()
                 .port(port)
                 .path("/foo/x").build();
         server.createContext("/foo", new TestEchoHandler());
