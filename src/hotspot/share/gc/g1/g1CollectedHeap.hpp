3bb8eeed4817d490a1089aa567434af3f303c4f8
Goetz Lindenmaier <goetz@openjdk.org>
2024-05-03 06:22:47 +0000

    8317809: Insertion of free code blobs into code cache can be very slow during class unloading
    Reviewed-by: phh, adinn
    Backport-of: 30817b742300f10f566e6aee3a8c1f8af4ab3083

--- src/hotspot/share/gc/g1/g1CollectedHeap.hpp
+++ src/hotspot/share/gc/g1/g1CollectedHeap.hpp
@@ -1265,6 +1265,8 @@ public:
   // Performs cleaning of data structures after class unloading.
   void complete_cleaning(bool class_unloading_occurred);
 
+  void unload_classes_and_code(const char* description, BoolObjectClosure* cl, GCTimer* timer);
+
   // Verification
 
   // Perform any cleanup actions necessary before allowing a verification.
