df5119653b4a893a05be73d5ee3703c5e3c755c0
Dmitrii Morskii <dmitrii.morskii@jetbrains.com>
2024-05-06 20:14:44 +0200

    Revert "JBR-6372: implement detecting of OS theme on linux"
    This reverts commit a657e4e2cbce139f2c5b53c7fb5f30d81f99e311.
    
    (cherry picked from commit c626c9f2204d6249a9cc788c2e176f4f4cc7def3)

--- src/java.desktop/unix/classes/sun/awt/X11/XToolkit.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XToolkit.java
@@ -1880,7 +1880,6 @@ public final class XToolkit extends UNIXToolkit implements Runnable {
                                                      localEnv.displayChanged());
             }
         }
-        super.initializeDesktopProperties();
     }
 
     /**
