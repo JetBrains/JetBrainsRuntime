f45bc929d32c7a54949c947781fcaac2b52d045e
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2024-05-08 12:05:29 +0400

    JBR-7158 Wayland: scale with wp_viewport instead of buffer scale

--- src/java.desktop/unix/native/common/java2d/wl/WLBuffers.c
+++ src/java.desktop/unix/native/common/java2d/wl/WLBuffers.c
@@ -270,9 +270,6 @@ struct WLSurfaceBufferManager {
     /// Does not exceed MAX_BUFFERS_IN_USE elements.
     WLSurfaceBuffer *   buffersInUse;  // only accessed under showLock
 
-    /// The scale of wl_surface (see Wayland docs for more info on that).
-    jint                scale;         // only accessed under showLock
-
     pthread_mutex_t     drawLock;
 
     /**
@@ -760,7 +757,7 @@ SendShowBufferToWayland(WLSurfaceBufferManager * manager)
 
     // wl_buffer_listener will release bufferForShow when Wayland's done with it
     wl_surface_attach(manager->wlSurface, buffer->wlBuffer, 0, 0);
-    wl_surface_set_buffer_scale(manager->wlSurface, manager->scale);
+    //NB: do not specify scale for the buffer; we scale with wp_viewporter
 
     // Better wait for the frame event so as not to overwhelm Wayland with
     // frequent surface updates that it cannot deliver to the screen anyway.
@@ -935,7 +932,6 @@ HaveEnoughMemoryForWindow(jint width, jint height)
 WLSurfaceBufferManager *
 WLSBM_Create(jint width,
              jint height,
-             jint scale,
              jint bgPixel,
              jint wlShmFormat,
              jobject surfaceDataWeakRef,
@@ -955,7 +951,6 @@ WLSBM_Create(jint width,
 
     manager->bufferForDraw.width = width;
     manager->bufferForDraw.height = height;
-    manager->scale = scale;
     manager->bgPixel = bgPixel;
     manager->format = wlShmFormat;
     manager->surfaceDataWeakRef = surfaceDataWeakRef;
@@ -1116,7 +1111,7 @@ WLSB_DataGet(WLDrawBuffer * buffer)
 }
 
 void
-WLSBM_SizeChangeTo(WLSurfaceBufferManager * manager, jint width, jint height, jint scale)
+WLSBM_SizeChangeTo(WLSurfaceBufferManager * manager, jint width, jint height)
 {
     if (!HaveEnoughMemoryForWindow(width, height)) {
         JNU_ThrowOutOfMemoryError(getEnv(), "Wayland surface buffer too large");
@@ -1125,13 +1120,7 @@ WLSBM_SizeChangeTo(WLSurfaceBufferManager * manager, jint width, jint height, ji
 
     MUTEX_LOCK(manager->drawLock);
     MUTEX_LOCK(manager->showLock);
-    const bool change_needed =
-               manager->bufferForDraw.width != width
-            || manager->bufferForDraw.height != height
-            || manager->scale != scale;
-    manager->scale = scale;
-
-    if (change_needed) {
+    if (manager->bufferForDraw.width != width || manager->bufferForDraw.height != height) {
         manager->bufferForDraw.width  = width;
         manager->bufferForDraw.height = height;
         manager->bufferForDraw.resizePending = true;
