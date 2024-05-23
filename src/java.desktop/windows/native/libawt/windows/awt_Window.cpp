b86ff0c215b32aaa7e0d3c888a7a901ebc24c5d7
Dmitrii Morskii <dmitrii.morskii@jetbrains.com>
2024-05-23 02:54:50 +0100

    JBR-7128 Use the correct WmSize event type for JFrame moved to another monitor
    author: Sergei Tachenov

--- src/java.desktop/windows/native/libawt/windows/awt_Window.cpp
+++ src/java.desktop/windows/native/libawt/windows/awt_Window.cpp
@@ -1905,7 +1905,7 @@ void AwtWindow::WmDPIChanged(const LPARAM &lParam) {
     RECT *r = (RECT *) lParam;
     ReshapeNoScale(r->left, r->top, r->right - r->left, r->bottom - r->top);
     CheckIfOnNewScreen(true);
-    WmSize(SIZENORMAL, r->right - r->left, r->bottom - r->top);
+    WmSize(GetCurrentWmSizeType(), r->right - r->left, r->bottom - r->top);
 }
 
 MsgRouting AwtWindow::WmEraseBkgnd(HDC hDC, BOOL& didErase)
@@ -2326,6 +2326,10 @@ BOOL AwtWindow::CheckIfOnNewScreenWithDifferentScale() {
         }
     }
     return FALSE;
+}
+
+UINT AwtWindow::GetCurrentWmSizeType() {
+    return SIZENORMAL;
 }
 
 /*
