10d250de5edaa4a76855864640399f51c962f17a
Dmitrii Morskii <dmitrii.morskii@jetbrains.com>
2024-05-23 02:54:50 +0100

    JBR-7128 Use the correct WmSize event type for JFrame moved to another monitor
    author: Sergei Tachenov
    (cherry picked from commit 01f1c7e15d5bced6536732e3244863b0707c929b)

--- src/java.desktop/windows/native/libawt/windows/awt_Window.h
+++ src/java.desktop/windows/native/libawt/windows/awt_Window.h
@@ -133,6 +133,7 @@ public:
     virtual int  GetScreenImOn();
     virtual BOOL CheckIfOnNewScreen(BOOL force);
     virtual BOOL CheckIfOnNewScreenWithDifferentScale();
+    virtual UINT GetCurrentWmSizeType();
     virtual void Grab();
     virtual void Ungrab();
     virtual void Ungrab(BOOL doPost);
