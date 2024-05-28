3f8e02ea3c63af5409c703f6a49053ccb30dd30e
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2024-05-28 22:25:37 +0200

    JBR-5615 add `sun.java2d.logDisplays` VM option
    It prints to stdout whenever display configuration is changed.

--- src/java.desktop/share/classes/sun/java2d/SunGraphicsEnvironment.java
+++ src/java.desktop/share/classes/sun/java2d/SunGraphicsEnvironment.java
@@ -53,6 +53,7 @@ import sun.font.FontManagerFactory;
 import sun.font.FontManagerForSGE;
 import sun.font.FontUtilities;
 import sun.java2d.pipe.Region;
+import sun.security.action.GetBooleanAction;
 import sun.security.action.GetPropertyAction;
 
 /**
@@ -86,6 +87,21 @@ public abstract class SunGraphicsEnvironment extends GraphicsEnvironment
             System.setProperty("swing.bufferPerWindow", "false"); // todo: until JRE-489 is fixed
         }
         debugScale = uiScaleEnabled ? getScaleFactor("sun.java2d.uiScale") : -1;
+    }
+
+    protected enum LogDisplay {
+        ADDED,
+        REMOVED,
+        CHANGED;
+
+        @SuppressWarnings("removal")
+        public static final boolean ENABLED = AccessController.doPrivileged(new GetBooleanAction("sun.java2d.logDisplays"));
+
+        public void log(int id, Object bounds, double scale) {
+            if (!ENABLED) return;
+            System.out.println("DISPLAY " + this + ": #" + id +
+                    ", " + bounds + ", scale=" + scale);
+        }
     }
 
     protected GraphicsDevice[] screens;
