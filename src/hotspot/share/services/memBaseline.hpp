e3d00a573c8b09e64318a3d9f27cf2a26af4db7b
María Arias de Reyna <ariasdereyna@redhat.com>
2024-05-29 13:55:21 +0000

    8289182: NMT: MemTracker::baseline should return void
    Backport-of: b96ba19807845739b36274efb168dd048db819a3

--- src/hotspot/share/services/memBaseline.hpp
+++ src/hotspot/share/services/memBaseline.hpp
@@ -90,7 +90,7 @@ class MemBaseline {
     _baseline_type(Not_baselined) {
   }
 
-  bool baseline(bool summaryOnly = true);
+  void baseline(bool summaryOnly = true);
 
   BaselineType baseline_type() const { return _baseline_type; }
 
@@ -190,7 +190,7 @@ class MemBaseline {
 
  private:
   // Baseline summary information
-  bool baseline_summary();
+  void baseline_summary();
 
   // Baseline allocation sites (detail tracking only)
   bool baseline_allocation_sites();
