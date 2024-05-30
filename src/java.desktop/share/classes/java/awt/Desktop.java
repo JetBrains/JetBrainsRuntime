4c8396bf9de0f5fbbcbb599e3592912a0735d90b
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2024-05-30 18:59:18 +0200

    JBR-6357 JBR API v3
    JBR API frontend is moved into a separate repository.
    Rewritten proxy generation, bridges removed, invokedynamic is used instead.
    Mapping is now specified using annotations.
    Support for extension methods.
    Support for arrays and generics.
    Added JBR API implementation version.
    
    JBR-7232 Refactor deriveFontWithFeatures & JBRFileDialog JBR API
    
    (cherry picked from commit a4804efa962ce46e0ca471f0dc5245734eacb77a)

--- src/java.desktop/share/classes/java/awt/Desktop.java
+++ src/java.desktop/share/classes/java/awt/Desktop.java
@@ -47,6 +47,7 @@ import java.util.Objects;
 
 import javax.swing.JMenuBar;
 
+import com.jetbrains.exported.JBRApi;
 import sun.awt.SunToolkit;
 import sun.security.util.SecurityConstants;
 
@@ -939,6 +940,7 @@ public class Desktop {
         return peer.moveToTrash(file);
     }
 
+    @JBRApi.Provided("DesktopActions.Handler")
     private interface DesktopActionsHandler {
         void open(File file) throws IOException;
         void edit(File file) throws IOException;
@@ -971,7 +973,8 @@ public class Desktop {
     }
     private static volatile DesktopActions actions;
 
-    static void setDesktopActionsHandler(DesktopActionsHandler h) {
+    @JBRApi.Provides("DesktopActions#setHandler")
+    private static void setDesktopActionsHandler(DesktopActionsHandler h) {
         try {
             actions = new DesktopActions(h);
         } catch (Exception e) {
