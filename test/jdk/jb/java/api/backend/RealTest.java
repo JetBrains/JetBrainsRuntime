bc132a58917d456acf4c659565a92a6740216075
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2024-05-30 20:59:18 +0400

    JBR-6357 JBR API v3
    JBR API frontend is moved into a separate repository.
    Rewritten proxy generation, bridges removed, invokedynamic is used instead.
    Mapping is now specified using annotations.
    Support for extension methods.
    Support for arrays and generics.
    Added JBR API implementation version.
    
    JBR-7232 Refactor deriveFontWithFeatures & JBRFileDialog JBR API
    
    (cherry picked from commit a4804efa962ce46e0ca471f0dc5245734eacb77a)

--- test/jdk/jb/java/api/backend/RealTest.java
+++ test/jdk/jb/java/api/backend/RealTest.java
@@ -24,28 +24,42 @@
 /*
  * @test
  * @modules java.base/com.jetbrains.internal:+open
- * @build com.jetbrains.* com.jetbrains.api.Real com.jetbrains.jbr.Real
- * @run main RealTest
+ * @build com.jetbrains.* com.jetbrains.test.api.Real com.jetbrains.test.jbr.Real
+ * @run main/othervm -Djetbrains.runtime.api.extendRegistry=true RealTest
  */
 
+import com.jetbrains.Extensions;
 import com.jetbrains.internal.JBRApi;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 import static com.jetbrains.Util.*;
-import static com.jetbrains.api.Real.*;
-import static com.jetbrains.jbr.Real.*;
+import static com.jetbrains.test.api.Real.*;
 
 public class RealTest {
 
     public static void main(String[] args) {
-        init()
-                .service(Service.class.getName(), ServiceImpl.class.getName())
-                .twoWayProxy(Api2Way.class.getName(), JBR2Way.class.getName())
-                .twoWayProxy(ApiLazyNumber.class.getName(), JBRLazyNumber.class.getName());
+        init("RealTest", Map.of(Extensions.FOO, new Class[] {Proxy.class}, Extensions.BAR, new Class[] {Proxy.class}));
 
         // Get service
         Service service = Objects.requireNonNull(JBRApi.getService(Service.class));
+
+        // Proxy passthrough
+        Proxy p = Objects.requireNonNull(service.getProxy());
+        Proxy np = Objects.requireNonNull(service.passthrough(p));
+        if (p.getClass() != np.getClass()) {
+            throw new Error("Different classes when passing through the same object");
+        }
+
+        // Client proxy passthrough
+        ClientImpl c = new ClientImpl();
+        ClientImpl nc = Objects.requireNonNull(service.passthrough(c));
+        if (c.getClass() != nc.getClass()) {
+            throw new Error("Different classes when passing through the same object");
+        }
 
         // Test JBR-side proxy wrapping & unwrapping
         Api2Way stw = Objects.requireNonNull(service.get2Way());
@@ -65,14 +79,50 @@ public class RealTest {
         Objects.requireNonNull(tw.value);
 
         // Passing through null object -> null
-        requireNull(service.passthrough(null));
-
-        if (!service.isSelf(service)) {
-            throw new Error("service.isSelf(service) == false");
-        }
+        requireNull(service.passthrough((Api2Way) null));
 
         if (service.sum(() -> 200, () -> 65).get() != 265) {
             throw new Error("Lazy numbers conversion error");
+        }
+
+        // Check extensions
+        if (!JBRApi.isExtensionSupported(Extensions.FOO)) {
+            throw new Error("FOO extension must be supported");
+        }
+        if (JBRApi.isExtensionSupported(Extensions.BAR)) {
+            throw new Error("BAR extension must not be supported");
+        }
+        try {
+            service.getProxy().foo();
+            throw new Error("FOO extension was disabled but call succeeded");
+        } catch (UnsupportedOperationException ignore) {}
+        try {
+            service.getProxy().bar();
+            throw new Error("BAR extension was disabled but call succeeded");
+        } catch (UnsupportedOperationException ignore) {}
+        // foo() must succeed when enabled
+        JBRApi.getService(Service.class, Extensions.FOO).getProxy().foo();
+        // Asking for BAR must return null, as it is not supported
+        requireNull(JBRApi.getService(Service.class, Extensions.FOO, Extensions.BAR));
+        requireNull(JBRApi.getService(Service.class, Extensions.BAR));
+
+        // Test specialized (implicit) List proxy
+        List<Api2Way> list = Objects.requireNonNull(service.testList(null));
+        Api2Way listItem = new TwoWayImpl();
+        list.add(listItem);
+        if (list.size() != 1) {
+            throw new Error("Unexpected List size");
+        }
+        if (list.get(0) != listItem) {
+            throw new Error("Unexpected List item");
+        }
+        service.testList(list);
+        if (!list.isEmpty()) {
+            throw new Error("Unexpected List size");
+        }
+        list = new ArrayList<>();
+        if (list != service.testList(list)) {
+            throw new Error("List passthrough mismatch");
         }
     }
 
