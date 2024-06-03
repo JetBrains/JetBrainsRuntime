7b5a9eb080533f3379b45f31c63a7d2b4b2f6884
Xiaolong Peng <xlpeng@amazon.com>
2024-06-03 08:16:32 +0000

    8331771: ZGC: Remove OopMapCacheAlloc_lock ordering workaround
    Reviewed-by: phh
    Backport-of: 5746137e8a46e1eb964fe8c4de015a62dc17a745

--- src/hotspot/share/gc/shared/collectedHeap.hpp
+++ src/hotspot/share/gc/shared/collectedHeap.hpp
@@ -95,7 +95,6 @@ class CollectedHeap : public CHeapObj<mtGC> {
   friend class VMStructs;
   friend class JVMCIVMStructs;
   friend class IsSTWGCActiveMark; // Block structured external access to _is_stw_gc_active
-  friend class DisableIsSTWGCActiveMark; // Disable current IsSTWGCActiveMark
   friend class MemAllocator;
   friend class ParallelObjectIterator;
 
