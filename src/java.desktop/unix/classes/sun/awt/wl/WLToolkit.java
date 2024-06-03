79f5fa49dc80c8b6d85ed0e55251ec8e74242679
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2024-06-03 17:47:45 +0400

    JBR-7237 Separate display connect from WLToolkit initialization

--- src/java.desktop/unix/classes/sun/awt/wl/WLToolkit.java
+++ src/java.desktop/unix/classes/sun/awt/wl/WLToolkit.java
@@ -36,6 +36,7 @@ import sun.awt.PeerEvent;
 import sun.awt.SunToolkit;
 import sun.awt.UNIXToolkit;
 import sun.awt.datatransfer.DataTransferer;
+import sun.awt.wl.WLDisplay;
 import sun.util.logging.PlatformLogger;
 
 import java.awt.*;
@@ -137,12 +138,12 @@ public class WLToolkit extends UNIXToolkit implements Runnable {
     private final WLClipboard clipboard;
     private final WLClipboard selection;
 
-    private static native void initIDs();
+    private static native void initIDs(long displayPtr);
 
     static {
         if (!GraphicsEnvironment.isHeadless()) {
             keyboard = new WLKeyboard();
-            initIDs();
+            initIDs(WLDisplay.getInstance().getDisplayPtr());
         }
         initialized = true;
     }
