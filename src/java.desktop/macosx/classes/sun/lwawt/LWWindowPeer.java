dc025d82997435baed34afa08feea48ae146117f
Nikita Tsarev <nikita.tsarev@jetbrains.com>
2024-06-05 17:07:30 +0200

    JBR-6588: JBR API for inspecting certain properties of KeyEvents
    (cherry picked from commit cf2abf34d76697f20e8cc7ef9456f6054dcb0968)

--- src/java.desktop/macosx/classes/sun/lwawt/LWWindowPeer.java
+++ src/java.desktop/macosx/classes/sun/lwawt/LWWindowPeer.java
@@ -59,6 +59,7 @@ import java.awt.peer.KeyboardFocusManagerPeer;
 import java.awt.peer.WindowPeer;
 import java.util.List;
 
+import java.util.Map;
 import java.util.Objects;
 import javax.swing.JComponent;
 
@@ -1004,7 +1005,7 @@ public class LWWindowPeer
      */
     @Override
     public void notifyKeyEvent(int id, long when, int modifiers,
-                               int keyCode, char keyChar, int keyLocation)
+                               int keyCode, char keyChar, int keyLocation, Map<String, Object> properties)
     {
         LWKeyboardFocusManagerPeer kfmPeer = LWKeyboardFocusManagerPeer.getInstance();
         Component focusOwner = kfmPeer.getCurrentFocusOwner();
@@ -1021,6 +1022,7 @@ public class LWWindowPeer
         AWTAccessor.getKeyEventAccessor().setExtendedKeyCode(keyEvent,
                 (keyChar == KeyEvent.CHAR_UNDEFINED) ? keyCode
                 : ExtendedKeyCodes.getExtendedKeyCodeForChar(keyChar));
+        AWTAccessor.getKeyEventAccessor().setJBRExtraProperties(keyEvent, properties);
         postEvent(keyEvent);
     }
 
