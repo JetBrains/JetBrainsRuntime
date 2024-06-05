e93bdd994bd2bdb939feb9eff2dc79ed1ced456b
Dmitrii Morskii <dmitrii.morskii@jetbrains.com>
2024-06-05 12:42:51 +0100

    JBR-7438 tune updateCursorImmediately method

--- src/java.desktop/share/classes/java/awt/Component.java
+++ src/java.desktop/share/classes/java/awt/Component.java
@@ -4882,6 +4882,9 @@ public abstract class Component implements ImageObserver, MenuContainer,
             eventLog.finest("{0}", e);
         }
 
+        if (id == MouseEvent.MOUSE_ENTERED && getToolkit() instanceof SunToolkit toolkit) {
+            toolkit.updateLastMouseEventComponent(this);
+        }
         /*
          * 0. Set timestamp and modifiers of current event.
          */
@@ -7161,6 +7164,12 @@ public abstract class Component implements ImageObserver, MenuContainer,
         {
             KeyboardFocusManager.getCurrentKeyboardFocusManager().
                 setGlobalPermanentFocusOwner(null);
+        }
+
+        if (getToolkit() instanceof SunToolkit toolkit) {
+            if (toolkit.getLastMouseEventComponent() == this) {
+                toolkit.updateLastMouseEventComponent(null);
+            }
         }
 
         synchronized (getTreeLock()) {
