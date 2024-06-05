7ac9c9a6e7f2d93bcf347f4bdc63faaad43115ff
Martin Doerr <mdoerr@openjdk.org>
2024-06-05 08:39:55 +0000

    8305825: getBounds API returns wrong value resulting in multiple Regression Test Failures on Ubuntu 23.04
    Reviewed-by: mbaesken
    Backport-of: 7723611d5d5f8959d69d1000fa117166e3d3c5fb

--- src/java.desktop/unix/classes/sun/awt/X11/XWM.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XWM.java
@@ -1419,6 +1419,9 @@ final class XWM
               case UNITY_COMPIZ_WM:
                   res = new Insets(28, 1, 1, 1);
                   break;
+              case MUTTER_WM:
+                  res = new Insets(37, 0, 0, 0);
+                  break;
               case MOTIF_WM:
               case OPENLOOK_WM:
               default:
@@ -1430,6 +1433,7 @@ final class XWM
         }
         return res;
     }
+
     /*
      * Some buggy WMs ignore window gravity when processing
      * ConfigureRequest and position window as if the gravity is Static.
