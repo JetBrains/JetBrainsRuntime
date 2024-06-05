7d2b0265ae7d48c7f12fdefea2acbd2dd8508c59
Dmitrii Morskii <dmitrii.morskii@jetbrains.com>
2024-06-05 12:42:51 +0100

    JBR-7438 tune updateCursorImmediately method
    (cherry picked from commit 49b1001c2cf44503c917e38a2a341ccb68c9612c)

--- src/java.desktop/macosx/classes/sun/lwawt/LWCursorManager.java
+++ src/java.desktop/macosx/classes/sun/lwawt/LWCursorManager.java
@@ -26,111 +26,37 @@
 package sun.lwawt;
 
 import java.awt.Component;
-import java.awt.Container;
 import java.awt.Cursor;
 import java.awt.Point;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 import sun.awt.AWTAccessor;
-import sun.awt.SunToolkit;
+import sun.awt.CachedCursorManager;
 
-public abstract class LWCursorManager {
+public abstract class LWCursorManager extends CachedCursorManager {
 
-    /**
-     * A flag to indicate if the update is scheduled, so we don't process it
-     * twice.
-     */
-    private final AtomicBoolean updatePending = new AtomicBoolean(false);
-
-    protected LWCursorManager() {
-    }
-
-    /**
-     * Sets the cursor to correspond the component currently under mouse.
-     *
-     * This method should not be executed on the toolkit thread as it
-     * calls to user code (e.g. Container.findComponentAt).
-     */
-    public final void updateCursor() {
-        updatePending.set(false);
-        updateCursorImpl();
-    }
-
-    /**
-     * Schedules updating the cursor on the corresponding event dispatch
-     * thread for the given window.
-     *
-     * This method is called on the toolkit thread as a result of a
-     * native update cursor request (e.g. WM_SETCURSOR on Windows).
-     */
-    public final void updateCursorLater(final LWWindowPeer window) {
-        if (updatePending.compareAndSet(false, true)) {
-            Runnable r = new Runnable() {
-                @Override
-                public void run() {
-                    updateCursor();
-                }
-            };
-            SunToolkit.executeOnEventHandlerThread(window.getTarget(), r);
-        }
-    }
-
-    private void updateCursorImpl() {
-        final Point cursorPos = getCursorPosition();
-        final Component c = findComponent(cursorPos);
-        final Cursor cursor;
+    @Override
+    protected Cursor getCursorByPosition(final Point cursorPos, Component c) {
         final Object peer = LWToolkit.targetToPeer(c);
         if (peer instanceof LWComponentPeer) {
             final LWComponentPeer<?, ?> lwpeer = (LWComponentPeer<?, ?>) peer;
             final Point p = lwpeer.getLocationOnScreen();
-            cursor = lwpeer.getCursor(new Point(cursorPos.x - p.x,
-                                                cursorPos.y - p.y));
-        } else {
-            cursor = (c != null) ? c.getCursor() : null;
+            return lwpeer.getCursor(new Point(cursorPos.x - p.x,
+                    cursorPos.y - p.y));
         }
-        setCursor(cursor);
+        return null;
     }
 
-    /**
-     * Returns the first visible, enabled and showing component under cursor.
-     * Returns null for modal blocked windows.
-     *
-     * @param cursorPos Current cursor position.
-     * @return Component or null.
-     */
-    private static final Component findComponent(final Point cursorPos) {
+    @Override
+    protected Component getComponentUnderCursor() {
         final LWComponentPeer<?, ?> peer = LWWindowPeer.getPeerUnderCursor();
-        Component c = null;
         if (peer != null && peer.getWindowPeerOrSelf().getBlocker() == null) {
-            c = peer.getTarget();
-            if (c instanceof Container) {
-                final Point p = peer.getLocationOnScreen();
-                c = AWTAccessor.getContainerAccessor().findComponentAt(
-                    (Container) c, cursorPos.x - p.x, cursorPos.y - p.y, false);
-
-            }
-            while (c != null) {
-                final Object p = AWTAccessor.getComponentAccessor().getPeer(c);
-                if (c.isVisible() && c.isEnabled() && p != null) {
-                    break;
-                }
-                c = c.getParent();
-            }
+            return peer.getTarget();
         }
-        return c;
+        return null;
     }
 
-    /**
-     * Returns the current cursor position.
-     */
-    // TODO: make it public to reuse for MouseInfo
-    protected abstract Point getCursorPosition();
-
-    /**
-     * Sets a cursor. The cursor can be null if the mouse is not over a Java
-     * window.
-     * @param cursor the new {@code Cursor}.
-     */
-    protected abstract void setCursor(Cursor cursor);
+    @Override
+    protected Point getLocationOnScreen(Component component) {
+        return AWTAccessor.getComponentAccessor().getPeer(component).getLocationOnScreen();
+    }
 }
