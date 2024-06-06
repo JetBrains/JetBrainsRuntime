3eb56a51b640b69a094edccb991808669f494bf6
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2024-06-06 18:09:36 +0200

    Fix WindowMoveService on Wayland
    (cherry picked from commit 00f213d0ffad50000670f4d1c932d3fadf857c9b)

--- src/java.desktop/unix/classes/sun/awt/WindowMoveService.java
+++ src/java.desktop/unix/classes/sun/awt/WindowMoveService.java
@@ -2,6 +2,7 @@ package sun.awt;
 
 import com.jetbrains.exported.JBRApi;
 import sun.awt.X11.WindowMoveServiceX11;
+import sun.awt.wl.WLWindowMoveService;
 
 import java.awt.*;
 
@@ -12,15 +13,15 @@ public interface WindowMoveService {
     private static WindowMoveService create() {
         JBRApi.ServiceNotAvailableException exception;
         try {
-            return new WindowMoveServiceX11();
+            return new WLWindowMoveService();
         } catch (JBRApi.ServiceNotAvailableException e) {
             exception = e;
         }
-//        try {
-//            return new WindowMoveServiceWL();
-//        } catch (JBRApi.ServiceNotAvailableException e) {
-//            exception.addSuppressed(e);
-//        }
+        try {
+            return new WindowMoveServiceX11();
+        } catch (JBRApi.ServiceNotAvailableException e) {
+            exception.addSuppressed(e);
+        }
         throw exception;
     }
 
