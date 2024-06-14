27db52d423dde1b5a3a6325c966cff63a75961aa
Martin Doerr <mdoerr@openjdk.org>
2024-06-14 15:13:09 +0000

    8319793: C2 compilation fails with "Bad graph detected in build_loop_late" after JDK-8279888
    Reviewed-by: mbaesken
    Backport-of: b922f8d45951250b7c39cb179b9bc1a8a6256a9e

--- src/hotspot/share/opto/node.hpp
+++ src/hotspot/share/opto/node.hpp
@@ -1116,7 +1116,14 @@ public:
   // Set control or add control as precedence edge
   void ensure_control_or_add_prec(Node* c);
 
-//----------------- Code Generation
+  // Returns a clone of the current node that's pinned (if the current node is not) for nodes found in array accesses
+  // (Load and range check CastII nodes).
+  // This is used when an array access is made dependent on 2 or more range checks (range check smearing or Loop Predication).
+  virtual Node* pin_array_access_node() const {
+    return nullptr;
+  }
+
+  //----------------- Code Generation
 
   // Ideal register class for Matching.  Zero means unmatched instruction
   // (these are cloned instead of converted to machine nodes).
