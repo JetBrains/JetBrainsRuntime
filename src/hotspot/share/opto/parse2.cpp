ff10d2cd11195726d128b15434fb2ee84dd7a7b7
Martin Doerr <mdoerr@openjdk.org>
2024-06-14 16:22:45 +0000

    8322996: BoxLockNode creation fails with assert(reg < CHUNK_SIZE) failed: sanity
    Backport-of: 69586e7bdffe1a840c3a86e6ec83568de24c6fe5

--- src/hotspot/share/opto/parse2.cpp
+++ src/hotspot/share/opto/parse2.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -2779,6 +2779,7 @@ void Parse::do_one_bytecode() {
   }
 
 #ifndef PRODUCT
+  if (failing()) { return; }
   if (C->should_print_igv(1)) {
     IdealGraphPrinter* printer = C->igv_printer();
     char buffer[256];
