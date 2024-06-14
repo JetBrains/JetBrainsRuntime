db2ca01889173e4e891d114c1d36fd37bc839f3b
bourgesl <bourges.laurent@gmail.com>
2024-06-14 17:24:40 +0200

    JBR-4530: make opengl & metal handle colorMatching on non-SRGB profile consistently (controlled by the system property 'sun.java2d.osx.colorMatching') + updated MacOSLayerColorTest to test color matching setting on OpenGL & Metal pipelines
    (cherry picked from commit 2db47e604e1e3d1913f6ff5645be1b174b99f1ac)

--- test/jdk/java/awt/Graphics2D/MacOSLayerColorTest.java
+++ test/jdk/java/awt/Graphics2D/MacOSLayerColorTest.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2019, 2023, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2023, JetBrains s.r.o.. All rights reserved.
+ * Copyright (c) 2019, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, 2024, JetBrains s.r.o.. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,7 +25,6 @@
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.geom.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.*;
@@ -41,67 +40,93 @@ import javax.swing.*;
  * @bug 8311165
  * @requires os.family == "mac"
  * @summary [macosx] compares displayed colors versus input colors on macos opengl or metal pipelines.
- * @run main/othervm -Dsun.java2d.uiScale=1.0 -Dsun.java2d.opengl=true MetalLayerColorTest
- * @run main/othervm -Dsun.java2d.uiScale=1.0 -Dsun.java2d.metal=true MetalLayerColorTest
- * @run main/othervm -Dsun.java2d.uiScale=1.0 -Dsun.java2d.metal=true -Dsun.java2d.metal.colorMatching=true MetalLayerColorTest
+ * @run main/othervm -Dsun.java2d.opengl=true MacOSLayerColorTest
+ * @run main/othervm -Dsun.java2d.opengl=true -Dsun.java2d.osx.colorMatching=false MacOSLayerColorTest
+ * @run main/othervm -Dsun.java2d.metal=true MacOSLayerColorTest
+ * @run main/othervm -Dsun.java2d.metal=true -Dsun.java2d.osx.colorMatching=false MacOSLayerColorTest
+ * @run main/othervm -Dsun.java2d.metal=true -Dsun.java2d.metal.colorMatching=false MacOSLayerColorTest
  * @author lbourges
- *
- *
+ * @comment
  * Expected results:
- * As opengl is the reference implementation on MacOS since 20 years, it uses directly sRGB values (no color matching).
- * If the monitor is using display P3, colors are displayed as sRGB colors => color differences are important !
+ * As the OpenGL pipeline was the reference implementation on OSX for 20 years, the CGLLayer used directly sRGB values
+ * (no color matching) until mid-2024 when the JBR-4530 patch made the color matching work with OpenGL by default, i.e.
+ * the window colorspace is set to sRGB to let macOS convert sRGB to the monitor color space.
+ * .
+ * The system property 'sun.java2d.osx.colorMatching' (mid-2024) controls the color matching setting (true by default).
  *
- * With MacOS Monitor profile 'sRGB':
+ * If the monitor is using display P3 (non-SRGB profile), colors are displayed without color matching as sRGB colors
+ * => color differences are important (more-saturated or distorted colors) !
+ *
+ * -Dsun.java2d.opengl=true:
+ *
+ * With macOS Monitor profile 'sRGB':
  *      Java2D pipeline: OpenGL
  *      COLOR_TOLERANCE: 5
  *      Stats: delta[729] sum: 0 avg: 0.0 [0 | 0]
  *      Max colors: 729
  *      Bad colors: 0
  *
- * With MacOS Monitor profile 'XDR Display P3 (1600 nits)':
+ * With macOS Monitor profile 'XDR Display P3 (1600 nits)' -Dsun.java2d.osx.colorMatching=true (default after mid-2024):
  *      Java2D pipeline: OpenGL
+ *      ColorMatching: true
  *      COLOR_TOLERANCE: 5
- *      OpenGL pipeline enabled on screen 1
+ *      Stats: delta[729] sum: 389 avg: 0.533 [0 | 5]
+ *      Max colors: 729
+ *      Bad colors: 0
+ *
+ * With macOS Monitor profile 'XDR Display P3 (1600 nits)' -Dsun.java2d.osx.colorMatching=false (default before mid-2024):
+ *      Java2D pipeline: OpenGL
+ *      ColorMatching: false
+ *      COLOR_TOLERANCE: 5
  *      Stats: delta[729] sum: 16534 avg: 22.68 [0 | 96]
  *      Max colors: 729
  *      Bad colors: 615
-
  *
- * For the apple Metal pipeline, CAMetalLayer was converting sRGB to monitor color space (color matching) until 2023.06.
- * A new system property has been introduced to enable back color matching (disabled by default to mimic opengl output)
  *
- * With MacOS Monitor profile 'sRGB':
+ * For the Metal pipeline, the CAMetalLayer is converting sRGB to the monitor color space (color matching).
+ * To mimic the former OpenGL behaviour (no color matching), the system property 'sun.java2d.metal.colorMatching' was
+ * introduced in mid-2023. For compatibility reasons, this former property is preserved as a fallback of the new
+ * property 'sun.java2d.osx.colorMatching' controlling the color matching setting on macOS.
+ *
+ * -Dsun.java2d.metal=true:
+ *
+ * With macOS Monitor profile 'sRGB':
  *      Java2D pipeline: Metal
+ *      ColorMatching: true
  *      COLOR_TOLERANCE: 5
  *      Stats: delta[729] sum: 0 avg: 0.0 [0 | 0]
  *      Max colors: 729
  *      Bad colors: 0
  *
- * With MacOS Monitor profile 'Display P3 (1600 nits)' -Dsun.java2d.metal.colorMatching=true (before 2023.06.28):
+ * With macOS Monitor profile 'Display P3 (1600 nits)' -Dsun.java2d.metal.colorMatching=true (default):
  *      Java2D pipeline: Metal
+ *      ColorMatching: true
  *      COLOR_TOLERANCE: 5
  *      Metal pipeline enabled on screen 1
  *      Stats: delta[729] sum: 389 avg: 0.533 [0 | 5]
  *      Max colors: 729
  *      Bad colors: 0
  *
- * With MacOS Monitor profile 'Display P3 (1600 nits)' -Dsun.java2d.metal.colorMatching=false (new default on 2023.06.28):
+ * With macOS Monitor profile 'Display P3 (1600 nits)' -Dsun.java2d.metal.colorMatching=false:
  *      Java2D pipeline: Metal
+ *      ColorMatching: false
  *      COLOR_TOLERANCE: 5
  *      Metal pipeline enabled on screen 1
  *      Stats: delta[729] sum: 16534 avg: 22.68 [0 | 96]
  *      Max colors: 729
  *      Bad colors: 615
  *
- * With this fix, the metal pipeline renders colors like opengl pipeline (without any color matching) on XDR display.
+ *  Note: with other monitor profiles, color differences may be more or less important (see Stats).
  */
-public class MetalLayerColorTest {
+public class MacOSLayerColorTest {
 
     private final static boolean TRACE_MARKER = false;
     private final static boolean TRACE = false;
 
-    private final static int BW = 30;
-    private final static int BH = 30;
+    private final static boolean SAVE_IMAGE = false;
+
+    private final static int BW = 20;
+    private final static int BH = 20;
 
     private final static int MW = BW / 2;
     private final static int MH = BH / 2;
@@ -157,16 +182,11 @@ public class MetalLayerColorTest {
         private final float[] bx;
         private final float[] by;
         private final int n;
-
-        private final float bw;
-        private final float bh;
         final float width;
         final float height;
 
         Blocks(int n, float bw, float bh) {
             this.n = n;
-            this.bw = bw;
-            this.bh = bh;
             bx = new float[n];
             by = new float[n];
 
@@ -224,7 +244,7 @@ public class MetalLayerColorTest {
                 }
                 stats.add(delta);
             }
-            System.out.println("Stats: "+stats.toString());
+            System.out.println("Stats: " + stats);
 
             return res;
         }
@@ -232,17 +252,11 @@ public class MetalLayerColorTest {
 
     static class PerfMeter {
 
-        private String name;
-
         private JPanel panel;
-        private AtomicInteger markerIdx = new AtomicInteger(0);
-        private AtomicLong markerPaintTime = new AtomicLong(0);
+        private final AtomicInteger markerIdx = new AtomicInteger(0);
+        private final AtomicLong markerPaintTime = new AtomicLong(0);
 
-        PerfMeter(String name) {
-            this.name = name;
-        }
-
-        PerfMeter exec() throws Exception {
+        void exec() throws Exception {
             final CountDownLatch latchFrame = new CountDownLatch(1);
 
             final JFrame f = new JFrame(
@@ -307,6 +321,7 @@ public class MetalLayerColorTest {
             final Robot robot = new Robot();
             int cycle = 0;
             int frame = 0;
+            int res = -1;
 
             while (frame < COUNT) {
                 sync();
@@ -350,20 +365,16 @@ public class MetalLayerColorTest {
                             System.out.println("All markers match.");
                         }
 
-                        final AffineTransform tx = parent.getGraphicsConfiguration().getDefaultTransform();
-
-                        final Rectangle windowRect = new Rectangle(
-                                (int) (x0 * tx.getScaleX()), (int) (y0 * tx.getScaleY()),
-                                (int) (w * tx.getScaleX()), (int) (h * tx.getScaleY()));
-
                         if (frame == 0) {
+                            final Rectangle windowRect = new Rectangle(x0, y0, w, h);
                             if (TRACE) {
                                 System.out.println("parent: " + parent);
                                 System.out.println("insets: " + insets);
-                                System.out.println("tx: " + tx);
-
                                 System.out.println("windowRect: " + windowRect);
                             }
+                            // ensure mouse is outside the region of interest:
+                            robot.mouseMove(x0 + w, y0);
+
                             final BufferedImage capture = robot.createScreenCapture(windowRect);
 
                             if (TRACE) {
@@ -371,18 +382,16 @@ public class MetalLayerColorTest {
                                 System.out.println("image height: " + capture.getHeight());
                             }
 
-                            ImageIO.write(capture, "png", new File("capture_"
-                                + ((IS_RDR_METAL ? (IS_RDR_METAL_CM ? "metal_cm1" : "metal_cm0") : "opengl")) +".png")
-                            );
-
-                            final int res = blocks.test(capture);
+                            if (SAVE_IMAGE) {
+                                ImageIO.write(capture, "png", new File("capture_"
+                                        + (IS_RDR_METAL ? "metal" : "opengl") + "_"
+                                        + (IS_RDR_CM_ON ? "cm1" : "cm0") + ".png")
+                                );
+                            }
+                            res = blocks.test(capture);
 
                             System.out.println("Max colors: " + N);
                             System.out.println("Bad colors: " + res);
-
-                            if (false) {
-                                break;
-                            }
                         }
 
                         frame++;
@@ -395,7 +404,7 @@ public class MetalLayerColorTest {
                 try {
                     Thread.sleep(CYCLE_DELAY);
                 } catch (InterruptedException ex) {
-                    ex.printStackTrace();
+                    // no-op
                 }
                 if (cycle >= MAX_MEASURE_CYCLES) {
                     System.out.println("Max cycle : " + cycle);
@@ -409,12 +418,15 @@ public class MetalLayerColorTest {
                 f.dispose();
             });
             latchFrame.await();
-            return this;
+
+            if (IS_RDR_CM_ON && (res != 0)) {
+                throw new RuntimeException("Expected 0 bad colors (but " + res + ") with color matching!");
+            }
         }
     }
 
     private static boolean IS_RDR_METAL = false;
-    private static boolean IS_RDR_METAL_CM = false;
+    private static boolean IS_RDR_CM_ON = false;
 
     public static void main(String[] args) {
         if (System.getProperty("sun.java2d.opengl") == null && System.getProperty("sun.java2d.metal") == null) {
@@ -428,17 +440,22 @@ public class MetalLayerColorTest {
         }
         System.out.println("Java2D pipeline: " + (IS_RDR_METAL ? "Metal" : "OpenGL"));
 
-        if (IS_RDR_METAL) {
-            IS_RDR_METAL_CM = !"false".equalsIgnoreCase(System.getProperty("sun.java2d.metal.colorMatching"));
-            System.out.println("IS_RDR_METAL_CM: " + IS_RDR_METAL_CM);
+        String colorMatchingProp = System.getProperty("sun.java2d.osx.colorMatching");
+        if (colorMatchingProp == null) {
+            // fallback on the former metal property:
+            colorMatchingProp = System.getProperty("sun.java2d.metal.colorMatching");
         }
+        IS_RDR_CM_ON = !"false".equalsIgnoreCase(colorMatchingProp);
+        System.out.println("ColorMatching: " + IS_RDR_CM_ON);
 
         System.out.println("COLOR_TOLERANCE: " + COLOR_TOLERANCE);
 
         try {
-            new PerfMeter("FlatBoxAA").exec();
+            new PerfMeter().exec();
+        } catch (RuntimeException re) {
+            throw re;
         } catch (Exception e) {
-            new RuntimeException("Test failed.", e);
+            throw new RuntimeException("Test failed.", e);
         }
     }
 
@@ -467,10 +484,6 @@ public class MetalLayerColorTest {
         return delta;
     }
 
-    private static boolean isAlmostEqual(final Color c, final Color exp, final boolean dump) {
-        return (delta(c, exp, dump) <= COLOR_TOLERANCE);
-    }
-
     /**
      * Statistics as long values
      */
@@ -484,13 +497,6 @@ public class MetalLayerColorTest {
 
         public StatLong(final String name) {
             this.name = name;
-        }
-
-        public void reset() {
-            count = 0L;
-            sum = 0L;
-            min = Integer.MAX_VALUE;
-            max = Integer.MIN_VALUE;
         }
 
         public void add(final int val) {
@@ -520,7 +526,7 @@ public class MetalLayerColorTest {
             return toString(new StringBuilder(128)).toString();
         }
 
-        public final StringBuilder toString(final StringBuilder sb) {
+        public StringBuilder toString(final StringBuilder sb) {
             sb.append(name).append('[').append(count);
             sb.append("] sum: ").append(sum).append(" avg: ");
             sb.append(trimTo3Digits(((double) sum) / count));
