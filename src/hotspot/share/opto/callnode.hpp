a1aacbc253c5d30035e5bb418c755a022f3feed4
Martin Doerr <mdoerr@openjdk.org>
2024-06-14 16:22:24 +0000

    8317299: safepoint scalarization doesn't keep track of the depth of the JVM state
    Reviewed-by: phh
    Backport-of: 6d911f68a3244c40a62ab2570dfec68b8d0ed5d8

--- src/hotspot/share/opto/callnode.hpp
+++ src/hotspot/share/opto/callnode.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -509,7 +509,7 @@ public:
 class SafePointScalarObjectNode: public TypeNode {
   uint _first_index; // First input edge relative index of a SafePoint node where
                      // states of the scalarized object fields are collected.
-                     // It is relative to the last (youngest) jvms->_scloff.
+  uint _depth;       // Depth of the JVM state the _first_index field refers to
   uint _n_fields;    // Number of non-static fields of the scalarized object.
   DEBUG_ONLY(Node* _alloc;)
 
@@ -523,7 +523,7 @@ public:
 #ifdef ASSERT
                             Node* alloc,
 #endif
-                            uint first_index, uint n_fields);
+                            uint first_index, uint depth, uint n_fields);
   virtual int Opcode() const;
   virtual uint           ideal_reg() const;
   virtual const RegMask &in_RegMask(uint) const;
@@ -532,7 +532,7 @@ public:
 
   uint first_index(JVMState* jvms) const {
     assert(jvms != nullptr, "missed JVMS");
-    return jvms->scloff() + _first_index;
+    return jvms->of_depth(_depth)->scloff() + _first_index;
   }
   uint n_fields()    const { return _n_fields; }
 
