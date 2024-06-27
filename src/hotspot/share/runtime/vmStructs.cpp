69ee1d0005366d8db45554ad3b68d7057a98e1df
Martin Balao <mbalao@openjdk.org>
2024-06-27 15:35:11 +0000

    8303466: C2: failed: malformed control flow. Limit type made precise with MaxL/MinL
    Reviewed-by: roland
    Backport-of: cc894d849aa5f730d5a806acfc7a237cf5170af1

--- src/hotspot/share/runtime/vmStructs.cpp
+++ src/hotspot/share/runtime/vmStructs.cpp
@@ -1552,6 +1552,8 @@ typedef PaddedEnd<ObjectMonitor>              PaddedObjectMonitor;
   declare_c2_type(MaxNode, AddNode)                                       \
   declare_c2_type(MaxINode, MaxNode)                                      \
   declare_c2_type(MinINode, MaxNode)                                      \
+  declare_c2_type(MaxLNode, MaxNode)                                      \
+  declare_c2_type(MinLNode, MaxNode)                                      \
   declare_c2_type(MaxFNode, MaxNode)                                      \
   declare_c2_type(MinFNode, MaxNode)                                      \
   declare_c2_type(MaxDNode, MaxNode)                                      \
