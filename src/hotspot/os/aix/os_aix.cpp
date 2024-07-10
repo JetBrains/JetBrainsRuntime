829a0c5df1e55e3ba521a3d4b86f9049608162df
Liming Liu <lliu@openjdk.org>
2024-07-10 12:00:23 +0000

    8315923: pretouch_memory by atomic-add-0 fragments huge pages unexpectedly 8324781: runtime/Thread/TestAlwaysPreTouchStacks.java failed with Expected a higher ratio between stack committed and reserved 8325218: gc/parallel/TestAlwaysPreTouchBehavior.java fails
    Reviewed-by: shade
    Backport-of: a65a89522d2f24b1767e1c74f6689a22ea32ca6a

--- src/hotspot/os/aix/os_aix.cpp
+++ src/hotspot/os/aix/os_aix.cpp
@@ -1875,6 +1875,10 @@ void os::pd_realign_memory(char *addr, size_t bytes, size_t alignment_hint) {
 void os::pd_free_memory(char *addr, size_t bytes, size_t alignment_hint) {
 }
 
+size_t os::pd_pretouch_memory(void* first, void* last, size_t page_size) {
+  return page_size;
+}
+
 void os::numa_make_global(char *addr, size_t bytes) {
 }
 
@@ -3010,4 +3014,3 @@ void os::print_memory_mappings(char* addr, size_t bytes, outputStream* st) {}
 void os::jfr_report_memory_info() {}
 
 #endif // INCLUDE_JFR
-
