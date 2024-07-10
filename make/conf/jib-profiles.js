686b6824c158e2c5a285f12c94747ec9d7f5ea49
George Adams <gdams@openjdk.org>
2024-07-10 08:13:46 +0000

    8300806: Update googletest to v1.13.0
    Reviewed-by: sgehwolf
    Backport-of: ccf2f5837b31cddd24ec81f7f67107d9fc03c294

--- make/conf/jib-profiles.js
+++ make/conf/jib-profiles.js
@@ -1242,7 +1242,7 @@ var getJibProfilesDependencies = function (input, common) {
         gtest: {
             organization: common.organization,
             ext: "tar.gz",
-            revision: "1.8.1"
+            revision: "1.13.0+1.0"
         },
     };
 
