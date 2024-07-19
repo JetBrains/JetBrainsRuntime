097d0b678506b824afd5cf2e5ee4341bd7fa93db
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2024-07-19 15:54:01 +0200

    JBR-7419 Refactor Vulkan code
    - Separate instance and device-specific function tables
    - Avoid using device from global context when possible
    - Set up debug logger
    
    (cherry picked from commit af4eb8b7587d8f307d938b09c6fcb15b537e869d)

--- src/java.desktop/share/native/common/java2d/vulkan/CArrayUtil.h
+++ src/java.desktop/share/native/common/java2d/vulkan/CArrayUtil.h
@@ -19,9 +19,9 @@ void* CARR_array_realloc(CARR_array_t* vec, size_t new_capacity);
 /**
  * Allocate array
  * @param T type of elements
- * @param SIZE size of the array
+ * @param CAPACITY capacity of the array
  */
-#define ARRAY_ALLOC(T, SIZE) (T*)CARR_array_alloc(sizeof(T), SIZE)
+#define ARRAY_ALLOC(T, CAPACITY) (T*)CARR_array_alloc(sizeof(T), CAPACITY)
 
 
 #define ARRAY_T(P) (CARR_array_t *)((char*)P - offsetof(CARR_array_t, data))
@@ -51,12 +51,30 @@ void* CARR_array_realloc(CARR_array_t* vec, size_t new_capacity);
 #define ARRAY_FREE(P) free(ARRAY_T(P))
 
 /**
- * Apply function to the vector
+ * Apply function to the array elements
  * @param P pointer to the first data element of the array
  * @param F function to apply
  */
-#define ARRAY_APPLY(P, F) do {                            \
+#define ARRAY_APPLY(P, F) do {                                   \
     for (uint32_t _i = 0; _i < ARRAY_SIZE(P); _i++) F(&(P[_i])); \
+} while(0)
+
+/**
+ * Apply function to the array elements, passing pointer to an element as first parameter
+ * @param P pointer to the first data element of the array
+ * @param F function to apply
+ */
+#define ARRAY_APPLY_LEADING(P, F, ...) do {                                   \
+    for (uint32_t _i = 0; _i < ARRAY_SIZE(P); _i++) F(&(P[_i]), __VA_ARGS__); \
+} while(0)
+
+/**
+ * Apply function to the array elements, passing pointer to an element as last parameter
+ * @param P pointer to the first data element of the array
+ * @param F function to apply
+ */
+#define ARRAY_APPLY_TRAILING(P, F, ...) do {                                  \
+    for (uint32_t _i = 0; _i < ARRAY_SIZE(P); _i++) F(__VA_ARGS__, &(P[_i])); \
 } while(0)
 
 /**
