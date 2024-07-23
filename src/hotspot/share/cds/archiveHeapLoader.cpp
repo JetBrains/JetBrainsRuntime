eef95a6a092463b474eed92c8713b4f4bdab31df
luwang1103 <576420940@qq.com>
2024-07-23 08:46:04 +0000

    8334239: Introduce macro for ubsan method/function exclusions
    Backport-of: ff30240926224b2f98e173bcd606c157af788919

--- src/hotspot/share/cds/archiveHeapLoader.cpp
+++ src/hotspot/share/cds/archiveHeapLoader.cpp
@@ -33,6 +33,7 @@
 #include "memory/iterator.inline.hpp"
 #include "memory/resourceArea.hpp"
 #include "memory/universe.hpp"
+#include "sanitizers/ub.hpp"
 #include "utilities/bitMap.inline.hpp"
 #include "utilities/copy.hpp"
 
@@ -59,9 +60,7 @@ ptrdiff_t ArchiveHeapLoader::_mapped_heap_delta = 0;
 
 // Every mapped region is offset by _mapped_heap_delta from its requested address.
 // See FileMapInfo::heap_region_requested_address().
-#if defined(__clang__) || defined(__GNUC__)
-__attribute__((no_sanitize("undefined")))
-#endif
+ATTRIBUTE_NO_UBSAN
 void ArchiveHeapLoader::init_mapped_heap_relocation(ptrdiff_t delta, int dumptime_oop_shift) {
   assert(!_mapped_heap_relocation_initialized, "only once");
   if (!UseCompressedOops) {
