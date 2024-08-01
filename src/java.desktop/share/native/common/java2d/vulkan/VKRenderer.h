146393362867f77fa7c29947521ae544c7f216b4
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2024-08-01 13:34:25 +0200

    JBR-7452 Vulkan: Reuse VkRenderPass for multiple renderers (#428)
    Moved shared VkRenderPass to the logical device
    
    (cherry picked from commit 3d782ed0f01ced266bdbd903d0ade5407bd2f05a)

--- src/java.desktop/share/native/common/java2d/vulkan/VKRenderer.h
+++ src/java.desktop/share/native/common/java2d/vulkan/VKRenderer.h
@@ -34,7 +34,6 @@
 #include "VKSurfaceData.h"
 
 struct VKRenderer {
-    VkRenderPass        renderPass;
     VkDescriptorSetLayout descriptorSetLayout;
     VkDescriptorPool    descriptorPool;
     VkDescriptorSet     descriptorSets;
