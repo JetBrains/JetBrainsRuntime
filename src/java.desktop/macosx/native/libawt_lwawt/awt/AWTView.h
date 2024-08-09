158b104bf483b3af32a1c05ff6d7a4bdf530617e
Nikita Tsarev <nikita.tsarev@jetbrains.com>
2024-08-09 22:35:10 +0200

    JBR-7426: Fix cancelling press-and-hold causing some future key events being swallowed
    (cherry picked from commit a07d5e4a60c94ca2cc334f8956e121c0ed3e958e)

--- src/java.desktop/macosx/native/libawt_lwawt/awt/AWTView.h
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/AWTView.h
@@ -49,8 +49,7 @@
     NSString* actualCharacters;
 
     BOOL fEnablePressAndHold;
-    BOOL fInPressAndHold;
-    BOOL fPAHNeedsToSelect;
+    BOOL fIsPressAndHold;
 
     id cglLayer; // is a sublayer of view.layer
 
