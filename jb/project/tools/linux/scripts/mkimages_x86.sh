25033bf64f2f22527092277ca1c9eb0580c59795
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2024-08-19 16:54:04 +0400

    JBR-7517 build JBR artefacts with CDS archives
    (cherry picked from commit 0b5462b3ec5130dcfe731172542ad4f53f7df54d)

--- jb/project/tools/linux/scripts/mkimages_x86.sh
+++ jb/project/tools/linux/scripts/mkimages_x86.sh
@@ -51,7 +51,7 @@ function create_image_bundle {
   __cds_opt=''
 
   if is_musl; then libc_type_suffix='musl-' ; fi
-  if [ "$__arch_name" == "$JBRSDK_BUNDLE" ]; then __cds_opt="--generate-cds-archive"; fi
+  __cds_opt="--generate-cds-archive"
 
   [ "$bundle_type" == "fd" ] && [ "$__arch_name" == "$JBRSDK_BUNDLE" ] && __bundle_name=$__arch_name && fastdebug_infix="fastdebug-"
   JBR=${__bundle_name}-${JBSDK_VERSION}-linux-${libc_type_suffix}x86-${fastdebug_infix}b${build_number}
