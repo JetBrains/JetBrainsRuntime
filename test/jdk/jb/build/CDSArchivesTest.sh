630485672d79208b5ca43a0c198e29f8ee06ee9f
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2024-08-19 16:54:04 +0400

    JBR-7517 build JBR artefacts with CDS archives

--- test/jdk/jb/build/CDSArchivesTest.sh
+++ test/jdk/jb/build/CDSArchivesTest.sh
@@ -11,15 +11,6 @@ if [ -z "${TESTJAVA}" ]; then
   exit 1
 fi
 
-IMPLEMENTOR_VERSION=$(cat ${TESTJAVA}/release | grep "IMPLEMENTOR_VERSION" | cut -d"=" -f2)
-
-echo "Checking ${IMPLEMENTOR_VERSION}"
-if [ "${IMPLEMENTOR_VERSION}" != *"JBRSDK"* ]; then
-  echo "Test executed for JBRSDK only"
-  echo "skipping the test"
-  exit 0
-fi
-
 FIND="/usr/bin/find"
 files=$(${FIND} ${TESTJAVA} -name "*.jsa")
 ls -l $files
