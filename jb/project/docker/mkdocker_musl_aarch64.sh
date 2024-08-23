542b17e1563cbddfe25546b64434023f948f7df2
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2024-08-23 14:07:21 +0400

    JBR-7532 upgrade alpine up to 3.14, specify versions of installing packages, and deploy the latest available jdk20 (same as in jbr21)
    (cherry picked from commit 794eab0e1fbfe2f15b2fae0f132d2bf70e0283fd)

--- jb/project/docker/mkdocker_musl_aarch64.sh
+++ jb/project/docker/mkdocker_musl_aarch64.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 set -x
 
 # This script creates a Docker image suitable for building musl AArch64 variant
-# of the JetBrains Runtime version 17.
+# of the JetBrains Runtime version 21.
 
-BOOT_JDK_REMOTE_FILE=zulu17.32.13-ca-jdk17.0.2-linux_musl_aarch64.tar.gz
-BOOT_JDK_SHA=6b920559abafbe9bdef386a20ecf3a2f318bc1f0d8359eb1f95aee26606bbc70
+BOOT_JDK_REMOTE_FILE=zulu20.32.11-ca-jdk20.0.2-linux_musl_aarch64.tar.gz
+BOOT_JDK_SHA=eec57cf744c2438f695221f041d4804de3033ad33b6dba769d3359813ba3f90d
 BOOT_JDK_LOCAL_FILE=boot_jdk_musl_aarch64.tar.gz
 
 if [ ! -f $BOOT_JDK_LOCAL_FILE ]; then
@@ -22,7 +22,7 @@ sha256sum -c - <<EOF
 $BOOT_JDK_SHA *$BOOT_JDK_LOCAL_FILE
 EOF
 
-docker build -t jbr17buildenv -f Dockerfile.musl_aarch64 .
+docker build -t jetbrains/runtime:jbr21env_musl_aarch64 -f Dockerfile.musl_aarch64 .
 
 # NB: the resulting container can (and should) be used without the network
 # connection (--network none) during build in order to reduce the chance
