f08deaac20fe8de0bfb5df3bd56aa7206cd17f26
Nikita Gubarkov <Nikita.Gubarkov@jetbrains.com>
2024-08-28 15:11:20 +0200

    JBR-7570 Implemented ring buffer. Added lazy implicit initialization for dynamic arrays. (#451)
    (cherry picked from commit 7b89fe231181af6641d1439d3e954b383b231da4)

--- src/java.desktop/share/native/common/java2d/vulkan/CArrayUtil.c
+++ src/java.desktop/share/native/common/java2d/vulkan/CArrayUtil.c
@@ -8,25 +8,48 @@ void* CARR_array_alloc(size_t elem_size, size_t capacity) {
     if (pvec == NULL) {
         return NULL;
     }
-    pvec->elem_size = elem_size;
     pvec->size = 0;
     pvec->capacity = capacity;
     return pvec->data;
 }
 
-void* CARR_array_realloc(CARR_array_t* vec, size_t new_capacity) {
+void* CARR_array_realloc(CARR_array_t* vec, size_t elem_size, size_t new_capacity) {
     if (vec->capacity == new_capacity) {
         return vec->data;
     }
     CARR_array_t* new_vec =
-            (CARR_array_t*)((char*)CARR_array_alloc(vec->elem_size, new_capacity) - offsetof(CARR_array_t, data));
+            (CARR_array_t*)((char*)CARR_array_alloc(elem_size, new_capacity) - offsetof(CARR_array_t, data));
     if (new_vec == NULL) {
-        return NULL;
+        return vec == NULL ? NULL : vec->data;
     }
     new_vec->capacity = new_capacity;
     new_vec->size = MIN(vec->size, new_capacity);
-    new_vec->elem_size = vec->elem_size;
-    memcpy(new_vec->data, vec->data, new_vec->size*new_vec->elem_size);
+    memcpy(new_vec->data, vec->data, new_vec->size*elem_size);
     free(vec);
     return new_vec->data;
+}
+
+void* CARR_ring_buffer_realloc(CARR_ring_buffer_t* buf, size_t elem_size, size_t new_capacity) {
+    if (buf != NULL && buf->capacity == new_capacity) {
+        return buf->data;
+    }
+    CARR_ring_buffer_t* new_buf =
+            (CARR_ring_buffer_t*) malloc(elem_size * new_capacity + offsetof(CARR_ring_buffer_t, data));
+    if (new_buf == NULL) {
+        return NULL;
+    }
+    new_buf->head = new_buf->tail = 0;
+    new_buf->capacity = new_capacity;
+    if (buf != NULL) {
+        if (buf->tail > buf->head) {
+            new_buf->tail = buf->tail - buf->head;
+            memcpy(new_buf->data, buf->data + buf->head*elem_size, new_buf->tail*elem_size);
+        } else if (buf->tail < buf->head) {
+            new_buf->tail = buf->capacity + buf->tail - buf->head;
+            memcpy(new_buf->data, buf->data + buf->head*elem_size, (buf->capacity-buf->head)*elem_size);
+            memcpy(new_buf->data + (new_buf->tail-buf->tail)*elem_size, buf->data, buf->tail*elem_size);
+        }
+        free(buf);
+    }
+    return new_buf->data;
 }
