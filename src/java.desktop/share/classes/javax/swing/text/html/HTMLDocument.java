2001d39ce1c0f603e9ef547e25c086ea30780fab
Goetz Lindenmaier <goetz@openjdk.org>
2024-09-10 05:51:50 +0000

    8274505: Too weak variable type leads to unnecessary cast in java.desktop
    Backport-of: 9ca6bf0d22ac39d4ee29c756903038f09087ca12

--- src/java.desktop/share/classes/javax/swing/text/html/HTMLDocument.java
+++ src/java.desktop/share/classes/javax/swing/text/html/HTMLDocument.java
@@ -4225,7 +4225,7 @@ public class HTMLDocument extends DefaultStyledDocument {
                 try {
                     if (offset == 0 || !getText(offset - 1, 1).equals("\n")) {
                         // Need to insert a newline.
-                        AttributeSet newAttrs = null;
+                        SimpleAttributeSet newAttrs = null;
                         boolean joinP = true;
 
                         if (offset != 0) {
@@ -4259,9 +4259,8 @@ public class HTMLDocument extends DefaultStyledDocument {
                             // sure and set the name (otherwise it will be
                             // inherited).
                             newAttrs = new SimpleAttributeSet();
-                            ((SimpleAttributeSet)newAttrs).addAttribute
-                                              (StyleConstants.NameAttribute,
-                                               HTML.Tag.CONTENT);
+                            newAttrs.addAttribute(StyleConstants.NameAttribute,
+                                                  HTML.Tag.CONTENT);
                         }
                         ElementSpec es = new ElementSpec(newAttrs,
                                      ElementSpec.ContentType, NEWLINE, 0,
