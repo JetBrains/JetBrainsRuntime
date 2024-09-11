0e7141b6fc995a1cb3e6d9515a871f416585ecc8
bourgesl <bourges.laurent@gmail.com>
2024-09-11 22:23:25 +0200

    JBR-7616: added ThreadUtilities.lwc_plog(env, formatMsg, ...) to use LWCToolkit's PlatformLogger instead of NSlog (only as fallback now) used by MTLRenderQueue, updated MTLUtils to share mtlDstTypeToStr(op)

--- src/java.desktop/macosx/native/libosxapp/ThreadUtilities.h
+++ src/java.desktop/macosx/native/libosxapp/ThreadUtilities.h
@@ -149,4 +149,7 @@ __attribute__((visibility("default")))
 
 JNIEXPORT void OSXAPP_SetJavaVM(JavaVM *vm);
 
+/* LWCToolkit's PlatformLogger wrapper */
+JNIEXPORT void lwc_plog(JNIEnv* env, const char *formatMsg, ...);
+
 #endif /* __THREADUTILITIES_H */
