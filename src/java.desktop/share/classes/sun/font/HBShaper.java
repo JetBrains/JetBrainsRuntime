59b62773dfc6fa7b7fb47609d68f56b03ebe01e9
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2024-09-16 18:13:49 +0200

    JBR-7556 Check negative glyphID in HBShaper
    (cherry picked from commit 76852278f403f612caeafe3f8a2063e310402cd7)

--- src/java.desktop/share/classes/sun/font/HBShaper.java
+++ src/java.desktop/share/classes/sun/font/HBShaper.java
@@ -343,6 +343,7 @@ public class HBShaper {
 
         Font2D font2D = scopedVars.get().font();
         int glyphID = font2D.charToGlyphRaw(unicode);
+        if (glyphID < 0) glyphID = 0;
         @SuppressWarnings("restricted")
         MemorySegment glyphIDPtr = glyph.reinterpret(4);
         glyphIDPtr.setAtIndex(JAVA_INT, 0, glyphID);
@@ -359,6 +360,7 @@ public class HBShaper {
     ) {
         Font2D font2D = scopedVars.get().font();
         int glyphID = font2D.charToVariationGlyphRaw(unicode, variation_selector);
+        if (glyphID < 0) glyphID = 0;
         @SuppressWarnings("restricted")
         MemorySegment glyphIDPtr = glyph.reinterpret(4);
         glyphIDPtr.setAtIndex(JAVA_INT, 0, glyphID);
