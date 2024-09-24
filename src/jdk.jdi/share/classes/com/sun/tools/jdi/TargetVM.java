d739bac5c7ef1c0cebe0cf1912450d53e28703c6
Goetz Lindenmaier <goetz@openjdk.org>
2024-09-24 08:01:15 +0000

    8339384: Unintentional IOException in jdk.jdi module when JDWP end of stream occurs
    Backport-of: 2305d18e8d53dbbf341b580b60f9ed21f408bff1

--- src/jdk.jdi/share/classes/com/sun/tools/jdi/TargetVM.java
+++ src/jdk.jdi/share/classes/com/sun/tools/jdi/TargetVM.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -124,8 +124,9 @@ public class TargetVM implements Runnable {
                 byte b[] = connection.readPacket();
                 if (b.length == 0) {
                     done = true;
+                } else {
+                    p = Packet.fromByteArray(b);
                 }
-                p = Packet.fromByteArray(b);
             } catch (IOException e) {
                 done = true;
             }
