61fb429cf0cf69cd5bd6b64762b34e1b946d0cf4
Andrew Lu <andrewlu@openjdk.org>
2024-09-25 01:58:39 +0000

    8327401: Some jtreg tests fail on Wayland without any tracking bug 8312111: open/test/jdk/java/awt/Robot/ModifierRobotKey/ModifierRobotKeyTest.java fails on ubuntu 23.04
    Reviewed-by: goetz
    Backport-of: 286cc163fd4f8361a633dcb6494965020d02b506

--- test/jdk/java/awt/Robot/ModifierRobotKey/ModifierRobotKeyTest.java
+++ test/jdk/java/awt/Robot/ModifierRobotKey/ModifierRobotKeyTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -88,7 +88,7 @@ public class ModifierRobotKeyTest extends KeyAdapter {
 
         String os = System.getProperty("os.name").toLowerCase();
 
-        if (os.contains("os x"))
+        if (os.contains("os x") || os.contains("linux"))
             textKeys[1] = KeyEvent.VK_K;
         else
             textKeys[1] = KeyEvent.VK_I;
