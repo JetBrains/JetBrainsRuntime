f0a17ace7a51cdbb73870c1db1f6bbf0cd0ae528
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2024-09-26 22:10:11 +0200

    JBR-7673 Cleanup docker scripts
    (cherry picked from commit 473686a92efe4f1c7ed2f5900acf5a9444260204)

--- jb/project/docker/mkdocker_musl_x64.sh
+++ jb/project/docker/mkdocker_musl_x64.sh
@@ -1,28 +1,10 @@
 #!/bin/bash
 
-set -euo pipefail
-set -x
+set -euxo pipefail
 
-# This script creates a Docker image suitable for building musl-x64 variant
-# of the JetBrains Runtime version 21.
+# This script creates a Docker image suitable for building musl x64 variant
 
-BOOT_JDK_REMOTE_FILE=zulu20.32.11-ca-jdk20.0.2-linux_musl_x64.tar.gz
-BOOT_JDK_SHA=fca5081dd6da847fcd06f5b755e58edae22d6784f21b81bf73da2b538f842c07
-BOOT_JDK_LOCAL_FILE=boot_jdk_musl_amd64.tar.gz
-
-if [ ! -f $BOOT_JDK_LOCAL_FILE ]; then
-    # Obtain "boot JDK" from outside of the container.
-    wget -nc https://cdn.azul.com/zulu/bin/${BOOT_JDK_REMOTE_FILE} -O $BOOT_JDK_LOCAL_FILE
-else
-    echo "boot JDK \"$BOOT_JDK_LOCAL_FILE\" present, skipping download"
-fi
-
-# Verify that what we've downloaded can be trusted.
-sha256sum -c - <<EOF
-$BOOT_JDK_SHA *$BOOT_JDK_LOCAL_FILE
-EOF
-
-docker build -t jetbrains/runtime:jbr21env_musl_x64 -f Dockerfile.musl_x64 .
+docker build --platform=linux/amd64 -t jetbrains/runtime:alpine14_x64 -f Dockerfile.alpine .
 
 # NB: the resulting container can (and should) be used without the network
 # connection (--network none) during build in order to reduce the chance
