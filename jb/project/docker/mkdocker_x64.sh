055d15b96b4460516f154b663f919592acdce68a
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2024-09-26 22:10:11 +0200

    JBR-7673 Cleanup docker scripts
    (cherry picked from commit 473686a92efe4f1c7ed2f5900acf5a9444260204)

--- jb/project/docker/mkdocker_x64.sh
+++ jb/project/docker/mkdocker_x64.sh
@@ -2,9 +2,9 @@
 
 set -euxo pipefail
 
-# This script creates a Docker image suitable for building musl AArch64 variant
+# This script creates a Docker image suitable for building x64 variant
 
-docker build --platform=linux/aarch64 -t jetbrains/runtime:alpine14_aarch64 -f Dockerfile.alpine .
+docker build --platform=linux/amd64 -t jetbrains/runtime:oraclelinux8_x64 -f Dockerfile.oraclelinux .
 
 # NB: the resulting container can (and should) be used without the network
 # connection (--network none) during build in order to reduce the chance
