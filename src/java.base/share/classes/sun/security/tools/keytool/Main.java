1c87938a567f8e67438ff60bacd6cc49302aa897
Goetz Lindenmaier <goetz@openjdk.org>
2024-10-01 10:47:58 +0000

    8331391: Enhance the keytool code by invoking the buildTrustedCerts method for essential options
    Backport-of: c9bee173d61f4accfc4adc280ab5d21600191756

--- src/java.base/share/classes/sun/security/tools/keytool/Main.java
+++ src/java.base/share/classes/sun/security/tools/keytool/Main.java
@@ -1125,7 +1125,6 @@ public final class Main {
             }
         }
 
-        KeyStore cakstore = buildTrustedCerts();
         // -trustcacerts can be specified on -importcert, -printcert or -printcrl.
         // Reset it so that warnings on CA cert will remain for other command.
         if (command != IMPORTCERT && command != PRINTCERT
@@ -1134,6 +1133,7 @@ public final class Main {
         }
 
         if (trustcacerts) {
+            KeyStore cakstore = buildTrustedCerts();
             if (cakstore != null) {
                 caks = cakstore;
             } else {
