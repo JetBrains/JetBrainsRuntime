03e1b3783dc646e45fb6f2912c6aa34e9b8d032b
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2024-10-09 14:00:30 +0200

    JBR-7579 Fix SurfaceManager.cacheMap retaining strong references.
    (cherry picked from commit b8434796aee56626b60b5fcf27c9cbb74ebff6cb)

--- src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLRenderQueue.m
+++ src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLRenderQueue.m
@@ -691,10 +691,13 @@ Java_sun_java2d_metal_MTLRenderQueue_flushBuffer
                     jlong pData = NEXT_LONG(b);
                     BMTLSDOps *mtlsdo = (BMTLSDOps *)jlong_to_ptr(pData);
                     if (mtlsdo != NULL) {
-                        CONTINUE_IF_NULL(mtlc);
-                        [mtlc.glyphCacheAA free];
-                        [mtlc.glyphCacheLCD free];
-                        [mtlc commitCommandBuffer:YES display:NO];
+                        if (mtlc != NULL) {
+                            [mtlc.glyphCacheAA free];
+                            [mtlc.glyphCacheLCD free];
+                            [mtlc commitCommandBuffer:YES display:NO];
+                            mtlc = NULL;
+                        }
+                        dstOps = NULL;
                         MTLSD_Delete(env, mtlsdo);
                     }
                     break;
@@ -705,7 +708,13 @@ Java_sun_java2d_metal_MTLRenderQueue_flushBuffer
                     jlong pData = NEXT_LONG(b);
                     BMTLSDOps *mtlsdo = (BMTLSDOps *)jlong_to_ptr(pData);
                     if (mtlsdo != NULL) {
-                        CONTINUE_IF_NULL(mtlc);
+                        if (mtlc != NULL) {
+                            [mtlc.glyphCacheAA free];
+                            [mtlc.glyphCacheLCD free];
+                            [mtlc commitCommandBuffer:YES display:NO];
+                            mtlc = NULL;
+                        }
+                        dstOps = NULL;
                         MTLSD_Delete(env, mtlsdo);
                         if (mtlsdo->privOps != NULL) {
                             free(mtlsdo->privOps);
