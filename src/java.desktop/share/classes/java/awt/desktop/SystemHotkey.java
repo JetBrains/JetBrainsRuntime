98cc27168cd232f35ce4cc7c1d1c0b2ed4e721da
Nikita Tsarev <nikita.tsarev@jetbrains.com>
2024-10-10 14:20:45 +0200

    JBR-6324: JBR API for System Shortcuts (macOS)
    (cherry picked from commit 22d406a76b15a8fd845cd8d1b2b578bd87751730)

--- src/java.desktop/share/classes/java/awt/desktop/SystemHotkey.java
+++ src/java.desktop/share/classes/java/awt/desktop/SystemHotkey.java
@@ -14,26 +14,42 @@ import java.awt.event.KeyEvent;
 import java.awt.event.InputEvent;
 
 /**
- * Provides info about system hotkeys
+ * Provides info about system hotkeys.
+ * Deprecated, use JBR SystemShortcuts API instead.
  */
 public class SystemHotkey extends AWTKeyStroke {
-    private static final long serialVersionUID = -6593119259058157651L;
+    private static final long serialVersionUID = 4340163519946285280L;
     private static final PlatformLogger ourLog = PlatformLogger.getLogger(java.awt.desktop.SystemHotkey.class.getName());
     private static final Map<Integer, String> ourCodeDescriptionCache = new HashMap<>();
+    private static volatile Runnable changeEventHandler = null;
 
     private final int myNativeKeyCode;
+    private final String myId;
     private final String myDescription;
     
     SystemHotkey(char keyChar, int javaKeyCode, int javaModifiers, String description, int nativeKeyCode) {
-      super(keyChar, javaKeyCode, javaModifiers, true);
-      this.myNativeKeyCode = nativeKeyCode;
-      this.myDescription = description;
+        this(keyChar, javaKeyCode, javaModifiers, description, nativeKeyCode, description);
+    }
+
+    SystemHotkey(char keyChar, int javaKeyCode, int javaModifiers, String description, int nativeKeyCode, String id) {
+        super(keyChar, javaKeyCode, javaModifiers, true);
+        this.myNativeKeyCode = nativeKeyCode;
+        this.myId = id;
+        this.myDescription = description;
     }
     
     public String toString() {
-      return String.format("desc='%s' char=%s mod='%s' nativeKeyCode=0x%X ['%s'] javaKeyCode=0x%X",
-              String.valueOf(myDescription), String.valueOf(getKeyChar()), InputEvent.getModifiersExText(getModifiers()),
+      return String.format("id='%s' desc='%s' char=%s mod='%s' nativeKeyCode=0x%X ['%s'] javaKeyCode=0x%X",
+              String.valueOf(myId), String.valueOf(myDescription), String.valueOf(getKeyChar()), InputEvent.getModifiersExText(getModifiers()),
               myNativeKeyCode, getOsxKeyCodeDescription(myNativeKeyCode), getKeyCode());
+    }
+
+    /**
+     * Gets unique hotkey identifier
+     * @return hotkey identifier
+     */
+    public String getId() {
+        return myId;
     }
 
     /**
@@ -67,16 +83,32 @@ public class SystemHotkey extends AWTKeyStroke {
     }
 
     private static native String osxKeyCodeDescription(int osxCode);
+
+    /**
+     * Set the event handler, which is fired when the user changes a system shortcut.
+     * @param changeEventHandler event handler
+     */
+    public static void setChangeEventHandler(Runnable changeEventHandler) {
+        SystemHotkey.changeEventHandler = changeEventHandler; // store to volatile
+    }
+
+    private static void onChange() {
+        // called from native code from any thread
+        var handler = changeEventHandler; // load from volatile
+        if (handler != null) {
+            EventQueue.invokeLater(handler);
+        }
+    }
 }
 
 class SystemHotkeyReader {
     private final List<SystemHotkey> myResult = new ArrayList<>();
 
-    void add(int keyCode, String keyChar, int modifiers, String desc) {
+    void add(int keyCode, String keyChar, int modifiers, String desc, String id) {
         myResult.add(new SystemHotkey(
                 keyChar == null || keyChar.isEmpty() ? KeyEvent.CHAR_UNDEFINED : keyChar.charAt(0),
                 keyCode == -1 ? KeyEvent.VK_UNDEFINED : osx2java(keyCode),
-                modifiers, desc, keyCode
+                modifiers, desc, keyCode, (id == null) ? desc : id
         ));
     }
 
