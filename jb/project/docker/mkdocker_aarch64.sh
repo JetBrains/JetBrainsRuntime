45664280f49070eca66e99db441fdece67c95c9b
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2024-10-11 13:12:53 +0400

    JBR-7663 Wayland: make gtk-shell1 protocol support optional
    (cherry picked from commit 30e6c720b67631cc2650d8f0d56ada01e6562774)

--- jb/project/docker/mkdocker_aarch64.sh
+++ jb/project/docker/mkdocker_aarch64.sh
@@ -2,6 +2,7 @@
 
 set -euxo pipefail
 
+wget https://raw.githubusercontent.com/GNOME/gtk/refs/heads/main/gdk/wayland/protocol/gtk-shell.xml
 # This script creates a Docker image suitable for building AArch64 variant
 
 docker build --platform=linux/aarch64 -t jetbrains/runtime:oraclelinux8_aarch64 -f Dockerfile.oraclelinux .
--- jb/project/docker/mkdocker_x64.sh
+++ jb/project/docker/mkdocker_x64.sh
@@ -2,6 +2,7 @@
 
 set -euxo pipefail
 
+wget https://raw.githubusercontent.com/GNOME/gtk/refs/heads/main/gdk/wayland/protocol/gtk-shell.xml
 # This script creates a Docker image suitable for building x64 variant
 
 docker build --platform=linux/amd64 -t jetbrains/runtime:oraclelinux8_x64 -f Dockerfile.oraclelinux .
