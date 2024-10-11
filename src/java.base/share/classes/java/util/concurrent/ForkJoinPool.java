dfcd8d2eecfe62e91070244d8eb10906dc3c812d
Christoph Langer <clanger@openjdk.org>
2024-10-11 21:37:47 +0200

    8341989: [21u] Back out JDK-8327501 and JDK-8328366
    Reviewed-by: goetz

--- src/java.base/share/classes/java/util/concurrent/ForkJoinPool.java
+++ src/java.base/share/classes/java/util/concurrent/ForkJoinPool.java
@@ -981,7 +981,9 @@ public class ForkJoinPool extends AbstractExecutorService {
             boolean isCommon = (pool.workerNamePrefix == null);
             @SuppressWarnings("removal")
             SecurityManager sm = System.getSecurityManager();
-            if (sm != null && isCommon)
+            if (sm == null)
+                return new ForkJoinWorkerThread(null, pool, true, false);
+            else if (isCommon)
                 return newCommonWithACC(pool);
             else
                 return newRegularWithACC(pool);
