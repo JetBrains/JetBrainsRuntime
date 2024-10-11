acce8f9a93e052d8070716c3f69a4e1c8183533e
Goetz Lindenmaier <goetz@openjdk.org>
2024-10-11 07:51:11 +0000

    8334719: (se) Deferred close of SelectableChannel may result in a Selector doing the final close before concurrent I/O on channel has completed
    Reviewed-by: lucy
    Backport-of: 9bb675f89dd1eeec423ca96cb3f96d29f5de477c

--- src/java.base/share/classes/sun/nio/ch/ServerSocketChannelImpl.java
+++ src/java.base/share/classes/sun/nio/ch/ServerSocketChannelImpl.java
@@ -653,6 +653,9 @@ class ServerSocketChannelImpl
 
     @Override
     public void kill() {
+        // wait for any accept operation to complete before trying to close
+        acceptLock.lock();
+        acceptLock.unlock();
         synchronized (stateLock) {
             if (state == ST_CLOSING) {
                 tryFinishClose();
