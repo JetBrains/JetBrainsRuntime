a072a4dd242b5aad990259c249529132dd033195
Christoph Langer <clanger@openjdk.org>
2024-10-11 21:37:47 +0200

    8341989: [21u] Back out JDK-8327501 and JDK-8328366
    Reviewed-by: goetz

--- test/jdk/java/util/concurrent/tck/ForkJoinPool9Test.java
+++ test/jdk/java/util/concurrent/tck/ForkJoinPool9Test.java
@@ -79,9 +79,6 @@ public class ForkJoinPool9Test extends JSR166TestCase {
             assertSame(ForkJoinPool.commonPool(), ForkJoinTask.getPool());
             Thread currentThread = Thread.currentThread();
 
-            ClassLoader preexistingContextClassLoader =
-                    currentThread.getContextClassLoader();
-
             Stream.of(systemClassLoader, null).forEach(cl -> {
                 if (randomBoolean())
                     // should always be permitted, without effect
@@ -98,11 +95,6 @@ public class ForkJoinPool9Test extends JSR166TestCase {
                     () -> System.getProperty("foo"),
                     () -> currentThread.setContextClassLoader(
                         classLoaderDistinctFromSystemClassLoader));
-            else {
-                currentThread.setContextClassLoader(classLoaderDistinctFromSystemClassLoader);
-                assertSame(currentThread.getContextClassLoader(), classLoaderDistinctFromSystemClassLoader);
-                currentThread.setContextClassLoader(preexistingContextClassLoader);
-            }
             // TODO ?
 //          if (haveSecurityManager
 //              && Thread.currentThread().getClass().getSimpleName()
