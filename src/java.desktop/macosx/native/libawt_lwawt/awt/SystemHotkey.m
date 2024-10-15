34718d09194a8b4934823086850c8464d6e041c0
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2024-10-15 13:06:53 +0400

    JBR-7748 java/awt/font/JNICheck/FreeTypeScalerJNICheck.java: JNI call made without checking exceptions when required to from CallVoidMethod

--- src/java.desktop/macosx/native/libawt_lwawt/awt/SystemHotkey.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/SystemHotkey.m
@@ -85,6 +85,7 @@ void plog(int logLevel, const char *formatMsg, ...) {
 
     (*env)->CallVoidMethod(env, loggerObject, mid, jstr);
     (*env)->DeleteLocalRef(env, jstr);
+    CHECK_EXCEPTION();
 }
 
 static const char * toCString(id obj) {
@@ -662,6 +663,7 @@ JNIEXPORT void JNICALL Java_java_awt_desktop_SystemHotkeyReader_readSystemHotkey
             (*env)->CallVoidMethod(
                     env, reader, methodAdd, (jint)vkeyCode, jkeyChar, (jint)jmodifiers, jdesc, jid
             );
+            CHECK_EXCEPTION();
         }
     );
 }
