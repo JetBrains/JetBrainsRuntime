4f8249d30e4ef6d3e87833423498a8ed0e19d78b
Nikita Gubarkov <Nikita.Gubarkov@jetbrains.com>
2024-10-18 00:07:05 +0200

    JBR-4725 File dialog modality
    (cherry picked from commit 2a5b044c9587a9f087db30984efab1724f14d8b4)

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CFileDialog.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CFileDialog.java
@@ -99,6 +99,7 @@ class CFileDialog implements FileDialogPeer {
                         ownerPtr,
                         title,
                         dialogMode,
+                        target.getModalityType().ordinal(),
                         target.isMultipleMode(),
                         navigateApps,
                         chooseDirectories,
@@ -196,7 +197,8 @@ class CFileDialog implements FileDialogPeer {
         return ret;
     }
 
-    private native String[] nativeRunFileDialog(long ownerPtr, String title, int mode,
+    private native String[] nativeRunFileDialog(
+            long ownerPtr, String title, int mode, int modality,
             boolean multipleMode, boolean shouldNavigateApps,
             boolean canChooseDirectories, boolean canChooseFiles,
             boolean canCreateDirectories, boolean hasFilenameFilter, String[] allowedFileTypes,
