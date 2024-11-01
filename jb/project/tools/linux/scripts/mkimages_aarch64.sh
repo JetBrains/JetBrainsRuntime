7d859509c0595cc93929a6a2f16b19b75028c8f0
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2024-11-01 16:09:34 +0400

    JBR-6144 enable building JBR with Vulkan
    (cherry picked from commit 9fe7b136598e2679dd6f4b12f959c6db0ef9bf99)

--- jb/project/tools/linux/scripts/mkimages_aarch64.sh
+++ jb/project/tools/linux/scripts/mkimages_aarch64.sh
@@ -35,6 +35,7 @@ function do_configure {
     --with-version-opt=b"$build_number" \
     --with-boot-jdk="$BOOT_JDK" \
     --enable-cds=yes \
+    --with-vulkan \
     $DISABLE_WARNINGS_AS_ERRORS \
     $STATIC_CONF_ARGS \
     $REPRODUCIBLE_BUILD_OPTS \
