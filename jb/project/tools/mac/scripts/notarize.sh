53493a1d848f6ee7fd748d95fc9b8a53556a51e2
sergey.shelomentsev <sergey.shelomentsev@jetbrains.com>
2024-11-03 16:58:45 +0200

    JBR-7800 Fix notarization of jbrsdk (sign libs and execs inside jmod files)
    (cherry picked from commit dbb42d10f531dda0fb75a500565f9f510ea2bf55)

--- jb/project/tools/mac/scripts/notarize.sh
+++ jb/project/tools/mac/scripts/notarize.sh
@@ -30,11 +30,34 @@ trap "rm -f \"$PWD/tmp_key\"" INT EXIT RETURN
 echo -n "${APPLE_PRIVATE_KEY}" > tmp_key
 
 log "Notarizing $APP_PATH..."
-xcrun notarytool submit --key tmp_key --key-id "${APPLE_KEY_ID}" --issuer "${APPLE_ISSUER_ID}" "$APP_PATH" 2>&1 | tee "notarytool.submit.out"
+xcrun notarytool submit --key tmp_key --key-id "${APPLE_KEY_ID}" --issuer "${APPLE_ISSUER_ID}" "$APP_PATH" 2>&1 --wait| tee "notarytool.submit.out"
 REQUEST_ID="$(grep -e " id: " "notarytool.submit.out" | grep -oE '([0-9a-f-]{36})'| head -n1)"
 
-xcrun notarytool wait "$REQUEST_ID" --key tmp_key --key-id "${APPLE_KEY_ID}" --issuer "${APPLE_ISSUER_ID}" --timeout 6h ||:
-xcrun notarytool log  "$REQUEST_ID" --key tmp_key --key-id "${APPLE_KEY_ID}" --issuer "${APPLE_ISSUER_ID}" developer_log.json ||:
-xcrun notarytool info "$REQUEST_ID" --key tmp_key --key-id "${APPLE_KEY_ID}" --issuer "${APPLE_ISSUER_ID}"
+waitOutput=$(xcrun notarytool wait "$REQUEST_ID" --key tmp_key --key-id "${APPLE_KEY_ID}" --issuer "${APPLE_ISSUER_ID}" --timeout 6h)
+if [ $? -ne 0 ]; then
+  log "Notarizing failed (wait command)"
+  echo "$waitOutput"
+  exit 1
+else
+  echo "$waitOutput"
+fi
+
+logOutout=$(xcrun notarytool log  "$REQUEST_ID" --key tmp_key --key-id "${APPLE_KEY_ID}" --issuer "${APPLE_ISSUER_ID}" developer_log.json)
+if [ $? -ne 0 ]; then
+  log "Notarizing failed (log command)"
+  echo "$logOutout"
+  exit 1
+else
+  echo "$logOutout"
+fi
+
+infoOUtput=$(xcrun notarytool info "$REQUEST_ID" --key tmp_key --key-id "${APPLE_KEY_ID}" --issuer "${APPLE_ISSUER_ID}")
+if [ $? -ne 0 ]; then
+  log "Notarizing failed (info command)"
+  echo "$infoOUtput"
+  exit 1
+else
+  echo "$infoOUtput"
+fi
 
 log "Notarizing finished"
