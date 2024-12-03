52c2f8f2a4d6e7f2521863866b53aab85743b493
Francisco Ferrari Bihurriet <fferrari@openjdk.org>
2024-12-03 19:05:42 +0000

    8342905: Thread.setContextClassloader from thread in FJP commonPool task no longer works after JDK-8327501 redux
    Reviewed-by: sgehwolf, mbalao
    Backport-of: 872ae1347198408ffd62559ca8c1a7420c4a8108

--- src/java.base/share/classes/java/lang/System.java
+++ src/java.base/share/classes/java/lang/System.java
@@ -2684,6 +2684,11 @@ public final class System {
             public String getLoaderNameID(ClassLoader loader) {
                 return loader != null ? loader.nameAndId() : "null";
             }
+
+            @Override
+            public boolean allowSecurityManager() {
+                return System.allowSecurityManager();
+            }
         });
     }
 
