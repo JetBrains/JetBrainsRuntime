c85be82b077ffed37cd8272742413b8f8e6d0e9f
bourgesl <bourges.laurent@gmail.com>
2024-12-07 00:36:58 +0100

    JBR-5497: simple fix to avoid deadlocks on macOS + mirroring displays: - added more instrumentation on awt threads and AWTThreading.invokeAndWait() to detect and diagnose deadlocks - enhanced awtLockListener to adopt nanotime for time accuracy + more advanced logging (caller, wait time) with histograms - major ThreadUtilities performOnMainThread refactoring to observe thread coordination (state, callstacks) in order to avoid deadlocks while the appkit/main thread is waiting (performOnMainThreadWaiting:YES) in conflict with LWCToolkit.invokeLater() blocked in doAWTRunLoop (2nd) - fixed CPlatformWindow.flushBuffers() to use LWCToolkit.invokeLater() to avoid deadlocks (EDT <-> main) if the the system property '-Dawt.mac.flushBuffers.invokeLater=true' - use the new CGraphicsDevice.IsMirroring() to enable fix in CPlatformWindow.flushBuffers() too - removed changes for JBR-5461 + cleanup + simplified awtLock() + keep few more invokeLater() when computer returns from sleep or displayChanged() to avoid deadlocks until solved definitely - fixed review comments

--- src/java.desktop/share/classes/sun/awt/AWTThreading.java
+++ src/java.desktop/share/classes/sun/awt/AWTThreading.java
@@ -27,7 +27,10 @@ import java.util.function.Consumer;
  * Note: the log messages are tested by jdk/jb/java/awt/Toolkit/LWCToolkitInvokeAndWaitTest.java
  */
 public class AWTThreading {
+
     private static final PlatformLogger logger = PlatformLogger.getLogger(AWTThreading.class.getName());
+
+    private static final boolean TRACE_RUN_LOOP = false;
 
     private static final Runnable EMPTY_RUNNABLE = () -> {};
 
@@ -122,7 +125,7 @@ public class AWTThreading {
         try {
             return callable.call();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.severe("AWTThreading.executeWaitToolkit: failure", e);
         }
         return null;
     }
@@ -157,6 +160,8 @@ public class AWTThreading {
                 }
             }
 
+            if (TRACE_RUN_LOOP) logger.info("AWTThreading.execute: callable " + callable);
+
             FutureTask<T> task = new FutureTask<>(callable) {
                 @Override
                 protected void done() {
@@ -171,6 +176,8 @@ public class AWTThreading {
 
             try {
                 while (!task.isDone() || !currentQueue.isEmpty()) {
+                    if (TRACE_RUN_LOOP) logger.info("AWTThreading.execute: poll event = will be WAITING for : " + timeout + " " + unit);
+
                     InvocationEvent event;
                     if (timeout >= 0 && unit != null) {
                         event = currentQueue.poll(timeout, unit);
@@ -182,15 +189,17 @@ public class AWTThreading {
                         synchronized (invocations) {
                             invocations.remove(currentQueue);
                         }
-                        new RuntimeException("Waiting for the invocation event timed out").printStackTrace();
+                        logger.warning("AWTThreading.execute: ",
+                                new RuntimeException("Waiting for the invocation event timed out"));
                         break;
                     }
+                    if (TRACE_RUN_LOOP) logger.info("AWTThreading.execute: dispatch event: " + event);
                     event.dispatch();
                 }
                 return task.isCancelled() ? null : task.get();
 
             } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
+                logger.severe("AWTThreading.execute: failure", e);
             }
         } finally {
             level--;
@@ -246,7 +255,7 @@ public class AWTThreading {
                 return eventRef.get();
             }
         }
-        return TrackedInvocationEvent.create(source, onDispatched, () -> {}, catchThrowables);
+        return TrackedInvocationEvent.create(source, onDispatched, EMPTY_RUNNABLE, catchThrowables);
     }
 
     @SuppressWarnings("serial")
@@ -292,6 +301,8 @@ public class AWTThreading {
                   catchThrowables);
 
             futureResult.whenComplete((r, ex) -> {
+                if (TRACE_RUN_LOOP) logger.info("TrackedInvocationEvent.whenComplete: awaiting " + (System.currentTimeMillis() - creationTime) + " ms)");
+
                 if (ex != null) {
                     String message = ex.getMessage() + " (awaiting " + (System.currentTimeMillis() - creationTime) + " ms)";
                     if (logger.isLoggable(PlatformLogger.Level.FINE)) {
@@ -300,6 +311,9 @@ public class AWTThreading {
                     } else if (logger.isLoggable(PlatformLogger.Level.INFO)) {
                         StackTraceElement[] stack = throwable.getStackTrace();
                         logger.info(message + ". Originated at " + stack[stack.length - 1]);
+                    } else {
+                        logger.severe("AWTThreading.TrackedInvocationEvent.whenComplete: failure", ex);
+                        logger.severe("AWTThreading.TrackedInvocationEvent.whenComplete: caller:", throwable);
                     }
                 }
             });
