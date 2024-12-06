cc647987c21a0c5b6dcbe5ecaef5f227ae69b57d
bourgesl <bourges.laurent@gmail.com>
2024-12-07 00:36:58 +0100

    JBR-5497: simple fix to avoid deadlocks on macOS + mirroring displays: - added more instrumentation on awt threads and AWTThreading.invokeAndWait() to detect and diagnose deadlocks - enhanced awtLockListener to adopt nanotime for time accuracy + more advanced logging (caller, wait time) with histograms - major ThreadUtilities performOnMainThread refactoring to observe thread coordination (state, callstacks) in order to avoid deadlocks while the appkit/main thread is waiting (performOnMainThreadWaiting:YES) in conflict with LWCToolkit.invokeLater() blocked in doAWTRunLoop (2nd) - fixed CPlatformWindow.flushBuffers() to use LWCToolkit.invokeLater() to avoid deadlocks (EDT <-> main) if the the system property '-Dawt.mac.flushBuffers.invokeLater=true' - use the new CGraphicsDevice.IsMirroring() to enable fix in CPlatformWindow.flushBuffers() too - removed changes for JBR-5461 + cleanup + simplified awtLock() + keep few more invokeLater() when computer returns from sleep or displayChanged() to avoid deadlocks until solved definitely - fixed review comments

--- src/java.desktop/macosx/classes/sun/lwawt/LWToolkit.java
+++ src/java.desktop/macosx/classes/sun/lwawt/LWToolkit.java
@@ -29,7 +29,6 @@ import java.awt.*;
 import java.awt.List;
 import java.awt.datatransfer.*;
 import java.awt.dnd.DropTarget;
-import java.awt.image.*;
 import java.awt.peer.*;
 import java.security.*;
 import java.util.*;
@@ -41,7 +40,12 @@ import sun.awt.util.ThreadGroupUtils;
 
 import static sun.lwawt.LWWindowPeer.PeerType;
 
+import sun.util.logging.PlatformLogger;
+
+
 public abstract class LWToolkit extends SunToolkit implements Runnable {
+
+    private static final PlatformLogger log = PlatformLogger.getLogger(LWToolkit.class.getName());
 
     private static final int STATE_NONE = 0;
     private static final int STATE_INIT = 1;
@@ -147,8 +151,8 @@ public abstract class LWToolkit extends SunToolkit implements Runnable {
                 synchronized (this) {
                     wait();
                 }
-            } catch (InterruptedException z) {
-                // TODO: log
+            } catch (InterruptedException ie) {
+                log.fine("LWToolkit.run: interrupted");
                 break;
             }
         }
@@ -168,10 +172,8 @@ public abstract class LWToolkit extends SunToolkit implements Runnable {
                         break;
                     }
                 }
-            } catch (Throwable t) {
-                // TODO: log
-                System.err.println("Exception on the toolkit thread");
-                t.printStackTrace(System.err);
+            } catch (Throwable th) {
+                log.severe("LWToolkit.run: failure", th);
             }
         }
         //XXX: if that's a secondary loop, jump back to the STATE_MESSAGELOOP
