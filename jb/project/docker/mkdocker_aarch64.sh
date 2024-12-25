437918b13833507d3b8c31f10f233332530e1f6d
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2024-12-25 18:18:07 +0400

    JBR-8072 move JBR docker images to registry.jetbrains.team

--- jb/project/docker/mkdocker_aarch64.sh
+++ jb/project/docker/mkdocker_aarch64.sh
@@ -4,7 +4,10 @@ set -euxo pipefail
 
 # This script creates a Docker image suitable for building AArch64 variant
 
-docker build --platform=linux/aarch64 -t jetbrains/runtime:oraclelinux7_aarch64 -f Dockerfile.oraclelinux.aarch64 .
+docker build \
+  --platform=linux/aarch64 \
+  -t registry.jetbrains.team/p/jbre/containers/oraclelinux7_aarch64:latest \
+  -f Dockerfile.oraclelinux.aarch64 .
 
 # NB: the resulting container can (and should) be used without the network
 # connection (--network none) during build in order to reduce the chance
