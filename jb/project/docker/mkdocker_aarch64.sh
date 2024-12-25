58689f10641401108661e8a4d251ba5180c44cc5
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2024-12-25 18:26:33 +0400

    JBR-8071 enable OL7 extended support for docker images

--- jb/project/docker/mkdocker_aarch64.sh
+++ jb/project/docker/mkdocker_aarch64.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
 # This script creates a Docker image suitable for building AArch64 variant
+
+echo "ULN username:";
+read username;
+
+echo "ULN password:";
+read -s password;
+
+echo "CSI:";
+read csi;
 
 docker build \
   --platform=linux/aarch64 \
   -t registry.jetbrains.team/p/jbre/containers/oraclelinux7_aarch64:latest \
+  --build-arg profilename=OL7_aarch64 \
+  --build-arg username=$username \
+  --build-arg password=$password \
+  --build-arg csi=$csi \
   -f Dockerfile.oraclelinux.aarch64 .
 
 # NB: the resulting container can (and should) be used without the network
