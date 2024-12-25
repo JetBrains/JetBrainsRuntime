724031c6c710548927976ce9fd57e2231b400c80
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2024-12-25 04:21:36 +0400

    JBR-8072 move JBR docker images to registry.jetbrains.team

--- jb/project/docker/mkdocker_musl_x64.sh
+++ jb/project/docker/mkdocker_musl_x64.sh
@@ -5,7 +5,11 @@ set -euxo pipefail
 wget https://raw.githubusercontent.com/GNOME/gtk/refs/heads/main/gdk/wayland/protocol/gtk-shell.xml
 # This script creates a Docker image suitable for building musl x64 variant
 
-docker build --platform=linux/amd64 -t jetbrains/runtime:alpine14_x64 -f Dockerfile.alpine .
+docker build \
+  --platform=linux/amd64 \
+  -t registry.jetbrains.team/p/jbre/containers/alpine14_x64:latest \
+  --no-cache \
+  -f Dockerfile.alpine .
 
 # NB: the resulting container can (and should) be used without the network
 # connection (--network none) during build in order to reduce the chance
