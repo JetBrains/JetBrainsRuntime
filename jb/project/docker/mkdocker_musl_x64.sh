0af14737a1a11d237d4153160ab350e46eba5037
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2024-12-25 18:18:07 +0400

    JBR-8072 move JBR docker images to registry.jetbrains.team

--- jb/project/docker/mkdocker_musl_x64.sh
+++ jb/project/docker/mkdocker_musl_x64.sh
@@ -4,7 +4,10 @@ set -euxo pipefail
 
 # This script creates a Docker image suitable for building musl x64 variant
 
-docker build --platform=linux/amd64 -t jetbrains/runtime:alpine14_x64 -f Dockerfile.alpine .
+docker build \
+  --platform=linux/amd64 \
+  -t registry.jetbrains.team/p/jbre/containers/alpine14_x64:latest \
+  -f Dockerfile.alpine .
 
 # NB: the resulting container can (and should) be used without the network
 # connection (--network none) during build in order to reduce the chance
--- jb/project/docker/mkdocker_x64.sh
+++ jb/project/docker/mkdocker_x64.sh
@@ -4,7 +4,10 @@ set -euxo pipefail
 
 # This script creates a Docker image suitable for building x64 variant
 
-docker build --platform=linux/amd64 -t jetbrains/runtime:oraclelinux7_x64 -f Dockerfile.oraclelinux.x86_64 .
+docker build \
+  --platform=linux/amd64 \
+  -t registry.jetbrains.team/p/jbre/containers/oraclelinux7_x64:latest \
+  -f Dockerfile.oraclelinux.x86_64 .
 
 # NB: the resulting container can (and should) be used without the network
 # connection (--network none) during build in order to reduce the chance
