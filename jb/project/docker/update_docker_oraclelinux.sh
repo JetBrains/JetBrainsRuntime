637f120c55c872582c6000c6056e1c540470b703
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2024-12-26 13:15:58 +0400

    JBR-8073 automate checking on availability of new updates for OL7

---
+++ jb/project/docker/update_docker_oraclelinux.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+function fatal {
+    echo "FATAL ERROR: " $1
+    exit 1
+}
+
+DOCKERFILE=$1
+DATE=$(date +"%Y-%m-%d")
+
+echo "Checking for updates on $DATE..."
+
+yum install -q -y yum-utils
+yum makecache
+[ $? != 0 ] && fatal "yum makecache"
+
+UPDATES=$(yum check-update -q -C 2>/dev/null | awk '/^\S/ {print $1}' | sed 's/\.[^.]*$//')
+
+NUPDATES=0
+for P in $UPDATES; do ((NUPDATES++)); done
+[ $NUPDATES -eq 0 ] && echo "NO UPDATES on $DATE" && exit 0
+
+echo "NEED TO UPDATE IMAGE"
+echo "$NUPDATES packages updated: " $UPDATES
+
+[ ! -f $DOCKERFILE ] && fatal "no $DOCKERFILE"
+
+DNUPDATES=0
+for P in $UPDATES; do
+    N=$(grep -c "$P-[0-9]" $DOCKERFILE)
+    [ $N -gt 1 ] && fatal "More than one match for $P in $DOCKERFILE, can't create a patch"
+    DP=$(grep "$P-[0-9]" $DOCKERFILE)
+    if [ -n "$DP" ]; then
+        ((DNUPDATES++))
+    fi
+done
+
+[ $DNUPDATES -eq 0 ] && echo "No need to update $DOCKERFILE" && exit 0
+
+F=${DOCKERFILE}.new
+rm -f $F
+cp $DOCKERFILE $F
+[ $? != 0 ] && fatal "copying $DOCKERFILE"
+
+for P in $UPDATES; do
+    DP=$(grep "$P-[0-9]" $DOCKERFILE | awk '{print $1}')
+    if [ -n "$DP" ]; then
+        LATEST=$(repoquery --queryformat="%{name}-%{version}-%{release}" $P)
+        [ -z "$LATEST" ] && fatal "Can't determine the latest version of $P"
+        echo "$DP -> $LATEST"
+        sed -i "s/\b$DP\b/$LATEST/g" "$F"
+    fi
+done
+
+diff $DOCKERFILE $F > /dev/null 2>&1
+[ $? -eq 0 ] && echo "No changes to $DOCKERFILE, just update the image" && exit 0
+
+echo "Apply this patch to $DOCKERFILE:"
+diff -C 3 $DOCKERFILE $F
