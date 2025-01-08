2905b996b9966d630be982c4667519eba7f2cd6c
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2025-01-08 14:40:27 +0400

    JBR-8104 OL8: enable premier support for docker images

--- jb/project/docker/mkdocker_aarch64.sh
+++ jb/project/docker/mkdocker_aarch64.sh
@@ -4,10 +4,23 @@ set -euo pipefail
 
 # This script creates a Docker image suitable for building AArch64 variant
 
+echo "ULN username:"
+read username
+
+echo "ULN password:"
+read -s password
+
+echo "CSI:"
+read csi
+
 docker build \
   --platform=linux/aarch64 \
-  -t registry.jetbrains.team/p/jbre/containers/oraclelinux8_aarch64:latest \
   --no-cache \
+  -t registry.jetbrains.team/p/jbre/containers/oraclelinux8_aarch64:latest \
+  --build-arg profilename=OL8_aarch64 \
+  --build-arg username=$username \
+  --build-arg password=$password \
+  --build-arg csi=$csi \
   -f Dockerfile.oraclelinux .
 
 # NB: the resulting container can (and should) be used without the network
