d832551979dcc0a6b47f1ce1ebc2cea67d4b897f
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2025-01-08 14:40:27 +0400

    JBR-8104 OL8: enable premier support for docker images

--- jb/project/docker/mkdocker_musl_aarch64.sh
+++ jb/project/docker/mkdocker_musl_aarch64.sh
@@ -2,8 +2,9 @@
 
 set -euxo pipefail
 
-wget https://raw.githubusercontent.com/GNOME/gtk/refs/heads/main/gdk/wayland/protocol/gtk-shell.xml
 # This script creates a Docker image suitable for building musl AArch64 variant
+
+wget https://raw.githubusercontent.com/GNOME/gtk/refs/heads/main/gdk/wayland/protocol/gtk-shell.xml
 
 docker build \
   --platform=linux/aarch64 \
