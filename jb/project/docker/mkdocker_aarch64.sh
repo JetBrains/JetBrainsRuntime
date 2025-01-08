0fa2d24a376a686b81e8af6d351aec816d4bbbd0
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2025-01-08 14:40:27 +0400

    JBR-8104 OL8: enable premier support for docker images

--- jb/project/docker/mkdocker_aarch64.sh
+++ jb/project/docker/mkdocker_aarch64.sh
@@ -2,13 +2,27 @@
 
 set -euo pipefail
 
-wget https://raw.githubusercontent.com/GNOME/gtk/refs/heads/main/gdk/wayland/protocol/gtk-shell.xml
 # This script creates a Docker image suitable for building AArch64 variant
+
+echo "ULN username:"
+read username
+
+echo "ULN password:"
+read -s password
+
+echo "CSI:"
+read csi
+
+wget https://raw.githubusercontent.com/GNOME/gtk/refs/heads/main/gdk/wayland/protocol/gtk-shell.xml
 
 docker build \
   --platform=linux/aarch64 \
-  -t registry.jetbrains.team/p/jbre/containers/oraclelinux8_aarch64:latest \
   --no-cache \
+  -t registry.jetbrains.team/p/jbre/containers/oraclelinux8_aarch64:latest \
+  --build-arg profilename=OL8_aarch64 \
+  --build-arg username=$username \
+  --build-arg password=$password \
+  --build-arg csi=$csi \
   -f Dockerfile.oraclelinux .
 
 # NB: the resulting container can (and should) be used without the network
--- jb/project/docker/mkdocker_x64.sh
+++ jb/project/docker/mkdocker_x64.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
+
+# This script creates a Docker image suitable for building x64 variant
+
+echo "ULN username:"
+read username
+
+echo "ULN password:"
+read -s password
+
+echo "CSI:"
+read csi
 
 wget https://raw.githubusercontent.com/GNOME/gtk/refs/heads/main/gdk/wayland/protocol/gtk-shell.xml
-# This script creates a Docker image suitable for building x64 variant
 
 docker build \
   --platform=linux/amd64 \
-  -t registry.jetbrains.team/p/jbre/containers/oraclelinux8_x64:latest \
   --no-cache \
+  -t registry.jetbrains.team/p/jbre/containers/oraclelinux8_x64:latest \
+  --build-arg profilename=OL8_x64 \
+  --build-arg username=$username \
+  --build-arg password=$password \
+  --build-arg csi=$csi \
   -f Dockerfile.oraclelinux .
-
-
 
 # NB: the resulting container can (and should) be used without the network
 # connection (--network none) during build in order to reduce the chance
