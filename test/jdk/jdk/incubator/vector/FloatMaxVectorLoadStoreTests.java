92171b8c952420b3dc8fcd0bcb7ff55228ea9849
Goetz Lindenmaier <goetz@openjdk.org>
2025-01-13 15:21:45 +0000

    8325659: Normalize Random usage by incubator vector tests
    Backport-of: 4bba445d835837db5ab145edb24030fc6f42ec24

--- test/jdk/jdk/incubator/vector/FloatMaxVectorLoadStoreTests.java
+++ test/jdk/jdk/incubator/vector/FloatMaxVectorLoadStoreTests.java
@@ -24,6 +24,9 @@
 /*
  * @test
  * @enablePreview
+ * @key randomness
+ *
+ * @library /test/lib
  * @modules jdk.incubator.vector java.base/jdk.internal.vm.annotation
  * @run testng/othervm --add-opens jdk.incubator.vector/jdk.incubator.vector=ALL-UNNAMED
  *      -XX:-TieredCompilation FloatMaxVectorLoadStoreTests
