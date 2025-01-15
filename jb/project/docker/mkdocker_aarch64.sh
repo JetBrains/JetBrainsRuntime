9786a90b766ddb93ec5bd1e60a55d4f9fd27d067
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2025-01-15 12:29:23 +0400

    Revert "JBR-8072 move JBR docker images to registry.jetbrains.team"
    This reverts commit 66bd338d23b3782139f24eaf130251dba17eda04.

--- jb/project/docker/mkdocker_aarch64.sh
+++ jb/project/docker/mkdocker_aarch64.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-set -euo pipefail
+set -euxo pipefail
 
 # This script creates a Docker image suitable for building AArch64 variant
 
-docker build \
-  --platform=linux/aarch64 \
-  -t registry.jetbrains.team/p/jbre/containers/oraclelinux8_aarch64:latest \
-  --no-cache \
-  -f Dockerfile.oraclelinux .
+docker build --platform=linux/aarch64 -t jetbrains/runtime:oraclelinux8_aarch64 -f Dockerfile.oraclelinux .
 
 # NB: the resulting container can (and should) be used without the network
 # connection (--network none) during build in order to reduce the chance
