db238ece28ce5b4cc3355a47e5288d01d76d4853
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2025-01-22 14:37:57 +0100

    JBR-8091 X: jb/java/wayland/RobotGet tests thorw java.awt.AWTException: headless environment
    Do not run test logic in the headless environment

--- test/jdk/jb/java/awt/wayland/RobotGetPixelsTest.java
+++ test/jdk/jb/java/awt/wayland/RobotGetPixelsTest.java
@@ -52,6 +52,11 @@ public class RobotGetPixelsTest {
     }
 
     public static void main(String[] args) throws InterruptedException, AWTException {
+        if (GraphicsEnvironment.getLocalGraphicsEnvironment().isHeadlessInstance()) {
+            System.out.println("No WLToolkit, skipping test");
+            return;
+        }
+
         final Robot robot = new Robot();
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Robot Test");
