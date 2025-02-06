859820795fcfe14889c8c2d505ef9c56c4bcf470
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2025-02-06 22:33:45 +0100

    JBR-7700 Classes from package `java.io.` use `java.nio.file` inside
    The option can be enabled/disabled by specifying `-Djbr.java.io.use.nio=true/false`

--- src/hotspot/share/cds/cdsProtectionDomain.hpp
+++ src/hotspot/share/cds/cdsProtectionDomain.hpp
@@ -81,6 +81,7 @@ public:
   static Handle get_shared_jar_manifest(int shared_path_index, TRAPS);
   static Handle get_shared_jar_url(int shared_path_index, TRAPS);
   static oop to_file_URL(const char* path, Handle url_h, TRAPS);
+  static void cds_preload_helper_init(TRAPS);
   static Handle get_protection_domain_from_classloader(Handle class_loader,
                                                        Handle url, TRAPS);
   static Handle get_shared_protection_domain(Handle class_loader,
