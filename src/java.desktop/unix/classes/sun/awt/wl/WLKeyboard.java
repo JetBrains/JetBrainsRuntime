3e7b3f4c1a6b9cf5627c9304e7a5528e37091a1f
Nikita Tsarev <nikita.tsarev@jetbrains.com>
2025-02-07 01:18:59 +0100

    JBR-7994: Properly report non-base-level function keys [WLToolkit]

--- src/java.desktop/unix/classes/sun/awt/wl/WLKeyboard.java
+++ src/java.desktop/unix/classes/sun/awt/wl/WLKeyboard.java
@@ -112,49 +112,15 @@ class WLKeyboard {
         }
     }
 
-    public static final int XKB_SHIFT_MASK = 1 << 0;
-    public static final int XKB_CAPS_LOCK_MASK = 1 << 1;
-    public static final int XKB_CTRL_MASK = 1 << 2;
-    public static final int XKB_ALT_MASK = 1 << 3;
-    public static final int XKB_NUM_LOCK_MASK = 1 << 4;
-    public static final int XKB_MOD3_MASK = 1 << 5;
-    public static final int XKB_META_MASK = 1 << 6;
-    public static final int XKB_MOD5_MASK = 1 << 7;
-
     private final KeyRepeatManager keyRepeatManager;
 
     private native void initialize(KeyRepeatManager keyRepeatManager);
 
-    public int getModifiers() {
-        int result = 0;
-        int mask = getXKBModifiersMask();
+    public native int getModifiers();
 
-        if ((mask & XKB_SHIFT_MASK) != 0) {
-            result |= InputEvent.SHIFT_DOWN_MASK;
-        }
+    public native boolean isCapsLockPressed();
 
-        if ((mask & XKB_CTRL_MASK) != 0) {
-            result |= InputEvent.CTRL_DOWN_MASK;
-        }
-
-        if ((mask & XKB_ALT_MASK) != 0) {
-            result |= InputEvent.ALT_DOWN_MASK;
-        }
-
-        if ((mask & XKB_META_MASK) != 0) {
-            result |= InputEvent.META_DOWN_MASK;
-        }
-
-        return result;
-    }
-
-    public boolean isCapsLockPressed() {
-        return (getXKBModifiersMask() & XKB_CAPS_LOCK_MASK) != 0;
-    }
-
-    public boolean isNumLockPressed() {
-        return (getXKBModifiersMask() & XKB_NUM_LOCK_MASK) != 0;
-    }
+    public native boolean isNumLockPressed();
 
     public void onLostFocus() {
         assert EventQueue.isDispatchThread();
