8005dc713535811de47de66113c7e52c6d86704b
bourgesl <bourges.laurent@gmail.com>
2025-02-14 16:09:52 +0100

    JBR-5497: change the default value for the system property "awt.mac.flushBuffers.invokeLater" to 'enabled' to avoid any potential freeze (safe) until a better solution

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformWindow.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformWindow.java
@@ -1192,8 +1192,8 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
                 new GetPropertyAction(invokeLaterKey));
         final int result;
         if (invokeLaterArg == null) {
-            // default = 'auto':
-            result = INVOKE_LATER_AUTO;
+            // default = 'enabled' to avoid any potential freeze (safe) until better solution:
+            result = INVOKE_LATER_ENABLED;
         } else {
             switch (invokeLaterArg.toLowerCase()) {
                 default:
