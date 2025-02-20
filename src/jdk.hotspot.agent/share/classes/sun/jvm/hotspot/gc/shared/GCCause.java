7f7ece2140f5c86f16a19975e2cde8fd60573dda
vlad20012 <beskvlad@gmail.com>
2025-02-20 17:18:07 +0100

    JBR-8303 Provide JBR API method to perform GC with more intensive heap shrinking

--- src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/gc/shared/GCCause.java
+++ src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/gc/shared/GCCause.java
@@ -55,6 +55,7 @@ public enum GCCause {
 
   _dcmd_gc_run ("Diagnostic Command"),
   _jbr_gc_run("JBR full GC"),
+  _jbr_shrinking_gc_run("JBR shrinking GC"),
 
   _shenandoah_allocation_failure_evac ("Allocation Failure During Evacuation"),
   _shenandoah_stop_vm ("Stopping VM"),
