6c5aa1adbde0cd70406f9d8b1fdd53ad9fbd968b
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2025-02-23 14:47:21 +0400

    JBR-7867 Notarization scripts: fail build if signing of separate files are failed - revert signapp.sh

--- jb/project/tools/mac/scripts/signapp.sh
+++ jb/project/tools/mac/scripts/signapp.sh
@@ -9,6 +9,7 @@ export COPYFILE_DISABLE=true
 
 INPUT_FILE=$1
 EXPLODED=$2.exploded
+BACKUP_JMODS=$2.backup
 USERNAME=$3
 PASSWORD=$4
 CODESIGN_STRING=$5
@@ -28,6 +29,8 @@ if test -d "$EXPLODED"; then
 fi
 rm -rf "$EXPLODED"
 mkdir "$EXPLODED"
+rm -rf "$BACKUP_JMODS"
+mkdir "$BACKUP_JMODS"
 
 log "Unzipping $INPUT_FILE to $EXPLODED ..."
 tar -xzvf "$INPUT_FILE" --directory $EXPLODED
