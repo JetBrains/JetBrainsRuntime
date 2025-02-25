7ec293383085d503ad47ec73edf865c75b994d4a
Goetz Lindenmaier <goetz@openjdk.org>
2025-02-25 16:13:58 +0000

    8327476: Upgrade JLine to 3.26.1
    Reviewed-by: mdoerr
    Backport-of: d02ad34144917a36911296b20ce28381ead3f601

--- src/jdk.internal.le/share/classes/jdk/internal/org/jline/terminal/spi/SystemStream.java
+++ src/jdk.internal.le/share/classes/jdk/internal/org/jline/terminal/spi/SystemStream.java
@@ -1,18 +1,15 @@
 /*
- * Copyright (c) 2002-2016, the original author(s).
+ * Copyright (c) 2023, the original author(s).
  *
  * This software is distributable under the BSD license. See the terms of the
  * BSD license in the documentation provided with this software.
  *
  * https://opensource.org/licenses/BSD-3-Clause
  */
-package jdk.internal.org.jline.reader;
+package jdk.internal.org.jline.terminal.spi;
 
-/**
- *
- */
-@FunctionalInterface
-public interface Widget extends Binding {
-
-    boolean apply();
+public enum SystemStream {
+    Input,
+    Output,
+    Error
 }
