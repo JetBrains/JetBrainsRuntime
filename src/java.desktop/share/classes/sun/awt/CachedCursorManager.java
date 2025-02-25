60e17cde37a913c51550d7e56788d9ff3ef5fac5
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2025-02-25 13:43:26 +0400

    Revert "JBR-7838 added getTreeLock to updateCursorImpl"
    This reverts commit 5a9b8a048891aa9ee23a56bf38a773813f4c4630.

--- src/java.desktop/share/classes/sun/awt/CachedCursorManager.java
+++ src/java.desktop/share/classes/sun/awt/CachedCursorManager.java
@@ -75,20 +75,15 @@ public abstract class CachedCursorManager {
 
     private void updateCursorImpl() {
         final Point cursorPos = getCursorPosition();
-        final Component component = getComponentUnderCursor();
-        if (component != null) {
-            synchronized (component.getTreeLock()) {
-                final Component parentComponent = findComponent(cursorPos, component);
-                Cursor cursor = getCursorByPosition(cursorPos, parentComponent);
+        final Component c = findComponent(cursorPos);
+        Cursor cursor = getCursorByPosition(cursorPos, c);
 
-                if (cursor == null) {
-                    cursor = (parentComponent != null) ? parentComponent.getCursor() : null;
-                }
+        if (cursor == null) {
+            cursor = (c != null) ? c.getCursor() : null;
+        }
 
-                if (cursor != null) {
-                    setCursor(cursor);
-                }
-            }
+        if (cursor != null) {
+            setCursor(cursor);
         }
     }
 
@@ -102,20 +97,22 @@ public abstract class CachedCursorManager {
      * @param cursorPos Current cursor position.
      * @return Component or null.
      */
-    private Component findComponent(final Point cursorPos, Component currentComponent) {
-        Component component = currentComponent;
-        if (component instanceof Container && component.isShowing()) {
-            final Point p = getLocationOnScreen(component);
-            component = AWTAccessor.getContainerAccessor().findComponentAt(
-                    (Container) component, cursorPos.x - p.x, cursorPos.y - p.y, false);
+    protected Component findComponent(final Point cursorPos) {
+        Component component = getComponentUnderCursor();
+        if (component != null) {
+            if (component instanceof Container && component.isShowing()) {
+                final Point p = getLocationOnScreen(component);
+                component = AWTAccessor.getContainerAccessor().findComponentAt(
+                        (Container) component, cursorPos.x - p.x, cursorPos.y - p.y, false);
 
-        }
-        while (component != null) {
-            final Object p = AWTAccessor.getComponentAccessor().getPeer(component);
-            if (component.isVisible() && component.isEnabled() && p != null) {
-                break;
             }
-            component = component.getParent();
+            while (component != null) {
+                final Object p = AWTAccessor.getComponentAccessor().getPeer(component);
+                if (component.isVisible() && component.isEnabled() && p != null) {
+                    break;
+                }
+                component = component.getParent();
+            }
         }
         return component;
     }
