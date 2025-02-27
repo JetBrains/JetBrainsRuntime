3db2ca9e3995444726b906686a2431b3367aaa03
Martin Doerr <mdoerr@openjdk.org>
2025-02-27 12:49:37 +0000

    8345146: [PPC64] Make intrinsic conversions between bit representations of half precision values and floats
    Reviewed-by: rrich
    Backport-of: b42d79eb6a6d497dc63718c2854609bebca4498c

--- src/hotspot/cpu/ppc/vm_version_ppc.hpp
+++ src/hotspot/cpu/ppc/vm_version_ppc.hpp
@@ -97,6 +97,8 @@ public:
   static bool supports_fast_class_init_checks() { return true; }
   constexpr static bool supports_stack_watermark_barrier() { return true; }
 
+  static bool supports_float16() { return PowerArchitecturePPC64 >= 9; }
+
   static bool is_determine_features_test_running() { return _is_determine_features_test_running; }
   // CPU instruction support
   static bool has_fsqrt()   { return (_features & fsqrt_m) != 0; }
