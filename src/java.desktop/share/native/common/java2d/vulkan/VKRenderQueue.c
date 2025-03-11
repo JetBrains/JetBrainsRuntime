4a94eadd90861bf703f61f1ab2f35cdfbedadc64
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2025-03-11 12:35:15 +0100

    JBR-8418 Vulkan: RenderPerfTest Image test does not work properly
    Passed transform to VKRenderer code
    Flush content of the destination surface

--- src/java.desktop/share/native/common/java2d/vulkan/VKRenderQueue.c
+++ src/java.desktop/share/native/common/java2d/vulkan/VKRenderQueue.c
@@ -530,14 +530,29 @@ JNIEXPORT void JNICALL Java_sun_java2d_vulkan_VKRenderQueue_flushBuffer
                     .m11 = m11, .m02 = m02, .m12 = m12
                 };
 
-                VKRenderer_GetContext()->transform = transform;
-
+                if (VKRenderer_GetContext()->surface != NULL &&
+                    VK_IS_NEQ_TRANSFORM(&VKRenderer_GetContext()->transform, &transform))
+                {
+                  // TODO: Consider replacing this with setting a new transform
+                  //  in VKRenderingContext and increment transformModCount,
+                  //  then check for changed transform modification counter in
+                  //  VKRenderer_Validate, do VKRenderer_FlushDraw and push
+                  //  new constants into the shader
+                  VKRenderer_FlushSurface(VKRenderer_GetContext()->surface);
+                  VKRenderer_GetContext()->transform = transform;
+                }
             }
             break;
         case sun_java2d_pipe_BufferedOpCodes_RESET_TRANSFORM:
             {
                 J2dRlsTraceLn(J2D_TRACE_VERBOSE,
-                    "VKRenderQueue_flushBuffer: RESET_TRANSFORM")
+                    "VKRenderQueue_flushBuffer: RESET_TRANSFORM");
+                if (VKRenderer_GetContext()->surface != NULL &&
+                    VK_IS_NEQ_TRANSFORM(&VKRenderer_GetContext()->transform, &VK_ID_TRANSFORM))
+                {
+                  VKRenderer_FlushSurface(VKRenderer_GetContext()->surface);
+                  VKRenderer_GetContext()->transform = VK_ID_TRANSFORM;
+                }
             }
             break;
 
