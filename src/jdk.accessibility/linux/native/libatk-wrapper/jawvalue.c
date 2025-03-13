07799cabbe26aad424365791c70e35d766e42d42
Tatiana Elfimova <tatiana.elfimova@jetbrains.com>
2025-03-13 19:21:53 +0200

    [vars null check] jawvalue: null check for vars

--- src/jdk.accessibility/linux/native/libatk-wrapper/jawvalue.c
+++ src/jdk.accessibility/linux/native/libatk-wrapper/jawvalue.c
@@ -65,14 +65,18 @@ gpointer jaw_value_data_init(jobject ac) {
     ValueData *data = g_new0(ValueData, 1);
 
     JNIEnv *jniEnv = jaw_util_get_jni_env();
+    CHECK_NULL(jniEnv, NULL);
     jclass classValue =
         (*jniEnv)->FindClass(jniEnv, "org/GNOME/Accessibility/AtkValue");
+    CHECK_NULL(classValue, NULL);
     jmethodID jmid = (*jniEnv)->GetStaticMethodID(
         jniEnv, classValue, "create_atk_value",
         "(Ljavax/accessibility/AccessibleContext;)Lorg/GNOME/Accessibility/"
         "AtkValue;");
+    CHECK_NULL(jmid, NULL);
     jobject jatk_value =
         (*jniEnv)->CallStaticObjectMethod(jniEnv, classValue, jmid, ac);
+    CHECK_NULL(jatk_value, NULL);
     data->atk_value = (*jniEnv)->NewGlobalRef(jniEnv, jatk_value);
 
     return data;
@@ -81,9 +85,12 @@ gpointer jaw_value_data_init(jobject ac) {
 void jaw_value_data_finalize(gpointer p) {
     JAW_DEBUG_ALL("%p", p);
     ValueData *data = (ValueData *)p;
-    JNIEnv *jniEnv = jaw_util_get_jni_env();
+    CHECK_NULL(data, );
 
-    if (data && data->atk_value) {
+    JNIEnv *jniEnv = jaw_util_get_jni_env();
+    CHECK_NULL(jniEnv, );
+
+    if (data->atk_value) {
         (*jniEnv)->DeleteGlobalRef(jniEnv, data->atk_value);
         data->atk_value = NULL;
     }
@@ -99,16 +106,23 @@ static void get_g_value_from_java_number(JNIEnv *jniEnv, jobject jnumber,
     }
 
     jclass classByte = (*jniEnv)->FindClass(jniEnv, "java/lang/Byte");
+    CHECK_NULL(classByte, );
     jclass classDouble = (*jniEnv)->FindClass(jniEnv, "java/lang/Double");
+    CHECK_NULL(classDouble, );
     jclass classFloat = (*jniEnv)->FindClass(jniEnv, "java/lang/Float");
+    CHECK_NULL(classFloat, );
     jclass classInteger = (*jniEnv)->FindClass(jniEnv, "java/lang/Integer");
+    CHECK_NULL(classInteger, );
     jclass classLong = (*jniEnv)->FindClass(jniEnv, "java/lang/Long");
+    CHECK_NULL(classLong, );
     jclass classShort = (*jniEnv)->FindClass(jniEnv, "java/lang/Short");
+    CHECK_NULL(classShort, );
 
     jmethodID jmid;
 
     if ((*jniEnv)->IsInstanceOf(jniEnv, jnumber, classByte)) {
         jmid = (*jniEnv)->GetMethodID(jniEnv, classByte, "byteValue", "()B");
+        CHECK_NULL(jmid, );
         g_value_init(value, G_TYPE_CHAR);
         g_value_set_schar(
             value, (gchar)(*jniEnv)->CallByteMethod(jniEnv, jnumber, jmid));
@@ -119,6 +133,7 @@ static void get_g_value_from_java_number(JNIEnv *jniEnv, jobject jnumber,
     if ((*jniEnv)->IsInstanceOf(jniEnv, jnumber, classDouble)) {
         jmid =
             (*jniEnv)->GetMethodID(jniEnv, classDouble, "doubleValue", "()D");
+        CHECK_NULL(jmid, );
         g_value_init(value, G_TYPE_DOUBLE);
         g_value_set_double(
             value, (gdouble)(*jniEnv)->CallDoubleMethod(jniEnv, jnumber, jmid));
@@ -128,6 +143,7 @@ static void get_g_value_from_java_number(JNIEnv *jniEnv, jobject jnumber,
 
     if ((*jniEnv)->IsInstanceOf(jniEnv, jnumber, classFloat)) {
         jmid = (*jniEnv)->GetMethodID(jniEnv, classFloat, "floatValue", "()F");
+        CHECK_NULL(jmid, );
         g_value_init(value, G_TYPE_FLOAT);
         g_value_set_float(
             value, (gfloat)(*jniEnv)->CallFloatMethod(jniEnv, jnumber, jmid));
@@ -138,6 +154,7 @@ static void get_g_value_from_java_number(JNIEnv *jniEnv, jobject jnumber,
     if ((*jniEnv)->IsInstanceOf(jniEnv, jnumber, classInteger) ||
         (*jniEnv)->IsInstanceOf(jniEnv, jnumber, classShort)) {
         jmid = (*jniEnv)->GetMethodID(jniEnv, classInteger, "intValue", "()I");
+        CHECK_NULL(jmid, );
         g_value_init(value, G_TYPE_INT);
         g_value_set_int(value,
                         (gint)(*jniEnv)->CallIntMethod(jniEnv, jnumber, jmid));
@@ -147,6 +164,7 @@ static void get_g_value_from_java_number(JNIEnv *jniEnv, jobject jnumber,
 
     if ((*jniEnv)->IsInstanceOf(jniEnv, jnumber, classLong)) {
         jmid = (*jniEnv)->GetMethodID(jniEnv, classLong, "longValue", "()J");
+        CHECK_NULL(jmid, );
         g_value_init(value, G_TYPE_INT64);
         g_value_set_int64(
             value, (gint64)(*jniEnv)->CallLongMethod(jniEnv, jnumber, jmid));
@@ -161,19 +179,26 @@ static void jaw_value_get_current_value(AtkValue *obj, GValue *value) {
     if (!obj || !value) {
         g_warning("Null argument passed to function");
         return;
+    }
+
     g_value_unset(value);
     JAW_GET_VALUE(obj, );
 
     jclass classAtkValue =
         (*env)->FindClass(env, "org/GNOME/Accessibility/AtkValue");
+    if(!classAtkValue) {
+      (*env)->DeleteGlobalRef(env, atk_value);
+      return;
+    }
     jmethodID jmid = (*env)->GetMethodID(
         env, classAtkValue, "get_current_value", "()Ljava/lang/Number;");
+    if(!jmid) {
+      (*env)->DeleteGlobalRef(env, atk_value);
+      return;
+    }
     jobject jnumber = (*env)->CallObjectMethod(env, atk_value, jmid);
     (*env)->DeleteGlobalRef(env, atk_value);
-
-    if (!jnumber) {
-        return;
-    }
+    CHECK_NULL(jnumber, );
 
     get_g_value_from_java_number(env, jnumber, value);
 }
@@ -190,8 +215,16 @@ static void jaw_value_set_value(AtkValue *obj, const gdouble value) {
 
     jclass classAtkValue =
         (*env)->FindClass(env, "org/GNOME/Accessibility/AtkValue");
+    if (!classAtkValue) {
+      (*env)->DeleteGlobalRef(env, atk_value);
+      return;
+    }
     jmethodID jmid = (*env)->GetMethodID(env, classAtkValue, "set_value",
                                          "(Ljava/lang/Number;)V");
+    if (!jmid) {
+      (*env)->DeleteGlobalRef(env, atk_value);
+      return;
+    }
     (*env)->CallVoidMethod(env, atk_value, jmid, (jdouble)value);
     (*env)->DeleteGlobalRef(env, atk_value);
 }
@@ -208,10 +241,22 @@ static AtkRange *jaw_value_get_range(AtkValue *obj) {
 
     jclass classAtkValue =
         (*env)->FindClass(env, "org/GNOME/Accessibility/AtkValue");
+    if (!classAtkValue) {
+      (*env)->DeleteGlobalRef(env, atk_value);
+      return NULL;
+    }
     jmethodID jmidMin =
         (*env)->GetMethodID(env, classAtkValue, "get_minimum_value", "()D");
+    if (!jmidMin) {
+      (*env)->DeleteGlobalRef(env, atk_value);
+      return NULL;
+    }
     jmethodID jmidMax =
         (*env)->GetMethodID(env, classAtkValue, "get_maximum_value", "()D");
+    if (!jmidMax) {
+      (*env)->DeleteGlobalRef(env, atk_value);
+      return NULL;
+    }
     AtkRange *ret = atk_range_new(
         (gdouble)(*env)->CallDoubleMethod(env, atk_value, jmidMin),
         (gdouble)(*env)->CallDoubleMethod(env, atk_value, jmidMax),
@@ -220,6 +265,11 @@ static AtkRange *jaw_value_get_range(AtkValue *obj) {
     return ret;
 }
 
+/*
+ * Gets the minimum increment by which the value of this object may be changed.
+ * If zero, the minimum increment is undefined, which may mean that it is limited
+ * only by the floating point precision of the platform.
+ */
 static gdouble jaw_value_get_increment(AtkValue *obj) {
     JAW_DEBUG_C("%p", obj);
 
@@ -232,10 +282,19 @@ static gdouble jaw_value_get_increment(AtkValue *obj) {
 
     jclass classAtkValue =
         (*env)->FindClass(env, "org/GNOME/Accessibility/AtkValue");
+    if (!classAtkValue) {
+      (*env)->DeleteGlobalRef(env, atk_value);
+      return 0;
+    }
     jmethodID jmid =
         (*env)->GetMethodID(env, classAtkValue, "get_increment", "()D");
+    if (!jmid) {
+      (*env)->DeleteGlobalRef(env, atk_value);
+      return 0;
+    }
     gdouble ret = (*env)->CallDoubleMethod(env, atk_value, jmid);
     (*env)->DeleteGlobalRef(env, atk_value);
+    CHECK_NULL(ret, 0);
     return ret;
 }
 
