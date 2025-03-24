6d2fda43e6e4d8beec6e04cc71868ba60d1fe66e
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2025-03-24 17:52:39 +0100

    JBR-8479 Support Vulkan accelerated mode in perf scripts
    Added the new option, minor refactoring

--- jb/project/tools/perf/run_inc.sh
+++ jb/project/tools/perf/run_inc.sh
@@ -1,3 +1,4 @@
+export LC_ALL=C
 ST=1 # sleep between iterations
 
 # number of iterations (jvm spawned)
@@ -30,8 +31,11 @@ rendering_options:
   -opengl # OpenGL pipeline (windows, linux, macOS)
   -metal  # Metal pipeline (macOS)
   -vulkan # Vulkan pipeline (WLToolkit)
+  -accelsd # Vulkan full acceleration (WLToolkit, Vulkan)
   -tk tk_name # AWT toolkit (linux: WLToolkit|XToolkit)
   -scale # UI scale
+  -N num # Number of iterations (JVM runs)
+  -R num # Number of repeats in the test
 EOM
 
 while [ $# -ge 1 ] ; do
@@ -45,12 +49,33 @@ while [ $# -ge 1 ] ; do
     -vulkan) J2D_OPTS=$J2D_OPTS" -Dsun.java2d.vulkan=true"    
       shift  
         ;;
+    -accelsd) J2D_OPTS=$J2D_OPTS" -Dsun.java2d.vulkan.accelsd=true"
+      shift
+        ;;
     -tk) shift
       if [ $# -ge 1 ] ; then
         J2D_OPTS=$J2D_OPTS" -Dawt.toolkit.name="$1
          shift
       else
          echo "Invalid parameters for -tk option. Use: -tk tkname"
+         exit 1
+      fi
+      ;;
+    -N) shift
+      if [ $# -ge 1 ] ; then
+         N=$1  
+         shift
+      else
+         echo "Invalid parameters for -N option. Use: -N <number>"
+         exit 1
+      fi
+      ;;
+    -R) shift
+      if [ $# -ge 1 ] ; then
+         R=$1  
+         shift
+      else
+         echo "Invalid parameters for -R option. Use: -R <number>"
          exit 1
       fi
       ;;
