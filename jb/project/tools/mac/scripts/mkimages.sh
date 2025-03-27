ccac233f15e6e59968a8ae6049b49ac0ae0bc541
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2025-03-27 08:34:21 +0400

    fixup! JBR-8315 specify XCODE_PATH for JBR building on macOS

--- jb/project/tools/mac/scripts/mkimages.sh
+++ jb/project/tools/mac/scripts/mkimages.sh
@@ -25,11 +25,15 @@ source jb/project/tools/common/scripts/common.sh
 JCEF_PATH=${JCEF_PATH:=./jcef_mac}
 BOOT_JDK=${BOOT_JDK:=$(/usr/libexec/java_home -v 17)}
 XCODE_PATH=${XCODE_PATH:-}
-if [ -z "${CONTINUOUS_INTEGRATION:-}"]; then
-  [ -d "$XCODE_PATH" ] && WITH_XCODE_PATH="--with-xcode-path=$XCODE_PATH" || WITH_XCODE_PATH=""
+if [ -d "$XCODE_PATH" ]; then
+  WITH_XCODE_PATH="--with-xcode-path=$XCODE_PATH"
 else
-  if [ -d "$XCODE_PATH" ]; then
-    WITH_XCODE_PATH="--with-xcode-path=$XCODE_PATH"
+  if [ -z "${CONTINUOUS_INTEGRATION:-}"]; then
+    WITH_XCODE_PATH=""
+    if [ -n "${XCODE_PATH}"]; then
+      echo "XCode not found in the directory: ${XCODE_PATH}"
+      echo "default XCode will be used"
+    fi
   else
     if [ -z "${XCODE_PATH}"]; then
       echo "specify XCode via setting XCODE_PATH"
