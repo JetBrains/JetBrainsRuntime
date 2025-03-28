aa7902f385d07081997849ee9c93b44e6655b5a3
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2025-03-29 03:27:15 +0400

    JBR-8315 introduce the option -c that causes builds to fail under CI if XCODE_PATH is invalid

--- jb/project/tools/mac/scripts/mkimages.sh
+++ jb/project/tools/mac/scripts/mkimages.sh
@@ -25,7 +25,24 @@ source jb/project/tools/common/scripts/common.sh
 JCEF_PATH=${JCEF_PATH:=./jcef_mac}
 BOOT_JDK=${BOOT_JDK:=$(/usr/libexec/java_home -v 17)}
 XCODE_PATH=${XCODE_PATH:-}
-[ -d "$XCODE_PATH" ] && WITH_XCODE_PATH="--with-xcode-path=$XCODE_PATH" || WITH_XCODE_PATH=""
+if [ -d "$XCODE_PATH" ]; then
+  WITH_XCODE_PATH="--with-xcode-path=$XCODE_PATH"
+else
+  if [ -z "${CONTINUOUS_INTEGRATION:-}" ]; then
+    WITH_XCODE_PATH=""
+    if [ -n "${XCODE_PATH}" ]; then
+      echo "XCode not found in the directory: ${XCODE_PATH}"
+      echo "default XCode will be used"
+    fi
+  else
+    if [ -z "${XCODE_PATH}" ]; then
+      echo "specify XCode via setting XCODE_PATH"
+    else
+      echo "XCode not found in the directory: ${XCODE_PATH}"
+    fi
+    do_exit 1
+  fi
+fi
 
 function do_configure {
   sh configure \
