6f1724c471e9f9c20283da0b1683f37ad4121e32
Nikita Tsarev <nikita.tsarev@jetbrains.com>
2025-04-04 15:53:09 +0200

    JBR-8533: Fix wrong keys and modifiers being reported for certain non-function key combinations [WLToolkit]

--- test/jdk/java/awt/wakefield/WakefieldTestDriver.java
+++ test/jdk/java/awt/wakefield/WakefieldTestDriver.java
@@ -102,9 +102,10 @@ public class WakefieldTestDriver {
 
         final String socketName = SOCKET_NAME_PREFIX + ProcessHandle.current().pid();
         final Process westonProcess = launchWeston(nScreens, screenWidth, screenHeight, socketName);
+        Thread.sleep(1000);
         try {
             System.out.println("Running test with WAYLAND_DISPLAY=" + socketName);
-            final ProcessBuilder pb = ProcessTools.createTestJvm(jvmArgs);
+            final ProcessBuilder pb = ProcessTools.createTestJavaProcessBuilder(jvmArgs);
             final Map<String, String> env = pb.environment();
             env.put("WAYLAND_DISPLAY", socketName);
 
