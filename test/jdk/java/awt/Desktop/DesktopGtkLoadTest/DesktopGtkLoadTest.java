adf103f5c9d709272c53179cdee360fcd8416608
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2025-04-07 15:49:54 +0400

    JBR-8572 Wayland: java/awt/Desktop/DesktopGtkLoadTest/DesktopGtkLoadTest.java: Wrong GTK library version: null

--- test/jdk/java/awt/Desktop/DesktopGtkLoadTest/DesktopGtkLoadTest.java
+++ test/jdk/java/awt/Desktop/DesktopGtkLoadTest/DesktopGtkLoadTest.java
@@ -28,9 +28,12 @@
  * @summary AWT_Desktop/Automated/Exceptions/BasicTest loads incorrect GTK
  * version when jdk.gtk.version=3
  * @requires (os.family == "linux")
+ * @library /test/lib
  * @run main DesktopGtkLoadTest
  */
 
+import jdk.test.lib.process.OutputAnalyzer;
+import jdk.test.lib.process.ProcessTools;
 
 import java.awt.*;
 import java.io.*;
@@ -43,26 +46,14 @@ public class DesktopGtkLoadTest {
     }
 
     public static void main(String[] args) throws Exception {
-        Process p = Runtime.getRuntime().exec(System.getProperty("java.home") +
-                "/bin/java -Djdk.gtk.version=3 -Djdk.gtk.verbose=true " +
-                "-cp " + System.getProperty("java.class.path", ".") +
-                " DesktopGtkLoadTest$RunDesktop");
-        p.waitFor();
-        try (BufferedReader br = new BufferedReader(
-                                   new InputStreamReader(p.getErrorStream()))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-                if (line.contains("Looking for GTK2 library")) {
-                    break;
-                }
-                if (line.contains("Looking for GTK3 library")) {
-                    return;
-                }
-            }
-            throw new RuntimeException("Wrong GTK library version: \n" + line);
-        }
-
+        final ProcessBuilder pbJava = ProcessTools.createTestJavaProcessBuilder(
+                "-Djdk.gtk.version=3",
+                "-Djdk.gtk.verbose=true",
+                RunDesktop.class.getName()
+        );
+        final OutputAnalyzer output = ProcessTools.executeProcess(pbJava);
+        output.shouldNotContain("Looking for GTK2 library");
+        output.shouldContain("Looking for GTK3 library");
     }
 
 }
