0ea521a32e6aca111841b149fe68153a2db45655
Tatiana Elfimova <tatiana.elfimova@jetbrains.com>
2025-04-08 17:28:07 +0300

    AtkInterface: documentation + refactoring

--- src/jdk.accessibility/linux/classes/org/GNOME/Accessibility/AtkInterface.java
+++ src/jdk.accessibility/linux/classes/org/GNOME/Accessibility/AtkInterface.java
@@ -22,18 +22,77 @@ package org.GNOME.Accessibility;
 import java.lang.annotation.Native;
 
 public interface AtkInterface {
-    @Native int INTERFACE_ACTION = 0x00000001;
-    @Native int INTERFACE_COMPONENT = 0x00000002;
-    @Native int INTERFACE_DOCUMENT = 0x00000004;
-    @Native int INTERFACE_EDITABLE_TEXT = 0x00000008;
-    @Native int INTERFACE_HYPERLINK = 0x00000010;
-    @Native int INTERFACE_HYPERTEXT = 0x00000020;
-    @Native int INTERFACE_IMAGE = 0x00000040;
-    @Native int INTERFACE_SELECTION = 0x00000080;
-    @Native int INTERFACE_STREAMABLE_CONTENT = 0x00000100;
-    @Native int INTERFACE_TABLE = 0x00000200;
-    @Native int INTERFACE_TABLE_CELL = 0x00000400;
-    @Native int INTERFACE_TEXT = 0x00000800;
-    @Native int INTERFACE_VALUE = 0x00001000;
+    /**
+     * The ATK interface provided by UI components
+     * which the user can activate/interact with.
+     */
+    @Native
+    int INTERFACE_ACTION = 0x00000001;
+
+    /**
+     * The ATK interface provided by UI components
+     * which occupy a physical area on the screen.
+     * which the user can activate/interact with.
+     */
+    @Native
+    int INTERFACE_COMPONENT = 0x00000002;
+
+    /**
+     * The ATK interface which represents the toplevel container for document content.
+     */
+    @Native
+    int INTERFACE_DOCUMENT = 0x00000004;
+
+    /**
+     * The ATK interface implemented by components containing user-editable text content.
+     */
+    @Native
+    int INTERFACE_EDITABLE_TEXT = 0x00000008;
+
+    /**
+     * The ATK interface which provides standard mechanism for manipulating hyperlinks.
+     */
+    @Native
+    int INTERFACE_HYPERTEXT = 0x00000020;
+
+    /**
+     * The ATK Interface implemented by components
+     * which expose image or pixmap content on-screen.
+     */
+    @Native
+    int INTERFACE_IMAGE = 0x00000040;
+
+    /**
+     * The ATK interface implemented by container objects
+     * whose AtkObject children can be selected.
+     */
+    @Native
+    int INTERFACE_SELECTION = 0x00000080;
+
+    /**
+     * The ATK interface implemented for UI components
+     * which contain tabular or row/column information.
+     */
+    @Native
+    int INTERFACE_TABLE = 0x00000200;
+
+    /**
+     * The ATK interface implemented for a cell inside a two-dimentional AtkTable.
+     */
+    @Native
+    int INTERFACE_TABLE_CELL = 0x00000400;
+
+    /**
+     * The ATK interface implemented by components with text content.
+     */
+    @Native
+    int INTERFACE_TEXT = 0x00000800;
+
+    /**
+     * The ATK interface implemented by valuators and components
+     * which display or select a value from a bounded range of values.
+     */
+    @Native
+    int INTERFACE_VALUE = 0x00001000;
 }
 
