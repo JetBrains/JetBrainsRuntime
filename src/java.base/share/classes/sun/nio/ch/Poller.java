6236ffb7a6ae48ba179dc382991cf9bfa2546cae
Satyen Subramaniam <ssubramaniam@openjdk.org>
2025-04-11 14:21:37 +0000

    8327370: (ch) sun.nio.ch.Poller.register throws AssertionError
    Backport-of: d2bebffb1fd26fae4526afd33a818ee776b7102e

--- src/java.base/share/classes/sun/nio/ch/Poller.java
+++ src/java.base/share/classes/sun/nio/ch/Poller.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -151,7 +151,12 @@ public abstract class Poller {
     private void register(int fdVal) throws IOException {
         Thread previous = map.putIfAbsent(fdVal, Thread.currentThread());
         assert previous == null;
-        implRegister(fdVal);
+        try {
+            implRegister(fdVal);
+        } catch (Throwable t) {
+            map.remove(fdVal);
+            throw t;
+        }
     }
 
     /**
