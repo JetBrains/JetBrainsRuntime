1f039fe2044edbd09d62914bc99e01facfc453c4
Goetz Lindenmaier <goetz@openjdk.org>
2025-04-11 13:21:52 +0000

    8344361: Restore null return for invalid services from legacy providers
    Backport-of: e20bd018c4046870d0cf632bb8e5440cb9f5c3c2

--- src/java.base/share/classes/java/security/Provider.java
+++ src/java.base/share/classes/java/security/Provider.java
@@ -1288,6 +1288,7 @@ public abstract class Provider extends Properties {
             s = legacyMap.get(key);
             if (s != null && !s.isValid()) {
                 legacyMap.remove(key, s);
+                return null;
             }
         }
 
