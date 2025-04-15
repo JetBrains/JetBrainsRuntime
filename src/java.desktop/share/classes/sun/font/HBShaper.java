d3047ece84dd8e926716ec28feeaea7a4a0c7057
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2025-04-15 17:39:45 +0200

    JBR-8282 Port 8353230: Emoji rendering regression after JDK-8208377

--- src/java.desktop/share/classes/sun/font/HBShaper.java
+++ src/java.desktop/share/classes/sun/font/HBShaper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -342,7 +342,7 @@ public class HBShaper {
     ) {
 
         Font2D font2D = scopedVars.get().font();
-        int glyphID = font2D.charToGlyph(unicode);
+        int glyphID = font2D.charToGlyphRaw(unicode);
         if (glyphID < 0) glyphID = 0;
         @SuppressWarnings("restricted")
         MemorySegment glyphIDPtr = glyph.reinterpret(4);
@@ -359,7 +359,7 @@ public class HBShaper {
         MemorySegment user_data   /* Not used */
     ) {
         Font2D font2D = scopedVars.get().font();
-        int glyphID = font2D.charToVariationGlyph(unicode, variation_selector);
+        int glyphID = font2D.charToVariationGlyphRaw(unicode, variation_selector);
         if (glyphID < 0) glyphID = 0;
         @SuppressWarnings("restricted")
         MemorySegment glyphIDPtr = glyph.reinterpret(4);
