9d1b0c6bcc19977927a99701df45fc2d3f97877a
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2025-05-12 20:17:00 +0200

    JBR-8664 Optimize sun.nio.fs.UnixPath.compareTo

--- src/java.base/unix/classes/sun/nio/fs/UnixPath.java
+++ src/java.base/unix/classes/sun/nio/fs/UnixPath.java
@@ -37,6 +37,7 @@ import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.nio.file.spi.FileSystemProvider;
+import java.util.Arrays;
 import java.util.Objects;
 
 import jdk.internal.access.JavaLangAccess;
@@ -718,23 +719,7 @@ class UnixPath implements Path {
 
     @Override
     public int compareTo(Path other) {
-        int len1 = path.length;
-        int len2 = ((UnixPath) other).path.length;
-
-        int n = Math.min(len1, len2);
-        byte v1[] = path;
-        byte v2[] = ((UnixPath) other).path;
-
-        int k = 0;
-        while (k < n) {
-            int c1 = v1[k] & 0xff;
-            int c2 = v2[k] & 0xff;
-            if (c1 != c2) {
-                return c1 - c2;
-            }
-           k++;
-        }
-        return len1 - len2;
+        return Arrays.compareUnsigned(path, ((UnixPath) other).path);
     }
 
     @Override
