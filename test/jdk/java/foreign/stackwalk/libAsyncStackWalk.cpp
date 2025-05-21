e1dca348276c6bf555b69f2980cad5cc607a12fe
Goetz Lindenmaier <goetz@openjdk.org>
2025-05-21 08:25:48 +0000

    8324799: Use correct extension for C++ test headers
    Reviewed-by: mbaesken
    Backport-of: 998d0baab0fd051c38d9fd6021628eb863b80554

--- test/jdk/java/foreign/stackwalk/libAsyncStackWalk.cpp
+++ test/jdk/java/foreign/stackwalk/libAsyncStackWalk.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,7 +22,7 @@
  */
 
 #include "export.h"
-#include "testlib_threads.h"
+#include "testlib_threads.hpp"
 
 typedef void (*CB_t)(void);
 
