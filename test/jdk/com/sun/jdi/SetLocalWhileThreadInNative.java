d1474de066c1e45f4e7455cf08decbefb0dd9967
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2025-05-26 15:55:31 +0200

    JBR-8859 Fix assertion in jtreg:test/jdk/com/sun/jdi/SetLocalWhileThreadInNative.java
    The test relied on hard-coded constant that knows internals of FileInputStream.read calls, and these internals changed in JBR-7700.

--- test/jdk/com/sun/jdi/SetLocalWhileThreadInNative.java
+++ test/jdk/com/sun/jdi/SetLocalWhileThreadInNative.java
@@ -40,6 +40,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.sun.jdi.*;
 import com.sun.jdi.event.*;
@@ -159,7 +160,14 @@ public class SetLocalWhileThreadInNative extends TestScaffold {
         for (StackFrame ff : stack_frames) {
             System.out.println("frame[" + i++ +"]: " + ff.location().method());
         }
-        StackFrame frame = mainThread.frame(2);
+        StackFrame frame = mainThread.frame(
+                // frame[0]: java.io.FileInputStream.read0()
+                // frame[1]: java.io.FileInputStream.implRead()
+                // frame[2]: java.io.FileInputStream.read()
+                // frame[3]: SetLocalWhileThreadInNativeTarget.dontinline_testMethod()
+                // frame[4]: SetLocalWhileThreadInNativeTarget.main(java.lang.String[])
+                3
+        );
         Asserts.assertEQ(frame.location().method().toString(), "SetLocalWhileThreadInNativeTarget.dontinline_testMethod()");
         List<LocalVariable> localVars = frame.visibleVariables();
         boolean changedLocal = false;
