78e020a40262d74b43eca141b6dc03a1d5076d3f
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2025-06-14 02:17:43 +0400

    Revert "fixup! JBR-8548 Add possibility to build without out-of-process part in Linux."
    This reverts commit e32b9d4ebcd0f331293d917ba9f2572b232717f5.

--- jb/project/tools/linux/scripts/mkimages_x64.sh
+++ jb/project/tools/linux/scripts/mkimages_x64.sh
@@ -102,7 +102,7 @@ function create_image_bundle {
 
   # jmod does not preserve file permissions (JDK-8173610)
   [ -f "$IMAGES_DIR"/"$__root_dir"/lib/jcef_helper ] && chmod a+x "$IMAGES_DIR"/"$__root_dir"/lib/jcef_helper
-  if [ ! -n "${JCEF_BUILD_LEGACY:-}" ]; then
+  if [! -n "${JCEF_BUILD_LEGACY:-}" ]; then
     [ -f "$IMAGES_DIR"/"$__root_dir"/lib/cef_server ] && chmod a+x "$IMAGES_DIR"/"$__root_dir"/lib/cef_server
   fi
 
