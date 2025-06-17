2a83c8773f3fbcf54f5ab34942f560dbd2fec6e5
Goetz Lindenmaier <goetz@openjdk.org>
2025-06-17 08:05:13 +0000

    8286194: ExecutorShutdown test fails intermittently
    Backport-of: 04df8b74379c9de7b20931fea1642f82569d3a2d

--- src/java.net.http/share/classes/jdk/internal/net/http/Http1Exchange.java
+++ src/java.net.http/share/classes/jdk/internal/net/http/Http1Exchange.java
@@ -148,13 +148,26 @@ class Http1Exchange<T> extends ExchangeImpl<T> {
         }
 
         final void setSubscription(Flow.Subscription subscription) {
-            this.subscription = subscription;
-            whenSubscribed.complete(subscription);
+            Flow.Subscription sub;
+            synchronized (this) {
+                if ((sub = this.subscription) == null) {
+                    this.subscription = sub = subscription;
+                }
+            }
+            if (sub == subscription) {
+                whenSubscribed.complete(subscription);
+            } else subscription.cancel();
         }
 
         final void cancelSubscription() {
             try {
-                subscription.cancel();
+                Flow.Subscription sub;
+                synchronized (this) {
+                    if ((sub = this.subscription) == null) {
+                        this.subscription = sub = HttpBodySubscriberWrapper.NOP;
+                    }
+                }
+                sub.cancel();
             } catch(Throwable t) {
                 String msg = "Ignoring exception raised when canceling BodyPublisher subscription";
                 if (debug.on()) debug.log("%s: %s", msg, t);
@@ -779,9 +792,10 @@ class Http1Exchange<T> extends ExchangeImpl<T> {
                     return;
                 }
 
-                if (debug.on()) debug.log(() -> "hasOutgoing = " + hasOutgoing());
+                if (debug.on()) debug.log(() -> "hasOutgoing = " + hasOutgoing() + ", demand = " + demand.get());
                 while (hasOutgoing() && demand.tryDecrement()) {
                     DataPair dp = getOutgoing();
+                    if (debug.on()) debug.log("outgoing: " + dp);
                     if (dp == null)
                         break;
 
@@ -803,7 +817,10 @@ class Http1Exchange<T> extends ExchangeImpl<T> {
                             // The next Subscriber will eventually take over.
 
                         } else {
-                            if (checkRequestCancelled()) return;
+                            if (checkRequestCancelled()) {
+                                if (debug.on()) debug.log("Request cancelled!");
+                                return;
+                            }
                             if (debug.on())
                                 debug.log("onNext with " + Utils.remaining(data) + " bytes");
                             subscriber.onNext(data);
