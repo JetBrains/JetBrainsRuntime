df3ea97e3556fe813a69981aa261d3e44ec66496
Goetz Lindenmaier <goetz@openjdk.org>
2025-06-17 08:08:12 +0000

    8167252: Some of Charset.availableCharsets() does not contain itself
    Backport-of: 3eeb681a0de87baa12b6eac5966e7f707b76c8bf

--- src/jdk.charsets/share/classes/sun/nio/cs/ext/JISAutoDetect.java
+++ src/jdk.charsets/share/classes/sun/nio/cs/ext/JISAutoDetect.java
@@ -60,7 +60,8 @@ public class JISAutoDetect
         return ((cs.name().equals("US-ASCII"))
                 || (cs instanceof SJIS)
                 || (cs instanceof EUC_JP)
-                || (cs instanceof ISO2022_JP));
+                || (cs instanceof ISO2022_JP)
+                || (cs instanceof JISAutoDetect));
     }
 
     public boolean canEncode() {
