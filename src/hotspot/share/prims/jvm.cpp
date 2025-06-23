ae81650748ed418101b893bb765424d8e00a9cd6
Jan Kratochvil <jkratochvil@openjdk.org>
2025-06-23 12:08:25 +0000

    8261242: [Linux] OSContainer::is_containerized() returns true when run outside a container
    Reviewed-by: dcherepanov, mbaesken, sgehwolf
    Backport-of: 18cffb604a5f56ef59e56840f2bb9fd27f8485db

--- src/hotspot/share/prims/jvm.cpp
+++ src/hotspot/share/prims/jvm.cpp
@@ -110,6 +110,9 @@
 #if INCLUDE_MANAGEMENT
 #include "services/finalizerService.hpp"
 #endif
+#ifdef LINUX
+#include "osContainer_linux.hpp"
+#endif
 
 #include <errno.h>
 
@@ -487,6 +490,15 @@ JVM_END
 JVM_LEAF(jboolean, JVM_IsUseContainerSupport(void))
 #ifdef LINUX
   if (UseContainerSupport) {
+    return JNI_TRUE;
+  }
+#endif
+  return JNI_FALSE;
+JVM_END
+
+JVM_LEAF(jboolean, JVM_IsContainerized(void))
+#ifdef LINUX
+  if (OSContainer::is_containerized()) {
     return JNI_TRUE;
   }
 #endif
