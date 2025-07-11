0f8560ebd512275b592f29aecf860299a8789145
Nikita Provotorov <nikita.provotorov@jetbrains.com>
2025-07-11 17:24:45 +0200

    JBR-5673: Wayland: support input methods.
    Part 4.1: WLInputMethod native-side implementation:
    * Introducing and implementing all basic JNI upcalls and downcalls ;
    * Introducing the native IMContext type and methods for working with it. The type represents the native counterpart of WLInputMethod state ;
    * Implementing all bridging methods to/from the "text-input-unstable-v3" protocol.

--- src/java.desktop/unix/classes/sun/awt/wl/im/zwp_text_input_v3/WLInputMethodZwpTextInputV3.java
+++ src/java.desktop/unix/classes/sun/awt/wl/im/zwp_text_input_v3/WLInputMethodZwpTextInputV3.java
@@ -186,6 +186,53 @@ final class WLInputMethodZwpTextInputV3 extends InputMethodAdapter {
 
     /* JNI downcalls section */
 
+    /** Initializes all static JNI references ({@code jclass}, {@code jmethodID}, etc.) required by this class for functioning. */
+    private static native void initIDs();
+
+    /** @return pointer to the newly created native context associated with {@code this}. */
+    private native long createNativeContext() throws AWTException;
+    /** Disposes the native context created previously via {@link #createNativeContext()}. */
+    private static native void disposeNativeContext(long contextPtr);
+
+    /*private native void zwp_text_input_v3_destroy(long contextPtr);*/ // No use-cases for this currently
+    private native void zwp_text_input_v3_enable(long contextPtr);
+    private native void zwp_text_input_v3_disable(long contextPtr);
+    /*private native void zwp_text_input_v3_set_surrounding_text();*/   // Not supported currently
+    private native void zwp_text_input_v3_set_cursor_rectangle(long contextPtr, int surfaceLocalX, int surfaceLocalY, int width, int height);
+    private native void zwp_text_input_v3_set_content_type(long contextPtr, int hint, int purpose);
+    private native void zwp_text_input_v3_set_text_change_cause(long contextPtr, int changeCause);
+    private native void zwp_text_input_v3_commit(long contextPtr);
+
 
     /* JNI upcalls section */
+
+    /** Called in response to {@code zwp_text_input_v3::enter} events. */
+    private void zwp_text_input_v3_onEnter(long enteredWlSurfacePtr) {
+        assert EventQueue.isDispatchThread();
+    }
+
+    /** Called in response to {@code zwp_text_input_v3::leave} events. */
+    private void zwp_text_input_v3_onLeave(long leftWlSurfacePtr) {
+        assert EventQueue.isDispatchThread();
+    }
+
+    /** Called in response to {@code zwp_text_input_v3::preedit_string} events. */
+    private void zwp_text_input_v3_onPreeditString(byte[] preeditStrUtf8, int cursorBeginUtf8Byte, int cursorEndUtf8Byte) {
+        assert EventQueue.isDispatchThread();
+    }
+
+    /** Called in response to {@code zwp_text_input_v3::commit_string} events. */
+    private void zwp_text_input_v3_onCommitString(byte[] commitStrUtf8) {
+        assert EventQueue.isDispatchThread();
+    }
+
+    /** Called in response to {@code zwp_text_input_v3::delete_surrounding_text} events. */
+    private void zwp_text_input_v3_onDeleteSurroundingText(long numberOfUtf8BytesBeforeToDelete, long numberOfUtf8BytesAfterToDelete) {
+        assert EventQueue.isDispatchThread();
+    }
+
+    /** Called in response to {@code zwp_text_input_v3::done} events. */
+    private void zwp_text_input_v3_onDone(long doneSerial) {
+        assert EventQueue.isDispatchThread();
+    }
 }
