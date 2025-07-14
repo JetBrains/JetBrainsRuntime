1e8527ec032bbf4e34b43f775c175bf368ff62ba
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2025-07-14 16:13:02 +0200

    JBR-8740 Vulkan: Optimize BLIT (Part 4)
    Optimized validation of rendering state.

--- src/java.desktop/share/native/common/java2d/vulkan/VKRenderQueue.c
+++ src/java.desktop/share/native/common/java2d/vulkan/VKRenderQueue.c
@@ -352,10 +352,9 @@ JNIEXPORT void JNICALL Java_sun_java2d_vulkan_VKRenderQueue_flushBuffer
                                         dx1, dy1, dx2, dy2);
                 } else {
                     jshort srctype = EXTRACT_SHORT(packedParams, OFFSET_SRCTYPE);
-                    VKBlitLoops_Blit(env, ptr_to_jlong(src),
-                                     xform, hint, srctype,
-                                     sx1, sy1, sx2, sy2,
-                                     dx1, dy1, dx2, dy2);
+                    VKBlitLoops_Blit(env, src, srctype, hint,
+                                    sx1, sy1, sx2, sy2,
+                                    dx1, dy1, dx2, dy2);
                 }
                 context->surface = oldSurface;
                 J2dRlsTraceLn2(J2D_TRACE_VERBOSE, "VKRenderQueue_flushBuffer: BLIT %p -> %p ", src, dst)
