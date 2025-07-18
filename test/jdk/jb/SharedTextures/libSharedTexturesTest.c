7051edee9af18a1d0c3694cab1cd94927fda39c2
Vladimir Kharitonov <Vladimir.Kharitonov@jetbrains.com>
2025-07-19 01:08:19 +0200

    clean up the test

--- test/jdk/jb/SharedTextures/libSharedTexturesTest.c
+++ test/jdk/jb/SharedTextures/libSharedTexturesTest.c
@@ -4,32 +4,17 @@
 #include <GL/gl.h>
 #include <stdlib.h>
 
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h" // Place stb_image_write.h in your project directory
-
-
 static int gTextureType = -1;
 
 static const METAL_TEXTURE_TYPE = 1;
 static const OPENGL_TEXTURE_TYPE = 2;
 
+#ifdef _WIN32
 static HGLRC gSharedContext = 0;
 static int gPixelFormat = 0;
 
 static HDC dc = 0;
 static HGLRC gGLContext = 0;
-
-typedef void (APIENTRY *glBindTextureType)(GLenum target, GLuint texture);
-typedef void (APIENTRY *glGenTexturesType)(GLsizei n, GLuint *textures);
-typedef void (APIENTRY *glTexParameteriType)(GLenum target, GLenum pname, GLint param);
-typedef void (APIENTRY *glTexImage2DType)(GLenum target, GLint level, GLint internalformat, GLsizei width, GLsizei height, GLint border, GLenum format, GLenum type, const GLvoid *pixels);
-typedef void (APIENTRY *glDeleteTexturesType)(GLsizei n, const GLuint *textures);
-
-static glBindTextureType pglBindTexture;
-static glGenTexturesType pglGenTextures;
-static glTexParameteriType pglTexParameteri;
-static glTexImage2DType pglTexImage2D;
-static glDeleteTexturesType pglDeleteTextures;
 
 static int initOpenGL() {
     dc = GetDC(0);
@@ -64,15 +49,6 @@ static int initOpenGL() {
         return 0;
     }
 
-    pglGenTextures = glGenTextures; // (glGenTexturesType)wglGetProcAddress("glGenTextures");
-    pglBindTexture = glBindTexture; // (glBindTextureType)wglGetProcAddress("glBindTexture");
-    pglTexParameteri = glTexParameteri; // (glTexParameteriType)wglGetProcAddress("glTexParameteri");
-    pglTexImage2D = glTexImage2D; // (glTexImage2DType)wglGetProcAddress("glTexImage2D");
-    pglDeleteTextures = glDeleteTextures; // (glDeleteTexturesType)wglGetProcAddress("glDeleteTextures");
-
-    if (!pglGenTextures|| !pglBindTexture|| !pglTexParameteri|| !pglTexImage2D || !pglDeleteTextures) {
-        return 0;
-    }
     wglMakeCurrent(0, 0);
     return 1;
 }
@@ -124,38 +100,6 @@ static void check_gl_error(JNIEnv *env, const char* msgPrefix) {
     }
 }
 
-static void saveTextureToPng(const char* path, GLuint textureId, int width, int height) {
-    pglBindTexture(GL_TEXTURE_2D, textureId);
-
-    unsigned char* data = (unsigned char*)malloc(width * height * 4);
-    if (!data) {
-        printf("Failed to allocate buffer for texture readback\n");
-        return;
-    }
-
-    glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-
-    unsigned char* flipped = (unsigned char*)malloc(width * height * 4);
-    if (!flipped) {
-        printf("Failed to allocate buffer for vertical flip\n");
-        free(data);
-        return;
-    }
-    for (int y = 0; y < height; ++y) {
-        memcpy(flipped + (height - 1 - y) * width * 4,
-               data + y * width * 4,
-               width * 4);
-    }
-
-    int res = stbi_write_png(path, width, height, 4, flipped, width * 4);
-    if (!res) {
-        printf("Failed to write PNG file: %s\n", path);
-    }
-
-    free(data);
-    free(flipped);
-}
-
 JNIEXPORT jlong JNICALL Java_SharedTexturesTest_createTexture
         (JNIEnv *env, jclass clazz, jbyteArray byteArray, jint width, jint height) {
     if (!wglMakeCurrent(dc, gGLContext)) {
@@ -180,31 +124,30 @@ JNIEXPORT jlong JNICALL Java_SharedTexturesTest_createTexture
     jbyte *pixels = (*env)->GetByteArrayElements(env, byteArray, NULL);
 
     GLuint texId = 0;
-    pglGenTextures(1, &texId);
-    check_gl_error(env, "pglGenTextures");
-    pglBindTexture(GL_TEXTURE_2D, texId);
-    check_gl_error(env, "pglBindTexture");
+    glGenTextures(1, &texId);
+    check_gl_error(env, "glGenTextures");
+    glBindTexture(GL_TEXTURE_2D, texId);
+    check_gl_error(env, "glBindTexture");
 
 
-    pglTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    check_gl_error(env, "pglTexParameteri GL_TEXTURE_MAG_FILTER");
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    check_gl_error(env, "glTexParameteri GL_TEXTURE_MAG_FILTER");
 
-    pglTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    check_gl_error(env, "pglTexParameteri GL_TEXTURE_MIN_FILTER");
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    check_gl_error(env, "glTexParameteri GL_TEXTURE_MIN_FILTER");
 
     #define GL_CLAMP_TO_EDGE 0x812F
-    pglTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    pglTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-    pglTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
                   GL_RGBA, GL_UNSIGNED_BYTE, (const void*)pixels);
 
     check_gl_error(env, "glTexImage2D");
 
     (*env)->ReleaseByteArrayElements(env, byteArray, pixels, JNI_ABORT);
-    pglBindTexture(GL_TEXTURE_2D, 0);
-
-    saveTextureToPng("c:/develop/text.png", texId, width, height);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glFinish();
 
     return (jlong)texId;
 }
@@ -213,7 +156,7 @@ JNIEXPORT void JNICALL Java_SharedTexturesTest_disposeTexture
         (JNIEnv *env, jclass clazz, jlong texture) {
     if (gTextureType == OPENGL_TEXTURE_TYPE) {
         GLuint texId = (GLuint)texture;
-        pglDeleteTextures(1, &texId);\
+        glDeleteTextures(1, &texId);\
     }
 }
 
@@ -232,3 +175,9 @@ JNIEXPORT void JNICALL Java_SharedTexturesTest_releaseContext
         }
     }
 }
+
+#endif
+
+#ifdef LINUX
+// todo
+#endif
