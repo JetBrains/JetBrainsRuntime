2f52408c765b3472fa7db854f902137865afa8a5
Goetz Lindenmaier <goetz@openjdk.org>
2025-08-04 07:03:10 +0000

    8346751: Internal java compiler error with type annotations in constants expression in constant fields
    Backport-of: 0395593a8a1c01a87ae36552c0f2cc9c67e8bbd8

--- src/jdk.compiler/share/classes/com/sun/tools/javac/code/Flags.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/code/Flags.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -393,6 +393,11 @@ public class Flags {
      * Flag to indicate that the class/interface was declared with the non-sealed modifier.
      */
     public static final long NON_SEALED = 1L<<63; // ClassSymbols
+
+    /**
+     * Flag to indicate type annotations have been queued for field initializers.
+     */
+    public static final long FIELD_INIT_TYPE_ANNOTATIONS_QUEUED = 1L<<53; // VarSymbols
 
     /**
      * Describe modifier flags as they migh appear in source code, i.e.,
