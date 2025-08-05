928f21661bf4536e8dbda0b0cbf9a05de10d3786
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2025-08-06 01:38:11 +0200

    JBR-7419 Refactor Vulkan code
    - Separate instance and device-specific function tables
    - Avoid using device from global context when possible
    - Set up debug logger
    
    (cherry picked from commit af4eb8b7587d8f307d938b09c6fcb15b537e869d)

--- src/java.desktop/unix/native/common/java2d/vulkan/WLVKSurfaceData.c
+++ src/java.desktop/unix/native/common/java2d/vulkan/WLVKSurfaceData.c
@@ -83,6 +83,7 @@ Java_sun_java2d_vulkan_WLVKSurfaceData_assignSurface(JNIEnv *env, jobject wsd, j
         return;
     }
     VKGraphicsEnvironment* ge = VKGE_graphics_environment();
+    VKLogicalDevice* logicalDevice = ge->currentDevice;
     wlvksdo->wl_surface = (struct wl_surface*)jlong_to_ptr(wlSurfacePtr);
 
     if (vkwinsdo->surface == VK_NULL_HANDLE) {
@@ -100,8 +101,8 @@ Java_sun_java2d_vulkan_WLVKSurfaceData_assignSurface(JNIEnv *env, jobject wsd, j
         }
     }
 
-    VKSD_InitImageSurface(&vkwinsdo->vksdOps);
-    VKSD_InitWindowSurface(vkwinsdo);
+    VKSD_InitImageSurface(logicalDevice, &vkwinsdo->vksdOps);
+    VKSD_InitWindowSurface(logicalDevice, vkwinsdo);
     J2dRlsTraceLn(J2D_TRACE_INFO, "WLVKSurfaceData_assignSurface: Created WaylandSurfaceKHR");
 
     J2dTraceLn(J2D_TRACE_INFO, "WLVKSurfaceData_assignSurface wl_surface(%p) wl_display(%p)", wlvksdo->wl_surface, wl_display);
