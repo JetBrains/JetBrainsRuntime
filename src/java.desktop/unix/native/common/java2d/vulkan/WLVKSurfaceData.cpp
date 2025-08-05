dd9d6f23953e31b4b52b2a94911a2a2ac446e8db
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2025-08-06 01:03:09 +0200

    JBR-5973 Implement rendering of no-AA shapes with Vulkan pipeline
    Get rid of maxTextureSize in Vulkan code. This concept was introduced to fix macOS-specific bugs and don't map well to Vulkan implementation, as this value is tied to specific device and texture format, so get rid of it for now and see whether we need it at all.
    
    Refactored native surface data hierarchy. There was a C-style "inheritance" model with VKSDOps having an SurfaceDataOps as its first member and conversions back and forth between them. And then also privOps - pointer to the platform-specific part (WLVK). This was refactored into plain inheritance: SurfaceDataOps -> VKSurfaceData -> VKSwapchainSurfaceData -> WLVKSurfaceData
    
    State management, synchronization & layout transition. Now using dynamic rendering and synchronization2 extensions.
    Each device has a single timeline semaphore (basically 64-bit counter), monotonically increasing as device executes our commands, allowing us to track the state of the submitted batches and reuse resources which are no longer in use.
    
    Split command recording into primary and secondary command buffers.
    This allows us to record commands "in the past", before current render pass started, which gives possibility for some heavy optimizations:
    1. When we suddenly need some texture in the middle of the render pass - no need to stop render pass in order to insert necessary synchronization - we can do it as if we knew it beforehand.
    2. When we draw something and then clear the surface - just erase all commands inside current render pass we recorded earlier, so the actual drawing will never happen.
    
    Shaders are compiled with glslc or glslangValidator and bytecode is inlined directly into libawt_wlawt
    
    Memory management via VMA, vertex buffer pool, shader push constants.
    
    Other refactoring.
    
    (cherry picked from commit 9ceaebbb60468b225b1c49a56def55447cf5aade)

--- src/java.desktop/unix/native/common/java2d/vulkan/WLVKSurfaceData.cpp
+++ src/java.desktop/unix/native/common/java2d/vulkan/WLVKSurfaceData.cpp
@@ -36,121 +36,32 @@
 
 extern struct wl_display *wl_display;
 
-/**
- * This is the implementation of the general surface LockFunc defined in
- * SurfaceData.h.
- */
-jint
-WLVKSD_Lock(JNIEnv *env,
-          SurfaceDataOps *ops,
-          SurfaceDataRasInfo *pRasInfo,
-          jint lockflags)
-{
-#ifndef HEADLESS
-    VKSDOps *vsdo = (VKSDOps*)ops;
-    J2dTrace(J2D_TRACE_INFO, "WLVKSD_Unlock: %p\n", ops);
-    pthread_mutex_lock(&((WLVKSDOps*)vsdo->privOps)->lock);
-#endif
-    return SD_SUCCESS;
-}
-
-
-static void
-WLVKSD_GetRasInfo(JNIEnv *env,
-                SurfaceDataOps *ops,
-                SurfaceDataRasInfo *pRasInfo)
-{
-#ifndef HEADLESS
-    VKSDOps *vsdo = (VKSDOps*)ops;
-#endif
-}
-
-static void
-WLVKSD_Unlock(JNIEnv *env,
-            SurfaceDataOps *ops,
-            SurfaceDataRasInfo *pRasInfo)
-{
-#ifndef HEADLESS
-    VKSDOps *vsdo = (VKSDOps*)ops;
-    J2dTrace(J2D_TRACE_INFO, "WLVKSD_Unlock: %p\n", ops);
-    pthread_mutex_unlock(&((WLVKSDOps*)vsdo->privOps)->lock);
-#endif
-}
-
-static void
-WLVKSD_Dispose(JNIEnv *env, SurfaceDataOps *ops)
-{
-#ifndef HEADLESS
-    /* ops is assumed non-null as it is checked in SurfaceData_DisposeOps */
-    VKSDOps *vsdo = (VKSDOps*)ops;
-    J2dTrace(J2D_TRACE_INFO, "WLSD_Dispose %p\n", ops);
-    WLVKSDOps *wlvksdOps = (WLVKSDOps*)vsdo->privOps;
-    pthread_mutex_destroy(&wlvksdOps->lock);
-    if (wlvksdOps->wlvkSD !=nullptr) {
-        delete wlvksdOps->wlvkSD;
-        wlvksdOps->wlvkSD = nullptr;
-    }
-#endif
-}
 extern "C" JNIEXPORT void JNICALL Java_sun_java2d_vulkan_WLVKSurfaceData_initOps
-        (JNIEnv *env, jclass vksd, jint width, jint height, jint scale, jint backgroundRGB) {
+        (JNIEnv *env, jobject vksd, jint width, jint height, jint scale, jint backgroundRGB) {
 #ifndef HEADLESS
-    VKSDOps *vsdo = (VKSDOps*)SurfaceData_InitOps(env, vksd, sizeof(VKSDOps));
-    J2dRlsTraceLn(J2D_TRACE_INFO, "WLVKSurfaceData_initOps: %p", vsdo);
-    jboolean hasException;
-    if (vsdo == NULL) {
-        JNU_ThrowOutOfMemoryError(env, "Initialization of SurfaceData failed.");
-        return;
-    }
-
-    if (width <= 0) {
-        width = 1;
-    }
-    if (height <= 0) {
-        height = 1;
-    }
-
-    WLVKSDOps *wlvksdOps = (WLVKSDOps *)malloc(sizeof(WLVKSDOps));
-
-    if (wlvksdOps == NULL) {
-        JNU_ThrowOutOfMemoryError(env, "creating native WLVK ops");
-        return;
-    }
-
-    vsdo->privOps = wlvksdOps;
-    vsdo->sdOps.Lock = WLVKSD_Lock;
-    vsdo->sdOps.Unlock = WLVKSD_Unlock;
-    vsdo->sdOps.GetRasInfo = WLVKSD_GetRasInfo;
-    vsdo->sdOps.Dispose = WLVKSD_Dispose;
-    pthread_mutexattr_t attr;
-    pthread_mutexattr_init(&attr);
-    // Recursive mutex is required because blit can be done with both source
-    // and destination being the same surface (during scrolling, for example).
-    // So WLSD_Lock() should be able to lock the same surface twice in a row.
-    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE_NP);
-    pthread_mutex_init(&wlvksdOps->lock, &attr);
-    wlvksdOps->wlvkSD = new WLVKSurfaceData(width, height, scale, backgroundRGB);
+    J2dTrace(J2D_TRACE_INFO, "Create WLVKSurfaceData with size %d x %d and scale %d\n", width, height, scale);
+    width /= scale; // TODO This is incorrect, but we'll deal with this later, we probably need to do something on Wayland side for app-controlled scaling
+    height /= scale; // TODO This is incorrect, but we'll deal with this later, we probably need to do something on Wayland side for app-controlled scaling
+    auto *sd = new WLVKSurfaceData(width, height, scale, backgroundRGB);
+    sd->attachToJavaSurface(env, vksd);
 #endif /* !HEADLESS */
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_sun_java2d_vulkan_WLVKSurfaceData_assignSurface(JNIEnv *env, jobject wsd,
-                                             jlong wlSurfacePtr)
+Java_sun_java2d_vulkan_WLVKSurfaceData_assignSurface(JNIEnv *env, jobject wsd, jlong wlSurfacePtr)
 {
 #ifndef HEADLESS
-    VKSDOps *vsdo = (VKSDOps*)SurfaceData_GetOps(env, wsd);
-    if (vsdo == NULL) {
+    auto sd = (WLVKSurfaceData*)SurfaceData_GetOps(env, wsd);
+    if (sd == nullptr) {
         return;
     }
 
-    WLVKSDOps *wlvksdOps = (WLVKSDOps*)vsdo->privOps;
-
-    wl_surface* wlSurface = (struct wl_surface*)jlong_to_ptr(wlSurfacePtr);
+    auto wlSurface = (struct wl_surface*)jlong_to_ptr(wlSurfacePtr);
     J2dTraceLn(J2D_TRACE_INFO, "WLVKSurfaceData_assignSurface wl_surface(%p) wl_display(%p)",
                wlSurface, wl_display);
 
     try {
-        wlvksdOps->wlvkSD->validate(wlSurface);
+        sd->validate(wlSurface);
     } catch (std::exception& e) {
         J2dRlsTrace(J2D_TRACE_ERROR, "WLVKSurfaceData_assignSurface: %s\n", e.what());
     }
@@ -162,6 +73,7 @@ Java_sun_java2d_vulkan_WLVKSurfaceData_flush(JNIEnv *env, jobject wsd)
 {
 #ifndef HEADLESS
     J2dTrace(J2D_TRACE_INFO, "WLVKSurfaceData_flush\n");
+    // TODO?
 #endif /* !HEADLESS */
 }
 
@@ -169,36 +81,22 @@ extern "C" JNIEXPORT void JNICALL
 Java_sun_java2d_vulkan_WLVKSurfaceData_revalidate(JNIEnv *env, jobject wsd,
                                              jint width, jint height, jint scale)
 {
+    width /= scale; // TODO This is incorrect, but we'll deal with this later, we probably need to do something on Wayland side for app-controlled scaling
+    height /= scale; // TODO This is incorrect, but we'll deal with this later, we probably need to do something on Wayland side for app-controlled scaling
 #ifndef HEADLESS
-    VKSDOps *vsdo = (VKSDOps*)SurfaceData_GetOps(env, wsd);
-    if (vsdo == NULL) {
+    auto sd = (WLVKSurfaceData*)SurfaceData_GetOps(env, wsd);
+    if (sd == nullptr) {
         return;
     }
     J2dTrace(J2D_TRACE_INFO, "WLVKSurfaceData_revalidate to size %d x %d and scale %d\n", width, height, scale);
 
-    WLVKSDOps *wlvksdOps = (WLVKSDOps*)vsdo->privOps;
     try {
-        wlvksdOps->wlvkSD->revalidate(width, height, scale);
-        wlvksdOps->wlvkSD->update();
+        sd->revalidate(width, height, scale);
     } catch (std::exception& e) {
         J2dRlsTrace(J2D_TRACE_ERROR, "WLVKSurfaceData_revalidate: %s\n", e.what());
     }
 
 #endif /* !HEADLESS */
-}
-
-extern "C" JNIEXPORT void JNICALL
-JNI_OnUnload(JavaVM *vm, void *reserved) {
-#ifndef HEADLESS
-    VKGraphicsEnvironment::dispose();
-#endif /* !HEADLESS */
-}
-
-
-WLVKSurfaceData::WLVKSurfaceData(uint32_t w, uint32_t h, uint32_t s, uint32_t bgc)
-        :VKSurfaceData(w, h, s, bgc), _wl_surface(nullptr), _surface_khr(nullptr), _swapchain_khr(nullptr)
-{
-    J2dTrace(J2D_TRACE_INFO, "Create WLVKSurfaceData with size %d x %d and scale %d\n", w, h, s);
 }
 
 void WLVKSurfaceData::validate(wl_surface* wls)
@@ -207,133 +105,12 @@ void WLVKSurfaceData::validate(wl_surface* wls)
         return;
     }
 
+    auto& device = VKGraphicsEnvironment::graphics_environment()->default_device();
+    device.waitIdle(); // TODO wait until device is done with old swapchain
+    auto surface = VKGraphicsEnvironment::graphics_environment()->vk_instance()
+            .createWaylandSurfaceKHR({{}, wl_display, wls});
+
     _wl_surface = wls;
-
-    vk::WaylandSurfaceCreateInfoKHR createInfoKhr = {
-            {}, wl_display, _wl_surface
-    };
-
-    _surface_khr =
-            VKGraphicsEnvironment::graphics_environment()->vk_instance().createWaylandSurfaceKHR(
-                    createInfoKhr);
+    reset(device, std::move(surface));
     revalidate(width(), height(), scale());
-    update();
-}
-
-void WLVKSurfaceData::revalidate(uint32_t w, uint32_t h, uint32_t s)
-{
-    if (s == scale() && w == width() && h == height() ) {
-        if (!*_surface_khr || *_swapchain_khr) {
-            J2dTraceLn(J2D_TRACE_INFO,
-                       "WLVKSurfaceData_revalidate is skipped: surface_khr(%p) swapchain_khr(%p)",
-                       *_surface_khr, *_swapchain_khr);
-            return;
-        }
-    } else {
-        VKSurfaceData::revalidate(w, h, s);
-        if (!*_surface_khr) {
-            J2dTraceLn(J2D_TRACE_INFO, "WLVKSurfaceData_revalidate is skipped: surface_khr(%p)",
-                       *_surface_khr);
-            return;
-        }
-    }
-
-    vk::SwapchainCreateInfoKHR swapchainCreateInfoKhr{
-            {},
-            *_surface_khr,
-            1, vk::Format::eB8G8R8A8Unorm,
-            vk::ColorSpaceKHR::eVkColorspaceSrgbNonlinear,
-            {width()/scale(), height()/scale()},
-            1,
-            vk::ImageUsageFlagBits::eColorAttachment | vk::ImageUsageFlagBits::eTransferDst,
-            vk::SharingMode::eExclusive,
-            0,
-            nullptr,
-            vk::SurfaceTransformFlagBitsKHR::eIdentity,
-            vk::CompositeAlphaFlagBitsKHR::eOpaque,
-            vk::PresentModeKHR::eImmediate,
-            false, *_swapchain_khr
-    };
-
-    auto& device = VKGraphicsEnvironment::graphics_environment()->default_device();
-    _swapchain_khr = device.createSwapchainKHR(swapchainCreateInfoKhr);
-}
-
-void WLVKSurfaceData::set_bg_color(uint32_t bgc)
-{
-    if (bg_color() == bgc) {
-        return;
-    }
-    VKSurfaceData::set_bg_color(bgc);
-    update();
-}
-
-void WLVKSurfaceData::update()
-{
-    if (!*_swapchain_khr) {
-        return;
-    }
-    auto& device = VKGraphicsEnvironment::graphics_environment()->default_device();
-    vk::SemaphoreCreateInfo semInfo = {};
-    vk::raii::Semaphore presentCompleteSem = device.createSemaphore(semInfo);
-
-    std::pair<vk::Result, uint32_t> img = _swapchain_khr.acquireNextImage(0, *presentCompleteSem, nullptr);
-    if (img.first!=vk::Result::eSuccess) {
-        J2dRlsTraceLn(J2D_TRACE_INFO, "failed to get image");
-    }
-    else {
-        J2dRlsTraceLn(J2D_TRACE_INFO, "obtained image");
-    }
-    auto images = _swapchain_khr.getImages();
-
-    vk::CommandPoolCreateInfo poolInfo{
-            {vk::CommandPoolCreateFlagBits::eResetCommandBuffer},
-            static_cast<uint32_t>(device.queue_family())
-    };
-
-    auto pool = device.createCommandPool(poolInfo);
-
-    vk::CommandBufferAllocateInfo buffInfo{
-            *pool, vk::CommandBufferLevel::ePrimary, (uint32_t) 1, nullptr
-    };
-
-    auto buffers = device.allocateCommandBuffers(buffInfo);
-
-    vk::CommandBufferBeginInfo begInfo{
-    };
-
-    uint32_t alpha = (bg_color() >> 24) & 0xFF;
-    uint32_t red = (bg_color() >> 16) & 0xFF;
-    uint32_t green = (bg_color() >> 8) & 0xFF;
-    uint32_t blue = bg_color() & 0xFF;
-    vk::ClearColorValue color = {
-            static_cast<float>(red)/255.0f,
-            static_cast<float>(green)/255.0f,
-            static_cast<float>(blue)/255.0f,
-            static_cast<float>(alpha)/255.0f
-    };
-
-    std::vector<vk::ImageSubresourceRange> range = {{
-                                                            vk::ImageAspectFlagBits::eColor,
-                                                            0, 1, 0, 1}};
-    buffers[0].begin(begInfo);
-    buffers[0].clearColorImage(images[img.second], vk::ImageLayout::eSharedPresentKHR, color, range);
-    buffers[0].end();
-
-    vk::SubmitInfo submitInfo{
-            nullptr, nullptr, *buffers[0], nullptr
-    };
-
-    auto queue = device.getQueue(device.queue_family(), 0);
-
-    queue.submit(submitInfo, nullptr);
-    queue.waitIdle();
-    vk::PresentInfoKHR presentInfo;
-    presentInfo.swapchainCount = 1;
-    presentInfo.pSwapchains = &*_swapchain_khr;
-    presentInfo.pImageIndices = &(img.second);
-
-    queue.presentKHR(presentInfo);
-    queue.waitIdle();
-    device.waitIdle();
 }
