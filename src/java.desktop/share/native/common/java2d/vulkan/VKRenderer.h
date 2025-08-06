9969c16dabebb714b7f4c59f6cab624c6826a053
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2025-08-06 02:14:25 +0200

    JBR-7575 Vulkan: Implement composites (blending and XOR mode)
    - Implemented dynamic pipeline compilation.
    - Added 64-bit per pixel format usage in debug mode for testing.
    - Now passing colors from Java to Vulkan with straight alpha.
    
    (cherry picked from commit 3d7baad6876caf2097a2fa67d6239b6ea3ce209b)

--- src/java.desktop/share/native/common/java2d/vulkan/VKRenderer.h
+++ src/java.desktop/share/native/common/java2d/vulkan/VKRenderer.h
@@ -31,10 +31,14 @@
 #include "VKPipelines.h"
 
 struct VKRenderingContext {
-    VKSDOps*    surface;
-    VKTransform transform;
-    VkRect2D    clipRect;
-    Color       color;
+    VKSDOps*        surface;
+    VKTransform     transform;
+    VkRect2D        clipRect;
+    Color           color;
+    VKCompositeMode composite;
+    // Extra alpha is not used when painting with plain color,
+    // in this case color.a already includes it.
+    float extraAlpha;
 };
 
 typedef struct {
