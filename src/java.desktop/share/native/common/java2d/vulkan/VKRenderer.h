65800f473ea77a2b517b2a845f5489caa8c200f3
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2025-08-06 02:53:57 +0200

    JBR-8430 Vulkan: move RenderingContext into Renderer
    Moved context to the VKRenderer

--- src/java.desktop/share/native/common/java2d/vulkan/VKRenderer.h
+++ src/java.desktop/share/native/common/java2d/vulkan/VKRenderer.h
@@ -30,11 +30,21 @@
 #include "VKTypes.h"
 #include "VKPipelines.h"
 
+#define NO_CLIP ((VkRect2D) {{0, 0}, {0x7FFFFFFFU, 0x7FFFFFFFU}})
+
 struct VKRenderingContext {
     VKSDOps*        surface;
     VKTransform     transform;
+
+    // We keep this color separately from renderColor,
+    // because we need consistent state when switching between XOR and alpha
+    // composite modes. This variable holds last value set by SET_COLOR, while
+    // renderColor holds color, currently used for drawing, which may have
+    // also been provided by SET_XOR_COMPOSITE.
     Color           color;
+    Color           renderColor;
     VKCompositeMode composite;
+
     // Extra alpha is not used when painting with plain color,
     // in this case color.a already includes it.
     float extraAlpha;
@@ -54,7 +64,7 @@ VKRenderer* VKRenderer_Create(VKDevice* device);
 /**
  * Setup pipeline for drawing. Returns FALSE if the surface is not yet ready for drawing.
  */
-VkBool32 VKRenderer_Validate(const VKRenderingContext* context, VKShader shader, VkPrimitiveTopology topology);
+VkBool32 VKRenderer_Validate(VKShader shader, VkPrimitiveTopology topology);
 
 /**
  * Record commands into the primary command buffer (outside of a render pass).
@@ -106,23 +116,24 @@ void VKRenderer_ConfigureSurface(VKSDOps* surface, VkExtent2D extent, VKDevice* 
 
 // Blit operations.
 
-void VKRenderer_TextureRender(const VKRenderingContext* context,
-                              VKImage *destImage, VKImage *srcImage,
+void VKRenderer_TextureRender(VKImage *destImage, VKImage *srcImage,
                               VkBuffer vertexBuffer, uint32_t vertexNum);
 
 // Drawing operations.
 
-void VKRenderer_RenderRect(const VKRenderingContext* context, VkBool32 fill,
-                           jint x, jint y, jint w, jint h);
+void VKRenderer_RenderRect(VkBool32 fill, jint x, jint y, jint w, jint h);
 
-void VKRenderer_RenderParallelogram(const VKRenderingContext* context, VkBool32 fill,
+void VKRenderer_RenderParallelogram(VkBool32 fill,
                                     jfloat x11, jfloat y11,
                                     jfloat dx21, jfloat dy21,
                                     jfloat dx12, jfloat dy12);
 
-void VKRenderer_FillSpans(const VKRenderingContext* context, jint spanCount, jint *spans);
+void VKRenderer_FillSpans(jint spanCount, jint *spans);
 
-void VKRenderer_MaskFill(const VKRenderingContext* context,
-                         jint x, jint y, jint w, jint h, jint maskoff, jint maskscan, jint masklen, uint8_t* mask);
+void
+VKRenderer_MaskFill(jint x, jint y, jint w, jint h,
+                    jint maskoff, jint maskscan, jint masklen, uint8_t *mask);
+
+VKRenderingContext* VKRenderer_GetContext();
 
 #endif //VKRenderer_h_Included
