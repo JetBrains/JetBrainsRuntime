8d9317e7cd5b7f444f70d03736098f6c7485f715
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2025-08-06 22:47:11 +0200

    JBR-8738 Vulkan: Optimize ISO_BLIT

--- src/java.desktop/share/classes/sun/java2d/vulkan/VKOffScreenSurfaceData.java
+++ src/java.desktop/share/classes/sun/java2d/vulkan/VKOffScreenSurfaceData.java
@@ -82,9 +82,4 @@ public class VKOffScreenSurfaceData extends VKSurfaceData {
         }
         return result;
     }
-
-    @Override
-    public boolean isOnScreen() {
-        return false;
-    }
 }
