7536f77d8ac6868ab26ac85655e9ea50a67984f5
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2025-08-11 14:56:55 +0400

    JBR-5480 Include more information in OOME crash reports
    (cherry picked from commit 44f8dc85dacd13be414dc3b9043e55a0cc0eacb0)

--- src/hotspot/share/utilities/vmError.hpp
+++ src/hotspot/share/utilities/vmError.hpp
@@ -71,6 +71,12 @@ class VMError : public AllStatic {
   // used for reattempt step logic
   static const size_t _reattempt_required_stack_headroom;
 
+  static const int   OOME_STACKTRACE_BUFSIZE = 16*K;
+  static const int   OOME_STACKTRACE_COUNT   = 4;
+  static char        _oome_stacktrace[OOME_STACKTRACE_COUNT][OOME_STACKTRACE_BUFSIZE];
+  static int         _oome_free_index;
+  static void *      _ballast_memory;
+
   // Thread id of the first error. We must be able to handle native thread,
   // so use thread id instead of Thread* to identify thread.
   static volatile intptr_t _first_error_tid;
@@ -225,6 +231,12 @@ public:
 
   static void set_handshake_timed_out_thread(intptr_t thread_addr);
   static void set_safepoint_timed_out_thread(intptr_t thread_addr);
+
+  static void init();
+  static void record_oome_stack(const char *message);
+  static void print_oome_stacks(outputStream *st);
+  static void print_classloaders_stats(outputStream *st);
+  static void print_dup_classes(outputStream *st);
 };
 
 class VMErrorCallback {
