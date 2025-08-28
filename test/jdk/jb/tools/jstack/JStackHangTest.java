ed72aaa0c9c7267c17391d5302cb4522d496ca3f
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2025-08-28 06:46:39 +0400

    fixup! JBR-9274 test against streaming output for attach API

--- test/jdk/jb/tools/jstack/JStackHangTest.java
+++ test/jdk/jb/tools/jstack/JStackHangTest.java
@@ -1,92 +1,50 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
+import jdk.test.lib.process.OutputAnalyzer;
+import jdk.test.lib.process.ProcessTools;
+
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Stream;
 
 /**
  * @test
  * @bug 8354460
- * @summary checking streaming output for attach API
- * @run main/othervm JStackHangTest
+ * @summary <code>JStackHangTest</code> verifies the streaming output of the attach API. It launches
+ *       <code>JStackLauncher</code> in a child VM and ensures <code>jstack</code> does not hang.
+ * @library /test/lib
+ * @compile JStackHangTest.java
+ * @run main/othervm -Djdk.attach.vm.streaming=true JStackHangTest
  */
 
-//-Djdk.attach.vm.streaming=false
 public class JStackHangTest {
 
-    public static final String JAVA_HOME = System.getProperty("java.home");
-    public static final String JAVA_BIN = JAVA_HOME + File.separator + "bin";
-    public static final String JSTACK_PATH = JAVA_BIN + File.separator + "jstack";
+    static final int JSTACK_WAIT_TIME = JStackLauncher.JSTACK_WAIT_TIME * 2;
 
-    static final int JSTACK_WAIT_TIME = 5000;
+    public static final int CODE_NOT_RETURNED = 100;
 
-    static final int THREAD_COUNT = 10;
-    static final int STACK_DEPTH = 100;
+    public static void main(String[] args) throws Exception {
 
-    static Process process;
-    static CountDownLatch latch = new CountDownLatch(THREAD_COUNT);
+        ProcessBuilder pb = ProcessTools.createTestJavaProcessBuilder(
+                "-Djdk.attach.vm.streaming=" + System.getProperty("jdk.attach.vm.streaming"),
+                JStackLauncher.class.getName());
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        // Create threads with deep stacks
-        List<Thread> threads = createManyThreadsWithDeepStacks();
+        Process process = ProcessTools.startProcess("Child", pb);
+        OutputAnalyzer outputAnalyzer = new OutputAnalyzer(process);
 
-        // Run jstack against current process
-        System.out.println("jstack starting");
-        process = new ProcessBuilder(JSTACK_PATH, String.valueOf(ProcessHandle.current().pid())).start();
+        int returnCode;
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        Stream<String> output = reader.lines();
-        output.forEach(System.out::println);
+        if (!process.waitFor(JSTACK_WAIT_TIME, TimeUnit.MILLISECONDS)) {
+            System.out.println("jstack did not completed in " + JSTACK_WAIT_TIME + " ms.");
 
-        process.waitFor(JSTACK_WAIT_TIME, TimeUnit.MILLISECONDS);
-        System.out.println("wait for process = " + process);
+            System.out.println("killing all the jstack processes");
+            ProcessTools.executeCommand("killall", "-9", "jstack");
+            returnCode = CODE_NOT_RETURNED;
 
-        threads.forEach(Thread::interrupt);
-    }
-
-    public static List<Thread> createManyThreadsWithDeepStacks() throws InterruptedException {
-        List<Thread> threads = new ArrayList<>();
-
-        for (int threadIndex = 0; threadIndex < THREAD_COUNT; threadIndex++) {
-            System.out.println("Creating thread " + threadIndex);
-
-            Thread t = new DeepStackThread(threadIndex);
-
-            t.start();
-            threads.add(t);
+        } else {
+            returnCode = outputAnalyzer.getExitValue();
+            System.out.println("returnCode = " + returnCode);
         }
 
-        latch.await();
-
-        return threads;
-    }
-
-    static class DeepStackThread extends Thread {
-        DeepStackThread(int index) {
-            super.setName("DeepStackThread-" + index);
-        }
-
-        @Override
-        public synchronized void run() {
-            try {
-                createDeepStack(STACK_DEPTH);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-            System.out.println("Thread " + getName() + " exiting");
-        }
-
-        void createDeepStack(int depth) throws InterruptedException {
-            if (depth > 0) {
-                createDeepStack(depth - 1);
-            } else {
-                latch.countDown();
-                Thread.sleep(Long.MAX_VALUE);
-            }
-        }
+        if (returnCode == CODE_NOT_RETURNED)
+            throw new RuntimeException("jstack: failed to start or hanged");
+        else
+            System.out.println("jstack: completed successfully");
     }
 }
---
+++ test/jdk/jb/tools/jstack/JStackLauncher.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+/**
+ * <code>JStackLauncher</code> is used to launch <code>jstack</code> in the test of streaming output for the attach API
+ *
+ */
+
+public class JStackLauncher {
+
+    public static final String JAVA_HOME = System.getProperty("java.home");
+    public static final String JAVA_BIN = JAVA_HOME + File.separator + "bin";
+    public static final String JSTACK_PATH = JAVA_BIN + File.separator + "jstack";
+
+    static final int JSTACK_WAIT_TIME = 5000;
+
+    static final int THREAD_COUNT = 10;
+    static final int STACK_DEPTH = 100;
+
+    static Process process;
+    static CountDownLatch latch = new CountDownLatch(THREAD_COUNT);
+
+    public static void main(String[] args) throws InterruptedException {
+        // Create threads with deep stacks
+        List<Thread> threads = createManyThreadsWithDeepStacks();
+        System.out.println("jdk.attach.vm.streaming = " + System.getProperty("jdk.attach.vm.streaming"));
+        // Run jstack against current process
+        System.out.println("Starting jstack...");
+        try {
+            process = new ProcessBuilder(JSTACK_PATH, String.valueOf(ProcessHandle.current().pid())).start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Reading jstack output...");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        Stream<String> output = reader.lines();
+        output.forEach(System.out::println);
+
+        if (!process.waitFor(JSTACK_WAIT_TIME, TimeUnit.MILLISECONDS)) {
+            process.destroyForcibly();
+            throw new RuntimeException("jstack failed to complete in " + JSTACK_WAIT_TIME + " ms.");
+        }
+
+        threads.forEach(Thread::interrupt);
+    }
+
+    public static List<Thread> createManyThreadsWithDeepStacks() throws InterruptedException {
+        List<Thread> threads = new ArrayList<>();
+
+        for (int threadIndex = 0; threadIndex < THREAD_COUNT; threadIndex++) {
+            System.out.println("Creating thread " + threadIndex);
+
+            Thread t = new DeepStackThread(threadIndex);
+
+            t.start();
+            threads.add(t);
+        }
+        latch.await();
+
+        return threads;
+    }
+
+    static class DeepStackThread extends Thread {
+        DeepStackThread(int index) {
+            super.setName("DeepStackThread-" + index);
+        }
+
+        @Override
+        public synchronized void run() {
+            try {
+                createDeepStack(STACK_DEPTH);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.println("Thread " + getName() + " exiting");
+        }
+
+        void createDeepStack(int depth) throws InterruptedException {
+            if (depth > 0) {
+                createDeepStack(depth - 1);
+            } else {
+                latch.countDown();
+                Thread.sleep(Long.MAX_VALUE);
+            }
+        }
+    }
+}
