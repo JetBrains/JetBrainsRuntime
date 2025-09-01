7d15dee8e2c7ec0a813d56a0c69e6dc2d0c1056e
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2025-09-01 13:52:52 +0400

    JBR-9288 Wayland: use builtin window decorations in KDE

--- src/java.desktop/unix/classes/sun/awt/wl/WLToolkit.java
+++ src/java.desktop/unix/classes/sun/awt/wl/WLToolkit.java
@@ -137,6 +137,8 @@ public class WLToolkit extends UNIXToolkit implements Runnable {
 
     private static Boolean sunAwtDisableGtkFileDialogs = null;
 
+    private static final boolean isKDE;
+
     private static native void initIDs(long displayPtr);
 
     static {
@@ -146,6 +148,8 @@ public class WLToolkit extends UNIXToolkit implements Runnable {
             VKEnv.init(display);
             initIDs(display);
         }
+        String desktop = System.getenv("XDG_CURRENT_DESKTOP");
+        isKDE = desktop != null && desktop.toLowerCase().contains("kde");
         initialized = true;
     }
 
@@ -1075,4 +1079,7 @@ public class WLToolkit extends UNIXToolkit implements Runnable {
         return WLCursorManager.getInstance();
     }
 
+    public static boolean isKDE() {
+        return isKDE;
+    }
 }
