9d72ad6e92a0f9ae2062291f14f69ea612782a38
Rui Li <ruiamzn@amazon.com>
2025-09-08 15:25:52 +0000

    8352533: Report useful IOExceptions when jspawnhelper fails
    Backport-of: 5c73dfc28cbd6801ac85c6685fb8c77aad3ab0b7

--- test/jdk/java/lang/ProcessBuilder/Basic.java
+++ test/jdk/java/lang/ProcessBuilder/Basic.java
@@ -28,6 +28,7 @@
  *      6464154 6523983 6206031 4960438 6631352 6631966 6850957 6850958
  *      4947220 7018606 7034570 4244896 5049299 8003488 8054494 8058464
  *      8067796 8224905 8263729 8265173 8272600 8231297 8282219 8285517
+ *      8352533
  * @key intermittent
  * @summary Basic tests for Process and Environment Variable code
  * @modules java.base/java.lang:open
@@ -89,6 +90,7 @@ public class Basic {
     /* Used for regex String matching for long error messages */
     static final String PERMISSION_DENIED_ERROR_MSG = "(Permission denied|error=13)";
     static final String NO_SUCH_FILE_ERROR_MSG = "(No such file|error=2)";
+    static final String SPAWNHELPER_FAILURE_MSG = "(Possible reasons:)";
 
     /**
      * Returns the number of milliseconds since time given by
@@ -321,7 +323,9 @@ public class Basic {
         } catch (IOException e) {
             String m = e.getMessage();
             if (EnglishUnix.is() &&
-                ! matches(m, PERMISSION_DENIED_ERROR_MSG))
+                !matches(m, PERMISSION_DENIED_ERROR_MSG))
+                unexpected(e);
+            if (matches(m, SPAWNHELPER_FAILURE_MSG))
                 unexpected(e);
         } catch (Throwable t) { unexpected(t); }
     }
@@ -431,7 +435,9 @@ public class Basic {
                         } catch (IOException e) {
                             String m = e.getMessage();
                             if (EnglishUnix.is() &&
-                                ! matches(m, NO_SUCH_FILE_ERROR_MSG))
+                                !matches(m, NO_SUCH_FILE_ERROR_MSG))
+                                unexpected(e);
+                            if (matches(m, SPAWNHELPER_FAILURE_MSG))
                                 unexpected(e);
                         } catch (Throwable t) { unexpected(t); }
 
@@ -444,7 +450,9 @@ public class Basic {
                         } catch (IOException e) {
                             String m = e.getMessage();
                             if (EnglishUnix.is() &&
-                                ! matches(m, NO_SUCH_FILE_ERROR_MSG))
+                                !matches(m, NO_SUCH_FILE_ERROR_MSG))
+                                unexpected(e);
+                            if (matches(m, SPAWNHELPER_FAILURE_MSG))
                                 unexpected(e);
                         } catch (Throwable t) { unexpected(t); }
 
@@ -2052,6 +2060,8 @@ public class Basic {
             if (EnglishUnix.is() &&
                 ! matches(m, NO_SUCH_FILE_ERROR_MSG))
                 unexpected(e);
+            if (matches(m, SPAWNHELPER_FAILURE_MSG))
+                unexpected(e);
         } catch (Throwable t) { unexpected(t); }
 
         //----------------------------------------------------------------
@@ -2069,6 +2079,8 @@ public class Basic {
                     || (EnglishUnix.is() &&
                         ! matches(m, NO_SUCH_FILE_ERROR_MSG)))
                     unexpected(e);
+                if (matches(m, SPAWNHELPER_FAILURE_MSG))
+                    unexpected(e);
             } catch (Throwable t) { unexpected(t); }
 
         //----------------------------------------------------------------
@@ -2084,6 +2096,8 @@ public class Basic {
             if (! matches(m, "in directory")
                 || (EnglishUnix.is() &&
                     ! matches(m, NO_SUCH_FILE_ERROR_MSG)))
+                unexpected(e);
+            if (matches(m, SPAWNHELPER_FAILURE_MSG))
                 unexpected(e);
         } catch (Throwable t) { unexpected(t); }
 
@@ -2143,8 +2157,9 @@ public class Basic {
                         op.f();
                         fail();
                     } catch (IOException expected) {
-                        check(expected.getMessage()
-                              .matches("[Ss]tream [Cc]losed"));
+                        String m = expected.getMessage();
+                        check(m.matches("[Ss]tream [Cc]losed"));
+                        check(!matches(m, SPAWNHELPER_FAILURE_MSG));
                     }
                 }
             }
@@ -2196,8 +2211,12 @@ public class Basic {
                             }
                             equal(-1, r);
                         } catch (IOException ioe) {
-                            if (!ioe.getMessage().equals("Stream closed")) {
+                            String m = ioe.getMessage();
+                            if (!m.equals("Stream closed")) {
                                 // BufferedInputStream may throw IOE("Stream closed").
+                                unexpected(ioe);
+                            }
+                            if (matches(m, SPAWNHELPER_FAILURE_MSG)) {
                                 unexpected(ioe);
                             }
                         } catch (Throwable t) { unexpected(t); }}};
@@ -2253,6 +2272,9 @@ public class Basic {
                                 ! (msg.matches(".*Bad file.*") ||
                                         msg.matches(".*Stream closed.*")))
                                 unexpected(e);
+                            if (matches(msg, SPAWNHELPER_FAILURE_MSG)) {
+                                unexpected(e);
+                            }
                         }
                         catch (Throwable t) { unexpected(t); }}};
                 reader.setDaemon(true);
@@ -2345,6 +2367,9 @@ public class Basic {
             if (EnglishUnix.is() &&
                 ! matches(m, PERMISSION_DENIED_ERROR_MSG))
                 unexpected(e);
+            if (matches(m, SPAWNHELPER_FAILURE_MSG)) {
+                unexpected(e);
+            }
         } catch (Throwable t) { unexpected(t); }
 
         new File("emptyCommand").delete();
