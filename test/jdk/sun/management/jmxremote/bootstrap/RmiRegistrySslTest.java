ed00e13bab87d138e17736162e17bcc9d31e17cb
Sean Coffey <coffeys@openjdk.org>
2025-09-11 15:29:35 +0000

    8341496: Improve JMX connections 8367277: Fix copyright header in JMXInterfaceBindingTest.java
    Reviewed-by: dfuchs
    Backport-of: 76c1173ff5780b18cdb9ba3a519ee83abf651a3a

--- test/jdk/sun/management/jmxremote/bootstrap/RmiRegistrySslTest.java
+++ test/jdk/sun/management/jmxremote/bootstrap/RmiRegistrySslTest.java
@@ -180,6 +180,7 @@ public class RmiRegistrySslTest {
             initTestEnvironment();
 
             List<String> command = new ArrayList<>();
+            command.add("-Djdk.rmi.ssl.client.enableEndpointIdentification=false");
             Collections.addAll(command, Utils.getTestJavaOpts());
             command.add("-Dtest.src=" + TEST_SRC);
             command.add("-Dtest.rmi.port=" + port);
