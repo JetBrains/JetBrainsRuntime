774524f82c12a042c95ec317565f0d1fcd986525
bourgesl <bourges.laurent@gmail.com>
2025-09-15 07:55:43 +0200

    JBR-9351 jb/java/awt/Counters/UpdateWindowsCounter.java fails by time out
    Fixed Timers to be daemon

--- src/java.desktop/share/classes/sun/java2d/marlin/RendererStats.java
+++ src/java.desktop/share/classes/sun/java2d/marlin/RendererStats.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -391,7 +391,7 @@ public final class RendererStats implements MarlinConst {
             Runtime.getRuntime().addShutdownHook(hook);
 
             if (USE_DUMP_THREAD) {
-                final Timer statTimer = new Timer("RendererStats");
+                final Timer statTimer = new Timer("RendererStats", true);
                 statTimer.scheduleAtFixedRate(new TimerTask() {
                     @Override
                     public void run() {
