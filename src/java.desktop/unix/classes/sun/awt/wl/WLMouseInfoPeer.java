2e17737dd1d1669a68ec41c3d9e7d2708f15910d
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2025-09-18 17:07:55 +0400

    JBR-6990 Wayland: utilize relative-pointer-unstable-v1 protocol

--- src/java.desktop/unix/classes/sun/awt/wl/WLMouseInfoPeer.java
+++ src/java.desktop/unix/classes/sun/awt/wl/WLMouseInfoPeer.java
@@ -24,11 +24,16 @@
  */
 package sun.awt.wl;
 
+import sun.awt.RelativePointerMovementInfoProvider;
+
 import java.awt.Point;
 import java.awt.Window;
 import java.awt.peer.MouseInfoPeer;
 
-public class WLMouseInfoPeer implements MouseInfoPeer  {
+public class WLMouseInfoPeer implements MouseInfoPeer, RelativePointerMovementInfoProvider {
+    private final Object mouseDeltaLock = new Object();
+    private double mouseDeltaX = 0.0;
+    private double mouseDeltaY = 0.0;
 
     @Override
     public int fillPointWithCoords(Point point) {
@@ -55,4 +60,22 @@ public class WLMouseInfoPeer implements MouseInfoPeer  {
         return HOLDER.instance;
     }
 
+    void accumulatePointerDelta(double dx, double dy) {
+        synchronized (mouseDeltaLock) {
+            mouseDeltaX += dx;
+            mouseDeltaY += dy;
+        }
+    }
+
+    public Point getAccumulatedMouseDeltaAndReset() {
+        var p = new Point();
+        synchronized (mouseDeltaLock) {
+            int idx = (int) mouseDeltaX;
+            int idy = (int) mouseDeltaY;
+            p.setLocation(idx, idy);
+            mouseDeltaX = mouseDeltaX - idx;
+            mouseDeltaY = mouseDeltaY - idy;
+        }
+        return p;
+    }
 }
