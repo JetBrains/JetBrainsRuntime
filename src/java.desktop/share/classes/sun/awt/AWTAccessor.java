6893d02f69924c84d6741ceb24d49ce7e33c713e
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2025-09-22 15:42:46 +0200

    JBR-9301 Vulkan: SwingSet2 crash window server
    Clear native peer on windowClosing in Frame object
    
    (cherry picked from commit 441bb9d12ebff4bf2e1629115b9414b0b22ec858)

--- src/java.desktop/share/classes/sun/awt/AWTAccessor.java
+++ src/java.desktop/share/classes/sun/awt/AWTAccessor.java
@@ -37,6 +37,7 @@ import java.awt.event.InputEvent;
 import java.awt.event.InvocationEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowListener;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferStrategy;
 import java.awt.peer.ComponentPeer;
@@ -357,6 +358,8 @@ public final class AWTAccessor {
         double getCounterPerSecond(Window w, String counterName);
 
         void dumpStats(Window w, boolean reset, StringBuilder sb);
+
+        void addWindowListener(Window w, WindowListener listener);
     }
 
     /**
