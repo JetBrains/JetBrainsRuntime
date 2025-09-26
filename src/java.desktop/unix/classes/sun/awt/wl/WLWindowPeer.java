7e0a36af0782ac9db10b9f946a82daa6a02c0644
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2025-09-26 16:00:09 +0400

    JBR-9384 Wayland: ShowPopupAfterHidePopupTest.java fails

--- src/java.desktop/unix/classes/sun/awt/wl/WLWindowPeer.java
+++ src/java.desktop/unix/classes/sun/awt/wl/WLWindowPeer.java
@@ -33,6 +33,7 @@ import sun.java2d.wl.WLSMSurfaceData;
 
 import javax.swing.JRootPane;
 import javax.swing.RootPaneContainer;
+import java.awt.AWTEvent;
 import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Component;
@@ -101,7 +102,7 @@ public class WLWindowPeer extends WLComponentPeer implements WindowPeer, Surface
 
     @Override
     protected void wlSetVisible(boolean v) {
-        if (!v) ungrab();
+        if (!v) ungrab(true);
 
         if (v && targetIsWlPopup() && shouldBeFocusedOnShowing()) {
             requestWindowFocus();
@@ -206,7 +207,7 @@ public class WLWindowPeer extends WLComponentPeer implements WindowPeer, Surface
 
     @Override
     public void dispose() {
-        ungrab();
+        ungrab(true);
         resetCornerMasks();
         super.dispose();
     }
@@ -248,20 +249,29 @@ public class WLWindowPeer extends WLComponentPeer implements WindowPeer, Surface
 
     public void grab() {
         if (grabbingWindow != null && !isGrabbing()) {
-            grabbingWindow.ungrab();
+            grabbingWindow.ungrab(false);
         }
         grabbingWindow = this;
     }
 
-    public void ungrab() {
+    public void ungrab(boolean externalUngrab) {
         if (isGrabbing()) {
             grabbingWindow = null;
-            WLToolkit.postEvent(new UngrabEvent(getTarget()));
+            if (externalUngrab) {
+                WLToolkit.postEvent(new UngrabEvent(getTarget()));
+            }
         }
     }
 
     private boolean isGrabbing() {
         return this == grabbingWindow;
+    }
+
+    @Override
+    void handleJavaWindowFocusEvent(AWTEvent e) {
+        if (e.getID() == WindowEvent.WINDOW_LOST_FOCUS) {
+            ungrab(true);
+        }
     }
 
     @Override
