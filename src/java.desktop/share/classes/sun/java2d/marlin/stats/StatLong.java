ec52942ff54e574f5ca499a3583e7542bea53a53
bourgesl <bourges.laurent@gmail.com>
2025-09-26 16:17:43 +0200

    JBR-9408 Fix Marlin renderer statistics
    Revert JBR-9283 changes to StatLong + fixed Long constants

--- src/java.desktop/share/classes/sun/java2d/marlin/stats/StatLong.java
+++ src/java.desktop/share/classes/sun/java2d/marlin/stats/StatLong.java
@@ -31,11 +31,13 @@ package sun.java2d.marlin.stats;
 public class StatLong {
 
     public final String name;
-    public long count, sum, min, max;
+    public long count = 0L;
+    public long sum = 0L;
+    public long min = Long.MAX_VALUE;
+    public long max = Long.MIN_VALUE;
 
     public StatLong(final String name) {
         this.name = name;
-        reset();
     }
 
     public void reset() {
@@ -73,10 +75,12 @@ public class StatLong {
     }
 
     public final StringBuilder toString(final StringBuilder sb) {
-        sb.append(name).append('[').append(count);
-        sb.append("] sum: ").append(sum).append(" avg: ");
-        sb.append(trimTo3Digits(((double) sum) / count));
-        sb.append(" [").append(min).append(" - ").append(max).append("]");
+        sb.append(name).append('[').append(count).append("]");
+        if (count != 0L) {
+            sb.append(" sum: ").append(sum).append(" avg: ");
+            sb.append(trimTo3Digits(((double) sum) / count));
+            sb.append(" [").append(min).append(" | ").append(max).append("]");
+        }
         return sb;
     }
 
@@ -87,7 +91,7 @@ public class StatLong {
      * @return double value with only 3 decimal digits
      */
     public static double trimTo3Digits(final double value) {
-        return Double.isFinite(value) ? ((long) (1e3d * value)) / 1e3d : Double.NaN;
+        return ((long) (1e3d * value)) / 1e3d;
     }
 }
 
