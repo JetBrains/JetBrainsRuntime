49496669ea49ac1fb4bc7159e80cc203af5a6bb9
bourgesl <bourges.laurent@gmail.com>
2025-09-29 10:12:07 +0200

    JDK-8341381 Random lines appear in graphic causing by the fix of JDK-8297230 - Fix cubic offsetting artefacts (sort cubic roots + fixed numerical accuracy problem in ROC^2-w^2 = 0 solver + fixed EliminateInf) - Restored lower precision using ulp(float) in point, line or flat bezier curve checks
    (cherry picked from commit e72b87e6538dda97e6f0f2840040c6864b3f146e)

--- src/java.desktop/share/classes/sun/java2d/marlin/DMarlinRenderingEngine.java
+++ src/java.desktop/share/classes/sun/java2d/marlin/DMarlinRenderingEngine.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -566,7 +566,7 @@ public final class DMarlinRenderingEngine extends RenderingEngine
     }
 
     private static boolean nearZero(final double num) {
-        return Math.abs(num) < 2.0d * Math.ulp(num);
+        return Math.abs(num) < 2.0d * Helpers.ulp(num);
     }
 
     abstract static class NormalizingPathIterator implements PathIterator {
