752dc830b4abfab0d9b59c7fa488de84a016c559
bourgesl <bourges.laurent@gmail.com>
2025-09-29 10:12:07 +0200

    JDK-8341381 Random lines appear in graphic causing by the fix of JDK-8297230 - Fix cubic offsetting artefacts (sort cubic roots + fixed numerical accuracy problem in ROC^2-w^2 = 0 solver + fixed EliminateInf) - Restored lower precision using ulp(float) in point, line or flat bezier curve checks

--- src/java.desktop/share/classes/sun/java2d/marlin/Version.java
+++ src/java.desktop/share/classes/sun/java2d/marlin/Version.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -27,7 +27,7 @@ package sun.java2d.marlin;
 
 public final class Version {
 
-    private static final String VERSION = "marlin-0.9.4.7-Unsafe-OpenJDK";
+    private static final String VERSION = "marlin-0.9.4.9-Unsafe-OpenJDK";
 
     public static String getVersion() {
         return VERSION;
