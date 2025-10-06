10fe9646704c19189a2619f7c428cf924bc87cd9
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2025-10-06 22:02:10 +0200

    JBR-9450 Vulkan: Unify pipelines

--- src/java.desktop/share/glsl/vulkan/common.glsl
+++ src/java.desktop/share/glsl/vulkan/common.glsl
@@ -1,6 +1,17 @@
 
+#ifdef STAGE_VERT
+layout(push_constant) uniform PushConstants { mat2x3 transform; } push;
+
+vec4 transformToDeviceSpace(vec2 v) {
+    return vec4(vec3(v, 1.0) * push.transform, 0.0, 1.0);
+}
+#endif
+
 #define ALPHA_TYPE_PRE_MULTIPLIED 0U
 #define ALPHA_TYPE_STRAIGHT       1U
+
+layout (constant_id = 0) const uint const_InAlphaType  = ALPHA_TYPE_PRE_MULTIPLIED;
+layout (constant_id = 1) const uint const_OutAlphaType = ALPHA_TYPE_PRE_MULTIPLIED;
 
 vec4 convertAlpha(vec4 color, uint inType, uint outType) {
     if (inType == ALPHA_TYPE_STRAIGHT && outType == ALPHA_TYPE_PRE_MULTIPLIED) {
@@ -10,11 +21,10 @@ vec4 convertAlpha(vec4 color, uint inType, uint outType) {
     } else return color;
 }
 
-#ifdef ALPHA_TYPE_SPEC_INDEX
-layout (constant_id = ALPHA_TYPE_SPEC_INDEX  ) const uint const_InAlphaType  = ALPHA_TYPE_PRE_MULTIPLIED;
-layout (constant_id = ALPHA_TYPE_SPEC_INDEX+1) const uint const_OutAlphaType = ALPHA_TYPE_PRE_MULTIPLIED;
-
 vec4 convertAlpha(vec4 color) {
     return convertAlpha(color, const_InAlphaType, const_OutAlphaType);
 }
-#endif
+
+vec4 decodeColor(uint srgb) {
+    return vec4((uvec4(srgb) >> uvec4(16, 8, 0, 24)) & 0xFFU) / 255.0;
+}
