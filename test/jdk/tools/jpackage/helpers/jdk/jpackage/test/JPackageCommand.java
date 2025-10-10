311c5a6df01aa29e6be009ae89f1154a0a725546
Goetz Lindenmaier <goetz@openjdk.org>
2025-10-10 09:16:01 +0000

    8350102: Decouple jpackage test-lib Executor.Result and Executor classes
    Backport-of: 3487f8cbd55b06d332d897a010ae8eb371dd4956

--- test/jdk/tools/jpackage/helpers/jdk/jpackage/test/JPackageCommand.java
+++ test/jdk/tools/jpackage/helpers/jdk/jpackage/test/JPackageCommand.java
@@ -59,7 +59,7 @@ import jdk.jpackage.test.Functional.ThrowingSupplier;
  * anything. The simplest is to compile test application and pack in a jar for
  * use on jpackage command line.
  */
-public final class JPackageCommand extends CommandArguments<JPackageCommand> {
+public class JPackageCommand extends CommandArguments<JPackageCommand> {
 
     public JPackageCommand() {
         prerequisiteActions = new Actions();
@@ -791,7 +791,7 @@ public final class JPackageCommand extends CommandArguments<JPackageCommand> {
                 .createExecutor()
                 .execute(expectedExitCode);
 
-        if (result.exitCode == 0) {
+        if (result.exitCode() == 0) {
             executeVerifyActions();
         }
 
