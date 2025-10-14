ee03f66b52df1097c0a6d0bce00dd79bda71411b
Goetz Lindenmaier <goetz@openjdk.org>
2025-10-14 14:15:13 +0000

    8343875: Minor improvements of jpackage test library
    Reviewed-by: mbaesken
    Backport-of: 95a00f8a188048952871a10dc428566b18b91cb8

--- test/jdk/tools/jpackage/helpers/jdk/jpackage/test/TKit.java
+++ test/jdk/tools/jpackage/helpers/jdk/jpackage/test/TKit.java
@@ -23,7 +23,6 @@
 package jdk.jpackage.test;
 
 import java.io.Closeable;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -107,11 +106,18 @@ final public class TKit {
             ThrowingRunnable.toRunnable(action).run();
         } else {
             try (PrintStream logStream = openLogStream()) {
-                extraLogStream = logStream;
-                ThrowingRunnable.toRunnable(action).run();
-            } finally {
-                extraLogStream = null;
+                withExtraLogStream(action, logStream);
             }
+        }
+    }
+
+    static void withExtraLogStream(ThrowingRunnable action, PrintStream logStream) {
+        var oldExtraLogStream = extraLogStream;
+        try {
+            extraLogStream = logStream;
+            ThrowingRunnable.toRunnable(action).run();
+        } finally {
+            extraLogStream = oldExtraLogStream;
         }
     }
 
@@ -187,11 +193,7 @@ final public class TKit {
     }
 
     public static boolean isLinuxAPT() {
-        if (!isLinux()) {
-            return false;
-        }
-        File aptFile = new File("/usr/bin/apt-get");
-        return aptFile.exists();
+        return isLinux() && Files.exists(Path.of("/usr/bin/apt-get"));
     }
 
     private static String addTimestamp(String msg) {
@@ -273,6 +275,22 @@ final public class TKit {
     public static void error(String v) {
         log("ERROR: " + v);
         throw new AssertionError(v);
+    }
+
+    static void assertAssert(boolean expectedSuccess, Runnable runnable) {
+        try {
+            runnable.run();
+        } catch (AssertionError err) {
+            if (expectedSuccess) {
+                assertUnexpected("Assertion failed");
+            } else {
+                return;
+            }
+        }
+
+        if (!expectedSuccess) {
+            assertUnexpected("Assertion passed");
+        }
     }
 
     private final static String TEMP_FILE_PREFIX = null;
@@ -580,7 +598,7 @@ final public class TKit {
                     msg));
         }
 
-        traceAssert(String.format("assertEquals(%d): %s", expected, msg));
+        traceAssert(concatMessages(String.format("assertEquals(%d)", expected), msg));
     }
 
     public static void assertNotEquals(long expected, long actual, String msg) {
@@ -590,8 +608,8 @@ final public class TKit {
                     msg));
         }
 
-        traceAssert(String.format("assertNotEquals(%d, %d): %s", expected,
-                actual, msg));
+        traceAssert(concatMessages(String.format("assertNotEquals(%d, %d)", expected,
+                actual), msg));
     }
 
     public static void assertEquals(String expected, String actual, String msg) {
@@ -603,7 +621,7 @@ final public class TKit {
                     msg));
         }
 
-        traceAssert(String.format("assertEquals(%s): %s", expected, msg));
+        traceAssert(concatMessages(String.format("assertEquals(%s)", expected), msg));
     }
 
     public static void assertNotEquals(String expected, String actual, String msg) {
@@ -611,8 +629,8 @@ final public class TKit {
         if ((actual != null && !actual.equals(expected))
                 || (expected != null && !expected.equals(actual))) {
 
-            traceAssert(String.format("assertNotEquals(%s, %s): %s", expected,
-                actual, msg));
+            traceAssert(concatMessages(String.format("assertNotEquals(%s, %s)", expected,
+                actual), msg));
             return;
         }
 
@@ -626,7 +644,7 @@ final public class TKit {
                     value), msg));
         }
 
-        traceAssert(String.format("assertNull(): %s", msg));
+        traceAssert(concatMessages("assertNull()", msg));
     }
 
     public static void assertNotNull(Object value, String msg) {
@@ -635,7 +653,7 @@ final public class TKit {
             error(concatMessages("Unexpected null value", msg));
         }
 
-        traceAssert(String.format("assertNotNull(%s): %s", value, msg));
+        traceAssert(concatMessages(String.format("assertNotNull(%s)", value), msg));
     }
 
     public static void assertTrue(boolean actual, String msg) {
@@ -655,7 +673,7 @@ final public class TKit {
             error(concatMessages("Failed", msg));
         }
 
-        traceAssert(String.format("assertTrue(): %s", msg));
+        traceAssert(concatMessages("assertTrue()", msg));
     }
 
     public static void assertFalse(boolean actual, String msg, Runnable onFail) {
@@ -667,7 +685,7 @@ final public class TKit {
             error(concatMessages("Failed", msg));
         }
 
-        traceAssert(String.format("assertFalse(): %s", msg));
+        traceAssert(concatMessages("assertFalse()", msg));
     }
 
     public static void assertPathExists(Path path, boolean exists) {
@@ -826,7 +844,7 @@ final public class TKit {
             List<String> actual, String msg) {
         currentTest.notifyAssert();
 
-        traceAssert(String.format("assertStringListEquals(): %s", msg));
+        traceAssert(concatMessages("assertStringListEquals()", msg));
 
         String idxFieldFormat = Functional.identity(() -> {
             int listSize = expected.size();
@@ -856,7 +874,7 @@ final public class TKit {
                     expectedStr));
         });
 
-        if (expected.size() < actual.size()) {
+        if (actual.size() > expected.size()) {
             // Actual string list is longer than expected
             error(concatMessages(String.format(
                     "Actual list is longer than expected by %d elements",
@@ -866,7 +884,7 @@ final public class TKit {
         if (actual.size() < expected.size()) {
             // Actual string list is shorter than expected
             error(concatMessages(String.format(
-                    "Actual list is longer than expected by %d elements",
+                    "Actual list is shorter than expected by %d elements",
                     expected.size() - actual.size()), msg));
         }
     }
