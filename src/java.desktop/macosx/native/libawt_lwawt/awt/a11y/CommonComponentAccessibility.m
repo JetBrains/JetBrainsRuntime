e7404bfdefe10029a451464de1f75df9a4a81df2
bourgesl <bourges.laurent@gmail.com>
2025-10-17 17:37:38 +0200

    JBR-8651 Pycharm Crashing after lock/sleep: SIGABRT at # C [libsystem_kernel.dylib+0x9388] __pthread_kill / __displaycb_handle_block_invoke
    Rewritten exception handling to adopt the improved NSApplicationAWT exception handler (log all exceptions, avoid crash GUI)
    Fixed JNI_COCOA_EXIT(env) usages to test also pending JNI exceptions
    Added JNI_COCOA_EXIT_FATAL(message) used by NSApplicationAWT.sendEvent to report a crash with full details
    Unified logException to report both crash and exceptions
    Added isAWTCrashOnException() using the system property 'apple.awt.crashOnException' to crash on any exception occuring in NSApplication level
    Added missing CHECK_EXCEPTION after (*env)->Call...Method()
    Intercept all exception in NSApplicationAWT as NSExceptionHandlerDelegate + added tests in LWCToolkit (native) and Java code to test all exceptions are reported in logs and caught properly

--- src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/CommonComponentAccessibility.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/CommonComponentAccessibility.m
@@ -1339,6 +1339,7 @@ static void treeNodeExpandedCollapsedImpl(
         const jobject cAccessibleGlobal = (*env)->NewGlobalRef(env, cAccessible);
 
         if ((*env)->ExceptionCheck(env) == JNI_TRUE) {
+            (*env)->ExceptionDescribe(env);
             if (cAccessibleGlobal != NULL) {
                 (*env)->DeleteGlobalRef(env, cAccessibleGlobal);
             }
@@ -1527,7 +1528,7 @@ void nativeAnnounceAppKit(
 
     JNIEnv* const env = [ThreadUtilities getJNIEnv];
     if (env == NULL) { // unlikely
-        NSLog(@"%s: failed to get JNIEnv instance\n%@\n", __func__, [NSThread callStackSymbols]);
+        NSAPP_AWT_LOG_MESSAGE(@"Failed to get JNIEnv instance");
         return; // I believe it's dangerous to go on announcing in that case
     }
 
