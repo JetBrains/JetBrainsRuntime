7d3d0242097c29cc40406bfbda63b2a15f347fbd
Nikita Tsarev <nikita.tsarev@jetbrains.com>
2025-10-22 11:03:28 +0200

    JBR-9527: Fix NPE with WLDataDevice.performDeletionsOnEDT() when headless [WLToolkit]

--- src/java.desktop/unix/classes/sun/awt/wl/WLToolkit.java
+++ src/java.desktop/unix/classes/sun/awt/wl/WLToolkit.java
@@ -276,7 +276,9 @@ public class WLToolkit extends UNIXToolkit implements Runnable {
                     WLToolkit.awtLock();
                     try {
                         dispatchEventsOnEDT();
-                        dataDevice.performDeletionsOnEDT();
+                        if (dataDevice != null) {
+                            dataDevice.performDeletionsOnEDT();
+                        }
                     } finally {
                         eventsQueued.release();
                         WLToolkit.awtUnlock();
