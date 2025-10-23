59b38df17e89fe25fbe074c15c67fbea641bbc75
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2025-10-23 17:55:53 +0200

    JBR-9531 Prevent unexpected recursive usage of java.io over nio wrappers
    Shortly said, we don't need to handle recursive invocations of the `java.io` to nio adapters, while this recursion leads to problems.
    
    # Problem 
    
    Since we're not allowed to modify public API of Java classes, an unreliable workaround `IoOverNio.PARENT_FOR_FILE_CHANNEL_IMPL` had been introduced. This trick lets us pass some object into a called function without adding a new function argument.
    
    The trick backfired at the following code:
    
    ```java
    // at java.base/java.io.IoOverNioFileSystem.initializeStreamUsingNio (simplified code)
    IoOverNio.PARENT_FOR_FILE_CHANNEL_IMPL.set(owner);
    return initializeStreamsUsingNio0(owner, nioFs, file, nioPath, optionsForChannel, channelCleanable);
    
    
    // at java.base/java.io.IoOverNioFileSystem.initializeStreamsUsingNio0
    channel = nioFs.provider().newFileChannel(nioPath, optionsForChannel);
    ```
    
    The intention of setting `PARENT_FOR_FILE_CHANNEL_IMPL` is to path `owner` inside a constructor of `sun.nio.ch.FileChannelImpl` which may be called by `newFileChannel(nioPath, optionsForChannel)`.
    
    The implementation of `newFileChannel` in IntelliJ triggered class loading. The classloader of IntelliJ triggered the call `java.nio.file.Files.readAllBytes`. The latter code also invoked the constructor of `FileChannelImpl`, and the constructor got from the thread-local variable the value for the field `parent`. That value was supposed to be passed to a different invocation of the constructor.
    
    The observable result of this bug was a `NullPointerExceptions` from `java.io.FileOutputStream.close`. Hypothetically, this bug could also lead to closing file descriptors earlier as expected in other unpredictable places.
    
    # Rejected approaches
    
    * The cleanest solution could be passing specific function arguments through function arguments. 
      
      However, we may neither create a new public function in the module `java.base`, nor add an argument to an existing one. Thus, we have to keep dealing with thread-local variables.
    * To hold several values in the thread-local variable. 
    
      In this case, the constructor of `FileChannelImpl` should somehow choose the right value from the list. The only possible way for filtering values is by the provided path. It doesn't look like a performant and reliable solution.
     
    # Chosen solution
     
    This commit disables recursive invocations of `java.io` from `java.nio` adapters.
      
    The chosen solution is tied to specifics of IntelliJ. The reason for introducing java.io over java.nio adapter was to be able to access files from remote machines without rewriting old code. It is not expected that an implementation of `java.nio.file.spi.FileSystemProvider` accesses the file system using `java.io`. The only imaginable way to get a `java.io` call from `java.nio` is class loading. In case of IntelliJ, it's assumed that the class loader never accesses classes and jars from a remote machine.

--- test/jdk/jb/java/io/IoOverNio/testNio/ManglingFileSystemProvider.java
+++ test/jdk/jb/java/io/IoOverNio/testNio/ManglingFileSystemProvider.java
@@ -23,19 +23,12 @@
 
 package testNio;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.channels.FileChannel;
 import java.nio.channels.SeekableByteChannel;
-import java.nio.file.AccessDeniedException;
-import java.nio.file.AccessMode;
-import java.nio.file.CopyOption;
-import java.nio.file.DirectoryStream;
-import java.nio.file.FileStore;
-import java.nio.file.FileSystem;
-import java.nio.file.LinkOption;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
+import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributeView;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.DosFileAttributeView;
@@ -48,6 +41,7 @@ import java.nio.file.spi.FileSystemProvider;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class ManglingFileSystemProvider extends FileSystemProvider {
     public static final String extraContent = "3x7r4";
@@ -168,6 +162,7 @@ public class ManglingFileSystemProvider extends FileSystemProvider {
 
     @Override
     public FileChannel newFileChannel(Path path, Set<? extends OpenOption> options, FileAttribute<?>... attrs) throws IOException {
+        triggerSporadicFileAccess();
         return new ManglingFileChannel(defaultProvider.newFileChannel(unwrap(path), options, attrs));
     }
 
@@ -178,11 +173,13 @@ public class ManglingFileSystemProvider extends FileSystemProvider {
 
     @Override
     public DirectoryStream<Path> newDirectoryStream(Path dir, DirectoryStream.Filter<? super Path> filter) throws IOException {
+        triggerSporadicFileAccess();
         return new ManglingDirectoryStream(manglingFs, (ManglingPath) dir, defaultProvider.newDirectoryStream(unwrap(dir), filter));
     }
 
     @Override
     public void createDirectory(Path dir, FileAttribute<?>... attrs) throws IOException {
+        triggerSporadicFileAccess();
         if (prohibitFileTreeModifications) {
             throw new AccessDeniedException(dir.toString(), null, "Test: All file tree modifications are prohibited");
         }
@@ -191,6 +188,7 @@ public class ManglingFileSystemProvider extends FileSystemProvider {
 
     @Override
     public void delete(Path path) throws IOException {
+        triggerSporadicFileAccess();
         if (prohibitFileTreeModifications) {
             throw new AccessDeniedException(path.toString(), null, "Test: All file tree modifications are prohibited");
         }
@@ -199,6 +197,7 @@ public class ManglingFileSystemProvider extends FileSystemProvider {
 
     @Override
     public void copy(Path source, Path target, CopyOption... options) throws IOException {
+        triggerSporadicFileAccess();
         if (prohibitFileTreeModifications) {
             throw new AccessDeniedException(source.toString(), null, "Test: All file tree modifications are prohibited");
         }
@@ -209,6 +208,7 @@ public class ManglingFileSystemProvider extends FileSystemProvider {
 
     @Override
     public void move(Path source, Path target, CopyOption... options) throws IOException {
+        triggerSporadicFileAccess();
         if (prohibitFileTreeModifications) {
             throw new AccessDeniedException(source.toString(), null, "Test: All file tree modifications are prohibited");
         }
@@ -219,6 +219,7 @@ public class ManglingFileSystemProvider extends FileSystemProvider {
 
     @Override
     public boolean isSameFile(Path path, Path path2) throws IOException {
+        triggerSporadicFileAccess();
         Path source2 = unwrap(path);
         Path target2 = unwrap(path2);
         return defaultProvider.isSameFile(source2, target2);
@@ -226,16 +227,19 @@ public class ManglingFileSystemProvider extends FileSystemProvider {
 
     @Override
     public boolean isHidden(Path path) throws IOException {
+        triggerSporadicFileAccess();
         return defaultProvider.isHidden(unwrap(path));
     }
 
     @Override
     public FileStore getFileStore(Path path) throws IOException {
+        triggerSporadicFileAccess();
         return defaultProvider.getFileStore(unwrap(path));
     }
 
     @Override
     public void checkAccess(Path path, AccessMode... modes) throws IOException {
+        triggerSporadicFileAccess();
         defaultProvider.checkAccess(unwrap(path), modes);
         if (denyAccessToEverything) {
             throw new AccessDeniedException(path.toString(), null, "Test: No access rules to anything");
@@ -244,11 +248,13 @@ public class ManglingFileSystemProvider extends FileSystemProvider {
 
     @Override
     public <V extends FileAttributeView> V getFileAttributeView(Path path, Class<V> type, LinkOption... options) {
+        triggerSporadicFileAccess();
         return wrap(defaultProvider.getFileAttributeView(unwrap(path), type, options));
     }
 
     @Override
     public <A extends BasicFileAttributes> A readAttributes(Path path, Class<A> type, LinkOption... options) throws IOException {
+        triggerSporadicFileAccess();
         return wrap(defaultProvider.readAttributes(unwrap(path), type, options));
     }
 
@@ -259,6 +265,7 @@ public class ManglingFileSystemProvider extends FileSystemProvider {
 
     @Override
     public void setAttribute(Path path, String attribute, Object value, LinkOption... options) throws IOException {
+        triggerSporadicFileAccess();
         if (prohibitFileTreeModifications) {
             throw new AccessDeniedException(path.toString(), null, "Test: All file tree modifications are prohibited");
         }
@@ -267,11 +274,13 @@ public class ManglingFileSystemProvider extends FileSystemProvider {
 
     @Override
     public boolean exists(Path path, LinkOption... options) {
+        triggerSporadicFileAccess();
         return defaultProvider.exists(unwrap(path), options);
     }
 
     @Override
     public void createSymbolicLink(Path link, Path target, FileAttribute<?>... attrs) throws IOException {
+        triggerSporadicFileAccess();
         if (prohibitFileTreeModifications) {
             throw new AccessDeniedException(link.toString(), null, "Test: All file tree modifications are prohibited");
         }
@@ -302,5 +311,27 @@ public class ManglingFileSystemProvider extends FileSystemProvider {
             return (V) new ManglingBasicFileAttributeView(basicView);
         }
         return view;
+    }
+
+    private static final AtomicLong counter = new AtomicLong();
+
+    static void triggerSporadicFileAccess() {
+        if (FileSystems.getDefault() != null) {
+            try {
+                var tempFile = new File(System.getProperty("java.io.tmpdir"), "test.tmp." + counter.incrementAndGet());
+                try {
+                    try (var out = new java.io.FileOutputStream(tempFile)) {
+                        out.write(1);
+                    }
+                    try (var in = new java.io.FileInputStream(tempFile)) {
+                        in.read();
+                    }
+                } finally {
+                    tempFile.delete();
+                }
+            } catch (Exception e) {
+                throw new Error(e);
+            }
+        }
     }
 }
