9e834100cb486300b9fb38ece10a496ab6624bf7
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2025-11-01 03:24:52 +0400

    JBR-8651 remove logging producing warnings in stderr

--- src/java.desktop/macosx/native/libosxapp/ThreadUtilities.m
+++ src/java.desktop/macosx/native/libosxapp/ThreadUtilities.m
@@ -370,7 +370,6 @@ AWT_ASSERT_APPKIT_THREAD;
             [ThreadUtilities resetTraceContext];
         }
     }
-    doLog([ThreadUtilities getJNIEnvUncached], "setAppkitThreadGroup: exit");
 }
 
 + (NSString*)getCaller:(NSString*)prefixSymbol {
