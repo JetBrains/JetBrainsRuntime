3058ed71fcc847e963dab254175fad6a8eda897e
Goetz Lindenmaier <goetz@openjdk.org>
2025-10-31 10:11:36 +0000

    7124287: [macosx] JTableHeader doesn't get focus after pressing F8 key
    Backport-of: 77b27893969d84de7c890a60c3cccb99e89d5d91

--- src/java.desktop/macosx/classes/com/apple/laf/AquaKeyBindings.java
+++ src/java.desktop/macosx/classes/com/apple/laf/AquaKeyBindings.java
@@ -383,7 +383,8 @@ public class AquaKeyBindings {
             "ENTER", "selectNextRowCell",
             "shift ENTER", "selectPreviousRowCell",
             "alt TAB", "focusHeader",
-            "alt shift TAB", "focusHeader"
+            "alt shift TAB", "focusHeader",
+            "F8", "focusHeader"
         }));
     }
 
