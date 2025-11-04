b75048f7bb97cb73d4d58ebad9414a3e423a8dcf
bourgesl <bourges.laurent@gmail.com>
2025-11-04 10:03:15 +0100

    Revert "JBR-8651 remove logging producing warnings in stderr"
    This reverts commit 0a9db6d343adbfbcec713176bb2170d1fcdb7a7b.

--- src/java.desktop/macosx/native/libosxapp/ThreadUtilities.m
+++ src/java.desktop/macosx/native/libosxapp/ThreadUtilities.m
@@ -370,6 +370,7 @@ AWT_ASSERT_APPKIT_THREAD;
             [ThreadUtilities resetTraceContext];
         }
     }
+    doLog([ThreadUtilities getJNIEnvUncached], "setAppkitThreadGroup: exit");
 }
 
 + (NSString*)getCaller:(NSString*)prefixSymbol {
