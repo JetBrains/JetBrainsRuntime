0278e8ef5ac2ebbbff6d2b5398c483b7f33b22b0
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2025-11-14 13:12:42 +0400

    JBR-6187 Wayland: implement server-side decoration support
    Use -Dsun.awt.wl.WindowDecorationStyle=server to activate

--- src/java.desktop/unix/native/libawt_wlawt/WLToolkit.h
+++ src/java.desktop/unix/native/libawt_wlawt/WLToolkit.h
@@ -32,6 +32,8 @@
 #include "viewporter.h"
 #include "relative-pointer-unstable-v1.h"
 #include "text-input-unstable-v3.h"
+#include "xdg-decoration-unstable-v1.h"
+
 #include "jvm_md.h"
 #include "jni_util.h"
 
@@ -70,6 +72,7 @@ extern struct zwp_primary_selection_device_manager_v1 *zwp_selection_dm; // opti
 extern struct zxdg_output_manager_v1 *zxdg_output_manager_v1; // optional, check for NULL before use
 extern struct zwp_relative_pointer_manager_v1* relative_pointer_manager;
 extern struct zwp_text_input_manager_v3 *zwp_text_input_manager; // optional, check for NULL before use
+extern struct zxdg_decoration_manager_v1* xdg_decoration_manager; // optional, check for NULL before use
 
 JNIEnv *getEnv();
 
