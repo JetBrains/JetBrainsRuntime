a4538d5a655b49120fc4a37b412d4deee925c84c
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2026-01-16 09:54:53 +0400

    JBR-5466 jb/hotspot/JNIRefsInCrashLog.java: 'hs_err_42.txt' missing from stdout/stderr
    (cherry picked from commit e511c26794fde700b756636ad7be76c0f0e8999c)

--- src/hotspot/share/runtime/mutexLocker.cpp
+++ src/hotspot/share/runtime/mutexLocker.cpp
@@ -163,6 +163,8 @@ Monitor* JVMCI_lock                   = nullptr;
 Monitor* JVMCIRuntime_lock            = nullptr;
 #endif
 
+Mutex*   OOMEStacks_lock              = nullptr;
+
 // Only one RecursiveMutex
 RecursiveMutex* MultiArray_lock       = nullptr;
 
@@ -363,6 +365,7 @@ void mutex_init() {
 #endif
   MUTEX_DEFL(JvmtiThreadState_lock          , PaddedMutex  , VThreadTransition_lock); // Used by JvmtiThreadState/JvmtiEventController
   MUTEX_DEFL(SharedDecoder_lock             , PaddedMutex  , NmtVirtualMemory_lock);  // Must be lower than NmtVirtualMemory_lock due to MemTracker::print_containing_region
+  MUTEX_DEFN(OOMEStacks_lock                , PaddedMutex  , safepoint);
 
   // Allocate RecursiveMutex
   MultiArray_lock = new RecursiveMutex();
