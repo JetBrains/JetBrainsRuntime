f4975af371aa1d31635f756a57dd4e13849221a1
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2026-01-22 17:45:44 +0200

    JBR-9901 Move LWAWT classes to 'unix'

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformResponder.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformResponder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011–2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -36,9 +36,6 @@ import java.awt.event.InputEvent;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.KeyEvent;
 import java.util.HashMap;
-import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Translates NSEvents/NPCocoaEvents into AWT events.
