5c3075cfd2b17d8a70efb027be87ba272576a175
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2026-01-22 17:45:44 +0200

    JBR-9901 Move LWAWT classes to 'unix'

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CFLayer.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CFLayer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021–2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,7 +26,7 @@
 package sun.lwawt.macosx;
 
 import sun.java2d.SurfaceData;
-import sun.java2d.NullSurfaceData;
+
 import java.awt.GraphicsConfiguration;
 import java.awt.Rectangle;
 import java.awt.Transparency;
