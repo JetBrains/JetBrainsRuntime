e3a7361620ad656c861221ad465293991c38d3a9
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2026-01-26 13:02:12 +0200

    JBR-9901 Abstract macOS-specific logic around CGraphicsDevice in LWWindowPeer

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformWindow.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformWindow.java
@@ -34,6 +34,7 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Frame;
+import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.Insets;
@@ -683,6 +684,17 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
     @Override
     public void setMaximizedBounds(int x, int y, int w, int h) {
         execute(ptr -> nativeSetNSWindowStandardFrame(ptr, x, y, w, h));
+    }
+
+    @Override
+    public Rectangle getDefaultMaximizedBounds(GraphicsConfiguration config) {
+        Insets screenInsets = ((CGraphicsDevice) config.getDevice()).getScreenInsets();
+        Rectangle gcBounds = config.getBounds();
+        return new Rectangle(
+                gcBounds.x + screenInsets.left,
+                gcBounds.y + screenInsets.top,
+                gcBounds.width - screenInsets.left - screenInsets.right,
+                gcBounds.height - screenInsets.top - screenInsets.bottom);
     }
 
     private boolean isMaximized() {
