5bf8592b5a1ef5fc4c5f5f63ce6054d8ce487996
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2026-01-26 13:11:36 +0200

    JBR-9901 Abstract macOS-specific window handle calculation logic in LWWindowPeer

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformEmbeddedFrame.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformEmbeddedFrame.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011–2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -203,5 +203,10 @@ public final class CPlatformEmbeddedFrame implements PlatformWindow {
     @Override
     public boolean isUnderMouse() {
         throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public long getWindowHandle() {
+        return 0;
     }
 }
