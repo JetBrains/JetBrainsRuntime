2f968c87ec695f2f2969a2b2bd61b1bcf353fafc
Goetz Lindenmaier <goetz@openjdk.org>
2026-01-26 15:09:42 +0000

    8374178: Missing include in systemDictionary.cpp after JDK-8365526
    Backport-of: 9715e6da8355a103d9066bd15ce68b4773cbadcb

--- src/hotspot/share/classfile/systemDictionary.cpp
+++ src/hotspot/share/classfile/systemDictionary.cpp
@@ -55,6 +55,7 @@
 #include "memory/resourceArea.hpp"
 #include "memory/universe.hpp"
 #include "oops/access.inline.hpp"
+#include "oops/constantPool.inline.hpp"
 #include "oops/instanceKlass.hpp"
 #include "oops/klass.inline.hpp"
 #include "oops/method.inline.hpp"
