6862c960e0e1d4f471be21da221b8a8783def2ca
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2026-01-26 13:11:36 +0200

    Abstract macOS-specific window handle calculation logic in LWWindowPeer

--- src/java.desktop/macosx/classes/sun/lwawt/LWWindowPeer.java
+++ src/java.desktop/macosx/classes/sun/lwawt/LWWindowPeer.java
@@ -77,7 +77,6 @@ import sun.java2d.SurfaceData;
 import sun.java2d.loops.Blit;
 import sun.java2d.loops.CompositeType;
 import sun.java2d.pipe.Region;
-import sun.lwawt.macosx.CPlatformWindow;
 import sun.util.logging.PlatformLogger;
 
 public class LWWindowPeer
@@ -1444,12 +1443,7 @@ public class LWWindowPeer
      */
     @Override
     public long getWindowHandle() {
-        final long[] handle = new long[1];
-        PlatformWindow window = getPlatformWindow();
-        if (window instanceof CPlatformWindow) {
-            ((CPlatformWindow)window).execute(ptr -> handle[0] = ptr);
-        }
-        return handle[0];
+        return platformWindow.getWindowHandle();
     }
 
     @Override
