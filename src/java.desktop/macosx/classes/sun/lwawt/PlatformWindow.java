936db8c0eab6f1d4c5a79867477db3327ad54418
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2026-01-26 13:02:12 +0200

    JBR-9901 Abstract macOS-specific logic around CGraphicsDevice in LWWindowPeer

--- src/java.desktop/macosx/classes/sun/lwawt/PlatformWindow.java
+++ src/java.desktop/macosx/classes/sun/lwawt/PlatformWindow.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011–2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -70,6 +70,10 @@ public interface PlatformWindow {
      * Sets the maximized bounds.
      */
     public default void setMaximizedBounds(int x, int y, int w, int h){}
+
+    public default Rectangle getDefaultMaximizedBounds(GraphicsConfiguration config) {
+        return config.getBounds();
+    }
 
     /*
      * Returns the graphics device where the window is.
