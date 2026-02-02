d38b022d98c26c195ac07500872bb0f404d8a149
Kirill Shirokov <kshiroko@openjdk.org>
2026-02-02 07:34:18 +0000

    8330016: Stress seed should be initialized for runtime stub compilation
    Backport-of: 95d2f8072e91e8df80e49e341f4fdb4464a2616e

--- src/hotspot/share/opto/compile.hpp
+++ src/hotspot/share/opto/compile.hpp
@@ -1232,6 +1232,9 @@ class Compile : public Phase {
   int random();
   bool randomized_select(int count);
 
+  // seed random number generation and log the seed for repeatability.
+  void initialize_stress_seed(const DirectiveSet* directive);
+
   // supporting clone_map
   CloneMap&     clone_map();
   void          set_clone_map(Dict* d);
