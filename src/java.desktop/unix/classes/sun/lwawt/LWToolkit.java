c050867a9951468a845bc81ac1f50af4bbf3b8ae
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2026-02-11 14:39:07 +0200

    JBR-9903 Pass context component to updateCursorImmediately method

--- src/java.desktop/unix/classes/sun/lwawt/LWToolkit.java
+++ src/java.desktop/unix/classes/sun/lwawt/LWToolkit.java
@@ -552,7 +552,7 @@ public abstract class LWToolkit extends SunToolkit implements Runnable, ToolkitA
     }
 
     @Override
-    public void updateCursorImmediately() {
+    public void updateCursorImmediately(LWComponentPeerAPI peer) {
         getCursorManager().updateCursor();
     }
 
