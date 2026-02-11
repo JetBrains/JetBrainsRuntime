cd42b8fbfb647dd26eaa766ca46cd7e066b8185a
Nikita Tsarev <nikita.tsarev@jetbrains.com>
2026-02-11 15:04:09 +0100

    JBR-9480 macOS LWAWT: Reset drag images on exceptions

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CDragSourceContextPeer.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CDragSourceContextPeer.java
@@ -122,9 +122,11 @@ public final class CDragSourceContextPeer extends SunDragSourceContextPeer {
                 fDragCImage = CImage.getCreator().createFromImageImmediately(fDragImage);
             } catch(Exception e) {
                 // image creation may fail for any reason
+                resetDragImage();
                 throw new InvalidDnDOperationException("Drag image can not be created.");
             }
             if (fDragCImage == null) {
+                resetDragImage();
                 throw new InvalidDnDOperationException("Drag image is not ready.");
             }
 
@@ -156,6 +158,7 @@ public final class CDragSourceContextPeer extends SunDragSourceContextPeer {
         }
 
         catch (Exception e) {
+            resetDragImage();
             throw new InvalidDnDOperationException("failed to create native peer: " + e);
         }
 
@@ -176,11 +179,7 @@ public final class CDragSourceContextPeer extends SunDragSourceContextPeer {
                     e.printStackTrace();
                 } finally {
                     releaseNativeDragSource(nativeDragSource);
-                    fDragImage = null;
-                    if (fDragCImage != null) {
-                        fDragCImage.dispose();
-                        fDragCImage = null;
-                    }
+                    resetDragImage();
                 }
             };
             new Thread(null, dragRunnable, "Drag", 0, false).start();
@@ -189,6 +188,7 @@ public final class CDragSourceContextPeer extends SunDragSourceContextPeer {
             setNativeContext(0);
             releaseNativeDragSource(nativeDragSource);
             SunDropTargetContextPeer.setCurrentJVMLocalSourceTransferable(null);
+            resetDragImage();
             throw new InvalidDnDOperationException("failed to start dragging thread: " + e);
         }
     }
@@ -468,6 +468,15 @@ public final class CDragSourceContextPeer extends SunDragSourceContextPeer {
         }
 
         return null;
+    }
+
+    private void resetDragImage() {
+        fDragImage = null;
+        if (fDragCImage != null) {
+            fDragCImage.dispose();
+            fDragCImage = null;
+        }
+        fDragImageOffset = null;
     }
 
     /**
