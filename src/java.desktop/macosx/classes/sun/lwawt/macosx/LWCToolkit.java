bc826af3baf089f54548d2c435b97d053db36578
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2026-02-12 16:08:47 +0200

    JBR-9903 Abstract LWToolkit usages in LWWindowPeer via ToolkitAPI
    (cherry picked from commit e2de908ae0cb032a86fb27e1f856977bdd64c89a)

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/LWCToolkit.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/LWCToolkit.java
@@ -1202,7 +1202,7 @@ public final class LWCToolkit extends LWToolkit {
     }
 
     @Override
-    protected PlatformWindow getPlatformWindowUnderMouse() {
+    public PlatformWindow getPlatformWindowUnderMouse() {
         return CPlatformWindow.nativeGetTopmostPlatformWindowUnderMouse();
     }
 
