22535a2fe5e5db5c0db05660e16292d182d1678f
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2026-02-12 16:08:47 +0200

    Abstract LWToolkit usages in LWWindowPeer via ToolkitAPI

--- src/java.desktop/unix/classes/sun/lwawt/LWToolkit.java
+++ src/java.desktop/unix/classes/sun/lwawt/LWToolkit.java
@@ -377,8 +377,6 @@ public abstract class LWToolkit extends SunToolkit implements Runnable, ToolkitA
         return new LWMouseInfoPeer();
     }
 
-    protected abstract PlatformWindow getPlatformWindowUnderMouse();
-
     @Override
     public final PrintJob getPrintJob(Frame frame, String doctitle,
                                       Properties props) {
@@ -523,5 +521,10 @@ public abstract class LWToolkit extends SunToolkit implements Runnable, ToolkitA
     @Override
     public void updateCursorImmediately() {
         getCursorManager().updateCursor();
+    }
+
+    @Override
+    public void updateCursorLater(Window target) {
+        getCursorManager().updateCursorLater(target);
     }
 }
