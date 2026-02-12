5c92299a523a5140b14ee0ad6c51a6bec4709d00
Vishal Chand <vishalvc@amazon.com>
2026-02-12 10:33:52 +0000

    8369881: C2: Unexpected node in SuperWord truncation: ReverseBytesS, ReverseBytesUS
    Backport-of: aa194c6a5a21aca64d454e4c5eeed1464c8f190b

--- src/hotspot/share/opto/superword.cpp
+++ src/hotspot/share/opto/superword.cpp
@@ -2589,6 +2589,8 @@ static bool can_subword_truncate(Node* in, const Type* type) {
   case Op_RotateRight:
   case Op_RotateLeft:
   case Op_PopCountI:
+  case Op_ReverseBytesS:
+  case Op_ReverseBytesUS:
   case Op_ReverseBytesI:
   case Op_ReverseI:
   case Op_CountLeadingZerosI:
