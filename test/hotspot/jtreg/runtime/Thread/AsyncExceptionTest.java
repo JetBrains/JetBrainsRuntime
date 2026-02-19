3e5e447e9c187af155218330d6569338169d9267
Goetz Lindenmaier <goetz@openjdk.org>
2026-02-19 13:05:25 +0000

    8362834: Several runtime/Thread tests should mark as /native
    Reviewed-by: mdoerr
    Backport-of: 699b8112f8da7ceef2aa2a3ddb326aee88b29f8c

--- test/hotspot/jtreg/runtime/Thread/AsyncExceptionTest.java
+++ test/hotspot/jtreg/runtime/Thread/AsyncExceptionTest.java
@@ -27,7 +27,7 @@
  * @requires vm.compiler1.enabled | vm.compiler2.enabled
  * @summary Stress delivery of asynchronous exceptions.
  * @library /test/hotspot/jtreg/testlibrary
- * @run main/othervm -Xcomp
+ * @run main/othervm/native -Xcomp
                      -XX:CompileCommand=dontinline,AsyncExceptionTest::internalRun2
                      -XX:CompileCommand=compileonly,AsyncExceptionTest::internalRun1
                      -XX:CompileCommand=compileonly,AsyncExceptionTest::internalRun2
