79531009c262d2c328ead5dcdd9d02daa3286704
Goetz Lindenmaier <goetz@openjdk.org>
2026-02-19 13:05:25 +0000

    8362834: Several runtime/Thread tests should mark as /native
    Reviewed-by: mdoerr
    Backport-of: 699b8112f8da7ceef2aa2a3ddb326aee88b29f8c

--- test/hotspot/jtreg/runtime/Thread/TestBreakSignalThreadDump.java
+++ test/hotspot/jtreg/runtime/Thread/TestBreakSignalThreadDump.java
@@ -28,7 +28,7 @@
  * @summary Check that Ctrl-\ or Ctrl-Break (on Windows) causes HotSpot VM to print a full thread dump.
  * @library /vmTestbase
  *          /test/lib
- * @run driver TestBreakSignalThreadDump
+ * @run main/native TestBreakSignalThreadDump
  */
 
 /*
@@ -40,7 +40,7 @@
  * @requires !vm.asan
  * @library /vmTestbase
  *          /test/lib
- * @run driver TestBreakSignalThreadDump load_libjsig
+ * @run main/native TestBreakSignalThreadDump load_libjsig
  */
 
 import java.nio.file.Files;
