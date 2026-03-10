89d0a25f3325ed59e331c45fb20283231b452d16
Nikita Provotorov <nikita.provotorov@jetbrains.com>
2026-03-10 02:15:31 +0100

    JBR-9736 Wayland: assertion error in text_input_unstable_v3.ClientComponentCaretPositionTracker.stopTrackingCurrentComponent
    Fix the copyright notice of WLInputMethodZwpTextInputV3.java.

--- src/java.desktop/unix/classes/sun/awt/wl/im/text_input_unstable_v3/WLInputMethodZwpTextInputV3.java
+++ src/java.desktop/unix/classes/sun/awt/wl/im/text_input_unstable_v3/WLInputMethodZwpTextInputV3.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025-2026 JetBrains s.r.o.
+ * Copyright 2025, 2026 JetBrains s.r.o.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
