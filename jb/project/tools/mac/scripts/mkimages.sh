f0283e6735a7e59f547f2178b3c58c0849f9b859
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2026-03-30 15:38:30 +0400

    JBR-10050 Move CEF framework inside cef_server.app

--- jb/project/tools/mac/scripts/mkimages.sh
+++ jb/project/tools/mac/scripts/mkimages.sh
@@ -106,7 +106,17 @@ function create_image_bundle {
   cp -R "$JSDK"/../MacOS "$JRE_CONTENTS"
   cp "$JSDK"/../Info.plist "$JRE_CONTENTS"
 
-  [ -n "$bundle_type" ] && (cp -a $JCEF_PATH/Frameworks "$JRE_CONTENTS" || do_exit $?)
+  if [ "$bundle_type" == "jcef" ]; then
+    mkdir -p "$JRE_CONTENTS/Frameworks"
+    cp -a $JCEF_PATH/Frameworks/cef_server.app                          "$JRE_CONTENTS/Frameworks" || do_exit $?
+    cp -a "$JCEF_PATH/Frameworks/Chromium Embedded Framework.framework" "$JRE_CONTENTS/Frameworks/cef_server.app/Contents/Frameworks" || do_exit $?
+    cp -a "$JCEF_PATH/Frameworks/jcef Helper (GPU).app"                 "$JRE_CONTENTS/Frameworks/cef_server.app/Contents/Frameworks" || do_exit $?
+    cp -a "$JCEF_PATH/Frameworks/jcef Helper (Plugin).app"              "$JRE_CONTENTS/Frameworks/cef_server.app/Contents/Frameworks" || do_exit $?
+    cp -a "$JCEF_PATH/Frameworks/jcef Helper (Renderer).app"            "$JRE_CONTENTS/Frameworks/cef_server.app/Contents/Frameworks" || do_exit $?
+    cp -a "$JCEF_PATH/Frameworks/jcef Helper.app"                       "$JRE_CONTENTS/Frameworks/cef_server.app/Contents/Frameworks" || do_exit $?
+  else
+    [ -n "$bundle_type" ] && (cp -a $JCEF_PATH/Frameworks "$JRE_CONTENTS" || do_exit $?)
+  fi
 
   echo Creating "$JBR".tar.gz ...
   # Normalize timestamp
