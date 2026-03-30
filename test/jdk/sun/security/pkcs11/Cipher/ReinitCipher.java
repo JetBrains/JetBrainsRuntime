61ddd7e69cb2059669b8f9f3b38dd69be78a417d
Goetz Lindenmaier <goetz@openjdk.org>
2026-03-30 08:09:32 +0000

    8365863: /test/jdk/sun/security/pkcs11/Cipher tests skip without SkippedException
    Reviewed-by: rschmelter
    Backport-of: 993babb326f937dc1630a5a8fa5e469a64c51206

--- test/jdk/sun/security/pkcs11/Cipher/ReinitCipher.java
+++ test/jdk/sun/security/pkcs11/Cipher/ReinitCipher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -33,6 +33,8 @@
  * @run main/othervm -Djava.security.manager=allow ReinitCipher sm
  */
 
+import jtreg.SkippedException;
+
 import java.security.Provider;
 import java.util.Random;
 import javax.crypto.Cipher;
@@ -47,8 +49,7 @@ public class ReinitCipher extends PKCS11Test {
     @Override
     public void main(Provider p) throws Exception {
         if (p.getService("Cipher", "ARCFOUR") == null) {
-            System.out.println("Not supported by provider, skipping");
-            return;
+            throw new SkippedException("Algorithm ARCFOUR is not supported by provider, skipping");
         }
         Random random = new Random();
         byte[] data1 = new byte[10 * 1024];
