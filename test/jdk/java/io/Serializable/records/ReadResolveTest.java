e9066a64513960e5dad95c28e1ed11feb4b585e6
Roland Mesde <rmesde@openjdk.org>
2026-04-13 16:53:35 +0000

    8373623: Refactor Serialization tests for Records to JUnit
    Reviewed-by: phh
    Backport-of: 1e357e9e976bfb0abc9d4e14bfb1572693622af8

--- test/jdk/java/io/Serializable/records/ReadResolveTest.java
+++ test/jdk/java/io/Serializable/records/ReadResolveTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,8 +25,8 @@
  * @test
  * @bug 8246774
  * @summary Basic tests for readResolve
- * @run testng ReadResolveTest
- * @run testng/othervm/java.security.policy=empty_security.policy ReadResolveTest
+ * @run junit ReadResolveTest
+ * @run junit/othervm/java.security.policy=empty_security.policy ReadResolveTest
  */
 
 import java.io.ByteArrayInputStream;
@@ -36,15 +36,19 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serial;
 import java.io.Serializable;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
 import static java.lang.String.format;
 import static java.lang.System.out;
-import static org.testng.Assert.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 /**
  * Tests records being used as a serial proxy.
  */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ReadResolveTest {
 
     static class C1 implements Serializable {
@@ -95,7 +99,6 @@ public class ReadResolveTest {
         }
     }
 
-    @DataProvider(name = "objectsToSerialize")
     public Object[][] objectsToSerialize() {
         return new Object[][] {
                 new Object[] { new C1(3,4)        },
@@ -104,13 +107,14 @@ public class ReadResolveTest {
         };
     }
 
-    @Test(dataProvider = "objectsToSerialize")
+    @ParameterizedTest
+    @MethodSource("objectsToSerialize")
     public void testSerialize(Object objectToSerialize) throws Exception {
         out.println("\n---");
         out.println("serializing : " + objectToSerialize);
         Object deserializedObj = serializeDeserialize(objectToSerialize);
         out.println("deserialized: " + deserializedObj);
-        assertEquals(deserializedObj, objectToSerialize);
+        assertEquals(objectToSerialize, deserializedObj);
     }
 
     // -- null replacement
@@ -129,7 +133,7 @@ public class ReadResolveTest {
         out.println("serializing : " + objectToSerialize);
         Object deserializedObj = serializeDeserialize(objectToSerialize);
         out.println("deserialized: " + deserializedObj);
-        assertEquals(deserializedObj, null);
+        assertEquals(null, deserializedObj);
     }
 
     // --- infra
