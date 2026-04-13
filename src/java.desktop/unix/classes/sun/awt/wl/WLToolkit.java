f000e1773d405d5d69e08566111d9a9fce051e53
Nikita Tsarev <nikita.tsarev@jetbrains.com>
2026-04-13 18:21:35 +0200

    JBR-10255 Wayland: Grace region for multiclick

--- src/java.desktop/unix/classes/sun/awt/wl/WLToolkit.java
+++ src/java.desktop/unix/classes/sun/awt/wl/WLToolkit.java
@@ -143,6 +143,7 @@ public class WLToolkit extends UNIXToolkit implements Runnable, ToolkitAPI {
 
     private static final int MOUSE_BUTTONS_COUNT = 7;
     private static final int AWT_MULTICLICK_DEFAULT_TIME_MS = 500;
+    private static final int AWT_MULTICLICK_MOUSE_MOVE_THRESHOLD_JAVA_UNITS = 5;
 
     private static boolean initialized = false;
     private static Thread toolkitThread;
@@ -354,6 +355,10 @@ public class WLToolkit extends UNIXToolkit implements Runnable, ToolkitAPI {
     static int getMulticlickTime() {
         /* TODO: get from the system somehow */
         return AWT_MULTICLICK_DEFAULT_TIME_MS;
+    }
+
+    static int getMulticlickMouseMoveThresholdJavaUnits() {
+        return AWT_MULTICLICK_MOUSE_MOVE_THRESHOLD_JAVA_UNITS;
     }
 
     private static WLInputState inputState = WLInputState.initialState();
