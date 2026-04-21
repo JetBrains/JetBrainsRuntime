c734013e4d1b9a73f1cb647b8a25b7b49beb6382
Goetz Lindenmaier <goetz@openjdk.org>
2026-04-21 05:53:05 +0000

    8365623: test/jdk/sun/security/pkcs11/tls/ tests skipped without skip exception
    Backport-of: f7b5db1a5be2d8a56bb88c7df0451709ab68de49

--- test/jdk/sun/security/pkcs11/tls/TestPRF.java
+++ test/jdk/sun/security/pkcs11/tls/TestPRF.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -41,6 +41,8 @@ import java.util.Arrays;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+
+import jtreg.SkippedException;
 import sun.security.internal.spec.TlsPrfParameterSpec;
 
 public class TestPRF extends PKCS11Test {
@@ -54,8 +56,7 @@ public class TestPRF extends PKCS11Test {
     @Override
     public void main(Provider provider) throws Exception {
         if (provider.getService("KeyGenerator", "SunTlsPrf") == null) {
-            System.out.println("Provider does not support algorithm, skipping");
-            return;
+            throw new SkippedException("Provider does not support algorithm, skipping");
         }
 
         try (BufferedReader reader = Files.newBufferedReader(
