0ec698bb3de7a60935be2502ecf7ab10a19e6a11
William Kemper <wkemper@openjdk.org>
2026-05-08 17:15:53 +0000

    8380846: GenShen: Remove the experimental option to disable adaptive tenuring
    Backport-of: 4e7901576851ebe22b40349bf0e155b39ca6c34a

--- src/hotspot/share/gc/shenandoah/shenandoah_globals.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoah_globals.hpp
@@ -84,9 +84,6 @@
           "many consecutive young-gen collections have been "               \
           "completed following the preceding old-gen collection.")          \
                                                                             \
-  product(bool, ShenandoahGenerationalAdaptiveTenuring, true, EXPERIMENTAL, \
-          "(Generational mode only) Dynamically adapt tenuring age.")       \
-                                                                            \
   product(bool, ShenandoahGenerationalCensusIgnoreOlderCohorts, true,       \
                                                                EXPERIMENTAL,\
           "(Generational mode only) Ignore mortality rates older than the " \
@@ -111,8 +108,7 @@
           "(Generational mode only) Cohort mortality rates below this "     \
           "value will be treated as indicative of longevity, leading to "   \
           "tenuring. A lower value delays tenuring, a higher value hastens "\
-          "it. Used only when ShenandoahGenerationalhenAdaptiveTenuring is "\
-          "enabled.")                                                       \
+          "it.")                                                       \
           range(0.001,0.999)                                                \
                                                                             \
   product(size_t, ShenandoahGenerationalTenuringCohortPopulationThreshold,  \
