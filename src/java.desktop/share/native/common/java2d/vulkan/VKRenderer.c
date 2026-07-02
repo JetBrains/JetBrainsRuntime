b4b989a67ba8e364666cad79926d8b360ce8c81b
Anton Shangareev <anton.shangareev@jetbrains.com>
2026-07-02 17:26:51 +0400

    JBR-10448 Vulkan: synchronously update requestedExtent & requestedDevice

--- src/java.desktop/share/native/common/java2d/vulkan/VKRenderer.c
+++ src/java.desktop/share/native/common/java2d/vulkan/VKRenderer.c
@@ -1091,8 +1091,11 @@ void VKRenderer_BlitBackingImageContentOnto(VKSDOps *surface, VKImage *targetIma
 
 void VKRenderer_ConfigureSurface(VKSDOps* surface, VkExtent2D extent, VKDevice* device) {
     assert(surface != NULL);
-    surface->requestedExtent = extent;
-    surface->requestedDevice = device;
+
+    // Unused here, because we're updating these synchronously
+    (void) extent;
+    (void) device;
+
     // We must only do pending flush between frames.
     if (surface->renderPass != NULL && surface->renderPass->pendingFlush)  {
         if (surface->renderPass->pendingCommands) {
