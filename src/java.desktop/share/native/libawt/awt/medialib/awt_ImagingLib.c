85266f04db69909041f0d8f4cec8aff4168a8d14
Jan Kratochvil <jkratochvil@openjdk.org>
2026-07-07 16:11:06 +0000

    8381039: Enhance AWT ImagingLib
    Reviewed-by: andrew
    Backport-of: b07d52c9f8d7e8a91e5b4db5a13c08ee551513d3

--- src/java.desktop/share/native/libawt/awt/medialib/awt_ImagingLib.c
+++ src/java.desktop/share/native/libawt/awt/medialib/awt_ImagingLib.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -2216,7 +2216,11 @@ allocateArray(JNIEnv *env, BufImageS_t *imageP,
     /* Means we need to fill in alpha */
     if (!cvtToDefault && addAlpha) {
         *mlibImagePP = (*sMlibSysFns.createFP)(MLIB_BYTE, 4, width, height);
-        if (*mlibImagePP != NULL) {
+        if (*mlibImagePP == NULL) {
+            (*env)->ReleasePrimitiveArrayCritical(env, rasterP->jdata, dataP,
+                                                  JNI_ABORT);
+            return -1;
+        } else {
             unsigned int *dstP  = (unsigned int *)
                 mlib_ImageGetData(*mlibImagePP);
             int dstride = (*mlibImagePP)->stride>>2;
@@ -2232,10 +2236,10 @@ allocateArray(JNIEnv *env, BufImageS_t *imageP,
                     dP[x] = sP[x] | 0xff000000;
                 }
             }
+            (*env)->ReleasePrimitiveArrayCritical(env, rasterP->jdata, dataP,
+                                                  JNI_ABORT);
+            return 0;
         }
-        (*env)->ReleasePrimitiveArrayCritical(env, rasterP->jdata, dataP,
-                                              JNI_ABORT);
-        return 0;
     }
     else if ((hintP->packing & BYTE_INTERLEAVED) == BYTE_INTERLEAVED) {
         int nChans = (cmP->isDefaultCompatCM ? 4 : hintP->numChans);
@@ -2250,6 +2254,11 @@ allocateArray(JNIEnv *env, BufImageS_t *imageP,
                                               hintP->sStride,
                                               (unsigned char *)dataP
                                               + hintP->dataOffset);
+        if (*mlibImagePP == NULL) {
+            (*env)->ReleasePrimitiveArrayCritical(env, rasterP->jdata, dataP,
+                                                  JNI_ABORT);
+            return -1;
+        }
     }
     else if ((hintP->packing & SHORT_INTERLEAVED) == SHORT_INTERLEAVED) {
         *mlibImagePP = (*sMlibSysFns.createStructFP)(MLIB_SHORT,
@@ -2259,6 +2268,11 @@ allocateArray(JNIEnv *env, BufImageS_t *imageP,
                                               imageP->raster.scanlineStride*2,
                                               (unsigned short *)dataP
                                               + hintP->channelOffset);
+        if (*mlibImagePP == NULL) {
+            (*env)->ReleasePrimitiveArrayCritical(env, rasterP->jdata, dataP,
+                                                  JNI_ABORT);
+            return -1;
+        }
     }
     else {
         /* Release the data array */
@@ -2358,6 +2372,11 @@ allocateRasterArray(JNIEnv *env, RasterS_t *rasterP,
                                               width, height,
                                               rasterP->scanlineStride*4,
                                               (unsigned char *)dataP + offset);
+        if (*mlibImagePP == NULL) {
+            (*env)->ReleasePrimitiveArrayCritical(env, rasterP->jdata, dataP,
+                                                  JNI_ABORT);
+            return -1;
+        }
         *dataPP = dataP;
         return 0;
     case sun_awt_image_IntegerComponentRaster_TYPE_BYTE_SAMPLES:
@@ -2386,6 +2405,11 @@ allocateRasterArray(JNIEnv *env, RasterS_t *rasterP,
                                               width, height,
                                               rasterP->scanlineStride,
                                               (unsigned char *)dataP + offset);
+        if (*mlibImagePP == NULL) {
+            (*env)->ReleasePrimitiveArrayCritical(env, rasterP->jdata, dataP,
+                                                  JNI_ABORT);
+            return -1;
+        }
         *dataPP = dataP;
         return 0;
     case sun_awt_image_IntegerComponentRaster_TYPE_USHORT_SAMPLES:
@@ -2416,6 +2440,11 @@ allocateRasterArray(JNIEnv *env, RasterS_t *rasterP,
                                                      width, height,
                                                      rasterP->scanlineStride*2,
                                                      (unsigned char *)dataP + offset);
+        if (*mlibImagePP == NULL) {
+            (*env)->ReleasePrimitiveArrayCritical(env, rasterP->jdata, dataP,
+                                                  JNI_ABORT);
+            return -1;
+        }
         *dataPP = dataP;
         return 0;
 
