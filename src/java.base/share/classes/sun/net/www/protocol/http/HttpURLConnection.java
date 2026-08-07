ee4363e9feee2ca1ec267485fa60fcfaa65090ff
Alexey Bakhtin <abakhtin@openjdk.org>
2026-08-07 10:36:50 +0000

    8384708: Enhance HTTP Connections
    Reviewed-by: andrew
    Backport-of: ee475da6b8c53cd9383fdd45f1c57c75a0aa4b46

--- src/java.base/share/classes/sun/net/www/protocol/http/HttpURLConnection.java
+++ src/java.base/share/classes/sun/net/www/protocol/http/HttpURLConnection.java
@@ -365,6 +365,9 @@ public class HttpURLConnection extends java.net.HttpURLConnection {
     private boolean tryTransparentNTLMProxy = true;
     private boolean useProxyResponseCode = false;
 
+    // used when redirecting to compare current and previous proxies
+    private Proxy lastProxy;
+
     /* Used by Windows specific code */
     private Object authObj;
 
@@ -1370,7 +1373,6 @@ public class HttpURLConnection extends java.net.HttpURLConnection {
         // If the user has set either of these headers then do not remove them
         isUserServerAuth = requests.getKey("Authorization") != -1;
         isUserProxyAuth = requests.getKey("Proxy-Authorization") != -1;
-
         try {
             do {
                 if (!checkReuseConnection())
@@ -1380,6 +1382,14 @@ public class HttpURLConnection extends java.net.HttpURLConnection {
                     return cachedInputStream;
                 }
 
+                // we may need to remove proxy-authorization
+                Proxy p = http.getHttpProxy();
+                // if we're not using a proxy or if the proxy to be used is not
+                // the same as the originally set one, then remove it
+                if (p == null || (lastProxy != null && !lastProxy.equals(p))) {
+                    requests.remove("Proxy-Authorization");
+                    lastProxy = null;
+                }
                 /* REMIND: This exists to fix the HttpsURLConnection subclass.
                  * Hotjava needs to run on JDK1.1FCS.  Do proper fix once a
                  * proper solution for SSL can be found.
@@ -1410,7 +1420,7 @@ public class HttpURLConnection extends java.net.HttpURLConnection {
                     disconnectInternal();
                     throw new IOException ("Invalid Http response");
                 }
-                if (respCode == HTTP_PROXY_AUTH) {
+                if (respCode == HTTP_PROXY_AUTH && tunnelState() != TunnelState.TUNNELING) {
                     if (streaming()) {
                         disconnectInternal();
                         throw new HttpRetryException (
@@ -1962,6 +1972,7 @@ public class HttpURLConnection extends java.net.HttpURLConnection {
 
                 if (respCode == HTTP_OK) {
                     setTunnelState(TunnelState.TUNNELING);
+                    savedRequests.remove("Proxy-Authorization");
                     break;
                 }
                 // we don't know how to deal with other response code
@@ -2480,6 +2491,7 @@ public class HttpURLConnection extends java.net.HttpURLConnection {
     {
         assert isLockHeldByCurrentThread();
 
+        lastProxy = http.getHttpProxy();
         disconnectInternal();
         if (streaming()) {
             throw new HttpRetryException (RETRY_MSG3, stat, loc);
